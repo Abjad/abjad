74d39ddb7d737a1574db8a3bdc4f52ca375e1be5
root <root>
2008-01-18 02:32:55 +0000

    Changed leaf/leafspanner.py to leaf/leafspannerinterface.py.

--- trunk/abjad/containers/containerspanner.py
+++ trunk/abjad/containers/containerspanner.py
@@ -5,7 +5,7 @@
 from .. helpers.hasname import hasname
 from .. core.interface import _Interface
 
-class ContainerSpannerInterface(_Interface):
+class SpannerInterface(_Interface):
 
    def __init__(self, client):
       _Interface.__init__(self, client, 'Spanner')
