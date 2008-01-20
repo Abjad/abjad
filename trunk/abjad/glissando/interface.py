00e2a8987be0551ece532dbef4adf6a30c2dd709
root <root>
2008-01-20 05:05:46 +0000

    Stripped _Parser from GlissandoInterface. Made GlissandoInterface call _Interface __init__ according to four-argument signature. Moved GlissandoInterface.clear( ) to _Interface.clear( ). Overrode GlissandoInterface.clear( ) to clear out GlissandoInterface._set (indicator of glissando set by hand). Move GlissandoInterface._before to _Interface._before.

--- trunk/abjad/glissando/interface.py
+++ trunk/abjad/glissando/interface.py
@@ -1,12 +1,12 @@
 from .. core.interface import _Interface
-from .. core.parser import _Parser
 
 class GlissandoInterface(_Interface):
 
    def __init__(self, client):
-      self._client = None
-      self._parser = _Parser( )
+      _Interface.__init__(self, client, 'Glissando', ['Glissando'] )
       self._set = None
+
+   ### OVERRIDES ###
 
    def __nonzero__(self):
       return bool(self._set)
@@ -15,21 +15,13 @@ class GlissandoInterface(_Interface):
       assert isinstance(arg, bool)
       return bool(self._set) == arg
 
+   ### METHODS ###
+
    def clear(self):
       self._set = None
-      for key, value in self.__dict__.items( ):
-         if not key.startswith('_'):
-            delattr(self, key)
+      _Interface.clear(self)
 
-   @property
-   def _before(self):
-      result = [ ]
-      for key, value in self.__dict__.items( ):
-         if not key.startswith('_'):
-            result.append('\once \override Glissando %s = %s' % (
-               self._parser.formatAttribute(key),
-               self._parser.formatValue(value)))
-      return result
+   ### FORMATTING ###
 
    @property
    def _right(self):
