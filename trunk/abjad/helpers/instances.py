031735e437adb745eb90709416dcbdaaf83f810b
Trevor Bača <trevorbaca@gmail.com>
2008-01-21 22:09:27 +0000

    New instances.py helper with public instances( ) function. Use instances to visit any one Abjad score component and return all instances of classname; use instances to visit any Python list or tuple of Abjad score components and return all instances of classname. instances( ) grants a certain type of polymorphism and makes Abjad score components and lists look the same to the composer. The instances( ) helper is analagous to the bound _Component.getInstances( ) method.

---
+++ trunk/abjad/helpers/instances.py
@@ -0,0 +1,26 @@
+def _traverse(expr, v):
+   v.visit(expr)
+   if isinstance(expr, (list, tuple)):
+      for m in expr:
+         _traverse(m, v)
+   if hasattr(expr, '_music'):
+      for m in expr._music:
+         _traverse(m, v)
+   if hasattr(v, 'unvisit'):
+      v.unvisit(expr)
+
+
+### TODO - write reg tests ###
+def instances(expr, classname):
+   '''Return all class instances with classname in expr;
+      works on score components, lists, tuples.'''
+   class Visitor(object):
+      def __init__(self, classname):
+         self.classname = classname
+         self.result = []
+      def visit(self, node): 
+         if hasattr(node, 'kind') and node.kind(classname):
+            self.result.append(node)
+   v = Visitor(classname)
+   _traverse(expr, v)
+   return v.result 
