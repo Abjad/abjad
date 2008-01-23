eb0676222030fdbfbc797c3144916f2d209714ea
Trevor Bača <trevorbaca@gmail.com>
2008-01-23 22:55:16 +0000

    Extended octavation spanner to set LilyPond middleCPosition context setting on the first spanned leaf and before other spanned leaves with clef changes. Extended octavation spanner to set middleCPosition after last spanned note according to clef of last spanned note.

--- trunk/abjad/octavation/spanner.py
+++ trunk/abjad/octavation/spanner.py
@@ -7,20 +7,25 @@ class Octavation(_Spanner):
       self.start = start
       self.stop = stop
 
+   ### TODO - test the shit out of the middleCPosition stuff, esp
+   ###        clef changes in the middle of an octavation spanner
+
    def _before(self, leaf):
       result = [ ]
+      position = leaf.clef.middleCPosition - 7 * self.start
       if self._isMyFirstLeaf(leaf):
          result.append(r'#(set-octavation %s)' % self.start)
-         #if leaf.clef.name != 'treble':
-         #   position = leaf.clef.middleCPosition - 7 * self.start
-         #   result.append(r'\set Staff.middleCPosition = #%s' % position)
+      ### TODO - this is a ghetto way of checking for a clef change
+      ###        maybe better to create a clef interface to hold info?
+      if self._isMyFirstLeaf(leaf) or hasattr(leaf, '_clef'):
+         position = leaf.clef.middleCPosition - 7 * self.start
+         result.append(r'\set Staff.middleCPosition = #%s' % position)
       return result
 
    def _after(self, leaf):
       result = [ ]
       if self._isMyLastLeaf(leaf):
          result.append(r'#(set-octavation %s)' % self.stop)
-         #if self.leaves[0].clef.name != 'treble':
-         #   result.append(r'\set Staff.middleCPosition = #%s' % 
-         #      leaf.clef.middleCPosition)
+         position = leaf.clef.middleCPosition - 7 * self.stop
+         result.append(r'\set Staff.middleCPosition = #%s' % position)
       return result
