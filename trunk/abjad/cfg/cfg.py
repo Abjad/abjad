4e6662bfd779a6f7c47583590f732d236a037def
Trevor Bača <trevorbaca@gmail.com>
2008-02-13 14:43:52 +0000

    Added ABJADPERSISTENCE environment variable. Use for storing pickled objects.

--- trunk/abjad/cfg/cfg.py
+++ trunk/abjad/cfg/cfg.py
@@ -1,5 +1,6 @@
 from os import environ
 
+ABJADPERSISTENCE = environ.get('ABJADPERSISTENCE', '/home/abjab/persistence/')
 ABJADOUTPUT = environ.get('ABJADOUTPUT', '/home/abjad/output/')
 VERSIONFILE = ABJADOUTPUT + '.version'
 PDFVIEWER = environ.get('PDFVIEWER', 'open')
