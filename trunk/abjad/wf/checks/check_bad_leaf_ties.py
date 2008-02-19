cd1518442fb4759f9311ee4198acff396313038c
Víctor Adán <vctradn@gmail.com>
2008-02-19 00:54:30 +0000

    Added wf/checks/check_bad_leaf_ties.py. Leafs are now checked for valid ties. e.g. c'4~ c'4 is valid. c'4~ d'4 is invalid.

---
+++ trunk/abjad/wf/checks/check_bad_leaf_ties.py
@@ -0,0 +1,13 @@
+from ... helpers.instances import instances
+from .. tools import _report
+
+
+def check_bad_leaf_ties(expr, report = True, ret = 'violators'):
+   violators = [ ]
+   leaves = instances(expr, '_Leaf')
+   for leaf in leaves:
+      if leaf.tie and leaf.next:
+         if leaf.pitch != leaf.next.pitch:
+            violators.append(leaf)
+   total = len(leaves)
+   return _report(report, ret, violators, total, 'leaves with bad ties.')
