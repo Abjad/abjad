29771e3052a117b9176a5ae9bf6d120d0d859e41
Trevor Bača <trevorbaca@gmail.com>
2008-02-25 19:51:30 +0000

    Taught check_beamed_quarters( ) about ComplexBeams.

--- trunk/abjad/wf/checks/check_beamed_quarters.py
+++ trunk/abjad/wf/checks/check_beamed_quarters.py
@@ -6,9 +6,12 @@ def check_beamed_quarters(expr, report = True, ret = 'violators'):
    violators = [ ]
    leaves = instances(expr, '_Leaf')
    for leaf in leaves:
-      if hasattr(leaf, 'beam') and leaf.beam.spanned:
-         if leaf.beam._flags < 1:
-            violators.append(leaf)
+      if hasattr(leaf, 'beam'):
+         if leaf.beam.spanned:
+            beam = leaf.beam.spanner
+            if not beam.__class__.__name__ == 'ComplexBeam':
+               if leaf.beam._flags < 1:
+                  violators.append(leaf)
    bad = len(violators)
    total = len(leaves)
    msg = 'quarter (or greater) durations in beam.'
