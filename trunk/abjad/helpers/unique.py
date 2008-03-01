bfa25aad2ce945056a3daa3de8ab2b39af5ab180
root <root>
2008-03-01 21:11:44 +0000

    Swapped out crazy locals( )-based implementation of unique( ) in favor of manifesting a set as a list.

--- trunk/abjad/helpers/unique.py
+++ trunk/abjad/helpers/unique.py
@@ -1,5 +1,8 @@
 def unique(ll):
    '''Return unique elements in list ll;
-      recipe from Chris Mora.'''
+      recipe from Chris Mora;
+      
+      Mora == slow. Just use set( ). Sorry, Chris.'''
 
-   return [x for x in ll if x not in locals( )['_[1]']]
+   #return [x for x in ll if x not in locals( )['_[1]']]
+   return list(set(ll))
