beeb719cbf9543c0f34fe5ed902e82f4c1444b90
root <root>
2008-03-02 04:40:26 +0000

    Added not yet implemented leaf_scale.py helper function.

---
+++ trunk/abjad/helpers/leaf_scale.py
@@ -0,0 +1,11 @@
+def leaf_scale(leaf, duration):
+   '''
+      Docs.
+
+      Example:
+      
+      >>> scale_to(Note(0, (1, 8)), (5, 13))
+      FixedDurationTuplet((5, 13), [Note(0, (1, 2))])
+   '''
+
+   pass
