d4aaef42254cf6b4a7088cd0f7923a10253f952e
Trevor Bača <trevorbaca@gmail.com>
2008-03-12 07:05:54 +0000

    New public helper measure_with_rests( ) to create, for example, Measure((9, 16), [Rest((8, 16)), Rest((1, 16))]) from measure_with_rests((9, 16)). Use for creating a rest-filled but otherwise empty measure with aribtrary duration. Doesn't yet work for meters with nonbinary denominator.

---
+++ trunk/abjad/helpers/measure_with_rests.py
@@ -0,0 +1,18 @@
+from abjad.helpers.leaf_list import leaf_list
+from abjad.measure.measure import Measure
+from abjad.rest.rest import Rest
+
+
+def measure_with_rests(meter_token):
+   '''Return measure filled with one or more rests.
+
+      abjad> measure_with_rests((9, 16))
+      Measure(9/16, [r2, r16])
+
+      See /test/test_measure_with_rests.py for more examples.
+   '''   
+
+   assert isinstance(meter_token, tuple)
+   assert len(meter_token) == 2
+
+   return Measure(meter_token, leaf_list(Rest, meter_token))
---
+++ trunk/abjad/test/test_measure_with_rests.py
@@ -0,0 +1,28 @@
+from abjad import *
+from py.test import raises
+
+
+def test_measure_with_rests_01( ):
+   '''Works with binary numerator and binary denominator.'''
+   measure = measure_with_rests((4, 4))
+   assert measure.meter.pair == (4, 4)
+   assert len(measure) == 1
+   assert isinstance(measure[0], Rest)
+   assert measure[0].duration.pair == (1, 1)
+   
+
+def test_measure_with_rests_02( ):
+   '''Works with nonbinary numerator and binary denominator.'''
+   measure = measure_with_rests((5, 4))
+   assert measure.meter.pair == (5, 4)
+   assert len(measure) == 2
+   assert isinstance(measure[0], Rest)
+   assert isinstance(measure[1], Rest)
+   assert measure[0].duration.pair == (1, 1)
+   assert measure[1].duration.pair == (1, 4)
+   
+
+def test_measure_with_rests_03( ):
+   '''Doesn't work with nonbinary denominator;
+      TODO -- make work with nonbinary denominator.'''
+   assert raises(ValueError, 'measure_with_rests((4, 5))')
