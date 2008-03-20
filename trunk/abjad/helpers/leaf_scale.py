d12d79041083d100a2881eb84076f1dd2d8efb26
root <root>
2008-03-20 04:02:10 +0000

    Changed explicit type comparisons to calls against isinstance( ).

--- trunk/abjad/helpers/leaf_scale.py
+++ trunk/abjad/helpers/leaf_scale.py
@@ -1,6 +1,7 @@
 from .. duration.rational import Rational
 from converge_to_power2 import converge_to_power2
 from .. tuplet.fd.tuplet import FixedDurationTuplet
+from abjad.leaf.leaf import _Leaf
 ### TODO: (or rather questions) 
 ### - should multipliers be retained in scaling ?
 ### - change ValueError in non notehead-assignable notes to InvalidDurationError?
@@ -12,25 +13,15 @@ def leaf_scale(new_dur, leaf):
       >>> leaf_scale((5, 13), Note(0, (1, 8)))
       FixedDurationTuplet((5, 13), [Note(0, (1, 4))])
    '''
-   ### which is better: leaf.kind vs. isinstance?
-   ### btw: should kind be private?
-   #from abjad import _Leaf
-   #assert isinstance(leaf, _Leaf) 
-   assert leaf.kind('_Leaf')
-   assert type(new_dur) in (list, tuple, Rational)
-   if type(new_dur) in (list, tuple):
+   assert isinstance(leaf, _Leaf) 
+   assert isinstance(new_dur, (list, tuple, Rational))
+   if isinstance(new_dur, (list, tuple)):
       new_dur = Rational(*new_dur)
    try:
       leaf.duration = new_dur
       return leaf
    except ValueError:
       leaf.duration = converge_to_power2(leaf.duration, new_dur)
-#      parent = leaf._parent
-#      if parent is not None:
-#         indx = parent.index(leaf)
-#         result = FixedDurationTuplet(new_dur, [leaf])
-#         parent[indx] = result
-#      else:
       result = FixedDurationTuplet(new_dur, [leaf])
       return result
 
