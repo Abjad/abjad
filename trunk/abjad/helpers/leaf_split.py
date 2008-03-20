c8f35aa3775b3d8de59d03fe6116a00f89336e3e
root <root>
2008-03-20 03:23:18 +0000

    Minor changes to leaf_split( ). Changed explicit type checks to calls against isinstance( ). Added call to leaf spanner interface die( ).

--- trunk/abjad/helpers/leaf_split.py
+++ trunk/abjad/helpers/leaf_split.py
@@ -1,21 +1,19 @@
 from attributes import _transfer_all_attributes
 from .. duration.rational import Rational
 from leaf_scale import leaf_scale
-from abjad import Note
+from abjad.leaf.leaf import _Leaf
 
 def leaf_split(split_dur, leaf):
-   assert leaf.kind('_Leaf')
-   assert type(split_dur) in (int, list, tuple, Rational)
-   if type(split_dur) in (list, tuple):
+   assert isinstance(leaf, _Leaf)
+   assert isinstance(split_dur, (int, list, tuple, Rational))
+   if isinstance(split_dur, (list, tuple)):
       split_dur = Rational(*split_dur)
 
    if split_dur == 0 or split_dur >= leaf.duration:
       return [leaf]
    else:
       l1 = leaf.copy()
-      for s in l1.spanners.get():
-         s.die()
-
+      l1.spanners.die()
       parent = leaf._parent
       if parent:
          indx = parent.index(leaf)
