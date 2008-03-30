234229c81f886d5fd9614dd252f1b0f77f945a8b
root <root>
2008-03-30 21:50:26 +0000

    Generalized instance iterate( ) helper.

---
+++ trunk/abjad/helpers/iterate.py
@@ -0,0 +1,14 @@
+from abjad.helpers.hasname import hasname
+
+
+def iterate(expr, classname):
+   if hasname(expr, classname):
+      yield expr
+   if isinstance(expr, (list, tuple)):
+      for m in expr:
+         for x in iterate(m, classname):
+            yield x
+   if hasattr(expr, '_music'):
+      for m in expr._music:
+         for x in iterate(m, classname):
+            yield x
