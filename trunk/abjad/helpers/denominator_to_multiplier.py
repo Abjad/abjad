c3e8a8aadbe7f364135a417fa97b51bb1c3f78bd
root <root>
2008-04-01 23:09:19 +0000

    First version of public excise( ) helper which cuts a single leaf out of an arbitrarily complex score, shrinking durated containers and renotating meters as required.

---
+++ trunk/abjad/helpers/denominator_to_multiplier.py
@@ -0,0 +1,41 @@
+from abjad.duration.rational import Rational
+from math import log
+
+def _denominator_to_multiplier(d):
+   '''
+      Given positive integer denominator d,
+      get the prolation that d carries.
+
+      Result will always be <= 1.
+
+      abjad> for n in range(1, 19):
+      ...     print n, denominator_to_multiplier(n)
+      ... 
+      1 (1, 1)
+      2 (1, 1)
+      3 (2, 3)
+      4 (1, 1)
+      5 (4, 5)
+      6 (2, 3)
+      7 (4, 7)
+      8 (1, 1)
+      9 (8, 9)
+      10 (4, 5)
+      11 (8, 11)
+      12 (2, 3)
+      13 (8, 13)
+      14 (4, 7)
+      15 (8, 15)
+      16 (1, 1)
+      17 (16, 17)
+      18 (8, 9)
+   '''
+
+   ### TODO - decide whether we need this helper or not;
+   ###        may be possible to do with measure.duration.multiplier alone.
+      
+   assert isinstance(d, int)
+   assert d > 0
+   while d % 2 == 0:
+      d = int(d / 2)
+   return Rational(2 ** int(log(d, 2)), d)
---
+++ trunk/abjad/helpers/excise.py
@@ -0,0 +1,35 @@
+from abjad.helpers.denominator_to_multiplier import _denominator_to_multiplier
+from abjad.helpers.in_terms_of import _in_terms_of
+from abjad.measure.measure import Measure
+from abjad.tuplet.fd.tuplet import FixedDurationTuplet
+
+### TODO -- don't know if this works yet with *nested* tuplets
+###         don't know if this works with fixed *multiplier* tuplets
+###         don't know if this works with plain vanilla containers
+
+def excise(leaf):
+   '''Remove leaf from all sequential containers in leaf's parentage;
+      shrink duration of any enclosing durated containers.
+   '''
+   prolated_leaf_duration = leaf.duration.prolated
+   parent = leaf._parent
+   while parent is not None and not parent.parallel:
+      if isinstance(parent, FixedDurationTuplet):
+         immediate_parent_prolated = parent.duration.multiplier * leaf.duration
+         parent.duration -= immediate_parent_prolated
+      elif isinstance(parent, Measure):
+         old_denominator = parent.meter.denominator
+         naive_meter = parent.meter.duration - prolated_leaf_duration
+         better_meter = _in_terms_of(naive_meter, old_denominator)
+         parent.meter = better_meter
+         new_denominator = parent.meter.denominator
+         old_prolation = _denominator_to_multiplier(old_denominator)
+         new_prolation = _denominator_to_multiplier(new_denominator)
+         adjusted_prolation = old_prolation / new_prolation
+         for x in parent:
+            if isinstance(x, FixedDurationTuplet):
+               x.duration *= adjusted_prolation
+            else:
+               FixedDurationTuplet(x.duration * adjusted_prolation, [x])
+      parent = parent._parent
+   leaf._die( )
