0d4887bbd5c02659edadcd7da3eb311be5a386ff
Trevor Bača <trevorbaca@gmail.com>
2008-04-29 11:56:42 +0000

    Cleaned up comments in unique( ).

--- trunk/abjad/helpers/unique.py
+++ trunk/abjad/helpers/unique.py
@@ -1,8 +1,3 @@
 def unique(ll):
-   '''Return unique elements in list ll;
-      recipe from Chris Mora;
-      
-      Mora == slow. Just use set( ). Sorry, Chris.'''
-
-   #return [x for x in ll if x not in locals( )['_[1]']]
+   '''Return unique elements in list ll.'''
    return list(set(ll))
