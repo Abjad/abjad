8b80d909fe281294e384105072a46646bd385dd0
Víctor Adán <vctradn@gmail.com>
2008-05-30 16:42:57 +0000

    Major rewrite of _ArticulationInterface. _ArticulationInterface no longer inherits from list. _ArticultationInterface now implements its own getters and setters to allow for a flexible interface. Main enhancement: we can now say Note.articulations = [('staccato', 'up'), ('marcato', 'down')]. Backwards compatible, so old interface still works: Note.articulations = ['staccato', 'marcato'] sets direction to default. Also added tests to test_articulations_interface.

--- trunk/abjad/articulations/interface.py
+++ trunk/abjad/articulations/interface.py
@@ -1,18 +1,103 @@
-from .. core.interface import _Interface
+from abjad.core.interface import _Interface
+from abjad.articulations.articulation import _Articulation
 
-class _ArticulationsInterface(list, _Interface):
+class _ArticulationsInterface(_Interface):
+#class _ArticulationsInterface(list, _Interface):
 
    def __init__(self, client):
       _Interface.__init__(self, client, 'Articulations', ['Articulations'])
+      self._articulations = [ ]
 
    def __repr__(self):
-      if len(self):
-         return 'Articulations(%s)' % ', '.join(self)
+      if len(self._articulations):
+         return 'Articulations(%s)' % ', '.join([str(x) 
+            for x in self._articulations])
       else:
          return 'Articulations( )'
+
+   ### FORMATTING ###
 
    @property
    def _right(self):
       result = [ ]
-      result.extend(['\\' + x for x in self])
+      result.extend([x.lily for x in self._articulations])
       return result
+
+   ### MANAGEMENT ###
+
+   def append(self, expr):
+      expr = self._makeArticulation(expr)
+      self._articulations.append(expr)
+
+   def extend(self, expr):
+      expr = [self._makeArticulation(x) for x in expr]
+      self._articulations.extend(expr)
+
+   def remove(self, expr):
+      expr = self._makeArticulation(expr)
+      for a in self._articulations:
+         if expr == a:
+            self._articulations.remove(a)
+
+   def pop(self, i = -1):
+      return self._articulations.pop(i)
+      
+   def insert(self, i, expr):
+      expr = self._makeArticulation(expr)
+      self._articulations.insert(i, expr)
+
+   def sort(self):
+      def cmp(x, y):
+         if x.string < y.string:
+            return -1
+         elif x.string > y.string:
+            return 1
+         else:
+            return 0
+      self._articulations.sort(cmp)
+
+   ### OVERRIDES ###
+
+   def __len__(self):
+      return len(self._articulations)
+
+   def __contains__(self, expr):
+      expr = self._makeArticulation(expr)
+      for a in self._articulations:
+         if expr == a:
+            return True
+      return False
+
+   def __setitem__(self, i, expr):
+      if isinstance(i, int):
+         if i < 0:
+            j = len(self) + i
+         else:
+            j = i
+         expr = self._makeArticulation(expr)
+         self._articulations[j] = expr 
+      # slice
+      else:
+         assert isinstance(expr, list)
+         expr = [self._makeArticulation(x) for x in expr]
+         print expr
+         self._articulations[i.start : i.stop] = expr
+
+   def __getitem__(self, expr):
+      return self._articulations[expr]
+
+   def __delitem__(self, i):
+      del(self._articulations[i])
+
+   ### UTILS ###
+
+   def _makeArticulation(self, expr):
+      if isinstance(expr, _Articulation):
+         return expr
+      elif isinstance(expr, (list, tuple)):
+         return _Articulation(*expr)
+      elif isinstance(expr, str):
+         return _Articulation(expr)
+      else:
+         raise ValueError('can not create _Articulation.')
+         
--- trunk/abjad/test/test_articulations_interface.py
+++ trunk/abjad/test/test_articulations_interface.py
@@ -1,4 +1,5 @@
 from abjad import *
+from py.test import raises
 
 
 def test_articulations_interface_01( ):
@@ -8,43 +9,67 @@ def test_articulations_interface_01( ):
    assert t.format == "c'4"
 
 
-def test_articulations_interface_02( ):
+def test_articulations_interface_02a( ):
    t = Note(0, (1, 4))
    t.articulations = ['staccato']
    assert len(t.articulations) == 1
-   assert t.format == "c'4 \\staccato"
+   assert t.format == "c'4 -\\staccato"
 
 
-def test_articulations_interface_03( ):
+def test_articulations_interface_02b( ):
+   '''Articulations can be set as list of (string, direction) pairs.'''
+   t = Note(0, (1, 4))
+   t.articulations = [('staccato', 'up')]
+   assert len(t.articulations) == 1
+   assert t.format == "c'4 ^\\staccato"
+
+def test_articulations_interface_03a( ):
    t = Note(0, (1, 4))
    t.articulations = ['staccato', 'marcato']
    assert len(t.articulations) == 2
-   assert t.format == "c'4 \\staccato \\marcato"
+   assert t.format == "c'4 -\\staccato -\\marcato"
 
+def test_articulations_interface_03b( ):
+   '''Articulations can be set as list of (string, direction) pairs.'''
+   t = Note(0, (1, 4))
+   t.articulations = [('staccato', 'up'), ('marcato', 'down')]
+   assert len(t.articulations) == 2
+   assert t.format == "c'4 ^\\staccato _\\marcato"
+
+def test_articulations_interface_03c( ):
+   '''Articulations can be set as list of (string, direction) pairs.'''
+   t = Note(0, (1, 4))
+   t.articulations = [('staccato', 'up'), 'marcato']
+   assert len(t.articulations) == 2
+   assert t.format == "c'4 ^\\staccato -\\marcato"
 
 def test_articulations_interface_04( ):
+   '''Append.'''
    t = Note(0, (1, 4))
    t.articulations.append('staccato')
    assert len(t.articulations) == 1
-   assert t.format == "c'4 \\staccato"
+   assert t.format == "c'4 -\\staccato"
 
 
 def test_articulations_interface_05( ):
+   '''Extend.'''
    t = Note(0, (1, 4))
    t.articulations.extend(['staccato', 'marcato'])
    assert len(t.articulations) == 2
-   assert t.format == "c'4 \\staccato \\marcato"
+   assert t.format == "c'4 -\\staccato -\\marcato"
 
 
 def test_articulations_interface_06( ):
+   '''Sort works on articulations.'''
    t = Note(0, (1, 4))
    t.articulations.extend(['staccato', 'marcato'])
    t.articulations.sort( )
    assert len(t.articulations) == 2
-   assert t.format == "c'4 \\marcato \\staccato"
+   assert t.format == "c'4 -\\marcato -\\staccato"
    
    
 def test_articulations_interface_07( ):
+   '''Includes works on articulations.'''
    t = Note(0, (1, 4))
    t.articulations.extend(['staccato', 'marcato'])
    assert 'staccato' in t.articulations
@@ -52,6 +77,7 @@ def test_articulations_interface_07( ):
 
 
 def test_articulations_interface_08( ):
+   '''Pop.'''
    t = Note(0, (1, 4))
    t.articulations.extend(['staccato', 'marcato'])
    assert len(t.articulations) == 2
@@ -61,7 +87,8 @@ def test_articulations_interface_08( ):
    assert len(t.articulations) == 0
 
 
-def test_articulations_interface_08( ):
+def test_articulations_interface_09( ):
+   '''Remove.'''
    t = Note(0, (1, 4))
    t.articulations.extend(['staccato', 'marcato'])
    assert len(t.articulations) == 2
@@ -71,7 +98,8 @@ def test_articulations_interface_08( ):
    assert len(t.articulations) == 0
    
    
-def test_articulations_interface_09( ):
+def test_articulations_interface_10( ):
+   '''Articulations can be set with empty list.'''
    t = Note(0, (1, 4))
    t.articulations.extend(['staccato', 'marcato'])
    assert len(t.articulations) == 2
@@ -79,9 +107,17 @@ def test_articulations_interface_09( ):
    assert len(t.articulations) == 0
 
 
-def test_articulations_interface_10( ):
+def test_articulations_interface_11( ):
+   '''Articulations can be set with None.'''
    t = Note(0, (1, 4))
    t.articulations.extend(['staccato', 'marcato'])
    assert len(t.articulations) == 2
    t.articulations = None
    assert len(t.articulations) == 0
+   assert repr(t.articulations) == 'Articulations( )'
+
+def test_articulations_interface_12( ):
+   '''Articulations can only be directly set with list or tuple.'''
+   t = Note(0, (1, 4))
+   assert raises(ValueError, "t.articulations = 'staccato'")
+   
