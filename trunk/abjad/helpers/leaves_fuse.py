254a35890d27c9982c58106866cc91e1e9d2a82c
Víctor Adán <vctradn@gmail.com>
2008-07-17 05:30:05 +0000

    Added leaves_fuse_binary( ) helper and test_leaves_fuse_binary.py

---
+++ trunk/abjad/helpers/leaves_fuse.py
@@ -0,0 +1,19 @@
+from abjad.helpers.leaf_scale import leaf_scale_binary
+from abjad.helpers.instances import instances
+
+
+def leaves_fuse_binary(data):
+   '''Fuse duration of all leaves in <data> into a single leaf, 
+   if possible. Only properties of the first leaf are kept.'''
+   ### are all the leaves in data siblings? do we care.
+   ### are they beads of the same thread?
+   leaves = instances(data, '_Leaf')
+   if len(leaves) > 0:
+      dur = 0
+      for l in leaves:
+         dur += l.duration
+      ### delete all but the first
+      for l in leaves[1:]:
+         l._die( )
+      return leaf_scale_binary(dur, leaves[0])
+
