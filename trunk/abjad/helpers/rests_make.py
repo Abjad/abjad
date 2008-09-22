f248b9386a71edbf78afcb88ab2b5866e9e6d8df
Víctor Adán <vctradn@gmail.com>
2008-09-22 04:05:38 +0000

    rests_make( ) helper function now returns tied rests.

--- trunk/abjad/helpers/rests_make.py
+++ trunk/abjad/helpers/rests_make.py
@@ -1,5 +1,6 @@
-from abjad.rest.rest import Rest
 from abjad.helpers.duration_token_decompose import _duration_token_decompose
+from abjad.rest.rest import Rest
+from abjad.tie.spanner import Tie
 
 def rests_make(dur, pitch=None):
    '''Make (pitched) rest(s) by splitting dur if necessary.
@@ -9,5 +10,7 @@ def rests_make(dur, pitch=None):
       rest =  Rest(wd)
       rest.pitch = pitch
       result.append( rest )
+   if len(result) > 1:
+      Tie(result)
    return result
 
