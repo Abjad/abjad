caee64d85fd28c4341c391e9a0256b54b0abad0e
Trevor Bača <trevorbaca@gmail.com>
2008-11-03 14:04:27 +0000

    Built raw file for TOC. Added TOC_SECTION tag parser to raw_parser.py. Updated css/abjad.css to style TOC structurally.

--- trunk/abjad/documentation/scr/raw_parser.py
+++ trunk/abjad/documentation/scr/raw_parser.py
@@ -12,6 +12,7 @@ class FileParser(object):
       self.output =  [ ]
       self.tags = [SECTION( ), INTERFACE( ), DEFINITION( ),
          COMMENTS( ), TO_DO( ), CLASS_NAMES( ), INTRODUCTION( ),
+         TOC_SECTION( ),
          LILY( ), ABJAD( )]
 
    def writeOutput(self):
@@ -269,6 +270,41 @@ class INTRODUCTION(_TagParser):
             self.output.append('<div class="introduction">\n')
          elif '</introduction>' in line:
             self.output.append('</div class="introduction">\n')
+         else:
+            self.output.append(line)
+
+
+class TOC_SECTION(_TagParser):
+
+   def __init__(self):
+      self.output = [ ]
+      self.within = False
+
+   def parse(self, lines):
+      for line in lines:
+         if '<toc-section>' in line:
+            self.within = True
+            self.output.append('<div class="toc-section">\n')
+         elif '</toc-section>' in line:
+            self.within = False
+            self.output.append('</div class="toc-section">\n')
+         elif '<header>' in line:
+            if self.within:
+               name = line.replace('<header>', '').strip( )
+               output = '<h3>%s</h3>\n' % name
+               self.output.append(output)
+            else:
+               self.output.append(line)
+         elif '<body>' in line:
+            if self.within:
+               self.output.append('<div class="body">\n')
+            else:
+               self.output.append(line)
+         elif '</body>' in line:
+            if self.within:
+               self.output.append('</div class="body">\n')
+            else:
+               self.output.append(line)
          else:
             self.output.append(line)
 
