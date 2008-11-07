0761448ac9d3b1601cfb8d75089396ac0aba5bc9
Trevor Bača <trevorbaca@gmail.com>
2008-11-07 00:16:23 +0000

    Removed unused _numerator and _denominator from _LeafDurationInterface.

--- trunk/abjad/leaf/duration.py
+++ trunk/abjad/leaf/duration.py
@@ -12,8 +12,8 @@ class _LeafDurationInterface(_DurationInterface):
       _DurationInterface.__init__(self, _client)
       self.multiplier = None
       self.written = duration
-      self._numerator = self.written._numerator
-      self._denominator = self.written._denominator
+#      self._numerator = self.written._numerator
+#      self._denominator = self.written._denominator
 
 #   ### OVERLOADS ###
 #
