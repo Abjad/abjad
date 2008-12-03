66245fd6bb3f6e2e1c03dd7ba8fe8057736d4a3e
Trevor Bača <trevorbaca@gmail.com>
2008-12-03 21:06:45 +0000

    Fixes in test/test_pitch_init.py.

--- trunk/abjad/test/test_pitch_init.py
+++ trunk/abjad/test/test_pitch_init.py
@@ -20,79 +20,79 @@ def test_pitch_init_01( ):
 def test_pitch_init_02( ):
    '''Integer pitch initialization.'''
    p = Pitch(13)
-   assert repr(p) == "Pitch('cs', 5)"
+   assert repr(p) == "Pitch(cs, 5)"
    assert p.altitude == 7
    assert p.degree == 1
    assert p.letter == 'c'
-   assert p.lily == "cs'"
+   assert p.lily == "cs''"
    assert p.name == 'cs'
    assert p.number == 13
    assert p.octave == 5
    assert p.pair == ('cs', 5)
    assert p.pc == 1
-   assert p.ticks == "'"
+   assert p.ticks == "''"
 
 
 def test_pitch_init_03( ):
    '''Name-and-octave initialization.'''
    p = Pitch('df', 5)
-   assert repr(p) == "Pitch('df', 5)"
+   assert repr(p) == "Pitch(df, 5)"
    assert p.altitude == 8
    assert p.degree == 2
    assert p.letter == 'd'
-   assert p.lily == "df'"
+   assert p.lily == "df''"
    assert p.name == 'df'
    assert p.number == 13
    assert p.octave == 5
    assert p.pair == ('df', 5)
    assert p.pc == 1
-   assert p.ticks == "'"
+   assert p.ticks == "''"
 
 
 def test_pitch_init_04( ):
    '''Pitch pair initialization.'''
    p = Pitch(('df', 5))
-   assert repr(p) == "Pitch('df', 5)"
+   assert repr(p) == "Pitch(df, 5)"
    assert p.altitude == 8
    assert p.degree == 2
    assert p.letter == 'd'
-   assert p.lily == "df'"
+   assert p.lily == "df''"
    assert p.name == 'df'
    assert p.number == 13
    assert p.octave == 5
    assert p.pair == ('df', 5)
    assert p.pc == 1
-   assert p.ticks == "'"
+   assert p.ticks == "''"
 
 
 def test_pitch_init_05( ):
    '''Pitch reference initialization.'''
    r = Pitch('df', 5)
-   p = Pitch(p)
-   assert repr(p) == "Pitch('df', 5)"
+   p = Pitch(r)
+   assert repr(p) == "Pitch(df, 5)"
    assert p.altitude == 8
    assert p.degree == 2
    assert p.letter == 'd'
-   assert p.lily == "df'"
+   assert p.lily == "df''"
    assert p.name == 'df'
    assert p.number == 13
    assert p.octave == 5
    assert p.pair == ('df', 5)
    assert p.pc == 1
-   assert p.ticks == "'"
+   assert p.ticks == "''"
 
 
 def test_pitch_init_06( ):
    '''Pitch number-and-letter initialization.'''
    p = Pitch(13, 'd')
-   assert repr(p) == "Pitch('df', 5)"
+   assert repr(p) == "Pitch(df, 5)"
    assert p.altitude == 8
    assert p.degree == 2
    assert p.letter == 'd'
-   assert p.lily == "df'"
+   assert p.lily == "df''"
    assert p.name == 'df'
    assert p.number == 13
    assert p.octave == 5
    assert p.pair == ('df', 5)
    assert p.pc == 1
-   assert p.ticks == "'"
+   assert p.ticks == "''"
