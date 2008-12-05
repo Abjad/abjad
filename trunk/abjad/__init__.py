0bf6c0144f4f2d49bddb9307831692680316c7ee
Trevor Bača <trevorbaca@gmail.com>
2008-12-05 16:25:21 +0000

    Small abjad/__init__.py change. Abjad initializer now skips over any stray abjad/*.py files sitting at root level. So having a local abjad/test.py or abjad/foo.py file has no way of adversely affecting the auto-loading code in the abjad/__init__.py file.

--- trunk/abjad/__init__.py
+++ trunk/abjad/__init__.py
@@ -9,13 +9,17 @@ def _load_classes(forbid_list):
    import os
    for root, dirs, files in os.walk(__path__[0]):
       root = root[root.rindex('abjad'):]
-      for file in files:
-         if file.endswith('py') and not file.startswith(('_', '.')):
-            if all([y not in root for y in forbid_list]):
-               module = '.'.join([root.replace(os.sep, '.'), file[:-3]])
-               for key, value in _my_import(module).__dict__.items( ):
-                  if getattr(value, '__module__', None) == module:
-                     globals( )[key] = value
+      if root == 'abjad':
+         # skip over any stray abjad/*.py files at root level!
+         pass
+      else:
+         for file in files:
+            if file.endswith('py') and not file.startswith(('_', '.')):
+               if all([y not in root for y in forbid_list]):
+                  module = '.'.join([root.replace(os.sep, '.'), file[:-3]])
+                  for key, value in _my_import(module).__dict__.items( ):
+                     if getattr(value, '__module__', None) == module:
+                        globals( )[key] = value
 
 def _remove_modules():
    from types import ModuleType
@@ -25,4 +29,6 @@ def _remove_modules():
 
 #_load_classes(('svn', 'tools', 'wf', 'test', 'checks'))
 _load_classes(('documentation', 'svn', 'tools', 'test', 'checks'))
+print 'loaded classes'
 _remove_modules()
+print 'removed modules'
