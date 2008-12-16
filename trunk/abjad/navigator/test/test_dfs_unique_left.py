c5f481e1296adab5ed2b5b96dcb9c73def88f974
Trevor Bača <trevorbaca@gmail.com>
2008-12-16 18:00:15 +0000

    New bound _DFSUniqueLeft( ) and _DFSUniqueRight( ) methods available on _Navigator.
    The new methods return generators that iterate the score containing self._client starting from self._client and yield each node at most one time. ('Unique' in the method name highlights this behavior that limits each node to at most one apperance in the generator output.)
    
    The iteration is a preorder traversal such that self._client will yield before children of self._client.
    
    Theses different flavors of depth-first search will probably generalize later.

--- trunk/abjad/navigator/test/test_dfs_unique_left.py
+++ trunk/abjad/navigator/test/test_dfs_unique_left.py
@@ -2,13 +2,13 @@ from abjad import *
 import py.test
 
 
-def test_dfs_capped_left_01( ):
+def test_dfs_unique_left_01( ):
    '''
-   Capped left-to-right DFS on a single note.
+   Unique left-to-right DFS on a single note.
    '''
 
    t = Note(0, (1, 8))
-   g = t._navigator._DFSCappedLeft( )
+   g = t._navigator._DFSUniqueLeft( )
 
    r'''
    c'4
@@ -22,13 +22,13 @@ def test_dfs_capped_left_01( ):
    '''
 
 
-def test_dfs_capped_left_02( ):
+def test_dfs_unique_left_02( ):
    '''
-   Capped left-to-right DFS on an empty container.
+   Unique left-to-right DFS on an empty container.
    '''
 
    t = Sequential([ ])
-   g = t._navigator._DFSCappedLeft( )
+   g = t._navigator._DFSUniqueLeft( )
 
    r'''
    {
@@ -43,14 +43,14 @@ def test_dfs_capped_left_02( ):
    '''
 
 
-def test_dfs_capped_left_03( ):
+def test_dfs_unique_left_03( ):
    '''
-   Capped left-to-right DFS on a flat container.
+   Unique left-to-right DFS on a flat container.
    '''
 
    t = Sequential(run(4))
    appictate(t)
-   g = t._navigator._DFSCappedLeft( )
+   g = t._navigator._DFSUniqueLeft( )
    
    r'''
    {
@@ -63,37 +63,29 @@ def test_dfs_capped_left_03( ):
 
    assert g.next( ) is t
    assert g.next( ) is t[0]
-   assert g.next( ) is t
    assert g.next( ) is t[1]
-   assert g.next( ) is t
    assert g.next( ) is t[2]
-   assert g.next( ) is t
    assert g.next( ) is t[3]
-   assert g.next( ) is t
    assert py.test.raises(StopIteration, 'g.next( )')
 
    r'''
    Sequential(c'8, cs'8, d'8, ef'8)
    Note(c', 8)
-   Sequential(c'8, cs'8, d'8, ef'8)
    Note(cs', 8)
-   Sequential(c'8, cs'8, d'8, ef'8)
    Note(d', 8)
-   Sequential(c'8, cs'8, d'8, ef'8)
    Note(ef', 8)
-   Sequential(c'8, cs'8, d'8, ef'8)
    '''
 
 
-def test_dfs_capped_left_04( ):
+def test_dfs_unique_left_04( ):
    '''
-   Capped left-to-right DFS on nested container.
+   Unique left-to-right DFS on nested container.
    '''
 
    t = Sequential(run(4))
    t.insert(2, Sequential(run(2)))
    appictate(t)
-   g = t._navigator._DFSCappedLeft( )
+   g = t._navigator._DFSUniqueLeft( )
 
    r'''
    {
@@ -110,35 +102,21 @@ def test_dfs_capped_left_04( ):
 
    assert g.next( ) is t
    assert g.next( ) is t[0]
-   assert g.next( ) is t
    assert g.next( ) is t[1]
-   assert g.next( ) is t
    assert g.next( ) is t[2]
    assert g.next( ) is t[2][0]
-   assert g.next( ) is t[2]
    assert g.next( ) is t[2][1]
-   assert g.next( ) is t[2]
-   assert g.next( ) is t
    assert g.next( ) is t[3]
-   assert g.next( ) is t
    assert g.next( ) is t[4]
-   assert g.next( ) is t
    assert py.test.raises(StopIteration, 'g.next( )')
 
    r'''
    Sequential(c'8, cs'8, Sequential(d'8, ef'8), e'8, f'8)
    Note(c', 8)
-   Sequential(c'8, cs'8, Sequential(d'8, ef'8), e'8, f'8)
    Note(cs', 8)
-   Sequential(c'8, cs'8, Sequential(d'8, ef'8), e'8, f'8)
    Sequential(d'8, ef'8)
    Note(d', 8)
-   Sequential(d'8, ef'8)
    Note(ef', 8)
-   Sequential(d'8, ef'8)
-   Sequential(c'8, cs'8, Sequential(d'8, ef'8), e'8, f'8)
    Note(e', 8)
-   Sequential(c'8, cs'8, Sequential(d'8, ef'8), e'8, f'8)
    Note(f', 8)
-   Sequential(c'8, cs'8, Sequential(d'8, ef'8), e'8, f'8)
    '''
--- trunk/abjad/navigator/test/test_dfs_unique_right.py
+++ trunk/abjad/navigator/test/test_dfs_unique_right.py
@@ -2,13 +2,13 @@ from abjad import *
 import py.test
 
 
-def test_dfs_capped_right_01( ):
+def test_dfs_unique_right_01( ):
    '''
-   Capped right-to-left DFS on a single note.
+   Unique right-to-left DFS on a single note.
    '''
 
    t = Note(0, (1, 8))
-   g = t._navigator._DFSCappedRight( )
+   g = t._navigator._DFSUniqueRight( )
 
    r'''
    c'4
@@ -22,13 +22,13 @@ def test_dfs_capped_right_01( ):
    '''
 
 
-def test_dfs_capped_right_02( ):
+def test_dfs_unique_right_02( ):
    '''
-   Capped right-to-left DFS on an empty container.
+   Unique right-to-left DFS on an empty container.
    '''
 
    t = Sequential([ ])
-   g = t._navigator._DFSCappedRight( )
+   g = t._navigator._DFSUniqueRight( )
 
    r'''
    {
@@ -43,14 +43,14 @@ def test_dfs_capped_right_02( ):
    '''
 
 
-def test_dfs_capped_right_03( ):
+def test_dfs_unique_right_03( ):
    '''
-   Capped right-to-left DFS on a flat container.
+   Unique right-to-left DFS on a flat container.
    '''
 
    t = Sequential(run(4))
    appictate(t)
-   g = t._navigator._DFSCappedRight( )
+   g = t._navigator._DFSUniqueRight( )
    
    r'''
    {
@@ -63,37 +63,29 @@ def test_dfs_capped_right_03( ):
 
    assert g.next( ) is t
    assert g.next( ) is t[3]
-   assert g.next( ) is t
    assert g.next( ) is t[2]
-   assert g.next( ) is t
    assert g.next( ) is t[1]
-   assert g.next( ) is t
    assert g.next( ) is t[0]
-   assert g.next( ) is t
    assert py.test.raises(StopIteration, 'g.next( )')
 
    r'''
    Sequential(c'8, cs'8, d'8, ef'8)
    Note(ef', 8)
-   Sequential(c'8, cs'8, d'8, ef'8)
    Note(d', 8)
-   Sequential(c'8, cs'8, d'8, ef'8)
    Note(cs', 8)
-   Sequential(c'8, cs'8, d'8, ef'8)
    Note(c', 8)
-   Sequential(c'8, cs'8, d'8, ef'8)
    '''
 
 
-def test_dfs_capped_right_04( ):
+def test_dfs_unique_right_04( ):
    '''
-   Capped right-to-left DFS on nested container.
+   Unique right-to-left DFS on nested container.
    '''
 
    t = Sequential(run(4))
    t.insert(2, Sequential(run(2)))
    appictate(t)
-   g = t._navigator._DFSCappedRight( )
+   g = t._navigator._DFSUniqueRight( )
 
    r'''
    {
@@ -110,35 +102,21 @@ def test_dfs_capped_right_04( ):
 
    assert g.next( ) is t
    assert g.next( ) is t[4]
-   assert g.next( ) is t
    assert g.next( ) is t[3]
-   assert g.next( ) is t
    assert g.next( ) is t[2]
    assert g.next( ) is t[2][1]
-   assert g.next( ) is t[2]
    assert g.next( ) is t[2][0]
-   assert g.next( ) is t[2]
-   assert g.next( ) is t
    assert g.next( ) is t[1]
-   assert g.next( ) is t
    assert g.next( ) is t[0]
-   assert g.next( ) is t
    assert py.test.raises(StopIteration, 'g.next( )')
 
    r'''
    Sequential(c'8, cs'8, Sequential(d'8, ef'8), e'8, f'8)
    Note(f', 8)
-   Sequential(c'8, cs'8, Sequential(d'8, ef'8), e'8, f'8)
    Note(e', 8)
-   Sequential(c'8, cs'8, Sequential(d'8, ef'8), e'8, f'8)
    Sequential(d'8, ef'8)
    Note(ef', 8)
-   Sequential(d'8, ef'8)
    Note(d', 8)
-   Sequential(d'8, ef'8)
-   Sequential(c'8, cs'8, Sequential(d'8, ef'8), e'8, f'8)
    Note(cs', 8)
-   Sequential(c'8, cs'8, Sequential(d'8, ef'8), e'8, f'8)
    Note(c', 8)
-   Sequential(c'8, cs'8, Sequential(d'8, ef'8), e'8, f'8)
    '''
