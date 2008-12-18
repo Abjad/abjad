27dc47e932160e2033b4403a4a99749ef186e190
Trevor Bača <trevorbaca@gmail.com>
2008-12-18 04:06:43 +0000

    Commented out unused _Interface._copy( ) method.

--- trunk/abjad/core/interface.py
+++ trunk/abjad/core/interface.py
@@ -6,12 +6,12 @@ class _Interface(_Abjad):
    def __init__(self, client):
       self._client = client
 
-   ### PRIVATE METHODS ###
-
-   def _copy(self):
-      from copy import copy
-      client = self._client
-      self._client = None
-      result = copy(self)
-      self._client = client
-      return result
+#   ### PRIVATE METHODS ###
+#
+#   def _copy(self):
+#      from copy import copy
+#      client = self._client
+#      self._client = None
+#      result = copy(self)
+#      self._client = client
+#      return result
