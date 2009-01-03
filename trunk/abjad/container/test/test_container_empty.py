3792d255cc106253dcdb05a2949877a0c2d9838a
Trevor Bača <trevorbaca@gmail.com>
2009-01-03 19:50:20 +0000

    Renamed, broke out and beautified container tests.

---
+++ trunk/abjad/container/test/test_container_empty.py
@@ -0,0 +1,14 @@
+from abjad import *
+
+
+def test_container_empty_01( ):
+   '''
+   Abjad allows empty containers.
+   '''
+
+   t = Container([ ])
+
+   assert t.duration.contents == Rational(0)
+   assert t.duration.prolated == Rational(0)
+   assert t.format == ''
+   assert len(t) == 0
