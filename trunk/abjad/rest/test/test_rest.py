98341d476f04e27100ed289bce27cb95854c29df
Trevor Bača <trevorbaca@gmail.com>
2009-01-03 19:31:29 +0000

    Renamed and beautified tests.

--- trunk/abjad/rest/test/test_rest.py
+++
@@ -1,9 +0,0 @@
-from abjad import *
-
-
-def test_rest_01( ):
-   '''Typical rest.'''
-   r = Rest((1, 4))
-   assert repr(r) == 'Rest(4)'
-   assert r.format == 'r4'
-   assert r.duration.written == r.duration.prolated == Rational(1, 4)
--- trunk/abjad/staff/test/test_staff.py
+++
@@ -1,7 +0,0 @@
-from abjad import *
-
-
-def test_staff_invocation_01( ):
-   t = Staff([ ])
-   t.invocation.type = 'RhythmicStaff'
-   assert t.format == '\\new RhythmicStaff {\n}'
