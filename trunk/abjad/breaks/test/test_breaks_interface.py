d2e40386f714bc6bb5ffe4b29e00d9c3815b51c0
Trevor Bača <trevorbaca@gmail.com>
2009-01-03 19:05:44 +0000

    Beautified breaks testfile.

--- trunk/abjad/breaks/test/test_breaks_interface.py
+++ trunk/abjad/breaks/test/test_breaks_interface.py
@@ -1,39 +1,49 @@
 from abjad import *
 
 
-def test_breaks_01( ):
-   '''Line break on nonempty container.'''
-   t = Staff(Note(0, (1, 4)) * 4)
+def test_breaks_interface_01( ):
+   '''
+   Line break at closing of nonempty container.
+   '''
+
+   t = Staff(scale(4))
    t.breaks.line = True
-   assert t.format == "\\new Staff {\n\tc'4\n\tc'4\n\tc'4\n\tc'4\n\t\\break\n}"
+
    r'''
    \new Staff {
-      c'4
-      c'4
-      c'4
-      c'4
-      \break
+           c'8
+           d'8
+           e'8
+           f'8
+           \break
    }
    '''
 
+   assert t.format == "\\new Staff {\n\tc'8\n\td'8\n\te'8\n\tf'8\n\t\\break\n}"
 
-def test_breaks_02( ):
-   '''Page break on nonempty container.'''
-   t = Staff(Note(0, (1, 4)) * 4)
+
+def test_breaks_interface_02( ):
+   '''
+   Page break at closing of nonempty container.
+   '''
+
+   t = Staff(scale(4))
    t.breaks.page = True
-   assert t.format == "\\new Staff {\n\tc'4\n\tc'4\n\tc'4\n\tc'4\n\t\\pageBreak\n}"
+
    r'''
    \new Staff {
-      c'4
-      c'4
-      c'4
-      c'4
-      \pageBreak
+           c'8
+           d'8
+           e'8
+           f'8
+           \pageBreak
    }
    '''
 
+   assert t.format == "\\new Staff {\n\tc'8\n\td'8\n\te'8\n\tf'8\n\t\\pageBreak\n}"
 
-def test_breaks_03( ):
+
+def test_breaks_interface_03( ):
    '''Both line and page break on nonempty container.'''
    t = Staff(Note(0, (1, 4)) * 4)
    t.breaks.line = True
@@ -51,7 +61,7 @@ def test_breaks_03( ):
    '''
 
 
-def test_breaks_04( ):
+def test_breaks_interface_04( ):
    '''Line break on leaf.'''
    t = Note(0, (1, 4))
    t.breaks.line = True
@@ -62,7 +72,7 @@ def test_breaks_04( ):
    '''
 
 
-def test_breaks_05( ):
+def test_breaks_interface_05( ):
    '''Page break on leaf.'''
    t = Note(0, (1, 4))
    t.breaks.page = True
@@ -73,7 +83,7 @@ def test_breaks_05( ):
    '''
 
 
-def test_breaks_06( ):
+def test_breaks_interface_06( ):
    '''Both line and page break on leaf.'''
    t = Note(0, (1, 4))
    t.breaks.line = True
@@ -86,7 +96,7 @@ def test_breaks_06( ):
    '''
 
 
-def test_breaks_07( ):
+def test_breaks_interface_07( ):
    '''The clear( ) method clears both line and page breaks
       on containers.'''
    t = Staff(Note(0, (1, 4)) * 4)
@@ -103,7 +113,7 @@ def test_breaks_07( ):
    }
    '''
 
-def test_breaks_08( ):
+def test_breaks_interface_08( ):
    '''The clear( ) method clears both line and page breaks
       on leaves.'''
    t = Note(0, (1, 4))
@@ -113,21 +123,21 @@ def test_breaks_08( ):
    assert t.format == "c'4"
 
 
-def test_breaks_09( ):
+def test_breaks_interface_09( ):
    '''_BreaksInterface returns True when 'line' is True.'''
    t = Note(0, (1, 4))
    t.breaks.line = True
    assert bool(t.breaks)
    
 
-def test_breaks_10( ):
+def test_breaks_interface_10( ):
    '''_BreaksInterface returns True when 'page' is True.'''
    t = Note(0, (1, 4))
    t.breaks.page = True
    assert bool(t.breaks)
    
 
-def test_breaks_11( ):
+def test_breaks_interface_11( ):
    '''_BreaksInterface returns True when both
       'line' and 'page' are True.'''
    t = Note(0, (1, 4))
@@ -136,7 +146,7 @@ def test_breaks_11( ):
    assert bool(t.breaks)
    
 
-def test_breaks_12( ):
+def test_breaks_interface_12( ):
    '''_BreaksInterface returns False when neither
       'line' nor 'page' are True.'''
    t = Note(0, (1, 4))
