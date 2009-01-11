581cd8fcab26d6e78c6f78c9cb11dcf53127acb9
John Mandereau <john>
2009-01-11 23:26:41 +0000

    Unset LANG for getting svn info

--- trunk/abjad/cfg/abjad_version.py
+++ trunk/abjad/cfg/abjad_version.py
@@ -4,7 +4,7 @@ import os
 
 
 os.chdir(ABJADPATH)
-os.system('svn info > %s' % VERSIONFILE)
+os.system('LANG= svn info > %s' % VERSIONFILE)
 abjad_version = file(VERSIONFILE, 'r').readlines( )
 abjad_version = [line for line in abjad_version if line.startswith('Revision')]
 abjad_version = abjad_version[0].split(':')[1].strip( )
