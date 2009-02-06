579869b81d88e8fefcc077208dce57a91d03760b
Trevor Bača <trevorbaca@gmail.com>
2009-02-06 14:00:57 +0000

    Updated _denominator_to_multiplier( ) helper to use _next_least_power_of_two( ) helper. New test_denominator_to_multiplier.py file.

--- trunk/abjad/helpers/denominator_to_multiplier.py
+++ trunk/abjad/helpers/denominator_to_multiplier.py
@@ -1,42 +1,24 @@
+from abjad.helpers.next_least_power_of_two import _next_least_power_of_two
 from abjad.rational.rational import Rational
-from math import log
 
 
 def _denominator_to_multiplier(d):
    '''
-      Given positive integer denominator d,
-      get the prolation that d carries.
+   Return prolation attaching to denominator 'd'; result always <= 1.
 
-      Result will always be <= 1.
+   abjad> for n in range(1, 9):
+   ...     print n, _denominator_to_multiplier(n)
+   ... 
+   1 1
+   2 1
+   3 2/3
+   4 1
+   5 4/5
+   6 2/3
+   7 4/7
+   8 1
 
-      abjad> for n in range(1, 19):
-      ...     print n, denominator_to_multiplier(n)
-      ... 
-      1 (1, 1)
-      2 (1, 1)
-      3 (2, 3)
-      4 (1, 1)
-      5 (4, 5)
-      6 (2, 3)
-      7 (4, 7)
-      8 (1, 1)
-      9 (8, 9)
-      10 (4, 5)
-      11 (8, 11)
-      12 (2, 3)
-      13 (8, 13)
-      14 (4, 7)
-      15 (8, 15)
-      16 (1, 1)
-      17 (16, 17)
-      18 (8, 9)
+   See helpers/test/test_denominator_to_multiplier.py for examples.
    '''
 
-   ### TODO - decide whether we need this helper or not;
-   ###        may be possible to do with measure.duration.multiplier alone.
-      
-   assert isinstance(d, int)
-   assert d > 0
-   while d % 2 == 0:
-      d = int(d / 2)
-   return Rational(2 ** int(log(d, 2)), d)
+   return Rational(_next_least_power_of_two(d), d)
