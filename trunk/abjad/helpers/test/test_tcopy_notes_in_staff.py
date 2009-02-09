1d6729424532e22252859d5243ecad8c574d1bd9
Trevor Bača <trevorbaca@gmail.com>
2009-02-09 21:55:56 +0000

    Modernized tcopy( ) tests.

--- trunk/abjad/helpers/test/test_tcopy_notes_in_staff.py
+++ trunk/abjad/helpers/test/test_tcopy_notes_in_staff.py
@@ -2,67 +2,36 @@ from abjad import *
 
 
 def test_tcopy_notes_in_staff_01( ):
-   t = Staff([Note(n, (1, 8)) for n in range(8)])
-   u = tcopy(t[0 : 1])
-   assert isinstance(u, Staff)
-   assert len(u) == 1
-   assert isinstance(u[0], Note)
-   assert u[0].pitch.number == t[0].pitch.number
-   assert id(u[0]) is not id(t[0])
-   assert u[0]._parent == u
+   '''Copy adjacent notes in staff.'''
+
+   t = Staff(scale(4))
+   u = tcopy(t[:2])
+
+   r'''
+   \new Staff {
+           c'8
+           d'8
+   }
+   '''
+
+   assert check(t)
+   assert check(u)
+   assert u.format == "\\new Staff {\n\tc'8\n\td'8\n}"
 
 
 def test_tcopy_notes_in_staff_02( ):
-   t = Staff([Note(n, (1, 8)) for n in range(8)])
-   u = tcopy(t[1 : 2])
-   assert isinstance(u, Staff)
-   assert len(u) == 1
-   assert isinstance(u[0], Note)
-   assert u[0].pitch.number == t[1].pitch.number
-   assert id(u[0]) is not id(t[1])
-   assert u[0]._parent == u
+   '''Copy adjacent notes in staff.'''
+
+   t = Staff(scale(4))
+   u = tcopy(t[-2:])
+
+   r'''
+   \new Staff {
+           e'8
+           f'8
+   }
+   '''
    
-
-def test_tcopy_notes_in_staff_03( ):
-   t = Staff([Note(n, (1, 8)) for n in range(8)])
-   u = tcopy(t[-1 :])
-   assert isinstance(u, Staff)
-   assert len(u) == 1
-   assert isinstance(u[0], Note)
-   assert u[0].pitch.number == t[-1].pitch.number
-   assert id(u[0]) is not id(t[-1])
-   assert u[0]._parent == u
-   
-
-def test_tcopy_notes_in_staff_04( ):
-   t = Staff([Note(n, (1, 8)) for n in range(8)])
-   u = tcopy(t[-2:-1])
-   assert isinstance(u, Staff)
-   assert len(u) == 1
-   assert isinstance(u[0], Note)
-   assert u[0].pitch.number == t[-2].pitch.number
-   assert id(u[0]) is not id(t[-2])
-   assert u[0]._parent == u
-
-
-def test_tcopy_notes_in_staff_05( ):
-   t = Staff([Note(n, (1, 8)) for n in range(8)])
-   u = tcopy(t[0:3])
-   assert isinstance(u, Staff)
-   assert len(u) == 3
-   for i, x in enumerate(u):
-      assert x.pitch.number == t[i].pitch.number
-      assert id(x) is not id(t[i])
+   assert check(t)
    assert check(u)
-
-
-def test_tcopy_notes_in_staff_06( ):
-   t = Staff([Note(n, (1, 8)) for n in range(8)])
-   u = tcopy(t[1:7])
-   assert isinstance(u, Staff)
-   assert len(u) == 6
-   for i, x in enumerate(u):
-      j = i + 1
-      assert x.pitch.number == t[j].pitch.number
-      assert id(x) is not id(t[j])
-   assert check(u)
+   assert u.format == "\\new Staff {\n\te'8\n\tf'8\n}"
--- trunk/abjad/helpers/test/test_tcopy_tuplets_in_staff.py
+++ trunk/abjad/helpers/test/test_tcopy_tuplets_in_staff.py
@@ -1,58 +1,28 @@
 from abjad import *
 
 
-### TODO - implement some sort of 'crossing' copy.
-### given t = Staff(FixedDurationTuplet((2, 8), Note(0, (1, 8)) * 3) * 3)
-### there is currently no way to copy, eg, leaves 0 - 5
-### because leaves 0 - 3 belong to tuplet 0 
-### while leaves 4 - 5 belong to tuplet 1;
-### in other words, leaves 0 - 5 'cross' tuplet boundaries.
-### not sure what the right interface here would be;
-### it isn't possible to say t.leaves(0, 5) because t.leaves
-### is merely a (built-in) list.
-### this may motivate a (non-OOP) copy( ) function;
-### then copy(t, (0, 5), indices = 'leaves') might make sense;
-### or leafCopy(t, (0, 5)), or something similar.
-
-def test_tcopy_tuplets_in_staff_01( ):
-   t = Staff(FixedDurationTuplet((2, 8), Note(0, (1, 8)) * 3) * 3)
-   u = tcopy(t[0:1])
-   assert isinstance(u, Staff)
-   assert len(u) == 1
-   assert isinstance(u[0], FixedDurationTuplet)
-   assert u[0].duration.preprolated == t[0].duration.preprolated
-   assert id(u[0]) is not id(t[0])
-   assert u[0]._parent == u
-   
-
 def test_tcopy_tuplets_in_staff_02( ):
-   t = Staff(FixedDurationTuplet((2, 8), Note(0, (1, 8)) * 3) * 3)
-   u = tcopy(t[1:2])
-   assert isinstance(u, Staff)
-   assert len(u) == 1
-   assert isinstance(u[0], FixedDurationTuplet)
-   assert u[0].duration.preprolated == t[1].duration.preprolated
-   assert id(u[0]) is not id(t[1])
-   assert u[0]._parent == u
-   
+   '''Copy adjacent, whole tuplets from staff.'''
 
-def test_tcopy_tuplets_in_staff_03( ):
-   t = Staff(FixedDurationTuplet((2, 8), Note(0, (1, 8)) * 3) * 3)
-   u = tcopy(t[-1:])
-   assert isinstance(u, Staff)
-   assert len(u) == 1
-   assert isinstance(u[0], FixedDurationTuplet)
-   assert u[0].duration.preprolated == t[-1].duration.preprolated
-   assert id(u[0]) is not id(t[-1])
-   assert u[0]._parent == u
+   t = Staff(FixedDurationTuplet((2, 8), run(3)) * 3)
+   diatonicize(t)
+   u = tcopy(t[1:])
 
+   r'''
+   \new Staff {
+           \times 2/3 {
+                   f'8
+                   g'8
+                   a'8
+           }
+           \times 2/3 {
+                   b'8
+                   c''8
+                   d''8
+           }
+   }
+   '''
 
-def test_tcopy_tuplets_in_staff_04( ):
-   t = Staff(FixedDurationTuplet((2, 8), Note(0, (1, 8)) * 3) * 3)
-   u = tcopy(t[-2:-1])
-   assert isinstance(u, Staff)
-   assert len(u) == 1
-   assert isinstance(u[0], FixedDurationTuplet)
-   assert u[0].duration.preprolated == t[-2].duration.preprolated
-   assert id(u[0]) is not id(t[-2])
-   assert u[0]._parent == u
+   assert check(t)
+   assert check(u) 
+   assert u.format == "\\new Staff {\n\t\\times 2/3 {\n\t\tf'8\n\t\tg'8\n\t\ta'8\n\t}\n\t\\times 2/3 {\n\t\tb'8\n\t\tc''8\n\t\td''8\n\t}\n}"
