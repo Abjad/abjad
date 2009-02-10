3eaf01a90192b0a24ccb84faabd7f77f1610025d
Trevor Bača <trevorbaca@gmail.com>
2009-02-10 00:11:46 +0000

    Bug fix to lcopy( ) helper. Previously lcopy(expr, i, j) copied leaves from *the governor of* expr, rather than from expr itself. Now lcopy(expr, i, j) copies leaves from expr itself.
    See the new helpers/test/test_lcopy_local_leaf_index.py file for examples.

--- trunk/abjad/helpers/lcopy.py
+++ trunk/abjad/helpers/lcopy.py
@@ -7,7 +7,11 @@ from abjad.leaf.leaf import _Leaf
 
 
 def lcopy(expr, start = 0, stop = None):
-   '''With stop = None, copy all leaves.'''
+   '''Copy consecutive leaves from start to stop in expr;
+      copy all structure in the parentage of copied leaves,
+      trimming and shrinking containers as necessary.
+      
+      When stop is None, copy all leaves from start in expr.'''
 
    # trivial leaf lcopy
    if isinstance(expr, _Leaf):
@@ -16,7 +20,6 @@ def lcopy(expr, start = 0, stop = None):
    # copy leaves from sequential containers only.
    if expr.parallel:
       raise ContiguityError('can not lcopy leaves from parallel container.')
-   #assert not expr.parallel
 
    # assert valid start and stop
    leaves = expr.leaves
@@ -25,15 +28,27 @@ def lcopy(expr, start = 0, stop = None):
       stop = len(leaves)
    assert stop > start
 
+   # new: find start and stop leaves in expr
+   start_leaf_in_expr = leaves[start]
+   stop_leaf_in_expr = leaves[stop - 1]
+
    # find governor
-   #governor = leaves[start]._parentage._governor
    governor = leaves[start].parentage._governor
+
+   # new: find start and stop leaves in governor
+   governor_leaves = governor.leaves
+   start_index_in_governor = governor_leaves.index(start_leaf_in_expr)
+   stop_index_in_governor = governor_leaves.index(stop_leaf_in_expr)
 
    # copy governor
    governor_copy = governor.copy( )
    copy_leaves = governor_copy.leaves
-   start_leaf = copy_leaves[start]
-   stop_leaf = copy_leaves[stop - 1]
+   #start_leaf = copy_leaves[start]
+   #stop_leaf = copy_leaves[stop - 1]
+
+   # new: find start and stop leaves in copy of governor
+   start_leaf = copy_leaves[start_index_in_governor]
+   stop_leaf = copy_leaves[stop_index_in_governor]
 
    #print start_leaf, stop_leaf
    #print ''
