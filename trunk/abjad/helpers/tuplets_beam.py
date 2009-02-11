195fa7ef3adab084759a30ba0b61aacf5c8b0cc4
Trevor Bača <trevorbaca@gmail.com>
2009-02-11 15:24:11 +0000

    New tuplets_beam( ) helper.
    Extensions to the public interface: you can now say tuplets_beam(expr) for any expression expr. The helper iterates all tuplets and attached an Abjad Beam spanner to each. Example:
    
       t = Voice(FixedDurationTuplet((2, 8), run(3)) * 2)
       diatonicize(t)
       tuplets_beam(t)
    
       \new Voice {
          \times 2/3 {
             c'8 [
             d'8
             e'8 ]
          }
          \times 2/3 {
             f'8 [
             g'8
             a'8 ]
          }
       }
    
    See helpers/test/tesst_tuplets_beam.py for this same example.

---
+++ trunk/abjad/helpers/tuplets_beam.py
@@ -0,0 +1,13 @@
+from abjad.beam.spanner import Beam
+from abjad.helpers.iterate import iterate
+
+
+def tuplets_beam(expr):
+   '''Beam every tuplet in expr.
+
+      TODO: this should probably beam only top-level 
+            or bottom-level tuplets; the current implementation
+            will beam nested tuplets in a nested way.'''
+
+   for measure in iterate(expr, '_Tuplet'):
+      Beam(measure)
