6d92ab109d1b31a9dbcbeabca6d3a8af95266b58
Trevor Bača <trevorbaca@gmail.com>
2009-02-11 20:58:49 +0000

    New leaves_multiply( ) helper.
    Extensions to the public interface: you can now say leaves_multiply(expr, n = 1) for any expression expr. The helper iterates all leaves, t_i, in expr and inserts n extra copies of t for every t in t_i. Example:
    
       t = Voice(scale(3))
       p = Beam(t[:])
       leaves_multiply(t, n = 1)
    
       \new Voice {
          c'8 [
          c'8
          d'8
          d'8
          e'8
          e'8 ]
       }
    
    See helpers/test/test_leaves_multiply.py for another example.

---
+++ trunk/abjad/helpers/test/test_leaves_multiply.py
@@ -0,0 +1,48 @@
+from abjad import *
+
+
+def test_leaves_multiply_01( ):
+   '''Multiply each leaf in voice by 1.'''
+
+   t = Voice(scale(3))
+   p = Beam(t[:])
+   leaves_multiply(t, n = 1)
+
+   r'''
+   \new Voice {
+      c'8 [
+      c'8
+      d'8
+      d'8
+      e'8
+      e'8 ]
+   }
+   '''
+
+   assert check(t)
+   assert t.format == "\\new Voice {\n\tc'8 [\n\tc'8\n\td'8\n\td'8\n\te'8\n\te'8 ]\n}"
+
+
+def test_leaves_multiply_02( ):
+   '''Multiply each leaf in voice by 2.'''
+
+   t = Voice(scale(3))
+   Beam(t[:])
+   leaves_multiply(t, n = 2)
+
+   r'''
+   \new Voice {
+      c'8 [
+      c'8
+      c'8
+      d'8
+      d'8
+      d'8
+      e'8
+      e'8
+      e'8 ]
+   }
+   '''
+
+   assert check(t)
+   assert t.format == "\\new Voice {\n\tc'8 [\n\tc'8\n\tc'8\n\td'8\n\td'8\n\td'8\n\te'8\n\te'8\n\te'8 ]\n}"
