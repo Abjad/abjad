1c188713c3ec5db354d93ccc6b5e0eacb07e11cb
Víctor Adán <vctradn@gmail.com>
2009-02-11 07:02:14 +0000

    New zip_cycle( ) helper available. Similar to zip( ), but returns a list the size of the longest of the two lists passed for zipping and cycles over the elements of the shortest list. See helpers/test/test_zip_cycle.py for examples.

---
+++ trunk/abjad/helpers/zip_cycle.py
@@ -0,0 +1,23 @@
+from __future__ import division
+import math
+
+def zip_cycle(first, second):
+   '''
+   Cyclical zip. Like zip( ), but will return a list of length 
+   max(len(first), len(second)) and will cycle over the elements of the
+   shortest list.
+   '''
+   if not isinstance(first, (list, tuple)):
+      first = [first]
+   if not isinstance(second, (list, tuple)):
+      second = [second]
+
+   if len(first) > len(second):
+      m = int(math.ceil(len(first) / len(second)))
+      second *= m
+   elif len(first) < len(second):
+      m = int(math.ceil(len(second) / len(first)))
+      first *= m
+   return zip(first, second)
+
+   
