a16ba16b5a6dd823995b003bed63496bd95a018f
Trevor Bača <trevorbaca@gmail.com>
2009-02-13 22:07:22 +0000

    Added 'style' keyword to measures_beam( ) helper. Keyword can set to either None or 'complex'. When set to None, the helper spans all measures in expr with a plain old Beam spanner. When set to 'complex', the helper spans all measures in expr with a ComplexBeam spanner. See helpers/test/test_measures_beam.py for an example of each setting.

--- trunk/abjad/helpers/measures_beam.py
+++ trunk/abjad/helpers/measures_beam.py
@@ -1,9 +1,15 @@
 from abjad.beam.spanner import Beam
+from abjad.beam.complex import ComplexBeam
 from abjad.helpers.iterate import iterate
 
 
-def measures_beam(expr):
+def measures_beam(expr, style = 'complex'):
    '''Beam every measure in expr.'''
 
    for measure in iterate(expr, '_Measure'):
-      Beam(measure)
+      if style == 'complex':
+         ComplexBeam(measure)
+      elif style is None:
+         Beam(measure)
+      else:
+         raise ValueError('unknown beam style.')
--- trunk/abjad/helpers/test/test_measures_beam.py
+++ trunk/abjad/helpers/test/test_measures_beam.py
@@ -2,11 +2,11 @@ from abjad import *
 
 
 def test_measures_beam_01( ):
-   '''Beam all measures in expr.'''
+   '''Beam all measures in expr with plain old Beam spanner.'''
 
    t = Staff(RigidMeasure((2, 8), run(2)) * 3)
    diatonicize(t)
-   measures_beam(t)
+   measures_beam(t, style = None)
 
    r'''
    \new Staff {
@@ -24,3 +24,33 @@ def test_measures_beam_01( ):
 
    assert check(t)
    assert t.format == "\\new Staff {\n\t\t\\time 2/8\n\t\tc'8 [\n\t\td'8 ]\n\t\t\\time 2/8\n\t\te'8 [\n\t\tf'8 ]\n\t\t\\time 2/8\n\t\tg'8 [\n\t\ta'8 ]\n}"
+
+
+def test_measures_beam_02( ):
+   '''Beam all measures in expr with ComplexBeam.'''
+
+   t = Staff(RigidMeasure((2, 8), run(2)) * 2)
+   diatonicize(t)
+   measures_beam(t, style = 'complex')
+
+   r'''
+   \new Staff {
+         \time 2/8
+         \set stemLeftBeamCount = #0
+         \set stemRightBeamCount = #1
+         c'8 [
+         \set stemLeftBeamCount = #1
+         \set stemRightBeamCount = #0
+         d'8 ]
+         \time 2/8
+         \set stemLeftBeamCount = #0
+         \set stemRightBeamCount = #1
+         e'8 [
+         \set stemLeftBeamCount = #1
+         \set stemRightBeamCount = #0
+         f'8 ]
+   }
+   '''
+
+   assert check(t)
+   assert t.format == "\\new Staff {\n\t\t\\time 2/8\n\t\t\\set stemLeftBeamCount = #0\n\t\t\\set stemRightBeamCount = #1\n\t\tc'8 [\n\t\t\\set stemLeftBeamCount = #1\n\t\t\\set stemRightBeamCount = #0\n\t\td'8 ]\n\t\t\\time 2/8\n\t\t\\set stemLeftBeamCount = #0\n\t\t\\set stemRightBeamCount = #1\n\t\te'8 [\n\t\t\\set stemLeftBeamCount = #1\n\t\t\\set stemRightBeamCount = #0\n\t\tf'8 ]\n}"
