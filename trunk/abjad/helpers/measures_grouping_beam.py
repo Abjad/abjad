b467830a328022e863604015c86eb431848474e0
Trevor Bača <trevorbaca@gmail.com>
2009-02-13 23:27:14 +0000

    New measures_grouping_beam( ) helper.
    Extensions to the public interface: you can now say measures_grouping_beam(measures), for any sequence of measures. The helper strips any beams attached to measures and applies a ComplexBeam to the measures in measures, running a single span-beam between the contents of the measures. Example:
    
       t = Voice(RigidMeasure((2, 8), run(2)) * 2)
       diatonicize(t)
       measures_grouping_beam(t[:])
    
       \new Voice {
             \time 2/8
             \set stemLeftBeamCount = #0
             \set stemRightBeamCount = #1
             c'8 [
             \set stemLeftBeamCount = #1
             \set stemRightBeamCount = #1
             d'8
             \time 2/8
             \set stemLeftBeamCount = #1
             \set stemRightBeamCount = #1
             e'8
             \set stemLeftBeamCount = #1
             \set stemRightBeamCount = #0
             f'8 ]
       }
    
    See helpers/test/test_measures_grouping_beam.py for this example.

---
+++ trunk/abjad/helpers/measures_grouping_beam.py
@@ -0,0 +1,14 @@
+from abjad.beam.complex import ComplexBeam
+
+
+def measures_grouping_beam(measures):
+   '''Apply ComplexBeam to all measures in measures;
+      set p.durations equal to preprolated measure durations.'''
+
+   durations = [ ]
+   for measure in measures:
+      print measure
+      measure.beam.unspan( )
+      durations.append(measure.duration.preprolated)
+   beam = ComplexBeam(measures, durations = durations, span = 1)
+   return beam
---
+++ trunk/abjad/helpers/test/test_measures_grouping_beam.py
@@ -0,0 +1,32 @@
+from abjad import *
+
+
+def test_measures_grouping_beam_01( ):
+   '''Apply ComplexBeam to all measures in measures;
+      set p.durations equal to preprolated measure durations.'''
+
+   t = Voice(RigidMeasure((2, 8), run(2)) * 2)
+   diatonicize(t)
+   measures_grouping_beam(t[:])
+
+   r'''
+   \new Voice {
+         \time 2/8
+         \set stemLeftBeamCount = #0
+         \set stemRightBeamCount = #1
+         c'8 [
+         \set stemLeftBeamCount = #1
+         \set stemRightBeamCount = #1
+         d'8
+         \time 2/8
+         \set stemLeftBeamCount = #1
+         \set stemRightBeamCount = #1
+         e'8
+         \set stemLeftBeamCount = #1
+         \set stemRightBeamCount = #0
+         f'8 ]
+   }
+   '''
+
+   assert check(t)
+   assert t.format == "\\new Voice {\n\t\t\\time 2/8\n\t\t\\set stemLeftBeamCount = #0\n\t\t\\set stemRightBeamCount = #1\n\t\tc'8 [\n\t\t\\set stemLeftBeamCount = #1\n\t\t\\set stemRightBeamCount = #1\n\t\td'8\n\t\t\\time 2/8\n\t\t\\set stemLeftBeamCount = #1\n\t\t\\set stemRightBeamCount = #1\n\t\te'8\n\t\t\\set stemLeftBeamCount = #1\n\t\t\\set stemRightBeamCount = #0\n\t\tf'8 ]\n}"
