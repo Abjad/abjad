89165349e190c8f5f7bc1133a97a511039bb7e50
Trevor Bača <trevorbaca@gmail.com>
2009-02-14 19:19:23 +0000

    New tripartition(n, **kwargs) helper available.
    Examples:
    
       assert tripartition(0, biggest = 'right') == (0, 0, 0)
       assert tripartition(1, biggest = 'right') == (0, 0, 1)
       assert tripartition(2, biggest = 'right') == (1, 0, 1)
       assert tripartition(3, biggest = 'right') == (1, 1, 1)
       assert tripartition(4, biggest = 'right') == (1, 1, 2)
       assert tripartition(5, biggest = 'right') == (2, 1, 2)
       assert tripartition(6, biggest = 'right') == (2, 2, 2)
       assert tripartition(7, biggest = 'right') == (2, 2, 3)
       assert tripartition(8, biggest = 'right') == (3, 2, 3)
       assert tripartition(9, biggest = 'right') == (3, 3, 3)
    
    There are two keywords, 'biggest' and 'smallest'. Both keywords can be set to 'left', 'middle' or 'right'.
    
    See helpers/test/test_tripartition.py for examples of each keyword setting.

---
+++ trunk/abjad/helpers/tripartition.py
@@ -0,0 +1,26 @@
+def tripartition(n, smallest = 'middle', biggest = 'middle'):
+   '''Partition integer n into three parts, returned as a tuple.'''
+
+   assert isinstance(n, int)
+   assert smallest in ('left', 'middle', 'right')
+   assert biggest in ('left', 'middle', 'right')
+   
+   small = int(n / 3)
+   big = small + 1
+
+   if n % 3 == 0:
+      return small, small, small
+   elif n % 3 == 1:
+      if biggest == 'left':
+         return big, small, small
+      elif biggest == 'middle':
+         return small, big, small
+      elif biggest == 'right':
+         return small, small, big
+   elif n % 3 == 2:
+      if smallest == 'left':
+         return small, big, big
+      elif smallest == 'middle':
+         return big, small, big
+      elif smallest == 'right':
+         return big, big, small
