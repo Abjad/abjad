81cb644dc38abdd0eea8d49bdebe57880218525d
Trevor Bača <trevorbaca@gmail.com>
2009-02-14 01:25:12 +0000

    label_leaf_durations( ) now accounts for ties.

--- trunk/abjad/helpers/label_durations.py
+++ trunk/abjad/helpers/label_durations.py
@@ -1,16 +1,31 @@
 from abjad.helpers.iterate import iterate
 
 
-def label_leaf_durations(expr, show = ['written', 'prolated']):
+def label_leaf_durations(expr, 
+   show = ['written', 'prolated'], ties = 'together'):
    '''Iterate expr and label all written and prolated durations.'''
    
    for leaf in iterate(expr, '_Leaf'):
-      if leaf.duration.multiplier is not None:
-         multiplier = '* %s' % str(leaf.duration.multiplier)
+      if ties == 'together':
+         if not leaf.tie.spanned:
+            if leaf.duration.multiplier is not None:
+               multiplier = '* %s' % str(leaf.duration.multiplier)
+            else:
+               multiplier = ''
+            if 'written' in show:
+               label = r'\small %s%s' % (leaf.duration.written, multiplier)
+               leaf.markup.down.append(label)
+            if 'prolated' in show:
+               leaf.markup.down.append(r'\small %s' % leaf.duration.prolated)
+         elif leaf.tie.spanner._isMyFirstLeaf(leaf):
+            tie = leaf.tie.spanner
+            if 'written' in show:
+               written = sum([x.duration.written for x in tie])
+               label = r'\small %s' % written
+               leaf.markup.down.append(label)
+            if 'prolated' in show:
+               prolated = sum([x.duration.prolated for x in tie])
+               label = r'\small %s' % prolated
+               leaf.markup.down.append(label)
       else:
-         multiplier = ''
-      if 'written' in show:
-         label = r'\small %s%s' % (leaf.duration.written, multiplier)
-         leaf.markup.down.append(label)
-      if 'prolated' in show:
-         leaf.markup.down.append(r'\small %s' % leaf.duration.prolated)
+         raise ValueError('unknown value for tie treatment.')
