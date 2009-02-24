00244bf0ef95f8d4fbdf2df8f5381c867a585321
Trevor Bača <trevorbaca@gmail.com>
2009-02-24 13:39:19 +0000

    New _are_successive_components(ll) private helper.
    Return True when ll is a Python list and when either
    
       1. each of the elements in ll is an orphan Abjad component, or
       2. all elements in ll share the same (container) parent
    
    Intended for type-checking helper function input.
    Generalization of _are_contiguous_music_elements, _are_orphan_components.
    
    NOTE: 
    
    Helper functions that handle only *containerized* components
    should assert _are_contiguous_music_elements.
    
    Helper functions that handle both containerized components AND
    also components in a built-in Python list (say a Python list of
    measures prior to staff insertion) should instead assert
    _are_successive_components.
    
    (So _are_successive_components is more lenient than 
    _are_contiguous_music_elements.)
    
    See helpers/test/test_are_successive_components.py for examples.

--- trunk/abjad/helpers/contiguity.py
+++ trunk/abjad/helpers/contiguity.py
@@ -21,18 +21,32 @@ def _are_contiguous_music_elements(ll):
    Check for contiguous leaves (which have 'next' and 'prev' handles)
    and also tuplets, measures and other types of input (which don't).
 
-   TODO: This is old code;
-         should probably be replaced by a newer, threading-based check.
+   Intended for type-checking helper function input.
+
+   TODO: 
+
+   This old code should probably be replaced by newer, threading-based check.
    '''
 
-   parent = ll[0]._parent
+   try:
+      parent = ll[0]._parent
+   except (TypeError, IndexError):
+      return False
+
    if parent is None:
       return False
+
    first = ll[0]
    index = parent.index(first)
    for element in ll[1 : ]:
       index += 1
-      if element is not parent[index]:
+      #if element is not parent[index]:
+      #   return False
+      try:
+         next_in_parent = parent[index]
+      except IndexError:
+         return False
+      if element is not next_in_parent:
          return False
    return True
 
@@ -54,3 +68,28 @@ def _are_orphan_components(ll):
       return False
 
    return True
+
+
+def _are_successive_components(ll):
+   '''Return True when ll is a Python list and when either
+   each of the elements in ll is an orphan Abjad component or when
+   all elements in ll share the same (container) parent,
+   otherwise False.
+
+   Intended for type-checking helper function input.
+   Generalization of _are_contiguous_music_elements, _are_orphan_components.
+
+   NOTE: 
+
+   Helper functions that handle only *containerized* components
+   should assert _are_contiguous_music_elements.
+
+   Helper functions that handle both containerized components AND
+   also components in a built-in Python list (say a Python list of
+   measures prior to staff insertion) should instead assert
+   _are_successive_components.
+
+   (So _are_successive_components is more lenient than 
+   _are_contiguous_music_elements.)'''
+   
+   return _are_contiguous_music_elements(ll) or _are_orphan_components(ll)
