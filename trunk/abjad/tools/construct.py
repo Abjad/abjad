267317c658602691bcfa9f763d5dfcfc2fc644da
Víctor Adán <vctradn@gmail.com>
2009-02-26 16:22:25 +0000

    New notes_curve(pitches, total, start, stop, exp, written) note constructor available. Use this to create runs of 'continuously' accelerating/decelerating notes. notes_curve( ) returns a train of notes with 'continuously' changing effective durations given as the written duration argument times the computed interpolated multipliers.  The default written duration is 1/8 note. The durations are interpolated from start duration argument to stop duration argument. The function returns as many notes as necessary to fill the total duration requested. The pitches of the notes are set cyclically from the pitches list.

--- trunk/abjad/tools/construct.py
+++ trunk/abjad/tools/construct.py
@@ -218,3 +218,32 @@ def note_train(pitch, written_duration, total_duration,
       multiplied_remainder = ~prolation * remainder_duration
       result.extend(notes_prolated(pitch, [multiplied_remainder]))
    return result
+
+
+from abjad.tools import mathtools
+
+def notes_curve(pitches, total, start, stop, exp='cosine', 
+   written=Rational(1, 8)):
+   '''
+   Returns a train of notes with 'continuously' changing effective durations
+   given as the written duration argument times the computed interpolated 
+   multipliers. The default written duration is 1/8 note.
+   The durations are interpolated from start duration
+   argument to stop duration argument. 
+   The function returns as many interpolation values as necessary to 
+   fill the total duration requested.
+   The pitches of the notes are set cyclically from the pitches list.
+   '''
+   total = Rational(*_duration_token_unpack(total))
+   start = Rational(*_duration_token_unpack(start))
+   stop = Rational(*_duration_token_unpack(stop))
+   written = Rational(*_duration_token_unpack(written))
+
+   dts = mathtools.interpolate_divide(total, start, stop, exp)
+   result = [ ]
+   for i, dt in enumerate(dts):
+      note = Note(pitches[i % len(pitches)], written)
+      note.duration.multiplier = dt / written 
+      result.append(note)
+   return result
+   
