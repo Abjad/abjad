6efe9568e9312d1844e8d9c3f9d1addc4be57b11
Trevor Bača <trevorbaca@gmail.com>
2009-03-03 16:04:16 +0000

    New divide_leaf(leaf, divisions, prolation_type) helper.
    Extensions to the public interface: you can now say divide_leaf(leaf, divisions = 2, prolation_type = 'diminution') for any leaf t. The helper bequeaths the score position of leaf with a newly created fixed-duration tuplet, such that the target duration of the newly created tuplet equals the written duration of t, and such that the newly created tuplet contains 'divisions' number of evenly spaced notes. The prolation keyword can be set to either 'diminution' or 'augmentation' and is used only in those cases where there exist two different ways of notating the same tuplet ratio, for example, to choose between 6:5 and 3:5, the first of which is a diminution and the second of which is an augmentation.
    
       t = divide_leaf(Note(0, (3, 16)), 5, 'diminution')
       \fraction \times 3/5 {
          c'16
          c'16
          c'16
          c'16
          c'16
       }
    
       t = divide_leaf(Note(0, (3, 16)), 5, 'diminution')
       \fraction \times 6/5 {
          c'32
          c'32
          c'32
          c'32
          c'32
       }
    
    See helpers/test/test_divide_leaf.py for further examples.

---
+++ trunk/abjad/helpers/divide_leaf.py
@@ -0,0 +1,29 @@
+from abjad.note.note import Note
+from abjad.helpers.prolated_to_written import _prolated_to_written
+from abjad.tuplet.fd.tuplet import FixedDurationTuplet
+
+
+def divide_leaf(leaf, divisions = 2, prolation = 'diminution'):
+   '''Newer and better tuplet-maker.
+      Compare with divide( ) helper.'''
+
+   # find target duration of fixed-duration tuplet
+   target_duration = leaf.duration.written
+
+   # find prolated duration of each note in tuplet
+   prolated_duration = target_duration / divisions
+
+   # find written duration of each note in tuplet
+   written_duration = _prolated_to_written(prolated_duration, prolation)
+
+   # make tuplet notes
+   notes = Note(0, written_duration) * divisions
+
+   # make tuplet
+   tuplet = FixedDurationTuplet(target_duration, notes)
+
+   # bequeath leaf position in score structure to tuplet
+   leaf.bequeath(tuplet) 
+
+   # return tuplet
+   return tuplet
