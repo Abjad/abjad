6cfb176bc484815d7722983f282cb588ab1aba37
Trevor Bača <trevorbaca@gmail.com>
2009-03-04 23:52:52 +0000

    New leaf_duration_scale(leaf, multiplier) helper.
    Extensions to the public interface: you can now say leaf_duration_scale(leaf, multiplier) for any Abjad leaf and any positive multiplier. The helper wraps leaf_duration_change( ) and returns either a rewritten version of t, or t with following tied notes, or t within a fixed-multiplier tuplet.
    
    Example:
    
       t = Note(0, (1, 4))
       leaf_duration_scale(t, Rational(5, 4))
       assert t.tie.spanner.written == Rational(5, 16)
    
    See helpers/test/test_leaf_duration_scale.py for this and four other examples.

---
+++ trunk/abjad/helpers/test/test_leaf_duration_scale.py
@@ -0,0 +1,27 @@
+from abjad import *
+
+
+def test_leaf_duration_scale_01( ):
+   t = Note(0, (1, 4))
+   leaf_duration_scale(t, Rational(1, 2))
+   assert t.duration.written == Rational(1, 8)
+
+
+def test_leaf_duration_scale_02( ):
+   t = Note(0, (1, 4))
+   leaf_duration_scale(t, Rational(2))
+   assert t.duration.written == Rational(1, 2)
+
+
+def test_leaf_duration_scale_03( ):
+   t = Note(0, (1, 4))
+   leaf_duration_scale(t, Rational(5, 4))
+   assert t.tie.spanner.written == Rational(5, 16)
+
+
+def test_leaf_duration_scale_04( ):
+   t = Note(0, (1, 4))
+   leaf_duration_scale(t, Rational(2, 3))
+   assert t.duration.written == Rational(1, 4)
+   assert t.duration.prolated == Rational(1, 6)
+
