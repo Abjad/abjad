af3369818b6026959441241a218735ccfdd8c9fa
Trevor Bača <trevorbaca@gmail.com>
2009-03-04 23:22:20 +0000

    New tuplet_contents_fix(tuplet) helper.
    Extensions to the public interface: you can now say tuplet_contents_fix(tuplet) for any Abjad tuplet. The helper doubles (or halves) note values within tuplet as appropriate to bring tuplet multiplier to something strictly greater than 1/2 and strictly less than 2.
    
    Example:
    
       t = FixedDurationTuplet((2, 8), scale(3, Rational(1, 4)))
       assert not _is_tuplet_multiplier(t.duration.multiplier)
    
       \times 1/3 {
          c'4
          d'4
          e'4
       }
    
       tuplet_contents_fix(t)
    
       \times 2/3 {
          c'8
          d'8
          e'8
       }
    
       assert check(t)
       assert _is_tuplet_multiplier(t.duration.multiplier)
    
    See helpers/test/test_tuplet_contents_fix.py for this and two other examples.

---
+++ trunk/abjad/helpers/tuplet_contents_fix.py
@@ -0,0 +1,33 @@
+from abjad.helpers.chop import chop
+from abjad.helpers.leaf_duration_change import leaf_duration_change
+from abjad.leaf.leaf import _Leaf
+from abjad.rational.rational import Rational
+from abjad.tuplet.fd.tuplet import FixedDurationTuplet
+from abjad.tuplet.fm.tuplet import FixedMultiplierTuplet
+import math
+
+
+def tuplet_contents_fix(tuplet):
+   '''Scale tuplet contents by power of two
+      if tuplet multiplier less than 1/2 or greater than 2.
+   
+      Return tuplet.'''
+
+   # check input
+   if isinstance(tuplet, FixedMultiplierTuplet):
+      raise NotImplemented
+   elif not isinstance(tuplet, FixedDurationTuplet):
+      raise ValueError('must be tuplet.')
+
+   # find tuplet multiplier
+   integer_exponent = chop(math.log(tuplet.duration.multiplier, 2))
+   leaf_multiplier = Rational(2) ** integer_exponent
+
+   # scale leaves in tuplet by power of two
+   for component in tuplet[:]:
+      if isinstance(component, _Leaf):
+         old_written_duration = component.duration.written
+         new_written_duration = leaf_multiplier * old_written_duration
+         leaf_duration_change(component, new_written_duration)
+
+   return tuplet
