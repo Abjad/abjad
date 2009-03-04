cadb4785eb9161a5597d97c50221502d2693e70b
Trevor Bača <trevorbaca@gmail.com>
2009-03-04 19:33:46 +0000

    New _is_tuplet_multiplier(multiplier) private helper.
    Returns True when multiplier is an Abjad rational that is strictly greater than 1/2 and strictly less than 2.
    
       assert not _is_tuplet_multiplier(Rational(1, 10))
       assert not _is_tuplet_multiplier(Rational(2, 10))
       assert not _is_tuplet_multiplier(Rational(3, 10))
       assert not _is_tuplet_multiplier(Rational(4, 10))
       assert not _is_tuplet_multiplier(Rational(5, 10))
       assert _is_tuplet_multiplier(Rational(6, 10))
       assert _is_tuplet_multiplier(Rational(7, 10))
       assert _is_tuplet_multiplier(Rational(8, 10))
       assert _is_tuplet_multiplier(Rational(9, 10))
       assert _is_tuplet_multiplier(Rational(10, 10))
    
       assert _is_tuplet_multiplier(Rational(11, 10))
       assert _is_tuplet_multiplier(Rational(12, 10))
       assert _is_tuplet_multiplier(Rational(13, 10))
       assert _is_tuplet_multiplier(Rational(14, 10))
       assert _is_tuplet_multiplier(Rational(15, 10))
       assert _is_tuplet_multiplier(Rational(16, 10))
       assert _is_tuplet_multiplier(Rational(17, 10))
       assert _is_tuplet_multiplier(Rational(18, 10))
       assert _is_tuplet_multiplier(Rational(19, 10))
       assert not _is_tuplet_multiplier(Rational(20, 10))
    
    See helpers/test/test_is_tuplet_multiplier.py for these two examples.

---
+++ trunk/abjad/helpers/test/test_is_tuplet_multiplier.py
@@ -0,0 +1,34 @@
+from abjad.helpers.is_tuplet_multiplier import _is_tuplet_multiplier
+from abjad import *
+
+
+def test_is_tuplet_multiplier_01( ):
+   '''True when multiplier is a rational strictly greater than 1/2
+      and strictly less than 2.'''
+
+   assert not _is_tuplet_multiplier(Rational(1, 10))
+   assert not _is_tuplet_multiplier(Rational(2, 10))
+   assert not _is_tuplet_multiplier(Rational(3, 10))
+   assert not _is_tuplet_multiplier(Rational(4, 10))
+   assert not _is_tuplet_multiplier(Rational(5, 10))
+   assert _is_tuplet_multiplier(Rational(6, 10))
+   assert _is_tuplet_multiplier(Rational(7, 10))
+   assert _is_tuplet_multiplier(Rational(8, 10))
+   assert _is_tuplet_multiplier(Rational(9, 10))
+   assert _is_tuplet_multiplier(Rational(10, 10))
+
+
+def test_is_tuplet_multiplier_02( ):
+   '''True when multiplier is a rational strictly greater than 1/2
+      and strictly less than 2.'''
+
+   assert _is_tuplet_multiplier(Rational(11, 10))
+   assert _is_tuplet_multiplier(Rational(12, 10))
+   assert _is_tuplet_multiplier(Rational(13, 10))
+   assert _is_tuplet_multiplier(Rational(14, 10))
+   assert _is_tuplet_multiplier(Rational(15, 10))
+   assert _is_tuplet_multiplier(Rational(16, 10))
+   assert _is_tuplet_multiplier(Rational(17, 10))
+   assert _is_tuplet_multiplier(Rational(18, 10))
+   assert _is_tuplet_multiplier(Rational(19, 10))
+   assert not _is_tuplet_multiplier(Rational(20, 10))
