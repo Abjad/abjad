37e74037bcfa188caaafe7b8eaf659ab6ab350ee
Trevor Bača <trevorbaca@gmail.com>
2009-03-05 20:44:15 +0000

    Extended _MeterInterface with private _selfCanContribute, _selfShouldContribute and _parentCanContribute read-only properties to help determine exactly which component, if any, should contribute LiyPond time signature indication at format-time. This helps take care of 'warring' components battling it out at-format time to see whether first-note-in-measure, first-tuplet-in-measure or, indeed, the measure itself should contribute LilyPond time signature indication at format-time.

--- trunk/abjad/meter/interface.py
+++ trunk/abjad/meter/interface.py
@@ -15,34 +15,53 @@ class _MeterInterface(_Interface, _GrobHandler):
 
    ### PRIVATE ATTRIBUTES ###
 
-   ### NOTE: _MeterInterface formats only _opening and not _before.
-   ###       The reason for this is that LilyPond meter indications
-   ###       of the form \time 5/16 need print only once,
-   ###       from the measure, rather than printing twice, once from
-   ###       the measure and once from the first leaf in measure.
-   ###       There's a larger question here about which component(s)
-   ###       are to be responsible for printing meter indications
-   ###       at format-time.
+   @property
+   def _before(self):
+      '''List of formatting contributions at _before location.'''
+      return self._opening
 
    @property
    def _opening(self):
+      '''List of formatting contributions at _opening location.'''
       result = [ ]
       result.extend(_GrobHandler._before.fget(self))
-      effective = self.effective
-      #if not effective.suppress:
-      #if not self.suppress and not effective.suppress:
+      if self._selfShouldContribute:
+         result.append(self.effective.format)
+      return result
+
+   @property
+   def _selfCanContribute(self):
+      r'''True when self is able to contribute LilyPond \time.'''
       if not self.suppress:
          if self._client.kind('DynamicMeasure'):
-            result.append(self.effective.format)
-         else:
-            if self.forced or self.change:
-               result.append(self.effective.format)
-      return result
+            return True
+         elif self.forced or self.change:
+            return True
+      return False
+
+   @property
+   def _selfShouldContribute(self):
+      r'''True when self should contribute LilyPond \time.'''
+      return self._selfCanContribute and not self._parentCanContribute
+
+   @property
+   def _parentCanContribute(self):
+      r'''True when any parent, other than self, can contribute LP \time.'''
+      parentage = self._client.parentage.parentage[1:]
+      for parent in parentage:
+         try:
+            if parent.meter._selfCanContribute:
+               return True
+         except AttributeError:
+            pass
+      return False
 
    ### PUBLIC ATTRIBUTES ###
 
    @property
    def change(self):
+      '''True if meter of client differs from 
+         meter of component previous to client.'''
       client = self._client
       return bool(client.prev and \
          client.prev.meter.effective != self.effective)
@@ -52,6 +71,7 @@ class _MeterInterface(_Interface, _GrobHandler):
 
    @property
    def effective(self):
+      '''Return reference to meter effectively governing client.'''
       client = self._client
       if client.kind('DynamicMeasure'):
          if client.denominator:
@@ -72,6 +92,7 @@ class _MeterInterface(_Interface, _GrobHandler):
 
    @apply
    def forced( ):
+      '''Read / write attribute to set meter explicitly.'''
       def fget(self):
          return self._forced
       def fset(self, arg):
@@ -88,6 +109,8 @@ class _MeterInterface(_Interface, _GrobHandler):
 
    @apply
    def suppress( ):
+      r'''Read / write attribute to suppress contribution
+         of LilyPond \time indication at format-time.'''
       def fget(self):
          return self._suppress
       def fset(self, arg):
