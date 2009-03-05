8c9b1edabe815d84804484c35c87e09c6681f46c
Víctor Adán <vctradn@gmail.com>
2009-03-05 19:13:12 +0000

    Updated clef formatting in test_tuplet_formatter_reveal_format_contributions.py.

--- trunk/abjad/tuplet/test/test_tuplet_formatter_reveal_format_contributions.py
+++ trunk/abjad/tuplet/test/test_tuplet_formatter_reveal_format_contributions.py
@@ -12,15 +12,15 @@ def test_tuplet_formatter_reveal_format_locations_01( ):
       <_AccidentalInterface>
          #(set-accidental-style 'forget)
       <_ClefInterface>
-         \clef treble
+         \clef "treble"
    _opening
       <_AccidentalInterface>
          #(set-accidental-style 'forget)
       <_ClefInterface>
-         \clef treble
+         \clef "treble"
    _closing
       <_BarLineInterface>
          \bar "|."
    '''
 
-   assert t.formatter._revealFormatContributions( ) == '_before\n\t<_AccidentalInterface>\n\t\t#(set-accidental-style \'forget)\n\t<_ClefInterface>\n\t\t\\clef treble\n_opening\n\t<_AccidentalInterface>\n\t\t#(set-accidental-style \'forget)\n\t<_ClefInterface>\n\t\t\\clef treble\n_closing\n\t<_BarLineInterface>\n\t\t\\bar "|."\n'
+   assert t.formatter._revealFormatContributions( ) == '_before\n\t<_AccidentalInterface>\n\t\t#(set-accidental-style \'forget)\n\t<_ClefInterface>\n\t\t\\clef "treble"\n_opening\n\t<_AccidentalInterface>\n\t\t#(set-accidental-style \'forget)\n\t<_ClefInterface>\n\t\t\\clef "treble"\n_closing\n\t<_BarLineInterface>\n\t\t\\bar "|."\n'
