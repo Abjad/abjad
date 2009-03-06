7fcd9080e0ea9d33acb754b8c785d824e9cfa3f8
Trevor Bača <trevorbaca@gmail.com>
2009-03-06 20:29:41 +0000

    New _components_likely_multiplier(components) private helper.
    The helper looks at all tie chains in components list and checks to see if they all exhibit the same (non-assignable) factor in the numerator of their written durations. The intent of the function is to help undo measure subsumption.
    
       t = Staff(scale(4))
       container_contents_scale(t, Rational(5, 4))
       assert _components_likely_multiplier(t[:]) == Rational(5, 4)
    
    See helpers/test/test_components_likely_multiplier.py for this and five other examples.

---
+++ trunk/abjad/helpers/components_likely_multiplier.py
@@ -0,0 +1,32 @@
+from abjad.helpers.are_components import _are_components
+from abjad.helpers.is_tie_chain import _is_tie_chain
+from abjad.helpers.iterate_chained_contents import iterate_chained_contents
+from abjad.helpers.next_least_power_of_two import _next_least_power_of_two
+from abjad.helpers.tie_chain_written import tie_chain_written
+from abjad.helpers.unique import unique
+from abjad.rational.rational import Rational
+
+
+def _components_likely_multiplier(components):
+   '''Heuristic function to guess at a likely multiplier
+      that may have been applied to the components in list
+      at some point during a previous composition-time transform.
+
+      Otherwise, return None.
+
+      Implemented to help reverse measure subsumption.'''
+
+   assert _are_components(components)
+
+   chain_duration_numerators = [ ]
+   for expr in iterate_chained_contents(components):
+      if _is_tie_chain(expr):
+         chain_duration = tie_chain_written(expr)   
+         chain_duration_numerators.append(chain_duration._n)
+       
+   if len(unique(chain_duration_numerators)) == 1:
+      numerator = chain_duration_numerators[0]
+      denominator = _next_least_power_of_two(numerator)
+      likely_multiplier = Rational(numerator, denominator)
+      if likely_multiplier != Rational(1):
+         return likely_multiplier
---
+++ trunk/abjad/helpers/test/test_components_likely_multiplier.py
@@ -0,0 +1,52 @@
+from abjad import *
+from abjad.helpers.components_likely_multiplier import _components_likely_multiplier
+from abjad.tools import construct
+
+
+def test_components_likely_multiplier_01( ):
+   '''Components were likely multiplier by 5/4.'''
+
+   t = Staff(scale(4))
+   container_contents_scale(t, Rational(5, 4)) 
+   assert _components_likely_multiplier(t[:]) == Rational(5, 4)
+
+
+def test_components_likely_multiplier_02( ):
+   '''Components were likely multiplier by 3/2.'''
+
+   t = Staff(scale(4))
+   container_contents_scale(t, Rational(3, 2)) 
+   assert _components_likely_multiplier(t[:]) == Rational(3, 2)
+
+
+def test_components_likely_multiplier_03( ):
+   '''Components were likely multiplier by 4/4.'''
+
+   t = Staff(scale(4))
+   container_contents_scale(t, Rational(7, 4)) 
+   assert _components_likely_multiplier(t[:]) == Rational(7, 4)
+
+
+def test_components_likely_multiplier_04( ):
+   '''Components likely multiplier not recoverable.'''
+
+   t = Staff(scale(4))
+   container_contents_scale(t, Rational(2)) 
+   assert _components_likely_multiplier(t[:]) is None
+
+
+def test_components_likely_multiplier_05( ):
+   '''Components likely multiplier not recoverable.'''
+
+   t = Staff(scale(4))
+   container_contents_scale(t, Rational(1, 2)) 
+   assert _components_likely_multiplier(t[:]) is None
+
+
+def test_components_likely_multiplier_06( ):
+   '''Components multiplier recoverable only to within one power of two.'''
+
+   t = Staff(scale(4))
+   container_contents_scale(t, Rational(10, 4)) 
+   assert not _components_likely_multiplier(t[:]) == Rational(10, 4)
+   assert _components_likely_multiplier(t[:]) == Rational(5, 4)
