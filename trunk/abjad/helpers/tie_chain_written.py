6e4667c5fd6fccc14ae12c01fde05537a38017b9
Trevor Bača <trevorbaca@gmail.com>
2009-03-06 20:19:41 +0000

    New tie_chain_writte(tie_chain) public helper.
    Extensions to the public interface: you can now say tie_chain_written(tie_chain) for any tie chain. The helper returns the sum of the written duration of all leaves in chain.
    
       notes = construct.notes(0, [(5, 16)])
       assert tie_chain_written(notes[0].tie.chain) == Rational(5, 16)
    
    See helpers/test/test_tie_chain_written.py for this and one other example.

---
+++ trunk/abjad/helpers/tie_chain_written.py
@@ -0,0 +1,10 @@
+from abjad.exceptions.exceptions import MissingSpannerError
+from abjad.helpers.is_tie_chain import _is_tie_chain
+
+
+def tie_chain_written(tie_chain):
+   assert _is_tie_chain(tie_chain)
+   try:
+      return tie_chain[0].tie.spanner.written
+   except MissingSpannerError:
+      return tie_chain[0].duration.written
