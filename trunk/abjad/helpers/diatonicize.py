32bd27796810812e2610f37ac46d31977ed6276d
Trevor Bača <trevorbaca@gmail.com>
2009-03-06 20:15:41 +0000

    diatonicize( ) helper now handles tie chains correctly.
       t = Staff(construct.notes(0, [(5, 32)] * 4))
       diatonicize(t)
    
       \new Staff {
          c'8 ~
          c'32
          d'8 ~
          d'32
          e'8 ~
          e'32
          f'8 ~
          f'32
       }
    
    See helpers/test/test_diatonicize.py for this example.

--- trunk/abjad/helpers/diatonicize.py
+++ trunk/abjad/helpers/diatonicize.py
@@ -1,7 +1,7 @@
-from abjad.helpers.instances import instances
+from abjad.chord.chord import Chord
+from abjad.helpers.iterate_tie_chains import iterate_tie_chains
+from abjad.note.note import Note
 
-
-### TODO - write tests
 
 def diatonicize(expr):
    '''
@@ -12,11 +12,13 @@ def diatonicize(expr):
 
    diatonic_residues = (0, 2, 4, 5, 7, 9, 11)
    length = len(diatonic_residues)
-   for i, x in enumerate(instances(expr, '_Leaf')):
+   for i, tie_chain in enumerate(iterate_tie_chains(expr)):
       pitch = int(i / length) * 12 + diatonic_residues[i % length] 
-      if x.kind('Note'):
-         x.pitch = pitch
-      elif x.kind('Chord'):
-         x.pitches = [pitch] 
+      if isinstance(tie_chain[0], Note):
+         for note in tie_chain:
+            note.pitch = pitch
+      elif isinstance(tie_chain[0], Chord):
+         for chord in tie_chain:
+            chord.pitches = [pitch]
       else:
          pass
