6c3a0c268062f06e3b286870415a4c8694659f6b
Trevor Bača <trevorbaca@gmail.com>
2009-03-06 20:26:41 +0000

    New iterate_chained_contents(expr) public helper.
    Extensions to the public interface: you can now say iterate_chained_contents(expr) for any Abjad expression. The helper returns successive tie chains and containers at the *top level* of expr.
    
       t = Staff(construct.notes(0, [(5, 32)] * 4))
       t.insert(4, FixedDurationTuplet((2, 8), run(3)))
       diatonicize(t)
    
       \new Staff {
          c'8 ~
          c'32
          \times 2/3 {
             d'8
             e'8
             f'8
          }
          g'8 ~
          g'32
          a'8 ~
          a'32
          b'8 ~
          b'32
       }
    
       chained_contents = list(iterate_chained_contents(t))
    
       assert chained_contents[0] == t[0].tie.chain
       assert chained_contents[1] == t[2].tie.chain
       assert chained_contents[2] is t[4]
       assert chained_contents[3] == t[5].tie.chain
       assert chained_contents[4] == t[7].tie.chain
    
    See helpers/test/test_iterate_chained_contents.py for this example.

---
+++ trunk/abjad/helpers/iterate_chained_contents.py
@@ -0,0 +1,23 @@
+from abjad.container.container import Container
+from abjad.leaf.leaf import _Leaf
+from abjad.exceptions.exceptions import TieChainError
+
+
+def iterate_chained_contents(expr):
+   '''Iterate expr but return tie chains in place of leaves.
+      Crossing ties raise TieChainError.'''
+
+   if isinstance(expr, _Leaf):
+      if len(expr.tie.chain) == 1:
+         yield expr.tie.chain
+      else:
+         raise TieChainError('can not only one leaf in tie chain.')
+   elif isinstance(expr, (list, Container)):
+      for component in expr:
+         if isinstance(component, _Leaf):
+            if not component.tie.spanned or component.tie.last:
+               yield component.tie.chain
+         elif isinstance(component, Container):
+            yield component
+   else:
+      raise ValueError('input must be iterable.')
---
+++ trunk/abjad/helpers/test/test_iterate_chained_contents.py
@@ -0,0 +1,36 @@
+from abjad import *
+from abjad.tools import construct
+
+
+def test_iterate_chained_contents_01( ):
+   '''Iterate toplevel contents with tie chains in place of leaves.'''
+
+   t = Staff(construct.notes(0, [(5, 32)] * 4))
+   t.insert(4, FixedDurationTuplet((2, 8), run(3)))
+   diatonicize(t)
+
+   r'''
+   \new Staff {
+      c'8 ~
+      c'32
+      \times 2/3 {
+         d'8
+         e'8
+         f'8
+      }
+      g'8 ~
+      g'32
+      a'8 ~
+      a'32
+      b'8 ~
+      b'32
+   }
+   '''
+
+   chained_contents = list(iterate_chained_contents(t))
+
+   assert chained_contents[0] == t[0].tie.chain
+   assert chained_contents[1] == t[2].tie.chain
+   assert chained_contents[2] is t[4]
+   assert chained_contents[3] == t[5].tie.chain
+   assert chained_contents[4] == t[7].tie.chain
