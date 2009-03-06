7a9a8a8a00b62de9440d3452b8811206285e7364
Trevor Bača <trevorbaca@gmail.com>
2009-03-06 00:21:52 +0000

    New components_parentage_detach(components, level = 'top') public helper.
    Extensions to the public inteface: you can now say components_parentage_detach(components, level) for any list of Abjad components. The components need not be successive. When level = 'top', the helper detaches the parent of every Abjad component at the top level of list. When level = 'all', the helper detaches the parent of every Abjad component at all levels of list.
    
       t = Staff([Voice(scale(4))])
       v = t[0]
       notes = v[:]
    
       components_parentage_detach([t], level = 'all')
    
       assert _are_orphan_components(list(iterate(t, '_Component')))
    
    See helpers/test/test_components_parentage_detach.py for this example and one other.

---
+++ trunk/abjad/helpers/components_parentage_detach.py
@@ -0,0 +1,34 @@
+from abjad.helpers.are_components import _are_components
+from abjad.helpers.iterate import iterate
+
+
+def components_parentage_detach(components, level = 'top'):
+   '''Detach parent from every Abjad component at
+      specified level in list.
+
+      Components need not be successive.
+
+      Compare with spanners_detach(component, level = 'top').
+
+      Return newly orphaned components.'''
+
+   assert _are_components(components) 
+
+   if level == 'all':
+      return _components_parentage_detach_all(components)
+   elif level == 'top':
+      return _components_parentage_detach_top(components)
+   else:
+      raise ValueError("level must be 'top' or 'all'.")
+
+
+def _components_parentage_detach_all(components):
+   for component in list(iterate(components, '_Component')):
+      component.parentage.detach( )
+   return components
+
+
+def _components_parentage_detach_top(components):
+   for component in components:
+      component.parentage.detach( )
+   return components
---
+++ trunk/abjad/helpers/test/test_components_parentage_detach.py
@@ -0,0 +1,26 @@
+from abjad.helpers.are_orphan_components import _are_orphan_components
+from abjad import *
+
+
+def test_components_parentage_detach_01( ):
+   '''Detach parent from Abjad components at top level of list.'''
+
+   t = Staff([Voice(scale(4))])
+   v = t[0]
+   notes = v[:]
+
+   components_parentage_detach(v[:], level = 'top')
+
+   assert _are_orphan_components(notes)
+
+
+def test_components_parentage_detach_02( ):
+   '''Detach parent from Abjad components at all levels of list.'''
+
+   t = Staff([Voice(scale(4))])
+   v = t[0]
+   notes = v[:]
+
+   components_parentage_detach([t], level = 'all')
+
+   assert _are_orphan_components(list(iterate(t, '_Component')))
