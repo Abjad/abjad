28d044a84cc378718589dfa29749526c0da08b76
Trevor Bača <trevorbaca@gmail.com>
2009-03-06 20:13:59 +0000

    New _is_binary_equivalent_meter(meter) private helper.
    True when meter is binary or when meter is nonbinary but mathematically equivalent to some binary meter.
    
       assert not _is_binary_equivalent_meter(Meter(1, 12))
       assert not _is_binary_equivalent_meter(Meter(2, 12))
       assert _is_binary_equivalent_meter(Meter(3, 12))
       assert not _is_binary_equivalent_meter(Meter(4, 12))
       assert not _is_binary_equivalent_meter(Meter(5, 12))
       assert _is_binary_equivalent_meter(Meter(6, 12))
       assert not _is_binary_equivalent_meter(Meter(7, 12))
       assert not _is_binary_equivalent_meter(Meter(8, 12))
       assert _is_binary_equivalent_meter(Meter(9, 12))
       assert not _is_binary_equivalent_meter(Meter(10, 12))
       assert not _is_binary_equivalent_meter(Meter(11, 12))
       assert _is_binary_equivalent_meter(Meter(12, 12))
    
    See helpers/test/test_is_binary_equivalent_meter.py for this and two other examples.

---
+++ trunk/abjad/helpers/is_binary_equivalent_meter.py
@@ -0,0 +1,18 @@
+from abjad.helpers.is_power_of_two import _is_power_of_two
+from abjad.meter.meter import Meter
+from abjad.rational.rational import Rational
+
+
+def _is_binary_equivalent_meter(meter):
+   '''True when meter is binary or when meter is nonbinary
+      but mathematically equivalent to some binary meter,
+      otherwise False.'''
+   
+   # check input
+   assert isinstance(meter, Meter)
+
+   # express meter as rational and reduce to relatively prime terms
+   meter_as_rational = Rational(meter.numerator, meter.denominator)
+
+   # return True if reduced meter denominator is power of two
+   return _is_power_of_two(meter_as_rational._d)
