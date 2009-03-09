79cda71d7f429fa8b600e24d49decfbc6d22e69c
Trevor Bača <trevorbaca@gmail.com>
2009-03-09 23:30:58 +0000

    container_split(container, index) helper has been resurrected.
    The new container_split(container, index) helper works almost like the old container_split(container, index) helper, but with one change. Both helpers support the same interface. But the old container_split(container, index) helper implemented a modify-original-insert-one-copy pattern. The new container_split(container, index) helper wraps container_hew(container, index) and implements an empty-original-insert-two-copies pattern instead.
    
    Example:
    
       t = Voice(FixedDurationTuplet((2, 8), run(3)) * 2)
       tuplet = t[1]
       diatonicize(t)
       Beam(t[:])
    
       \new Voice {
               \times 2/3 {
                       c'8 [
                       d'8
                       e'8
               }
               \times 2/3 {
                       f'8
                       g'8
                       a'8 ]
               }
       }
    
       \new Voice {
               \times 2/3 {
                       c'8 [
                       d'8
                       e'8
               }
               \times 2/3 {
                       f'8 ]
               }
               \times 2/3 {
                       g'8 [
                       a'8 ]
               }
       }
    
       assert check(t)
       assert left.format == "\\times 2/3 {\n\tf'8 ]\n}"
       assert right.format == "\\times 2/3 {\n\tg'8 [\n\ta'8 ]\n}"
       assert tuplet.format == ''
       assert t.format == "\\new Voice {\n\t\\times 2/3 {\n\t\tc'8 [\n\t\td'8\n\t\te'8\n\t}\n\t\\times 2/3 {\n\t\tf'8 ]\n\t}\n\t\\times 2/3 {\n\t\tg'8 [\n\t\ta'8 ]\n\t}\n}"
    
    Note that differences appear between container_split(container, index) and container_hew(container, index) *ONLY WHEN DEALING WITH BEAMS*. Specifically, container_hew( ) *preserves* beams while container_split( ) *fractures* beams.
    
    (In fact, it might even make the most sense to rename container_split( ) as container_fracture( ) to suggest beaming fracturing.)
    
    See helpers/test/test_container_split.py for many examples.

--- trunk/abjad/helpers/container_split.py
+++ trunk/abjad/helpers/container_split.py
@@ -1,14 +1,54 @@
+from abjad.helpers.container_hew import container_hew
 
-#def container_split(container, index):
-#   '''
-#   Splits container in two at given index position.
-#   '''
-#   if index == 0 or index > len(container):
-#      return [container, None]
-#   c1 = container
-#   c2 = container.copy( )
-#   del(c1[index:])
-#   del(c2[0:index])
-#   if container._parent:
-#      container._parent[index:index] = [c2]
-#   return [c1, c2]
+
+def container_split(container, index):
+   r'''Splits container in two at given index position.
+      Compare with container_hew(container, index).
+
+      Both helpers break container just before index.
+      However, container_hew( ) *preserves* spanners.
+      And container_split( ) *fractures* spanners.
+
+      Both helpers create two new copies of container.
+      Both helpers empty original container of contents.
+
+      Return split parts.
+      
+      Example of splitting a beamed triplet in voice:
+
+      t = Voice(FixedDurationTuplet((2, 8), run(3)) * 2)
+      tuplet = t[1]
+      diatonicize(t)
+      Beam(t[:])
+
+      \new Voice {
+              \times 2/3 {
+                      c'8 [
+                      d'8
+                      e'8
+              }
+              \times 2/3 {
+                      f'8
+                      g'8
+                      a'8 ]
+              }
+      }
+
+      left, right = container_split(tuplet, 1)
+
+      \new Voice {
+              \times 2/3 {
+                      c'8 [
+                      d'8
+                      e'8
+              }
+              \times 2/3 {
+                      f'8 ]
+              }
+              \times 2/3 {
+                      g'8 [
+                      a'8 ]
+              }
+      }'''
+
+   return container_hew(container, index, spanners = 'fracture')
---
+++ trunk/abjad/helpers/test/test_container_split.py
@@ -0,0 +1,258 @@
+from abjad import *
+import py.test
+
+
+def test_container_split_01( ):
+   '''Split beamed triplet.'''
+
+   t = Voice(FixedDurationTuplet((2, 8), run(3)) * 2)
+   tuplet = t[1]
+   diatonicize(t)
+   Beam(t[:])
+
+   r'''
+   \new Voice {
+           \times 2/3 {
+                   c'8 [
+                   d'8
+                   e'8
+           }
+           \times 2/3 {
+                   f'8
+                   g'8
+                   a'8 ]
+           }
+   }
+   '''
+
+   left, right = container_split(tuplet, 1)
+
+   r'''
+   \new Voice {
+           \times 2/3 {
+                   c'8 [
+                   d'8
+                   e'8
+           }
+           \times 2/3 {
+                   f'8 ]
+           }
+           \times 2/3 {
+                   g'8 [
+                   a'8 ]
+           }
+   }
+   '''
+
+   assert check(t)
+   assert left.format == "\\times 2/3 {\n\tf'8 ]\n}"
+   assert right.format == "\\times 2/3 {\n\tg'8 [\n\ta'8 ]\n}"
+   assert tuplet.format == ''
+   assert t.format == "\\new Voice {\n\t\\times 2/3 {\n\t\tc'8 [\n\t\td'8\n\t\te'8\n\t}\n\t\\times 2/3 {\n\t\tf'8 ]\n\t}\n\t\\times 2/3 {\n\t\tg'8 [\n\t\ta'8 ]\n\t}\n}"
+
+
+def test_container_split_02( ):
+   '''Split binary measure.'''
+
+   t = Voice(RigidMeasure((3, 8), run(3)) * 2)
+   m = t[1]
+   Beam(t[:])
+   diatonicize(t)
+
+   r'''
+   \new Voice {
+                   \time 3/8
+                   c'8 [
+                   d'8
+                   e'8
+                   \time 3/8
+                   f'8
+                   g'8
+                   a'8 ]
+   }
+   '''
+
+   left, right = container_split(m, 1)
+
+   r'''
+   \new Voice {
+                   \time 3/8
+                   c'8 [
+                   d'8
+                   e'8
+                   \time 1/8
+                   f'8 ]
+                   \time 2/8
+                   g'8 [
+                   a'8 ]
+   }
+   '''
+
+   assert check(t)
+   assert left.format == "\t\\time 1/8\n\tf'8 ]"
+   assert right.format == "\t\\time 2/8\n\tg'8 [\n\ta'8 ]"
+   assert py.test.raises(UnderfullMeasureError, 'm.format')
+   assert t.format == "\\new Voice {\n\t\t\\time 3/8\n\t\tc'8 [\n\t\td'8\n\t\te'8\n\t\t\\time 1/8\n\t\tf'8 ]\n\t\t\\time 2/8\n\t\tg'8 [\n\t\ta'8 ]\n}"
+
+
+def test_container_split_03( ):
+   '''Split nonbinary measure.'''
+
+   t = Voice(RigidMeasure((3, 9), run(3)) * 2)
+   m = t[1]
+   Beam(t[:])
+   diatonicize(t)
+
+   r'''
+   \new Voice {
+                   \time 3/9
+                   \scaleDurations #'(8 . 9) {
+                           c'8 [
+                           d'8
+                           e'8
+                   }
+                   \time 3/9
+                   \scaleDurations #'(8 . 9) {
+                           f'8
+                           g'8
+                           a'8 ]
+                   }
+   }
+   '''
+
+   left, right = container_split(m, 1)
+
+   r'''
+   \new Voice {
+                   \time 3/9
+                   \scaleDurations #'(8 . 9) {
+                           c'8 [
+                           d'8
+                           e'8
+                   }
+                   \time 1/9
+                   \scaleDurations #'(8 . 9) {
+                           f'8 ]
+                   }
+                   \time 2/9
+                   \scaleDurations #'(8 . 9) {
+                           g'8 [
+                           a'8 ]
+                   }
+   }
+   '''
+
+   assert check(t)
+   assert left.format == "\t\\time 1/9\n\t\\scaleDurations #'(8 . 9) {\n\t\tf'8 ]\n\t}"
+   assert right.format == "\t\\time 2/9\n\t\\scaleDurations #'(8 . 9) {\n\t\tg'8 [\n\t\ta'8 ]\n\t}"
+   assert py.test.raises(UnderfullMeasureError, 'm.format')
+   assert t.format == "\\new Voice {\n\t\t\\time 3/9\n\t\t\\scaleDurations #'(8 . 9) {\n\t\t\tc'8 [\n\t\t\td'8\n\t\t\te'8\n\t\t}\n\t\t\\time 1/9\n\t\t\\scaleDurations #'(8 . 9) {\n\t\t\tf'8 ]\n\t\t}\n\t\t\\time 2/9\n\t\t\\scaleDurations #'(8 . 9) {\n\t\t\tg'8 [\n\t\t\ta'8 ]\n\t\t}\n}"
+
+
+def test_container_split_04( ):
+   '''A single container can be split in two by the middle.
+      No parent.'''
+
+   t = Voice(scale(4))
+   Beam(t[:])
+
+   r'''
+   \new Voice {
+      c'8 [
+      d'8
+      e'8
+      f'8 ]
+   }
+   '''
+
+   left, right = container_split(t, 2)
+
+   r'''
+   \new Voice {
+           c'8 [
+           d'8
+   }
+   '''
+
+   r'''
+   \new Voice {
+           e'8
+           f'8 ]
+   }
+   '''
+
+   assert left.format == "\\new Voice {\n\tc'8 [\n\td'8\n}"
+   assert right.format == "\\new Voice {\n\te'8\n\tf'8 ]\n}"
+   assert t.format == '\\new Voice {\n}'
+
+
+def test_container_split_05( ):
+   '''A single container 'split' at index 0 gives
+      an empty lefthand part and a complete righthand part.
+      Original container empties contents.'''
+
+   t = Staff([Voice(scale(4))])
+   v = t[0]
+   Beam(v)
+
+   r'''
+   \new Staff {
+           \new Voice {
+                   c'8 [
+                   d'8
+                   e'8
+                   f'8 ]
+           }
+   }
+   '''
+
+   left, right = container_split(v, 0)
+
+   r'''
+   \new Staff {
+           \new Voice {
+           }
+           \new Voice {
+                   c'8 [
+                   d'8
+                   e'8
+                   f'8 ]
+           }
+   }
+   '''
+
+   assert left.format == '\\new Voice {\n}'
+   assert right.format == "\\new Voice {\n\tc'8 [\n\td'8\n\te'8\n\tf'8 ]\n}"
+   assert v.format == '\\new Voice {\n}'
+   assert t.format == "\\new Staff {\n\t\\new Voice {\n\t}\n\t\\new Voice {\n\t\tc'8 [\n\t\td'8\n\t\te'8\n\t\tf'8 ]\n\t}\n}"
+
+
+def test_container_split_06( ):
+   '''Split container at index > len(container).
+      Lefthand part instantiates with all contents.
+      Righthand part instantiates empty.
+      Original container empties contents.'''
+
+   t = Staff([Voice(scale(4))])
+   v = t[0]
+   Beam(v)
+
+   left, right = container_split(v, 10)
+
+   r'''
+   \new Staff {
+           \new Voice {
+                   c'8 [
+                   d'8
+                   e'8
+                   f'8 ]
+           }
+           \new Voice {
+           }
+   }
+   '''
+
+   assert left.format == "\\new Voice {\n\tc'8 [\n\td'8\n\te'8\n\tf'8 ]\n}"
+   assert right.format == '\\new Voice {\n}'
+   assert v.format == '\\new Voice {\n}'
+   assert t.format == "\\new Staff {\n\t\\new Voice {\n\t\tc'8 [\n\t\td'8\n\t\te'8\n\t\tf'8 ]\n\t}\n\t\\new Voice {\n\t}\n}"
