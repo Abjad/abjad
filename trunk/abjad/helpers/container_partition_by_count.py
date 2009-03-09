38ce5bd2916873b90f1f590660178a469a058906
Trevor Bača <trevorbaca@gmail.com>
2009-03-09 22:17:34 +0000

    Detailed container_partition_by_counts(container, counts) doc string.

--- trunk/abjad/helpers/container_partition_by_count.py
+++ trunk/abjad/helpers/container_partition_by_count.py
@@ -7,10 +7,40 @@ from abjad.helpers.pairwise import pairwise
 ## TODO: Possibly implement this as a generalization of lcopy( )?
 
 def container_partition_by_count(container, counts):
-   '''Return list of new copies of container;
-      Lenght of the elements in return list equal,
-      in order, the integer elements in 'counts'.'''
+   r'''container is any Abjad container to partition.
+      counts is a Python list of zero or more positive integers.
 
+      Partition container and all parents of container.
+      Do not act in place.
+      Instead, return list of parts equal in number to len(counts).
+
+      The helpers wraps lcopy( ).
+      This means that the original structure remains unchanged.
+      Also that resulting parts cut all the way up into voice.
+   
+      Example:
+
+      t = Voice([FixedDurationTuplet((2, 8), scale(3))])
+      Beam(t[0][:])
+      left, right = container_partition_by_count(t[0], [1, 2])
+
+      left:
+
+      \new Voice {
+              \times 2/3 {
+                      c'8 [ ]
+              }
+      } 
+
+      right:
+
+      \new Voice {
+              \times 2/3 {
+                      d'8 [
+                      e'8 ]
+              }
+      }'''
+   
    assert isinstance(container, Container)
    assert all([isinstance(x, int) for x in counts])
 
