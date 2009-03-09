a56fdcf3e8812e16bfb3227ac1518e993edb6062
Trevor Bača <trevorbaca@gmail.com>
2009-03-09 12:17:35 +0000

    Added comment asking about algorithm efficiency in _OffsetInterface.

--- trunk/abjad/offset/interface.py
+++ trunk/abjad/offset/interface.py
@@ -26,12 +26,17 @@ class _OffsetInterface(_Interface):
 
    ## OBSERVER PATTERN INTERFACE ##
 
+   ## TODO: Should this method be private?
    def update(self):
       self._updateContext( )
       self._updateScore( )
 
    ## TODO: these work fine now, but the method for computing the offset
    ## seems suboptimal. For one, Parengate._threadParentage might deprecate. 
+   ## TODO: Wouldn't this method be better implemented by finding the
+   ##       'root' of the context in which self._client lives,
+   ##       and then performing a single depth-first search (using _DFS)
+   ##       to visit each node-in-context only once?
    def _updateContext(self):
       offset = Rational(0, 1)
       prev = self._client._navigator._prev
@@ -41,6 +46,7 @@ class _OffsetInterface(_Interface):
          prev = prev._navigator._prev
       self._context = offset
 
+   ## TODO: See comment above about using depth-first search.
    def _updateScore(self):
       offset = Rational(0, 1)
       prev = self._client._navigator._prev
