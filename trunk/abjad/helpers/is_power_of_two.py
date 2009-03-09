c8dc76514410e8df9ed23999904fd3b057751f14
Trevor Bača <trevorbaca@gmail.com>
2009-03-09 21:24:13 +0000

    Private _is_integer_power_of_two(expr) now accepts both integers and Rationals as input.
       assert _is_power_of_two(Rational(1, 1))
       assert _is_power_of_two(Rational(1, 2))
       assert not _is_power_of_two(Rational(1, 3))
       assert _is_power_of_two(Rational(1, 4))
       assert not _is_power_of_two(Rational(1, 5))
       assert not _is_power_of_two(Rational(1, 6))
       assert not _is_power_of_two(Rational(1, 7))
       assert _is_power_of_two(Rational(1, 8))
       assert not _is_power_of_two(Rational(1, 9))
       assert not _is_power_of_two(Rational(1, 10))
       assert not _is_power_of_two(Rational(1, 11))
       assert not _is_power_of_two(Rational(1, 12))

--- trunk/abjad/helpers/is_power_of_two.py
+++ trunk/abjad/helpers/is_power_of_two.py
@@ -1,2 +1,13 @@
-def _is_power_of_two(n):
-   return not bool(n & (n - 1))
+from abjad.rational.rational import Rational
+
+
+def _is_power_of_two(expr):
+   '''True when expr is an integer or Rational power of two,
+      otherwise False.'''
+
+   if isinstance(expr, int):
+      return not bool(expr & (expr - 1))
+   elif isinstance(expr, Rational):
+      return _is_power_of_two(expr._n * expr._d)
+   else:
+      return False
--- trunk/abjad/helpers/test/test_is_power_of_two.py
+++ trunk/abjad/helpers/test/test_is_power_of_two.py
@@ -3,7 +3,8 @@ from abjad.helpers.is_power_of_two import _is_power_of_two
 
 
 def test_is_power_of_two_01( ):
-   '''Return True when n is an integer power of two, otherwise False.'''
+   '''Return True when expr is an integer or Rational power of two, 
+      otherwise False.'''
 
    assert _is_power_of_two(0)
    assert _is_power_of_two(1)
@@ -21,7 +22,8 @@ def test_is_power_of_two_01( ):
 
 
 def test_is_power_of_two_02( ):
-   '''Return True when n is an integer power of two, otherwise False.'''
+   '''Return True when expr is an integer or Rational power of two, 
+      otherwise False.'''
 
    assert _is_power_of_two(0)
    assert not _is_power_of_two(-1)
@@ -36,3 +38,40 @@ def test_is_power_of_two_02( ):
    assert not _is_power_of_two(-10)
    assert not _is_power_of_two(-11)
    assert not _is_power_of_two(-12)
+
+
+def test_is_power_of_two_03( ):
+   '''Return True when expr is an integer or Rational power of two, 
+      otherwise False.'''
+
+   assert _is_power_of_two(Rational(0))
+   assert _is_power_of_two(Rational(1))
+   assert _is_power_of_two(Rational(2))
+   assert not _is_power_of_two(Rational(3))
+   assert _is_power_of_two(Rational(4))
+   assert not _is_power_of_two(Rational(5))
+   assert not _is_power_of_two(Rational(6))
+   assert not _is_power_of_two(Rational(7))
+   assert _is_power_of_two(Rational(8))
+   assert not _is_power_of_two(Rational(9))
+   assert not _is_power_of_two(Rational(10))
+   assert not _is_power_of_two(Rational(11))
+   assert not _is_power_of_two(Rational(12))
+   
+
+def test_is_power_of_two_04( ):
+   '''Return True when expr is an integer or Rational power of two, 
+      otherwise False.'''
+
+   assert _is_power_of_two(Rational(1, 1))
+   assert _is_power_of_two(Rational(1, 2))
+   assert not _is_power_of_two(Rational(1, 3))
+   assert _is_power_of_two(Rational(1, 4))
+   assert not _is_power_of_two(Rational(1, 5))
+   assert not _is_power_of_two(Rational(1, 6))
+   assert not _is_power_of_two(Rational(1, 7))
+   assert _is_power_of_two(Rational(1, 8))
+   assert not _is_power_of_two(Rational(1, 9))
+   assert not _is_power_of_two(Rational(1, 10))
+   assert not _is_power_of_two(Rational(1, 11))
+   assert not _is_power_of_two(Rational(1, 12))
