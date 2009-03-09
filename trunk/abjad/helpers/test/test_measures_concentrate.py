87b1d26e438d17c2525587fb02d7e2fb4164237d
Trevor Bača <trevorbaca@gmail.com>
2009-03-09 21:57:52 +0000

    measures_concentrate(expr, concentration_pairs) now creates new meters correctly from plain old binary input.

--- trunk/abjad/helpers/test/test_measures_concentrate.py
+++ trunk/abjad/helpers/test/test_measures_concentrate.py
@@ -2,7 +2,9 @@ from abjad import *
 
 
 def test_measures_concentrate_01( ):
-   '''Concentrate one measure three times.'''
+   '''Concentrate one measure three times.
+      Meter 3/8 goes to 9/24.
+      Numerator and denominator both triple.'''
 
    t = RigidMeasure((3, 8), scale(3))
    Beam(t[:])
@@ -28,7 +30,9 @@ def test_measures_concentrate_01( ):
 
 
 def test_measures_concentrate_02( ):
-   '''Concentrate one measure four times over five.'''
+   '''Concentrate one measure four times over five.
+      Meter 3/16 goes to 12/80.
+      Numerator quadruples and denominator quintuples.'''
 
    t = RigidMeasure((3, 16), scale(3, Rational(1, 16)))
    Beam(t[:])
@@ -54,3 +58,55 @@ def test_measures_concentrate_02( ):
 
    assert check(t)
    assert t.format == "\t\\time 12/80\n\t\\scaleDurations #'(4 . 5) {\n\t\tc'64 [\n\t\td'64\n\t\te'64 ]\n\t\tc'64 [\n\t\td'64\n\t\te'64 ]\n\t\tc'64 [\n\t\td'64\n\t\te'64 ]\n\t\tc'64 [\n\t\td'64\n\t\te'64 ]\n\t}"
+
+
+def test_measures_concentrate_03( ):
+   '''Concentrate one measure four times over four.
+      Meter 3/16 goes to 12/64.
+      Numerator and denominator both quadruple.'''
+
+   t = RigidMeasure((3, 16), scale(3, Rational(1, 16)))
+   Beam(t[:])
+   measures_concentrate(t, [(4, 4)])
+
+   r'''
+        \time 12/64
+        c'64 [
+        d'64
+        e'64 ]
+        c'64 [
+        d'64
+        e'64 ]
+        c'64 [
+        d'64
+        e'64 ]
+        c'64 [
+        d'64
+        e'64 ]
+   '''
+
+   assert check(t)
+   assert t.format == "\t\\time 12/64\n\tc'64 [\n\td'64\n\te'64 ]\n\tc'64 [\n\td'64\n\te'64 ]\n\tc'64 [\n\td'64\n\te'64 ]\n\tc'64 [\n\td'64\n\te'64 ]"
+
+
+def test_measures_concentrate_04( ):
+   '''Concentrate one measure two times over four.
+      Meter 3/16 goes to 6/64.
+      Numerator doubles and denominator quadruples.'''
+
+   t = RigidMeasure((3, 16), scale(3, Rational(1, 16)))
+   Beam(t[:])
+   measures_concentrate(t, [(2, 4)])
+
+   r'''
+        \time 6/64
+        c'64 [
+        d'64
+        e'64 ]
+        c'64 [
+        d'64
+        e'64 ]
+   '''
+
+   assert check(t)
+   assert t.format == "\t\\time 6/64\n\tc'64 [\n\td'64\n\te'64 ]\n\tc'64 [\n\td'64\n\te'64 ]"
