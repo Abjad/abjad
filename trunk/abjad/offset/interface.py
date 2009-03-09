b21dcd5c741bb6b2fc6d7abde1b92ea78b442ae5
Trevor Bača <trevorbaca@gmail.com>
2009-03-09 11:43:59 +0000

    Removed comments and cleaned up code in _UpdateInterface and _OffsetInterface. Preparing to copy _OffsetInterface to make new _NumberingInterface.

--- trunk/abjad/offset/interface.py
+++ trunk/abjad/offset/interface.py
@@ -10,14 +10,7 @@ class _OffsetInterface(_Interface):
       self._context = Rational(0)
       updateInterface._observers.append(self)
 
-
-   ### PUBLIC PROPERTIES ###
-
-   @property
-   def score(self):
-      if not self._client._update._currentToRoot:
-         self._client._update._updateAll( )
-      return self._score
+   ## PUBLIC ATTRIBUTES ##
 
    @property
    def context(self):
@@ -25,21 +18,24 @@ class _OffsetInterface(_Interface):
          self._client._update._updateAll( )
       return self._context
 
+   @property
+   def score(self):
+      if not self._client._update._currentToRoot:
+         self._client._update._updateAll( )
+      return self._score
 
-   ### OBSERVER PATTERN INTERFACE ###
+   ## OBSERVER PATTERN INTERFACE ##
 
    def update(self):
       self._updateContext( )
       self._updateScore( )
 
-   ### TODO: these work fine now, but the method for computing the offset
-   ### seems suboptimal. For one, Parengate._threadParentage might deprecate. 
+   ## TODO: these work fine now, but the method for computing the offset
+   ## seems suboptimal. For one, Parengate._threadParentage might deprecate. 
    def _updateContext(self):
       offset = Rational(0, 1)
       prev = self._client._navigator._prev
-      #self_parentage = self._client._parentage._threadParentage
       self_parentage = self._client.parentage._threadParentage
-      #while prev and prev._parentage._threadParentage == self_parentage:
       while prev and prev.parentage._threadParentage == self_parentage:
          offset += prev.duration.prolated
          prev = prev._navigator._prev
--- trunk/abjad/update/interface.py
+++ trunk/abjad/update/interface.py
@@ -1,14 +1,13 @@
 from abjad.core.interface import _Interface
-from abjad.rational.rational import Rational
 
-### UpdateInterface and OffsetInterface implement the Observer pattern. 
-### UpdateInterface holds and updates the state of the branch.
-### UpdateInterface holds a list of all the observers.
-### UpdateInterface calls ("notifies") all observers to update themselves
-### when requested by any observer.
-### Observers "pull" the state of the tree from the "subject" 
-### (UpdateInterface) and tell it to updateAll( ) if tree has changed.
 
+## UpdateInterface and OffsetInterface implement the Observer pattern. 
+## UpdateInterface holds and updates the state of the branch.
+## UpdateInterface holds a list of all the observers.
+## UpdateInterface calls ("notifies") all observers to update themselves
+## when requested by any observer.
+## Observers "pull" the state of the tree from the "subject"
+## (UpdateInterface) and tell it to updateAll( ) if tree has changed.
 
 class _UpdateInterface(_Interface):
 
@@ -17,37 +16,22 @@ class _UpdateInterface(_Interface):
       self._current = False
       self._observers = [ ]
 
-   ### PRIVATE ATTRIBUTES ###
+   ## PRIVATE ATTRIBUTES ##
 
    @property
    def _currentToRoot(self):
-      #for x in self._client._parentage._parentage:
-      #for x in self._client.parentage._parentage:
       for x in self._client.parentage.parentage:
          if not x._update._current:
             return False
       return True
       
-   ### PRIVATE METHODS ### 
+   ## PRIVATE METHODS ## 
 
    def _markForUpdateToRoot(self):
-      #for x in self._client._parentage._parentage:
-      #for x in self._client.parentage._parentage:
       for x in self._client.parentage.parentage:
          x._update._current = False
 
-#   def _updateAll(self):
-#      g = self._client._parentage._root._navigator._DFS( )
-#      score_offset = Rational(0)
-#      for node in g:
-#         if node.kind('_Leaf'):
-#            node.offset._special = score_offset
-#            score_offset += node.duration.prolated
-#         node._update._current = True
-
    def _updateAll(self):
-      #g = self._client._parentage._root._navigator._DFS( )
-      #g = self._client.parentage._root._navigator._DFS( )
       g = self._client.parentage.root._navigator._DFS( )
       for node in g:
          for o in node._update._observers:
