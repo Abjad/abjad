b70da55639130cd22341f7855cbed5ef3e7fbe0e
Trevor Bača <trevorbaca@gmail.com>
2009-03-11 15:36:35 +0000

    Renamed _ParentReceipt to _ParentageReceipt.

--- trunk/abjad/receipt/parentage.py
+++ trunk/abjad/receipt/parentage.py
@@ -1,6 +1,6 @@
-class _ParentReceipt(object):
-   '''Class to encapsulate parent of Abjad component
-      and index-in-parent of Abjad component.'''
+class _ParentageReceipt(object):
+   '''Class to encapsulate parent and index-in-parent 
+      of any Abjad component.'''
 
    def __init__(self, component, parent, index):
       self._component = component
