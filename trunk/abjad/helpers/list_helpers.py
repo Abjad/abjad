613c72b4a16d98da854c846046ecfbc59cd1df25
Trevor Bača <trevorbaca@gmail.com>
2009-03-11 11:03:58 +0000

    list_helpers(search_string) now accepts a more general search_string as input, rather than the less general start_string, as before.
    Example:
    
       abjad> list_helpers('x')
    
       Abjad r1185 implements 3 helpers containing the string 'x':
    
          cyclic_extend, excise, tuplet_contents_fix
    
    Also, list_helpers( ) no longer raises an IndexError when list_helpers( ) finds no helpers containing search_string.
    
    Example:
    
       abjad> list_helpers('foo')
    
       Abjad r1185 implements 0 helpers containing the string 'foo'.
    
    Note that there are still no tests for list_helpers(search_string) because the helper prints information but returns no values. And because the information that the helper prints depends strongly on the revision of Abjad that is running.

--- trunk/abjad/helpers/list_helpers.py
+++ trunk/abjad/helpers/list_helpers.py
@@ -4,14 +4,16 @@ from abjad.cfg.abjad_version import _get_abjad_version
 ## TODO: Would be nice to rename this function helpers( )
 ##       which will mean extending abjad/__init__.py
 
-def list_helpers(start_string = '', private = False):
-   '''List Abjad helpers starting with start_string.
+## TODO: Figure out how to write tests for this helper.
+
+def list_helpers(search_string = '', private = False):
+   '''List Abjad helpers containing search_string.
 
    Example:
 
    abjad> list_helpers('container')
 
-   Abjad r1027 implements 8 helpers starting in 'container':
+   Abjad r1027 implements 8 helpers containing 'container':
 
       container_contents_scale, container_glom_by_count, container_hew,
       container_partition_by_count, container_rest_by_count, 
@@ -31,29 +33,32 @@ def list_helpers(start_string = '', private = False):
 
    print ''
 
-   if start_string == '':
+   if search_string == '':
       total = len(helpers)
       print 'Abjad r%s implements %s total %shelpers:' % (
          version, total, scope_string)
    else:
-      helpers = [x for x in helpers if x.startswith(start_string)]
+      helpers = [x for x in helpers if search_string in x]
       total = len(helpers)
       number_suffix = '' if total == 1 else 's'
-      print "Abjad r%s implements %s %shelper%s starting in '%s':" % (
-         version, total, scope_string, number_suffix, start_string)
+      label_punctuation = ':' if total > 0 else '.'
+      label = "Abjad r%s implements %s %shelper%s "
+      label += "containing the string '%s'%s"
+      label %= (version, total, scope_string, number_suffix, 
+         search_string, label_punctuation)
+      print label
 
    if total > 0:
       print ''
-
-   line_width = 80
-   indent = '   '
-   output_line = indent + helpers[0]
-   for helper in helpers[1:]:
-      if len(output_line) + len(helper) > (line_width - 5):
-         print output_line + ','
-         output_line = indent + helper
-      else:
-         output_line += ', ' + helper
-   print output_line
+      line_width = 80
+      indent = '   '
+      output_line = indent + helpers[0]
+      for helper in helpers[1:]:
+         if len(output_line) + len(helper) > (line_width - 5):
+            print output_line + ','
+            output_line = indent + helper
+         else:
+            output_line += ', ' + helper
+      print output_line
 
    print ''
