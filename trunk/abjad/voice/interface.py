779be6e592ae1f1663d45d3c59b330fdcdbf62f1
Trevor Bača <trevorbaca@gmail.com>
2009-03-17 03:40:19 +0000

    Removed commented-out code in _VoiceInterface. _VoiceInterface is now slim again.

--- trunk/abjad/voice/interface.py
+++ trunk/abjad/voice/interface.py
@@ -7,8 +7,6 @@ class _VoiceInterface(_Interface, _FormatCarrier):
    def __init__(self, client):
       _Interface.__init__(self, client)
       _FormatCarrier.__init__(self)
-      ## TODO: Is _VoiceInterface._defaultSignature dead code?
-      self._defaultSignature = (0, )
       self.number = None
 
    ## PRIVATE ATTRIBUTES ##
@@ -25,17 +23,6 @@ class _VoiceInterface(_Interface, _FormatCarrier):
          result.append(voices[self.number])
       return result
 
-#   @property
-#   def _naiveSignature(self):
-#      '''Naive signature of this voice used to distinguish
-#         this voice from all other runtime objects.'''
-#      client = self._client
-#      if hasattr(client, 'invocation'):
-#         name = client.invocation.name
-#         if name is not None:
-#            return (name, )
-#      return (id(client), )
-
    ## TODO: Combine _opening and _before in many interfaces
    @property
    def _opening(self):
@@ -45,31 +32,6 @@ class _VoiceInterface(_Interface, _FormatCarrier):
 
    ## PUBLIC ATTRIBUTES ##
 
-#   @property
-#   def anonymous(self):
-#      '''True when this voice is not named, otherwise False.'''
-#      return not self.named
-#
-#   @property
-#   def default(self):
-#      '''TODO: Is _VoiceInterface.default dead code?'''
-#      return self.signature == self._defaultSignature
-#   
-#   @property
-#   def name(self):
-#      '''String name of context from which client voice signature derives,
-#         otherwise None.'''
-#      if self.named:
-#         return self.signature[0]
-#      else:
-#         return None
-#
-#   @property
-#   def named(self):
-#      '''True when voice signature first is string,
-#         otherwise False.'''
-#      return isinstance(self.signature[0], str)
-   
    @apply
    def number( ):
       '''LilyPond voice number 1 - 4 of this voice, or None.'''
@@ -80,57 +42,3 @@ class _VoiceInterface(_Interface, _FormatCarrier):
             raise ValueError('Voice number must be 1, 2, 3, 4 or None.')
          self._number = arg
       return property(**locals( ))
-
-#   @property
-#   def numeric(self):
-#      '''True when voice signature fist element is numeric, 
-#         otherwise False.'''
-#      first = self.signature[0]
-#      return isinstance(first, (int, long)) 
-#
-#   @property
-#   def signature(self):
-#      '''Return unique (id, ) 1-tuple of voice that governs client.
-#         TODO: Can't the implementation here be greatly simplified?
-#         TODO: Shouldn't this implement in _Parentage instead?
-#
-#         Notes:
-#
-#            * orphan leaves carry no voice signature
-#            * incorporated but noncontext leaves carry default voice'''
-#      from abjad.leaf.leaf import _Leaf
-#      from abjad.context.context import _Context
-#      parentage = self._client.parentage.parentage
-#      found_lilypond_expression = False
-#      signator = None
-#      for i, p in enumerate(parentage):
-#         if isinstance(p, _Leaf):
-#            signator = p
-#         elif isinstance(p, _Context) and not getattr(p, 'parallel', False):
-#            found_lilypond_expression = True
-#            return p.voice._naiveSignature 
-#         elif isinstance(p, _Context) and getattr(p, 'parallel', False):
-#            found_lilypond_expression = True
-#            if parentage.index(p) == 0:
-#               return p.voice._naiveSignature
-#            else:
-#               return parentage[i - 1].voice._naiveSignature
-#         elif not isinstance(p, _Context) and not getattr(p, 'parallel', False):
-#            found_lilypond_expression = True
-#            signator = p
-#         elif not isinstance(p, _Context) and getattr(p, 'parallel', False):
-#            found_lilypond_expression = True
-#            if p.parentage.orphan:
-#               if parentage.index(p) == 0:
-#                  return self._defaultSignature
-#               else:
-#                  return parentage[i - 1].voice._naiveSignature
-#            else:
-#               signator = p
-#         else:
-#            found_lilypond_expression = True
-#            raise ValueError('%s is unknown container.' % p)
-#      if found_lilypond_expression:
-#         return self._defaultSignature
-#      else:
-#         return None
