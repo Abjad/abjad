cb59e4184e8b4dbf64491ff332042bebae2f6f5e
Trevor Bača <trevorbaca@gmail.com>
2009-03-18 12:42:55 +0000

    Migrated incomplete components_copy( ) helper to use _are_strictly_contiguous_components_in_same_score( ) in place of _are_successive_components( ).

--- trunk/abjad/helpers/components_copy.py
+++ trunk/abjad/helpers/components_copy.py
@@ -1,11 +1,11 @@
 from abjad.exceptions.exceptions import ContiguityError
-from abjad.helpers.are_successive_components import _are_successive_components
+from abjad.helpers.are_strictly_contiguous_components_in_same_score import _are_strictly_contiguous_components_in_same_score
 from abjad.helpers.get_parent_and_index import _get_parent_and_index
 
 
 ## TODO: Finish implementation
 
-def components_copy(successive_components):
+def components_copy(components):
    '''Deep copy components.
       Copy the portions of all spanners attaching to components.
       Leave components unchanged.
@@ -14,9 +14,10 @@ def components_copy(successive_components):
       Example:'''
 
    # check input
-   if not _are_successive_components(components):
-      raise ContiguityError('input must be successive components.')
+   if not _are_strictly_contiguous_components_in_same_score(components):
+      raise ContiguityError(
+         'Input must be strictly contiguous components in same score.')
 
-   for component in successive_components:
+   for component in components:
 
       parent, index = _get_parent_and_index(component) 
