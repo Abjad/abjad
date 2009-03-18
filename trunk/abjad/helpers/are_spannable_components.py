3be46a9a1c3fbedbcf9da3be579b0fb6a7f94eeb
Trevor Bača <trevorbaca@gmail.com>
2009-03-18 16:56:02 +0000

    Reimplemented _are_spannable_components( ) to be merely a one-line wrapper around _are_thread_contiguous_components( ).

--- trunk/abjad/helpers/are_spannable_components.py
+++ trunk/abjad/helpers/are_spannable_components.py
@@ -1,25 +1,12 @@
-from abjad.helpers.are_strictly_contiguous_components_in_same_thread import _are_strictly_contiguous_components_in_same_thread
+from abjad.helpers.are_thread_contiguous_components import _are_thread_contiguous_components
 
-
-## TODO: Reimplement as wrapper around _are_thread_contiguous_components( ).
-## TODO: Implement _are_thread_contiguous_components( ).
 
 def _are_spannable_components(expr):
    '''True when expr is a Python list of Abjad components
       and when any one spanner can apply to all components in list.
       Otherwise False.
 
-      NOTE that this function is currently merely a literal wrapper
-      around _are_strictly_contiguous_components_in_same_thread( ).
-      The real code for this helper will be written once we go back 
-      and make the thread-identification stuff work perfectly.
+      This function is a one-line wrapper around
+      _are_thread_contiguous_components( ).'''
 
-      At this point _are_spannable_components( ) should be exactly
-      the same thing as the (currently nonexistent)
-      _are_thread_contiguous_components( ).
-
-      NOTE that you should use this _are_spannable_components( )
-      in any function that you want to EVENTUALLY use
-      _are_spannable_components( ), as a type of structural to-do.'''
-
-   return _are_strictly_contiguous_components_in_same_thread(expr)
+   return _are_thread_contiguous_components(expr)
