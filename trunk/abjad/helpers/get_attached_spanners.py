a4e26bd151240d2331d1246e806613cf5d90567f
Trevor Bača <trevorbaca@gmail.com>
2009-03-18 12:49:21 +0000

    Migrated _get_attached_spanners(components) to _are_strictly_contiguous_components_in_same_thread(components) instead of _are_successive_components(components).

--- trunk/abjad/helpers/get_attached_spanners.py
+++ trunk/abjad/helpers/get_attached_spanners.py
@@ -1,14 +1,20 @@
-from abjad.helpers.are_successive_components import _are_successive_components
+from abjad.helpers.are_strictly_contiguous_components_in_same_thread import _are_strictly_contiguous_components_in_same_thread
 
 
-def _get_attached_spanners(component_list):
+def _get_attached_spanners(components):
+   '''TODO: Write doc string.
+      TODO: Include example.'''
 
-   if not _are_successive_components(component_list):
-      raise ContiguityError('components must be successive.')
-
+   ## check input
+   if not _are_strictly_contiguous_components_in_same_thread(components):
+      raise ContiguityError(
+         'Input must be strictly contiguous components in same thread.')
+   
+   ## accumulate spanners
    spanners = set([ ])
-   for component in component_list:
+   for component in components:
       for spanner in list(component.spanners.attached):
          spanners.update((spanner, ))
       
+   ## return spanners
    return spanners
