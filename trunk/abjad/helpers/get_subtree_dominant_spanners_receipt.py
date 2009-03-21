9fff8dcf0a10f9df37011988bb2872e2df387861
Trevor Bača <trevorbaca@gmail.com>
2009-03-21 17:49:10 +0000

    Generalized _get_subtree_dominant_spanners_receipt(components) to work on any list of zero or more thread-contiguous Abjad components. The previous implementation of the helper worked on only a single instance of an Abjad component. See the test file for seven new examples.

--- trunk/abjad/helpers/get_subtree_dominant_spanners_receipt.py
+++ trunk/abjad/helpers/get_subtree_dominant_spanners_receipt.py
@@ -1,17 +1,29 @@
 from abjad.component.component import _Component
+from abjad.helpers.assert_components import _assert_components
 from abjad.helpers.iterate import iterate
 
 
-def _get_subtree_dominant_spanners_receipt(subtree):
+def _get_subtree_dominant_spanners_receipt(subtrees):
+   '''Return Python list of (spanner, index) pairs.
+      Each (spanner, index) pair gives a spanner which dominates
+      all components in 'subtrees' together with the start-index
+      at which spanner first encounters 'subtrees'.
 
-   if not isinstance(subtree, _Component):
-      raise TypeError('Must be Abjad component.')
+      Use this helper to 'lift' any and all spanners temporarily
+      from 'subtrees', perform some action to the underlying
+      score tree, and then reattach all spanners to new
+      score components.
+ 
+      TODO: Return custom _MultispannerReceipt instance.'''
 
-   subtree_begin = subtree.offset.score
-   subtree_end = subtree_begin + subtree.duration.prolated
+   _assert_components(subtrees, contiguity = 'thread')
+
+   first, last = subtrees[0], subtrees[-1]
+   subtree_begin = first.offset.score
+   subtree_end = last.offset.score + last.duration.prolated
 
    receipt = [ ]
-   for component in iterate(subtree, _Component):
+   for component in iterate(first, _Component):
       if component.offset.score == subtree_begin:
          for spanner in component.spanners.attached:
             if spanner.begin <= subtree_begin:
--- trunk/abjad/helpers/test/test_get_subtree_dominant_spanners_receipt.py
+++ trunk/abjad/helpers/test/test_get_subtree_dominant_spanners_receipt.py
@@ -3,7 +3,11 @@ from abjad.helpers.get_subtree_dominant_spanners_receipt import _get_subtree_dom
 
 
 def test_get_subtree_dominant_spanners_receipt_01( ):
-
+   '''Return Python list of (spanner, index) pairs.
+      Each (spanner, index) pair gives a spanner which dominates
+      all components in list, together with the start-index
+      at which spanner attaches to subelement of first
+      component in list.'''
 
    t = Voice(Sequential(run(2)) * 3)
    diatonicize(t)
@@ -26,8 +30,122 @@ def test_get_subtree_dominant_spanners_receipt_01( ):
       }
    }'''
 
-   receipt = _get_subtree_dominant_spanners_receipt(t[1])
+   receipt = _get_subtree_dominant_spanners_receipt(t[:1])
 
+   "Beam and trill dominate first sequential."
+
+   assert len(receipt) == 2
+   assert (beam, 0) in receipt
+   assert (trill, 0) in receipt
+
+
+def test_get_subtree_dominant_spanners_receipt_02( ):
+   '''Beam, glissando and trill all dominante second sequential.'''
+
+   t = Voice(Sequential(run(2)) * 3)
+   diatonicize(t)
+   beam = Beam(t[:2])
+   glissando = Glissando(t[1:])
+   trill = Trill(t.leaves)
+
+   receipt = _get_subtree_dominant_spanners_receipt(t[1:2])
+
+   assert len(receipt) == 3
    assert (beam, 1) in receipt
    assert (glissando, 0) in receipt
    assert (trill, 2) in receipt
+
+
+def test_get_subtree_dominant_spanners_receipt_03( ):
+   '''Glissando and trill dominate last sequential.'''
+
+   t = Voice(Sequential(run(2)) * 3)
+   diatonicize(t)
+   beam = Beam(t[:2])
+   glissando = Glissando(t[1:])
+   trill = Trill(t.leaves)
+
+   receipt = _get_subtree_dominant_spanners_receipt(t[-1:])
+
+   assert len(receipt) == 2
+   assert (glissando, 1) in receipt
+   assert (trill, 4) in receipt
+
+
+def test_get_subtree_dominant_spanners_receipt_04( ):
+   '''Beam and trill dominate first two sequentials.'''
+
+   t = Voice(Sequential(run(2)) * 3)
+   diatonicize(t)
+   beam = Beam(t[:2])
+   glissando = Glissando(t[1:])
+   trill = Trill(t.leaves)
+
+   receipt = _get_subtree_dominant_spanners_receipt(t[:2])
+
+   assert len(receipt) == 2
+   assert (beam, 0) in receipt
+   assert (trill, 0) in receipt
+
+
+def test_get_subtree_dominant_spanners_receipt_05( ):
+   '''Glissando and trill dominate last two sequentials.'''
+
+   t = Voice(Sequential(run(2)) * 3)
+   diatonicize(t)
+   beam = Beam(t[:2])
+   glissando = Glissando(t[1:])
+   trill = Trill(t.leaves)
+
+   receipt = _get_subtree_dominant_spanners_receipt(t[-2:])
+
+   assert len(receipt) == 2
+   assert (glissando, 0) in receipt
+   assert (trill, 2) in receipt
+
+
+def test_get_subtree_dominant_spanners_receipt_06( ):
+   '''Only trill dominates all three sequentials.'''
+
+   t = Voice(Sequential(run(2)) * 3)
+   diatonicize(t)
+   beam = Beam(t[:2])
+   glissando = Glissando(t[1:])
+   trill = Trill(t.leaves)
+
+   receipt = _get_subtree_dominant_spanners_receipt(t[:])
+
+   assert len(receipt) == 1
+   assert (trill, 0) in receipt
+
+
+def test_get_subtree_dominant_spanners_receipt_07( ):
+   '''Only trill dominates voice.'''
+
+   t = Voice(Sequential(run(2)) * 3)
+   diatonicize(t)
+   beam = Beam(t[:2])
+   glissando = Glissando(t[1:])
+   trill = Trill(t.leaves)
+
+   receipt = _get_subtree_dominant_spanners_receipt([t])
+
+   assert len(receipt) == 1
+   assert (trill, 0) in receipt
+
+
+def test_get_subtree_dominant_spanners_receipt_08( ):
+   '''Only trill dominates first two notes.
+      Note that trill attaches to notes.
+      Note that beam and glissando attach to sequentials.'''
+
+   t = Voice(Sequential(run(2)) * 3)
+   diatonicize(t)
+   beam = Beam(t[:2])
+   glissando = Glissando(t[1:])
+   trill = Trill(t.leaves)
+
+   receipt = _get_subtree_dominant_spanners_receipt(t.leaves[:2])
+
+   assert len(receipt) == 1
+   assert (trill, 0) in receipt
