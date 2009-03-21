668b05a9282404096dcb23fdbbc80bc7242074fe
Trevor Bača <trevorbaca@gmail.com>
2009-03-21 21:13:25 +0000

    New _get_dominant_spanners_between(left, right) private helper. The helper is a special version of _get_dominant_spanners_receipt( ). Use to find spanners that dominae a zero-length 'crack' between components, as in t[2:2]. The helper returns an unordered set of (spanner, index) pairs. Both 'left' and 'right' input arguments must be either an Abjad component or None. In the case that both 'left' and 'right' are Abjad components, they must be thread-contiguous. See helpers/test/test_get_dominant_spanners_between.py for four examples.

---
+++ trunk/abjad/helpers/get_dominant_spanners_between.py
@@ -0,0 +1,35 @@
+from abjad.component.component import _Component
+from abjad.helpers.assert_components import _assert_components
+
+
+def _get_dominant_spanners_between(left, right):
+   '''Return Python list of (spanner, index) pairs.
+      'left' must be either an Abjad component or None.
+      'right' must be either an Abjad component or None.
+
+      If both 'left' and 'right' are components,
+      then 'left' and 'right' must be thread-contiguous.
+
+      This is a special version of _get_dominant_spanners_receipt( ).
+      This version is useful for finding spanners that dominant
+      a zero-length 'crack' between components, as in t[2:2].
+
+      TODO: Return custom _MultispannerReceipt instance.'''
+      
+   if left is None or right is None:
+      return set([ ])
+
+   _assert_components([left, right], contiguity = 'thread')
+
+   dominant_spanners = left.spanners.contained & right.spanners.contained
+   components_after_gap = right._navigator._contemporaneousStartComponents
+   
+   receipt = set([ ])
+   for spanner in dominant_spanners:
+      for component in components_after_gap:
+         if component in spanner:
+            index = spanner.index(component)
+            receipt.add((spanner, index))
+            continue   
+
+   return receipt
---
+++ trunk/abjad/helpers/test/test_get_dominant_spanners_between.py
@@ -0,0 +1,83 @@
+from abjad import *
+from abjad.helpers.get_dominant_spanners_between import \
+   _get_dominant_spanners_between
+
+
+def test_get_dominant_spanners_between_01( ):
+   '''Return Python list of (spanner, index) pairs.
+      Each spanner dominates a *crack* between components.'''
+
+   t = Voice(Sequential(run(2)) * 3)
+   diatonicize(t)
+   beam = Beam(t[:2])
+   glissando = Glissando(t[1:])
+   trill = Trill(t.leaves)
+
+   r'''\new Voice {
+      {
+         c'8 [ \startTrillSpan
+         d'8
+      }
+      {
+         e'8 \glissando
+         f'8 ] \glissando
+      }
+      {
+         g'8 \glissando
+         a'8 \stopTrillSpan
+      }
+   }'''
+
+   "No spanners dominate t[0:0]"
+
+   receipt = _get_dominant_spanners_between(None, t[0])
+
+   assert len(receipt) == 0
+   assert receipt == set([ ])
+
+
+def test_get_dominant_spanners_between_02( ):
+   '''Beam and trill both dominate crack at t[1:1].'''
+
+   t = Voice(Sequential(run(2)) * 3)
+   diatonicize(t)
+   beam = Beam(t[:2])
+   glissando = Glissando(t[1:])
+   trill = Trill(t.leaves)
+
+   receipt = _get_dominant_spanners_between(t[0], t[1])
+
+   assert len(receipt) == 2
+   assert (beam, 1) in receipt
+   assert (trill, 2) in receipt
+
+
+def test_get_dominant_spanners_between_03( ):
+   '''Glissando and trill both dominate crack at t[2:2].'''
+
+   t = Voice(Sequential(run(2)) * 3)
+   diatonicize(t)
+   beam = Beam(t[:2])
+   glissando = Glissando(t[1:])
+   trill = Trill(t.leaves)
+
+   receipt = _get_dominant_spanners_between(t[1], t[2])
+   
+   assert len(receipt) == 2
+   assert (glissando, 1) in receipt
+   assert (trill, 4) in receipt
+
+
+def test_get_dominant_spanners_between_04( ):
+   '''No spanners dominate 'crack' following voice.'''
+
+   t = Voice(Sequential(run(2)) * 3)
+   diatonicize(t)
+   beam = Beam(t[:2])
+   glissando = Glissando(t[1:])
+   trill = Trill(t.leaves)
+
+   receipt = _get_dominant_spanners_between(t[2], None)
+  
+   assert len(receipt) == 0
+   assert receipt == set([ ])
