a075f55bfe1a347d66ea9b6806969213db90a161
Trevor Bača <trevorbaca@gmail.com>
2009-03-23 21:09:39 +0000

    Beautifed Container.insert( ) tests in preparation for reimplementation using spanner management tools in container setitem.

--- trunk/abjad/container/test/test_container_insert.py
+++ trunk/abjad/container/test/test_container_insert.py
@@ -2,132 +2,134 @@ from abjad import *
 
 
 def test_container_insert_01( ):
-   '''Insert works just before a spanner.'''
+   '''Insert component into container at index i.      
+      Fracture spanners to the left of index i.
+      Fracture spanners to the right of index i.      
+      Return Python list of fractured spanners.'''
+
+   "Insert works just before a spanner."
+
    t = Staff([Note(n, (1, 8)) for n in range(4)])
-   #Beam(t)
-   Beam(t[ : ])
+   Beam(t[:])
    t.insert(0, Rest((1, 4)))
-   assert t.format == "\\new Staff {\n\tr4\n\tc'8 [\n\tcs'8\n\td'8\n\tef'8 ]\n}"   
-   assert check(t)
-   '''
-   \new Staff {
+
+   r'''\new Staff {
            r4
            c'8 [
            cs'8
            d'8
            ef'8 ]
-   }
-   '''
+   }'''
+
+   assert check(t)
+   assert t.format == "\\new Staff {\n\tr4\n\tc'8 [\n\tcs'8\n\td'8\n\tef'8 ]\n}"   
    
 
 def test_container_insert_02( ):
    '''Insert works inside a spanner.'''
+
    t = Staff([Note(n, (1, 8)) for n in range(4)])
-   #Beam(t)
-   Beam(t[ : ])
+   Beam(t[:])
    t.insert(1, Rest((1, 4)))
-   assert t.format == "\\new Staff {\n\tc'8 [ ]\n\tr4\n\tcs'8 [\n\td'8\n\tef'8 ]\n}"
-   assert check(t)
-   '''
-   \new Staff {
+
+   r'''\new Staff {
            c'8 [ ]
            r4
            cs'8 [
            d'8
            ef'8 ]
-   }
-   '''
+   }'''
+
+   assert check(t)
+   assert t.format == "\\new Staff {\n\tc'8 [ ]\n\tr4\n\tcs'8 [\n\td'8\n\tef'8 ]\n}"
 
 
 def test_container_insert_03( ):
    '''Insert works just after a spanner.'''
+
    t = Staff([Note(n, (1, 8)) for n in range(4)])
-   #Beam(t)
-   Beam(t[ : ])
+   Beam(t[:])
    t.insert(4, Rest((1, 4)))
-   assert t.format == "\\new Staff {\n\tc'8 [\n\tcs'8\n\td'8\n\tef'8 ]\n\tr4\n}"
-   
-   assert check(t)
-   '''
-   \new Staff {
+
+   r'''\new Staff {
            c'8 [
            cs'8
            d'8
            ef'8 ]
            r4
-   }
-   '''
+   }'''
+
+   assert check(t)
+   assert t.format == "\\new Staff {\n\tc'8 [\n\tcs'8\n\td'8\n\tef'8 ]\n\tr4\n}"
 
 
 def test_container_insert_04( ):
    '''Insert works with really big positive values.'''
+
    t = Staff([Note(n, (1, 8)) for n in range(4)])
-   #Beam(t)
-   Beam(t[ : ])
+   Beam(t[:])
    t.insert(1000, Rest((1, 4)))
-   assert t.format == "\\new Staff {\n\tc'8 [\n\tcs'8\n\td'8\n\tef'8 ]\n\tr4\n}"
-   
-   assert check(t)
-   '''
-   \new Staff {
+
+   r'''\new Staff {
            c'8 [
            cs'8
            d'8
            ef'8 ]
-           r4
-   }
-   '''
+        r4
+   }'''
+
+   assert check(t)
+   assert t.format == "\\new Staff {\n\tc'8 [\n\tcs'8\n\td'8\n\tef'8 ]\n\tr4\n}"
 
 
 def test_container_insert_05( ):
    '''Insert works with negative values.'''
+
    t = Staff([Note(n, (1, 8)) for n in range(4)])
-   #Beam(t)
-   Beam(t[ : ])
+   Beam(t[:])
    t.insert(-1, Rest((1, 4)))
-   assert t.format == "\\new Staff {\n\tc'8 [\n\tcs'8\n\td'8 ]\n\tr4\n\tef'8 [ ]\n}"
-   assert check(t)
-   '''
-   \new Staff {
+
+   r'''\new Staff {
            c'8 [
            cs'8
            d'8 ]
            r4
            ef'8 [ ]
-   }
-   '''
+   }'''
+
+   assert check(t)
+   assert t.format == "\\new Staff {\n\tc'8 [\n\tcs'8\n\td'8 ]\n\tr4\n\tef'8 [ ]\n}"
 
 
 def test_container_insert_06( ):
    '''Insert works with really big negative values.'''
+
    t = Staff([Note(n, (1, 8)) for n in range(4)])
-   #Beam(t)
-   Beam(t[ : ])
+   Beam(t[:])
    t.insert(-1000, Rest((1, 4)))
-   assert t.format == "\\new Staff {\n\tr4\n\tc'8 [\n\tcs'8\n\td'8\n\tef'8 ]\n}"
-  
-   assert check(t)
-   '''
-   \new Staff {
+
+   r'''\new Staff {
            r4
            c'8 [
            cs'8
            d'8
            ef'8 ]
-   }
-   '''
+   }'''
 
-## PARENTAGE TESTS ##
+   assert check(t)
+   assert t.format == "\\new Staff {\n\tr4\n\tc'8 [\n\tcs'8\n\td'8\n\tef'8 ]\n}"
+
 
 def test_container_insert_07( ):
+   '''Inserting a note from one container into another container
+      switches note parent from first container to second.'''
+
    v = Voice(scale(4))
    t = Staff(run(8))
    note = v[0]
    t.insert(1, v[0])
+
    assert check(v)
    assert check(t)
    assert not note in v
    assert note.parentage.parent is t
-   
-
-
