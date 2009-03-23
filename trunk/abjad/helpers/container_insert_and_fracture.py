c76df45d4a232a4531c99fd5f3be3ac891a7df40
Trevor Bača <trevorbaca@gmail.com>
2009-03-23 21:21:39 +0000

    New container_insert_and_fracture(container, i, component) public helper. The helper copies the functionality of old Container.insert(i, component) exactly.
       Insert component into container at index i.
       Fracture spanners to the left of index i.
       Fracture spanners to the right of index i.
       Return Python list of fractured spanners.
    
       For nonfracturing insert, use Container __setitem__.
    
       Example:
    
       t = Voice(scale(4))
       Beam(t[:])
       container_insert_and_fracture(t, 1, Rest((1, 4)))
    
       \new Voice {
               c'8 [ ]
               r4
               d'8 [
               e'8
               f'8 ]
       }
    
    The point of this commit is to externalize and preserve the old Container.insert( ) functionality so that the new Container.insert( ) implementation can wrap Container.__setitem__ exactly. So insert( ) will continue to be part of the public inteface of container. But insert( ) will soon switch implementation from a spanner-fracturing operation to a spanner-preserving operation.
    
    See helpers/test/test_container_insert_and_fracture.py for seven examples total.

---
+++ trunk/abjad/helpers/container_insert_and_fracture.py
@@ -0,0 +1,40 @@
+from abjad.component.component import _Component
+from abjad.container.container import Container
+
+
+def container_insert_and_fracture(container, i, component):
+   r'''Insert component into container at index i.
+      Fracture spanners to the left of index i.
+      Fracture spanners to the right of index i.
+      Return Python list of fractured spanners.
+
+      For nonfracturing insert, use Container __setitem__.
+
+      Example:
+
+      t = Voice(scale(4))
+      Beam(t[:])
+      container_insert_and_fracture(t, 1, Rest((1, 4)))
+
+      \new Voice {
+              c'8 [ ]
+              r4
+              d'8 [
+              e'8
+              f'8 ]
+      }'''
+
+   assert isinstance(container, Container)
+   assert isinstance(i, int)
+   assert isinstance(component, _Component)
+
+   result = [ ]
+   component.parentage._switchParentTo(container)
+   container._music.insert(i, component)
+   if component.prev:
+      result.extend(component.prev.spanners.fracture(direction = 'right'))
+   if component.next:
+      result.extend(component.next.spanners.fracture(direction = 'left')) 
+   container._update._markForUpdateToRoot( )
+
+   return result
