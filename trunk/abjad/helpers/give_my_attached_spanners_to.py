463d158bf60abcbc2d9719df2a34e30886192339
Trevor Bača <trevorbaca@gmail.com>
2009-03-24 21:17:12 +0000

    New _give_my_attached_spanners_to(donor_component, recipient_components) private helper. Not composer-safe.
       Find all spanners attaching directly to 'donor_component'.
       Insert each component in 'recipient_components' into
       each spanner attaching directly to 'donor_component'.
       Remove 'donor_component' from each attaching spanner.
       Return 'donor_component'.
    
    Example:
    
       t = Voice(Sequential(run(2)) * 2)
       diatonicize(t)
       Beam(t[:])
    
       r'''\new Voice {
          {
             c'8 [
             d'8
          }
          {
             e'8
             f'8 ]
          }
       }'''
    
       donor = t[0]
       recipient = Voice(scale(4))
       _give_my_attached_spanners_to(donor, [recipient])
    
       "Container t is now ..."
    
       r'''\new Voice {
          {
             c'8
             d'8
          }
          {
             e'8
             f'8 ]
          }
       }'''
    
       "Recipient container is now ..."
    
       r'''\new Voice {
          c'8 [
          d'8
          e'8
          f'8
       }'''
    
       "Both container t and recipient container carry discontiguous spanners."
    
       assert not check(t)
       assert not check(recipient)
    
    See helpers/test/test_give_my_attached_spanners_to.py for this example.

---
+++ trunk/abjad/helpers/give_my_attached_spanners_to.py
@@ -0,0 +1,23 @@
+from abjad.helpers.assert_components import _assert_components
+
+
+def _give_my_attached_spanners_to(donor_component, recipient_components):
+   '''Find all spanners attaching directly to 'donor_component'.
+      Insert each component in 'recipient_components' into
+      each spanner attaching directly to 'donor_component'.
+      Remove 'donor_component' from each attaching spanner.
+      Return 'donor_component'.
+
+      Not composer-safe.'''
+
+   _assert_components([donor_component])
+   _assert_components(recipient_components, contiguity = 'thread')
+   
+   attached_spanners = list(donor_component.spanners.attached)
+   for attached_spanner in attached_spanners:
+      donor_index = attached_spanner.index(donor_component)
+      for recipient_component in reversed(recipient_components):
+         attached_spanner._insert(donor_index, recipient_component)
+      attached_spanner._remove(donor_component)
+
+   return donor_component
