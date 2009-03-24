6e60b9c03567686119b8130d04fca2a23d369c81
Trevor Bača <trevorbaca@gmail.com>
2009-03-24 13:22:54 +0000

    New _withdraw_from_crossing_spanners(components) private helper.
    The helper asserts 'components' equal to a thread-contiguous list of Abjad components. The helper then finds all spanners that cross 'components' and then removes 'components' from each crossing spanner.
    
    Note:
    
       This operation can leave score trees in a weird state.
       Operation should only be used in the middle of some other operation.
       Intended purpose is to strip components of crosssing spanners.
       Similar to stripping components of parentage.
       These two operations prepared components for reincorporation.
       Reincorporation means setting into some other score tree.
       Container setitem is probably primary consumer of this operation.
    
    Example:
    
       t = Voice(Sequential(run(2)) * 2)
       diatonicize(t)
       beam = Beam(t[0][:])
       slur = Slur(t[1][:])
       trill = Trill(t.leaves)
    
       \new Voice {
               {
                       c'8 [ \startTrillSpan
                       d'8 ]
               }
               {
                       e'8 (
                       f'8 ) \stopTrillSpan
               }
       }
    
       assert len(spanners) == 2
       assert beam in spanners
       assert trill in spanners
    
       _withdraw_from_crossing_spanners(t[0:1])
    
       \new Voice {
               {
                       c'8 [
                       d'8 ]
               }
               {
                       e'8 ( \startTrillSpan
                       f'8 ) \stopTrillSpan
               }
       }
    
       spanners = _get_contained_spanners(t[0:1])
       assert len(spanners) == 1
       assert beam in spanners
    
    See helpers/test/test_withdraw_from_crossing_spanners.py for this example and two others.

---
+++ trunk/abjad/helpers/test/test_withdraw_from_crossing_spanners.py
@@ -0,0 +1,119 @@
+from abjad import *
+from abjad.helpers.get_contained_spanners import _get_contained_spanners
+from abjad.helpers.withdraw_from_crossing_spanners import \
+   _withdraw_from_crossing_spanners
+import py.test
+
+
+def test_withdraw_from_crossing_spanners_01( ):
+   '''Withdraw thread-contiguous components from crossing spanners.'''
+
+   t = Voice(Sequential(run(2)) * 2)
+   diatonicize(t)
+   beam = Beam(t[0][:])
+   slur = Slur(t[1][:])
+   trill = Trill(t.leaves)
+
+   r'''\new Voice {
+           {
+                   c'8 [ \startTrillSpan
+                   d'8 ]
+           }
+           {
+                   e'8 (
+                   f'8 ) \stopTrillSpan
+           }
+   }'''
+
+   spanners = _get_contained_spanners([t])
+   assert len(spanners) == 3
+   assert beam in spanners
+   assert slur in spanners
+   assert trill in spanners
+
+   _withdraw_from_crossing_spanners([t])
+   assert len(spanners) == 3
+   assert beam in spanners
+   assert slur in spanners
+   assert trill in spanners
+
+
+def test_withdraw_from_crossing_spanners_02( ):
+   '''Withdraw thread-contiguous components from crossing spanners.'''
+
+   t = Voice(Sequential(run(2)) * 2)
+   diatonicize(t)
+   beam = Beam(t[0][:])
+   slur = Slur(t[1][:])
+   trill = Trill(t.leaves)
+
+   r'''\new Voice {
+           {
+                   c'8 [ \startTrillSpan
+                   d'8 ]
+           }
+           {
+                   e'8 (
+                   f'8 ) \stopTrillSpan
+           }
+   }'''
+
+   spanners = _get_contained_spanners(t[0:1])
+   assert len(spanners) == 2
+   assert beam in spanners
+   assert trill in spanners
+
+   _withdraw_from_crossing_spanners(t[0:1])
+
+   r'''\new Voice {
+           {
+                   c'8 [
+                   d'8 ]
+           }
+           {
+                   e'8 ( \startTrillSpan
+                   f'8 ) \stopTrillSpan
+           }
+   }'''
+
+   spanners = _get_contained_spanners(t[0:1])
+   assert len(spanners) == 1
+   assert beam in spanners
+
+   assert t.format == "\\new Voice {\n\t{\n\t\tc'8 [\n\t\td'8 ]\n\t}\n\t{\n\t\te'8 ( \\startTrillSpan\n\t\tf'8 ) \\stopTrillSpan\n\t}\n}"
+
+
+def test_withdraw_from_crossing_spanners_03( ):
+   '''Withdraw thread-contiguous components from crossing spanners.'''
+
+   t = Voice(Sequential(run(2)) * 2)
+   diatonicize(t)
+   beam = Beam(t[0][:])
+   slur = Slur(t[1][:])
+   trill = Trill(t.leaves)
+
+   r'''\new Voice {
+           {
+                   c'8 [ \startTrillSpan
+                   d'8 ]
+           }
+           {
+                   e'8 (
+                   f'8 ) \stopTrillSpan
+           }
+   }'''
+
+   spanners = _get_contained_spanners(t.leaves[2:3])
+   assert len(spanners) == 2
+   assert slur in spanners
+   assert trill in spanners
+
+   _withdraw_from_crossing_spanners(t.leaves[2:3])
+
+   spanners = _get_contained_spanners(t.leaves[2:3])
+   assert spanners == set([ ])
+
+   "Operation leaves score tree in weird state."
+   "Both slur and trill are now discontiguous."
+
+   assert not check(t)
---
+++ trunk/abjad/helpers/withdraw_from_crossing_spanners.py
@@ -0,0 +1,29 @@
+from abjad.component.component import _Component
+from abjad.helpers.assert_components import _assert_components
+from abjad.helpers.get_crossing_spanners import _get_crossing_spanners
+from abjad.helpers.iterate import iterate
+
+
+def _withdraw_from_crossing_spanners(components):
+   '''This operation can leave score trees in a weird state.
+      Operation should only be used in the middle of some other operation.
+      Intended purpose is to strip components of crosssing spanners.
+      Similar to stripping components of parentage.
+      These two operations prepared components for reincorporation.
+      Reincorporation means setting into some other score tree.
+      Container setitem is probably primary consumer of this operation.
+      Return None.'''
+
+   _assert_components(components, contiguity = 'thread')
+
+   crossing_spanners = _get_crossing_spanners(components) 
+
+   components_including_children = list(iterate(components, _Component))
+
+   for crossing_spanner in list(crossing_spanners):
+      print crossing_spanner
+      spanner_components = crossing_spanner._components[:]
+      for component in components_including_children:
+         if component in spanner_components:
+            crossing_spanner._components.remove(component)
+            component.spanners._spanners.discard(crossing_spanner)
