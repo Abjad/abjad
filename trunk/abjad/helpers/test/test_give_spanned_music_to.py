20f140f33df230d18d651790e9921245b6f56fad
Trevor Bača <trevorbaca@gmail.com>
2009-03-24 20:53:07 +0000

    New _give_spanned_music_to(donor_component, recipient_component) helper. Not composer-safe.
       If 'donor_component' has music and 'recipient_component'
       is an empty container, give music 'donor_component' to
       'recipient_component', leaving any spanners attaching
       to music of 'donor_component' in tact, including
       crossing spanners.
    
       If 'donor_component' has music and 'recipient_component'
       is not an empty container, raise TypeError.
    
       If 'donor_component' has no music, do nothing.
    
       Return 'donor_component'.
    
       Helper is not composer-safe and may cause discontiguous spanners.
    
    Example:
    
       t = Voice(Sequential(run(2)) * 2)
       diatonicize(t)
       Beam(t.leaves)
    
       r'''\new Voice {
          {
             c'8 [
             d'8
          }
          {
             e'8
             f'8 ]
          }
       }'''
    
       donor = t[0]
       recipient = Voice([ ])
       _give_spanned_music_to(donor, recipient)
    
       "Container t is now ..."
    
       r'''\new Voice {
          {
          }
          {
             e'8
             f'8 ]
          }
       }'''
    
       "Container t carries discontiguous spanners."
    
       assert not check(t)
    
       "Recipient container is now ..."
    
       r'''\new Voice {
          c'8 [
          d'8
       }'''
    
       "Recipient container carries discontiguous spanners."
    
       assert not check(recipient)
    
    See helpers/test/test_give_spanned_music_to.py for this and two further examples.

---
+++ trunk/abjad/helpers/test/test_give_spanned_music_to.py
@@ -0,0 +1,80 @@
+from abjad import *
+from abjad.helpers.give_spanned_music_to import _give_spanned_music_to
+import py.test
+
+
+def test_give_spanend_music_to_01( ):
+   '''Give spanned music from donor to recipient.
+      Helper is not composer-safe and results here in bad spanners.'''
+
+   t = Voice(Sequential(run(2)) * 2)
+   diatonicize(t)
+   Beam(t.leaves)
+
+   r'''\new Voice {
+      {
+         c'8 [
+         d'8
+      }
+      {
+         e'8
+         f'8 ]
+      }
+   }'''
+
+   donor = t[0]
+   recipient = Voice([ ])
+   _give_spanned_music_to(donor, recipient)
+
+   "Container t is now ..."
+   
+   r'''\new Voice {
+      {
+      }
+      {
+         e'8
+         f'8 ]
+      }
+   }'''
+
+   "Container t carries discontiguous spanners."
+ 
+   assert not check(t)
+   assert t.format == "\\new Voice {\n\t{\n\t}\n\t{\n\t\te'8\n\t\tf'8 ]\n\t}\n}"
+
+   "Recipient container is now ..."
+
+   r'''\new Voice {
+      c'8 [
+      d'8
+   }'''
+
+   "Recipient container carries discontiguous spanners."
+
+   assert not check(recipient)
+   assert recipient.format == "\\new Voice {\n\tc'8 [\n\td'8\n}"
+
+
+def test_give_spanned_music_to_02( ):
+   '''When donor is leaf, do nothing.'''
+
+   donor = Note(0, (1, 8))
+   recipient = Voice([ ])
+  
+   _give_spanned_music_to(donor, recipient)
+
+   assert check(donor)
+   assert donor.format == "c'8"
+
+   assert check(recipient)
+   assert recipient.format == '\\new Voice {\n}'
+
+
+def test_give_spanned_music_to_03( ):
+   '''When recipient is NOT empty container, raise TypeError.'''
+
+   donor = Voice(scale(4))
+   recipient = Voice(scale(4))
+
+   assert py.test.raises(
+      TypeError, '_give_spanned_music_to(donor, recipient)')
