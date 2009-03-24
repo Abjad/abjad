e3f7cb01a1fc06d1767128cf9d78f6cfd92b2a9f
Víctor Adán <vctradn@gmail.com>
2009-03-24 14:28:55 +0000

    Removed deprecated get_topmost_module.py from imports package.

--- trunk/abjad/tools/imports/get_functions_in_module.py
+++ trunk/abjad/tools/imports/get_functions_in_module.py
@@ -1,11 +1,9 @@
-#from abjad.tools.imports.get_topmost_module import _get_topmost_module
 import os
 
 
 def _get_functions_in_module(module_file):
    '''Collects and returns all functions defined in module_file.'''
    result = [ ]
-   #mod = _get_topmost_module(module_file)
    module_file = module_file.replace(os.sep, '.')
    mod = __import__(module_file, fromlist=['*'])
    for key, value in mod.__dict__.items( ):
