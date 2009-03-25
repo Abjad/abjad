75510feb340e5cc74c1fbda380e58bdf680c0e71
Trevor Bača <trevorbaca@gmail.com>
2009-03-25 11:09:48 +0000

    Unskipped spanner/test/test_spanner_insert.py. Updated test file to show that Spanner._insert(i, component) is now private. But spanner-mangling test at top of test file to show exactly how _insert( ) can mangle spanners and cause score not to check.

--- trunk/abjad/spanner/test/test_spanner_insert.py
+++ trunk/abjad/spanner/test/test_spanner_insert.py
@@ -1,11 +1,35 @@
 from abjad import *
-import py.test
 
-
-py.test.skip('Spanner changes.')
 
 def test_insert_01( ):
-   '''Spanners can insert at index 0.'''
+   '''Insert component in spanner at index i.
+      Add spanner to component's aggregator.
+      Component then knows about spanner and vice versa.
+      Not composer-safe.
+      Inserting into middle of spanner may leave discontiguous spanner.'''
+
+   t = Voice(scale(4))
+   p = Beam(t[:2])
+
+   r'''\new Voice {
+           c'8 [
+           d'8 ]
+           e'8
+           f'8
+   }'''
+
+   p._insert(1, t.leaves[-1])
+
+   "Interior insert leaves discontiguous spanner: Beam(c'8, f'8, d'8)."
+
+   assert not check(t)
+
+
+def test_insert_02( ):
+   '''Insert component at index zero in spanner.
+      This operation does not mangle spanner.
+      Operation is still not composer-safe, however.
+      Note that p.append( ) and p.append_left( ) are composer-safe.'''
 
    t = Voice(Sequential(run(2)) * 3)
    diatonicize(t)
@@ -26,7 +50,7 @@ def test_insert_01( ):
       }
    }'''
 
-   p.insert(0, t[0][1])
+   p._insert(0, t[0][1])
 
    r'''\new Voice {
       {
@@ -44,46 +68,3 @@ def test_insert_01( ):
    }'''
 
    assert t.format == "\\new Voice {\n\t{\n\t\tc'8\n\t\td'8 [\n\t}\n\t{\n\t\te'8\n\t\tf'8 ]\n\t}\n\t{\n\t\tg'8\n\t\ta'8\n\t}\n}"
-
-
-def test_insert_02( ):
-   '''Spanners can insert after last index.
-      Equivalent to append.'''
-
-   t = Voice(Sequential(run(2)) * 3)
-   diatonicize(t)
-   p = Beam(t[1])
-
-   r'''\new Voice {
-      {
-         c'8
-         d'8
-      }
-      {
-         e'8 [
-         f'8 ]
-      }
-      {
-         g'8
-         a'8
-      }
-   }'''
-
-   p.insert(len(p.components) + 1, t[2][0])
-
-   r'''\new Voice {
-      {
-         c'8
-         d'8
-      }
-      {
-         e'8 [
-         f'8
-      }
-      {
-         g'8 ]
-         a'8
-      }
-   }'''
-
-   assert t.format == "\\new Voice {\n\t{\n\t\tc'8\n\t\td'8\n\t}\n\t{\n\t\te'8 [\n\t\tf'8\n\t}\n\t{\n\t\tg'8 ]\n\t\ta'8\n\t}\n}"
