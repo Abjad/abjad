e6d1d9ef23417925686139168d445e9249a61a79
Trevor Bača <trevorbaca@gmail.com>
2009-03-26 22:28:31 +0000

    Reimplemented container_eject_contents(container) as a two-line wrapper around container delitem logic.

--- trunk/abjad/helpers/container_eject_contents.py
+++ trunk/abjad/helpers/container_eject_contents.py
@@ -2,16 +2,12 @@ from abjad.container.container import Container
 
 
 def container_eject_contents(container):
-   '''Remove any contents from container.
-      Container keeps position-in-score and position-in-spanners.
-
+   '''Remove contents from container.
       Return list of container contents.'''
    
    assert isinstance(container, Container)
 
    contents = container[:]
-   ## TODO: Implement components_parentage_detach_shallow( )
-   for component in contents:
-      component.parentage._detach( )
+   del(container[:])
 
    return contents
