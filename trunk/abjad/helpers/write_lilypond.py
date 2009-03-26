d5e933067f6ba63ff3b03a05c7a4440ae9410cb7
Víctor Adán <vctradn@gmail.com>
2009-03-26 21:13:37 +0000

    Extension to the public interface: write_lilypond(expr, file_name, template, title) now available. The function writes a given Abjad expression (_Component) out to a LilyPond input file with the given file_name. Use this function to create a LilyPond file anywhere in your system, with any name you want, for later retrieval or processing. Unlike show( ), the function does not call the LilyPond interpreter on the LilyPond input file created. Like show( ), it accepts a template optional argument and an optional title.

---
+++ trunk/abjad/helpers/write_lilypond.py
@@ -0,0 +1,24 @@
+from abjad.cfg.wrap_format import _wrap_format
+from abjad.cfg.write_preamble import _write_preamble
+from abjad.cfg.write_title import _write_title
+import os
+
+def write_lilypond(expr, name, template = None, title = None):
+   '''Writes a given Abjad expression `expr` out to a LilyPond input file
+   with the given `name`.
+   '''
+   name = os.path.expanduser(name)
+   if not name.endswith('.ly'):
+      name += '.ly'
+   try:
+      outfile = open(name, 'w')
+      _write_preamble(outfile, template)
+      _write_title(outfile, title)
+      outfile.write(_wrap_format(expr.format))
+      outfile.close( )
+   except IOError:
+      print 'ERROR: cound not open file %s' % name
+      dirname = os.path.dirname(name)
+      if dirname:
+         print 'Make sure "%s" exists in your sistem.' % dirname
+
