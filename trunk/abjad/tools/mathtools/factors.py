f1c7fa18d0b6146f8b774518465dea762ef0f65b
Víctor Adán <vctradn@gmail.com>
2009-04-01 15:24:41 +0000

    Added abjad/tools/mathtools/factors.py module forgotten in previous commit.

---
+++ trunk/abjad/tools/mathtools/factors.py
@@ -0,0 +1,18 @@
+def factors(n):
+   '''Returns all factors of n.'''
+
+   assert isinstance(n, int)
+   #assert n > 1
+   assert n > 0
+
+   d = 2
+   factors = [1]
+   while n > 1:
+      if n % d == 0:
+         factors.append(d)
+         n = n/d
+      else:
+         d = d + 1
+   return factors
+
+
