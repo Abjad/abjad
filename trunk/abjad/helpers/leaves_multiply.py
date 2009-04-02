70a7e8bddd224b86d80c99f8e0e62045de66469e
Trevor Bača <trevorbaca@gmail.com>
2009-04-02 23:55:51 +0000

    Tightened leaves_multiply( ) to a two-line loop with a call to copy_unspan( ).

--- trunk/abjad/helpers/leaves_multiply.py
+++ trunk/abjad/helpers/leaves_multiply.py
@@ -1,6 +1,5 @@
+from abjad.helpers.copy_unspan import copy_unspan
 from abjad.helpers.retroiterate import retroiterate
-from abjad.helpers.withdraw_from_attached_spanners import \
-   _withdraw_from_attached_spanners
 from abjad.leaf.leaf import _Leaf
 
 
@@ -9,17 +8,4 @@ def leaves_multiply(expr, total = 1):
       preserve parentage and spanners.'''
 
    for leaf in retroiterate(expr, _Leaf):
-      print 'debug: leaf %s' % leaf
-      _leaf_multiply(leaf, total)
-   
-
-def _leaf_multiply(leaf, total = 1):
-   '''Insert n copies of leaf after leaf.
-      Preserve parentage and spanners.'''
-
-   assert isinstance(leaf, _Leaf)
-   assert total > 0
-
-   new_leaves = leaf * (total - 1)
-   _withdraw_from_attached_spanners(new_leaves)
-   leaf.splice(new_leaves)
+      leaf.splice(copy_unspan([leaf], total - 1))
