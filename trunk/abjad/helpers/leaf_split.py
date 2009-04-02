c1dfbaabb0f26f3ae6c5f263c51a4e6fca69731a
Trevor Bača <trevorbaca@gmail.com>
2009-04-02 20:36:21 +0000

    Cleaned up more old code in helpers/leaf_split.py file.

--- trunk/abjad/helpers/leaf_split.py
+++ trunk/abjad/helpers/leaf_split.py
@@ -1,7 +1,8 @@
 from abjad.helpers.copy_unspan import copy_unspan
 from abjad.helpers.duration_token_unpack import _duration_token_unpack
 from abjad.helpers.is_power_of_two import _is_power_of_two
-from abjad.helpers.leaf_scale import leaf_scale, leaf_scale_binary
+from abjad.helpers.leaf_scale import leaf_scale
+from abjad.helpers.leaf_scale import leaf_scale_binary
 from abjad.leaf.leaf import _Leaf
 from abjad.rational.rational import Rational
 
@@ -11,9 +12,8 @@ def leaf_split(split_dur, leaf):
    split_dur = Rational(*_duration_token_unpack(split_dur))
    unprolated_split_dur = split_dur / leaf.duration.prolation
    if unprolated_split_dur == 0 or \
-      unprolated_split_dur >= leaf.duration.written:
+      leaf.duration.written <= unprolated_split_dur:
       return [leaf]
-
    new_leaf = copy_unspan([leaf])[0]
    leaf.splice_left([new_leaf])
    l1 = leaf_scale(unprolated_split_dur, new_leaf)
@@ -29,20 +29,18 @@ def leaf_split_binary(split_dur, leaf):
    denominator = unprolated_split_dur._d
    assert _is_power_of_two(denominator)
    if unprolated_split_dur == 0 or \
-      unprolated_split_dur >= leaf.duration.written:
+      leaf.duration.written <= unprolated_split_dur:
       return [leaf]
-   else:
-      new_leaf = leaf.copy()
-      ## remove afterGrace from new_leaf and Grace from leaf (l2)
-      new_leaf.grace.after = None
-      leaf.grace.before = None
-      ## remove articulations and dynamics
-      leaf.articulations = None
-      leaf.dynamics = None
-      new_leaf.spanners.clear( )
-      _link_new_leaf_to_parent(new_leaf, leaf)
-      _update_leaf_spanners(new_leaf, leaf)
-      l1 = leaf_scale_binary(unprolated_split_dur, new_leaf)
-      l2 = leaf_scale_binary(leaf.duration.written-unprolated_split_dur, leaf)
-      result = [l1, l2] 
-      return result
+
+   new_leaf = copy_unspan([leaf])[0]
+   leaf.splice_left([new_leaf])
+   ## remove afterGrace from new_leaf and Grace from leaf (l2)
+   new_leaf.grace.after = None
+   leaf.grace.before = None
+   ## remove articulations and dynamics
+   leaf.articulations = None
+   leaf.dynamics = None
+   l1 = leaf_scale_binary(unprolated_split_dur, new_leaf)
+   l2 = leaf_scale_binary(leaf.duration.written-unprolated_split_dur, leaf)
+   result = [l1, l2] 
+   return result
