d064ee69ef64cca95c5741717d7b0c89dc5a2475
Trevor Bača <trevorbaca@gmail.com>
2009-04-05 22:41:44 +0000

    New report( ) method implemented on spanner format interfaces.
    Example:
    
       t = Voice(scale(4))
       beam = Beam(t)
       beam.thickness = 3
    
       >>> f(t)
       \new Voice {
               \override Beam #'thickness = #3
               c'8 [
               d'8
               e'8
               f'8 ]
               \revert Beam #'thickness
       }
       >>> beam
       Beam(Voice{4})
    
    The spanner format interface report( ) method takes a list of leaves:
    
       >>> beam.format.report(t[:2])
       c'8
               before: ["\\override Beam #'thickness = #3"]
                after: []
                 left: []
                right: ['[']
    
       d'8
               before: []
                after: []
                 left: []
                right: []
    
    When no list is provided, report( ) reports on all leaves in spanner:
    
       >>> beam.format.report( )
       c'8
               before: ["\\override Beam #'thickness = #3"]
                after: []
                 left: []
                right: ['[']
    
       d'8
               before: []
                after: []
                 left: []
                right: []
    
       e'8
               before: []
                after: []
                 left: []
                right: []
    
       f'8
               before: []
                after: ["\\revert Beam #'thickness"]
                 left: []
                right: [']']
    
    Inherited by all spanner format interfaces.

--- trunk/abjad/spanner/format.py
+++ trunk/abjad/spanner/format.py
@@ -35,6 +35,17 @@ class _SpannerFormatInterface(_Abjad):
       result = [ ]
       return result
 
+   def report(self, leaves = None):
+      '''Print spanner format contributions for every leaf in leaves.'''
+      leaves = leaves or self.spanner.leaves
+      for leaf in leaves:
+         print leaf
+         print '\tbefore: %s' % self.before(leaf)
+         print '\t after: %s' % self.after(leaf)
+         print '\t  left: %s' % self.left(leaf)
+         print '\t right: %s' % self.right(leaf)
+         print ''
+
    def right(self, leaf):
       '''Spanner format contributions to output right of leaf.'''
       result = [ ]
