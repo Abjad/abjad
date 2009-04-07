2c55625173ff2ba0bb831b7c7b09479be30af664
Trevor Bača <trevorbaca@gmail.com>
2009-04-07 11:49:46 +0000

    Read-only 'client' attribute added to all Abjad interfaces. Kinda surprising obvious gap; interaces should implement *read-only* access to client to reduce underscore reference throughout code. It is only *write* access to client that should be discouraged.

--- trunk/abjad/core/interface.py
+++ trunk/abjad/core/interface.py
@@ -5,3 +5,9 @@ class _Interface(_Abjad):
 
    def __init__(self, client):
       self._client = client
+
+   ## PUBLIC ATTRIBUTES ##
+
+   @property
+   def client(self):
+      return self._client
