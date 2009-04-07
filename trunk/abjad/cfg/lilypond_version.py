675d23421f40bc03437197da44cd57bef30f0963
Víctor Adán <vctradn@gmail.com>
2009-04-07 14:53:33 +0000

    Fixed TODO in _get_lilypond_version( ) cfg helper. Replaced hard-coded assumed path to LilyPond on windows machines with a directory search: On windows, if LilyPond is not found in PATH, a Popen subprocess is started to search for the lilypond.exe file in the system. If not found, a SystemError is raised.

--- trunk/abjad/cfg/lilypond_version.py
+++ trunk/abjad/cfg/lilypond_version.py
@@ -4,12 +4,18 @@ import subprocess
 
 def _get_lilypond_version( ):
    if subprocess.mswindows and not 'LilyPond' in os.environ.get('PATH'):
-      ### TODO this would be better not hard-coded.
-      lilypond = '"C:\\Program Files\\LilyPond\\usr\\bin\\lilypond.exe"'
+      #lilypond = '"C:\\Program Files\\LilyPond\\usr\\bin\\lilypond.exe"'
+      command = r'dir "C:\Program Files\*.exe" /s /b | find "lilypond.exe"'
+      proc = subprocess.Popen(command, shell = True, stdout = subprocess.PIPE)
+      lilypond = proc.stdout.readline( )
+      lilypond = lilypond.strip('\r').strip('\n').strip( )
+      if lilypond == '':
+         raise SystemError('LilyPond not found on your Windowz box.')
    else:
       lilypond = 'lilypond'
-   proc = subprocess.Popen(lilypond + ' --version', shell=True, 
-      stdout=subprocess.PIPE)
+
+   command = lilypond + ' --version'
+   proc = subprocess.Popen(command, shell = True, stdout = subprocess.PIPE)
    lilypond_version = proc.stdout.readline( )
    lilypond_version = lilypond_version.split(' ')[-1]
    lilypond_version = lilypond_version.strip( )
