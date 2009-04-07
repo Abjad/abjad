36f9577e6522d93255cd648e99b36df42204d738
Trevor Bača <trevorbaca@gmail.com>
2009-04-07 16:55:31 +0000

    Staff change to public interface: you can no longer say t.staff == foo and t.staff = bar; use t.staff.effective == foo and t.staff.forced = bar instead.

--- trunk/abjad/staff/interface.py
+++ trunk/abjad/staff/interface.py
@@ -1,5 +1,6 @@
 from abjad.core.formatcontributor import _FormatContributor
 from abjad.core.interface import _Interface
+import types
 
 
 class _StaffInterface(_Interface, _FormatContributor):
@@ -15,28 +16,37 @@ class _StaffInterface(_Interface, _FormatContributor):
    def after(self):
       '''Used only when very last note in staff is staff changed.'''
       result = [ ]
-      if (self.changed or (not self._client.prev and self._forced)) and \
-         not self._client.next:
+      if (self.changed or (not self.client.prev and self.forced)) and \
+         not self.client.next:
          result.append(r'\change Staff = %s' % self.given.name)
       return result
 
    @property
    def before(self):
       result = [ ]
-      if self.changed or (not self._client.prev and self._forced):
+      if self.changed or (not self.client.prev and self.forced):
          result.append(r'\change Staff = %s' % self.effective.name)
       return result
 
    @property
    def changed(self):
-      return self._client.prev and \
-         self._client.prev.staff != self._client.staff
+      return self.client.prev and \
+         self.client.prev.staff.effective != self.client.staff.effective
 
    @property
    def effective(self):
-      return self._forced if self._forced else self.given
+      return self.forced if self.forced else self.given
+
+   @apply
+   def forced( ):
+      from abjad.staff.staff import Staff
+      def fget(self):
+         return self._forced
+      def fset(self, arg):
+         assert isinstance(arg, (Staff, types.NoneType))
+         self._forced = arg
 
    @property
    def given(self):
       from abjad.staff.staff import Staff
-      return self._client.parentage._first(Staff)
+      return self.client.parentage._first(Staff)
--- trunk/abjad/staff/test/test_staff_interface.py
+++ trunk/abjad/staff/test/test_staff_interface.py
@@ -2,18 +2,15 @@ from abjad import *
 
 
 def test_staff_interface_01( ):
-   '''
-   Staff changes work on the first note of a staff.
-   '''
+   '''Staff changes work on the first note of a staff.'''
 
    piano = PianoStaff(Staff(scale(4)) * 2)
    piano.parallel = True
    piano[0].name = 'RH'
    piano[1].name = 'LH'
-   piano[0][0].staff = piano[1]
+   piano[0][0].staff.forced = piano[1]
 
-   r'''
-   \new PianoStaff <<
+   r'''\new PianoStaff <<
       \context Staff = "RH" {
          \change Staff = LH
          c'8
@@ -28,25 +25,21 @@ def test_staff_interface_01( ):
          e'8
          f'8
       }
-   >>
-   '''
+   >>'''
 
    assert piano.format == '\\new PianoStaff <<\n\t\\context Staff = "RH" {\n\t\t\\change Staff = LH\n\t\tc\'8\n\t\t\\change Staff = RH\n\t\td\'8\n\t\te\'8\n\t\tf\'8\n\t}\n\t\\context Staff = "LH" {\n\t\tc\'8\n\t\td\'8\n\t\te\'8\n\t\tf\'8\n\t}\n>>'
 
 
 def test_staff_interface_02( ):
-   '''
-   Staff changes work on middle notes of a staff.
-   '''
+   '''Staff changes work on middle notes of a staff.'''
 
    piano = PianoStaff(Staff(scale(4)) * 2)
    piano.parallel = True
    piano[0].name = 'RH'
    piano[1].name = 'LH'
-   piano[0][1].staff = piano[1]
+   piano[0][1].staff.forced = piano[1]
 
-   r'''
-   \new PianoStaff <<
+   r'''\new PianoStaff <<
       \context Staff = "RH" {
          c'8
          \change Staff = LH
@@ -61,25 +54,21 @@ def test_staff_interface_02( ):
          e'8
          f'8
       }
-   >>
-   '''
+   >>'''
 
    assert piano.format == '\\new PianoStaff <<\n\t\\context Staff = "RH" {\n\t\tc\'8\n\t\t\\change Staff = LH\n\t\td\'8\n\t\t\\change Staff = RH\n\t\te\'8\n\t\tf\'8\n\t}\n\t\\context Staff = "LH" {\n\t\tc\'8\n\t\td\'8\n\t\te\'8\n\t\tf\'8\n\t}\n>>'
 
 
 def test_staff_interface_03( ):
-   '''
-   Staff changes work on the last note of a staff.
-   '''
+   '''Staff changes work on the last note of a staff.'''
 
    piano = PianoStaff(Staff(scale(4)) * 2)
    piano.parallel = True
    piano[0].name = 'RH'
    piano[1].name = 'LH'
-   piano[0][-1].staff = piano[1]
+   piano[0][-1].staff.forced = piano[1]
 
-   r'''
-   \new PianoStaff <<
+   r'''\new PianoStaff <<
       \context Staff = "RH" {
          c'8
          d'8
@@ -94,26 +83,22 @@ def test_staff_interface_03( ):
          e'8
          f'8
       }
-   >>
-   '''
+   >>'''
 
    assert piano.format == '\\new PianoStaff <<\n\t\\context Staff = "RH" {\n\t\tc\'8\n\t\td\'8\n\t\te\'8\n\t\t\\change Staff = LH\n\t\tf\'8\n\t\t\\change Staff = RH\n\t}\n\t\\context Staff = "LH" {\n\t\tc\'8\n\t\td\'8\n\t\te\'8\n\t\tf\'8\n\t}\n>>'
 
 
 def test_staff_interface_04( ):
-   '''
-   Adjacent staff-changed notes format nicely.
-   '''
+   '''Adjacent staff-changed notes format nicely.'''
 
    piano = PianoStaff(Staff(scale(4)) * 2)
    piano.parallel = True
    piano[0].name = 'RH'
    piano[1].name = 'LH'
-   for note in piano[0][ : 2]:
-      note.staff = piano[1]
+   for note in piano[0][:2]:
+      note.staff.forced = piano[1]
 
-   r'''
-   \new PianoStaff <<
+   r'''\new PianoStaff <<
       \context Staff = "RH" {
          \change Staff = LH
          c'8
@@ -128,7 +113,6 @@ def test_staff_interface_04( ):
          e'8
          f'8
       }
-   >>
-   '''
+   >>'''
 
    assert piano.format == '\\new PianoStaff <<\n\t\\context Staff = "RH" {\n\t\t\\change Staff = LH\n\t\tc\'8\n\t\td\'8\n\t\t\\change Staff = RH\n\t\te\'8\n\t\tf\'8\n\t}\n\t\\context Staff = "LH" {\n\t\tc\'8\n\t\td\'8\n\t\te\'8\n\t\tf\'8\n\t}\n>>'
