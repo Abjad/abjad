754fdf9e90434235c2e78d13ce77b3593ccc2c27
Trevor Bača <trevorbaca@gmail.com>
2009-04-07 12:25:56 +0000

    Tightened _ClefInterface. Changed clumsy self._client._navigator._prevBead references to self.client.prev now that navigation defaults to bead navigation. Change self._client references to protected self.client references.

--- trunk/abjad/clef/interface.py
+++ trunk/abjad/clef/interface.py
@@ -1,11 +1,17 @@
 from abjad.clef.clef import Clef
 from abjad.core.grobhandler import _GrobHandler
 from abjad.core.interface import _Interface
+import types
 
 
 class _ClefInterface(_Interface, _GrobHandler):
+   '''Handle LilyPond Clef grob.
+      Interface to find effective clef.
+      Interface to force clef changes.'''
    
    def __init__(self, client):
+      '''Bind client and LilyPond Clef grob.
+         Set forced to None.'''
       _Interface.__init__(self, client)
       _GrobHandler.__init__(self, 'Clef')
       self._forced = None
@@ -14,48 +20,61 @@ class _ClefInterface(_Interface, _GrobHandler):
 
    @property
    def change(self):
-      client = self._client
-      return bool(client._navigator._prevBead and \
-         client._navigator._prevBead.clef.name != self.name)
+      '''True if clef changes here, otherwise False.'''
+      #client = self._client
+      #return bool(client._navigator._prevBead and \
+      #   client._navigator._prevBead.clef.name != self.name)
+      return bool(hasattr(self.client, 'prev') and \
+         self.client.prev.clef.name != self.name)
 
    @property
    def effective(self):
-      cur = self._client
+      '''Return effective clef or else treble.'''
+      #cur = self._client
+      cur = self.client
       while cur is not None:
          if cur.clef._forced:
             return cur.clef._forced
          else:
-            cur = cur._navigator._prevBead
-      for x in self._client.parentage.parentage[1:]:
+            #cur = cur._navigator._prevBead
+            cur = getattr(cur, 'prev', None)
+      #for x in self._client.parentage.parentage[1:]:
+      for x in self.client.parentage.parentage[1:]:
          if hasattr(x, 'clef') and x.clef._forced:
             return x.clef._forced
       return Clef('treble')
 
    @apply
    def forced( ):
+      '''Forced clef change here.'''
       def fget(self):
          return self._forced
       def fset(self, arg):
-         if arg is None:
-            self._forced = None
-         elif isinstance(arg, str):
-            clef = Clef(arg)
-            self._forced = clef
-         elif isinstance(arg, Clef):
+         if isinstance(arg, (Clef, types.NoneType)):
             self._forced = arg
+         elif isinstance(arg, str):
+            self._forced = Clef(arg)
+#         if arg is None:
+#            self._forced = None
+#         elif isinstance(arg, str):
+#            clef = Clef(arg)
+#            self._forced = clef
+#         elif isinstance(arg, Clef):
+#            self._forced = arg
          else:
             raise ValueError('unknown clef specification.')
       return property(**locals( ))
 
    @property
    def name(self):
+      '''Name of effective clef as string.'''
       return self.effective.name
 
    @property
    def opening(self):
+      '''Format contribution at container opening or before leaf.'''
       result = [ ]
-      result.extend(_GrobHandler.before.fget(self))
+      #result.extend(_GrobHandler.before.fget(self))
       if self.forced or self.change:
          result.append(self.effective.format)
       return result
-
