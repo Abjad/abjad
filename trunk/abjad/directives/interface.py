74ab549228af1fba64c70dff406344aedb00e5cf
Trevor Bača <trevorbaca@gmail.com>
2009-04-07 14:39:55 +0000

    Tightened _UserDirectivesInterface.

--- trunk/abjad/directives/interface.py
+++ trunk/abjad/directives/interface.py
@@ -21,19 +21,5 @@ class _UserDirectivesInterface(_Interface, _UserComments):
       return property(**locals( ))
 
    @property
-   def contributions(self):
-      result = [ ]
-      result.append(('before', tuple(self.before)))
-      result.append(('opening', tuple(self.opening)))
-      result.append(('left', tuple(self.left)))
-      result.append(('right', tuple(self.right)))
-      result.append(('closing', tuple(self.closing)))
-      result.append(('after', tuple(self.after)))
-      return tuple(result)
-
-   ## PUBLIC METHODS ##
-
-   def clear(self):
-      '''Remove all user directives.'''
-      _UserComments.clear(self)
-      self.left = None
+   def locations(self):
+      return ('before', 'opening', 'left', 'right', 'closing', 'after')
