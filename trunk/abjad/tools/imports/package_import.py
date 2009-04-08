048e7dd63bb3196bfe0ec1ec200071cf4d112186
Víctor Adán <vctradn@gmail.com>
2009-04-08 23:51:35 +0000

    New _package_import(path, namespace) imports function available. Use this to have a shallow, *no-recursive* import of submodules into a package. The function treats packages (non-leaf modules) and (leaf)-modules differently. The function does two things:
    All packages (non-leaf sub-modules) found in the given `path` are imported to the given `namespace`.
    All the functions and classes inside modules (leaf-modules, *.py files) found in the given `path` are imported into the given `namespace`.'''
    
    This function will replace all calls to _import_functions_in_package_to_namespace( ) *except* for the one in the main abjad/__init__.py file. Here we still do want a recursive import to get all the helpers and main classes in the main namespaces, at least for the time being.  
    
    Future commit will replace all calls to _import_functions_in_package_to_namespace( ) for this new helper.

---
+++ trunk/abjad/tools/imports/package_import.py
@@ -0,0 +1,31 @@
+from abjad.tools.imports.get_functions_in_module import _get_functions_in_module
+import os
+
+def _package_import(path, namespace):
+   '''Non-recursive import helper. 
+   All packages (non-leaf sub-modules) in `path` are imported to the 
+   given `namespace`.
+   All the functions and classes inside modules (leaf-modules, *.py files) 
+   found in `path` are imported into `namespace`.'''
+   module = path[path.rindex('abjad'):]
+   module = module.replace(os.sep, '.')
+
+   for element in os.listdir(path):
+      if os.path.isfile(os.path.join(path, element)):
+         if not element.startswith('_') and element.endswith('.py'):
+            ## import function inside module
+            submod = os.path.join(module, element[:-3])
+            functions = _get_functions_in_module(submod)
+            for f in functions:
+               namespace[f.__name__] = f
+
+      elif os.path.isdir(os.path.join(path, element)):
+         if not element in ('.svn', 'test'):
+            #exec('from %s import %s' % (module, element))
+            submod = '.'.join([module, element])
+            print submod
+            namespace[element] = __import__(submod, fromlist =['*'])
+      else:
+         print 'Not a dir, not a file, what is %s?' % element
+
+   del(namespace['_package_import'])
