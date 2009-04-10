c09b1e12412472b529888f0891a5ffcdc35058ff
Trevor Bača <trevorbaca@gmail.com>
2009-04-10 17:08:18 +0000

    Beautified metertools import statements.

--- trunk/abjad/tools/metertools/make_binary.py
+++ trunk/abjad/tools/metertools/make_binary.py
@@ -1,7 +1,7 @@
 from abjad.meter.meter import Meter
 from abjad.rational.rational import Rational
-from abjad.tools.durtools.in_terms_of import in_terms_of
-from abjad.tools.mathtools.next_least_power_of_two import next_least_power_of_two
+from abjad.tools import durtools
+from abjad.tools import mathtools
 
 
 def make_binary(nonbinary_meter, contents_multiplier = Rational(1)):
@@ -20,16 +20,15 @@ def make_binary(nonbinary_meter, contents_multiplier = Rational(1)):
 
    # find binary denominator
    if contents_multiplier == Rational(1):
-      #binary_denominator = mathtools.next_least_power_of_two(nonbinary_denominator)
-      binary_denominator = next_least_power_of_two(nonbinary_denominator)
+      binary_denominator = mathtools.next_least_power_of_two(
+         nonbinary_denominator)
    else:
-      #binary_denominator = mathtools.next_least_power_of_two(nonbinary_denominator, 1)
-      binary_denominator = next_least_power_of_two(nonbinary_denominator, 1)
+      binary_denominator = mathtools.next_least_power_of_two(
+         nonbinary_denominator, 1)
 
    # find binary pair
    nonbinary_pair = (nonbinary_meter.numerator, nonbinary_meter.denominator)
-   #binary_pair = durtools.in_terms_of(nonbinary_pair, binary_denominator)
-   binary_pair = in_terms_of(nonbinary_pair, binary_denominator)
+   binary_pair = durtools.in_terms_of(nonbinary_pair, binary_denominator)
 
    # update meter numerator and denominator
    nonbinary_meter.numerator = binary_pair[0]
