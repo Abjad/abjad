b3ba922afb7363c0d6c2a264a3c8ba5c16aa8b37
Trevor Bača <trevorbaca@gmail.com>
2009-04-15 11:02:26 +0000

    Beautified tools/listtools tests.

--- trunk/abjad/tools/listtools/test/test_listtools_flatten.py
+++ trunk/abjad/tools/listtools/test/test_listtools_flatten.py
@@ -1,23 +1,32 @@
 from abjad import *
+import py.test
 
 
 def test_listtools_flatten_01( ):
    l = [1, 2, 3, 4, 5]
-   r = listtools.flatten(l)
-   assert r == l
+   new = listtools.flatten(l)
+   assert new == [1, 2, 3, 4, 5]
 
 
 def test_listtools_flatten_02( ):
    l = [(1, 2), [3, 4]]
-   r = listtools.flatten(l)
-   assert len(r) == 4
-   for e in r:
-      assert not isinstance(e, (list, tuple))
+   new = listtools.flatten(l)
+   assert new == [1, 2, 3, 4]
 
 
 def test_listtools_flatten_03( ):
    l = [(1, 2), [3, (4, 5)]]
-   r = listtools.flatten(l)
-   assert len(r) == 5
-   for e in r:
-      assert not isinstance(e, (list, tuple))
+   new = listtools.flatten(l)
+   assert new == [1, 2, 3, 4, 5]
+
+
+def test_listtools_flatten_04( ):
+   l = [(1, 2), [3, (4, 5)]]
+   new = listtools.flatten(l, ltypes = (list, ))
+   assert new == [(1, 2), 3, (4, 5)]
+
+
+def test_listtools_flatten_05( ):
+   l = [(1, 2), [3, (4, 5)]]
+   assert py.test.raises(AssertionError, 
+      'listtools.flatten(l, ltypes = (tuple, ))')
