15ce157cbc29ec0fc52c04d21172e2ec1b60f112
Trevor Bača <trevorbaca@gmail.com>
2009-04-20 14:58:00 +0000

    New split.leaves_cyclic_by_durations( ) function added in this commit as a proposed replacement for the current split.container_leaves_cyclic_by_durations( ). The two functions provide exactly the same resuls. The signatures of the two functions differ in the following way. The older split.container_leaves_cyclic_by_durations(t, durations) asks for container t. The newer split.leaves_cyclic_by_durations(leaves, durations) asks for strictly contiguous leaves in the same thread. The interface that asks for a list of leaves matches the newer container operations much more closely than the old interface that starts at container and indirectly operates on leaves.

--- trunk/abjad/tools/split/container_leaves_cyclic_by_durations.py
+++ trunk/abjad/tools/split/container_leaves_cyclic_by_durations.py
@@ -2,15 +2,11 @@ from abjad.metricgrid.spanner import MetricGrid
 from abjad.container.container import Container
 
 
-## TODO: Can this act on leaves outside of score? ##
-
 def container_leaves_cyclic_by_durations(container, durations):
    '''Split leaves contained at any level of depth in container.
       Take split points cyclically from 'durations' list.'''
 
    assert isinstance(container, Container)
-
    mg = MetricGrid(container, durations)
    mg.splitOnBar( )
    mg.clear( )
-   
---
+++ trunk/abjad/tools/split/leaves_cyclic_by_durations.py
@@ -0,0 +1,13 @@
+from abjad.leaf.leaf import _Leaf
+from abjad.metricgrid.spanner import MetricGrid
+from abjad.tools import check
+
+
+def leaves_cyclic_by_durations(leaves, durations):
+   '''Split leaves with split points taken cyclically from 'durations'.'''
+
+   check.assert_components(
+      leaves, klasses = (_Leaf), contiguity = 'strict', share = 'thread')
+   mg = MetricGrid(leaves, durations)
+   mg.splitOnBar( )
+   mg.clear( )
