abb7ad813f2eb2351be9df488c66ff5006fcd030
Trevor Bača <trevorbaca@gmail.com>
2009-04-20 13:03:24 +0000

    Patch to remove stray file from and add missing test file to previous commit.

--- trunk/abjad/tools/clonewp/test/test_clonewp_by_leaf_counts_with_parentage.py
+++ trunk/abjad/tools/clonewp/test/test_clonewp_by_leaf_counts_with_parentage.py
@@ -1,7 +1,7 @@
 from abjad import *
 
 
-def test_containertools_partition_by_leaf_counts_with_parentage_01( ):
+def test_clonewp_by_leaf_counts_with_parentage_01( ):
    '''Partition tuplet in voice.
       The helper wraps lcopy( ).
       This means that the original structure remains unchanged.
@@ -9,7 +9,7 @@ def test_containertools_partition_by_leaf_counts_with_parentage_01( ):
 
    t = Voice([FixedDurationTuplet((2, 8), construct.scale(3))])
    Beam(t[0][:])
-   left, right = containertools.partition_by_leaf_counts_with_parentage(t[0], [1, 2])
+   left, right = clonewp.by_leaf_counts_with_parentage(t[0], [1, 2])
 
    r'''\new Voice {
            \times 2/3 {
@@ -31,12 +31,12 @@ def test_containertools_partition_by_leaf_counts_with_parentage_01( ):
    assert right.format == "\\new Voice {\n\t\\times 2/3 {\n\t\td'8 [\n\t\te'8 ]\n\t}\n}"
 
 
-def test_containertools_partition_by_leaf_counts_with_parentage_02( ):
+def test_clonewp_by_leaf_counts_with_parentage_02( ):
    '''Partition voice.'''
 
    t = Voice(construct.scale(3))
    Beam(t[:])
-   result = containertools.partition_by_leaf_counts_with_parentage(t, [1, 2])
+   result = clonewp.by_leaf_counts_with_parentage(t, [1, 2])
 
    r'''\new Voice {
       c'8 [
