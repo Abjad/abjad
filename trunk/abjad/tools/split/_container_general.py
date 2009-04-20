668ab39a7ec60947c9acc6a44b20c5280726de32
Trevor Bača <trevorbaca@gmail.com>
2009-04-20 22:26:53 +0000

    Cleaned up implementation of split/_container_general.py with the introduction of new spannertools.give_attached_to_children( ).

--- trunk/abjad/tools/split/_container_general.py
+++ trunk/abjad/tools/split/_container_general.py
@@ -2,6 +2,7 @@ from abjad.measure.measure import _Measure
 from abjad.tools import containertools
 from abjad.tools import measuretools
 from abjad.tools import parenttools
+from abjad.tools import spannertools
 from abjad.tuplet.tuplet import _Tuplet
 
 
@@ -12,48 +13,35 @@ def _container_general(container, i, spanners = 'unfractured'):
       Use split.container_fractured( ) to fracture spanners.
       Use split.container_unfractured( ) to leave spanners unchanged.'''
 
-   # remember container multiplier, if any
+   ## remember container multiplier, if any
    container_multiplier = getattr(container.duration, 'multiplier', None)
 
-   # remember container meter denominator, if any
-   if isinstance(container, _Measure):
-      meter_denominator = container.meter.effective.denominator
-   else:
-      meter_denominator = None
-
+   ## partition music of input container
    left_music = container[:i]
    right_music = container[i:]
 
+   ## instantiate new left and right containers
    if isinstance(container, _Measure):
+      meter_denominator = container.meter.effective.denominator
       left_duration = sum([x.duration.prolated for x in left_music])
       right_duration = sum([x.duration.prolated for x in right_music])
-      #right = container.__class__(right_duration, container[i:])
-      #left = container.__class__(left_duration, container[:i])
-      right = container.__class__(right_duration, right_music)
       left = container.__class__(left_duration, left_music)
+      right = container.__class__(right_duration, right_music)
    elif isinstance(container, _Tuplet):
-      #right = container.__class__(1, container[i:])
-      #left = container.__class__(1, container[:i])
-      right = container.__class__(1, right_music)
+      meter_denominator = None
       left = container.__class__(1, left_music)
+      right = container.__class__(1, right_music)
    else:
-      #right = container.__class__(container[i:])
-      #left = container.__class__(container[:i])
-      right = container.__class__(right_music)
+      meter_denominator = None
       left = container.__class__(left_music)
+      right = container.__class__(right_music)
    
+   ## save left and right parts together for iteration
    parts = [left, right]
 
-#   print container, container.spanners.attached
-#   print left, left.spanners.attached
-#   print right, right.spanners.attached
-#   print ''
-   
-   ## QUESTION: Can this first branch be bequeath( ) or donate( )? ##
-
+   ## handle spanners and parentage manually
    parent, start, stop = parenttools.get_with_indices([container])
    if parent is not None:
-      #parent[start:stop+1] = [part for part in parts if len(part)]
       nonempty = [part for part in parts if len(part)]
       parent._music[start:stop+1] = nonempty
       for part in nonempty:
@@ -64,19 +52,11 @@ def _container_general(container, i, spanners = 'unfractured'):
          for part in nonempty:
             part.spanners._add(spanner)
    else:
-      ## TODO: Implement spannertools._give_attached_to( ) ##
-      for spanner in list(container.spanners.attached):
-         i = spanner.index(container)
-         spanner._components[i:i+1] = parts
-         parts.spanners.attached._add(spanner) # fix this line
+      spannertools.give_attached_to_children(container)
       left.parentage._switch(None)
       right.parentage._switch(None)
 
-   #print container, container.spanners.attached
-   #print left, left.spanners.attached
-   #print right, right.spanners.attached
-   #print ''
-
+   ## fracture spanners, if requested
    if spanners == 'fractured':
       if len(parts) == 2:
          left.spanners.fracture(direction = 'right')
