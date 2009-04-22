ef5ce9c349d7a55cdc3b3fb0feedf0e9f586ad13
Trevor Bača <trevorbaca@gmail.com>
2009-04-22 22:01:09 +0000

    New tietools.span_leaf_pair(left_leaf, right_leaf) function in tools/tietools package. The function applies, fuses, appends or appends left, as appropriate for whether left_leaf and right_leaf are spanned or unspanned. Externalizes a four- or five-part test.

---
+++ trunk/abjad/tools/tietools/span_leaf_pair.py
@@ -0,0 +1,33 @@
+from abjad.exceptions.exceptions import MissingSpannerError
+from abjad.leaf.leaf import _Leaf
+from abjad.tie.spanner import Tie
+from abjad.tools.tietools.are_in_same_spanner import \
+   are_in_same_spanner as tietools_are_in_same_spanner
+
+
+def span_leaf_pair(left, right):
+
+   assert isinstance(left, _Leaf)
+   assert isinstance(right, _Leaf)
+
+   if tietools_are_in_same_spanner([left, right]):
+      return 
+
+   try:
+      left_tie_spanner = left.tie.spanner
+   except MissingSpannerError:
+      left_tie_spanner = None
+
+   try:
+      right_tie_spanner = right.tie.spanner
+   except MissingSpannerError:
+      right_tie_spanner = None
+
+   if left_tie_spanner is not None and right_tie_spanner is not None:
+      left_tie_spanner.fuse(right_tie_spanner)
+   elif left_tie_spanner is not None and right_tie_spanner is None:
+      left_tie_spanner.append(right)
+   elif left_tie_spanner is None and right_tie_spanner is not None:
+      right_tie_spanner.append_left(left)
+   elif left_tie_spanner is None and right_tie_spanner is None:
+      Tie([left, right])
