1bf6c3edbd42916802bb04eadcf50510be25cffc
Trevor Bača <trevorbaca@gmail.com>
2009-04-22 14:54:04 +0000

    New tietools.are_in_same_spanner(components) helper.
    Example:
    
       t = Voice(construct.run(4))
       Tie(t[:2])
    
       r'''\new Voice {
          c'8 ~
          c'8
          c'8
          c'8
       }'''
    
       assert tietools.are_in_same_spanner(t[:2])
       assert not tietools.are_in_same_spanner(t[-2:])
       assert tietools.are_in_same_spanner(t[:1])
       assert not tietools.are_in_same_spanner(t[-1:])
       assert not tietools.are_in_same_spanner(t[:])
    
    The test file contains only this one example.

---
+++ trunk/abjad/tools/tietools/are_in_same_tie_spanner.py
@@ -0,0 +1,23 @@
+from abjad.exceptions.exceptions import MissingSpannerError
+from abjad.tools import check
+
+
+def are_in_same_spanner(components):
+   '''True if all components in list share same tie spanner,
+      otherwise False.'''
+
+   check.assert_components(components)
+   
+   try:
+      first = components[0]
+      try:
+         first_tie_spanner = first.tie.spanner
+         for component in components[1:]:
+            if component.tie.spanner is not first_tie_spanner:
+               return False
+      except MissingSpannerError:
+         return False
+   except IndexError:
+      return True
+
+   return True
