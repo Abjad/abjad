fa905d6d1702ee2cda0d12e54f813c30c59134f9
Trevor Bača <trevorbaca@gmail.com>
2009-04-23 22:19:49 +0000

    New tietools.group_by_parent(tie_chain) function.
    Examples:
    
       t = Staff(RigidMeasure((2, 8), construct.run(2)) * 2)
       Tie(t.leaves)
    
       r'''\new Staff {
             \time 2/8
             c'8 ~
             c'8 ~
             \time 2/8
             c'8 ~
             c'8
       }'''
    
       parts = tietools.group_by_parent(t.leaves[0].tie.chain)
    
       assert len(parts) == 2
       assert parts[0] == t.leaves[:2]
       assert parts[1] == t.leaves[2:]
    
    This sort of thing is useful for groups of tied leaves that can fuse together. Fusion across container boundaries is not possible; fusion within a container is possible.
    
    The test file contains two further examples.

---
+++ trunk/abjad/tools/tietools/test/test_tietools_group_by_parent.py
@@ -0,0 +1,53 @@
+from abjad import *
+
+
+def test_tietools_group_by_parent_01( ):
+   '''Group leaves in tie chain by immediate parent.'''
+
+   t = Staff(RigidMeasure((2, 8), construct.run(2)) * 2)
+   Tie(t.leaves)
+
+   r'''\new Staff {
+         \time 2/8
+         c'8 ~
+         c'8 ~
+         \time 2/8
+         c'8 ~
+         c'8
+   }'''
+
+   parts = tietools.group_by_parent(t.leaves[0].tie.chain)
+   
+   assert len(parts) == 2
+   assert parts[0] == t.leaves[:2]
+   assert parts[1] == t.leaves[2:]
+
+
+def test_tietools_group_by_parent_02( ):
+   '''Group leaves in tie chain by immediate parent.'''
+
+   t = Staff(construct.run(4))
+   Tie(t.leaves)
+
+   r'''\new Staff {
+         c'8 ~
+         c'8 ~
+         c'8 ~
+         c'8
+   }'''
+
+   parts = tietools.group_by_parent(t.leaves[0].tie.chain)
+   
+   assert len(parts) == 1
+   assert parts[0] == t.leaves
+
+
+def test_tietools_group_by_parent_03( ):
+   '''Group leaves in tie chain by immediate parent.'''
+
+   t = Note(0, (1, 4))
+   
+   parts = tietools.group_by_parent(t.tie.chain)
+   
+   assert len(parts) == 1
+   assert parts[0] == [t]
