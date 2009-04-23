af1b1fcd028e7a13e7def99365c643acbe92c0eb
Trevor Bača <trevorbaca@gmail.com>
2009-04-23 11:44:54 +0000

    Interface extension to measuretools.populate( ).
    Previous interface:
    
       measuretools.populate(expr, mode)
    
    New, extended interface:
    
       measuretools.populate(expr, mode, iterctrl = lambda measure, idx: True)
    
    This implementation of the measuretools.populate( ) accepts an iteration filter in the form of a binary function. The input parameters to the function are a reference to the current measure being populated, 'measure', and the index of iteration of that measure, 'idx'.
    
    
    Example 1. Skip odd-numbered measures only:
    
       t = Staff(RigidMeasure((2, 8), construct.run(2)) * 4)
       pitchtools.diatonicize(t)
    
       r'''\new Staff {
                       \time 2/8
                       c'8
                       d'8
                       \time 2/8
                       e'8
                       f'8
                       \time 2/8
                       g'8
                       a'8
                       \time 2/8
                       b'8
                       c''8
       }'''
    
       def iterctrl(measure, i):
          return i % 2 == 1
    
       measuretools.populate(t, 'skip', iterctrl)
    
       r'''\new Staff {
                       \time 2/8
                       c'8
                       d'8
                       \time 2/8
                       s1 * 1/4
                       \time 2/8
                       g'8
                       a'8
                       \time 2/8
                       s1 * 1/4
       }'''
    
    
    Example 2. Skip measures of length greater than 2:
    
       t = Staff([RigidMeasure((n, 8), construct.scale(n)) for n in (2, 3, 4)])
    
       r'''\new Staff {
                       \time 2/8
                       c'8
                       d'8
                       \time 3/8
                       c'8
                       d'8
                       e'8
                       \time 4/8
                       c'8
                       d'8
                       e'8
                       f'8
       }'''
    
       measuretools.populate(t, 'skip', lambda m, i: 2 < len(m))
    
       r'''\new Staff {
                       \time 2/8
                       c'8
                       d'8
                       \time 3/8
                       s1 * 3/8
                       \time 4/8
                       s1 * 1/2
       }'''
    
    
    The test file adds just these two new examples.

--- trunk/abjad/tools/measuretools/populate.py
+++ trunk/abjad/tools/measuretools/populate.py
@@ -8,7 +8,7 @@ from abjad.tools import iterate
 from abjad.tools import mathtools
 
 
-def populate(expr, mode):
+def populate(expr, mode, iterctrl = lambda measure, i: True):
    '''Populate each measure in 'expr' according to 'mode'.
 
       With mode = 'big-endian':
@@ -33,78 +33,84 @@ def populate(expr, mode):
          Empty the contents of each measure.'''
 
    if mode == 'big-endian':
-      _measures_populate_big_endian(expr)
+      _measures_populate_big_endian(expr, iterctrl)
    elif mode == 'little-endian':
-      _measures_populate_little_endian(expr)
+      _measures_populate_little_endian(expr, iterctrl)
    elif mode == 'meter series':
-      _measures_populate_meter_series(expr)
+      _measures_populate_meter_series(expr, iterctrl)
    elif mode == 'skip':
-      _measures_populate_skip(expr)
+      _measures_populate_skip(expr, iterctrl)
    elif durtools.is_token(mode):
-      _measures_populate_duration_train(expr, mode)
+      _measures_populate_duration_train(expr, mode, iterctrl)
    elif mode is None:
-      _measures_populate_none(expr)
+      _measures_populate_none(expr, iterctrl)
    else:
       raise ValueError('unknown measure population mode "%s".' % mode)
 
 
-def _measures_populate_big_endian(expr):
+def _measures_populate_big_endian(expr, iterctrl):
    from abjad.measure.measure import _Measure
-   for measure in iterate.naive(expr, _Measure):
-      meter = measure.meter.effective
-      written_duration = ~meter.multiplier * meter.duration
-      notes = construct.notes(0, written_duration)
-      measure[ : ] = notes
+   for i, measure in enumerate(iterate.naive(expr, _Measure)):
+      if iterctrl(measure, i):
+         meter = measure.meter.effective
+         written_duration = ~meter.multiplier * meter.duration
+         notes = construct.notes(0, written_duration)
+         measure[:] = notes
 
 
-def _measures_populate_little_endian(expr):
+def _measures_populate_little_endian(expr, iterctrl):
    from abjad.measure.measure import _Measure
-   for measure in iterate.naive(expr, _Measure):
-      meter = measure.meter.effective
-      written_duration = ~meter.multiplier * meter.duration
-      notes = construct.notes(
-         0, written_duration, direction = 'little-endian')
-      measure[ : ] = notes
+   for i, measure in enumerate(iterate.naive(expr, _Measure)):
+      if iterctrl(measure, i):
+         meter = measure.meter.effective
+         written_duration = ~meter.multiplier * meter.duration
+         notes = construct.notes(
+            0, written_duration, direction = 'little-endian')
+         measure[:] = notes
 
 
-def _measures_populate_duration_train(expr, written_duration):
+def _measures_populate_duration_train(expr, written_duration, iterctrl):
    from abjad.measure.measure import _Measure
    written_duration = Rational(written_duration)
-   for measure in iterate.naive(expr, _Measure):
-      meter = measure.meter.effective
-      total_duration = meter.duration
-      prolation = meter.multiplier
-      notes = construct.note_train(
-         0, written_duration, total_duration, prolation)
-      measure[ : ] = notes
-
-
-def _measures_populate_meter_series(expr):
-   from abjad.measure.measure import _Measure
-   for measure in iterate.naive(expr, _Measure):
-      meter = measure.meter.effective
-      denominator = mathtools.next_least_power_of_two(meter.denominator)
-      numerator = meter.numerator
-      notes = Note(0, (1, denominator)) * numerator
-      measure[:] = notes
-
-
-def _measures_populate_none(expr):
-   from abjad.measure.measure import _Measure
-   for measure in iterate.naive(expr, _Measure):
-      measure[ : ] = [ ]
-
-
-def _measures_populate_skip(expr):
-   from abjad.measure.measure import _Measure
-   for measure in iterate.naive(expr, _Measure):
-      skip = Skip(1)
-      ## allow zero-update iteration
-      forced_meter = measure.meter.forced
-      if forced_meter is not None:
-         meter = forced_meter
-      else:
+   for i, measure in enumerate(iterate.naive(expr, _Measure)):
+      if iterctrl(measure, i):
          meter = measure.meter.effective
-      skip.duration.multiplier = meter.duration * ~meter.multiplier
-      measure[ : ] = [skip]
-      measure.spanners._detach( )
+         total_duration = meter.duration
+         prolation = meter.multiplier
+         notes = construct.note_train(
+            0, written_duration, total_duration, prolation)
+         measure[:] = notes
+
+
+def _measures_populate_meter_series(expr, iterctrl):
+   from abjad.measure.measure import _Measure
+   for i, measure in enumerate(iterate.naive(expr, _Measure)):
+      if iterctrl(measure, i):
+         meter = measure.meter.effective
+         denominator = mathtools.next_least_power_of_two(meter.denominator)
+         numerator = meter.numerator
+         notes = Note(0, (1, denominator)) * numerator
+         measure[:] = notes
+
+
+def _measures_populate_none(expr, iterctrl):
+   from abjad.measure.measure import _Measure
+   for i, measure in enumerate(iterate.naive(expr, _Measure)):
+      if iterctrl(measure, i):
+         measure[:] = [ ]
+
+
+def _measures_populate_skip(expr, iterctrl):
+   from abjad.measure.measure import _Measure
+   for i, measure in enumerate(iterate.naive(expr, _Measure)):
+      if iterctrl(measure, i):
+         skip = Skip(1)
+         ## allow zero-update iteration
+         forced_meter = measure.meter.forced
+         if forced_meter is not None:
+            meter = forced_meter
+         else:
+            meter = measure.meter.effective
+         skip.duration.multiplier = meter.duration * ~meter.multiplier
+         measure[:] = [skip]
+         measure.spanners._detach( )
