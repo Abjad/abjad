ad55315c7303880ec2d27115cc7bfb0e1567b03d
Víctor Adán <vctradn@gmail.com>
2009-04-23 04:21:47 +0000

    Sphinx documentation script ``make-tools-toc`` deprecated (and removed) in favor of more general ``make-api-tocs``. This last script now generates the whole Abjad API.
    Also changed the behavior of _create_subdirs_interactive( ) and _write_file_interactive( ) so that the user query defaults to `Yes`, in other words, the user can hit either `y` or `Enter` to confirm writing a file or directory.

--- trunk/abjad/documentation/doc_sphinx/scr/_create_subdirs_interactive.py
+++ trunk/abjad/documentation/doc_sphinx/scr/_create_subdirs_interactive.py
@@ -6,7 +6,7 @@ def _create_subdirs_interactive(root_dir, dirs):
    for dir in dirs:
       new_dir = os.path.join(root_dir, dir)
       if not os.path.exists(new_dir):
-         msg = 'Will create new directory "%s". Proceed? [y/n]: ' % new_dir
+         msg = 'Will create new directory "%s". Proceed? [Y/n]: ' % new_dir
          input = raw_input(msg)
-         if input.lower( ) == 'y':
+         if input.lower( ) in ('y', ''):
             os.mkdir(new_dir)
--- trunk/abjad/documentation/doc_sphinx/scr/_write_file_interactive.py
+++ trunk/abjad/documentation/doc_sphinx/scr/_write_file_interactive.py
@@ -1,8 +1,8 @@
 
 def _write_file_interactive(content, file):
-   msg = 'Will write file "%s". Proceed? [y/n]:' % file
+   msg = 'Will write file "%s". Proceed? [Y/n]:' % file
    input = raw_input(msg)
-   if input.lower( ) == 'y':
+   if input.lower( ) in ('y', ''):
       listing_file = open(file, 'w')
       listing_file.write(content)
       listing_file.close( )
