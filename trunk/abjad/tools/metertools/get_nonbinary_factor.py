6173f2d644a1a392644f77a67fea17809c3ca291
Trevor Bača <trevorbaca@gmail.com>
2009-04-24 16:27:18 +0000

    New metertools.get_nonbinary_factor(meter) function.
    Description:
    
       Get nonbinary factor in meter denominator, else 1.
    
    
    Examples:
    
       assert metertools.get_nonbinary_factor(Meter(3, 12)) == 3
       assert metertools.get_nonbinary_factor(Meter(3, 18)) == 9
       assert metertools.get_nonbinary_factor(Meter(3, 19)) == 19
       assert metertools.get_nonbinary_factor(Meter(3, 20)) == 5
       assert metertools.get_nonbinary_factor(Meter(3, 21)) == 21
       assert metertools.get_nonbinary_factor(Meter(3, 24)) == 3
       assert metertools.get_nonbinary_factor(Meter(3, 28)) == 7
    
       assert metertools.get_nonbinary_factor(Meter(3, 8)) == 1

---
+++ trunk/abjad/tools/metertools/get_nonbinary_factor.py
@@ -0,0 +1,10 @@
+from abjad.meter.meter import Meter
+from abjad.tools import mathtools
+
+
+def get_nonbinary_factor(meter):
+   '''Return nonbinary factor in denominator of meter, else 1.'''
+
+   assert isinstance(meter, Meter)
+   nonbinary_factor = mathtools.remove_powers_of_two(meter.denominator)
+   return nonbinary_factor
