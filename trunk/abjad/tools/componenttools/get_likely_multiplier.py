74bf775630119bd2ab34378d297bea48534f78db
Trevor Bača <trevorbaca@gmail.com>
2009-04-26 00:15:11 +0000

    Patch to previous commit to point two functions to the new tietools.get_duration_written( ) instead of the deprecated tietools.duration_written( ).

--- trunk/abjad/tools/componenttools/get_likely_multiplier.py
+++ trunk/abjad/tools/componenttools/get_likely_multiplier.py
@@ -22,7 +22,7 @@ def get_likely_multiplier(components):
    chain_duration_numerators = [ ]
    for expr in iterate.chained_contents(components):
       if tietools.is_chain(expr):
-         chain_duration = tietools.duration_written(expr)   
+         chain_duration = tietools.get_duration_written(expr)   
          chain_duration_numerators.append(chain_duration._n)
        
    if len(listtools.unique(chain_duration_numerators)) == 1:
--- trunk/abjad/tools/divide/tie_chain.py
+++ trunk/abjad/tools/divide/tie_chain.py
@@ -9,7 +9,7 @@ def tie_chain(chain, divisions = 2, prolation = 'diminution'):
    '''Generalization of divide.leaf( ) function.'''
 
    # find target duration of fixed-duration tuplet
-   target_duration = tietools.duration_written(chain[0].tie.chain)
+   target_duration = tietools.get_duration_written(chain[0].tie.chain)
 
    # find prolated duration of each note in tuplet
    prolated_duration = target_duration / divisions
