0b95c8b880d7f71ce028170c88f33d58e8162caa
Víctor Adán <vctradn@gmail.com>
2009-04-27 16:37:06 +0000

    Replaced all instances of `float('infinity')` with sys.maxint. Only two occurrences found in abjad/tools/flatten.py.

--- trunk/abjad/tools/listtools/flatten.py
+++ trunk/abjad/tools/listtools/flatten.py
@@ -1,4 +1,6 @@
-def flatten(l, ltypes=(list, tuple), depth = float('infinity')):
+import sys
+
+def flatten(l, ltypes=(list, tuple), depth = sys.maxint):
    '''Flatten nested lists. Returns a 0-depth list or tuple.
       Set optional 'depth' keyword set to positive integer.
       Keyword controls depth to which flatten operates.
@@ -18,7 +20,8 @@ def flatten(l, ltypes=(list, tuple), depth = float('infinity')):
    abjad> listtools.flatten(t, depth = 2)
    [1, 2, 3, 4, 5, 6, 7, 8]'''
 
-   if depth < float('infinity'):
+   #if depth < float('infinity'):
+   if depth < sys.maxint:
       return _flatten_to_depth(l, depth)
 
    assert isinstance(l, ltypes)
