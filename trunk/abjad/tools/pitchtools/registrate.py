0a321e9eb0451a692103306f55e1278f9a1aaace
Trevor Bača <trevorbaca@gmail.com>
2009-05-08 12:28:11 +0000

    Two new tools/pitchtools functions:
       pitchtools.octave_transpositions(pitches, r)
       pitchtools.registrate(pitches, pcs)
    
    The pitchtools.octave_transpositions(pitches, r) function lists all octave transpositions of 'pitches' in (closed) range 'r':
    
          pitchtools.octave_transpositions([0, 2, 4], [0, 48])
          [[0, 2, 4], [12, 14, 16], [24, 26, 28], [36, 38, 40]]
    
    The pitchtools.registrate(pcs, pitches) turns pitch-classes into pitches:
    
       pcs = [10, 0, 2, 6, 8, 7, 5, 3, 1, 9, 4, 11]
       pitches = [10, 19, 20, 23, 24, 26, 27, 29, 30, 33, 37, 40]
       result = pitchtools.registrate(pcs, pitches)
    
       assert result == [10, 24, 26, 30, 20, 19, 29, 27, 37, 33, 40, 23]

---
+++ trunk/abjad/tools/pitchtools/registrate.py
@@ -0,0 +1,20 @@
+from abjad.tools import listtools
+
+
+def registrate(pcs, pitches):
+   '''Turn pitch-classes into pitches.
+
+   abjad> pitchtools.registrate(
+         [10, 0, 2, 6, 8, 7, 5, 3, 1, 9, 4, 11],
+         [10, 19, 20, 23, 24, 26, 27, 29, 30, 33, 37, 40]) 
+   [10, 24, 26, 30, 20, 19, 29, 27, 37, 33, 40, 23]'''
+
+   if isinstance(pcs, list):
+      r = [[p for p in pitches if p % 12 == pc] for pc in [x % 12 for x in pcs]]
+      r = listtools.flatten(r)
+   elif isinstance(pcs, int):
+      r = [p for p in pitches if p % 12 == pcs][0]
+   else:
+      raise TypeError
+
+   return r
