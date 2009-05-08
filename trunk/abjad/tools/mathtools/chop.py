8e159b961c05365d07a0c85a965d9cefeed17f87
Trevor Bača <trevorbaca@gmail.com>
2009-05-08 10:55:07 +0000

    Reimplemented mathtools.chop( ) as a one-line wrapper around built-in int( ).

--- trunk/abjad/tools/mathtools/chop.py
+++ trunk/abjad/tools/mathtools/chop.py
@@ -1,9 +1,4 @@
-## TODO: Reimplement as simply int(x)? ##
-
 def chop(x):
    '''Return the integer part of x.'''
-
-   if x >= 0:
-      return int(x)
-   else:
-      return -int(abs(x))
+   
+   return int(x)
