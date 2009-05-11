343072afc500e05a5d058be743fb61e5f4a772c9
Trevor Bača <trevorbaca@gmail.com>
2009-05-11 12:23:07 +0000

    Simplified _write_abjad_versionfile( ) and _get_abjad_version( ) with calls to svnversion utility provided as part of standard Subversion deployment.

--- trunk/abjad/cfg/abjad_version.py
+++ trunk/abjad/cfg/abjad_version.py
@@ -1,17 +1,21 @@
 from abjad.cfg.cfg import ABJADVERSIONFILE
 import os
-import subprocess
 
 
 def _get_abjad_version( ):
-   f = open(ABJADVERSIONFILE, 'r')
-   abjad_version = f.readlines( )
-   f.close( )
-   abjad_version = [line for line in abjad_version \
-                  if line.startswith('Revision')]
-   abjad_version = abjad_version[0].split(':')[1].strip( )
-   return abjad_version
+#   f = open(ABJADVERSIONFILE, 'r')
+#   abjad_version = f.readlines( )
+#   f.close( )
+#   abjad_version = [line for line in abjad_version \
+#                  if line.startswith('Revision')]
+#   abjad_version = abjad_version[0].split(':')[1].strip( )
+#   return abjad_version
+   return file(ABJADVERSIONFILE, 'r').read( ).strip( )
 
+
+## TODO: Replace call to os.popen( ) with subprocess ##
 
 def _write_abjad_versionfile( ):
-   os.system('LANG= svn info > %s' % ABJADVERSIONFILE)
+   #os.system('LANG= svn info > %s' % ABJADVERSIONFILE)
+   version = os.popen('svnversion').read( ).strip( )
+   file(ABJADVERSIONFILE, 'w').write(version)
