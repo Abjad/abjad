007793988b06b4ef55606c3f3f6e97e460e9345d
Víctor Adán <vctradn@gmail.com>
2009-05-20 22:16:44 +0000

    Added 'scr' to the list of directories that are not to be loaded by the mail __init__.py file.

--- trunk/abjad/__init__.py
+++ trunk/abjad/__init__.py
@@ -1,7 +1,7 @@
 from abjad.tools.imports.import_functions_in_package_to_namespace import \
    _import_functions_in_package_to_namespace
 
-_skip = ['book', 'checks', 'demos', 'documentation', 'svn', 'tools', 'test']
+_skip = ['book', 'checks', 'demos', 'documentation', 'scr', 'svn', 'tools', 'test']
 _import_functions_in_package_to_namespace(__path__[0], globals( ), _skip)
 
 from abjad.tools.io import *
