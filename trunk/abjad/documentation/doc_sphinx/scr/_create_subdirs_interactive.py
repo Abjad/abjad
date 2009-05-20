877febe935dd1899fec6eaed53d49f828a63ef65
Trevor Bača <trevorbaca@gmail.com>
2009-05-20 17:19:58 +0000

    Extended `documentation/doc_sphinx/scr/make-abjad-api` to bypass interactive mode at user discretion.

--- trunk/abjad/documentation/doc_sphinx/scr/_create_subdirs_interactive.py
+++ trunk/abjad/documentation/doc_sphinx/scr/_create_subdirs_interactive.py
@@ -1,12 +1,18 @@
 import os
 
-def _create_subdirs_interactive(root_dir, dirs):
-   '''Create directories `dirs` in directory `root_dir` interactively.
-   i.e. the user is prompted before the creation of each new directory.'''
+
+def _create_subdirs_interactive(root_dir, dirs, interactive):
+   '''Create directories `dirs` in directory `root_dir`.
+      If user specifies interactive mode, prompt before each.'''
+
    for dir in dirs:
       new_dir = os.path.join(root_dir, dir)
       if not os.path.exists(new_dir):
-         msg = 'Will create new directory "%s". Proceed? [Y/n]: ' % new_dir
-         input = raw_input(msg)
+         if interactive.lower( ) in ('y', ''):
+            msg = 'Will create new directory "%s". Proceed? [Y/n]: ' % new_dir
+            input = raw_input(msg)
+         else:
+            input = 'Y'
          if input.lower( ) in ('y', ''):
+            print 'Creating directory %s ...' % new_dir
             os.mkdir(new_dir)
