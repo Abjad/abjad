da317c47e51767f6b01b68bc98f592ffe2cbea19
Trevor Bača <trevorbaca@gmail.com>
2009-05-26 12:12:28 +0000

    Added docstring to ``durtools.seconds_to_clock_string( )``.
    Cosmetic change to the output of ``durtools.seconds_to_clock_string( )``. Function previously output values like ``1'17''``. Function now outputs values like ``1'17"``.

--- trunk/abjad/tools/durtools/seconds_to_clock_string.py
+++ trunk/abjad/tools/durtools/seconds_to_clock_string.py
@@ -2,7 +2,13 @@ from abjad.rational.rational import Rational
 
 
 def seconds_to_clock_string(total_seconds):
-   '''Format h'mm'' clock string from numeric seconds.'''
+   '''Format ``seconds`` as h'mm'' rounded string.
+
+      Example::
+  
+         abjad> durtools.seconds_to_clock_string(117)   
+         "1'57''"
+   '''
 
    assert isinstance(total_seconds, (int, float, Rational))
    if total_seconds < 0:
@@ -10,6 +16,6 @@ def seconds_to_clock_string(total_seconds):
 
    minutes = int(total_seconds / 60)
    seconds = str(int(total_seconds - minutes * 60)).zfill(2)
-   clock_string = "%s'%s''" % (minutes, seconds)
+   clock_string = "%s'%s\"" % (minutes, seconds)
 
    return clock_string
