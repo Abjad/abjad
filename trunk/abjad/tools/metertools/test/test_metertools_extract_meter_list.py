76d1c87b8a5c169aa794136ed2344888ea618903
Trevor Bača <trevorbaca@gmail.com>
2009-05-26 12:41:44 +0000

    New ``metertools.extract_meter_list( )`` function added to ``tools/metertools`` package.
    Function extracts ordered list of meter pairs from components.
    
    Example::
    
       t = Staff([RigidMeasure((2, 8), construct.scale(2)),
          RigidMeasure((3, 8), construct.scale(3)),
          RigidMeasure((4, 8), construct.scale(4))])
    
       r'''\new Staff {
                       \time 2/8
                       c'8
                       d'8
                       \time 3/8
                       c'8
                       d'8
                       e'8
                       \time 4/8
                       c'8
                       d'8
                       e'8
                       f'8
       }'''
    
       meter_list = metertools.extract_meter_list(t[:])
       assert meter_list == [(2, 8), (3, 8), (4, 8)]
    
    This example appears in the new ``tools/metertools/test/test_metertools_extract_meter_list.py`` testfile.

---
+++ trunk/abjad/tools/metertools/test/test_metertools_extract_meter_list.py
@@ -0,0 +1,43 @@
+from abjad import *
+
+
+def test_metertools_extract_meter_list_01( ):
+   '''Extract ordered list of meter pairs from components.'''
+
+   t = Staff([RigidMeasure((2, 8), construct.scale(2)),
+      RigidMeasure((3, 8), construct.scale(3)),
+      RigidMeasure((4, 8), construct.scale(4))])   
+
+   r'''\new Staff {
+                   \time 2/8
+                   c'8
+                   d'8
+                   \time 3/8
+                   c'8
+                   d'8
+                   e'8
+                   \time 4/8
+                   c'8
+                   d'8
+                   e'8
+                   f'8
+   }'''   
+
+   meter_list = metertools.extract_meter_list(t[:])
+   assert meter_list == [(2, 8), (3, 8), (4, 8)]
+
+
+def test_metertools_extract_meter_list_02( ):
+   '''Extract ordered list of meter pairs from components.'''
+
+   t = Staff(construct.scale(4))
+
+   r'''\new Staff {
+           c'8
+           d'8
+           e'8
+           f'8
+   }'''
+
+   meter_list = metertools.extract_meter_list(t[:])
+   assert meter_list == [ ]
