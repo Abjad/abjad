e38da88ab50870674b9f9f213024e582a81dfe2d
Trevor Bača <trevorbaca@gmail.com>
2009-05-27 20:23:32 +0000

    New ``group_seconds( )`` function added to ``tools/durtools`` package.
    The new ``group_seconds( )`` function implements the same interface as the existing ``group_prolated( )`` function. The two should be thought of as a pair.
    
    Signature:
    
       def group_seconds(components, durations, fill = 'exact', cyclic = False, rump = False)
    
    Parameters:
    
       * Group ``component`` according to ``durations`` in seconds.
       * When ``fill == exact``, parts must equal durations exactly.
       * When ``fill == less``, parts must be less or equal to durations.
       * When ``fill == greater``, parts must be greater or equal to durations.
       * If ``cyclic`` is True, read *durations* cyclically.
       * If ``rump`` is True and components remain, append as final part.
       * If ``rump`` is False and components remain, do not append final part.
    
    Example. Group components by duration in number of seconds. Read durations cyclically. If components remain, do not append final part::
    
       t = Staff(RigidMeasure((2, 8), construct.run(2)) * 4)
       pitchtools.diatonicize(t)
       tempo_spanner = Tempo(t[:])
       tempo_spanner.indication = TempoIndication(Rational(1, 4), 60)
    
       \new Staff {
             \time 2/8
             \tempo 4=60
             c'8
             d'8
             \time 2/8
             e'8
             f'8
             \time 2/8
             g'8
             a'8
             \time 2/8
             b'8
             c''8
             %% tempo 4=60 ends here
       }
    
       groups = durtools.group_seconds(t.leaves, [1.5], cyclic = True)
    
       "[[Note(c'', 8), Note(b', 8), Note(a', 8)], [Note(g', 8), Note(f', 8), Note(e', 8)]]"
    
       assert len(groups) == 2
       assert groups[0] == t.leaves[:3]
       assert groups[1] == t.leaves[3:6]
    
    New ``tools/durtools/test/test_durtools_group_seconds.py`` testfile added.

--- trunk/abjad/tools/durtools/group_seconds.py
+++ trunk/abjad/tools/durtools/group_seconds.py
@@ -1,17 +1,17 @@
 from abjad.tools.durtools._group import _group as durtools__group
 
 
-def group_prolated(
+def group_seconds(
    components, durations, fill = 'exact', cyclic = False, rump = False):
-   '''Group ``component`` according to prolated ``durations``.
+   '''Group ``component`` according to ``durations`` in seconds.
       When ``fill == exact``, parts must equal durations exactly.
       When ``fill == less``, parts must be less or equal to durations.
       When ``fill == greater``, parts must be greater or equal to durations.
       If ``cyclic`` is True, read *durations* cyclically.
       If ``rump`` is True and components remain, append as final part.
       If ``rump`` is False and components remain, do not append final part.'''
-   
-   duration_type = 'prolated'
 
-   return durtools__group(duration_type,
+   duration_type = 'seconds'
+
+   return durtools__group(duration_type, 
       components, durations, fill = fill, cyclic = cyclic, rump = rump)
