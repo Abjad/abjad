f0bbed6698c213a6f5e1fd300c35f49530fe48ea
Trevor Bača <trevorbaca@gmail.com>
2009-06-02 20:27:52 +0000

    Fixed ``test_breaks_interface_x.py`` TODO.
    Fixed ``test_breaks_interface_y.py`` TODO.

--- trunk/abjad/breaks/test/test_breaks_interface_y.py
+++ trunk/abjad/breaks/test/test_breaks_interface_y.py
@@ -1,27 +1,23 @@
 from abjad import *
 
 
-## TODO: Fix _BreaksInterface.y test. ##
+def test_breaks_interface_y_01( ):
+   '''``_BreaksInterface.y`` formats the \
+   LilyPond ``NonMusicalPaperColumn`` prob.'''
 
-#def test_breaks_interface_y_01( ):
-#   '''
-#   _BreaksInterface y formats the LilyPond NonMusicalPaperColumn prob.
-#   '''
-#
-#   t = RigidMeasure((4, 8), construct.scale(4))
-#   t[-1].breaks.line = True
-#   t[-1].breaks.y = 80
-#
-#   r'''
-#      \time 4/8
-#      c'8
-#      d'8
-#      e'8
-#      f'8
-#      \break
-#      \overrideProperty #"Score.NonMusicalPaperColumn"
-#      #'line-break-system-details
-#      #'((Y-offset . 80))
-#   '''
-#
-#   assert t.format == '\t\\time 4/8\n\tc\'8\n\td\'8\n\te\'8\n\tf\'8\n\t\\break\n\t\\overrideProperty #"Score.NonMusicalPaperColumn"\n\t#\'line-break-system-details\n\t#\'((Y-offset . 80))'
+   t = RigidMeasure((4, 8), construct.scale(4))
+   t.breaks.line = True
+   t.breaks.y = 80
+
+   r'''
+      \overrideProperty #"Score.NonMusicalPaperColumn"
+      #'line-break-system-details
+      #'((Y-offset . 80))
+      \time 4/8
+      c'8
+      d'8
+      e'8
+      f'8
+      \break'''
+
+   assert t.format == '\t\\overrideProperty #"Score.NonMusicalPaperColumn"\n\t#\'line-break-system-details\n\t#\'((Y-offset . 80))\n\t\\time 4/8\n\tc\'8\n\td\'8\n\te\'8\n\tf\'8\n\t\\break'
