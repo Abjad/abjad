7d3620f802fb7eb7363f19484597d01d1f706f2f
Trevor Bača <trevorbaca@gmail.com>
2009-06-02 03:06:09 +0000

    Greatly elaborate docstrings for two class attributes::
       _TupletFormatterSlotsInterface.slot_1
       _TupletFormatterSlotsInterface.slot_2
    
    Each attribute now carries a 70- or 80-line docstring, which is what's necessary to give an accurate representation of slot contents.
    
    Slots 3, 4, 5, 6 and 7 need similar work.

--- trunk/abjad/tuplet/slots.py
+++ trunk/abjad/tuplet/slots.py
@@ -3,14 +3,100 @@ from abjad.tools import durtools
 
 
 class _TupletFormatterSlotsInterface(_ContainerFormatterSlotsInterface):
+   '''Collect all tuplet format contributions and \
+      order by the seven canonical format slots.
 
-   def __init__(self, client):
-      _ContainerFormatterSlotsInterface.__init__(self, client)
+      *  Every slots interface binds to a formatter.
+      *  The :class:`_TupletFormatterSlotsInterface \
+         <abjad.tuplet.slots._TupletFormatterSlotsInterface>`
+         described here binds to the :class:`_TupletFormatter \
+         <abjad.tuplet.formatter._TupletFormatter>`.
+
+      ::
+
+         
+         abjad> t = FixedDurationTuplet((2, 8), construct.scale(3))
+         abjad> t.formatter
+         <_TupletFormatter>
+
+      ::
+
+         abjad> t.formatter.slots
+         <_TupletFormatterSlotsInterface>'''
+
+   def __init__(self, _client):
+      '''Init as type of :class:`_ContainerFormatterSlotsInterface \
+         <abjad.container.slots._ContainerFormatterSlotsInterface>`.'''
+
+      _ContainerFormatterSlotsInterface.__init__(self, _client)
       
    ## PUBLIC ATTRIBUTES ##
 
    @property
    def slot_1(self):
+      r'''Read-only tuple of format contributions \
+         to appear before tuplet opening.
+
+         There are four possible types of these and they appear \
+         in the following canonic order:
+
+         *  User comments to come before tuplet opening
+         *  User directives to come before tuplet opening
+         *  Interface overrides
+         *  Tuplet coloring
+
+         Newly created, unmodified tuplets make no format \
+         contributions to slot 1::
+
+            abjad> t = FixedDurationTuplet((2, 8), construct.scale(3))
+
+         ::
+
+            abjad> import pprint
+            abjad> pprint.pprint(t.formatter.slots.slot_1)
+            ([(<_UserComments>, 'before'), []],
+             [(<_UserDirectivesInterface>, 'before'), []],
+             [(<_InterfaceAggregator>, 'overrides'), []])
+
+         ::
+
+            abjad> print t.format
+            \times 2/3 {
+                    c'8
+                    d'8
+                    e'8
+            }
+
+         Modified tuplets, on the other hand, may make \
+         format contributions to slot 1::
+
+            abjad> t.comments.append('This is a tuplet')
+            abjad> t.notehead.color = 'red'
+            abjad> t.dots.color = 'red'
+     
+         ::
+
+            abjad> import pprint
+            abjad> pprint.pprint(t.formatter.slots.slot_1)
+            ([(<_UserComments>, 'before'), ['% This is a tuplet']],
+             [(<_UserDirectivesInterface>, 'before'), []],
+             [(<_InterfaceAggregator>, 'overrides'),
+              ["\\override Dots #'color = #red", "\\override NoteHead #'color = #red"]])
+   
+         ::
+
+            abjad> print t.format
+            % This is a tuplet
+            \override Dots #'color = #red
+            \override NoteHead #'color = #red
+            \times 2/3 {
+                    c'8
+                    d'8
+                    e'8
+            }
+            \revert Dots #'color
+            \revert NoteHead #'color'''
+
       result = [ ]
       tuplet = self.formatter.tuplet
       result.append(self.wrap(tuplet.comments, 'before'))
@@ -25,6 +111,97 @@ class _TupletFormatterSlotsInterface(_ContainerFormatterSlotsInterface):
 
    @property
    def slot_2(self):
+      r'''Read-only tuple of format contributions used \
+         to generate tuplet opening.
+
+         Most tuplets open with the *LilyPond* ``\times`` command only::
+
+            abjad> t = FixedDurationTuplet((2, 8), construct.scale(3))
+
+         ::
+
+            abjad> print t.format
+            \times 2/3 {
+                    c'8
+                    d'8
+                    e'8
+            }
+
+         This is apparent in the contents of the slot 2::
+
+            abjad> pprint.pprint(t.formatter.slots.slot_2)
+            ([(<_BracketsInterface>, 'open'), ['\\times 2/3 {']],)
+
+         Trivial tuplets carry a ratio of ``1:1``::
+
+            abjad> t = FixedDurationTuplet((3, 8), construct.scale(3)
+            abjad> t.duration.multiplier
+            Rational(1, 1)
+
+         Such tuplets output unscaled notes and rests::
+
+            abjad> print t.format
+                    c'8
+                    d'8
+                    e'8
+
+         For this reason, trivial tuplets make no \
+         format contributions to slot 2::
+
+            abjad> pprint.pprint(t.formatter.slots.slot_2)
+            ()
+
+         You can make trivial tuplets format as actual tuplets \
+         by setting ``color = True`` on the class of \
+         the tuplet in question. \
+         Colored trivial tuplets appear blue in printed output::
+
+            abjad> t = FixedDurationTuplet((3, 8), construct.scale(3))
+            abjad> FixedDurationTuplet.color = True
+
+         ::
+
+            abjad> print t.format
+            \tweak #'color #blue
+            \times 1/1 {
+                    c'8
+                    d'8
+                    e'8
+            }
+
+         ::
+
+            abjad> pprint.pprint(t.formatter.slots.slot_2)
+            ([(<_BracketsInterface>, 'open'), ['\\times 1/1 {']],)
+
+         Note that ``color`` is a tuplet class attribute, \
+         not a tuplet instance attribute.
+      
+         Finally, settings ``t.invisible = True`` causes the tuplet \
+         to output with the *LilyPond* ``\scaleDurations`` command \
+         instead of the the *Lilypond* ``\times`` command::
+
+            abjad> t = FixedDurationTuplet((2, 8), construct.scale(3))
+            abjad> t.invisible = True
+
+         ::
+
+            abjad> print t.format
+            \scaleDurations #'(2 . 3) {
+                    c'8
+                    d'8
+                    e'8
+            }
+
+         ::
+
+            abjad> pprint.pprint(t.formatter.slots.slot_2)
+            ([(FixedDurationTuplet(1/4, [c'8, d'8, e'8]), 'invisible'),
+              ["\\scaleDurations #'(2 . 3) {"]],)
+
+         Invisible tuplets carry neither tuplet bracket nor \
+         tuplet ratio in the printed output.'''
+
       result = [ ]
       formatter = self.formatter
       tuplet = formatter.tuplet
@@ -48,6 +225,9 @@ class _TupletFormatterSlotsInterface(_ContainerFormatterSlotsInterface):
 
    @property
    def slot_3(self):
+      '''Read-only tuple of format contributions to appear \
+         immediately after tuplet opening.'''
+
       result = [ ]
       tuplet = self.formatter.tuplet
       result.append(self.wrap(tuplet.comments, 'opening'))
@@ -58,6 +238,9 @@ class _TupletFormatterSlotsInterface(_ContainerFormatterSlotsInterface):
 
    @property
    def slot_5(self):
+      '''Read-only tuple of format contributions to appear \
+         immediately before tuplet closing.'''
+
       result = [ ]
       tuplet = self.formatter.tuplet
       result.append(self.wrap(tuplet.interfaces, 'closing'))
@@ -68,6 +251,10 @@ class _TupletFormatterSlotsInterface(_ContainerFormatterSlotsInterface):
 
    @property
    def slot_6(self):
+      '''Read-only tuplet of format contributions used \
+         to generate tuplet closing. \
+         Usually just a single ``}`` close brace.'''
+   
       result = [ ]
       tuplet = self.formatter.tuplet
       if tuplet.duration.multiplier:
@@ -78,6 +265,9 @@ class _TupletFormatterSlotsInterface(_ContainerFormatterSlotsInterface):
 
    @property
    def slot_7(self):
+      '''Read-only tuple of format contributions \
+         to appear immediately after tuplet closing.'''
+
       result = [ ]
       tuplet = self.formatter.tuplet
       result.append(self.wrap(tuplet.directives, 'after'))
