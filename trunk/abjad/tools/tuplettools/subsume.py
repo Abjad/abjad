379fe829c84e8186a50aeffee2140adafed3f1fa
Trevor Bača <trevorbaca@gmail.com>
2009-06-02 13:04:58 +0000

    Added realistic usage example to ``tuplettools.subsume( )`` docstring.
    Added the same example as a test case to ``tools/tuplettools/test_tuplettools_subsume.py`` tetfile.

--- trunk/abjad/tools/tuplettools/subsume.py
+++ trunk/abjad/tools/tuplettools/subsume.py
@@ -2,8 +2,44 @@ from abjad.tuplet.tuplet import _Tuplet
 
 
 def subsume(tuplet):
-   '''Make tuplet go away, leaving scaled tuplet contents.
-      Return empty tuplet outside of score.'''
+   r'''Scale ``tuplet`` contents and then bequeath in-score \
+      position of ``tuplet`` to contents.
+
+      Return orphaned ``tuplet`` emptied of all contents. ::
+
+         abjad> t = Staff(FixedDurationTuplet((3, 8), construct.scale(2)) * 2)
+         abjad> Beam(t.leaves)
+         abjad> print t.format
+         \new Staff {
+                 \fraction \times 3/2 {
+                         c'8 [
+                         d'8
+                 }
+                 \fraction \times 3/2 {
+                         c'8
+                         d'8 ]
+                 }
+         }
+
+      ::
+
+         abjad> tuplettools.subsume(t[0])
+         FixedDurationTuplet(3/8, [ ])
+         abjad> print t.format
+         \new Staff {
+                 c'8. [
+                 d'8.
+                 \fraction \times 3/2 {
+                         c'8
+                         d'8 ]
+                 }
+         }
+
+
+      .. note:: This function should probably be called ``scale_contents_and_bequeath( )``.
+
+      .. note:: ``bequeath( )`` should probably be called something else, too.
+   '''
 
    assert isinstance(tuplet, _Tuplet)
    from abjad.tools import containertools
