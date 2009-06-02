8d576d3812c17d0e53b6a297b948c49e012d4458
Trevor Bača <trevorbaca@gmail.com>
2009-06-02 19:11:49 +0000

    Elaborated ``parenttools.get_with_indices( )`` docstring.

--- trunk/abjad/tools/parenttools/get_with_indices.py
+++ trunk/abjad/tools/parenttools/get_with_indices.py
@@ -2,10 +2,34 @@ from abjad.tools import check
 
 
 def get_with_indices(components):
-   '''Return parent of components in list.
-      Return index of first component in list in parent.
-      Return inex of last component in list in parent.
-      Otherwise None, None, None.'''
+   r'''Return ``(parent, start, stop)`` triple with
+   
+   *  ``parent`` a reference to the parent of ``components``
+   *  ``start`` the index of ``components[0]`` in ``parent``
+   *  ``stop`` the index of ``components[-1]`` in ``parent``
+
+   Any of the three return values may equal ``None``.
+
+   ::
+
+      abjad> t = Staff(construct.scale(6))
+      abjad> print t.format
+      \new Staff {
+         c'8
+         d'8
+         e'8
+         f'8
+         g'8
+         a'8
+      }
+
+   ::
+
+      abjad> leaves = t[-2:]
+      abjad> leaves
+      [Note(g', 8), Note(a', 8)]
+      abjad> parenttools.get_with_indices(leaves)
+      (Staff{6}, 4, 5)'''
 
    check.assert_components(components, contiguity = 'strict', share = 'thread')
 
