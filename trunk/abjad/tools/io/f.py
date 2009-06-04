3926d0748173cacf7b04be9b1c3ca86d18c89169
Trevor Bača <trevorbaca@gmail.com>
2009-06-04 15:29:55 +0000

    Removed dead comment from f.py.

--- trunk/abjad/tools/io/f.py
+++ trunk/abjad/tools/io/f.py
@@ -1,13 +1,3 @@
-#def f(expr):
-#   if hasattr(expr, 'format'):
-#      print expr.format
-#   elif hasattr(expr, '__contains__'):
-#      for x in expr:
-#         print x.format
-#   else:
-#      raise TypeError(
-#         'must be format object or be iterable of format objects.')
-
 def f(expr):
    '''Print the `LilyPond` input code of `Abjad` expression *expr*.
    
