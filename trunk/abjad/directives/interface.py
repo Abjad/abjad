13f35fafde4a0df96825d91633c9799e78182832
Trevor Bača <trevorbaca@gmail.com>
2009-06-05 13:33:23 +0000

    Added basic docstrings to _UserDirectives interface.

--- trunk/abjad/directives/interface.py
+++ trunk/abjad/directives/interface.py
@@ -3,8 +3,11 @@ from abjad.comments.comments import _UserComments
 
 
 class _UserDirectivesInterface(_Interface, _UserComments):
+   '''Interface to handle literal *LilyPond* directives that \
+   are not yet modelled explicitly in *Abjad*.'''
 
    def __init__(self, component):
+      '''Init as a subclass of the user comments interface.'''
       _Interface.__init__(self, component)
       _UserComments.__init__(self)
       self._left = [ ]
@@ -14,6 +17,9 @@ class _UserDirectivesInterface(_Interface, _UserComments):
    @apply
    def left( ):
       def fget(self):
+         '''User directives to left of component.
+         
+         .. todo:: deprecated ``left`` formatting slot altogether.'''
          return self._left
       def fset(self, arg):
          assert arg is None
@@ -22,4 +28,5 @@ class _UserDirectivesInterface(_Interface, _UserComments):
 
    @property
    def locations(self):
+      '''Format locations into which user may place directives.'''
       return ('before', 'opening', 'left', 'right', 'closing', 'after')
