3cc2946e41b739948ec399fb17407e72e9f039d5
Trevor Bača <trevorbaca@gmail.com>
2009-06-06 21:52:38 +0000

    Removed unused action keyword from listtools.rotate( ).

--- trunk/abjad/tools/listtools/rotate.py
+++ trunk/abjad/tools/listtools/rotate.py
@@ -1,14 +1,20 @@
-def rotate(l, direction, n = 1, action = 'new'):
-   '''>>> l = range(10)
+def rotate(l, direction, n = 1):
+   '''Rotate the elements of *l* *n* places to the *direction*.
+   
+   * *direction* must be either `'left'` or `'right'`.
+
+   ::
+
+      >>> l = range(10)
       >>> listtools.rotate(l, 'left', 3)
       [3, 4, 5, 6, 7, 8, 9, 0, 1, 2]
+
+   ::
 
       >>> listtools.rotate(l, 'right', 4)
       [6, 7, 8, 9, 0, 1, 2, 3, 4, 5]
 
-      >>> listtools.rotate(l, 'left', 3, action = 'in place')
-      >>> l
-      [3, 4, 5, 6, 7, 8, 9, 0, 1, 2]'''
+   .. todo:: Eliminate *direction* in favor of signed *n*.'''
 
    if direction == 'left':
       result = l[n:] + l[:n]
@@ -17,9 +23,4 @@ def rotate(l, direction, n = 1, action = 'new'):
    else:
       raise ValueError("direction must be 'left' or 'right'.")
 
-   if action == 'new':
-      return result
-   elif action == 'in place':
-      l[:] = result
-   else:
-      raise ValueError("action must be 'new' or 'in place'.")
+   return result
--- trunk/abjad/tools/listtools/test/test_listtools_rotate.py
+++ trunk/abjad/tools/listtools/test/test_listtools_rotate.py
@@ -9,13 +9,3 @@ def test_listtools_rotate_01( ):
 
    new = listtools.rotate(t, 'right', 4)
    assert new == [6, 7, 8, 9, 0, 1, 2, 3, 4, 5]
-
-
-def test_listtools_rotate_02( ):
-
-   t = range(10)
-   listtools.rotate(t, 'left', 3, action = 'in place')
-   assert t == [3, 4, 5, 6, 7, 8, 9, 0, 1, 2]
-
-   listtools.rotate(t, 'right', 4, action = 'in place')
-   assert t == [9, 0, 1, 2, 3, 4, 5, 6, 7, 8]
