ef4c4b6d3e400321b8222d612d43327ac7a74ddb
Víctor Adán <vctradn@gmail.com>
2009-06-10 21:29:01 +0000

    Enhancement to ``abjad-book``.
    New ``CodeBlock( )`` class added to the ``abjad-book`` parser. This new class greatly cleans up the ``abjad-book`` parsing code by removing and encapsulating all code-block related processing from the main processing code found in ``AbjadTag``. 
    
    The new ``CodeBlock( )`` class also adds the facility (via the filterFromOutput list property) to filter out lines from final output. General messages printed by *Abjad* may be added here if not wanted in the final document. This is currently hard-coded and assumes no one will want certain kinds of *Abjad* messages in the final document. 
    
    ``abjad-book`` is now also smarter about rendering images for documents. If the source is a *LaTex* document, ``abjad-book`` renders vector images (PDF) instead of raster PNGs. ``abjad-book`` still renders PNG images for HTML and reST documents.  
    Note that while ``abjad-book`` uses the *ImageMagick* ``convert`` utility to crop raster images to the minimum size, it now uses ``pdfcrop`` do perform the same operation on PDFs. This means that users of ``abjad-book`` will now require these two extra utilities. Keep in mind for future documentation.

--- trunk/abjad/book/parser/abjadlatextag.py
+++ trunk/abjad/book/parser/abjadlatextag.py
@@ -3,9 +3,11 @@ from abjad.book.parser.abjadtag import _AbjadTag
 class AbjadLatexTag(_AbjadTag):
    def __init__(self, lines):
       _AbjadTag.__init__(self, lines)
-#      self._target_open_tag = '\\begin{minipage}{5in} \\footnotesize \\begin{verbatim}\n'
-#      self._target_close_tag = '\\end{verbatim} \\normalsize \\end{minipage}\n'
-      self._target_open_tag = '\\begin{lstlisting}[basicstyle=\\footnotesize, tabsize=4, showtabs=false, showspaces=false]\n'
+
+      self._target_open_tag = '\\begin{lstlisting}'
+      self._target_open_tag += '[basicstyle=\\footnotesize, tabsize=4, '
+      self._target_open_tag += 'showtabs=false, showspaces=false]\n'
+
       self._target_close_tag = '\\end{lstlisting}\n'
 
-      self._image_tag = '\\begin{center}\n \\includegraphics{images/%s.png}\n\\end{center}'
+      self._image_tag = '\\includegraphics{images/%s.pdf}\n'
--- trunk/abjad/book/parser/abjadtag.py
+++ trunk/abjad/book/parser/abjadtag.py
@@ -1,6 +1,6 @@
+from abjad.book.parser.codeblock import CodeBlock
 from abjad.book.parser.tagparser import _TagParser
 import os
-import re
 import shutil
 import subprocess
 import sys
@@ -18,25 +18,25 @@ class _AbjadTag(_TagParser):
 
 
    def process(self):
+      self._verifyTag( )
+
       ## create temp directory to work in
-      _createDirectory('tmp_out')
+      _create_directory('tmp_out')
       os.chdir('tmp_out')
 
       ## start processing
-      self._verifyTag( )
       self._parse( )
-      rendered = self._renderImages( )
+      self._renderImages( )
       
       ## clean up directories
       os.chdir('..')
-      if rendered: 
-         _createDirectory('images') 
-         for element in os.listdir('tmp_out'):
-            if element.endswith('.png'):
-               shutil.move(os.path.join('tmp_out', element), 'images')
-         #os.system('mv tmp_out/*.png images')
-      #os.system('rm -r tmp_out')
+      _create_directory('images') 
+      for element in os.listdir('tmp_out'):
+         #if element.endswith('.png'):
+         if element[-4:] in ('.png', '.pdf'):
+            shutil.move(os.path.join('tmp_out', element), 'images')
       shutil.rmtree('tmp_out')
+
       return self.output
 
 
@@ -46,142 +46,83 @@ class _AbjadTag(_TagParser):
       print 'Parsing file...'
       input = self._input[:]
       while len(input) > 0:
-         code, type = self._get_next_code_block(input)
-         if code:
-            output_codeblock, images = self._handle_code_block(code, type)
-            if output_codeblock:
+         codeblock = self._get_next_code_block(input)
+         if codeblock:
+            self._handle_code_block(codeblock)
+            if codeblock.finalOutputCode:
                self.output.append(self._target_open_tag)
-               self.output.extend(output_codeblock)
+               self.output.extend(codeblock.finalOutputCode)
                self.output.append(self._target_close_tag)
-            if images:
-               for image in images:
+            if codeblock.images:
+               for image in codeblock.images:
                   self.output.append(self._image_tag % image)
 
 
    def _get_next_code_block(self, input):
-      result = [ ]
-      type = None
+      result = CodeBlock( )
+      result.type = None
       in_block = False
       for line in input[:]:
          input.remove(line) 
          if self._open_tag in line:
             in_block = True
-            if 'hide=true' in line.replace(' ', '').lower( ):
-               type = 'hide'
+            result.type = self._get_block_type_from_open_tag(line)
          elif self._close_tag in line:
-            return result, type
+            return result
          elif in_block:
-            result.append(line)
+            result.preProcessedCode.append(line)
          else:
             self.output.append(line + os.linesep)
-      return None, None
+      return None
 
-   def _handle_code_block(self, lines, type):
-#      lines = [line.replace('\n', '') for line in lines]
-      code, images = self._parse_code_block(lines, type)
-      self._abjad_code.extend(code)
+
+   def _get_block_type_from_open_tag(self, line):
+      if 'hide=true' in line.replace(' ', '').lower( ):
+         return 'hide'
+
+
+   def _handle_code_block(self, codeblock):
+      self._abjad_code.extend(codeblock.toProcessCode)
       out = _execute_abjad_code(self._abjad_code)
-      out = _extract_code_block(out)
-      out = _recover_commented_show_directives(out)
-      out = _remove_hidden_directives(out)
-      out = _insert_abjad_prompt(out, lines)
-      ## TODO os.linesep here inserts a white space between every line in 
-      ## Windowz. Using '\r' instead seems to work on both Linux and 
-      ## Windowz. use '\r' instead?
-      return ['\t%s%s' % (line, os.linesep) for line in out], images
+      codeblock.postProcessedCode = _extract_code_block(out)
+      codeblock._collectImages( )
 
 
    def _renderImages(self):
-      rendered_image = False
       for file in os.listdir(os.curdir):
          if file.endswith('.ly'):
             print 'Rendering "%s"...' % file
-            #file_base = file.rstrip('.ly')
-            file_base = file.replace('.ly', '')
-            # NOTE: setting stderr = sys.stderr 
-            # below will print LilyPond messages
-            p = subprocess.Popen(
-               'lilypond --png -dresolution=300 %s' % file,
-               shell = True, stdout = subprocess.PIPE, 
-               stderr = subprocess.PIPE)
-            out, err = p.communicate( )
-            ## NOTE: why popen and not system here?
-            output = 'convert %s.png -trim -resample 40%% %s.png'
-            os.popen(output % (file_base, file_base))
-            rendered_image = True
-      return rendered_image
+            if 'latex' in self.__class__.__name__.lower( ):
+               self._renderPDFimage(file)
+            else:
+               self._renderPNGimage(file)
 
 
-   def _parse_code_block(self, lines, type):
-      '''Parses code and adds it to self._abjad_code. 
-      Returns names of images found.'''
-      images = [ ]
-      code = [ ]
-      ## handle code block type
-      if type == 'hide':
-         for i in range(len(lines)):
-            lines[i] += '<hide'
+   def _renderPDFimage(self, filename):
+      file_base = filename.replace('.ly', '')
+      p = subprocess.Popen('lilypond  %s' % filename,
+         shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+      out, err = p.communicate( )
+      output = 'pdfcrop %s.pdf %s.pdf'
+      os.popen(output % (file_base, file_base))
 
-      code.append('print "## START ##"')
-      for line in lines:
-         ## get indentation
-         indent = re.search('^ +', line)
-         if indent:
-            indent = indent.group( )
 
-         ## get abjad directive 
-         if '<hide' in line:
-            abjad_directive = line.replace('<hide', '#<hide')
-         else:
-            abjad_directive = line
-
-         ## handle abjad directive
-         if 'show(' in abjad_directive:
-            abjad_directive = '%s#abjad_comment#%s' % ((indent or ''), 
-               abjad_directive)
-         
-         code.append("%sprint '''%s '''" % ((indent or ''), abjad_directive))
-         code.append(abjad_directive)
-
-         ## handle and collect images
-         if abjad_directive.startswith('write'):
-            images.append(_get_image_name(abjad_directive))
-
-      return code, images
+   def _renderPNGimage(self, filename):
+      file_base = filename.replace('.ly', '')
+      p = subprocess.Popen('lilypond --png -dresolution=300 %s' % filename,
+         shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+      out, err = p.communicate( )
+      ## NOTE: why popen and not system here?
+      output = 'convert %s.png -trim -resample 40%% %s.png'
+      os.popen(output % (file_base, file_base))
 
 
 ## HELPERS ##
 
-def _insert_abjad_prompt(output_lines, input_lines):
-   result = [ ]
-   for oline in output_lines:
-      oline = oline.rstrip( )
-      if oline in input_lines:
-         oline = 'abjad> ' + oline
-      result.append(oline)
-   return result
-      
-def _get_image_name(directive):
-   try:
-      image_name = directive.split(',')[1].split(')')[0]
-      image_name = image_name.strip(' ').strip("'").strip('"')
-      return image_name
-   except IndexError:
-      print "Problem parsing 'write( )'. Did you forget to add a file name?"
+def _create_directory(dir):
+   if not os.path.isdir(dir):
+      os.mkdir(dir)
 
-def _remove_hidden_directives(lines):
-   '''remove hidden lines'''
-   for line in lines[:]:
-      if '#<hide' in line:
-         lines.remove(line)
-   return lines
-
-def _recover_commented_show_directives(lines):
-   ''' remove #abjad_comments# from temporarily commented show( ).'''
-   for i, line in enumerate(lines):
-      if line.startswith('#abjad_comment#'):
-         lines[i] = line.strip('#abjad_comment#')
-   return lines
 
 def _extract_code_block(lines):
    for i, line in enumerate(reversed(lines)):
@@ -189,6 +130,7 @@ def _extract_code_block(lines):
          del(lines[0: -i])
          break
    return lines
+
 
 def _execute_abjad_code(lines):
    p = subprocess.Popen('python', stdin = subprocess.PIPE, 
@@ -202,140 +144,3 @@ def _execute_abjad_code(lines):
    out = out.splitlines( )
    return out
 
-def _createDirectory(dir):
-   if not os.path.isdir(dir):
-      os.mkdir(dir)
-
-
-## OLD VERSION ##
-#class _AbjadTag(_TagParser):
-#   def __init__(self, lines):
-#      _TagParser.__init__(self, lines)
-#      self._close_tag = '</abjad>'
-#      self._open_tag = '<abjad>'
-#      self._target_open_tag = '<pre class="abjad">\n'
-#      self._target_close_tag = '</pre>\n'
-#      self._abjad_code = ['from abjad import *\n']
-#      self._image_tag = '<img alt="" src="images/%s.png"/>\n'
-#      self._images_collected = [ ]
-#
-#
-#   def process(self):
-#      self._verifyTag( )
-#      self._parse( )
-#      self._runAbjadCode( )
-#      return self.output
-#
-#
-#   ## PRIVATE METHODS ##
-#
-#   def _parse(self):
-#      print 'Parsing file...'
-#      input = self._input[:]
-#      while len(input) > 0:
-#         code = self._get_next_code_block(input)
-#         if code:
-#            output_codeblock = self._handle_code_block(code)
-#            if output_codeblock:
-#               self.output.append(self._target_open_tag)
-#               self.output.extend(output_codeblock)
-#               self.output.append(self._target_close_tag)
-#            if len(self._images_collected) > 0:
-#               while len(self._images_collected) > 0:
-#                  self.output.append(self._images_collected.pop(0))
-#
-#
-#   def _get_next_code_block(self, input):
-#      result = [ ]
-#      in_block = False
-#      for line in input[:]:
-#         input.remove(line) 
-#         if self._open_tag in line:
-#            in_block = True
-#         elif self._close_tag in line:
-#            return result
-#         elif in_block:
-#            result.append(line)
-#         else:
-#            self.output.append(line)
-#
-#
-#   def _handle_code_block(self, lines):
-#      result = [ ]
-#      for line in lines:
-#         if not 'hide>' in line:
-#            result.append(line)
-#         ## get abjad directive 
-#         if 'abjad>' in line:
-#            abjad_directive = line.replace('abjad>', '').strip( )
-#         elif 'hide> ' in line:
-#            abjad_directive = line.replace('hide>', '').strip( )
-#         elif '>>> ' in line:
-#            abjad_directive = line.replace('>>>', '').strip( )
-#         else:
-#            abjad_directive = None
-#         ## handle abjad directive
-#         if abjad_directive:
-#            if not abjad_directive.startswith('show'):
-#               self._abjad_code.append(abjad_directive)
-#            ## handle and collect images
-#            if abjad_directive.startswith('write'):
-#               try:
-#                  image_name = abjad_directive.split(',')[1]
-#                  image_name = image_name.strip(' ').rstrip(')').strip("'")
-#                  image = self._image_tag % image_name
-#                  self._images_collected.append(image)
-#               except IndexError:
-#                  print "write( ) function must be given a file name!"
-#      return result 
-#
-#
-#   def _makeImages(self):
-#      print 'Rendering score images with LilyPond...'
-#      for file in os.listdir(os.curdir):
-#         if file.endswith('.ly'):
-#            file_base = file.rstrip('.ly')
-#            # NOTE: setting stderr = sys.stderr 
-#            # below will print LilyPond messages
-#            p = subprocess.Popen(
-#               'lilypond --png -dresolution=300 %s' % file,
-#               shell = True, stdout = subprocess.PIPE, 
-#               stderr = subprocess.PIPE)
-#            p.communicate( )
-#            ## NOTE: why popen and not system here?
-#            output = 'convert %s.png -trim -resample 40%% %s.png'
-#            os.popen(output % (file_base, file_base))
-#
-#
-#   def _runAbjadCode(self):
-#      def _createTempDirectory( ):
-#         if not os.path.isdir('tmp_out'):
-#            os.mkdir('tmp_out')
-#
-#      def _createImagesDirectory( ):
-#         if not os.path.isdir('images'):
-#            os.mkdir('images')
-#
-#      ## write abjad code to file
-#      _createTempDirectory( )
-#      os.chdir('tmp_out')
-#      f = open('tmp_abjad.aj', 'w')
-#      f.writelines('\n'.join(self._abjad_code))
-#      f.close( )
-#      ## run it.
-#      p = subprocess.Popen('python tmp_abjad.aj', shell = True, 
-#         stdout = subprocess.PIPE, stderr = subprocess.PIPE)
-#      out, error =  p.communicate( )
-#      if error:
-#         #raise SyntaxError(error)
-#         print "\nERROR in Abjad script compilation:"
-#         print error
-#         os.chdir('..')
-#         os.system('rm -r tmp_out')
-#      else:
-#         self._makeImages( )
-#         os.chdir('..')
-#         _createImagesDirectory( ) 
-#         os.system('mv tmp_out/*.png images')
-#         os.system('rm -r tmp_out')
-#
