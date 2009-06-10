d1662ef2c44a07f7708e4ef089cbfcf7e9ddd61d
Trevor Bača <trevorbaca@gmail.com>
2009-06-10 19:19:14 +0000

    Removed spurious 'action' keyword from listtools.increase_cyclic( ).

--- trunk/abjad/tools/listtools/increase_cyclic.py
+++ trunk/abjad/tools/listtools/increase_cyclic.py
@@ -1,21 +1,32 @@
-def increase_cyclic(l, s, shield = True, trim = True, action = 'in place'):
-   '''Cyclically increase elements of l by the elements of s;
-      map nonpositive values to 1 by default.
+def increase_cyclic(l, s, shield = True, trim = True):
+   '''Cyclically increase elements of *l* by the elements of *s*.
+   Map nonpositive values to ``1`` by default.
 
-      >>> l = range(10)
-      >>> increase_cyclic(l, [2, 0])
-      >>> l
+   ::
+
+      abjad> l = range(10)
+      abjad> listtools.increase_cyclic(l, [2, 0])
       [2, 1, 4, 3, 6, 5, 8, 7, 10, 9]
 
-      >>> l = range(10)
-      >>> increase_cyclic(l, [10, -10])
-      >>> l
+   ::
+
+      abjad> l = range(10)
+      abjad> listtools.increase_cyclic(l, [10, -10])
       [10, 1, 12, 1, 14, 1, 16, 1, 18, 1]
 
-      >>> l = range(10)
-      >>> increase_cyclic(l, [10, -10], shield = False)
-      >>> l
-      [10, -9, 12, -7, 14, -5, 16, -3, 18, -1]'''
+   ::
+
+      abjad> l = range(10)
+      abjad> listtools.increase_cyclic(l, [10, -10], shield = False)
+      [10, -9, 12, -7, 14, -5, 16, -3, 18, -1]
+
+   Raise :exc:`TypeError` when *l* is neither list nor tuple::
+
+      abjad> listtools.increase_cyclic('foo', [10, -10])
+      TypeError'''
+
+   if not isinstance(l, (list, tuple)):
+      raise TypeError
 
    result = [ ]
 
@@ -25,7 +36,4 @@ def increase_cyclic(l, s, shield = True, trim = True, action = 'in place'):
          new = 1
       result.append(new)
 
-   if action == 'in place':
-      l[:] = result
-   else:
-      return result
+   return result
