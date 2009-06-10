7d1f5fc862b32304b7965b1243a915f518dd0a44
Trevor Bača <trevorbaca@gmail.com>
2009-06-10 11:50:43 +0000

    Removed duplicate and misnamed file from previous commit.

--- trunk/abjad/documentation/doc_sphinx/chapters/api/tools/mathtools/partition_integer_into_halves.py
+++
@@ -1,6 +0,0 @@
-abjad.tools.mathtools.integer_halve
-===================================
-
-.. automodule:: abjad.tools.mathtools.integer_halve
-
-.. autofunction:: abjad.tools.mathtools.integer_halve.integer_halve
