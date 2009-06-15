6c3e0766684076747b075ea329b441bd100f3cb1
Trevor Bača <trevorbaca@gmail.com>
2009-06-15 22:01:54 +0000

    Added _TextInterface usage example.

--- trunk/abjad/text/interface.py
+++ trunk/abjad/text/interface.py
@@ -4,8 +4,26 @@ from abjad.spanner.receptor import _SpannerReceptor
 
 
 class _TextInterface(_Interface, _GrobHandler, _SpannerReceptor):
-   '''Handle LilyPond TextScript grob.
-      Receive Abjad Text spanner.'''
+   r'''Handle LilyPond TextScript grob.
+   Receive Abjad Text spanner.
+
+   The `Abjad` :class:`~abjad.text.interface._TextInterface` handles
+   the `LilyPond` `TextScript` grob.
+
+   ::
+
+      abjad> t = Staff(construct.scale(4))
+      abjad> t[0].markup.up.append(r'\italic { Lento }')
+      abjad> t.text.staff_padding = 6
+      \new Staff \with {
+         \override TextScript #'staff-padding = #6
+      } {
+         c'8 ^ \markup { \italic { Lento } }
+         d'8
+         e'8
+         f'8
+      }
+   '''
 
    def __init__(self, client):
       '''Bind client and LilyPond TextScript grob.
