978340f79af1ea97917a7b14f71013214521ee6a
Trevor Bača <trevorbaca@gmail.com>
2009-06-19 12:54:51 +0000

    Removed misnamed file from previous commit.

--- trunk/abjad/documentation/doc_sphinx/chapters/api/tools/listtools/repeat_list_to_lenght.py
+++
@@ -1,6 +0,0 @@
-abjad.tools.listtools.repeat_to_length
-======================================
-
-.. automodule:: abjad.tools.listtools.repeat_to_length
-
-.. autofunction:: abjad.tools.listtools.repeat_to_length.repeat_to_length
