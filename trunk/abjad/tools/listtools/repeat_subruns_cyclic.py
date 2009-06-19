c6bbd58644acc432c10e37987ef88b35c426b7a5
Trevor Bača <trevorbaca@gmail.com>
2009-06-19 11:05:52 +0000

    Changed listtools.repeat_subruns_cyclic( ) docstring to use the Sphinx default  role variable names. Previous implementations used nonsemantic emphasis * * to mark up variable names.

--- trunk/abjad/tools/listtools/repeat_subruns_cyclic.py
+++ trunk/abjad/tools/listtools/repeat_subruns_cyclic.py
@@ -1,15 +1,13 @@
 from abjad.component.component import _Component
 
 
-def repeat_subruns_cyclic(l, subrun_indicators):
-   '''Repeat subruns in *l* according to *subrun_indicators*.
-   The *subrun_indicators* input parameter must be a list of 
-   zero or more ``(start, length, reps)`` triples.
+def repeat_subruns_cyclic(l, indicators):
+   '''Repeat subruns in `l` according to `indicators`.
+   The `indicators` input parameter must be a list of 
+   zero or more ``(start, length, repetitions)`` triples.
    The function copies ``l[start:start+length]`` and inserts 
-   ``reps`` times for each ``(start, length, reps)`` indicator 
-   in *subrun_indicators*.
-
-   Examples:
+   ``repetitions`` times for each ``(start, length, repetitions)`` indicator 
+   in `indicators`.
 
    To insert ``10`` repetitions of ``l[:2]`` at ``l[2:2]``::
    
@@ -33,29 +31,31 @@ def repeat_subruns_cyclic(l, subrun_indicators):
 
       abjad> listtools.repeat_subruns_cyclic(l, [(18, 8, 2)])
       [0, 1, 2, 3, 4, 5, 18, 19, 0, 1, 2, 3, 4, 5, 18, 19, 0, 1, 2, 3, 4, 
-      5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]'''
+      5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+
+   .. todo:: Reimplement this function to return a generator.'''
 
    assert isinstance(l, list)
    assert all([not isinstance(x, _Component) for x in l])
-   assert isinstance(subrun_indicators, list)
-   assert all([len(x) == 3 for x in subrun_indicators])
+   assert isinstance(indicators, list)
+   assert all([len(x) == 3 for x in indicators])
 
    len_l = len(l)
    instructions = [ ]
 
-   for start, length, reps in subrun_indicators:
+   for start, length, repetitions in indicators:
       new_slice = [ ]
       stop = start + length
       for i in range(start, stop):
          new_slice.append(l[i % len_l])
       index = stop % len_l
-      instruction = (index, new_slice, reps)
+      instruction = (index, new_slice, repetitions)
       instructions.append(instruction)
 
    result = l[:]
 
-   for index, new_slice, reps in reversed(sorted(instructions)):
-      for i in range(reps):
+   for index, new_slice, repetitions in reversed(sorted(instructions)):
+      for i in range(repetitions):
          result[index:index] = new_slice
          
    return result
