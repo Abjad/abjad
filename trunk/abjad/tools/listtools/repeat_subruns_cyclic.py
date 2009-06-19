9f8cd26de3c1964f49b6a0cd5d89211d7831c9a4
Trevor Bača <trevorbaca@gmail.com>
2009-06-19 11:29:35 +0000

    Extended listtools.repeat_subruns_cyclic( ) docstring.

--- trunk/abjad/tools/listtools/repeat_subruns_cyclic.py
+++ trunk/abjad/tools/listtools/repeat_subruns_cyclic.py
@@ -4,36 +4,56 @@ from abjad.component.component import _Component
 def repeat_subruns_cyclic(l, indicators):
    '''Repeat subruns in `l` according to `indicators`.
    The `indicators` input parameter must be a list of 
-   zero or more ``(start, length, repetitions)`` triples.
-   The function copies ``l[start:start+length]`` and inserts 
-   ``repetitions`` times for each ``(start, length, repetitions)`` indicator 
-   in `indicators`.
+   zero or more ``(start, length, count)`` triples.
+   For every ``(start, length, count)`` indicator in `indicators`,
+   the function copies ``l[start:start+length]`` and inserts 
+   ``count`` new copies of ``l[start:start+length]`` immediately
+   after ``l[start:start+lenght]`` in `l`.
 
-   To insert ``10`` repetitions of ``l[:2]`` at ``l[2:2]``::
+   .. note:: The function reads the value of ``count`` in every \
+      ``(start, length, count)`` triple not as the total number \
+      of occurrences of ``l[start:start+length]`` to appear in `l` \
+      after execution, but rather as the number of new occurrences \
+      of ``l[start:start+length]`` to appear in `l` after execution.
+
+   To insert ``10`` count of ``l[:2]`` at ``l[2:2]``::
    
       abjad> listtools.repeat_subruns_cyclic(range(20), [(0, 2, 10)])
       [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 
       2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
 
-   To insert ``5`` repetitions of ``l[10:12]`` at ``l[12:12]`` and then
-   insert ``5`` repetitions of ``l[:2]`` at ``l[2:2]``::
+   To insert ``5`` count of ``l[10:12]`` at ``l[12:12]`` and then
+   insert ``5`` count of ``l[:2]`` at ``l[2:2]``::
 
       abjad> listtools.repeat_subruns_cyclic(l, [(0, 2, 5), (10, 2, 5)])
       [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 
       10, 11, 10, 11, 10, 11, 10, 11, 10, 11, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+
+   .. note:: This function wraps around the end of `l` whenever \
+      ``len(l) < start + length``.
       
-   To insert ``2`` repetitions of ``[18, 19, 0, 1]`` at ``l[2:2]``::
+   To insert ``2`` count of ``[18, 19, 0, 1]`` at ``l[2:2]``::
 
       abjad> listtools.repeat_subruns_cyclic(l, [(18, 4, 2)])
       [0, 1, 18, 19, 0, 1, 18, 19, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
 
-   To insert ``2`` repetitions of ``[18, 19, 0, 1, 2, 3, 4]`` at ``l[4:4]``::
+   To insert ``2`` count of ``[18, 19, 0, 1, 2, 3, 4]`` at ``l[4:4]``::
 
       abjad> listtools.repeat_subruns_cyclic(l, [(18, 8, 2)])
       [0, 1, 2, 3, 4, 5, 18, 19, 0, 1, 2, 3, 4, 5, 18, 19, 0, 1, 2, 3, 4, 
       5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
 
-   .. todo:: Reimplement this function to return a generator.'''
+   .. todo:: Implement an optional `wrap` keyword to specify whether \
+      this function should wrap around the ened of `l` whenever \
+      ``len(l) < start + length`` or not.
+
+   .. todo:: Reimplement this function to return a generator.
+
+   Generalizations of this function would include functions to repeat subruns
+   in `l` to not only a certain count, as implemented here, but to a certain
+   sum or weight. That is, ``listtools.repeat_subruns_to_sum( )`` and
+   ``listtools.repeat_subruns_to_weight( )``.
+   '''
 
    assert isinstance(l, list)
    assert all([not isinstance(x, _Component) for x in l])
@@ -43,19 +63,19 @@ def repeat_subruns_cyclic(l, indicators):
    len_l = len(l)
    instructions = [ ]
 
-   for start, length, repetitions in indicators:
+   for start, length, count in indicators:
       new_slice = [ ]
       stop = start + length
       for i in range(start, stop):
          new_slice.append(l[i % len_l])
       index = stop % len_l
-      instruction = (index, new_slice, repetitions)
+      instruction = (index, new_slice, count)
       instructions.append(instruction)
 
    result = l[:]
 
-   for index, new_slice, repetitions in reversed(sorted(instructions)):
-      for i in range(repetitions):
+   for index, new_slice, count in reversed(sorted(instructions)):
+      for i in range(count):
          result[index:index] = new_slice
          
    return result
