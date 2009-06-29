f1d6c16ca38607344ec719f08fcaf6ab7658bbc4
Trevor Bača <trevorbaca@gmail.com>
2009-06-29 12:19:10 +0000

    Three changes to scm/abjad.scm:
    1. Old X-style dynamics, like ppX and ffX, removed. These old dynamic settings existed to center dynamics vertically with each other, for which LilyPond now provides global settings.
    
    2. Old shape notehead-based functions like \triangle and \semicircle removed. Recent changes to NoteHead remove the previously managed 'style' attribute. LilyPond now recognizes \once \override NoteHead #'style = #'do. So use that (which is pure LilyPond code) instead of \triangle (which was only an Abjad alias in scm/bjad.scm).
    
    3. Michael Lauer's \centerText function added. Taken from posts to the LilyPond user list in May.

--- trunk/abjad/scm/abjad.scm
+++ trunk/abjad/scm/abjad.scm
@@ -1,89 +1,3 @@
-ppppX = #(make-dynamic-script (markup #:combine 
-   #:transparent #:dynamic "f" 
-   #:line(#:hspace 0 #:dynamic "pppp" #:hspace 0)))
-pppX = #(make-dynamic-script (markup #:combine 
-   #:transparent #:dynamic "f" 
-   #:line(#:hspace 0 #:dynamic "ppp" #:hspace 0)))
-ppX = #(make-dynamic-script (markup #:combine 
-   #:transparent #:dynamic "f" 
-   #:line(#:hspace 0 #:dynamic "pp" #:hspace 0)))
-pX = #(make-dynamic-script (markup #:combine 
-   #:transparent #:dynamic "f" 
-   #:line(#:hspace 0 #:dynamic "p" #:hspace 0)))
-mpX = #(make-dynamic-script (markup #:combine 
-   #:transparent #:dynamic "f" 
-   #:line(#:hspace 0 #:dynamic "mp" #:hspace 0)))
-
-mX = #(make-dynamic-script (markup #:combine 
-   #:transparent #:dynamic "f" 
-   #:line(#:hspace 0 #:dynamic "m" #:hspace 0)))
-
-mfX = #(make-dynamic-script (markup #:combine 
-   #:transparent #:dynamic "f" 
-   #:line(#:hspace 0 #:dynamic "mf" #:hspace 0)))
-fX = #(make-dynamic-script (markup #:combine 
-   #:transparent #:dynamic "f" 
-   #:line(#:hspace 0 #:dynamic "f" #:hspace 0)))
-ffX = #(make-dynamic-script (markup #:combine 
-   #:transparent #:dynamic "f" 
-   #:line(#:hspace 0 #:dynamic "ff" #:hspace 0)))
-fffX = #(make-dynamic-script (markup #:combine 
-   #:transparent #:dynamic "f" 
-   #:line(#:hspace 0 #:dynamic "fff" #:hspace 0)))
-ffffX = #(make-dynamic-script (markup #:combine 
-   #:transparent #:dynamic "f" 
-   #:line(#:hspace 0 #:dynamic "ffff" #:hspace 0)))
-
-sfX = #(make-dynamic-script (markup #:combine 
-   #:transparent #:dynamic "f" 
-   #:line(#:hspace 0 #:dynamic "sf" #:hspace 0)))
-sffX = #(make-dynamic-script (markup #:combine 
-   #:transparent #:dynamic "f" 
-   #:line(#:hspace 0 #:dynamic "sff" #:hspace 0)))
-sfffX = #(make-dynamic-script (markup #:combine 
-   #:transparent #:dynamic "f" 
-   #:line(#:hspace 0 #:dynamic "sfff" #:hspace 0)))
-
-sfzX = #(make-dynamic-script (markup #:combine 
-   #:transparent #:dynamic "f" 
-   #:line(#:hspace 0 #:dynamic "sfz" #:hspace 0)))
-sffzX = #(make-dynamic-script (markup #:combine 
-   #:transparent #:dynamic "f" 
-   #:line(#:hspace 0 #:dynamic "sffz" #:hspace 0)))
-sfffzX = #(make-dynamic-script (markup #:combine 
-   #:transparent #:dynamic "f" 
-   #:line(#:hspace 0 #:dynamic "sfffz" #:hspace 0)))
-
-sfpX = #(make-dynamic-script (markup #:combine 
-   #:transparent #:dynamic "f" 
-   #:line(#:hspace 0 #:dynamic "sfp" #:hspace 0)))
-sfppX = #(make-dynamic-script (markup #:combine 
-   #:transparent #:dynamic "f" 
-   #:line(#:hspace 0 #:dynamic "sfpp" #:hspace 0)))
-sfpppX = #(make-dynamic-script (markup #:combine 
-   #:transparent #:dynamic "f" 
-   #:line(#:hspace 0 #:dynamic "sfppp" #:hspace 0)))
-
-sffpX = #(make-dynamic-script (markup #:combine 
-   #:transparent #:dynamic "f" 
-   #:line(#:hspace 0 #:dynamic "sffp" #:hspace 0)))
-sffppX = #(make-dynamic-script (markup #:combine 
-   #:transparent #:dynamic "f" 
-   #:line(#:hspace 0 #:dynamic "sffpp" #:hspace 0)))
-sffpppX = #(make-dynamic-script (markup #:combine 
-   #:transparent #:dynamic "f" 
-   #:line(#:hspace 0 #:dynamic "sffppp" #:hspace 0)))
-
-sfffpX = #(make-dynamic-script (markup #:combine 
-   #:transparent #:dynamic "f" 
-   #:line(#:hspace 0 #:dynamic "sfffp" #:hspace 0)))
-sfffppX = #(make-dynamic-script (markup #:combine 
-   #:transparent #:dynamic "f" 
-   #:line(#:hspace 0 #:dynamic "sfffpp" #:hspace 0)))
-sfffpppX = #(make-dynamic-script (markup #:combine 
-   #:transparent #:dynamic "f" 
-   #:line(#:hspace 0 #:dynamic "sfffppp" #:hspace 0)))
-
 beam = #(define-music-function (parser location left right) (number? number?)
       #{
          \set stemLeftBeamCount = #$left
@@ -92,34 +6,6 @@ beam = #(define-music-function (parser location left right) (number? number?)
 
 fraction = #(define-music-function (parser location music) (ly:music?)
 	#{ \tweak #'text #tuplet-number::calc-fraction-text $music #})
-
-triangle = #(define-music-function (parser location music) (ly:music?)
-	#{ \once \set shapeNoteStyles = ##(do do do do do do do) $music #})
-
-semicircle = #(define-music-function (parser location music) (ly:music?)
-	#{ \once \set shapeNoteStyles = ##(re re re re re re re) $music #})
-
-blackdiamond = #(define-music-function (parser location music) (ly:music?)
-	#{ \once \override NoteHead #'style = #'harmonic-black
-      $music #})
-
-tiltedtriangle = #(define-music-function (parser location music) (ly:music?)
-	#{ \once \set shapeNoteStyles = ##(fa fa fa fa fa fa fa) $music #})
-
-square = #(define-music-function (parser location music) (ly:music?)
-	#{ \once \set shapeNoteStyles = ##(la la la la la la la) $music #})
-
-wedge = #(define-music-function (parser location music) (ly:music?)
-	#{ \once \set shapeNoteStyles = ##(ti ti ti ti ti ti ti) $music #})
-
-diamond = #(define-music-function (parser location music) (ly:music?)
-	#{ \once \override NoteHead #'style = #'harmonic $music #})
-
-harmonic = #(define-music-function (parser location music) (ly:music?)
-	#{ \once \override NoteHead #'style = #'harmonic $music #})
-
-cross = #(define-music-function (parser location music) (ly:music?)
-	#{ \once \set shapeNoteStyles = ##(cross cross cross cross cross cross cross) $music #})
 
 white = #(define-music-function (parser location music) (ly:music?)
 	#{ \once \override NoteHead #'duration-log = #1 $music #})
@@ -156,3 +42,18 @@ locationWhichContext = #(define-music-function (parser location) ()
                     (ly:input-message location
                      "current voice is ~a"
                      (ly:context-id c))))))
+
+#(define lastNoteHeadWidth 0)
+
+#(define (centerTextFn grob grob-origin context)    
+   '''From Michael Lauer on LilyPond user list.'''
+   (cond 
+      ((grob::has-interface grob 'note-head-interface)                
+         (set! lastNoteHeadWidth (cdr (ly:grob-property grob 'X-extent))))
+      ((grob::has-interface grob 'text-script-interface)
+           (let* 
+               ((xext (ly:grob-property grob 'X-extent))
+               (offset (* (- lastNoteHeadWidth (car xext) (cdr xext)) 0.5)))
+            (ly:grob-set-property! grob 'X-offset offset)))))
+
+centerText = \applyOutput #'Voice #centerTextFn
