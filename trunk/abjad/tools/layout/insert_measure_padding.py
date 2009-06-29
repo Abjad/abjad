2f9fbb3adadbd815213d711a95bee54cbd3a29ff
Trevor Bača <trevorbaca@gmail.com>
2009-06-29 22:22:43 +0000

    layout.insert_measure_padding( ) extended to work with measures containing stacked voices. layout.insert_measure_padding( ) also extended with optional 'splice' keyword. See the API entry for details.

--- trunk/abjad/tools/layout/insert_measure_padding.py
+++ trunk/abjad/tools/layout/insert_measure_padding.py
@@ -1,3 +1,4 @@
+from abjad.leaf.leaf import _Leaf
 from abjad.measure.measure import _Measure
 from abjad.rational import Rational
 from abjad.rest import Rest
@@ -5,8 +6,10 @@ from abjad.tools import iterate
 import types
 
 
-def insert_measure_padding(expr, front, back):
-   r'''Iterate all measures in `expr`. Insert transparent rest equal
+def insert_measure_padding(expr, front, back, splice = False):
+   r'''.. versionadded:: 1.1.1
+
+   Iterate all measures in `expr`. Insert transparent rest equal
    to `front` at beginning of each measure. Insert transparent 
    rest equal to `back` at end of each measure. 
 
@@ -45,6 +48,65 @@ def insert_measure_padding(expr, front, back):
                       \revert Staff.TimeSignature #'stencil
       }
 
+   Works when measures contain stacked voices. ::
+
+      abjad> measure = DynamicMeasure(Voice(construct.run(2)) * 2)
+      abjad> measure.parallel = True
+      abjad> t = Staff(measure * 2)
+      abjad> pitchtools.diatonicize(t)
+      abjad> layout.insert_measure_padding(t, Rational(1, 32), Rational(1, 64))
+
+   ::
+
+      abjad> print t.format
+      \new Staff {
+            \time 19/64
+            \new Voice {
+               r32
+               c'8
+               d'8
+               r64
+            }
+            \new Voice {
+               r32
+               e'8
+               f'8
+               r64
+            }
+            \time 19/64
+            \new Voice {
+               r32
+               g'8
+               a'8
+               r64
+            }
+            \new Voice {
+               r32
+               b'8
+               c''8
+               r64
+            }
+      }
+
+   Set the optional `splice` keyword to ``True`` to extend edge
+   spanners over newly inserted rests. ::
+
+      abjad> t = DynamicMeasure(construct.scale(2))
+      abjad> Beam(t[:])
+      abjad> t.formatter.number.self = 'comment'
+      abjad> layout.insert_measure_padding(t, Rational(1, 32), Rational(1, 64), splice = True)
+
+   ::
+
+      abjad> print t.format
+      % start measure 1
+         \time 19/64
+         r32 [
+         c'8
+         d'8
+         r64 ]
+      % stop measure 1
+
    Raise :exc:`ValueError` when `front` is neither a positive
    rational nor ``None``.
    Raise :exc:`ValueError` when `back` is neither a positive
@@ -61,10 +123,29 @@ def insert_measure_padding(expr, front, back):
    if not isinstance(back, (Rational, types.NoneType)):
       raise ValueError
 
+   root = expr[0].parentage.root
+
+   ## forbid updates because _Component.splice_left( ) and ##
+   ## _Component.splice( ) call self.offset.prolated.stop  ##
+   root._update._forbidUpdate( )
+
    for measure in iterate.naive(expr, _Measure):
       if front is not None:
-         front_rest = Rest(front)
-         measure.insert(0, front_rest)
+         start_components = measure._navigator._contemporaneousStartContents
+         start_leaves = [x for x in start_components if isinstance(x, _Leaf)]
+         for start_leaf in start_leaves:
+            if splice:
+               start_leaf.splice_left([Rest(front)])
+            else:
+               start_leaf.extend_left_in_parent([Rest(front)])
       if back is not None:
-         back_rest = Rest(back)
-         measure.append(back_rest)  
+         stop_components = measure._navigator._contemporaneousStopContents
+         stop_leaves = [x for x in stop_components if isinstance(x, _Leaf)]
+         for stop_leaf in stop_leaves:
+            if splice:
+               stop_leaf.splice([Rest(back)])
+            else:
+               stop_leaf.extend_in_parent([Rest(back)])
+
+   ## allow updates after all calls to splice( ) are done. ##
+   root._update._allowUpdate( )
