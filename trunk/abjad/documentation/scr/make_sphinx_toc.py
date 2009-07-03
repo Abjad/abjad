83d5ad9dcdf3937eb5a63105c6641dd330a4e81c
Trevor Bača <trevorbaca@gmail.com>
2009-07-03 13:40:31 +0000

    Helpers for eventual restructuring of API in three sections: classes, interfaces, tool.

--- trunk/abjad/documentation/scr/make_sphinx_toc.py
+++ trunk/abjad/documentation/scr/make_sphinx_toc.py
@@ -1,5 +1,5 @@
-
 def make_sphinx_toc(content):
+#def make_sphinx_toc(klasses, interfaces, tools):
    result = 'Abjad API\n'
    result += '=' * (len(result) - 1)
    result += '\n\n'
@@ -8,4 +8,29 @@ def make_sphinx_toc(content):
    for line in content:
       result += '   %s' % line
       result += '\n'
+
+#   result += 'Classes'
+#   result += '\n\n'
+#   result += '.. toctree::'
+#   result += '   :maxdepth: 1'
+#   result += '\n'
+#   for line in klasses:
+#      result += '   %s\n' % line
+#  
+#   result += 'Interfaces'
+#   result += '\n\n'
+#   result += '.. toctree::'
+#   result += '   :maxdepth: 1'
+#   result += '\n'
+#   for line in klasses:
+#      result += '   %s\n' % line
+#  
+#   result += 'Tools'
+#   result += '\n\n'
+#   result += '.. toctree::'
+#   result += '   :maxdepth: 1'
+#   result += '\n'
+#   for line in klasses:
+#      result += '   %s\n' % line
+
    return result
