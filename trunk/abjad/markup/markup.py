b3275e826728df61cb9d48d5b1a360b1c1532f13
Trevor Bača <trevorbaca@gmail.com>
2009-07-05 22:11:34 +0000

    Added usage examples to Markup docstrings.

--- trunk/abjad/markup/markup.py
+++ trunk/abjad/markup/markup.py
@@ -1,7 +1,24 @@
 from abjad.core.abjadcore import _Abjad
+import types
 
 
 class Markup(_Abjad):
+   r'''Abjad wrapper around LilyPond markup.
+
+   Class inserts ``\markup { }`` wrapper around contents at format time. ::
+
+      abjad> markup = Markup(r'\bold { "This is markup text." }')
+      abjad> print markup.format
+      \markup { \bold { "This is markup text." } }
+
+   ::
+
+      abjad> markup.contents = '"New markup contents."'
+      abjad> print markup.format
+      \markup { "New markup contents." }
+
+   Markup contents must be set by hand.
+   '''
 
    def __init__(self, contents):
       self.contents = contents
@@ -19,19 +36,53 @@ class Markup(_Abjad):
    ## PUBLIC ATTRIBUTES ##
 
    @apply
-   def style( ):
+   def contents( ):
       def fget(self):
-         return self._style
+         '''Read / write string equal to markup contents.
+         
+         ::
+   
+            abjad> markup = Markup('"This is markup text."')
+            abjad> markup.contents
+            '"This is markup text."'
+         '''
+         return self._contents
       def fset(self, arg):
-         assert arg in self._styles
-         self._style = arg
+         assert isinstance(arg, (str, types.NoneType))
+         self._contents = arg
       return property(**locals( ))
-      
+
    @property
    def format(self):
+      '''Read-only LilyPond string of `self`.
+
+      ::
+
+         abjad> markup = Markup('"This is markup text.'")
+         abjad> print markup.format
+         \markup { "This is markup text." }
+      '''
       if self.style == 'backslash':
          return r'\markup { %s }' % self.contents
       elif self.style == 'scheme':
          return '#%s' % self.contents
       else:
          raise ValueError('unknown markup style.')
+
+   @apply
+   def style( ):
+      def fget(self):
+         '''Read / write attribute set to either 
+         ``'backslash'`` or ``'scheme'``.
+
+         Default to 'backslash'. ::
+
+            abjad> markup = Markup('"This is markup text."')
+            abjad> markup.style
+            'backslash'
+         '''
+         return self._style
+      def fset(self, arg):
+         assert arg in self._styles
+         self._style = arg
+      return property(**locals( ))
