9ce4bfda4b1f7b03890a076e24e7794e8e0ae53c
Trevor Bača <trevorbaca@gmail.com>
2009-07-06 11:05:58 +0000

    Minor change to structure of markup.big_centered_page_number( ).

--- trunk/abjad/tools/markup/test/test_markup_big_centered_page_number.py
+++ trunk/abjad/tools/markup/test/test_markup_big_centered_page_number.py
@@ -8,9 +8,9 @@ def test_markup_big_centered_page_number_01( ):
    r'''
    \markup { 
       \fill-line {
-      \bold \fontsize #3
+      \bold \fontsize #3 \concat {
       \on-the-fly #print-page-number-check-first
-      \fromproperty #'page:page-number-string } }
+      \fromproperty #'page:page-number-string } } }
    '''
    
-   assert t.format == "\\markup { \n   \\fill-line {\n   \\bold \\fontsize #3\n   \\on-the-fly #print-page-number-check-first\n   \\fromproperty #'page:page-number-string } }"
+   assert t.format == "\\markup { \n   \\fill-line {\n   \\bold \\fontsize #3 \\concat {\n   \\on-the-fly #print-page-number-check-first\n   \\fromproperty #'page:page-number-string } } }"
