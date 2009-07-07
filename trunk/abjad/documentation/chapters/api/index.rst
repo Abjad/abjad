78968bae2ff2154bde8f757f9d58f2df2305c8a7
Trevor Bača <trevorbaca@gmail.com>
2009-07-07 15:01:45 +0000

    New iterate.thread_from(expr) function. See the API entry for details.

--- trunk/abjad/documentation/chapters/api/index.rst
+++ trunk/abjad/documentation/chapters/api/index.rst
@@ -285,6 +285,7 @@ Abjad API
    tools/iterate/measure_prev
    tools/iterate/naive
    tools/iterate/thread
+   tools/iterate/thread_from
    tools/iterate/tie_chains
    tools/label/clear_leaves
    tools/label/leaf_depth
---
+++ trunk/abjad/tools/iterate/thread_from.py
@@ -0,0 +1,93 @@
+from abjad.component.component import _Component
+from abjad.navigator.dfs import depth_first_search
+
+
+def thread_from(expr, klass = None):
+   r'''.. versionadded:: 1.1.1
+
+   Yield left-to-right components in the thread of `expr`
+   starting from `expr`.
+
+   When ``klass = None`` return all components in the thread of `expr`.
+
+   When `klass` is set to some other Abjad class,
+   yield only `klass` instances in the thread of `expr`. ::
+
+      abjad> container = Container(Voice(construct.run(2)) * 2)
+      abjad> container.parallel = True
+      abjad> container[0].name = 'voice 1'
+      abjad> container[1].name = 'voice 2'
+      abjad> staff = Staff(container * 2)
+      abjad> pitchtools.diatonicize(staff)
+      abjad> print staff.format
+      \new Staff {
+              <<
+                      \context Voice = "voice 1" {
+                              c'8
+                              d'8
+                      }
+                      \context Voice = "voice 2" {
+                              e'8
+                              f'8
+                      }
+              >>
+              <<
+                      \context Voice = "voice 1" {
+                              g'8
+                              a'8
+                      }
+                      \context Voice = "voice 2" {
+                              b'8
+                              c''8
+                      }
+              >>
+      }
+
+   Starting from the first leaf in score. ::
+
+      abjad> for x in iterate.thread_from(staff.leaves[0], Note):
+      ...     x
+      ... 
+      Note(c', 8)
+      Note(d', 8)
+      Note(g', 8)
+      Note(a', 8)
+
+   Starting from the second leaf in score. ::
+
+      abjad> for x in iterate.thread_from(staff.leaves[1], Note):
+      ...     x
+      ... 
+      Note(d', 8)
+      Note(g', 8)
+      Note(a', 8)
+
+   Yield all components in thread. ::
+
+      abjad> for x in iterate.thread_from(staff.leaves[0]):
+      ...     x
+      ... 
+      Note(c', 8)
+      Voice{2}
+      Note(d', 8)
+      Voice{2}
+      Note(g', 8)
+      Note(a', 8)
+   
+   Note that this function is a special type of depth-first search.
+
+   Compare with :func:`iterate.thread() <abjad.tools.iterate.thread>`.
+   '''
+
+   ## set default class
+   if klass is None:
+      klass = _Component
+
+   ## save thread signature of input expression
+   expr_thread_signature = expr.thread.signature
+
+   ## iterate expr depth-first allowing to crawl UP into score
+   for x in depth_first_search(expr, capped = False):
+      if isinstance(x, klass):
+         if x.thread.signature == expr_thread_signature:
+            yield x
