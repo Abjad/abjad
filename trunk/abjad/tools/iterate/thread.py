389b95a789ea08f7f130a80bb6521adec40a40e0
Trevor Bača <trevorbaca@gmail.com>
2009-07-07 14:27:29 +0000

    Added usage examples to iterate.naive( ) and iterate.tie_chains( ). Made iterate.naive( ) and iterate.thread( ) usage examples parallel. Preserved versionadded tag in iterate.thread( ).

--- trunk/abjad/tools/iterate/thread.py
+++ trunk/abjad/tools/iterate/thread.py
@@ -1,22 +1,54 @@
 def thread(expr, klass, thread_signature):
    r'''.. versionadded:: 1.1.1
 
-   Iterates all instances of class `klass` that have the given 
-   `thread_signature`.
+   Yield left-to-right instances of `klass` in `expr` with
+   `thread_signature`. ::
 
-   Returns a generator.
+      abjad> container = Container(Voice(construct.run(2)) * 2)
+      abjad> container.parallel = True
+      abjad> container[0].name = 'voice 1'
+      abjad> container[1].name = 'vocie 2'
+      abjad> staff = Staff(container * 2)
+      abjad> pitchtools.diatonicize(staff)
+      abjad> print staff.format
+      \new Staff {
+              <<
+                      \context Voice = "voice 1" {
+                              c'8
+                              d'8
+                      }
+                      \context Voice = "vocie 2" {
+                              e'8
+                              f'8
+                      }
+              >>
+              <<
+                      \context Voice = "voice 1" {
+                              g'8
+                              a'8
+                      }
+                      \context Voice = "vocie 2" {
+                              b'8
+                              c''8
+                      }
+              >>
+      }
 
-   Example::
-      
-      abjad> v1 = Voice(construct.scale(4))
-      abjad> v2 = Voice(construct.run(2))
-      abjad> v1.name = 'piccolo'
-      abjad> v2.name = 'piccolo'
-      abjad> s = Staff([v1, v2])
-      abjad> iterate.thread(s, Note, v1.thread.signature)
-      <generator object at 0x8366d8c>
-      abjad> list(_)
-      [Note(c', 8), Note(d', 8), Note(e', 8), Note(f', 8), Note(c', 8), Note(c', 8)]
+   ::
+
+      abjad> signature = staff.leaves[0].thread.signature
+      abjad> for x in iterate.thread(staff, Note, signature):
+      ...     x
+      ... 
+      Note(c', 8)
+      Note(d', 8)
+      Note(g', 8)
+      Note(a', 8)
+
+   The important thing to note is that the function yields only
+   those leaves that sit in the same thread.
+
+   Compare with :func:`iterate.naive() <abjad.tools.iterate.naive>`.
    '''
 
    if isinstance(expr, klass) and expr.thread.signature == thread_signature:
--- trunk/abjad/tools/iterate/tie_chains.py
+++ trunk/abjad/tools/iterate/tie_chains.py
@@ -3,7 +3,40 @@ from abjad.tools.iterate.naive import naive
 
 
 def tie_chains(expr):
-   '''Yield successive tie chains in expr.'''
+   r'''Yield left-to-right tie chains in `expr`.
+
+   ::
+
+      abjad> notes construct.notes([0], [(5, 16), (1, 8), (1, 8), (5, 16)])
+      abjad> staff = Staff(notes)
+      abjad> tuplet = FixedDurationTuplet((2, 16), staff[1:3])
+      abjad> pitchtools.diatonicize(staff)
+      abjad> print staff.format
+      \new Staff {
+              c'4 ~
+              \times 2/3 {
+                      c'16
+                      d'8
+              }
+              e'8
+              f'4 ~
+              f'16
+      }
+
+   ::
+
+      abjad> for x in iterate.tie_chains(t):
+      ...     x
+      ... 
+      (Note(c', 4), Note(c', 16))
+      (Note(d', 8),)
+      (Note(e', 8),)
+      (Note(f', 4), Note(f', 16))
+
+   Note that one-note tie chains yield the same as other tie chains.
+
+   Note also that nested structures are no problem.
+   '''
 
    for leaf in naive(expr, _Leaf):
       if not leaf.tie.spanned or leaf.tie.last:
