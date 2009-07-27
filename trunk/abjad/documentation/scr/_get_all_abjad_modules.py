28ad46262291e5f61c73f64dd06ef3e80f5fcac1
Trevor Bača <trevorbaca@gmail.com>
2009-07-27 19:09:37 +0000

    New _get_all_abjad_modules( ) helper to return a list of all modules except initializers and other modules beginning in double underscore.

---
+++ trunk/abjad/documentation/scr/_get_all_abjad_modules.py
@@ -0,0 +1,25 @@
+from abjad.cfg.cfg import ABJADPATH
+import os
+
+
+def _get_all_abjad_names( ):
+   '''Return a list of all Abjad modules.
+   Exclude initializers and other modules beginning in double underscore.
+   '''
+
+   modules = [ ]
+   for current_root, dirs, files in os.walk(ABJADPATH):
+      if '.svn' not in current_root:
+         if 'test' not in current_root:
+            for file in files:
+               if file.endswith('.py'):
+                  if not file.startswith('__'):
+                     module = os.path.join(current_root, file)
+                     modules.append(module)
+   return modules
+   
+
+if __name__ == '__main__':
+   modules = _get_all_abjad_names( )
+   for x in modules:
+      print x
