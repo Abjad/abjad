4b478b698d40c3f1e48d81f44b0ccedea150f4ed
Trevor Bača <trevorbaca@gmail.com>
2009-08-04 23:12:31 +0000

    Taught the SpannersDiscontiguous check about the experimental private Spanner._contiguity_constraint attribute.
    Most spanners demand that spanner contents be thread-contiguous.
    But it has been possible to turn off this constraint for some
    revisions now by setting Spanner._contiguity_constraint to any
    value other than the string 'thread'. The SpannersDiscontiguous check
    now knows about this.
    
    New tempo/test/test_tempo_spanner_contiguity_constraint.py test file
    to show all of this.

--- trunk/abjad/checks/spanners_discontiguous.py
+++ trunk/abjad/checks/spanners_discontiguous.py
@@ -3,13 +3,18 @@ from abjad.tools.check.assess_components import assess_components
 
 
 class SpannersDiscontiguous(_Check):
-   '''Spanner components must be thread-contiguous.'''
+   '''There are now two different types of spanner.
+   Most spanners demand that spanner components be thread-contiguous.
+   But a few special spanners (like Tempo) do not make such a demand.
+   The check here consults the experimental `_contiguity_constraint`.
+   '''
 
    def _run(self, expr):
       violators = [ ]
       total, bad = 0, 0
       for spanner in expr.spanners.contained:
-         if not assess_components(spanner[:], contiguity = 'thread'):
-            violators.append(spanner)
+         if spanner._contiguity_constraint == 'thread':
+            if not assess_components(spanner[:], contiguity = 'thread'):
+               violators.append(spanner)
          total += 1
       return violators, total
