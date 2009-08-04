f910462bd51872856ffb008bf6125758783e81e1
Trevor Bača <trevorbaca@gmail.com>
2009-08-04 22:00:58 +0000

    New:
       t.duration.is_underfull
       t.duration.is_overfull
    
    For any rigid measure t.

---
+++ trunk/abjad/measure/rigid/test/test_rigid_measure_duration_is_overfull.py
@@ -0,0 +1,13 @@
+from abjad import *
+
+
+def test_rigid_measure_duration_is_overfull_01( ):
+
+   t = RigidMeasure((3, 8), construct.run(3))
+   assert not t.duration.is_overfull
+
+   t.meter.forced = Meter(2, 8)
+   assert t.duration.is_overfull
+
+   t.meter.forced = Meter(3, 8)
+   assert not t.duration.is_overfull
