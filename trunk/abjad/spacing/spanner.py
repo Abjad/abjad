660ffe55f794676e0f27a80b2cc765e917e459ab
Trevor Bača <trevorbaca@gmail.com>
2009-10-03 14:59:35 +0000

    Spacing spanner API entry examples now complete.

--- trunk/abjad/spacing/spanner.py
+++ trunk/abjad/spacing/spanner.py
@@ -7,9 +7,49 @@ import types
 class SpacingSpanner(_GrobHandlerSpanner):
    r'''Model a spacing section of musical score.
 
-   Handle LilyPond ``SpacingSpanner`` grob.
+   Interface to LilyPond ``\newSpacingSection`` command.
+   
+   Interface to LilyPond ``proportionalNotationDuration`` context setting.
 
-   Handle LilyPond ``\newSpacingSection`` command.
+   Handle LilyPond ``SpacingSpanner`` grob. ::
+
+      abjad> staff = Staff(RigidMeasure((2, 8), construct.run(2)) * 2)
+      abjad> pitchtools.diatonicize(staff)
+      abjad> spacing_spanner = SpacingSpanner(staff[1:])
+      abjad> spacing_spanner.new_section = True
+      abjad> spacing_spanner.proportional_notation_duration = Rational(1, 30)
+      abjad> spacing_spanner.strict_grace_spacing = True
+      abjad> spacing_spanner.strict_note_spacing = True
+      abjad> spacing_spanner.uniform_stretching = True
+      abjad> overridetools.promote(spacing_spanner, 'strict_grace_spacing', 'Score')
+      abjad> overridetools.promote(spacing_spanner, 'strict_note_spacing', 'Score')
+      abjad> overridetools.promote(spacing_spanner, 'uniform_stretching', 'Score')
+
+   ::
+
+      abjad> print staff.format
+      \new Staff {
+              {
+                      \time 2/8
+                      c'8
+                      d'8
+              }
+              {
+                      \time 2/8
+                      \newSpacingSection
+                      \set Score.proportionalNotationDuration = #(ly:make-moment 1 30)
+                      \override Score.SpacingSpanner #'strict-grace-spacing = ##t
+                      \override Score.SpacingSpanner #'strict-note-spacing = ##t
+                      \override Score.SpacingSpanner #'uniform-stretching = ##t
+                      e'8
+                      f'8
+                      \revert Score.SpacingSpanner #'strict-note-spacing
+                      \revert Score.SpacingSpanner #'strict-grace-spacing
+                      \revert Score.SpacingSpanner #'uniform-stretching
+                      %%% spacing section ends here %%%
+              }
+      }
+
    '''
 
    def __init__(self, music = None):
@@ -33,7 +73,20 @@ class SpacingSpanner(_GrobHandlerSpanner):
          r'''Read / write interface to LilyPond 
          ``\newSpacingSection`` command.
 
-         Set to ``True``, ``False`` or ``None``.
+         Set to ``True``, ``False`` or ``None``. ::
+
+            abjad> staff = Staff(construct.scale(4))
+            abjad> spacing_spanner = SpacingSpanner(staff[:])
+            abjad> spacing_spanner.new_section = True
+            abjad> print staff.format
+            \new Staff {
+                    \newSpacingSection
+                    c'8
+                    d'8
+                    e'8
+                    f'8
+                    %%% spacing section ends here %%%
+            }
          '''
          return self._new_section
       def fset(self, expr):
@@ -47,7 +100,26 @@ class SpacingSpanner(_GrobHandlerSpanner):
          r'''Read / write interface to LilyPond
          ``proportionalNotationDuration`` context property.
 
-         Set to a rational value or ``None``.
+         Set to a rational value or ``None``. ::
+
+            abjad> staff = Staff(construct.scale(4))
+            abjad> spacing_spanner = SpacingSpanner(staff[2:])
+            abjad> spacing_spanner.new_section = True
+            abjad> spacing_spanner.proportional_notation_duration = Rational(1, 30)
+            abjad> print t.format
+            \new Staff {
+                    c'8
+                    d'8
+                    \newSpacingSection
+                    \set Score.proportionalNotationDuration = #(ly:make-moment 1 30)
+                    e'8
+                    f'8
+                    %%% spacing section ends here %%%
+            }
+
+         .. note:: For ``propportional_notation_duration`` settings to have 
+            any effect, you must also create a new spacing section with 
+            ``new_section = True``.
          '''
          return self._proportional_notation_duration
       def fset(self, expr):
