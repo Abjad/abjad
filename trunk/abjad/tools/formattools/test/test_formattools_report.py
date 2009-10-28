9ae7335788d377335ca7d97cb26f7f1d9593539f
Trevor Bača <trevorbaca@gmail.com>
2009-10-28 16:56:35 +0000

    Removed unused test file.

--- trunk/abjad/tools/formattools/test/test_formattools_report.py
+++ trunk/abjad/tools/formattools/test/test_formattools_report.py
@@ -25,3 +25,50 @@ def test_formattools_report_02( ):
 
    result = formattools.report(spanner, output = 'str')
    assert result == "e'8\tbefore: []\n\t after: []\n\t  left: []\n\t right: ['[']\n\nf'8\tbefore: []\n\t after: []\n\t  left: []\n\t right: [']']\n"
+
+
+def test_formattools_report_03( ):
+   '''You can report on tuplets.'''
+
+   t = FixedDurationTuplet((2, 8), construct.scale(3))
+   t.accidental.style = 'forget'
+   t.barline.kind = '|.'
+   t.clef.forced = Clef('treble')
+
+   r'''
+   \times 2/3 {
+           #(set-accidental-style 'forget)
+           \clef "treble"
+           c'8
+           d'8
+           e'8
+           \bar "|."
+   }
+   '''
+
+   result = formattools.report(t, output = 'string')
+
+   r'''
+   slot_1
+   slot_2
+      BracketsInterface.open
+         \times 2/3 {
+   slot_3
+      InterfaceAggregator.opening
+            #(set-accidental-style 'forget)
+            \clef "treble"
+   slot_4
+      _TupletFormatter._contents
+            c'8
+            d'8
+            e'8
+   slot_5
+      InterfaceAggregator.closing
+            \bar "|."
+   slot_6
+      BracketsInterface.close
+         }
+   slot_7
+   '''
+
+   assert result == 'slot_1\nslot_2\n\tBracketsInterface.open\n\t\t\\times 2/3 {\nslot_3\n\tInterfaceAggregator.opening\n\t\t\t#(set-accidental-style \'forget)\n\t\t\t\\clef "treble"\nslot_4\n\t_TupletFormatter._contents\n\t\t\tc\'8\n\t\t\td\'8\n\t\t\te\'8\nslot_5\n\tInterfaceAggregator.closing\n\t\t\t\\bar "|."\nslot_6\n\tBracketsInterface.close\n\t\t}\nslot_7\n'
