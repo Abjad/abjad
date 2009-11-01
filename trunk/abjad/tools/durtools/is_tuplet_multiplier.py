ee1bf6b4040519ea6b27d072be69da9335654a05
Trevor Bača <trevorbaca@gmail.com>
2009-11-01 22:45:59 +0000

    Added durtools.in_terms_of( ) and durtools.is_tuplet_multiplier( ) usage examples.

--- trunk/abjad/tools/durtools/is_tuplet_multiplier.py
+++ trunk/abjad/tools/durtools/is_tuplet_multiplier.py
@@ -2,8 +2,36 @@ from abjad.rational import Rational
 
 
 def is_tuplet_multiplier(multiplier):
-   '''True when multiplier is an Abjad rational
-      strictly greater than 1/2 and strictly less than 2.'''
+   '''True when `multiplier` is a rational
+   strictly greater than 1/2 and strictly less than 2. ::
+
+      abjad> for n in range(17):
+      ...     rational = Rational(n, 8)
+      ...     multiplier = durtools.is_tuplet_multiplier(rational)
+      ...     print '%s\t%s' % (rational, multiplier)
+      ... 
+      0       False
+      1/8     False
+      1/4     False
+      3/8     False
+      1/2     False
+      5/8     True
+      3/4     True
+      7/8     True
+      1       True
+      9/8     True
+      5/4     True
+      11/8    True
+      3/2     True
+      13/8    True
+      7/4     True
+      15/8    True
+      2       False
+
+   This function models the idea that ``4:3``, ``4:5``, ``4:6``,
+   ``4:7`` are valid tuplet multipliers while ``4:2`` and ``4:8``
+   aren't.
+   '''
 
    if not isinstance(multiplier, Rational):
       raise TypeError('multiplier must be rational.')
