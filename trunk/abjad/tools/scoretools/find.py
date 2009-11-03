32ab2ad0f5a43d129a94313c9d01134291ab87e2
Trevor Bača <trevorbaca@gmail.com>
2009-11-03 20:59:43 +0000

    Docs: added scoretools.find( ) usage example.

--- trunk/abjad/tools/scoretools/find.py
+++ trunk/abjad/tools/scoretools/find.py
@@ -3,17 +3,42 @@ from abjad.tools import iterate
 
 
 def find(expr, name = None, klass = None, context = None):
-   '''Iterate expr. 
-      Find and return a Python list of all components in expr, such that:
+   '''Return a Python list of all components in `expr`, such that:
 
-         * component.name == name
-         * isinstance(component, klass)
-         * component.context == context
+   * ``component.name == name``
+   * ``isinstance(component, klass)``
+   * ``component.context == context``
 
-      Do not run tests where keyword is None.
+   Do not run tests where keyword is set to none.
 
-      For shallow traversal of container for numeric indices,
-      use Container.__getitem__(i) instead.'''
+   Example score. ::
+
+      abjad> flute_staff = Staff(construct.scale(4))
+      abjad> flute_staff.name = 'Flute'
+      abjad> violin_staff = Staff(construct.scale(4))
+      abjad> violin_staff.name = 'Violin'
+      abjad> staff_group = StaffGroup([flute_staff, violin_staff])
+      abjad> score = Score([staff_group])
+
+   Find components by name. ::
+
+      abjad> scoretools.find(score, name = 'Violin')
+      [Staff{4}]
+
+   Find components by class. ::
+
+      abjad> scoretools.find(score, klass = Staff)
+      [Staff{4}, Staff{4}]
+
+   Find components by context. ::
+
+      abjad> violin_staff.context = 'ViolinContext'
+      abjad> scoretools.find(score, context = 'ViolinContext')
+      [ViolinContext{4}]
+
+   .. note:: For shallow traversal of container for numeric indices,
+      use ``Container[i]`` instead.
+   '''
 
    result = [ ]
 
