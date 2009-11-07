8595fdc3ddcc1c5e76854e4bd27de13612c6f63f
Víctor Adán <vctradn@gmail.com>
2009-11-07 18:00:16 +0000

    Fixed attribute checking bug in tools.interpolate.divide( ) and added examples to documentation string. The function now returns floats instead of Rationals.

--- trunk/abjad/tools/interpolate/divide.py
+++ trunk/abjad/tools/interpolate/divide.py
@@ -4,35 +4,49 @@ from abjad.tools.interpolate.exponential import exponential
 
 
 def divide(total, start_frac, stop_frac, exp='cosine'):
-   '''Divide total into segments of sizes computed from interpolating 
-      between start_frac and stop_frac. 
-      Set exp='cosine' for cosine interpolation. This is the default. If set
-      to a numeric value, the interpolation is exponential and exp is the 
-      exponent.'''
+   '''Divide `total` into segments of sizes computed from interpolating 
+      between `start_frac` and `stop_frac`. 
+      Set ``exp='cosine'`` for cosine interpolation. This is the default. 
+      If set to a numeric value, the interpolation is exponential and 
+      `exp` is the exponent.
+      The resulting segments are scaled so that their sum equals `total`
+      exactly.
 
-   if stop_frac >= total or start_frac >= total:
-      raise ValueError('Both dividing fractions must be smaller than total.')
+      The function returns a list of floats.
+
+      Examples::
+
+         abjad> interpolate.divide(10, 1, 1, exp=1)
+         [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
+         abjad> sum(_)
+         10.0
+
+         abjad> interpolate.divide(10, 5, 1)
+         [4.7986734489043181, 2.8792040693425909, 1.3263207210948171, 
+         0.99580176065827419]
+         abjad> sum(_)
+         10.0 '''
+
+   if total <=0 :
+      raise ValueError("'total' must be > 0.")
+   if start_frac <= 0 or stop_frac <= 0:
+      raise ValueError("Both 'start_frac' and 'stop_frac' must be > 0.")
+   if (stop_frac + start_frac) > total:
+      raise ValueError("'start_frac' + 'stop_frac' must be < 'total'.")
 
    result = [ ]
    total = float(total)
-   while sum(result) < total:
+   partial_sum = 0
+   while partial_sum < total:
       if exp == 'cosine':
-         ip = cosine(start_frac, stop_frac, sum(result) / total)
+         ip = cosine(start_frac, stop_frac, partial_sum / total)
       else:
-         ip = exponential(start_frac, stop_frac, sum(result) / total, exp)
+         ip = exponential(start_frac, stop_frac, partial_sum / total, exp)
       result.append(ip)
-   result = [x * total / sum(result) for x in result]
-   result = [Rational(int(round(x * 10000, 5)), 10000) for x in result]
-   return result
+      partial_sum += ip
 
-#   while sum(result) <= (total - stop_frac):
-#      if exp == 'cosine':
-#         ip = cosine(start_frac, stop_frac, sum(result) / total)
-#      else:
-#         ip = exponential(start_frac, stop_frac, sum(result) / total, exp)
-#      ip = int(round(ip * 10000, 5))
-#      ip = Rational(ip, 10000)
-#      result.append(ip)
-#   result[-1] += total - sum(result)
-#   return result
+   ## scale result to fit total exaclty
+   result = [x * total / sum(result) for x in result]
+   #result = [Rational(int(round(x * 10000, 5)), 10000) for x in result]
+   return result
 
