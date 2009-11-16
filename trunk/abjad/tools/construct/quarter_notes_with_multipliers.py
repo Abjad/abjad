dc7f3f30b539e9577adc0859ef7b1cafb6ca1696
Trevor Bača <trevorbaca@gmail.com>
2009-11-16 03:22:52 +0000

    Fix: construct.quarter_notes_with_multipliers(pitches, durations) now returns multipliers correctly.

--- trunk/abjad/tools/construct/quarter_notes_with_multipliers.py
+++ trunk/abjad/tools/construct/quarter_notes_with_multipliers.py
@@ -4,34 +4,36 @@ from abjad.tools import durtools
 from abjad.tools import listtools
 
 
-def quarter_notes_with_multipliers(pitches, multipliers):
+def quarter_notes_with_multipliers(pitches, durations):
    r'''.. versionadded:: 1.1.2
 
-   Construct list of quarter notes with `multipliers`. ::
+   Construct list of `quarter_notes` with LilyPond multipliers
+   such that ``quarter_notes[i].duration.multiplied`` equals
+   ``durations[i]`` for ``i <= len(quarter_notes)``. ::
 
       abjad> construct.quarter_notes_with_multipliers([0, 2, 4, 5], [(1, 4), (1, 5), (1, 6), (1, 7)])
-      [Note(c', 4 * 1), Note(d', 4 * 5/4), Note(e', 4 * 3/2), Note(f', 4 * 7/4)]
+      [Note(c', 4 * 1), Note(d', 4 * 4/5), Note(e', 4 * 2/3), Note(f', 4 * 4/7)]
 
    Read `pitches` cyclically where the length of `pitches` is 
    less than the length of `multipliers`. ::
 
       abjad> construct.quarter_notes_with_multipliers([0], [(1, 4), (1, 5), (1, 6), (1, 7)])
-      [Note(c', 4 * 1), Note(c', 4 * 5/4), Note(c', 4 * 3/2), Note(c', 4 * 7/4)]
+      [Note(c', 4 * 1), Note(c', 4 * 4/5), Note(c', 4 * 2/3), Note(c', 4 * 4/7)]
 
    Read `multipliers` cyclically where the length of `multipliers` is
    less than the length of `pitches`. ::
 
       abjad> construct.quarter_notes_with_multipliers([0, 2, 4, 5], [(1, 5)])
-      [Note(c', 4 * 5/4), Note(d', 4 * 5/4), Note(e', 4 * 5/4), Note(f', 4 * 5/4)]
+      [Note(c', 4 * 4/5), Note(d', 4 * 4/5), Note(e', 4 * 4/5), Note(f', 4 * 4/5)]
    '''
 
    quarter_notes = [ ]
 
-   for pitch, multiplier in listtools.zip_cyclic(pitches, multipliers):
+   for pitch, duration in listtools.zip_cyclic(pitches, durations):
       quarter_note = Note(pitch, Rational(1, 4))
-      duration_token = durtools.token_unpack(multiplier)
-      multiplier = Rational(*duration_token)
-      multiplier = Rational(1, 4) / multiplier
+      duration_token = durtools.token_unpack(duration)
+      duration = Rational(*duration_token)
+      multiplier = duration / Rational(1, 4)
       quarter_note.duration.multiplier = multiplier
       quarter_notes.append(quarter_note)
 
--- trunk/abjad/tools/construct/test/test_construct_quarter_notes_with_multipliers.py
+++ trunk/abjad/tools/construct/test/test_construct_quarter_notes_with_multipliers.py
@@ -9,12 +9,12 @@ def test_construct_quarter_notes_with_multipliers_01( ):
 
    r'''
    \new Staff {
-           c'4 * 1
-           c'4 * 5/4
-           c'4 * 3/2
-           c'4 * 7/4
+        c'4 * 1
+        c'4 * 4/5
+        c'4 * 2/3
+        c'4 * 4/7
    }   
    '''
    
    assert check.wf(t)
-   assert t.format == "\\new Staff {\n\tc'4 * 1\n\tc'4 * 5/4\n\tc'4 * 3/2\n\tc'4 * 7/4\n}"
+   assert t.format == "\\new Staff {\n\tc'4 * 1\n\tc'4 * 4/5\n\tc'4 * 2/3\n\tc'4 * 4/7\n}"
