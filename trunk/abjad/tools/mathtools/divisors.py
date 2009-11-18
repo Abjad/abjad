9a9a7e13b982e95213e194dd8bd07717496f63fb
Trevor Bača <trevorbaca@gmail.com>
2009-11-18 05:06:16 +0000

    Optimization: mathtools.divisors( ).
    Contribution jared.grubb in Google #27.

--- trunk/abjad/tools/mathtools/divisors.py
+++ trunk/abjad/tools/mathtools/divisors.py
@@ -1,5 +1,8 @@
+import math
+
+
 def divisors(n):
-   '''Return a list of the integer divisors of *n* in increasing order::
+   '''Return a list of the integer divisors of `n` in increasing order::
 
       abjad> mathtools.divisors(84)
       [1, 2, 3, 4, 6, 7, 12, 14, 21, 28, 42, 84
@@ -20,12 +23,12 @@ def divisors(n):
       18 [1, 2, 3, 6, 9, 18]
       19 [1, 19]
 
-   Raise :exc:`TypeError` on noninteger *n*::
+   Raise :exc:`TypeError` on noninteger `n`::
 
       abjad> mathtools.divisors(7.5)
       TypeError
 
-   Raise :exc:`ValueError` on nonpositive integer *n*::
+   Raise :exc:`ValueError` on nonpositive integer `n`::
 
       abjad> mathtools.divisors(-1)
       ValueError'''
@@ -36,10 +39,18 @@ def divisors(n):
    if n <= 0:
       raise ValueError
    
-   result = [ ]
-   for i in range(1, n + 1):
-      quotient = float(n) / i
-      if quotient == int(quotient):
-         result.append(i)
-
-   return result
+   ## Find all divisors from 1 to sqrt(n)
+   divisors = [1]
+   for i in range(2, int(math.sqrt(n)) + 1):
+      if n % i == 0:
+         divisors.append(i)
+   
+   codivisors = [n / i for i in reversed(divisors)]
+   ## If the number is a perfect square
+   ## we dont want the sqrt in the list twice
+   if divisors[-1] == codivisors[0]:
+      divisors.pop()
+      
+   divisors.extend(codivisors)
+ 
+   return divisors
