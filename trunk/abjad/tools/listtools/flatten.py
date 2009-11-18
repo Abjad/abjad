9212c11dd6898e28f17c4ec0f39f84338935f3c6
Trevor Bača <trevorbaca@gmail.com>
2009-11-18 05:29:18 +0000

    Optimization: listtools.flatten( ).
    Contribution jared.grubb in Google #28.

--- trunk/abjad/tools/listtools/flatten.py
+++ trunk/abjad/tools/listtools/flatten.py
@@ -1,7 +1,4 @@
-import sys
-
-
-def flatten(l, ltypes = (list, tuple), depth = sys.maxint):
+def flatten(l, ltypes = (list, tuple), depth = -1):
    '''Flatten nested lists `l`. Return a 0-depth list or tuple.
    Set optional `depth` keyword set to positive integer.
    Keyword controls depth to which the function operates.
@@ -27,44 +24,22 @@ def flatten(l, ltypes = (list, tuple), depth = sys.maxint):
       [1, 2, 3, 4, 5, 6, 7, 8]
    '''
 
-   if depth < sys.maxint:
-      #return _flatten_to_depth(l, depth)
-      return _flatten_to_depth(l, ltypes, depth)
-
    assert isinstance(l, ltypes)
    ltype = type(l)
-   l = list(l)
-   i = 0
-   while i < len(l):
-      while isinstance(l[i], ltypes):
-         if not l[i]:
-            l.pop(i)
-            i -= 1
-            break
-         else:
-            l[i:i + 1] = l[i]
-      i += 1
-   return ltype(l)
+   return ltype(_flatten(l, ltypes, depth))
 
-
-def _flatten_to_depth(l, ltypes = (list, tuple), depth = sys.maxint):
-   assert isinstance(l, list)
-   assert isinstance(depth, int)
-   assert 0 <= depth
-
-   prev_sweep = l[:]
-   for sweep in range(depth):
-      cur_sweep = [ ]
-      at_bottom = True
-      for x in prev_sweep:
-         #if isinstance(x, list):
-         if isinstance(x, ltypes):
-            cur_sweep.extend(x)
-            at_bottom = False
-         else:
-            cur_sweep.append(x)
-      prev_sweep = cur_sweep
-      if at_bottom:
-         break
-
-   return prev_sweep
+# Creates an iterator that can generate a flattened list, 
+# descending down into child elements to a depth given in the
+# argments.
+# Note: depth < 0 is effectively equivalent to "infinity"
+def _flatten(lst, ltypes, depth):
+   if not isinstance(lst, ltypes):
+      yield lst
+   elif depth==0:
+      for i in lst:
+         yield i
+   else:
+      for i in lst:
+         # Flatten an iterable by one level
+         for j in _flatten(i, ltypes, depth-1):
+            yield j
