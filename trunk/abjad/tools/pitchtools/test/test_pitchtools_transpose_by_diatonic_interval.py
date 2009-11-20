f6704b44ec74965649d873e75c55bc2b2890f153
Trevor Bača <trevorbaca@gmail.com>
2009-11-20 06:26:40 +0000

    New:
       * pitchtools.transpose_by_chromatic_interval( )
       * pitchtools.transpose_by_diatonic_interval( )

---
+++ trunk/abjad/tools/pitchtools/test/test_pitchtools_transpose_by_diatonic_interval.py
@@ -0,0 +1,32 @@
+from abjad import *
+
+
+def test_pitchtools_transpose_by_diatonic_interval_01( ):
+   '''Transpose pitch.'''
+
+   pitch = Pitch(12)
+   interval = pitchtools.DiatonicInterval('minor', -3)
+   new = pitchtools.transpose_by_diatonic_interval(pitch, interval)
+   assert new == Pitch(9)
+   assert new is not pitch
+
+
+def test_pitchtools_transpose_by_diatonic_interval_02( ):
+   '''Transpose note.'''
+
+   note = Note(12, (1, 4))
+   interval = pitchtools.DiatonicInterval('minor', -3)
+   new = pitchtools.transpose_by_diatonic_interval(note, interval)
+   assert new.pitch == Pitch(9)
+   assert new is not note
+
+
+def test_pitchtools_transpose_by_diatonic_interval_03( ):
+   '''Transpose chord.'''
+
+   chord = Chord([12, 13, 14], (1, 4))
+   interval = pitchtools.DiatonicInterval('minor', -3)
+   new = pitchtools.transpose_by_diatonic_interval(chord, interval)
+   assert new.pitches == new.pitches == (
+      Pitch('a', 4), Pitch('as', 4), Pitch('b', 4))
+   assert new is not chord
--- trunk/abjad/tools/pitchtools/transpose_by_diatonic_interval.py
+++ trunk/abjad/tools/pitchtools/transpose_by_diatonic_interval.py
@@ -1,29 +1,51 @@
+from abjad.chord import Chord
+from abjad.note import Note
+from abjad.pitch import Pitch
+from abjad.tools import clone
+from abjad.tools.pitchtools.DiatonicInterval import DiatonicInterval
 from abjad.tools.pitchtools.diatonic_scale_degree_to_letter import \
    diatonic_scale_degree_to_letter
 
 
-def transpose_by_diatonic_interval(expr, diatonic_interval):
+def transpose_by_diatonic_interval(pitch_carrier, diatonic_interval):
    '''.. versionadded:: 1.1.2
 
-   Transpose all pitch carriers in `expr` by `diatonic_interval`. ::
+   Transpose `pitch_carrier` by `diatonic_interval`. ::
 
-      abjad>
-
-   .. todo:: FINISH IMPLEMENTATION.
+      abjad> pitch = Pitch(12)
+      abjad> diatonic_interval = pitchtools.DiatonicInterval('minor', -3)
+      abjad> pitchtools.transpose_by_diatonic_interval(pitch, diatonic_interval)
+      Pitch(a, 4)
    '''
 
    if not isinstance(diatonic_interval, DiatonicInterval):
       raise TypeError('must be diatonic interval.')
 
-   
+   if isinstance(pitch_carrier, Pitch):
+      return _transpose_pitch_by_diatonic_interval(
+         pitch_carrier, diatonic_interval)
+   elif isinstance(pitch_carrier, Note):
+      new_note = clone.unspan([pitch_carrier])[0]
+      new_pitch = _transpose_pitch_by_diatonic_interval(
+         pitch_carrier.pitch, diatonic_interval)
+      new_note.pitch = new_pitch
+      return new_note
+   elif isinstance(pitch_carrier, Chord):
+      new_chord = clone.unspan([pitch_carrier])[0]
+      for new_nh, old_nh in zip(
+         new_chord.note_heads, pitch_carrier.note_heads):
+         new_pitch = _transpose_pitch_by_diatonic_interval(
+            old_nh.pitch, diatonic_interval)
+         new_nh.pitch = new_pitch
+      return new_chord
+   else:
+      raise TypeError('must be pitch, note or chord.')
+
 
 def _transpose_pitch_by_diatonic_interval(pitch, diatonic_interval):
-
    if not isinstance(pitch, Pitch):
       raise TypeError('must be pitch.')
-
    chromatic_pitch_number = pitch.number + diatonic_interval.semitones
-   diatonic_scale_degree = add_staff_spaces(
-      pitch, diatonic_interval.staff_spaces)
+   diatonic_scale_degree = pitch.degree + diatonic_interval.staff_spaces
    letter = diatonic_scale_degree_to_letter(diatonic_scale_degree)
    return Pitch(chromatic_pitch_number, letter)
