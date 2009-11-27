054ac9873a8cccb57885a28ba758022aedcf0f6d
Trevor Bača <trevorbaca@gmail.com>
2009-11-27 05:04:42 +0000

    New:
       * listtools.sublists( )
    
    Optional min_length and max_length keyword parameters.

--- trunk/abjad/tools/listtools/sublists.py
+++ trunk/abjad/tools/listtools/sublists.py
@@ -1,7 +1,7 @@
 from abjad.tools import mathtools
 
 
-def sublists(l):
+def sublists(l, min_length = None, max_length = None):
    '''.. versionadded:: 1.1.2
 
    Yield all sublists of list `l` in binary string order. ::
@@ -10,12 +10,15 @@ def sublists(l):
       [[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3], [4], [1, 4], 
       [2, 4], [1, 2, 4], [3, 4], [1, 3, 4], [2, 3, 4], [1, 2, 3, 4]]
 
-   ::
+   Yield all sublists of list `l` greater than or equal to `min_length`. ::
 
-      abjad> list(listtools.sublists(list('text')))
-      [[], ['t'], ['e'], ['t', 'e'], ['x'], ['t', 'x'], ['e', 'x'], 
-      ['t', 'e', 'x'], ['t'], ['t', 't'], ['e', 't'], ['t', 'e', 't'], 
-      ['x', 't'], ['t', 'x', 't'], ['e', 'x', 't'], ['t', 'e', 'x', 't']]
+      abjad> list(listtools.sublists([1, 2, 3, 4], min_length = 3))
+      [[1, 2, 3], [1, 2, 4], [1, 3, 4], [2, 3, 4], [1, 2, 3, 4]]
+
+   Yield all sublists of list `l` less than or equal to `max_length`. ::
+
+      abjad> list(listtools.sublists([1, 2, 3, 4], max_length = 2))
+      [[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [4], [1, 4], [2, 4], [3, 4]]
 
    .. note :: ``listtools.sublists`` will deprecate when Abjad migrates to
       Python 2.6 because Python 2.6 includes built-in subset generation.
@@ -24,14 +27,20 @@ def sublists(l):
    if not isinstance(l, list):
       raise TypeError('%s must be list.' % l)
 
-   sublists = [ ]
-
    len_l = len(l)
    for i in range(2 ** len_l):
       binary_string = mathtools.binary_string(i)
       binary_string = binary_string.zfill(len_l)
-      subset = [ ]
+      sublist = [ ]
       for j, digit in enumerate(reversed(binary_string)):
          if digit == '1':
-            subset.append(l[j])
-      yield subset
+            sublist.append(l[j])
+      yield_sublist = True
+      if min_length is not None:
+         if len(sublist) < min_length:
+            yield_sublist = False
+      if max_length is not None:
+         if max_length < len(sublist):
+            yield_sublist = False
+      if yield_sublist:
+         yield sublist
--- trunk/abjad/tools/listtools/test/test_listtools_sublists.py
+++ trunk/abjad/tools/listtools/test/test_listtools_sublists.py
@@ -13,3 +13,10 @@ def test_listtools_sublists_02( ):
    l = list('text')
    generator = listtools.sublists(l) 
    assert list(generator) == [[], ['t'], ['e'], ['t', 'e'], ['x'], ['t', 'x'], ['e', 'x'], ['t', 'e', 'x'], ['t'], ['t', 't'], ['e', 't'], ['t', 'e', 't'], ['x', 't'], ['t', 'x', 't'], ['e', 'x', 't'], ['t', 'e', 'x', 't']]
+
+
+def test_listtools_sublists_03( ):
+
+   l = [1, 2, 3, 4]
+   generator = listtools.sublists(l, 2, 2)
+   assert list(generator) == [[1, 2], [1, 3], [2, 3], [1, 4], [2, 4], [3, 4]]
