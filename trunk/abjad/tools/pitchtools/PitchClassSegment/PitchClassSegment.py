720baf5a505f878ccce596f4cf2f0c5999441471
Trevor Bača <trevorbaca@gmail.com>
2009-12-07 03:28:24 +0000

    New:
       * PitchClassSegment.invert( )
       * PitchClassSegment.multiply( )
       * PitchClassSegment.retrograde( )
       * PitchClassSegment.rotate( )
       * PitchClassSegment.transpose( )

--- trunk/abjad/tools/pitchtools/PitchClassSegment/PitchClassSegment.py
+++ trunk/abjad/tools/pitchtools/PitchClassSegment/PitchClassSegment.py
@@ -1,5 +1,6 @@
 from abjad.tools.pitchtools.PitchClass import PitchClass
 from abjad.tools.pitchtools.PitchClassSet import PitchClassSet
+import copy
 
 
 class PitchClassSegment(list):
@@ -20,7 +21,6 @@ class PitchClassSegment(list):
    def __str__(self):
       return '<%s>' % self._format_string
       
-
    ## PRIVATE ATTRIBUTES ##
 
    @property
@@ -41,3 +41,22 @@ class PitchClassSegment(list):
    @property
    def pitch_classes(self):
       return tuple(self[:])
+
+   ## PUBLIC METHODS ##
+
+   def invert(self):
+      return PitchClassSegment([pc.invert( ) for pc in self])
+      
+   def multiply(self, n):
+      return PitchClassSegment([pc.multiply(n) for pc in self])
+
+   def retrograde(self):
+      return PitchClassSegment(reversed(self))
+
+   def rotate(self, n):
+      from abjad.tools import listtools
+      pitch_classes = listtools.rotate(self.pitch_classes, n)
+      return PitchClassSegment(pitch_classes)
+      
+   def transpose(self, n):
+      return PitchClassSegment([pc.transpose(n) for pc in self])
