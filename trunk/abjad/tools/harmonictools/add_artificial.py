dd24577910f11e45f9dd8d2c7bd404edb414bdf7
Trevor Bača <trevorbaca@gmail.com>
2009-12-09 19:54:24 +0000

    Patch to harmonictools for previous commit.

--- trunk/abjad/tools/harmonictools/add_artificial.py
+++ trunk/abjad/tools/harmonictools/add_artificial.py
@@ -4,7 +4,7 @@ from abjad.tools import pitchtools
 
 perfect_fourth = pitchtools.MelodicDiatonicInterval('perfect', 4)
 
-def add_artificial(note, diatonic_interval = perfect_fourth):
+def add_artificial(note, melodic_diatonic_interval = perfect_fourth):
    r'''Add artifical harmonic at `diatonic_interval` to `note`.
 
    ::
@@ -21,7 +21,7 @@ def add_artificial(note, diatonic_interval = perfect_fourth):
 
    chord = Chord(note)
    chord.append(chord[0].pitch.number)
-   chord[1].pitch = pitchtools.transpose_by_diatonic_interval(
-      chord[1].pitch, diatonic_interval)
+   chord[1].pitch = pitchtools.transpose_by_melodic_diatonic_interval(
+      chord[1].pitch, melodic_diatonic_interval)
    chord[1].style = 'harmonic'
    return chord
