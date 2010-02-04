0db68971db16b473d55f6646f21291c2d7797692
Trevor Bača <trevorbaca@gmail.com>
2010-02-04 16:57:34 +0000

    Extension:
       lilytools.parse_note_entry_string( )
    
    Function can now handle ties in input string.
    
    Example:
    
       note_entry_string = "g'4 a'4 ~ a'2 g'4. fs'8 e'4 d'4"
       container = lilytools.parse_note_entry_string(note_entry_string)
       staff = Staff([ ])
       staff[:] = container[:]
    
       \new Staff {
               g'4
               a'4 ~
               a'2
               g'4.
               fs'8
               e'4
               d'4
       }

--- trunk/abjad/tools/lilytools/parse_note_entry_string.py
+++ trunk/abjad/tools/lilytools/parse_note_entry_string.py
@@ -1,6 +1,9 @@
 from _parse_note_entry_token import _parse_note_entry_token
 from abjad.container import Container
+from abjad.exceptions import MissingSpannerError
+from abjad.spanners import Tie
 from abjad.tools import iterate
+from abjad.voice import Voice
 import re
 
 
@@ -11,28 +14,31 @@ def parse_note_entry_string(note_entry_string):
 
       abjad> note_entry_string = "g'2 a'2 g'4. fs'8 e'4 d'4"
       abjad> lilytools.parse_note_entry_string(note_entry_string)
+      {g'4, a'4, a'2, fs'8, e'4, d'4}
+      abjad> container[:]
       [Note(g', 2), Note(a', 2), Note(g', 4.), Note(fs', 8), Note(e', 4), Note(d', 4)]
    '''
 
    container = Container([ ])
    tokens = note_entry_string.split( ) 
-   tie_started = False
+   tie_next_leaf = False
    for token in tokens:
       if re.match('\w+', token) is not None: 
-         abjad_object = _parse_note_entry_token(token)
-         last_leaf = iterate.get_nth_leaf(container, -1)
-         if last_leaf is not None:
-            last_leaf.splice([abjad_object])
+         leaf = _parse_note_entry_token(token)
+         if tie_next_leaf:
+            last_leaf = iterate.get_nth_leaf(container, -1)
+            last_leaf.splice([leaf])
+            tie_next_leaf = False
          else:
-            container.append(abjad_object)
+            container.append(leaf) 
       elif token == '~':
-         last_leaf = iterate.leaves_backward_in(container, -1)
-         Tie([last_leaf])
-         tie_started = True
+         last_leaf = iterate.get_nth_leaf(container, -1)
+         try:
+            tie_spanner = last_leaf.tie.spanner
+         except MissingSpannerError:
+            Tie([last_leaf])
+         tie_next_leaf = True
       else:
          pass
 
-   result = container[:]
-   container[:] = [ ]
-
-   return result
+   return container
--- trunk/abjad/tools/lilytools/test/test_lilytools_parse_note_entry_string.py
+++ trunk/abjad/tools/lilytools/test/test_lilytools_parse_note_entry_string.py
@@ -4,8 +4,9 @@ from abjad import *
 def test_lilytools_parse_note_entry_string_01( ):
 
    note_entry_string = "g'2 a'2 g'4. fs'8 e'4 d'4"
-   leaves = lilytools.parse_note_entry_string(note_entry_string)
-   staff = Staff(leaves)
+   container = lilytools.parse_note_entry_string(note_entry_string)
+   staff = Staff([ ])
+   staff[:] = container[:]
 
    r'''
    \new Staff {
@@ -20,3 +21,26 @@ def test_lilytools_parse_note_entry_string_01( ):
    
    assert check.wf(staff)
    assert staff.format == "\\new Staff {\n\tg'2\n\ta'2\n\tg'4.\n\tfs'8\n\te'4\n\td'4\n}"
+
+
+def test_lilytools_parse_note_entry_string_02( ):
+
+   note_entry_string = "g'4 a'4 ~ a'2 g'4. fs'8 e'4 d'4"
+   container = lilytools.parse_note_entry_string(note_entry_string)
+   staff = Staff([ ])
+   staff[:] = container[:]
+
+   r'''
+   \new Staff {
+           g'4
+           a'4 ~
+           a'2
+           g'4.
+           fs'8
+           e'4
+           d'4
+   }
+   '''
+   
+   assert check.wf(staff)
+   assert staff.format == "\\new Staff {\n\tg'4\n\ta'4 ~\n\ta'2\n\tg'4.\n\tfs'8\n\te'4\n\td'4\n}"
