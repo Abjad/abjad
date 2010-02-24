57edc745e8cc755cf70840d08a9345d16907112f
Trevor Bača <trevorbaca@gmail.com>
2010-02-24 14:57:43 +0000

    Docs: updated usage example in layout.apply_layout_schema( ) API entry.

--- trunk/abjad/tools/layout/apply_layout_schema.py
+++ trunk/abjad/tools/layout/apply_layout_schema.py
@@ -10,55 +10,80 @@ from abjad.tools.layout.line_break_every_seconds import \
 
 def apply_layout_schema(expr, layout_schema, 
    klass = _Measure, adjust_eol = False, add_empty_bars = False):
-   r'''.. versionadded:: 1.1.2
+   r""".. versionadded:: 1.1.2
 
    Apply `layout_schema` to `expr`.
 
    The following example line breaks every 4 eighth notes,
-   layos out 5 systems per page, spaces systems 40 vertical spaces
+   lays out 5 systems per page, spaces systems 40 vertical spaces
    apart, leaves empty vertical space equivalent to a single system
-   at the top of the first page, and sets the two staves in each system
-   at offsets 0 and -15. ::
+   at the top of the first page, sets the first staff in each system
+   to alignment distance 0 and sets the second staff in each
+   system to alignment distance 15. ::
 
-      abjad> t = Staff(RigidMeasure((2, 8), construct.run(2)) * 4)
-      abjad> pitchtools.diatonicize(t)
-      abjad> schema = layout.LayoutSchema(Rational(4, 8), (40, 5, 1), (0, 15))
-      abjad> layout.apply_layout_schema(t, schema)
-      abjad> f(t)
-      \new Staff {
-              {
-                      \overrideProperty #"Score.NonMusicalPaperColumn"
-                      #'line-break-system-details
-                      #'((Y-offset . 40) (alignment-distances . (0 15)))
-                      \time 2/8
-                      c'8
-                      d'8
+      abjad> score = Score(2 * Staff(RigidMeasure((2, 8), construct.run(2)) * 4))
+      abjad> pitchtools.diatonicize(score)
+      abjad> schema = layout.LayoutSchema(Rational(4, 8), (40, 5, 1), (15, ))
+      abjad> layout.apply_layout_schema(score[0], schema)
+      abjad> f(score)
+      \new Score <<
+              \new Staff {
+                      {
+                              \overrideProperty #"Score.NonMusicalPaperColumn"
+                              #'line-break-system-details
+                              #'((Y-offset . 40) (alignment-distances . (15)))
+                              \time 2/8
+                              c'8
+                              d'8
+                      }
+                      {
+                              \time 2/8
+                              e'8
+                              f'8
+                              \break
+                              \noPageBreak
+                      }
+                      {
+                              \overrideProperty #"Score.NonMusicalPaperColumn"
+                              #'line-break-system-details
+                              #'((Y-offset . 80) (alignment-distances . (15)))
+                              \time 2/8
+                              g'8
+                              a'8
+                      }
+                      {
+                              \time 2/8
+                              b'8
+                              c''8
+                              \break
+                      }
               }
-              {
-                      \time 2/8
-                      e'8
-                      f'8
-                      \break
-                      \noPageBreak
+              \new Staff {
+                      {
+                              \time 2/8
+                              d''8
+                              e''8
+                      }
+                      {
+                              \time 2/8
+                              f''8
+                              g''8
+                      }
+                      {
+                              \time 2/8
+                              a''8
+                              b''8
+                      }
+                      {
+                              \time 2/8
+                              c'''8
+                              d'''8
+                      }
               }
-              {
-                      \overrideProperty #"Score.NonMusicalPaperColumn"
-                      #'line-break-system-details
-                      #'((Y-offset . 80) (alignment-distance . (0 15)))
-                      \time 2/8
-                      g'8
-                      a'8
-              }
-              {
-                      \time 2/8
-                      b'8
-                      c''8
-                      \break
-              }
-      }
+      >>
 
    .. todo:: document LayoutSchema.in_seconds.
-   '''
+   """
    
    if not isinstance(layout_schema, LayoutSchema):
       raise TypeError('must be layout schema.')
