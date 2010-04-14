36b8d40800a03b3ad629054208d6475c47f5a753
Trevor Bača <trevorbaca@gmail.com>
2010-04-14 01:18:27 +0000

    Renamed accidental test files.

---
+++ trunk/abjad/tools/pitchtools/Accidental/test/test_Accidental_compare.py
@@ -0,0 +1,34 @@
+from abjad import *
+
+
+def test_accidental_compare_01( ):
+   '''Accidentals equal by string compare equally.'''
+   k1, k2 = pitchtools.Accidental('s'), pitchtools.Accidental('s')
+   assert     k1 == k2
+   assert not k1 != k2
+   assert not k1 >  k2
+   assert     k1 >= k2
+   assert not k1 <  k2
+   assert     k1 <= k2
+
+
+def test_accidental_compare_02( ):
+   '''Accidentals unequal by string compare unequally.'''
+   k1, k2 = pitchtools.Accidental('s'), pitchtools.Accidental('f')
+   assert not k1 == k2
+   assert     k1 != k2
+   assert     k1 >  k2
+   assert     k1 >= k2
+   assert not k1 <  k2
+   assert not k1 <= k2
+
+
+def test_accidental_compare_03( ):
+   '''No accidental and forced natural compare in a special way.'''
+   k1, k2 = pitchtools.Accidental(''), pitchtools.Accidental('!')
+   assert not k1 == k2
+   assert     k1 != k2
+   assert not k1 >  k2
+   assert     k1 >= k2
+   assert not k1 <  k2
+   assert     k1 <= k2
