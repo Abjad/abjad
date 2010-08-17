430998ded8245f2352c2e2d54bb8dcd5e37e88c1
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2010-08-17 19:44:37 +0000

    lilyfiletools.parse_note_entry_string now handles chords and slurs.

--- trunk/abjad/tools/lilyfiletools/parse_note_entry_string.py
+++ trunk/abjad/tools/lilyfiletools/parse_note_entry_string.py
@@ -1,7 +1,10 @@
 from abjad.components.Container import Container
 from abjad.exceptions import MissingSpannerError
+from abjad.marks.Articulation import Articulation
+from abjad.tools.spannertools import SlurSpanner
 from abjad.tools.spannertools import TieSpanner
 from abjad.tools.lilyfiletools._parse_note_entry_token import _parse_note_entry_token
+from abjad.tools.lilyfiletools._parse_chord_entry_token import _parse_chord_entry_token
 from abjad.components.Voice import Voice
 import re
 
@@ -23,36 +26,96 @@ def parse_note_entry_string(note_entry_string):
 
    container = Container([ ])
    tokens = note_entry_string.split( ) 
+
+   is_chord = False
+   chord_tokens = [ ]
+
+   in_slur = False
+   slur_start_leaf = None
+
    tie_next_leaf = False
    waiting_on_bar_string = False
+
    for token in tokens:
-      if re.match('\w+', token) is not None: 
-         leaf = _parse_note_entry_token(token)
-         if tie_next_leaf:
-            last_leaf = leaftools.get_nth_leaf_in_expr(container, -1)
-            last_leaf.splice([leaf])
-            tie_next_leaf = False
+
+      if is_chord: # we are inside a chord
+
+         if re.match("[A-Za-z0-9,']+>[0-9]+", token) is not None:
+            chord_tokens.append(token)
+            leaf = _parse_chord_entry_token(' '.join(chord_tokens))
+            if tie_next_leaf:
+               last_leaf = leaftools.get_nth_leaf_in_expr(container, -1)
+               last_leaf.splice([leaf])
+               tie_next_leaf = False
+            else:
+               container.append(leaf)
+            is_chord = False
+            chord_tokens = [ ]
+
+         elif re.match('\w+', token) is not None:
+            chord_tokens.append(token)
+
          else:
-            container.append(leaf) 
-      elif token == '~':
-         last_leaf = leaftools.get_nth_leaf_in_expr(container, -1)
-         try:
-            tie_spanner = last_leaf.tie.spanner
-         except MissingSpannerError:
-            TieSpanner([last_leaf])
-         tie_next_leaf = True
-      elif token == r'\bar':
-         waiting_on_bar_string = True
-      elif token.startswith('"'):
-         bar_string = eval(token)
-         last_leaf = leaftools.get_nth_leaf_in_expr(container, -1)
-         last_leaf.misc.bar = bar_string
-         waiting_on_bar_string = False
-      elif token.startswith('\\'):
-         last_leaf = leaftools.get_nth_leaf_in_expr(container, -1)
-         articulation_string = token.strip('\\')
-         last_leaf.articulations.append(articulation_string)
+            pass
+
       else:
-         pass
+
+         if re.match('\w+', token) is not None:
+            leaf = _parse_note_entry_token(token)
+            if tie_next_leaf:
+               last_leaf = leaftools.get_nth_leaf_in_expr(container, -1)
+               last_leaf.splice([leaf])
+               tie_next_leaf = False
+            else:
+               container.append(leaf) 
+
+         elif re.match('<\w+', token) is not None:
+            is_chord = True
+            chord_tokens.append(token)
+
+         elif token == '~':
+            last_leaf = leaftools.get_nth_leaf_in_expr(container, -1)
+            try:
+               tie_spanner = last_leaf.tie.spanner
+            except MissingSpannerError:
+               TieSpanner([last_leaf])
+            tie_next_leaf = True
+
+         elif token == r'\bar':
+            waiting_on_bar_string = True
+
+         elif token.startswith('"'):
+            bar_string = eval(token)
+            last_leaf = leaftools.get_nth_leaf_in_expr(container, -1)
+            last_leaf.misc.bar = bar_string
+            waiting_on_bar_string = False
+
+         elif token.startswith(('\\', '-', '^', '_')):
+            last_leaf = leaftools.get_nth_leaf_in_expr(container, -1)
+            try:
+               if 0 <= token.index('\\'):
+                  last_leaf.articulations.append(token)
+            except:
+               last_leaf.articulations.append(Articulation(token[1], token[0]))
+
+         elif token == '(':
+            if in_slur:
+               raise Exception('Opening a slur when another is already open.')
+            in_slur = True
+            slur_start_leaf = leaftools.get_nth_leaf_in_expr(container, -1)
+
+         elif token == ')':
+            if not in_slur:
+               raise Exception('Closing a non-existent slur.')
+            in_slur = False
+            slur_stop_leaf = leaftools.get_nth_leaf_in_expr(container, -1)
+            start_index = container.index(slur_start_leaf)
+            stop_index = container.index(slur_stop_leaf)
+            SlurSpanner(container[start_index:stop_index + 1])
+            slur_start_leaf = None
+
+         else:
+            pass
+
 
    return container
--- trunk/abjad/tools/lilyfiletools/test/test_lilyfiletools_parse_note_entry_string.py
+++ trunk/abjad/tools/lilyfiletools/test/test_lilyfiletools_parse_note_entry_string.py
@@ -23,3 +23,27 @@ def test_lilyfiletools_parse_note_entry_string_01( ):
    
    assert componenttools.is_well_formed_component(staff)
    assert staff.format == '\\new Staff {\n\tg\'4\n\ta\'4 ~\n\ta\'2\n\t\\bar "||"\n\tg\'4.\n\tfs\'8\n\te\'4\n\td\'4 -\\fermata\n}'
+
+
+def test_lilyfiletools_parse_note_entry_string_02( ):
+
+   note_entry_string = r'''c'4 g'4 b8 c'8 d'8 -. ( ef'8 ^\marcato <bf cs' f'>4 c'4 )'''
+   container = lilyfiletools.parse_note_entry_string(note_entry_string)
+   staff = Staff([ ])
+   staff[:] = container[:]
+
+   r'''
+   \new Staff {
+           c'4
+           g'4
+           b8
+           c'8
+           d'8 -\staccato (
+           ef'8 ^\marcato
+           <bf cs' f'>4
+           c'4 )
+   }
+   '''  
+
+   assert componenttools.is_well_formed_component(staff)
+   assert staff.format == "\\new Staff {\n\tc'4\n\tg'4\n\tb8\n\tc'8\n\td'8 -\\staccato (\n\tef'8 ^\\marcato\n\t<bf cs' f'>4\n\tc'4 )\n}"
