e7ff3deaf43893c871534499bbbc0fd288a45168
Trevor Bača <trevorbaca@gmail.com>
2010-08-17 01:15:41 +0000

    Removed _GrobHandlerSpanner from codebase.
    Removed _PositionalGrobHandlerSpanner from codebase.

--- trunk/abjad/spanners/Spanner/_GrobHandlerSpanner.py
+++
@@ -1,9 +0,0 @@
-from abjad.core import _GrobHandler
-from abjad.spanners.Spanner.Spanner import Spanner
-
-
-class _GrobHandlerSpanner(Spanner, _GrobHandler):
-
-   def __init__(self, grob, music = None):
-      Spanner.__init__(self, music)
-      _GrobHandler.__init__(self, grob)
