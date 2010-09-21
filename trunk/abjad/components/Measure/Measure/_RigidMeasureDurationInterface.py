6305bd5f9b9cf7e08734073c9800406550241c51
Trevor Bača <trevorbaca@gmail.com>
2010-09-21 15:17:47 +0000

    Moved _RigidMeasureDurationInterface functionality up to _MeasureDurationInterface.
    In preparation for removing _Measure in favor of Measure.

--- trunk/abjad/components/Measure/Measure/_RigidMeasureDurationInterface.py
+++ trunk/abjad/components/Measure/Measure/_RigidMeasureDurationInterface.py
@@ -4,30 +4,32 @@ from abjad.tools import marktools
 
 class _RigidMeasureDurationInterface(_MeasureDurationInterface):
 
-   ## PUBLIC ATTRIBUTES ##
+#   ## PUBLIC ATTRIBUTES ##
+#
+#   @property
+#   def is_overfull(self):
+#      '''.. versionadded:: 1.1.1
+#
+#      True when prolated duration is greater than 
+#      effective meter duration.
+#      '''
+#
+#      return marktools.get_effective_time_signature(self._client).duration < self.prolated
+#      
+#
+#   @property
+#   def is_underfull(self):
+#      '''.. versionadded:: 1.1.1
+#
+#      True when prolated duration is less than 
+#      effective meter duration.
+#      '''
+#
+#      return self.prolated < marktools.get_effective_time_signature(self._client).duration
+#
+#   @property
+#   def preprolated(self):
+#      '''Measure contents duration times effective meter multiplier.'''
+#      return marktools.get_effective_time_signature(self._client).multiplier * self.contents
 
-   @property
-   def is_overfull(self):
-      '''.. versionadded:: 1.1.1
-
-      True when prolated duration is greater than 
-      effective meter duration.
-      '''
-
-      return marktools.get_effective_time_signature(self._client).duration < self.prolated
-      
-
-   @property
-   def is_underfull(self):
-      '''.. versionadded:: 1.1.1
-
-      True when prolated duration is less than 
-      effective meter duration.
-      '''
-
-      return self.prolated < marktools.get_effective_time_signature(self._client).duration
-
-   @property
-   def preprolated(self):
-      '''Measure contents duration times effective meter multiplier.'''
-      return marktools.get_effective_time_signature(self._client).multiplier * self.contents
+   pass
--- trunk/abjad/components/Measure/_MeasureDurationInterface.py
+++ trunk/abjad/components/Measure/_MeasureDurationInterface.py
@@ -18,13 +18,36 @@ class _MeasureDurationInterface(_MultipliedContainerDurationInterface):
    ## PUBLIC ATTRIBUTES ##
 
    @property
-   def multiplier(self):
-      return marktools.get_effective_time_signature(self._client).multiplier
-
-   @property
    def is_binary(self):
       return not self.is_nonbinary
 
    @property
    def is_nonbinary(self):
       return marktools.get_effective_time_signature(self._client).is_nonbinary
+
+   @property
+   def is_overfull(self):
+      '''.. versionadded:: 1.1.1
+
+      True when prolated duration is greater than 
+      effective meter duration.
+      '''
+      return marktools.get_effective_time_signature(self._client).duration < self.prolated
+
+   @property
+   def is_underfull(self):
+      '''.. versionadded:: 1.1.1
+
+      True when prolated duration is less than 
+      effective meter duration.
+      '''
+      return self.prolated < marktools.get_effective_time_signature(self._client).duration
+
+   @property
+   def multiplier(self):
+      return marktools.get_effective_time_signature(self._client).multiplier
+
+   @property
+   def preprolated(self):
+      '''Measure contents duration times effective meter multiplier.'''
+      return marktools.get_effective_time_signature(self._client).multiplier * self.contents
