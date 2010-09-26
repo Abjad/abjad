e040ba210deb357a4708fea8934b6e20ce72d828
Trevor Bača <trevorbaca@gmail.com>
2010-09-26 19:57:04 +0000

    Updated listtools.group_by_equality( ) API entry.

--- trunk/abjad/tools/listtools/group_by_equality.py
+++ trunk/abjad/tools/listtools/group_by_equality.py
@@ -1,12 +1,8 @@
 import itertools
 
-def group_by_equality(lst):
-   '''Group elements in *lst* by equality. All adjacent elements that
-   are equal are grouped together in a tuple. 
 
-   Returns a generator.
-
-   Examples::
+def group_by_equality(l):
+   '''Group elements in `l` by equality::
 
       abjad> l = [0, 0, -1, -1, 2, 3, -5, 1, 1, 5, -5]
 
@@ -14,9 +10,10 @@ def group_by_equality(lst):
    
       abjad> list(listtools.group_by_equality(l))
       [(0, 0), (-1, -1), (2,), (3,), (-5,), (1, 1), (5,), (-5,)] 
+
+   Return generator of tuples.
    '''
 
-
-   g = itertools.groupby(lst, lambda x: x)
+   g = itertools.groupby(l, lambda x: x)
    for n, group in g:
       yield tuple(group)
