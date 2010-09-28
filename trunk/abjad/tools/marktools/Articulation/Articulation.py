1fb614d9e59527288aef2861b3fd2c6ef837e045
Trevor Bača <trevorbaca@gmail.com>
2010-09-28 17:31:32 +0000

    Intermediate commit to replace articulations interface with marktools.Articulation class.

--- trunk/abjad/tools/marktools/Articulation/Articulation.py
+++ trunk/abjad/tools/marktools/Articulation/Articulation.py
@@ -1,9 +1,11 @@
-from abjad.core import _StrictComparator
-from abjad.core import _Immutable
+#from abjad.core import _StrictComparator
+#from abjad.core import _Immutable
+from abjad.tools.contexttools.Mark import Mark
 
 
-class Articulation(_StrictComparator, _Immutable):
-   '''Any staccato, tenuto, portato or other articulation:
+#class Articulation(_StrictComparator, _Immutable):
+class Articulation(Mark):
+   '''Abjad model of music articulation:
 
    ::
 
@@ -13,51 +15,10 @@ class Articulation(_StrictComparator, _Immutable):
 
    __slots__ = ('_string', '_direction')
 
-   def __new__(klass, *args):
-
-      self = object.__new__(klass)
-
-      assert len(args) in range(3)
-      if 2 <= len(args):
-         assert isinstance(args[0], (str, type(None)))
-         assert isinstance(args[1], (str, type(None)))
-         string, direction = args
-      elif len(args) == 1:
-         assert isinstance(args[0], (str, type(None)))
-         if args[0]:
-            splits = args[0].split('\\')
-            assert len(splits) in (1, 2)
-            if len(splits) == 1:
-               string, direction = args[0], None
-            elif len(splits) == 2:
-               string = splits[1]
-               if splits[0]:
-                  direction = splits[0]
-               else:
-                  direction = None
-         else:
-            string, direction = None, None
-      else:
-         string, direction = None, None
-
-      if direction in ('^', 'up'):
-         direction = '^'
-      elif direction in ('_', 'down'):
-         direction = '_'
-      elif direction in ('-', 'default', None):
-         direction = '-'
-      else:
-         raise ValueError('can not set articulation direction.')
-
-      object.__setattr__(self, '_string', string)
-      object.__setattr__(self, '_direction', direction)
-
-      return self
-
-   def __getnewargs__(self):
-      return (self.string, self.direction)
-
-#   def __init__(self, *args):
+#   def __new__(klass, *args):
+#
+#      self = object.__new__(klass)
+#
 #      assert len(args) in range(3)
 #      if 2 <= len(args):
 #         assert isinstance(args[0], (str, type(None)))
@@ -92,15 +53,62 @@ class Articulation(_StrictComparator, _Immutable):
 #
 #      object.__setattr__(self, '_string', string)
 #      object.__setattr__(self, '_direction', direction)
+#
+#      return self
+#
+#   def __getnewargs__(self):
+#      return (self.string, self.direction)
+
+   def __init__(self, *args):
+      assert len(args) in range(3)
+      if 2 <= len(args):
+         assert isinstance(args[0], (str, type(None)))
+         assert isinstance(args[1], (str, type(None)))
+         string, direction = args
+      elif len(args) == 1:
+         assert isinstance(args[0], (str, type(None)))
+         if args[0]:
+            splits = args[0].split('\\')
+            assert len(splits) in (1, 2)
+            if len(splits) == 1:
+               string, direction = args[0], None
+            elif len(splits) == 2:
+               string = splits[1]
+               if splits[0]:
+                  direction = splits[0]
+               else:
+                  direction = None
+         else:
+            string, direction = None, None
+      else:
+         string, direction = None, None
+
+      if direction in ('^', 'up'):
+         direction = '^'
+      elif direction in ('_', 'down'):
+         direction = '_'
+      elif direction in ('-', 'default', None):
+         direction = '-'
+      else:
+         raise ValueError('can not set articulation direction.')
+
+      object.__setattr__(self, '_string', string)
+      object.__setattr__(self, '_direction', direction)
+      self._format_slot = 'right'
 
    ## OVERLOADS ##
 
+   def __copy__(self, *args):
+      return type(self)(self.string, self.direction)
+
+   __deepcopy__ = __copy__
+
    def __eq__(self, expr):
-      assert isinstance(expr, Articulation)
-      if expr.string == self.string and self.direction == expr.direction:
-         return True
-      else:
-         return False
+      assert isinstance(expr, type(self))
+      if expr.string == self.string:
+         if self.direction == expr.direction:
+            return True
+      return False
 
    def __repr__(self):
       return "%s('%s')" % (self.__class__.__name__, self)
--- trunk/abjad/tools/marktools/__init__.py
+++ trunk/abjad/tools/marktools/__init__.py
@@ -3,5 +3,6 @@ from abjad.tools.importtools._package_import import _package_import
 _package_import(__path__[0], globals( ))
 
 from Annotation import Annotation
+from Articulation import Articulation
 from CommentMark import CommentMark
 from LilyPondCommandMark import LilyPondCommandMark
