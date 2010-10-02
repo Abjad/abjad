1337ef831de7b5ef6c718e60928248adeab775d5
Trevor Bača <trevorbaca@gmail.com>
2010-10-02 03:20:02 +0000

    Simplified two format helpers:
       _get_grob_override_format_contributions( )
       _get_grob_revert_format_contributions( )

--- trunk/abjad/tools/formattools/_get_grob_revert_format_contributions.py
+++ trunk/abjad/tools/formattools/_get_grob_revert_format_contributions.py
@@ -2,28 +2,8 @@ def _get_grob_revert_format_contributions(component):
    '''Alphabetized list of LilyPond grob reverts.
    '''
    from abjad.components._Leaf import _Leaf
-   from abjad.core.LilyPondGrobProxy import LilyPondGrobProxy
-   from abjad.core.LilyPondGrobProxyContextWrapper import LilyPondGrobProxyContextWrapper
-   from abjad.tools.lilyfiletools._make_lilypond_revert_string import \
-      _make_lilypond_revert_string
 
    result = [ ]
    if not isinstance(component, _Leaf):
-      for name, value in vars(component.override).iteritems( ):
-         #print name, value
-         if isinstance(value, LilyPondGrobProxyContextWrapper):
-            context_name, context_wrapper = name.lstrip('_'), value
-            #print context_name, context_wrapper
-            for grob_name, grob_override_namespace in vars(context_wrapper).iteritems( ):
-               #print grob_name, grob_override_namespace
-               for grob_attribute, grob_value in vars(grob_override_namespace).iteritems( ):
-                  #print grob_attribute, grob_value
-                  result.append(_make_lilypond_revert_string(
-                     grob_name, grob_attribute, context_name = context_name))
-         elif isinstance(value, LilyPondGrobProxy):
-            grob_name, grob_namespace = name, value
-            for grob_attribute, grob_value in vars(grob_namespace).iteritems( ):
-               result.append(_make_lilypond_revert_string(grob_name, grob_attribute))
-   ## guarantee predictable order of revert statements
-   result.sort( )
+      result.extend(component.override._list_format_contributions('revert'))
    return result
