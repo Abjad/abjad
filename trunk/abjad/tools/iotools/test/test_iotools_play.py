ec6efa5ed8cac05a3c483863cc45fae220cf4461
Víctor Adán <vctradn@gmail.com>
2010-12-13 16:21:04 +0000

    * Fixed bug in iotools.play( ). The function was failing on Containers. The problem was with the new LilyFile.score_block attribute. * Added play 'tests' to the test_iotools_play file. What's the best way to test these methods calling external programs?

--- trunk/abjad/tools/iotools/test/test_iotools_play.py
+++ trunk/abjad/tools/iotools/test/test_iotools_play.py
@@ -1,4 +1,16 @@
 from abjad import *
 
 
-## stub test module ##
+## What's the best way to test play?
+
+def test_iotools_play_01( ):
+   '''A note can be played.'''
+   n = Note(1, (1, 32))
+   play(n)
+
+
+def test_iotools_play_02( ):
+   '''A score can be played.'''
+   notes = [Note(i, (1, 64)) for i in range(10)]
+   t = Score([Staff(notes)])
+   play(t)
