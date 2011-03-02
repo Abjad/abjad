6e4d72f498d9de1053d38c3f3d9f5a502201e983
Trevor Bača <trevorbaca@gmail.com>
2011-03-02 18:03:03 +0000

    Removed formattools from top-level package initializer.

--- trunk/abjad/tools/__init__.py
+++ trunk/abjad/tools/__init__.py
@@ -3,7 +3,6 @@ from abjad.tools import chordtools
 from abjad.tools import componenttools
 from abjad.tools import containertools
 from abjad.tools import contexttools
-from abjad.tools import formattools
 from abjad.tools import gracetools
 from abjad.tools import layouttools
 from abjad.tools import lilyfiletools
