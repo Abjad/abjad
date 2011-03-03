8ff7da04542e08ca6b8a3e0d1f83eec013679d0d
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2011-03-03 06:16:06 +0000

    Wrote treetools.BoundedInterval tests.

--- trunk/abjad/tools/treetools/BoundedInterval/test/test_BoundedInterval_is_container_of_interval.py
+++ trunk/abjad/tools/treetools/BoundedInterval/test/test_BoundedInterval_is_container_of_interval.py
@@ -2,8 +2,62 @@ import py.test
 from abjad.tools.treetools import *
 
 
-py.test.skip('Tests not yet implemented.')
-
 def test_BoundedInterval_is_container_of_interval_01( ):
-   pass
+    a = BoundedInterval(-2, 2)
+    b = BoundedInterval(-7, -3)
+    assert not a.is_container_of_interval(b)
+    assert not b.is_container_of_interval(a)
 
+def test_BoundedInterval_is_container_of_interval_02( ):
+    a = BoundedInterval(-2, 2)
+    b = BoundedInterval(-6, -2)
+    assert not a.is_container_of_interval(b)
+    assert not b.is_container_of_interval(a)
+
+def test_BoundedInterval_is_container_of_interval_03( ):
+    a = BoundedInterval(-2, 2)
+    b = BoundedInterval(-4, 0)
+    assert not a.is_container_of_interval(b)
+    assert not b.is_container_of_interval(a)
+
+def test_BoundedInterval_is_container_of_interval_04( ):
+    a = BoundedInterval(-2, 2)
+    b = BoundedInterval(-2, 0)
+    assert a.is_container_of_interval(b)
+    assert not b.is_container_of_interval(a)
+
+def test_BoundedInterval_is_container_of_interval_05( ):
+    a = BoundedInterval(-2, 2)
+    b = BoundedInterval(-2, 2)
+    assert a.is_container_of_interval(b)
+    assert b.is_container_of_interval(a)
+
+def test_BoundedInterval_is_container_of_interval_06( ):
+    a = BoundedInterval(-2, 2)
+    b = BoundedInterval(-1, 1)
+    assert a.is_container_of_interval(b)
+    assert not b.is_container_of_interval(a)
+
+def test_BoundedInterval_is_container_of_interval_07( ):
+    a = BoundedInterval(-2, 2)
+    b = BoundedInterval(0, 2)
+    assert a.is_container_of_interval(b)
+    assert not b.is_container_of_interval(a)
+
+def test_BoundedInterval_is_container_of_interval_08( ):
+    a = BoundedInterval(-2, 2)
+    b = BoundedInterval(0, 4)
+    assert not a.is_container_of_interval(b)
+    assert not b.is_container_of_interval(a)
+
+def test_BoundedInterval_is_container_of_interval_09( ):
+    a = BoundedInterval(-2, 2)
+    b = BoundedInterval(2, 6)
+    assert not a.is_container_of_interval(b)
+    assert not b.is_container_of_interval(a)
+
+def test_BoundedInterval_is_container_of_interval_10( ):
+    a = BoundedInterval(-2, 2)
+    b = BoundedInterval(3, 7)
+    assert not a.is_container_of_interval(b)
+    assert not b.is_container_of_interval(a)
