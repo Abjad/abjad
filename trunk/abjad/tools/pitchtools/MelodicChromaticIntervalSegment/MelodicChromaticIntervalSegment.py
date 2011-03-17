2aec21be42ee343e8988ce28937bbd56ad3a0470
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2011-03-17 00:48:25 +0000

    Added two new MelodicChromaticIntervalSegment public attributes: slope and spread.

--- trunk/abjad/tools/pitchtools/MelodicChromaticIntervalSegment/MelodicChromaticIntervalSegment.py
+++ trunk/abjad/tools/pitchtools/MelodicChromaticIntervalSegment/MelodicChromaticIntervalSegment.py
@@ -1,4 +1,6 @@
+from fractions import Fraction
 from abjad.tools.pitchtools._IntervalSegment import _IntervalSegment
+from abjad.tools.pitchtools.HarmonicChromaticInterval import HarmonicChromaticInterval
 from abjad.tools.pitchtools.MelodicChromaticInterval import MelodicChromaticInterval
 from abjad.tools.pitchtools.MelodicChromaticIntervalClassSegment import MelodicChromaticIntervalClassSegment
 from abjad.tools.pitchtools.MelodicChromaticIntervalClassVector import MelodicChromaticIntervalClassVector
@@ -45,3 +47,32 @@ class MelodicChromaticIntervalSegment(_IntervalSegment):
    @property
    def melodic_chromatic_interval_class_vector(self):
       return MelodicChromaticIntervalClassVector(self)
+
+   @property
+   def slope(self):
+      '''The slope of a melodic interval segment is the sum of its intervals
+      divided by its length ::
+
+         abjad> MelodicChromaticIntervalSegment([1, 2]).slope
+         Fraction(3, 2)
+      '''
+      return Fraction.from_float(sum([x.number for x in self])) / len(self)
+
+   @property
+   def spread(self):
+      '''The maximum harmonic interval spanned by any combination of the intervals within
+      a harmonic chromatic interval segment ::
+
+         abjad> MelodicChromaticIntervalSegment([1, 2, -3, 1, -2, 1]).spread
+         HarmonicChromaticInterval(4)
+         abjad> MelodicChromaticIntervalSegment([1, 1, 1, 2, -3, -2]).spread
+         HarmonicChromaticInterval(5)
+      '''
+      current = maximum = minimum = 0
+      for x in self.melodic_chromatic_interval_numbers:
+         current += x
+         if maximum < current:
+            maximum = current
+         if current < minimum:
+            minimum = current
+      return HarmonicChromaticInterval(maximum - minimum)
---
+++ trunk/abjad/tools/pitchtools/MelodicChromaticIntervalSegment/test/test_MelodicChromaticIntervalSegment_slope.py
@@ -0,0 +1,15 @@
+from fractions import Fraction
+from abjad.tools.pitchtools import MelodicChromaticIntervalSegment
+
+
+def test_MelodicChromaticIntervalSegment_slope_01( ):
+   mcis = MelodicChromaticIntervalSegment([1])
+   assert mcis.slope == 1
+
+def test_MelodicChromaticIntervalSegment_slope_02( ):
+   mcis = MelodicChromaticIntervalSegment([-2, 1])
+   assert mcis.slope == Fraction(-1, 2)
+
+def test_MelodicChromaticIntervalSegment_slope_03( ):
+   mcis = MelodicChromaticIntervalSegment([1, 2, -3, 0, 2, -1, -1.5, 0.5])
+   assert mcis.slope == 0
