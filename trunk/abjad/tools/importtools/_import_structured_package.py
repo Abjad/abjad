b76e8b6c0cf456eeba5e6346082c03ae3ac38387
Trevor Bača <trevorbaca@gmail.com>
2011-04-20 19:15:42 +0000

    Added importtools._import_structured_package( ) as a shorter alias of a longer import tool.

---
+++ trunk/abjad/tools/importtools/_import_structured_package.py
@@ -0,0 +1,8 @@
+from abjad.tools.importtools._import_public_names_from_path_into_namespace import _import_public_names_from_path_into_namespace
+
+
+def _import_structured_package(path, namespace):
+   '''Alias _import_public_names_from_path_into_namespace.
+   '''
+   
+   _import_public_names_from_path_into_namespace(path, namespace)
