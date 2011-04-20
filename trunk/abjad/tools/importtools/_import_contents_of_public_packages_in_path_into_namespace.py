7a66dd04bb05336ce5f3fc75b6edc1007899111a
Trevor Bača <trevorbaca@gmail.com>
2011-04-20 17:20:44 +0000

    Added new import tool:
       importtools._import_contents_of_public_packages_in_path_into_namespace( )
    
    The function makes it possible to build __init__.py files that dynamically load public classes.

---
+++ trunk/abjad/tools/importtools/_import_contents_of_public_packages_in_path_into_namespace.py
@@ -0,0 +1,26 @@
+from abjad.tools.importtools._get_public_names_in_module import _get_public_names_in_module
+import os
+
+
+def _import_contents_of_public_packages_in_path_into_namespace(
+   path, namespace, package_root_name = 'abjad'):
+   r'''Inspect the top level of path.
+
+   Find public class packages and import class package contents into namespace.
+
+   Do not inspect lower levels of path.
+   '''
+
+   parent_path = path[path.rindex(package_root_name):]
+   parent_package = parent_path.replace(os.sep, '.')
+
+   for name in os.listdir(path):
+      if os.path.isdir(os.path.join(path, name)):
+         if name[0].isupper( ):
+            class_package = '.'.join([parent_package, name])
+            class_module = '.'.join([class_package, name])
+            public_names = _get_public_names_in_module(class_module)
+            for public_name in public_names:
+               namespace[public_name.__name__] = public_name
+
+   del(namespace['_import_contents_of_public_packages_in_path_into_namespace'])
--- trunk/abjad/tools/seqtools/__init__.py
+++ trunk/abjad/tools/seqtools/__init__.py
@@ -1,8 +1,5 @@
 from abjad.tools.importtools._import_public_names_from_path_into_namespace import _import_public_names_from_path_into_namespace
+from abjad.tools.importtools._import_contents_of_public_packages_in_path_into_namespace import _import_contents_of_public_packages_in_path_into_namespace
 
 _import_public_names_from_path_into_namespace(__path__[0], globals( ))
-
-from CyclicList import CyclicList
-from CyclicMatrix import CyclicMatrix
-from CyclicTuple import CyclicTuple
-from Matrix import Matrix
+_import_contents_of_public_packages_in_path_into_namespace(__path__[0], globals( ))
