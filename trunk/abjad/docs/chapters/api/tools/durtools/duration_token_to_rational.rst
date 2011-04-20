950594549d80195639e9545485b3d668f07a5127
Trevor Bača <trevorbaca@gmail.com>
2011-04-20 21:38:35 +0000

    Added new durtools.duration_token_to_rational( ).
    Examples:
    
       abjad> durtools.duration_token_to_rational((4, 16))
       Fraction(1, 4)
    
       abjad> durtools.duration_token_to_rational('8.')
       Fraction(3, 16)

---
+++ trunk/abjad/docs/chapters/api/tools/durtools/duration_token_to_rational.rst
@@ -0,0 +1,4 @@
+durtools.duration_token_to_rational
+===================================
+
+.. autofunction:: abjad.tools.durtools.duration_token_to_rational
---
+++ trunk/abjad/tools/durtools/duration_token_to_rational.py
@@ -0,0 +1,21 @@
+from abjad.tools.durtools.duration_token_to_duration_pair import duration_token_to_duration_pair
+from fractions import Fraction
+
+
+def duration_token_to_rational(duration_token):
+   '''.. versionadded:: 1.1.2
+
+   Convert `duration_token` to rational::
+
+      abjad> durtools.duration_token_to_rational((4, 16))
+      Fraction(1, 4)
+
+   ::
+
+      abjad> durtools.duration_token_to_rational('4.')
+      Fraction(3, 8)
+
+   Return fraction.
+   '''
+
+   return Fraction(*duration_token_to_duration_pair(duration_token))
