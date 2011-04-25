34e02da5ffff7f6ca8fab3557d91a6016a853b81
Trevor Bača <trevorbaca@gmail.com>
2011-04-25 18:37:50 +0000

    Removed one cyclic list test and one cyclic tuple test.
    Both tests could cause memory errors when run under certain CPU architectures.

--- trunk/abjad/tools/seqtools/CyclicTuple/test/test_seqtools_CyclicTuple___getslice__.py
+++ trunk/abjad/tools/seqtools/CyclicTuple/test/test_seqtools_CyclicTuple___getslice__.py
@@ -9,11 +9,3 @@ def test_seqtools_CyclicTuple___getslice___01( ):
    assert cyclic_tuple[:2] == (0, 1)
    assert cyclic_tuple[:10] == (0, 1, 2, 0, 1, 2, 0, 1, 2, 0)
    assert cyclic_tuple[2:10] == (2, 0, 1, 2, 0, 1, 2, 0)
-
-
-def test_seqtools_CyclicTuple___getslice___02( ):
-
-   cyclic_tuple = seqtools.CyclicTuple(range(3))
-
-   assert py.test.raises(MemoryError, 'cyclic_tuple[:]')
-   assert py.test.raises(MemoryError, 'cyclic_tuple[2:]')
