6c4b908bff03758b52dacf77b7d9dac219ce2e4c
Trevor Bača <trevorbaca@gmail.com>
2011-05-03 21:58:20 +0000

    Extended instrument tools transposition functions to set pitch_indication_is_at_sounding_pitch as appropriate.

--- trunk/abjad/tools/instrumenttools/test/test_instrumenttools_transpose_leaves_in_expr_from_fingered_pitch_to_sounding_pitch.py
+++ trunk/abjad/tools/instrumenttools/test/test_instrumenttools_transpose_leaves_in_expr_from_fingered_pitch_to_sounding_pitch.py
@@ -5,6 +5,10 @@ def test_instrumenttools_transpose_leaves_in_expr_from_fingered_pitch_to_soundin
 
    staff = Staff("<c' e' g'>4 d'4 r4 e'4")
    instrumenttools.Clarinet( )(staff)
+
+   for leaf in staff.leaves:
+      leaf.pitch_indication_is_at_sounding_pitch = False
+
    instrumenttools.transpose_leaves_in_expr_from_fingered_pitch_to_sounding_pitch(staff)
 
    r'''
@@ -17,5 +21,9 @@ def test_instrumenttools_transpose_leaves_in_expr_from_fingered_pitch_to_soundin
       d'4
    }
    '''
+
+   for leaf in staff.leaves:
+      if isinstance(leaf, (Note, Chord)):
+         assert leaf.pitch_indication_is_at_sounding_pitch
 
    assert staff.format == "\\new Staff {\n\t\\set Staff.instrumentName = \\markup { Clarinet }\n\t\\set Staff.shortInstrumentName = \\markup { Cl. }\n\t<bf d' f'>4\n\tc'4\n\tr4\n\td'4\n}"
--- trunk/abjad/tools/instrumenttools/test/test_instrumenttools_transpose_leaves_in_expr_from_sounding_pitch_to_fingered_pitch.py
+++ trunk/abjad/tools/instrumenttools/test/test_instrumenttools_transpose_leaves_in_expr_from_sounding_pitch_to_fingered_pitch.py
@@ -5,6 +5,11 @@ def test_instrumenttools_transpose_leaves_in_expr_from_sounding_pitch_to_fingere
 
    staff = Staff("<c' e' g'>4 d'4 r4 e'4")
    instrumenttools.Clarinet( )(staff)
+
+   for leaf in staff.leaves:
+      if isinstance(leaf, (Note, Chord)):
+         assert leaf.pitch_indication_is_at_sounding_pitch
+
    instrumenttools.transpose_leaves_in_expr_from_sounding_pitch_to_fingered_pitch(staff)
 
    r'''
@@ -17,5 +22,9 @@ def test_instrumenttools_transpose_leaves_in_expr_from_sounding_pitch_to_fingere
       fs'4
    }
    '''
+
+   for leaf in staff.leaves:
+      if isinstance(leaf, (Note, Chord)):
+         assert not leaf.pitch_indication_is_at_sounding_pitch
 
    assert staff.format == "\\new Staff {\n\t\\set Staff.instrumentName = \\markup { Clarinet }\n\t\\set Staff.shortInstrumentName = \\markup { Cl. }\n\t<d' fs' a'>4\n\te'4\n\tr4\n\tfs'4\n}"
