fa03f55725acaf91bac6961df859cce6febf2a24
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2011-05-03 19:10:49 +0000

    Created bridge clef stencil in abjad.scm, cleaned up abjad.scm.
    To use:
    
    abjad> staff = Staff(macros.scale(8))
    abjad> stencil = marktools.LilyPondCommandMark('bridgeClefStencil')
    abjad> contexttools.ClefMark('percussion')(staff)
    abjad> staff.override.clef.stencil = stencil
    abjad> f(staff)
    \new Staff \with {
        \override Clef #'stencil = \bridgeClefStencil
    } {
        \clef "percussion"
        c'8
        d'8
        e'8
        f'8
        g'8
        a'8
        b'8
        c''8
    }
    
    Changing the stencil does not affect middle-c positioning, so that must be applied separately.

--- trunk/abjad/cfg/abjad.scm
+++ trunk/abjad/cfg/abjad.scm
@@ -1,49 +1,65 @@
 beam = #(define-music-function (parser location left right) (number? number?)
-      #{
-         \set stemLeftBeamCount = #$left
-         \set stemRightBeamCount = #$right
-      #})
+   #{
+      \set stemLeftBeamCount = #$left
+      \set stemRightBeamCount = #$right
+   #})
+
 
 fraction = #(define-music-function (parser location music) (ly:music?)
-	#{ \tweak #'text #tuplet-number::calc-fraction-text $music #})
+   #{
+      \tweak #'text #tuplet-number::calc-fraction-text $music
+   #})
+
 
 white = #(define-music-function (parser location music) (ly:music?)
-	#{ \once \override NoteHead #'duration-log = #1 $music #})
+   #{
+      \once \override NoteHead #'duration-log = #1 $music
+   #})
+
 
 transparent = #(define-music-function (parser location music) (ly:music?)
-	#{ \once \override NoteHead #'transparent = ##t 
+   #{
+      \once \override NoteHead #'transparent = ##t
       \once \override NoteHead #'no-ledgers = ##t
       \once \override Stem #'transparent = ##t
       \once \override Dots #'transparent = ##t
-      $music #})
+      $music
+   #})
+
 
 headless = #(define-music-function (parser location music) (ly:music?)
-	#{ \once \override NoteHead #'transparent = ##t 
+   #{
+      \once \override NoteHead #'transparent = ##t 
       \once \override NoteHead #'no-ledgers = ##t
-      $music #})
+      $music
+   #})
+
 
 whichContext = #(define-music-function (parser location) ( )
-                (make-music 'ApplyContext
-                 'origin location
-                 'procedure (
-                   lambda (c)
-                   (display
-                    (string-append
-                     "\nCurrent voice is "
-                     (ly:context-id c)
-                     "\n")))))
+   (make-music 'ApplyContext
+               'origin location
+               'procedure
+      (lambda (c)
+         (display
+            (string-append
+               "\nCurrent voice is "
+               (ly:context-id c)
+               "\n")))))
+
 
 locationWhichContext = #(define-music-function (parser location) ( )
-                (make-music 'ApplyContext
-                 'origin location
-                 'procedure (
-                   lambda (c)
-                   (display
-                    (ly:input-message location
-                     "current voice is ~a"
-                     (ly:context-id c))))))
+   (make-music 'ApplyContext
+               'origin location
+               'procedure 
+      (lambda (c)
+         (display
+            (ly:input-message location
+               "current voice is ~a"
+               (ly:context-id c))))))
+
 
 #(define lastNoteHeadWidth 0)
+
 
 #(define (centerTextFn grob grob-origin context)    
    '''From Michael Lauer on LilyPond user list.'''
@@ -56,7 +72,9 @@ locationWhichContext = #(define-music-function (parser location) ( )
                (offset (* (- lastNoteHeadWidth (car xext) (cdr xext)) 0.5)))
             (ly:grob-set-property! grob 'X-offset offset)))))
 
+
 centerText = \applyOutput #'Voice #centerTextFn
+
 
 %% TODO: Parameterize x-value extra offset or take from global staff size %%
 adjustEOLMeterBarlineExtraOffset = #(define-music-function (parser location) ( )
@@ -71,3 +89,23 @@ adjustEOLMeterBarlineExtraOffset = #(define-music-function (parser location) ( )
       \once \override Score.BarLine #'after-line-breaking = #bar
       \once \override Score.SpanBar #'after-line-breaking = #bar
    #})
+
+
+bridgeClefStencil =
+    #(ly:make-stencil
+        `(path 0.2
+            `(rmoveto 0 3.5
+            rlineto 0 -7
+            rmoveto 0.5 7
+            rlineto 0 -7
+            rmoveto 0.5 7
+            rlineto 0 -7
+            rmoveto 0.5 7
+            rlineto 0 -7
+            rlineto 0.5 4.5
+            rlineto -2.5 0
+            rlineto 0.5 -4.5
+            rmoveto -1 6
+            rcurveto 1 0.5 2.5 0.5 3.5 0))
+        (cons -1 1)
+        (cons -3.5 3.5))
