b87256f808872674046cac5c909f4bc64461f8b4
Trevor Bača <trevorbaca@gmail.com>
2011-05-04 18:13:18 +0000

    Extended voice with new 'is_nonsemantic' boolean flag.
    The intent of this read / write attribute is to allow composers to tag invisible
    voices used to house time signatures indications, bar number directives or other 
    pieces of score-global non-musical information. Such nonsemantic voices can then
    be omitted from voice interation and other functions.

--- trunk/abjad/components/Voice/Voice.py
+++ trunk/abjad/components/Voice/Voice.py
@@ -16,9 +16,71 @@ class Voice(_Context):
       }
    '''
 
-   __slots__ = ( )
+   __slots__ = ('_is_nonsemantic', )
 
    def __init__(self, music = None, **kwargs):
       _Context.__init__(self, music)
       self.context = 'Voice'
       self._initialize_keyword_values(**kwargs)
+      self.is_nonsemantic = False
+
+   ## PUBLIC ATTRIBUTES ##
+
+   @apply
+   def is_nonsemantic( ):
+      def fget(self):
+         r'''Set indicator of nonsemantic voice::
+
+            abjad> measures = measuretools.make_measures_with_full_measure_spacer_skips([(1, 8), (5, 16), (5, 16))
+            abjad> voice = Voice(measures)
+            abjad> voice.name = 'HiddenTimeSignatureVoice'
+
+         ::
+
+            abjad> voice.is_nonsemantic = True
+
+         ::
+
+            abjad> f(voice)
+            \context Voice = "HiddenTimeSignatureVoice" {
+               {
+                  \time 1/8
+                  s1 * 1/8
+               }
+               {
+                  \time 5/16
+                  s1 * 5/16
+               }
+               {
+                  \time 5/16
+                  s1 * 5/16
+               }
+            }
+      
+         ::
+
+            abjad> voice.is_nonsemantic
+            True
+
+         Get indicator of nonsemantic voice::
+
+            abjad> voice = Voice([ ])
+
+         ::
+
+            abjad> voice.is_nonsemantic
+            False
+
+         Return boolean.
+
+         The intent of this read / write attribute is to allow composers to tag invisible
+         voices used to house time signatures indications, bar number directives or other 
+         pieces of score-global non-musical information. Such nonsemantic voices can then
+         be omitted from voice interation and other functions.
+         '''
+         return self._is_nonsemantic
+      def fset(self, arg):
+         if not isinstance(arg, type(True)):
+            raise TypeError
+         self._is_nonsemantic = arg
+      return property(**locals( ))
---
+++ trunk/abjad/components/Voice/test/test_Voice_is_nonsemantic.py
@@ -0,0 +1,23 @@
+from abjad import *
+import py.test
+
+
+def test_Voice_is_nonsemantic_01( ):
+
+   voice = Voice([ ])
+   assert not voice.is_nonsemantic
+
+
+def test_Voice_is_nonsemantic_02( ):
+
+   voice = Voice([ ])
+   voice.is_nonsemantic = True
+
+   assert voice.is_nonsemantic
+
+
+def test_Voice_is_nonsemantic_03( ):
+
+   voice = Voice([ ])
+
+   assert py.test.raises(TypeError, "voice.is_nonsemantic = 'foo'")
