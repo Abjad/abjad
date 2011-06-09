b62d7f4357b925925d887d067bb08ea50e91c8ab
Trevor Bača <trevorbaca@gmail.com>
2011-06-09 18:21:01 +0000

    Added tools/iotools/_update_docs_release_string.py module to update docs release string at the conclusion of every abj-update. Result is that HTML or LaTeX docs built after update will reflect version string (like r4416) correctly.

---
+++ trunk/abjad/tools/iotools/_update_docs_release_string.py
@@ -0,0 +1,20 @@
+from abjad.cfg.cfg import ABJADPATH
+from abjad.tools import cfgtools
+import os
+
+
+def _update_docs_release_string( ):
+   docs_conf_file_name = os.path.join(ABJADPATH, 'docs', 'conf.py')
+   docs_conf_file = file(docs_conf_file_name, 'r')
+   output_lines = [ ]
+   for input_line in docs_conf_file.readlines( ):
+      if input_line.startswith('release ='):
+         abjad_revision_string = cfgtools.get_abjad_revision_string( )
+         output_lines.append("release = '%s'\n" % abjad_revision_string)
+      else:
+         output_lines.append(input_line)
+   docs_conf_file.close( )
+   docs_conf_file = file(docs_conf_file_name, 'w')
+   output_lines = ''.join(output_lines)
+   docs_conf_file.write(output_lines)
+   docs_conf_file.close( )
--- trunk/abjad/tools/iotools/_write_abjad_version_file.py
+++ trunk/abjad/tools/iotools/_write_abjad_version_file.py
@@ -2,8 +2,6 @@ from abjad.cfg.cfg import ABJADVERSIONFILE
 import os
 
 
-## TODO: Replace call to os.popen( ) with subprocess ##
-
 def _write_abjad_version_file( ):
    version = os.popen('svnversion').read( ).strip( )
    file(ABJADVERSIONFILE, 'w').write(version)
