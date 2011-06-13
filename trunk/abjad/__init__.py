01d9d45c42465b25f921f1d5b5ce455e6a3afefc
Trevor Bača <trevorbaca@gmail.com>
2011-06-13 23:22:47 +0000

    Cleaned up abjad/__init__.py.

--- trunk/abjad/__init__.py
+++ trunk/abjad/__init__.py
@@ -1,35 +1,32 @@
 from abjad.tools.importtools._import_functions_in_package_to_namespace import _import_functions_in_package_to_namespace
-import os
-
-## import exceptions
-_exceptions_path = os.path.join(__path__[0], 'exceptions')
-_import_functions_in_package_to_namespace(_exceptions_path, __builtins__)
-
-from fractions import Fraction
-from abjad.tools.durtools import Duration
-from components import *
+from abjad.components import *
 from abjad.tools import *
-del cfgtools
-del layouttools
-del pitcharraytools
-del sievetools
-del tonalitytools
-import macros
-
-## import tools and io
+from abjad.tools.durtools import Duration
 from abjad.tools.iotools import f
 from abjad.tools.iotools import play
 from abjad.tools.iotools import show
-
+from fractions import Fraction
+import macros
+import os
 import sys
+
+## import Abjad exceptions in Python __builtins__ namespace
+_exceptions_path = os.path.join(__path__[0], 'exceptions')
+_import_functions_in_package_to_namespace(_exceptions_path, __builtins__)
+
 sys.ps1 = 'abjad> '
-del sys
-del os
 
 del cfg
+del cfgtools
 del components
 del core
-del importtools
 del exceptions
+del importtools
 del interfaces
+del layouttools
+del os
+del pitcharraytools
+del sievetools
+del sys
+del tonalitytools
 del tools
