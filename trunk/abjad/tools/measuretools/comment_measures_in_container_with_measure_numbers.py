b3a11a16d87a504f27619e9e4a93b7226e00bf2b
Trevor Bača <trevorbaca@gmail.com>
2011-06-26 22:38:59 +0000

    Fixed measuretools.comment_measures_in_container_with_measure_numbers( ).

--- trunk/abjad/tools/measuretools/comment_measures_in_container_with_measure_numbers.py
+++ trunk/abjad/tools/measuretools/comment_measures_in_container_with_measure_numbers.py
@@ -1,18 +1,10 @@
-from abjad.tools.containertools.Container import Container
-from abjad.tools.measuretools.Measure import Measure
-
-
-def comment_measures_in_container_with_measure_numbers(container, style = 'comment'):
-   r'''Label measure numbers in `container` according to `style`.
-
-   .. note:: functionality current not implemented.
-
-   Turn measure number labels on with ``style = 'comment'``. ::
+def comment_measures_in_container_with_measure_numbers(container):
+   r'''Comment measures in `container` with measure numbers::
 
       abjad> staff = Staff(Measure((2, 8), notetools.make_repeated_notes(2)) * 3)
       abjad> pitchtools.set_ascending_named_diatonic_pitches_on_nontied_pitched_components_in_expr(staff)
-      abjad> measuretools.comment_measures_in_container_with_measure_numbers(staff, style = 'comment') # doctest: +SKIP
-      abjad> f(staff) # doctest: +SKIP
+      abjad> measuretools.comment_measures_in_container_with_measure_numbers(staff)
+      abjad> f(staff)
       \new Staff {
               % start measure 1
               {
@@ -37,34 +29,13 @@ def comment_measures_in_container_with_measure_numbers(container, style = 'comme
               % stop measure 3
       }
 
-   Turn measure number labels off with ``style = None``. ::
-
-      abjad> measuretools.comment_measures_in_container_with_measure_numbers(staff, style = None) # doctest: +SKIP
-      abjad> f(staff) # doctest: +SKIP
-      \new Staff {
-              {
-                      \time 2/8
-                      c'8
-                      d'8
-              }
-              {
-                      \time 2/8
-                      e'8
-                      f'8
-              }
-              {
-                      \time 2/8
-                      g'8
-                      a'8
-              }
-      }
-
    .. versionchanged:: 1.1.2
       renamed ``label.measure_numbers( )`` to
       ``measuretools.comment_measures_in_container_with_measure_numbers( )``.
    '''
+   from abjad.tools import marktools
+   from abjad.tools import measuretools
 
-   if isinstance(container, Measure):
-      container._formatter.number.self = style
-   else:
-      container._formatter.number.measures = style
+   for measure in measuretools.iterate_measures_forward_in_expr(container):
+      marktools.Comment('start measure %s' % measure.measure_number, 'before')(measure)
+      marktools.Comment('stop measure %s' % measure.measure_number, 'after')(measure)   
--- trunk/abjad/tools/measuretools/test/test_measuretools_comment_measures_in_container_with_measure_numbers.py
+++ trunk/abjad/tools/measuretools/test/test_measuretools_comment_measures_in_container_with_measure_numbers.py
@@ -1,16 +1,13 @@
 from abjad import *
-import py.test
-py.test.skip('fix numbering after update reimplementation.')
 
 
 def test_measuretools_comment_measures_in_container_with_measure_numbers_01( ):
-   '''Label measure numbers with comments before and after
-   each measure.
+   '''Label measure numbers with comments before and after each measure.
    '''
 
-   t = Staff(Measure((2, 8), notetools.make_repeated_notes(2)) * 3)
-   pitchtools.set_ascending_named_diatonic_pitches_on_nontied_pitched_components_in_expr(t)
-   measuretools.comment_measures_in_container_with_measure_numbers(t, 'comment')
+   staff = Staff(Measure((2, 8), notetools.make_repeated_notes(2)) * 3)
+   pitchtools.set_ascending_named_diatonic_pitches_on_nontied_pitched_components_in_expr(staff)
+   measuretools.comment_measures_in_container_with_measure_numbers(staff)
 
    r'''
    \new Staff {
@@ -38,46 +35,17 @@ def test_measuretools_comment_measures_in_container_with_measure_numbers_01( ):
    }
    '''
 
-   assert t.format == "\\new Staff {\n\t% start measure 1\n\t{\n\t\t\\time 2/8\n\t\tc'8\n\t\td'8\n\t}\n\t% stop measure 1\n\t% start measure 2\n\t{\n\t\t\\time 2/8\n\t\te'8\n\t\tf'8\n\t}\n\t% stop measure 2\n\t% start measure 3\n\t{\n\t\t\\time 2/8\n\t\tg'8\n\t\ta'8\n\t}\n\t% stop measure 3\n}"
+   assert componenttools.is_well_formed_component(staff)
+   assert staff.format == "\\new Staff {\n\t% start measure 1\n\t{\n\t\t\\time 2/8\n\t\tc'8\n\t\td'8\n\t}\n\t% stop measure 1\n\t% start measure 2\n\t{\n\t\t\\time 2/8\n\t\te'8\n\t\tf'8\n\t}\n\t% stop measure 2\n\t% start measure 3\n\t{\n\t\t\\time 2/8\n\t\tg'8\n\t\ta'8\n\t}\n\t% stop measure 3\n}"
 
 
 def test_measuretools_comment_measures_in_container_with_measure_numbers_02( ):
-   '''Turn measure number labelling off with None.'''
-
-   t = Staff(Measure((2, 8), notetools.make_repeated_notes(2)) * 3)
-   pitchtools.set_ascending_named_diatonic_pitches_on_nontied_pitched_components_in_expr(t)
-   measuretools.comment_measures_in_container_with_measure_numbers(t, 'comment')
-   measuretools.comment_measures_in_container_with_measure_numbers(t, None)
-
-   r'''
-   \new Staff {
-           {
-                   \time 2/8
-                   c'8
-                   d'8
-           }
-           {
-                   \time 2/8
-                   e'8
-                   f'8
-           }
-           {
-                   \time 2/8
-                   g'8
-                   a'8
-           }
-   }
+   '''Works on measures, too, in addition to contexts.
    '''
 
-   assert t.format == "\\new Staff {\n\t{\n\t\t\\time 2/8\n\t\tc'8\n\t\td'8\n\t}\n\t{\n\t\t\\time 2/8\n\t\te'8\n\t\tf'8\n\t}\n\t{\n\t\t\\time 2/8\n\t\tg'8\n\t\ta'8\n\t}\n}"
-
-
-def test_measuretools_comment_measures_in_container_with_measure_numbers_03( ):
-   '''Works on measures, too, in addition to contexts.'''
-
-   t = Staff(Measure((2, 8), notetools.make_repeated_notes(2)) * 3)
-   pitchtools.set_ascending_named_diatonic_pitches_on_nontied_pitched_components_in_expr(t)
-   measuretools.comment_measures_in_container_with_measure_numbers(t[1], 'comment')
+   staff = Staff(Measure((2, 8), notetools.make_repeated_notes(2)) * 3)
+   pitchtools.set_ascending_named_diatonic_pitches_on_nontied_pitched_components_in_expr(staff)
+   measuretools.comment_measures_in_container_with_measure_numbers(staff[1])
 
    r'''
    \new Staff {
@@ -101,5 +69,5 @@ def test_measuretools_comment_measures_in_container_with_measure_numbers_03( ):
    }
    '''
 
-   assert componenttools.is_well_formed_component(t)
-   assert t.format == "\\new Staff {\n\t{\n\t\t\\time 2/8\n\t\tc'8\n\t\td'8\n\t}\n\t% start measure 2\n\t{\n\t\t\\time 2/8\n\t\te'8\n\t\tf'8\n\t}\n\t% stop measure 2\n\t{\n\t\t\\time 2/8\n\t\tg'8\n\t\ta'8\n\t}\n}"
+   assert componenttools.is_well_formed_component(staff)
+   assert staff.format == "\\new Staff {\n\t{\n\t\t\\time 2/8\n\t\tc'8\n\t\td'8\n\t}\n\t% start measure 2\n\t{\n\t\t\\time 2/8\n\t\te'8\n\t\tf'8\n\t}\n\t% stop measure 2\n\t{\n\t\t\\time 2/8\n\t\tg'8\n\t\ta'8\n\t}\n}"
