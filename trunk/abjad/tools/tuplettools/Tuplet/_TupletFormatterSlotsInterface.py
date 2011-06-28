59dc88b2dcf9c362d2ad9b388b43237243ea79a0
Trevor Bača <trevorbaca@gmail.com>
2011-06-28 19:46:38 +0000

    Removed format-time tuplet coloring.
    (Simplifies tuplet format code.)
    
    Color tuplets at composition time instead.

--- trunk/abjad/tools/tuplettools/Tuplet/_TupletFormatterSlotsInterface.py
+++ trunk/abjad/tools/tuplettools/Tuplet/_TupletFormatterSlotsInterface.py
@@ -11,36 +11,6 @@ class _TupletFormatterSlotsInterface(_ContainerFormatterSlotsInterface):
 
    @property
    def slot_1(self):
-      r'''Read-only tuple of format contributions
-      to appear before tuplet opening.
-
-      There are four possible types of these and they appear
-      in the following canonic order:
-
-      *  User comments to come before tuplet opening
-      *  Interface overrides
-      *  Tuplet coloring
-
-      Newly created, unmodified tuplets make no format
-      contributions to slot 1. ::
-
-         abjad> t = tuplettools.FixedDurationTuplet(Duration(2, 8), "c'8 d'8 e'8")
-         abjad> import pprint
-         abjad> pprint.pprint(t._formatter.slots.slot_1)
-         ([('comment_marks', ''), []],
-          [('lilypond_command_marks', ''), []],
-          [('overrides', 'overrides'), []])
-
-         abjad> print t.format
-         \times 2/3 {
-                 c'8
-                 d'8
-                 e'8
-         }
-
-      Modified tuplets, on the other hand, may make
-      format contributions to slot 1.
-      '''
       from abjad.tools.marktools._get_comment_format_contributions_for_slot import _get_comment_format_contributions_for_slot
       from abjad.core.LilyPondGrobOverrideComponentPlugIn._get_grob_override_format_contributions import _get_grob_override_format_contributions
       from abjad.core.LilyPondGrobOverrideComponentPlugIn._get_grob_revert_format_contributions import _get_grob_revert_format_contributions
@@ -50,73 +20,10 @@ class _TupletFormatterSlotsInterface(_ContainerFormatterSlotsInterface):
       result.append(_get_comment_format_contributions_for_slot(tuplet, 'before'))
       result.append(_get_lilypond_command_mark_format_contributions_for_slot(tuplet, 'before'))
       result.append(_get_grob_override_format_contributions(self._client._client))
-      if tuplet.duration.multiplier == 1 and \
-         hasattr(tuplet.__class__, 'color'):
-         contributor = (tuplet.__class__, 'color')
-         contributions = [r"\tweak #'color #blue"]
-         result.append([contributor, contributions])
       return tuple(result)
 
    @property
    def slot_2(self):
-      r'''Read-only tuple of format contributions used
-      to generate tuplet opening.
-
-      Most tuplets open with the LilyPond ``\times`` command only. ::
-
-         abjad> t = tuplettools.FixedDurationTuplet(Duration(2, 8), "c'8 d'8 e'8")
-         abjad> print t.format
-         \times 2/3 {
-                 c'8
-                 d'8
-                 e'8
-         }
-
-      This is apparent in the contents of the slot 2. ::
-
-         abjad> pprint.pprint(t._formatter.slots.slot_2)
-         ([('tuplet_brackets', 'open'), ['\\times 2/3 {']],)
-
-      Trivial tuplets carry a ratio of ``1:1``. ::
-
-         abjad> t = tuplettools.FixedDurationTuplet(Duration(3, 8), "c'8 d'8 e'8")
-         abjad> t.duration.multiplier
-         Duration(1, 1)
-
-      Such tuplets output unscaled notes and rests.
-
-      For this reason, trivial tuplets make no 
-      format contributions to slot 2.
-
-      You can make trivial tuplets format as actual tuplets
-      by setting ``color = True`` on the class of
-      the tuplet in question.
-      Colored trivial tuplets appear blue in printed output
-
-      Note that ``color`` is a tuplet class attribute,
-      not a tuplet instance attribute.
-   
-      Finally, settings ``t.is_invisible = True`` causes the tuplet
-      to output with the LilyPond ``\scaleDurations`` command
-      instead of the the Lilypond ``\times`` command. ::
-
-         abjad> t = tuplettools.FixedDurationTuplet(Duration(2, 8), "c'8 d'8 e'8")
-         abjad> t.is_invisible = True
-         abjad> print t.format
-         \scaleDurations #'(2 . 3) {
-                 c'8
-                 d'8
-                 e'8
-         }
-         abjad> pprint.pprint(t._formatter.slots.slot_2)
-         ([(FixedDurationTuplet(1/4, [c'8, d'8, e'8]), 'is_invisible'),
-           ["\\scaleDurations #'(2 . 3) {"]],)
-
-      Invisible tuplets carry neither tuplet bracket nor
-      tuplet ratio in the printed output.
-
-      Return list.
-      '''
       result = [ ]
       formatter = self.formatter
       tuplet = formatter.tuplet
@@ -128,10 +35,8 @@ class _TupletFormatterSlotsInterface(_ContainerFormatterSlotsInterface):
             contributions = [r"\scaleDurations #'(%s . %s) {" % (n, d)]
             result.append([contributor, contributions])
          else:
-            #contributor = (tuplet.brackets, 'open')
             contributor = ('tuplet_brackets', 'open')
-            if tuplet.duration.multiplier != 1 or \
-               hasattr(tuplet.__class__, 'color'):
+            if tuplet.duration.multiplier != 1:
                contributions = [r'%s\times %s %s' % (
                   formatter._fraction, 
                   tuplet.duration._multiplier_fraction_string,
@@ -144,8 +49,7 @@ class _TupletFormatterSlotsInterface(_ContainerFormatterSlotsInterface):
 
    @property
    def slot_3(self):
-      '''Read-only tuple of format contributions to appear
-      immediately after tuplet opening.
+      '''Read-only tuple of format contributions to appear immediately after tuplet opening.
       '''
       from abjad.tools.marktools._get_comment_format_contributions_for_slot import _get_comment_format_contributions_for_slot
       from abjad.core.LilyPondGrobOverrideComponentPlugIn._get_grob_override_format_contributions import _get_grob_override_format_contributions
@@ -160,8 +64,7 @@ class _TupletFormatterSlotsInterface(_ContainerFormatterSlotsInterface):
 
    @property
    def slot_5(self):
-      '''Read-only tuple of format contributions to appear
-      immediately before tuplet closing.
+      '''Read-only tuple of format contributions to appear immediately before tuplet closing.
       '''
       from abjad.tools.marktools._get_comment_format_contributions_for_slot import _get_comment_format_contributions_for_slot
       from abjad.core.LilyPondGrobOverrideComponentPlugIn._get_grob_override_format_contributions import _get_grob_override_format_contributions
@@ -176,9 +79,7 @@ class _TupletFormatterSlotsInterface(_ContainerFormatterSlotsInterface):
 
    @property
    def slot_6(self):
-      '''Read-only tuplet of format contributions used
-      to generate tuplet closing.
-      Usually just a single ``}`` close brace.
+      '''Read-only tuplet of format contributions used to generate tuplet closing.
       '''
       result = [ ]
       tuplet = self.formatter.tuplet
@@ -188,8 +89,7 @@ class _TupletFormatterSlotsInterface(_ContainerFormatterSlotsInterface):
 
    @property
    def slot_7(self):
-      '''Read-only tuple of format contributions
-      to appear immediately after tuplet closing.
+      '''Read-only tuple of format contributions to appear immediately after tuplet closing.
       '''
       from abjad.tools.marktools._get_comment_format_contributions_for_slot import _get_comment_format_contributions_for_slot
       from abjad.core.LilyPondGrobOverrideComponentPlugIn._get_grob_override_format_contributions import _get_grob_override_format_contributions
