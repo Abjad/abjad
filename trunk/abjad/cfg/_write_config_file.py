3ecf4bf2be15daf168b73e0611e38fe90e10ff7f
Trevor Bača <trevorbaca@gmail.com>
2011-08-23 18:10:59 +0000

    Reordered the way in which Abjad determines the value of the HOME environment variable.
    The check used to be HOME then APPDATA then HOMEPATH.
    
    The check is now HOME then HOMEPATH then APPDATA.
    
    Explanation: the change was made to help py.test run correctly under Windows.
    
    Tests all continue to run under OS X.

--- trunk/abjad/cfg/_write_config_file.py
+++ trunk/abjad/cfg/_write_config_file.py
@@ -1,6 +1,9 @@
-from abjad.cfg.cfg import ABJADCONFIG, ABJADPATH, HOME
+from abjad.cfg.cfg import ABJADCONFIG
+from abjad.cfg.cfg import ABJADPATH
+from abjad.cfg.cfg import HOME
 import os
 import time
+
 
 def _write_config_file(path, dict):
 
@@ -9,7 +12,7 @@ def _write_config_file(path, dict):
     preamble += '# Abjad configuration file, created by Abjad on %s.\n' % \
         time.strftime("%d %B %Y %H:%M:%S")
     preamble += '#\n'
-    preamble += '# This file is Python execfile( )d and should thus follow\n'
+    preamble += '# This file is Python execfile()d and should thus follow\n'
     preamble += "# Python's syntax.\n"
     preamble += '\n\n'
     preamble += "# Configuration Variables ---------------------------------"
--- trunk/abjad/cfg/cfg.py
+++ trunk/abjad/cfg/cfg.py
@@ -1,7 +1,18 @@
 import os
 
 
-HOME = os.environ.get('HOME') or os.environ.get('APPDATA') or os.environ.get('HOMEPATH')
+# Python evaluates the clauses in an or-statement one at a time from left to right.
+# Python returns true on the first clause that evaluates to true, or false if none eval to true.
+# So the order of the cascade here is set to HOME then HOMEPATH then APPDATA.
+# The reason for this is that HOME should be defined for all OS X and Linux distros.
+# Windows will not define HOME but will usually define both HOMEPATH and APPDATA.
+# But note that Windows usually defines HOMEPATH and APPDATA differently.
+# So hand testing between Windows and OS X.
+# Windows appears to set HOMEPATH similarly to the way OS X sets HOME.
+# As a final fallback, accept the setting of APPDATA under Windows.
+#HOME = os.environ.get('HOME') or os.environ.get('APPDATA') or os.environ.get('HOMEPATH')
+HOME = os.environ.get('HOME') or os.environ.get('HOMEPATH') or os.environ.get('APPDATA')
+
 ABJADCONFIG = os.path.join(HOME, '.abjad', 'config.py')
 ABJADPATH = os.path.abspath(os.path.dirname(__file__).rstrip('cfg'))
 ABJADVERSIONFILE = os.path.join(ABJADPATH, '.version')
