1299d55b3be1088fced236380cbc088d11e14f7f
Trevor Bača <trevorbaca@gmail.com>
2011-08-23 17:48:15 +0000

    Updated iotools.log().
    Helper now calls operating system-specific text editor 
    (and so should work correctly under Windows).

--- trunk/abjad/tools/iotools/log.py
+++ trunk/abjad/tools/iotools/log.py
@@ -1,11 +1,10 @@
+from abjad.cfg._get_text_editor import _get_text_editor
 from abjad.cfg._read_config_file import _read_config_file
 import os
 
 
-### TODO: Call operating-specific view program instead of vi. ###
-
 def log():
-    '''Open the LilyPond log file in the ``vi``::
+    '''Open the LilyPond log file in operating system-specific text editor::
 
         abjad> iotools.log() ### doctest: +SKIP
 
@@ -22,10 +21,12 @@ def log():
         Layout output to `0440.ps'...
         Converting to `./0440.pdf'...
 
-    Exit ``vi`` in the usual way with ``:q`` or equivalent to return to the Abjad interpreter.
+    Exit text editor in the usual way.
 
     Return none.
     '''
 
     ABJADOUTPUT = _read_config_file()['abjad_output']
-    os.system('vi %s' % os.path.join(ABJADOUTPUT, 'lily.log'))
+    text_editor = _get_text_editor()
+    command = '%s %s' % (text_editor, os.path.join(ABJADOUTPUT, 'lily.log'))
+    os.system(command)
