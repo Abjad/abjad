f39134e259548fb6a6395c9c5feac8091c60d09b
Trevor Bača <trevorbaca@gmail.com>
2011-08-30 13:23:55 +0000

    Built release 2.2.

--- trunk/setup.py
+++ trunk/setup.py
@@ -7,7 +7,7 @@ use_setuptools( ) ### this must come before setup import
 from setuptools import setup, find_packages
 
 setup(name = 'Abjad', 
-    version = '2.1',
+    version = '2.2',
     description = 'Abjad is a Python API for Formalized Score Control.',
     long_description = 'Abjad is an interactive software system designed to help composers build up complex pieces of music notation in an iterative and incremental way. Use Abjad to create a symbolic representation of all the notes, rests, staves, tuplets, beams and slurs in any score. Because Abjad extends the Python programming language, you can use Abjad to make systematic changes to your music as you work. And because Abjad wraps the powerful LilyPond music notation package, you can use Abjad to control the typographic details of all the symbols on the page.',
     author = 'Victor Adan and Trevor Baca',
