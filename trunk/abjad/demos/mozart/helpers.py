6821ac71f441e92a236fa9f084da510cd5726f95
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2011-09-08 05:06:01 +0000

    Improved Mozart demo.

--- trunk/abjad/demos/mozart/helpers.py
+++ trunk/abjad/demos/mozart/helpers.py
@@ -3,7 +3,7 @@ from abjad.demos.mozart.measures import measures
 import random
 
 
-def choose_measures( ):
+def choose_mozart_measures( ):
     chosen_measures = [ ]
     for i, choices in enumerate(measures):
         if i == 7: # get both alternative endings
@@ -14,25 +14,25 @@ def choose_measures( ):
     return chosen_measures    
 
 
-def build_measure(measure_dict):
+def build_one_mozart_measure(measure_dict):
     # parse the contents of a measure definition dictionary
     treble = iotools.parse_lilypond_input_string(measure_dict['t'])
     bass = iotools.parse_lilypond_input_string(measure_dict['b'])
     return treble, bass
 
     
-def build_score( ):
+def build_mozart_piano_staff( ):
     treble_staff = Staff([ ])
     bass_staff = Staff([ ])
 
     # select the measures to use
-    choices = choose_measures( )
+    choices = choose_mozart_measures( )
 
     # create and populate the volta containers
     treble_volta = Container([ ])
     bass_volta = Container([ ])
     for choice in choices[:7]:
-        treble, bass = build_measure(choice)
+        treble, bass = build_one_mozart_measure(choice)
         treble_volta.append(treble)
         bass_volta.append(bass)
 
@@ -46,7 +46,7 @@ def build_score( ):
     treble_alternative = Container([ ])
     bass_alternative = Container([ ])
     for choice in choices[7:9]:
-        treble, bass = build_measure(choice)
+        treble, bass = build_one_mozart_measure(choice)
         treble_alternative.append(treble)
         bass_alternative.append(bass)
 
@@ -58,7 +58,7 @@ def build_score( ):
 
     # create the remaining measures
     for choice in choices[9:]:
-        treble, bass = build_measure(choice)
+        treble, bass = build_one_mozart_measure(choice)
         treble_staff.append(treble)
         bass_staff.append(bass)
 
@@ -74,3 +74,24 @@ def build_score( ):
 
     # combine into a PianoStaff and return
     return scoretools.PianoStaff([treble_staff, bass_staff])
+
+
+def build_mozart_lily(piano_staff):
+
+    # wrap the PianoStaff with a LilyFile
+    lily = lilyfiletools.make_basic_lily_file(piano_staff)
+
+    # create some markup to use in our header block
+    title = "Ein Musickalisches Wuerfelspiel"
+    title_markup = markuptools.MarkupCommand('override', [schemetools.SchemePair('font-name', 'Helvetica Neue')], [title])
+    composer  = "W. A. Mozart (maybe?)"
+
+    # change various settings
+    lily.global_staff_size = 12
+    lily.header_block.title = markuptools.Markup(title_markup)
+    lily.header_block.composer = schemetools.SchemeString('W. A. Mozart (maybe?!?)')
+    lily.layout_block.ragged_right = True
+    lily.paper_block.markup_system_spacing__basic_distance = 20
+    lily.paper_block.paper_width = 180
+
+    return lily
