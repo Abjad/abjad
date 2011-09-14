e639862bb037ab80306d437ea8b6c4e773951773
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2011-09-14 22:43:45 +0000

    Implemented basic tokenizing in LilyParser.

--- trunk/abjad/tools/iotools/_LilyParser/_LilyParser.py
+++ trunk/abjad/tools/iotools/_LilyParser/_LilyParser.py
@@ -3,39 +3,47 @@ import re
 
 class _LilyParser(object):
 
+    _commands = (
+        '\\bar',
+        '\\clef',
+        '\\key',
+        '\\time',
+        '\\times',
+    )
+
     _tokens = (
-        ('ARTICULATION',     r'\^|\+|-\||>|.|_'),
-        ('BAR',              r'\\bar'),
+        ('ARTICULATION',     r'(_|-|\^)(\^|\+|-\||>|\.|_)'),
         ('BEAM_CLOSE',       r']'),
         ('BEAM_OPEN',        r'\['),
+
+        ('PARALLEL_CLOSE',   r'>>'),
+        ('PARALLEL_OPEN',    r'<<'),
         ('CHORD_CLOSE',      r'>'),
         ('CHORD_OPEN',       r'<'),
-        ('COMMAND',          r'\\\w+'),
-        ('CONTAINER_CLOSE',  r'}'),
-        ('CONTAINER_OPEN',   r'{'),
-        ('DURATION',         r'[1-9]\d*\.*'),
-        ('DYNAMIC',          r'\\(p+|f+|mp|mf|fp|sf|sff|sp|spp|sfz|rfz)$'),
-        ('FRACTION',         r'([1-9]\d*)/([1-9]\d*)'),
+
         ('HAIRPIN_CRESC',    r'\\<'),
         ('HAIRPIN_DECRESC',  r'\\>'),
         ('HAIRPIN_STOP',     r'\\!'),
-        ('KEY',              r'\\key'),
-        ('KEY_MODE',         r'(major|minor)'),
-        ('MARK_DIR_DOWN',    r'_'),
-        ('MARK_DIR_NEUTRAL', r'-'),
-        ('MARK_DIR_UP',      r'\^'),
-        ('OCTAVE',           r"('*|,*)$"),
-        ('PITCHCLASS',       r'[a-g][q]?(s*|f*)$'),
+        ('DYNAMIC',          r'\\(p+|f+|mp|mf|fp|sf|sff|sp|spp|sfz|rfz)$'),
+        ('COMMAND',          r'\\\w+'),
+
+        ('CONTAINER_CLOSE',  r'}'),
+        ('CONTAINER_OPEN',   r'{'),
+        ('DIRECTION',        r'_|-|\^'),
+
+        ('FRACTION',         r'[1-9]\d*/[1-9]\d*'),
+        ('DURATION',         r'[1-9]\d*\.*'),
+
+        ('OCTAVE_DOWN',      r",+"),
+        ('OCTAVE_UP',        r"'+"),
+        ('PITCH',            r"[a-g][q]?(s*|f*)"),
         ('REST',             r'[rR]'),
         ('SKIP',             r's'),
         ('SLUR_CLOSE',       r'\)'),
         ('SLUR_OPEN',        r'\('),
         ('STRING',           r'\"[\w\s]+\"'),
         ('TIE',              r'~'),
-        ('TIME',             r'\\time'),
-        ('TIMES',            r'\\times'),                         
-        ('TRILLSPAN_START',  r'\\startTrillSpan'),
-        ('TRILLSPAN_STOP',   r'\\stopTrillSpan'),
+        ('WORD',             r'\w+$'),
     )
 
     _regex = re.compile('|'.join(['(?P<%s>%s)' % (name, rule) for name, rule in _tokens]), re.MULTILINE)
@@ -49,9 +57,23 @@ class _LilyParser(object):
 
     ### PRIVATE METHODS ###
 
-    def _group_tokens(self, tokens):
+    def _group(self, tokens):
         pass
 
     def _tokenize(self, lily_string):
-        pass
-        
+        tokens = [ ]
+        position = 0
+        while position < len(lily_string):
+            # match whitespace
+            match = self._whitespace_regex.match(lily_string, position)
+            if match:
+                position = match.end( )
+            # match token
+            match = self._regex.match(lily_string, position)
+            if match is None:
+                raise Exception('Unknown token: ...%s...' % lily_string[position:position + 10])
+            position = match.end( )
+            value = match.group(match.lastgroup)
+            tokens.append((match.lastgroup, value))
+            print position, match.lastgroup, value
+        return tokens
