75cc512f1d1473c4dd8925b82059455404cf7d84
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2011-09-15 03:36:32 +0000

    Improved LilyPondParser tokenizing.
    abjad> from abjad.tools.iotools._LilyPondParser._LilyPondParser import _LilyPondParser
    abjad> parser = _LilyPondParser( )
    abjad> string = ''' \\new Staff { c4 df\'4.. eqs,,16 r4 } \\bar "|." '''
    abjad> for token, value in parser._tokenize(string): token, value
    ...
    ('NEW', '\\new')
    ('CONTEXT_TYPE', 'Staff')
    ('CONTAINER_OPEN', '{')
    ('PITCHCLASS', 'c')
    ('UNSIGNED_INT', '4')
    ('PITCHCLASS', 'df')
    ('OCTAVE_UP', "'")
    ('UNSIGNED_INT', '4')
    ('DOTS', '..')
    ('PITCHCLASS', 'eqs')
    ('OCTAVE_DOWN', ',,')
    ('UNSIGNED_INT', '16')
    ('REST', 'r')
    ('UNSIGNED_INT', '4')
    ('CONTAINER_CLOSE', '}')
    ('BAR', '\\bar')
    ('STRING', '"|."')

--- trunk/abjad/tools/iotools/_LilyPondParser/_LilyPondParser.py
+++ trunk/abjad/tools/iotools/_LilyPondParser/_LilyPondParser.py
@@ -3,12 +3,26 @@ import re
 
 class _LilyPondParser(object):
 
-    _commands = (
-        '\\bar',
-        '\\clef',
-        '\\key',
-        '\\time',
-        '\\times',
+    _callbacks = {
+        'COMMAND': '_callback_command_token',
+        'OBJ': '_callback_obj_token',
+    }
+
+    _commands = {
+        'BAR': ['STRING'],
+        'CLEF': ['SYMBOL'],
+        'CONTEXT': ['CONTEXT_TYPE', 'EQUALS', 'STRING'],
+        'KEY': ['PITCHCLASS', 'SYMBOL'],
+        'NEW': ['CONTEXT_TYPE'],
+        'TEMPO': ['UNSIGNED_INT', 'EQUALS', 'UNSIGNED_INT'],
+        'TIME': ['FRACTION'],
+        'TIMES': ['FRACTION'],
+    }
+
+    _context_types = (
+        'Score',
+        'Staff',
+        'Voice',
     )
 
     _tokens = (
@@ -21,6 +35,8 @@ class _LilyPondParser(object):
         ('CHORD_CLOSE',      r'>'),
         ('CHORD_OPEN',       r'<'),
 
+        ('EQUALS',           r'='),
+
         ('HAIRPIN_CRESC',    r'\\<'),
         ('HAIRPIN_DECRESC',  r'\\>'),
         ('HAIRPIN_STOP',     r'\\!'),
@@ -32,18 +48,21 @@ class _LilyPondParser(object):
         ('DIRECTION',        r'_|-|\^'),
 
         ('FRACTION',         r'[1-9]\d*/[1-9]\d*'),
-        ('DURATION',         r'[1-9]\d*\.*'),
+        ('UNSIGNED_INT',     r'[1-9]\d*'),
+        ('DOTS',             r'\.+'),
 
         ('OCTAVE_DOWN',      r",+"),
         ('OCTAVE_UP',        r"'+"),
-        ('PITCH',            r"[a-g][q]?(s*|f*)"),
+        ('PITCHCLASS',       r"[a-g][q]?(s*|f*)"),
         ('REST',             r'[rR]'),
         ('SKIP',             r's'),
         ('SLUR_CLOSE',       r'\)'),
         ('SLUR_OPEN',        r'\('),
-        ('STRING',           r'\"[\w\s]+\"'),
+        ('STRING',           r'\".+\"'),
         ('TIE',              r'~'),
-        ('WORD',             r'\w+$'),
+
+        ('SYMBOL',           r'\w+$'),
+        ('OBJ',              r'[A-Z][a-zA-Z]+'),
     )
 
     _regex = re.compile('|'.join(['(?P<%s>%s)' % (name, rule) for name, rule in _tokens]), re.MULTILINE)
@@ -60,6 +79,26 @@ class _LilyPondParser(object):
     def _group(self, tokens):
         pass
 
+    def _callback_command_token(self, token, value):
+        if value in [
+            '\\bar',
+            '\\clef',
+            '\\context',
+            '\\key',
+            '\\new',
+            '\\time',
+            '\\times']:
+            token = value[1:].upper( )
+        return token, value
+
+    def _callback_obj_token(self, token, value):
+        if value in [
+            'Score',
+            'Staff',
+            'Voice']:
+            token = 'CONTEXT_TYPE'
+        return token, value
+
     def _tokenize(self, lily_string):
         tokens = [ ]
         position = 0
@@ -68,12 +107,21 @@ class _LilyPondParser(object):
             match = self._whitespace_regex.match(lily_string, position)
             if match:
                 position = match.end( )
+                if position == len(lily_string):
+                    break
             # match token
             match = self._regex.match(lily_string, position)
             if match is None:
-                raise Exception('Unknown token: ...%s...' % lily_string[position:position + 10])
+                raise Exception('Unknown token at character %d: %s' % \
+                    (position, lily_string[position:position + 10]))
             position = match.end( )
+            token = match.lastgroup
             value = match.group(match.lastgroup)
-            tokens.append((match.lastgroup, value))
-            print position, match.lastgroup, value
+            # check for callbacks, add to token list
+            if token in self._callbacks:
+                callback = getattr(self, self._callbacks[token])
+                token, value = callback(token, value)
+                tokens.append(callback(token, value))
+            else:
+                tokens.append((token, value))
         return tokens
