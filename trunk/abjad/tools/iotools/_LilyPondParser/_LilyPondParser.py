d02b37d7b431eb799e41fa5c746c58521778a598
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2011-09-25 18:38:49 +0000

    Renamed _LilyPondTokenizer => _LilyPondLexer

--- trunk/abjad/tools/iotools/_LilyPondParser/_LilyPondParser.py
+++ trunk/abjad/tools/iotools/_LilyPondParser/_LilyPondParser.py
@@ -1,17 +1,17 @@
-from abjad.tools.iotools._LilyPondTokenizer._LilyPondTokenizer import _LilyPondTokenizer
+from abjad.tools.iotools._LilyPondLexer._LilyPondLexer import _LilyPondLexer
 from abjad.tools.iotools._LilyPondTokenGrouper._LilyPondTokenGrouper import _LilyPondTokenGrouper
 
 
 class _LilyPondParser(object):
 
     def __init__(self):
-        self._tokenizer = _LilyPondTokenizer( )
+        self._lexer = _LilyPondLexer( )
         self._grouper = _LilyPondTokenGrouper( )
 
     ### OVERRIDES ###
 
     def __call__(self, lily_string):
-        tokens = self._tokenizer(lily_string)
+        tokens = self._lexer(lily_string)
         objects = self._grouper(tokens, lily_string)
         # components = self._parse_objects(objects, lily_string)
         return objects
