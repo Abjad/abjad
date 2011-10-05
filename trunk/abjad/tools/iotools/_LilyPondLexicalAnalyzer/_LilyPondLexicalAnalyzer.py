1297f741ed76563e1593b7d879a81980c580b894
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2011-10-05 00:13:52 +0000

    Added basic regexes and LilyPond reserved keyword to _LilyPondLexicalAnalyzer.

--- trunk/abjad/tools/iotools/_LilyPondLexicalAnalyzer/_LilyPondLexicalAnalyzer.py
+++ trunk/abjad/tools/iotools/_LilyPondLexicalAnalyzer/_LilyPondLexicalAnalyzer.py
@@ -1,150 +1,260 @@
-import re
-from abjad.exceptions import *
-from abjad.tools.iotools._LilyPondToken._LilyPondToken import _LilyPondToken
+from ply import lex
+from ply.lex import TOKEN
 
 
 class _LilyPondLexicalAnalyzer(object):
 
-    _articulations = (
-        '\\accent', '\\accentus', '\\circulus', '\\coda', '\\downbow', '\\downmordent',
-        '\\downprall', '\\espressivo', '\\fermata', '\\flageolet', '\\halfopen',
-        '\\ictus', '\\lheel', '\\lineprall', '\\longfermata', '\\ltoe', '\\marcato',
-        '\\mordent', '\\open', '\\portato', '\\prall', '\\pralldown', '\\prallmordent',
-        '\\prallprall', '\\prallup', '\\reverseturn', '\\rheel', '\\rtoe', '\\segno',
-        '\\semicirculus', '\\shortfermata', '\\signumcongruentiae', '\\snappizzicato',
-        '\\staccatissimo', '\\staccato', '\\stopped', '\\tenuto', '\\thumb', '\\trill'
-        '\\turn', '\\upbow', '\\upmordent', '\\upprall', '\\varcoda', '\\verylongfermata',
-    )
+    A               = '[a-zA-Z\200-\377]'
+    AA              = '%s|_' % A
+    N               = '[0-9]'
+    AN              = '%s|%s' % (AA, N)
+    ANY_CHAR        = '(.|\n)'
+    PUNCT           = "[?!:'`]"
+    ACCENT          = '''\\[`'"^]'''
+    NATIONAL        = '[\001-\006\021-\027\031\036]'
+    TEX             = '%s|-|%s|%s|%s' % (AA, PUNCT, ACCENT, NATIONAL)
+    WORD            = '%s%s*' % (A, AN)
+    DASHED_WORD     = '%s(%s|-)*' % (A, AN)
+    DASHED_KEY_WORD = '\\%s' % DASHED_WORD
+    ALPHAWORD       = '%s+' % A
+    DIGIT           = '%s' % N
+    UNSIGNED        = '%s+' % N
+    E_UNSIGNED      = '\\%s+' % N
+    FRACTION        = '%s+\/%s+' % (N, N)
+    INT             = '-?%s' % UNSIGNED
+    REAL            = '(%s\.%s*)|(-?\.%s+)' % (INT, N, N)
+    KEYWORD         = '\\%s' % WORD
+    WHITE           = '[ \n\t\f\r]'
+    HORIZONTALWHITE = '[ \t]'
+    BLACK           = '[^ \n\t\f\r]'
+    RESTNAME        = '[rs]'
+    NOTECOMMAND     = '\\%s+' % A
+    MARKUPCOMMAND   = '\\(%s|[-_])+' % A
+    LYRICS          = '(%s|%s)[^0-9 \t\n\r\f]*' % (AA, TEX)
+    ESCAPED         = '''[nt\\'"]'''
+    EXTENDER        = '__'
+    HYPHEN          = '--'
+    BOM_UTF8        = '\357\273\277'
 
-    _blocks = (
-        '\\book', '\\header', '\\layout', '\\midi', '\\score',
-    )
-
-    _commands = (
-        '\\bar', '\\clef', '\\key', '\\markup', '\\tempo', '\\time', '\\times',
-    )
-
-    _context_types = (
-        'PianoStaff', 'Score', 'Staff', 'Voice',
-    )
-
-    _durations = (
-        '\\breve', '\\longa', '\\maxima',
-        '1', '2', '4', '8', '16', '32', '64', '128',
-    )
-
-    _dynamics = (
-        '\\mp', '\\mf',
-        '\\fp', '\\sf', '\\sff', '\\sp', '\\spp', '\\sfz', '\\rfz',
-        '\\p', '\\pp', '\\ppp', '\\pppp', '\\ppppp',
-        '\\f', '\\ff', '\\fff', '\\ffff', '\\fffff'
-    )
-     
-    _keywords = (
-        '\\context', '\\include', '\\new', '\\once', '\\override',
-        '\\version', '\\with',
-    )
-
-    _tokens = (
-        ('ARTICULATION_SHORTCUT', r'(_|-|\^)\s*(\^|\+|-\||>|\.|_)'),
-        ('BAR_CHECK',             r'\|'),
-        ('BEAM_CLOSE',            r']'),
-        ('BEAM_OPEN',             r'\['),
-        ('PARALLEL_CLOSE',        r'>>'),
-        ('PARALLEL_OPEN',         r'<<'),
-        ('CHORD_CLOSE',           r'>'),
-        ('CHORD_OPEN',            r'<'),
-        ('DOTS',                  r'(\.\s*)+'),
-        ('EQUALS',                r'='),
-        ('HAIRPIN_CRESC',         r'\\<'),
-        ('HAIRPIN_DECRESC',       r'\\>'),
-        ('HAIRPIN_STOP',          r'\\!'),
-        ('COMMAND',               r'\\[a-zA-Z]+'),
-        ('CONTAINER_CLOSE',       r'}'),
-        ('CONTAINER_OPEN',        r'{'),
-        ('DIRECTION',             r'_|-|\^'),
-        ('FRACTION',              r'[1-9]\d*/[1-9]\d*'),
-        ('INTEGER',               r'[1-9]\d*'),
-        ('OCTAVE_DOWN',           r"(,\s*)+"),
-        ('OCTAVE_UP',             r"('\s*)+"),
-        ('SYMBOL',                r'\w+'),
-        ('REST',                  r'[rR]'),
-        ('SKIP',                  r's'),
-        ('SLUR_CLOSE',            r'\)'),
-        ('SLUR_OPEN',             r'\('),
-        ('STRING',                r'\".+\"'),
-        ('TIE',                   r'~'),
-        ('TREMOLO',               r':'),
-    )
-
-    _token_callbacks = {
-        'ARTICULATION_SHORTCUT': '_token_callback_strip_whitespace',
-        'COMMAND':               '_token_callback_command',
-        'DOTS':                  '_token_callback_strip_whitespace',
-        'OCTAVE_DOWN':           '_token_callback_strip_whitespace',
-        'OCTAVE_UP':             '_token_callback_strip_whitespace',
-        'SYMBOL':                '_token_callback_symbol',
+    reserved = {
+        '\\accepts': 'ACCEPTS',
+        '\\addlyrics': 'ADDLYRICS',
+        '\\alias': 'ALIAS',
+        '\\alternative': 'ALTERNATIVE',
+        '\\book': 'BOOK',
+        '\\bookpart': 'BOOKPART',
+        '\\change': 'CHANGE',
+        '\\chordmode': 'CHORDMODE',
+        '\\chords': 'CHORDS',
+        '\\consists': 'CONSISTS',
+        '\\context': 'CONTEXT',
+        '\\default': 'DEFAULT',
+        '\\defaultchild': 'DEFAULTCHILD',
+        '\\denies': 'DENIES',
+        '\\description': 'DESCRIPTION',
+        '\\drummode': 'DRUMMODE',
+        '\\drums': 'DRUMS',
+        '\\figuremode': 'FIGUREMODE',
+        '\\figures': 'FIGURES',
+        '\\grobdescriptions': 'GROBDESCRIPTIONS',
+        '\\header': 'HEADER',
+        '\\invalid': 'INVALID',
+        '\\key': 'KEY',
+        '\\layout': 'LAYOUT',
+        '\\lyricmode': 'LYRICMODE',
+        '\\lyrics': 'LYRICS',
+        '\\lyricsto': 'LYRICSTO',
+        '\\mark': 'MARK',
+        '\\markup': 'MARKUP',
+        '\\markuplines': 'MARKUPLINES',
+        '\\midi': 'MIDI',
+        '\\name': 'NAME',
+        '\\notemode': 'NOTEMODE',
+        '\\octave': 'OCTAVE',
+        '\\once': 'ONCE',
+        '\\override': 'OVERRIDE',
+        '\\paper': 'PAPER',
+        '\\partial': 'PARTIAL',
+        '\\relative': 'RELATIVE',
+        '\\remove': 'REMOVE',
+        '\\repeat': 'REPEAT',
+        '\\rest': 'REST',
+        '\\revert': 'REVERT',
+        '\\score': 'SCORE',
+        '\\sequential': 'SEQUENTIAL',
+        '\\set': 'SET',
+        '\\simultaneous': 'SIMULTANEOUS',
+        '\\skip': 'SKIP',
+        '\\tempo': 'TEMPO',
+        '\\times': 'TIMES',
+        '\\transpose': 'TRANSPOSE',
+        '\\type': 'TYPE',
+        '\\unset': 'UNSET',
+        '\\with': 'WITH',
+        '\\time': 'TIME_T',
+        '\\new': 'NEWCONTEXT'
     }
 
-    _token_regex = re.compile('|'.join(['(?P<%s>%s)' % (name, rule) for name, rule in _tokens]), re.MULTILINE)
-        
-    _whitespace_regex = re.compile("\s*", re.MULTILINE)
+    tokens = ['NOTECOMMAND'] + reserved.values( )
 
-    ### OVERRIDES ###
+#%token ACCEPTS "\\accepts"
+#%token ADDLYRICS "\\addlyrics"
+#%token ALIAS "\\alias"
+#%token ALTERNATIVE "\\alternative"
+#%token BOOK "\\book"
+#%token BOOKPART "\\bookpart"
+#%token CHANGE "\\change"
+#%token CHORDMODE "\\chordmode"
+#%token CHORDS "\\chords"
+#%token CONSISTS "\\consists"
+#%token CONTEXT "\\context"
+#%token DEFAULT "\\default"
+#%token DEFAULTCHILD "\\defaultchild"
+#%token DENIES "\\denies"
+#%token DESCRIPTION "\\description"
+#%token DRUMMODE "\\drummode"
+#%token DRUMS "\\drums"
+#%token FIGUREMODE "\\figuremode"
+#%token FIGURES "\\figures"
+#%token GROBDESCRIPTIONS "\\grobdescriptions"
+#%token HEADER "\\header"
+#%token INVALID "\\invalid"
+#%token KEY "\\key"
+#%token LAYOUT "\\layout"
+#%token LYRICMODE "\\lyricmode"
+#%token LYRICS "\\lyrics"
+#%token LYRICSTO "\\lyricsto"
+#%token MARK "\\mark"
+#%token MARKUP "\\markup"
+#%token MARKUPLINES "\\markuplines"
+#%token MIDI "\\midi"
+#%token NAME "\\name"
+#%token NOTEMODE "\\notemode"
+#%token OCTAVE "\\octave"
+#%token ONCE "\\once"
+#%token OVERRIDE "\\override"
+#%token PAPER "\\paper"
+#%token PARTIAL "\\partial"
+#%token RELATIVE "\\relative"
+#%token REMOVE "\\remove"
+#%token REPEAT "\\repeat"
+#%token REST "\\rest"
+#%token REVERT "\\revert"
+#%token SCORE "\\score"
+#%token SEQUENTIAL "\\sequential"
+#%token SET "\\set"
+#%token SIMULTANEOUS "\\simultaneous"
+#%token SKIP "\\skip"
+#%token TEMPO "\\tempo"
+#%token TIMES "\\times"
+#%token TRANSPOSE "\\transpose"
+#%token TYPE "\\type"
+#%token UNSET "\\unset"
+#%token WITH "\\with"
 
-    def __call__(self, lily_string):
-        tokens = [ ]
-        lines = lily_string.split('\n')
-        for line_number, line in enumerate(lines):
-            column = 0
-            while column < len(line):
-                match = self._whitespace_regex.match(line, column)
-                if match:
-                    column = match.end( )
-                if column == len(line):
-                    break
-                match = self._token_regex.match(line, column)
-                if match is None:
-                    raise UnknownTokenLilyPondParserError(lily_string, line_number, column)
-                token = _LilyPondToken(
-                    kind = match.lastgroup,
-                    value = match.group(match.lastgroup),
-                    line = line_number,
-                    column = column)
-                column = match.end( )
-                if token.kind in self._token_callbacks:
-                    callback = getattr(self, self._token_callbacks[token.kind])
-                    token = callback(token, lily_string)
-                tokens.append(token)
-        return tokens
+#%token TIME_T "\\time"
+#%token NEWCONTEXT "\\new"
 
-    ### PRIVATE METHODS ###
+#%token CHORD_BASS "/+"
+#%token CHORD_CARET "^"
+#%token CHORD_COLON ":"
+#%token CHORD_MINUS "-"
+#%token CHORD_SLASH "/"
 
-    def _token_callback_command(self, token, lily_string):
-        if token.value in self._commands:
-            return token._replace(kind = token.value[1:].upper( ))
-        elif token.value in self._keywords:
-            return token._replace(kind = 'KEYWORD')
-        elif token.value in self._articulations:
-            return token._replace(kind = 'ARTICULATION')
-        elif token.value in ['\\longa', '\\breve', '\\maxima']:
-            return token._replace(kind = 'DURATION')
-        elif token.value in self._dynamics:
-            return token._replace(kind = 'DYNAMIC')
-        return token
-            
-    def _token_callback_strip_whitespace(self, token, lily_string):
-        new_value = ''
-        for c in token.value:
-            if not c.isspace( ):   
-                new_value += c
-        return token._replace(value = new_value)
-            
-    def _token_callback_symbol(self, token, lily_string):
-        if token.value in self._context_types:
-            return token._replace(kind = 'CONTEXT_TYPE')
-        else:
-            match = re.match(r"[a-g][q]?(s*|f*)", token.value)
-            if match is not None and match.end( ) == len(token.value):
-                token = token._replace(kind = 'PITCH_CLASS')
-        return token
+#%token ANGLE_OPEN "<"
+#%token ANGLE_CLOSE ">"
+#%token DOUBLE_ANGLE_OPEN "<<"
+#%token DOUBLE_ANGLE_CLOSE ">>"
+#%token E_BACKSLASH "\\"
+#%token E_ANGLE_CLOSE "\\>"
+#%token E_CHAR "\\C[haracter]"
+#%token E_CLOSE "\\)"
+#%token E_EXCLAMATION "\\!"
+#%token E_BRACKET_OPEN "\\["
+#%token E_OPEN "\\("
+#%token E_BRACKET_CLOSE "\\]"
+#%token E_ANGLE_OPEN "\\<"
+#%token E_PLUS "\\+"
+#%token E_TILDE "\\~"
+#%token EXTENDER "__"
+
+#%token FIGURE_CLOSE /* "\\>" */
+#%token FIGURE_OPEN /* "\\<" */
+#%token FIGURE_SPACE "_"
+#%token HYPHEN "--"
+
+#%token CHORDMODIFIERS
+#%token LYRIC_MARKUP
+#%token MULTI_MEASURE_REST
+
+#%token <i> DIGIT
+#%token <i> E_UNSIGNED
+#%token <i> UNSIGNED
+
+#/* Artificial tokens, for more generic function syntax */
+#%token <i> EXPECT_MARKUP;
+#%token <i> EXPECT_MUSIC;
+#%token <i> EXPECT_SCM;
+#%token <i> EXPECT_MARKUP_LIST
+#/* After the last argument. */
+#%token <i> EXPECT_NO_MORE_ARGS;
+
+#%token <scm> BOOK_IDENTIFIER
+#%token <scm> CHORDMODIFIER_PITCH
+#%token <scm> CHORD_MODIFIER
+#%token <scm> CHORD_REPETITION
+#%token <scm> CONTEXT_DEF_IDENTIFIER
+#%token <scm> CONTEXT_MOD_IDENTIFIER
+#%token <scm> DRUM_PITCH
+#%token <scm> DURATION_IDENTIFIER
+#%token <scm> EVENT_IDENTIFIER
+#%token <scm> FRACTION
+#%token <scm> LYRICS_STRING
+#%token <scm> LYRIC_MARKUP_IDENTIFIER
+#%token <scm> MARKUP_FUNCTION
+#%token <scm> MARKUP_LIST_FUNCTION
+#%token <scm> MARKUP_IDENTIFIER
+#%token <scm> MARKUPLINES_IDENTIFIER
+#%token <scm> MUSIC_FUNCTION
+#%token <scm> MUSIC_IDENTIFIER
+#%token <scm> NOTENAME_PITCH
+#%token <scm> NUMBER_IDENTIFIER
+#%token <scm> OUTPUT_DEF_IDENTIFIER
+#%token <scm> REAL
+#%token <scm> RESTNAME
+#%token <scm> SCM_IDENTIFIER
+#%token <scm> SCM_TOKEN
+#%token <scm> SCORE_IDENTIFIER
+#%token <scm> STRING
+#%token <scm> STRING_IDENTIFIER
+#%token <scm> TONICNAME_PITCH
+
+#    @TOKEN(NOTECOMMAND)
+    def t_NOTECOMMAND(self, t):
+        r'''\\[a-zA-Z\200-\377]+'''
+        t.type = self.reserved.get(t.value, 'NOTECOMMAND')
+        return t
+    
+    t_ignore = " \t"
+
+    def t_newline(self, t):
+        r'\n+'
+        t.lexer.lineno += t.value.count("\n")
+
+    def t_error(self, t):
+        print("Illegal character '%s'" % t.value[0])
+        t.lexer.skip(1)
+
+    def __init__(self, **kwargs):
+        self._lexer = lex.lex(module=self, **kwargs)
+
+    def __call__(self, input):
+        self._lexer.input(input)
+        while True:
+            token = self._lexer.token( )
+            if not token:
+                break
+            yield token
+        return
+
+
