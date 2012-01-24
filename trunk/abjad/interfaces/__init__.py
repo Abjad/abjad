8454de0d3498ee3d410c4d52a531028a2efdec8c
Trevor Bača <trevorbaca@gmail.com>
2012-01-24 17:16:30 +0000

    Replaced 'globals( )' with 'globals()' in three initializers.

--- trunk/abjad/interfaces/__init__.py
+++ trunk/abjad/interfaces/__init__.py
@@ -1,6 +1,6 @@
 from abjad.tools.importtools._import_structured_package import _import_structured_package
 
-_import_structured_package(__path__[0], globals( ))
+_import_structured_package(__path__[0], globals())
 
 from _OffsetInterface import _OffsetInterface
 from _NavigationInterface import _NavigationInterface
