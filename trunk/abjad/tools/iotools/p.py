3032e8132b9bc905c7cb001f26cd549a2260e21c
Trevor Bača <trevorbaca@gmail.com>
2012-02-05 13:03:58 +0000

    Added p() usage example.

--- trunk/abjad/tools/iotools/p.py
+++ trunk/abjad/tools/iotools/p.py
@@ -1,5 +1,26 @@
 def p(*args):
+    r'''.. versionadded:: 2.7
+
+    Parse `args` as LilyPond string::
+
+        abjad> p("{c'4 d'4 e'4 f'4 }")
+        {c'4, d'4, e'4, f'4}
+
+    ::
+
+        abjad> container = _
+        abjad> f(container)
+        {
+            c'4
+            d'4
+            e'4
+            f'4
+        }
+
+    Return Abjad expression.
+    '''
     from abjad.tools.lilypondparsertools import LilyPondParser
+
     if 1 < len(args):
         return LilyPondParser(default_language=args[1])(args[0])
     return LilyPondParser()(args[0])
