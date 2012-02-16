84c2dfdd7be69019d43d5bdfc355fbdbf7fb1b27
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2012-02-16 02:07:51 +0000

    Wrote HorizontalBracketSpanner and HairpinSpanner tests for LilyPondParser.

--- trunk/abjad/tools/lilypondparsertools/LilyPondParser/test/test_LilyPondParser__spanners__HairpinSpanner.py
+++ trunk/abjad/tools/lilypondparsertools/LilyPondParser/test/test_LilyPondParser__spanners__HairpinSpanner.py
@@ -3,10 +3,80 @@ from abjad import *
 from abjad.tools.lilypondparsertools import LilyPondParser
 
 
-py.test.skip('Not yet written.')
-def _01():
-    target = None
+def test_LilyPondParser__spanners__HairpinSpanner_01():
+    target = Staff(notetools.make_notes([0] * 5, [(1, 4)]))
+    spannertools.HairpinSpanner(target[:3], '<')
+    spannertools.HairpinSpanner(target[2:], '>')
+    contexttools.DynamicMark('ppp')(target[-1])
+
+    r'''\new Staff {
+        c'4 \<
+        c'4
+        c'4 \! \>
+        c'4
+        c'4 \ppp
+    }
+    '''
+
     parser = LilyPondParser()
     result = parser(target.format)
     assert target.format == result.format and target is not result
 
+
+def test_LilyPondParser__spanners__HairpinSpanner_02():
+    target = Container(notetools.make_notes([0] * 4, [(1, 4)]))
+    spannertools.HairpinSpanner(target[0:2], '<')
+    spannertools.HairpinSpanner(target[1:3], '<')
+    spannertools.HairpinSpanner(target[2:], '<')
+
+    r'''{
+        c'4 \<
+        c'4 \! \<
+        c'4 \! \<
+        c'4 \!
+    }
+    '''
+
+    input = r'''\relative c' { c \< c \< c \< c \! }'''
+    parser = LilyPondParser()
+    result = parser(input)
+    assert target.format == result.format and target is not result
+
+
+def test_LilyPondParser__spanners__HairpinSpanner_03():
+    '''Dynamic marks can terminate hairpins.'''
+    target = Staff(notetools.make_notes([0] * 3, [(1, 4)]))
+    spannertools.HairpinSpanner(target[0:2], '<')
+    spannertools.HairpinSpanner(target[1:], '>')
+    contexttools.DynamicMark('p')(target[1])
+    contexttools.DynamicMark('f')(target[-1])
+
+    r'''\new Staff {
+        c'4 \<
+        c'4 \p \>
+        c'4 \f
+    }
+    '''
+
+    input = r"\new Staff \relative c' { c \< c \p \> c \f }"
+    parser = LilyPondParser()
+    result = parser(input)
+    assert target.format == result.format and target is not result
+
+
+def test_LilyPondParser__spanners__HairpinSpanner_04():
+    '''Unterminated.'''
+    input = r'{ c \< c c c }'
+    assert py.test.raises(Exception, 'LilyPondParser()(input)')
+
+
+def test_LilyPondParser__spanners__HairpinSpanner_05():
+    '''Unbegun is okay.'''
+    input = r'{ c c c c \! }'
+    result = LilyPondParser()(input)
+
+
+def test_LilyPondParser__spanners__HairpinSpanner_06():
+    '''No double dynamic spans permitted.'''
+    input = r'{ c \< \> c c c \! }'
+    assert py.test.raises(Exception, 'LilyPondParser()(input)')
--- trunk/abjad/tools/lilypondparsertools/LilyPondParser/test/test_LilyPondParser__spanners__HorizontalBracketSpanner.py
+++ trunk/abjad/tools/lilypondparsertools/LilyPondParser/test/test_LilyPondParser__spanners__HorizontalBracketSpanner.py
@@ -3,10 +3,44 @@ from abjad import *
 from abjad.tools.lilypondparsertools import LilyPondParser
 
 
-py.test.skip('Not yet written.')
-def _01():
-    target = None
+def test_LilyPondParser__spanners__HorizontalBracketSpanner_01():
+    target = Container(notetools.make_notes([0] * 4, [(1, 4)]))
+    spannertools.HorizontalBracketSpanner(target[:])
+    spannertools.HorizontalBracketSpanner(target[:2])
+    spannertools.HorizontalBracketSpanner(target[2:])
+
+    r'''{
+        c'4 \startGroup \startGroup
+        c'4 \stopGroup
+        c'4 \startGroup
+        c'4 \stopGroup \stopGroup
+    }
+    '''
+
     parser = LilyPondParser()
     result = parser(target.format)
     assert target.format == result.format and target is not result
 
+
+def test_LilyPondParser__spanners__HorizontalBracketSpanner_02():
+    '''Starting and stopping on the same leaf.'''
+    input = r'''{ c \startGroup \stopGroup c c c }'''
+    assert py.test.raises(Exception, 'LilyPondParser()(input)')
+
+
+def test_LilyPondParser__spanners__HorizontalBracketSpanner_03():
+    '''One group stopping on a leaf, while another begins on the same leaf.'''
+    input = r'''{ c \startGroup c \stopGroup \startGroup c c \stopGroup }'''
+    assert py.test.raises(Exception, 'LilyPondParser()(input)')
+
+
+def test_LilyPondParser__spanners__HorizontalBracketSpanner_04():
+    '''Unterminated.'''
+    input = r'''{ c \startGroup c c c }'''
+    assert py.test.raises(Exception, 'LilyPondParser()(input)')
+
+
+def test_LilyPondParser__spanners__HorizontalBracketSpanner_05():
+    '''Unstarted.'''
+    input = r'''{ c c c c \stopGroup }'''
+    assert py.test.raises(Exception, 'LilyPondParser()(input)')
