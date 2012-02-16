a6a4f6b3ef66c2f68717cb30cf24b36a3728930d
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2012-02-16 02:29:39 +0000

    Completed initial round of LilyPondParser tests.

--- trunk/abjad/tools/lilypondparsertools/LilyPondParser/test/test_LilyPondParser__misc__chord_repetition.py
+++ trunk/abjad/tools/lilypondparsertools/LilyPondParser/test/test_LilyPondParser__misc__chord_repetition.py
@@ -3,10 +3,79 @@ from abjad import *
 from abjad.tools.lilypondparsertools import LilyPondParser
 
 
-py.test.skip('Not yet written.')
-def _01():
-    target = None
+def test_LilyPondParser__misc__chord_repetition_01():
+    target = Container([
+        Chord([0, 4, 7], (1, 4)),
+        Chord([0, 4, 7], (1, 4)),
+        Chord([0, 4, 7], (1, 4)),
+        Chord([0, 4, 7], (1, 4)),
+    ])
+
+    r'''{
+        <c' e' g'>4
+        <c' e' g'>4
+        <c' e' g'>4
+        <c' e' g'>4
+    }
+    '''
+
+    input = r'''{ <c' e' g'> q q q }'''
     parser = LilyPondParser()
-    result = parser(target.format)
+    result = parser(input)
     assert target.format == result.format and target is not result
 
+
+def test_LilyPondParser__misc__chord_repetition_02():
+    target = Staff([
+        Chord([0, 4, 7], (1, 8)),
+        Chord([0, 4, 7], (1, 8)),
+        Chord([0, 4, 7], (1, 4)),
+        Chord([0, 4, 7], (3, 16)),
+        Chord([0, 4, 7], (1, 16)),
+        Chord([0, 4, 7], (1, 4))
+    ])
+    contexttools.DynamicMark('p')(target[0])
+    marktools.Articulation('staccatissimo')(target[2])
+    markuptools.Markup('text', 'up')(target[3])
+    marktools.Articulation('staccatissimo')(target[-1])
+
+    r'''\new Staff {
+        <c' e' g'>8 \p
+        <c' e' g'>8
+        <c' e' g'>4 -\staccatissimo
+        <c' e' g'>8. ^ \markup { text }
+        <c' e' g'>16
+        <c' e' g'>4 -\staccatissimo
+    }
+    '''
+
+    input = r'''\new Staff { <c' e' g'>8\p q q4-| q8.^"text" q16 q4-| }'''
+    parser = LilyPondParser()
+    result = parser(input)
+    assert target.format == result.format and target is not result
+
+
+def test_LilyPondParser__misc__chord_repetition_03():
+    target = Container([
+        Chord([0, 4, 7], (1, 8)),
+        Note(12, (1, 8)),
+        Chord([0, 4, 7], (1, 8)),
+        Note(12, (1, 8)),
+        Rest((1, 4)),
+        Chord([0, 4, 7], (1, 4)),
+    ])
+
+    r'''{
+        <c' e' g'>8
+        c''8
+        <c' e' g'>8
+        c''8
+        r4
+        <c' e' g'>4
+    }
+    '''
+
+    input = r'''{ <c' e' g'>8 c'' q c'' r4 q }'''
+    parser = LilyPondParser()
+    result = parser(input)
+    assert target.format == result.format and target is not result
--- trunk/abjad/tools/lilypondparsertools/LilyPondParser/test/test_LilyPondParser__misc__default_duration.py
+++ trunk/abjad/tools/lilypondparsertools/LilyPondParser/test/test_LilyPondParser__misc__default_duration.py
@@ -3,10 +3,26 @@ from abjad import *
 from abjad.tools.lilypondparsertools import LilyPondParser
 
 
-py.test.skip('Not yet written.')
-def _01():
-    target = None
+def test_LilyPondParser__misc__default_duration_01():
+    target = Container(notetools.make_notes([0],
+        [(1, 4), (1, 2), (1, 2), (1, 8), (1, 8), (3, 16), (3, 16)]))
+    target[-2].duration_multiplier = Fraction(5, 17)
+    target[-1].duration_multiplier = Fraction(5, 17)
+
+    r'''{
+        c'4
+        c'2
+        c'2
+        c'8
+        c'8
+        c'8. * 5/17
+        c'8. * 5/17
+    }
+    '''
+
+    input = r'''{ c' c'2 c' c'8 c' c'8. * 5/17 c' }'''
+
     parser = LilyPondParser()
-    result = parser(target.format)
+    result = parser(input)
     assert target.format == result.format and target is not result
 
