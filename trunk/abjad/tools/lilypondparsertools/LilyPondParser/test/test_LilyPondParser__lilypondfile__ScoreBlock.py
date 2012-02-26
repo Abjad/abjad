472c0dc7566eade182fa76a6e1cf91883680964c
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2012-02-26 19:36:29 +0000

    Updates LilyPondParser tests to reflect change to ScoreBlock.

--- trunk/abjad/tools/lilypondparsertools/LilyPondParser/test/test_LilyPondParser__lilypondfile__ScoreBlock.py
+++ trunk/abjad/tools/lilypondparsertools/LilyPondParser/test/test_LilyPondParser__lilypondfile__ScoreBlock.py
@@ -9,11 +9,3 @@ def test_LilyPondParser__lilypondfile__ScoreBlock_01():
     parser = LilyPondParser()
     result = parser(target.format)
     assert target.format == result.format and target is not result
-
-
-def test_LilyPondParser__lilypondfile__ScoreBlock_02():
-    '''LilyPondParser cannot parse empty score blocks.'''
-    target = lilypondfiletools.ScoreBlock()
-    parser = LilyPondParser()
-    py.test.raises(LilyPondParserError, 'result = parser(target.format)')
-
