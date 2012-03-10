ad55fe62d0464bcac4232740f01c4928ca5b4351
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2012-03-10 18:11:04 +0000

    Removed musicxmltools.
    The package was never developed extensively.  Please consider patronizing the
    music21 project for MusicXML-to-Abjad importing.

--- trunk/abjad/tools/musicxmltools/_import_test_file.py
+++
@@ -1,9 +0,0 @@
-from xml.etree import ElementTree
-import os
-
-
-def _import_test_file():
-    f = open(os.path.join(os.path.dirname(__file__), 'test', 'SchbAvMaSample.xml'), 'r')
-    r = ElementTree.parse(f).getroot()
-    f.close()
-    return r
