43fb5937e351206fadeaaf981e49b0f71718c56a
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2012-03-11 18:04:31 +0000

    LilyPondParser now inherits from MutableAbjadObject.

--- trunk/abjad/tools/lilypondparsertools/LilyPondParser/LilyPondParser.py
+++ trunk/abjad/tools/lilypondparsertools/LilyPondParser/LilyPondParser.py
@@ -6,6 +6,7 @@ from ply import lex, yacc
 from ply.lex import LexToken
 
 from abjad import *
+from abjad.tools.abctools import MutableAbjadObject
 from abjad.tools.componenttools.Component import Component
 from abjad.tools.contexttools._Context import _Context
 from abjad.tools.leaftools.Leaf import Leaf
@@ -27,7 +28,7 @@ yacc.LRParser._monkey_patch_parse = _parse
 yacc.LRParser._monkey_patch_parse_debug = _parse_debug
 
 
-class LilyPondParser(object):
+class LilyPondParser(MutableAbjadObject):
     r'''Parses a subset of LilyPond input syntax:
 
     ::
