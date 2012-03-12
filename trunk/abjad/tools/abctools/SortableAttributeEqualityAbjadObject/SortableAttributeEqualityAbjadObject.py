877e8890b23ead8933c03f3dbda5161633e0d794
Trevor Bača <trevorbaca@gmail.com>
2012-03-12 00:24:34 +0000

    Tweaked abctools class hierarchy.
    SortableAttributeEqualityAbjadObject now inherits from AttributeEqualityAbjadObject.

--- trunk/abjad/tools/abctools/SortableAttributeEqualityAbjadObject/SortableAttributeEqualityAbjadObject.py
+++ trunk/abjad/tools/abctools/SortableAttributeEqualityAbjadObject/SortableAttributeEqualityAbjadObject.py
@@ -1,9 +1,8 @@
 from abc import ABCMeta
-from abjad.tools.abctools.AbjadObject import AbjadObject
+from abjad.tools.abctools.AttributeEqualityAbjadObject import AttributeEqualityAbjadObject
 
 
-# TODO: inherit from AttributeEqualityAbjadObject
-class SortableAttributeEqualityAbjadObject(AbjadObject):
+class SortableAttributeEqualityAbjadObject(AttributeEqualityAbjadObject):
     '''.. versionadded:: 2.0
 
     Abstact base class to confer sortable attribute-equality to any custom class.
@@ -18,14 +17,6 @@ class SortableAttributeEqualityAbjadObject(AbjadObject):
     __slots__ = ('_comparison_attribute', '_format_string')
 
     ### OVERLOADS ###
-
-    def __eq__(self, arg):
-        '''Initialize new object from `arg` and evaluate comparison attributes.
-
-        Return boolean.
-        '''
-        arg = self._massage_comparison_arg(arg)
-        return arg and self._comparison_attribute == arg._comparison_attribute
 
     def __ge__(self, arg):
         '''Initialize new object from `arg` and evaluate comparison attributes.
@@ -58,27 +49,3 @@ class SortableAttributeEqualityAbjadObject(AbjadObject):
         '''
         arg = self._massage_comparison_arg(arg)
         return arg and self._comparison_attribute < arg._comparison_attribute
-
-    def __ne__(self, arg):
-        '''Initialize new object from `arg` and evaluate comparison attributes.
-
-        Return boolean.
-        '''
-        return not self == arg
-
-    def __repr__(self):
-        '''Interpreter representation of object defined equal to class name and comparison attribute.
-
-        Return string.
-        '''
-        return '%s(%s)' % (self.__class__.__name__, self._comparison_attribute)
-
-    ### PRIVATE METHODS ###
-
-    def _massage_comparison_arg(self, arg):
-        if not isinstance(arg, type(self)):
-            try:
-                arg = type(self)(arg)
-            except (ValueError, TypeError):
-                return False
-        return arg
