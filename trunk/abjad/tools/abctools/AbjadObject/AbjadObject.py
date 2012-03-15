d9909e3fa980cb303ed177fe4b8769508008a8db
Trevor Bača <trevorbaca@gmail.com>
2012-03-15 18:34:12 +0000

    Extended AbjadObject indented reprs to work with multiple complex keyword objects, again.

--- trunk/abjad/tools/abctools/AbjadObject/AbjadObject.py
+++ trunk/abjad/tools/abctools/AbjadObject/AbjadObject.py
@@ -191,11 +191,14 @@ class AbjadObject(object):
                 if not isinstance(value, types.MethodType):
                     if hasattr(value, '_get_tools_package_qualified_repr_pieces'):
                         pieces = value._get_tools_package_qualified_repr_pieces(is_indented=is_indented)
-                        assert 2 < len(pieces)
-                        result.append('{}{}={}'.format(prefix, name, pieces[0]))
-                        for piece in pieces[1:-1]:
-                            result.append('{}{}'.format(prefix, piece)) 
-                        result.append('{}{}{}'.format(prefix, pieces[-1], suffix))
+                        if len(pieces) == 1:
+                            result.append('{}{}={}{}'.format(prefix, name, pieces[0], suffix))
+                        else:
+                            assert 3 <= len(pieces)
+                            result.append('{}{}={}'.format(prefix, name, pieces[0]))
+                            for piece in pieces[1:-1]:
+                                result.append('{}{}'.format(prefix, piece)) 
+                            result.append('{}{}{}'.format(prefix, pieces[-1], suffix))
                     elif hasattr(value, '_tools_package'):
                         result.append('{}{}={}.{!r}{}'.format(
                             prefix, name, value._tools_package, value, suffix))
