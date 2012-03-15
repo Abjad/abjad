851400c83cb1cab3aa0005d771e8fbdc57b9f6de
Trevor Bača <trevorbaca@gmail.com>
2012-03-15 05:39:50 +0000

    Extended Performer to work with new indenting repr.

--- trunk/abjad/tools/scoretools/Performer/test/test_Performer__tools_package_qualified_indented_repr.py
+++ trunk/abjad/tools/scoretools/Performer/test/test_Performer__tools_package_qualified_indented_repr.py
@@ -1,14 +1,22 @@
 from abjad import *
-import py
 
 
 def test_Performer__tools_package_qualified_indented_repr_01():
-    py.test.skip('make indented repr work.')
 
     performer = scoretools.Performer('Flute')
     performer.instruments.append(instrumenttools.Flute())
     performer.instruments.append(instrumenttools.AltoFlute())
 
-    assert performer._tools_package_qualified_repr == "scoretools.Performer(name='Flute', instruments=[instrumenttools.Flute(), instrumenttools.AltoFlute()])"
+    assert performer._tools_package_qualified_repr == "scoretools.Performer(name='Flute', instruments=instrumenttools.InstrumentInventory([instrumenttools.Flute(), instrumenttools.AltoFlute()]))"
 
-    assert performer._tools_package_qualified_indented_repr == '???'
+    r'''
+    scoretools.Performer(
+        name='Flute',
+        instruments=instrumenttools.InstrumentInventory([
+            instrumenttools.Flute(),
+            instrumenttools.AltoFlute()
+            ])
+        )
+    '''
+
+    assert performer._tools_package_qualified_indented_repr == "scoretools.Performer(\n\tname='Flute',\n\tinstruments=instrumenttools.InstrumentInventory([\n\t\tinstrumenttools.Flute(),\n\t\tinstrumenttools.AltoFlute()\n\t\t])\n\t)"
