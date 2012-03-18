8b662c9aa36be247f8a8efe2522c0a1e1c25a4df
Trevor Bača <trevorbaca@gmail.com>
2012-03-18 18:41:55 +0000

    Changed vocalist test files to conform to new, terser storage format.

--- trunk/abjad/tools/instrumenttools/BassVoice/test/test_instrumenttools_BassVoice__tools_package_qualified_indented_repr.py
+++ trunk/abjad/tools/instrumenttools/BassVoice/test/test_instrumenttools_BassVoice__tools_package_qualified_indented_repr.py
@@ -5,4 +5,4 @@ def test_instrumenttools_BassVoice__tools_package_qualified_indented_repr_01():
 
     voice = instrumenttools.BassVoice()
 
-    assert voice._tools_package_qualified_indented_repr == "instrumenttools.BassVoice(\n\tinstrument_name='bass voice',\n\tinstrument_name_markup=markuptools.Markup(\n\t\t'Bass voice',\n\t\tstyle_string='backslash'\n\t\t),\n\tshort_instrument_name='bass',\n\tshort_instrument_name_markup=markuptools.Markup(\n\t\t'Bass',\n\t\tstyle_string='backslash'\n\t\t)\n\t)"
+    assert voice._tools_package_qualified_indented_repr == 'instrumenttools.BassVoice()'
--- trunk/abjad/tools/instrumenttools/SopranoVoice/test/test_instrumenttools_SopranoVoice__tools_package_qualified_indented_repr.py
+++ trunk/abjad/tools/instrumenttools/SopranoVoice/test/test_instrumenttools_SopranoVoice__tools_package_qualified_indented_repr.py
@@ -5,4 +5,4 @@ def test_instrumenttools_SopranoVoice__tools_package_qualified_indented_repr_01(
 
     voice = instrumenttools.SopranoVoice()
 
-    assert voice._tools_package_qualified_indented_repr == "instrumenttools.SopranoVoice(\n\tinstrument_name='soprano voice',\n\tinstrument_name_markup=markuptools.Markup(\n\t\t'Soprano voice',\n\t\tstyle_string='backslash'\n\t\t),\n\tshort_instrument_name='soprano',\n\tshort_instrument_name_markup=markuptools.Markup(\n\t\t'Soprano',\n\t\tstyle_string='backslash'\n\t\t)\n\t)"
+    assert voice._tools_package_qualified_indented_repr == 'instrumenttools.SopranoVoice()'
