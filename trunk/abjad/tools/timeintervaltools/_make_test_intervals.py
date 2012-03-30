7f682c9a2ae5f744c6997f192591446fe05992b0
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2012-03-30 19:54:53 +0000

    Changed interval definitions in timeintervaltools._make_test_intervals()

--- trunk/abjad/tools/timeintervaltools/_make_test_intervals.py
+++ trunk/abjad/tools/timeintervaltools/_make_test_intervals.py
@@ -4,16 +4,16 @@ from abjad.tools.timeintervaltools import TimeInterval
 def _make_test_intervals():
 
     return [
-        TimeInterval(0, 3,   {'a': 1}),
-        TimeInterval(5, 13,  {'b': 2}),
-        TimeInterval(6, 10,  {'c': 3}),
-        TimeInterval(8, 9,   {'d': 4}),
-        TimeInterval(15, 23, {'e': 5}),
-        TimeInterval(16, 21, {'f': 6}),
-        TimeInterval(17, 19, {'g': 7}),
-        TimeInterval(19, 20, {'h': 8}),
-        TimeInterval(25, 30, {'i': 9}),
-        TimeInterval(26, 29, {'j': 10}),
-        TimeInterval(32, 34, {'k': 11}),
-        TimeInterval(34, 37, {'l': 12}),
+        TimeInterval(0, 3,   {'name': 'a'}),
+        TimeInterval(5, 13,  {'name': 'b'}),
+        TimeInterval(6, 10,  {'name': 'c'}),
+        TimeInterval(8, 9,   {'name': 'd'}),
+        TimeInterval(15, 23, {'name': 'e'}),
+        TimeInterval(16, 21, {'name': 'f'}),
+        TimeInterval(17, 19, {'name': 'g'}),
+        TimeInterval(19, 20, {'name': 'h'}),
+        TimeInterval(25, 30, {'name': 'i'}),
+        TimeInterval(26, 29, {'name': 'j'}),
+        TimeInterval(32, 34, {'name': 'k'}),
+        TimeInterval(34, 37, {'name': 'l'}),
     ]
