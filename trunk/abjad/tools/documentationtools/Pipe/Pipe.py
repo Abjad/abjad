fc2422eb9400a0e25b18b5e93c8a8075d5b1bc2f
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2012-04-02 19:54:38 +0000

    Improved documentationtools.Pipe documentation, added writeline method.

--- trunk/abjad/tools/documentationtools/Pipe/Pipe.py
+++ trunk/abjad/tools/documentationtools/Pipe/Pipe.py
@@ -5,19 +5,30 @@ from abjad.tools import abctools
 
 class Pipe(abctools.AbjadObject, subprocess.Popen):
     '''A two-way, non-blocking pipe for interprocess communication:
-
+    
     ::
-
+    
         abjad> from abjad.tools.documentationtools import Pipe
+    
+    ::
+    
         abjad> pipe = Pipe('python', ['-i'])
-        abjad> pipe.write('my_list = [1, 2, 3]\n')
-        abjad> pipe.write('print my_list')
-
+        abjad> pipe.writeline('my_list = [1, 2, 3]')
+        abjad> pipe.writeline('print my_list')
+    
     Return `Pipe` instance.
     '''
 
-    def __init__(self, exe='python', args=['-i'], timeout=0):
-        self.timeout = timeout
+    ### CLASS ATTRIBUTES ###
+
+    __slots__ = ('_arguments', '_executable', '_timeout')
+
+    ### INITIALIZER ###
+
+    def __init__(self, executable='python', arguments=['-i'], timeout=0):
+        self._arguments = arguments
+        self._executable = executable
+        self._timeout = timeout
         argv = [exe]
         if args != None:
             argv = argv + args
@@ -26,18 +37,21 @@ class Pipe(abctools.AbjadObject, subprocess.Popen):
             stdout=subprocess.PIPE,
             stderr=subprocess.STDOUT)
 
-    def close(self):
-        self.terminate()
-        self.wait()
+    ### PUBLIC ATTRIBUTES ###
 
-    def write(self, data):
-        poll = select.poll()
-        poll.register(self.stdin.fileno(), select.POLLOUT)
-        fd = poll.poll(self.timeout)
-        if len(fd):
-            f = fd[0]
-            if f[1] > 0:
-                self.stdin.write(data)
+    @property
+    def arguments(self):
+        return self._arguments
+
+    @property
+    def executable(self):
+        return self._executable
+
+    @property
+    def timeout(self):
+        return self._timeout
+
+    ### PRIVATE METHODS ###
 
     def _readbyte(self, n = 1):
         poll = select.poll()
@@ -48,10 +62,32 @@ class Pipe(abctools.AbjadObject, subprocess.Popen):
             if f[1] > 0:
                 return self.stdout.read(n)
 
+    ### PUBLIC METHODS ###
+
+    def close(self):
+        '''Close the pipe.'''
+        self.terminate()
+        self.wait()
+
     def read(self, n = 1):
+        '''Read from the pipe.'''
         c = self._readbyte()
         string = ""
         while c != None:
             string = string + str(c)
             c = self._readbyte()
         return string
+
+    def write(self, data):
+        '''Write `data` into the pipe.'''
+        poll = select.poll()
+        poll.register(self.stdin.fileno(), select.POLLOUT)
+        fd = poll.poll(self.timeout)
+        if len(fd):
+            f = fd[0]
+            if f[1] > 0:
+                self.stdin.write(data)
+
+    def writeline(self, data):
+        '''Write `data` into the pipe, terminated by a newline.'''
+        self.write('%s\n' % data)
