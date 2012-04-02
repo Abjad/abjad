d68457f00a4faf0804c21a175f007e4193fffdc8
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2012-04-02 18:25:22 +0000

    Added root_class keyword to documentationtools.InheritanceGraph, for filtering out inheritance arcs which do not include that root class.

--- trunk/abjad/tools/documentationtools/InheritanceGraph/InheritanceGraph.py
+++ trunk/abjad/tools/documentationtools/InheritanceGraph/InheritanceGraph.py
@@ -1,3 +1,5 @@
+import inspect
+import types
 from abjad.tools.datastructuretools import ImmutableDictionary
 
 
@@ -15,31 +17,66 @@ class InheritanceGraph(ImmutableDictionary):
         ...
         abjad> class B(A): pass
         ...
-        abjad> class C(A): pass
+        abjad> class C(B): pass
         ...
-        abjad> class D(B, C): pass
+        abjad> class D(B): pass
         ...
-        abjad> class E(object): pass
+        abjad> class E(C, D): pass
         ...
-        abjad> graph = InheritanceGraph(D, E)
+        abjad> class F(A): pass
+        ...
+
+    ::
+
+        abjad> graph = InheritanceGraph(F, E)
+
+    ::
+
         abjad> for parent, children in sorted(graph.items()):
         ...     parent, children
-        (<type 'object'>, (<class '__main__.A'>, <class '__main__.E'>))
-        (<class '__main__.A'>, (<class '__main__.B'>, <class '__main__.C'>))
-        (<class '__main__.B'>, (<class '__main__.D'>,))
-        (<class '__main__.C'>, (<class '__main__.D'>,))
-        (<class '__main__.D'>, ())
+        (<type 'object'>, (<class '__main__.A'>,))
+        (<class '__main__.A'>, (<class '__main__.B'>, <class '__main__.F'>))
+        (<class '__main__.B'>, (<class '__main__.C'>, <class '__main__.D'>))
+        (<class '__main__.C'>, (<class '__main__.E'>,))
+        (<class '__main__.D'>, (<class '__main__.E'>,))
         (<class '__main__.E'>, ())
+        (<class '__main__.F'>, ())
 
     ``InheritanceGraph`` may be instantiated from one or more instances, classes or
     modules.  If instantiated from a module, all public classes in that module
     will be taken into the graph.
 
+    A `root_class` keyword may be defined at instantiation, which filters out
+    all klasses from the graph which do not inherit from that `root_class` (or
+    are not already the `root_class`):
+
+    ::
+
+        abjad> graph = InheritanceGraph(A, B, C, D, E, F, root_class=B)
+        abjad> for parent, children in sorted(graph.items()):
+        ...     parent, children
+        (<class '__main__.B'>, (<class '__main__.C'>, <class '__main__.D'>))
+        (<class '__main__.C'>, (<class '__main__.E'>,))
+        (<class '__main__.D'>, (<class '__main__.E'>,))
+        (<class '__main__.E'>, ())
+
     Returns ``InheritanceGraph`` instance.
     '''
 
-    def __init__(self, *args):
+    ### CLASS ATTRIBUTES ###
+
+    __slots__ = ('_root_class')
+
+    ### INITIALIZER ###
+
+    def __init__(self, *args, **kwargs):
         assert 0 < len(args)
+
+        if 'root_class' in kwargs:
+            assert isinstance(kwargs['root_class'], types.TypeType)
+            self._root_class = kwargs['root_class']
+        else:
+            self._root_class = None
 
         klasses = []
         for x in args:
@@ -50,22 +87,46 @@ class InheritanceGraph(ImmutableDictionary):
             else:
                 klasses.append(x.__class__)
 
-        graph = { }
+        if self.root_class is None:
+            graph = {}
 
-        def _recurse(klass):
-            if klass not in graph:
-                graph[klass] = [ ]
-            for base in klass.__bases__:
-                if base not in graph:
-                    graph[base] = [ ]
-                if klass not in graph[base]:
-                    graph[base].append(klass)
-                _recurse(base)
+            def _recurse(klass):
+                if klass not in graph:
+                    graph[klass] = []
+                for base in klass.__bases__:
+                    if base not in graph:
+                        graph[base] = []
+                        _recurse(base)
+                    if klass not in graph[base]:
+                        graph[base].append(klass)
 
-        for klass in klasses:
-            _recurse(klass)
+            for klass in klasses:
+                _recurse(klass)
+
+        else:
+            graph = {self.root_class: []}
+
+            def _recurse(klass):
+                if self.root_class in inspect.getmro(klass):
+                    graph[klass] = []
+                for base in klass.__bases__:
+                    if base in graph:
+                        if klass not in graph[base]:
+                            graph[base].append(klass)
+                    elif self.root_class in inspect.getmro(base):
+                        graph[base] = [klass]
+                        _recurse(base)
+
+            for klass in klasses:
+                _recurse(klass)
 
         for k, v in graph.iteritems():
             graph[k] = tuple(sorted(v))
 
         dict.__init__(self, graph)
+
+    ### PUBLIC ATTRIBUTES ###
+
+    @property
+    def root_class(self):
+        return self._root_class
