95f1db8a5a62af6248a80f1fc9bba0c43f42840f
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2012-04-06 16:11:31 +0000

    Consolidated docs/scr/html-docs into docs/scr/make-abjad-api. Deleted all files but make-abjad-api from docs/scr.

--- trunk/abjad/docs/scr/_get_documenting_directories.py
+++
@@ -1,6 +0,0 @@
-def _get_documenting_directories():
-   '''Get top-level Abjad directories to search for
-   classes and functions to document in public API.
-   '''
-
-   return ['components', 'interfaces', 'marks', 'spanners', 'tools', ]
