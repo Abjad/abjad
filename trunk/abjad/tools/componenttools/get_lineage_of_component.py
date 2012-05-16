12269f190857aa6dcb882d636b03eeb7b0a77e89
Trevor Bača <trevorbaca@gmail.com>
2012-05-16 23:35:57 +0000

    Added function to componenttools API:
        * componenttools.get_lineage_of_component()
    
    The function returns a list. The list comprises all parents of component,
    component itself, and all descendents of component.
    
    See the API entry for an example.

---
+++ trunk/abjad/tools/componenttools/get_lineage_of_component.py
@@ -0,0 +1,42 @@
+def get_lineage_of_component(component):
+    r'''.. versionadded:: 2.9
+
+    Get lineage of `component`::
+
+        abjad> staff = Staff(r"c'4 \times 2/3 { d'8 e'8 f'8 }")
+
+    ::
+
+        f(staff)
+        \new Staff {
+            c'4
+            \times 2/3 {
+                d'8
+                e'8
+                f'8
+            }
+        }
+
+    ::
+
+        componenttools.get_lineage_of_component(staff[1])
+        [Staff{2}, Tuplet(2/3, [d'8, e'8, f'8]), Note("d'8"), Note("e'8"), Note("f'8")]
+
+    Return list of parentage, component and descendents.
+    '''
+    from abjad.tools import componenttools
+
+    # initialize result
+    result = []
+
+    # add parentage of component
+    result.extend(reversed(componenttools.get_proper_parentage_of_component(component)))
+
+    # add component
+    result.append(component)
+
+    # add descendents of component
+    result.extend(componenttools.get_proper_descendents_of_component(component))
+
+    # return result
+    return result
