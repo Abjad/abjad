70601f235886f2cbc2f467db3ffba085a12edb2c
Trevor Bača <trevorbaca@gmail.com>
2012-05-17 23:43:26 +0000

    Removed _NumberingInterface.

--- trunk/abjad/interfaces/_NumberingInterface/__init__.py
+++
@@ -1,1 +0,0 @@
-from _NumberingInterface import _NumberingInterface
--- trunk/abjad/interfaces/__init__.py
+++ trunk/abjad/interfaces/__init__.py
@@ -3,4 +3,3 @@ from abjad.tools import importtools
 importtools.import_structured_package(__path__[0], globals())
 
 from _OffsetInterface import _OffsetInterface
-from _NumberingInterface import _NumberingInterface
