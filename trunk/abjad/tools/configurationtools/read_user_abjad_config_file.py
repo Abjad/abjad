11e4d5eaf63c446246cd494ee63ba020cfc27a83
Trevor Bača <trevorbaca@gmail.com>
2012-05-23 00:15:26 +0000

    Fixed an iotools bug.
    Prior to this commit configurationtools.read_user_abjad_config_file() was reading
    the default config file.
    
    With this commig configurationtools.read_user_abjad_config_file() reads the user abjad config file.

--- trunk/abjad/tools/configurationtools/read_user_abjad_config_file.py
+++ trunk/abjad/tools/configurationtools/read_user_abjad_config_file.py
@@ -1,7 +1,7 @@
 from abjad.cfg.cfg import ABJADCONFIG
 
 
-def read_user_abjad_config_file(attribute_name=None):
+def read_user_abjad_config_file(attribute_name):
     '''Read the content of the config file ``$HOME/.abjad/config.py``.
 
     Returns a dictionary of var : value entries.
@@ -10,9 +10,4 @@ def read_user_abjad_config_file(attribute_name=None):
 
     configurationtools.verify_abjad_config_file()
 
-    config_file_dict = configurationtools.get_default_abjad_config_file_as_dict()
-
-    if attribute_name is not None:
-        return config_file_dict[attribute_name]['value']
-    else:
-        return config_file_dict
+    return configurationtools.get_user_abjad_config_file_as_dict()[attribute_name]
