601d887ea0546ad24f12746e0f97f4507b7d1796
Trevor Bača <trevorbaca@gmail.com>
2012-06-06 15:07:26 +0000

    Made configurationtools.get_abjad_startup_string() public.
    The configurationtools package no longer implements any private functionality.

--- trunk/abjad/tools/configurationtools/get_abjad_startup_string.py
+++ trunk/abjad/tools/configurationtools/get_abjad_startup_string.py
@@ -2,6 +2,6 @@ from abjad.tools.configurationtools.get_abjad_revision_string import get_abjad_r
 from abjad.tools.configurationtools.get_abjad_version_string import get_abjad_version_string
 
 
-def _get_abjad_startup_string():
+def get_abjad_startup_string():
 
     return 'Abjad %s (r%s)' % (get_abjad_version_string(), get_abjad_revision_string())
