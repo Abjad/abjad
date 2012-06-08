f515ef1ab8c4cbabeb6cfc4e16fd5981615b4300
Trevor Bača <trevorbaca@gmail.com>
2012-06-08 20:44:05 +0000

    Added test for nonbinary division.

---
+++ experimental/specificationtools/test/test_single_segment_nonbinary_solo.py
@@ -0,0 +1,23 @@
+from abjad.tools import *
+from helpers import *
+from specificationtools import ScoreSpecification
+import specificationtools.library as library
+
+
+def test_single_segment_nonbinary_solo_01():
+    '''Nonbinary division.
+    '''
+
+    specification = ScoreSpecification(scoretemplatetools.GroupedRhythmicStavesScoreTemplate(n=1))
+
+    segment = specification.append_segment()
+    segment.set_time_signatures(segment, [(4, 8), (3, 8)])
+    segment.set_divisions(segment.v1, [(1, 5)])
+    segment.set_rhythm(segment, library.tuplet_monads)
+
+    score = specification.interpret()
+
+    current_function_name = introspectiontools.get_current_function_name()
+    write_test_output(score, __file__, current_function_name, go=True)
+
+    assert score.format == read_test_output(__file__, current_function_name)
