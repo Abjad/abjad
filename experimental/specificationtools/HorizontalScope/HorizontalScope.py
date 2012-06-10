d4223293df7718218b0b2ce9ee5e1eba983484fe
Trevor Bača <trevorbaca@gmail.com>
2012-06-10 21:24:00 +0000

    Added HorizontalScope docs.
    Horizontal scope objects probably have a lot in common with time interval objects.
    
    Horizontal scope objects are designed to model very highly generalized start and 
    stop 'cursors' of any selection anywhere.
    
    That is, selection objects pair a list of context names together with a horizontal scope
    object to pick out the 'transparency' which specifies the score area over which
    to apply any operation whatsoever.
    
    Read the HorizontalScope docs for more information.

--- experimental/specificationtools/HorizontalScope/HorizontalScope.py
+++ experimental/specificationtools/HorizontalScope/HorizontalScope.py
@@ -4,6 +4,32 @@ from abjad.tools.abctools.AbjadObject import AbjadObject
 
 
 class HorizontalScope(AbjadObject):
+    r'''.. versionadded 1.0
+
+    Horizontal scope objects share much in common with time interval objects.
+
+    Horizontal scope is essentially a fancy pair.
+
+    Horizontal scope comprises a start time and a stop time.
+
+    The purpose of a horizontal scope is to highlight a contiguous block of time
+    somewhere in some segment and say "everything within my bounds if selected
+    for some upcoming operation."
+
+    That is, horizontal scopes make up the horizontal part of a selection.
+    The thing that makes up the vertical part of a selection is a list of
+    zero or more context names. 
+
+    So a horizontal scope is an important part of a selection.
+
+    Future design consideration: horizontal scope currently admits on a single criterion.
+    This means that we can currently only say things like "start at the start time
+    of measure ``0`` in the selection and stop at the stop time of measure ``6``
+    in the selection."
+    Probably both a `start_criterion` and a `stop_criterion` will be needed to
+    generalize correctly. This will allow us to say things like "start at the start time of
+    measure ``0`` in the selection and stop at the stop time of note ``20`` in the selection."
+    '''
 
     ### CLASS ATTRIBUTES ###
 
@@ -12,11 +38,11 @@ class HorizontalScope(AbjadObject):
     ### INITIALIZER ###
 
     def __init__(self, criterion, part=None, start=None, stop=None):
-        assert self.is_valid_criterion(criterion)
-        assert self.is_valid_part_token(part)
-        assert isinstance(start, (int, type(None)))
-        assert isinstance(stop, (int, type(None)))
-        assert self.are_concordant_input_values(part, start, stop)
+        assert self.is_valid_criterion(criterion), repr(criterion)
+        assert self.is_valid_part_token(part), repr(part)
+        assert isinstance(start, (int, type(None))), repr(start)
+        assert isinstance(stop, (int, type(None))), repr(stop)
+        assert self.are_concordant_input_values(part, start, stop), repr((start, stop))
         self.criterion = criterion
         self.part = part
         self.start = start
@@ -25,18 +51,50 @@ class HorizontalScope(AbjadObject):
     ### PUBLIC METHODS ###
 
     def all_are_component_subclasses(self, expr):
+        r'''True when `expr` is an iterable and all elements in `expr`
+        are subclasses of the Abjad component class. False otherwise.
+    
+        Abjad components can be used to pick out the `start` and `stop`
+        values of a horizontal scope.
+
+        Return boolean.
+        '''
         try:
             return all([issubclass(x, componenttools.Component) for x in expr])
         except:
             return False
 
     def are_concordant_input_values(self, part, start, stop):
+        r'''Test to make sure that `part`, `start`, `stop` all make sense together.
+
+        True when `part` is none.
+        True when `part`, `start`, `stop` are all nonnone.
+        Otherwise false.
+
+        Explanation of this remains to be written.
+
+        Return boolean.
+        '''
         if part is not None:
             if start is not None or stop is not None:
                 return False
         return True
         
     def is_valid_criterion(self, expr):
+        r'''True when `expr` is either none, a component class, or one of the strings
+        ``'divisions'`` or ``'measures'``. False otherwise.
+
+        The method indicates those things from which horziontal scope
+        `start` and `stop` times can be taken.
+
+        Tthe `start` and `stop` times of a horizontal scope can be taken
+        from anything in the score specification that is a measure, a division,
+        a tuplet, note, rest, chord or so on. The start and stop times of these
+        objects are precisely the things that can be used to draw the two
+        'cursors' that together comprise the temporal bounds of a horizontal scope.
+        
+        Return boolean.
+        '''
         if expr is None:
             return True
         elif self.all_are_component_subclasses(expr):
@@ -47,6 +105,23 @@ class HorizontalScope(AbjadObject):
             raise ValueError('invalid temporal scope criterion: {!r}'.format(expr))
 
     def is_valid_part_token(self, expr):
+        r'''True when `expr` has the form of a valid part token. False otherwise.
+
+        Valid part tokens look like ``[(1, 1, 1), 0]``.
+
+        This means to divide something by the ratio ``1:1:1`` and then to take the 0th part that results.
+
+        To divide something into thirds and take the last part you would need
+        a part token that looks like ``[(1, 1, 1), 2]``.
+
+        Part tokens arise from a type of compositional thinking that partitions larger
+        stretches of time into parts according to ratios in order to pick out
+        individual parts of the resulting partition.
+    
+        Future design consideration: part tokens should probably be modeled as an explicit class.
+
+        Return boolean.
+        '''
         if expr is None:
             return True
         elif isinstance(expr, (tuple, list)) and len(expr) == 2:
