a0f8a240dfe4977b0de0f24cf54dbb781c414309
Trevor Bača <trevorbaca@gmail.com>
2012-06-11 00:01:24 +0000

    Added AttributeNameEnumeration docs.

--- experimental/specificationtools/AttributeNameEnumeration/AttributeNameEnumeration.py
+++ experimental/specificationtools/AttributeNameEnumeration/AttributeNameEnumeration.py
@@ -2,6 +2,32 @@ from collections import OrderedDict
 
 
 class AttributeNameEnumeration(OrderedDict):
+    r'''.. versionadded:: 1.0
+
+    The allowable attribute names known to the ``specificationtools`` classes::
+
+        >>> from experimental import specificationtools
+
+    ::
+
+        >>> attribute_names = specificationtools.AttributeNameEnumeration()
+
+    ::
+
+        >>> for key, value in attribute_names.iteritems(): key, value
+        ... 
+        ('articulations', 0)
+        ('divisions', 1)
+        ('pitch_classes', 2)
+        ('rhythm', 3)
+        ('time_signatures', 4)
+
+    Attribute names are alphabetized.
+
+    The integer constants serve only to fit the attribute names into dictionary form.
+
+    The class will grow as more attributes are added to the system.
+    '''
 
     ### CLASS ATTIRBUTES ###
 
