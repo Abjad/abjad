9edd2a4c3e56b76c48dd95d0638159020f160f4c
Trevor Bača <trevorbaca@gmail.com>
2012-06-11 20:39:26 +0000

    Extended TemporalCursor examples.
    Feedback is now requested at any point.
    
    The examples provided in the docstring can be taken as a preliminary design proposal.
    
    Should now be clear how the design of TemporalCursor depends on that of ScoreObjectIndicator.
    
    This is accomplished through the idea of 'anchored' cursors.

--- experimental/specificationtools/TemporalCursor/TemporalCursor.py
+++ experimental/specificationtools/TemporalCursor/TemporalCursor.py
@@ -1,49 +1,119 @@
 from abjad.tools import durationtools
 from abjad.tools.abctools.AbjadObject import AbjadObject
+from experimental.specificationtools.ScoreObjectIndicator import ScoreObjectIndicator
 import fractions
 
 
 class TemporalCursor(AbjadObject):
     r'''.. versionadded:: 1.0
 
-    An infinitely thin vertical line coincident with exactly one timepoint in score.
+    An infinitely thin vertical line coincident with an arbitrary object-relative timepoint in score.
 
-    Temporal cursors are designed to model sophisticated timepoint location
-    made relative to an arbitrary object.
+    (A type of object-oriented delayed evaluation.)
 
-    Every temporal cursor resolves to a rational-valued score offset.
+    Temporal cursors are designed to pick out timepoints in a sophisticated way.
 
-    A temporal cursor is defined equal to a collection of the following three things::
+    Temporal cursors locate timepoints relative to arbitrary objects in score.
 
-        * score object indicator
-        * boolean start indicator
-        * rational start offset
+    Temporal cursors resolve to a rational-valued score offset.
 
-    All three values are optional.
+    Initialize with different combinations of optional `anchor`, 
+    `start`, `scalar` and `addendum`.
 
-    A score object indicator is defined equal to a collection of the following four things::
+    Import ``specificationtools``::
 
-        * score segment
-        * component class
-        * component predicate
-        * integer index
+        >>> from experimental import specificationtools
 
-    All four values are optional.
+    Pick out the timepoint equal to start of score::
 
-    Score object indicators should be modeled by a dedicated class.
-    So that will happen next before returning to the implementation here.
+        >>> specificationtools.TemporalCursor()
+        TemporalCursor()
+
+    Pick out the timepoint equal to end of score::
+
+        >>> specificationtools.TemporalCursor(start=False)
+        TemporalCursor(start=False)
+
+    Pick out the timepoint exactly ``1/8`` of a whole note into score::
+
+        >>> specificationtools.TemporalCursor(addendum=Fraction(1, 8))
+        TemporalCursor(addendum=Offset(1, 8))
+
+    Pick out the timepoint exactly one third of the way into score::
+
+        >>> specificationtools.TemporalCursor(scalar=Fraction(1, 3), start=False)
+        TemporalCursor(scalar=Fraction(1, 3), start=False)
+
+    Pick out the timepoint exactly ``1/8`` of a whole note after the first third of score::
+
+        >>> specificationtools.TemporalCursor(scalar=Fraction(1, 3), start=False, addendum=Fraction(1, 8))
+        TemporalCursor(scalar=Fraction(1, 3), start=False, addendum=Offset(1, 8))
+
+    Pick out the timepoint equal to the start of the segment with name ``'red'``::
+
+        >>> anchor = specificationtools.ScoreObjectIndicator(segment='red')
+
+    ::
+
+        >>> specificationtools.TemporalCursor(anchor=anchor)
+        TemporalCursor(anchor=ScoreObjectIndicator(segment='red'))
+
+    Pick out the timepoint equal to the end of the segment with name ``'red'``::
+
+        >>> specificationtools.TemporalCursor(anchor=anchor, start=False)
+        TemporalCursor(anchor=ScoreObjectIndicator(segment='red'), start=False)
+
+    Pick out the timepoint equal to ``1/8`` of a whole note after the start of 
+    the segment with name ``'red'``::
+
+        >>> specificationtools.TemporalCursor(anchor=anchor, addendum=Fraction(1, 8))
+        TemporalCursor(anchor=ScoreObjectIndicator(segment='red'), addendum=Offset(1, 8))
+
+    Pick out the timepoint equal to exactly one third of the way into
+    the segment with name ``'red'``::
+
+        >>> specificationtools.TemporalCursor(anchor=anchor, scalar=Fraction(1, 3), start=False)
+        TemporalCursor(anchor=ScoreObjectIndicator(segment='red'), scalar=Fraction(1, 3), start=False)
+
+    Pick out the point equal to ``1/8`` of a whole note after the end of the first third of
+    the segment with name ``'red'``::
+    
+        >>> specificationtools.TemporalCursor(anchor=anchor, scalar=Fraction(1, 3), start=False, addendum=Fraction(1, 8))
+        TemporalCursor(anchor=ScoreObjectIndicator(segment='red'), scalar=Fraction(1, 3), start=False, addendum=Offset(1, 8))
+
+    Pick out the timepoint equal to the start of note ``10`` in context ``'Voice 1'`` of
+    the segment with name ``'red'``::
+
+        >>> anchor = specificationtools.ScoreObjectIndicator(segment='red', context='Voice 1', klass=Note, index=10)
+        >>> specificationtools.TemporalCursor(anchor=anchor)
+        TemporalCursor(anchor=ScoreObjectIndicator(segment='red', context='Voice 1', klass=notetools.Note, index=10))
+
+    Pick out the timepoint equal to the end of note ``10`` in context ``'Voice 1'`` of
+    the segment with name ``'red'``::
+
+        >>> specificationtools.TemporalCursor(anchor=anchor, start=False)
+        TemporalCursor(anchor=ScoreObjectIndicator(segment='red', context='Voice 1', klass=notetools.Note, index=10), start=False)
+
+    Examples below reference the temporal curosr defined immediately above::
+
+        >>> temporal_cursor = _
+
+    Temporal cursors are immutable.
     '''
 
     ### INITIALIZER ###
 
-    def __init__(self, score_object_indicator=None, start_indicator=None, start_offset=None): 
-        assert isinstance(score_object_indicator, (ScoreObjectIndicator, type(None))), repr(score_object_indicator)
-        assert isinstance(start_indicator, (bool, type(None))), repr(start_indicator)
-        if start_offset is not None:
-            start_offset = durationtools.Offset(start_offset)
-        self._score_object_indicator = score_object_indicator
-        self._start_indicator = start_indicator
-        self._start_offset = start_offset
+    def __init__(self, anchor=None, scalar=None, start=None, addendum=None): 
+        assert isinstance(anchor, (ScoreObjectIndicator, type(None))), repr(anchor)
+        assert isinstance(scalar, (fractions.Fraction, type(None))), repr(scalar)
+        assert isinstance(start, (bool, type(None))), repr(start)
+        if addendum is not None:
+            addendum = durationtools.Offset(addendum)
+        assert isinstance(addendum, (durationtools.Offset, type(None))), repr(addendum)
+        self._anchor = anchor
+        self._scalar = scalar
+        self._start = start
+        self._addendum = addendum
 
     ### SPECIAL METHODS ###
 
@@ -57,7 +127,7 @@ class TemporalCursor(AbjadObject):
         '''
         if not isinstance(other, type(self)):
             return False
-        elif not self.score_object_indicator == other.score_object_indicator:
+        elif not self.anchor == other.anchor:
             return False
         elif not self.start == other.start:
             return False
@@ -97,10 +167,37 @@ class TemporalCursor(AbjadObject):
     ### READ-ONLY PUBLIC PROPERTIES ###
 
     @property
-    def score_object_indicator(self):
-        '''Score object indicator.
+    def addendum(self):
+        '''Temporal cursor addendum specified by user.
+
+            >>> temporal_cursor.addendum is None
+            True
+            
+        Return offset or none.
         '''
-        return self._score_object_indicator
+        return self._addendum
+
+    @property
+    def anchor(self):
+        '''Anchor of temporal cursor specified by user.
+        
+            >>> temporal_cursor.anchor
+            ScoreObjectIndicator(segment='red', context='Voice 1', klass=notetools.Note, index=10)
+
+        Return score object indicator or none.
+        '''
+        return self._anchor
+
+    @property
+    def scalar(self):
+        '''Temporal cursor scalar specified by user.
+
+            >>> temporal_cursor.scalar is None
+            True
+
+        Return fraction or none.
+        '''
+        return self._scalar
 
     @property
     def score_offset(self):
@@ -109,17 +206,12 @@ class TemporalCursor(AbjadObject):
         raise NotImplementedError
 
     @property
-    def start_indicator(self):
-        '''Start indicator.
+    def start(self):
+        '''Temporal cursor start indicator specified by user.
         
+            >>> temporal_cursor.start
+            False
+
         Return boolean or none.
         '''
-        return self._start_indicator
-
-    @property
-    def start_offset(self):
-        '''Score object offset.
-
-        Return offset or none.
-        '''
-        return self._start_offset
+        return self._start
