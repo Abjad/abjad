82a1aa1d5d71e64d72ab77b67b46566e62debee6
Trevor Bača <trevorbaca@gmail.com>
2012-06-13 19:27:39 +0000

    Added new ContextSelection class. The class inherits from Selection. Whereas the Selection class models arbitrary contexts taken over an arbitrary timespan, the new ContextSelection class models exactly one context taken over an arbitrary timespan.

---
+++ experimental/specificationtools/ContextSelection/ContextSelection.py
@@ -0,0 +1,25 @@
+from abjad.tools import contexttools
+from experimental.specificationtools.Selection.Selection import Selection
+
+
+class ContextSelection(Selection):
+    r'''.. versionadded:: 1.0
+
+    Exactly one context taken over arbitrary temporal scope.
+    '''
+
+    ### INITIALIZER ###
+
+    def __init__(self, context, scope=None):
+        assert isinstance(context, (str, contexttools.Context)), repr(context)
+        Selection.__init__(self, [context], scope=scope)
+
+    ### READ-ONLY PUBLIC PROPERTIES ###
+
+    @property
+    def context(self):
+        '''Name of context selection context specified by user.
+
+        Return string.
+        '''
+        return self.contexts[0]
---
+++ experimental/specificationtools/ContextSelection/__init__.py
@@ -0,0 +1,1 @@
+from ContextSelection import ContextSelection
