14261275f927d6ec2e5c0aa73591bdfadcc902fc
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2012-06-18 16:20:11 +0000

    Implemented optional regex searching in developerscripttools.ReplaceInFilesScript.
    The script now supports:
        - both naive and regex-based searches
        - forced replacement (as with the old replace-in-files script's "false")
        - file and folder name filtering via bash wildcards (i.e. *.txt)
    
    The current usage:
    
        $ abj-dev replace text --help
        usage: replace-in-files [-h] [--version] [--force] [--regex] [-WF PATTERN]
                                [-WD PATTERN]
                                path old new
    
        Replace text.
    
        positional arguments:
          path                  directory tree to be recursed over
          old                   old text
          new                   new text
    
        optional arguments:
          -h, --help            show this help message and exit
          --version             show program's version number and exit
          --force               force "yes" to every replacement
          --regex               treat "old" as a regular expression
          -WF PATTERN, --without-files PATTERN
                                Exclude files matching pattern(s)
          -WD PATTERN, --without-dirs PATTERN
                                Exclude folders matching pattern(s)

--- experimental/developerscripttools/ReplaceInFilesScript/ReplaceInFilesScript.py
+++ experimental/developerscripttools/ReplaceInFilesScript/ReplaceInFilesScript.py
@@ -1,6 +1,7 @@
 from experimental.developerscripttools.DirectoryScript import DirectoryScript
 import fnmatch
 import os
+import re
 
 
 class ReplaceInFilesScript(DirectoryScript):
@@ -56,9 +57,7 @@ class ReplaceInFilesScript(DirectoryScript):
 
     def _process_file(self, args, path):
 
-        old, new = args.old, args.new
-        length = len(args.old)
-        difference = len(args.old) - len(args.new)
+        changed_items = 0
         changed_lines = 0
 
         with open(path, 'r') as f:
@@ -66,45 +65,76 @@ class ReplaceInFilesScript(DirectoryScript):
 
         results = []
         for i, line in enumerate(lines):
-
-
-            changed = False
-            index = line.find(old)
-            while 0 <= index:
-
-                if args.force:
-                    line = line[:index] + new + line[index+length:]
-                    index += length - difference
-                    changed = True
-
-                else:
-                    carats = (' ' * index) + ('^' * length)
-                    print ''
-                    print '{}: {}'.format(path, i)
-                    print '{}'.format(line)
-            	    print '{}'.format(carats)
-                    print ''
-                    result = raw_input('Replace? [Y/n] > ').lower()
-                    while result not in ('', 'y', 'yes', 'n', 'no'):
-                        result = raw_input('Replace? [Y/n] > ').lower()
-                    if result in ('', 'y', 'yes'):
-                        line = line[:index] + new + line[index+length:]
-                        index += length - difference
-                        changed = True
-                    else:
-                        index += length
-
-                index = line.find(old, index)
-
+            line, changes = self._process_line(line, i, path, args.old, args.new, args.force)
             results.append(line)
-            if changed:
+            if changes:
+                changed_items += changes
                 changed_lines += 1
 
-            if results != lines:
-                with open(path, 'w') as f:
-                    f.write('\n'.join(results))
+        if results != lines:
+            with open(path, 'w') as f:
+                f.write('\n'.join(results))
 
-        return changed_lines
+        return changed_lines, changed_items
+
+    def _process_line(self, line, line_number, filename, search, replacement, force):
+        index, changes = 0, 0
+        index, length = search(line, index)
+
+        while 0 <= index:
+
+            should_replace = False
+            if force:
+                should_replace = True
+            else:
+                carats = (' ' * index) + ('^' * length)
+                print ''
+                print '{}: {}'.format(filename, line_number)
+                print ''
+                print '{}'.format(line)
+                print '{}'.format(carats)
+                print ''
+                result = raw_input('Replace? [Y/n] > ').lower()
+                while result not in ('', 'y', 'yes', 'n', 'no'):
+                    result = raw_input('Replace? [Y/n] > ').lower()
+                if result in ('', 'y', 'yes'):
+                    should_replace = True
+
+            if should_replace:
+                line = line[:index] + replacement + line[index+length:]
+                index += length - (length - len(replacement))
+                changes += 1
+            else:
+                index += length
+
+            index, length = search(line, index)
+
+        return line, changes
+
+    def _get_naive_search_callable(self, old):
+        class NaiveSearch(object):
+            def __init__(self, pattern):
+                self.pattern = pattern
+            def __call__(self, line, pos):
+                index = line.find(self.pattern)
+                if 0 <= index:
+                    return index, len(self.pattern)
+                return -1, 0
+        return NaiveSearch(old)
+
+    def _get_regex_search_callable(self, old):
+        class RegexSearch(object):
+            def __init__(self, pattern):
+                try:
+                    self.pattern = re.compile(pattern)
+                except:
+                    raise ValueError("Can't compile {!r} as a regex pattern.".format(pattern))
+            def __call__(self, line, pos):
+                match = self.pattern.search(line, pos)
+                if match is None:
+                    return -1, 0
+                return match.start(), match.end() - match.start()
+        return RegexSearch(old)
 
     ### PUBLIC METHODS ###
 
@@ -115,8 +145,17 @@ class ReplaceInFilesScript(DirectoryScript):
         skipped_dirs_patterns = self.skipped_directories + args.without_dirs
         skipped_files_patterns = self.skipped_files + args.without_files
 
+        if args.regex:
+            args.old = self._get_regex_search_callable(args.old)
+            index, length = args.old('', 0)
+            if 0 <= index:
+                raise ValueError('Regex pattern {!r} matches the empty string.'.format(args.old.pattern.pattern))
+        else:
+            args.old = self._get_naive_search_callable(args.old)
+
         changed_file_count = 0
         changed_line_count = 0
+        changed_item_count = 0
 
         for root, dirs, files in os.walk(args.path):
 
@@ -138,13 +177,15 @@ class ReplaceInFilesScript(DirectoryScript):
                 if not valid:
                     continue
 
-                changed_lines = self._process_file(args, os.path.join(root, file))
+                changed_lines, changed_items = self._process_file(args, os.path.join(root, file))
                 if changed_lines:
                     changed_file_count += 1
                     changed_line_count += changed_lines
+                    changed_item_count += changed_items
 
         print ''
-        print 'Changed {} lines in {} files.'.format(changed_line_count, changed_file_count)
+        print 'Replaces {} instances over {} lines in {} files.'.format(
+            changed_item_count, changed_line_count, changed_file_count)
 
     def setup_argument_parser(self, parser):
 
@@ -164,6 +205,11 @@ class ReplaceInFilesScript(DirectoryScript):
         parser.add_argument('--force',
             action='store_true',
             help='force "yes" to every replacement'
+            )
+
+        parser.add_argument('--regex',
+            action='store_true',
+            help='treat "old" as a regular expression',
             )
 
         parser.add_argument('-WF', '--without-files',
