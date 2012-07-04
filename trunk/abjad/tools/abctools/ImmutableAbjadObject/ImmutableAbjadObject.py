48f83e61477720fc82f2223590b9e08e9c23e936
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2012-07-04 18:12:14 +0000

    ImmutableAbjadObject now introspects its __new__ method, rather than __init__, to determine its mandatory and keyword argument values.

--- trunk/abjad/tools/abctools/ImmutableAbjadObject/ImmutableAbjadObject.py
+++ trunk/abjad/tools/abctools/ImmutableAbjadObject/ImmutableAbjadObject.py
@@ -23,3 +23,35 @@ class ImmutableAbjadObject(AbjadObject):
 
     def __init__(self, *args, **kwargs):
         pass
+
+    ### PRIVATE READ-ONLY ATTRIBUTES ###
+
+    @property
+    def _mandatory_argument_names(self):
+        initializer = type(self).__new__
+        if initializer.func_defaults:
+            keyword_argument_count = len(initializer.func_defaults)
+        else:
+            keyword_argument_count = 0
+        initializer_code = initializer.func_code
+        mandatory_argument_count = (initializer_code.co_argcount - keyword_argument_count - 1)
+        start_index, stop_index = 1, 1 + mandatory_argument_count
+        return initializer_code.co_varnames[start_index:stop_index]
+
+    ### PRIVATE METHODS ###
+
+    @classmethod
+    def _get_keyword_argument_names(cls):
+        if hasattr(cls.__new__, 'func_defaults'):
+            initializer = cls.__new__
+            if initializer.func_defaults:
+                keyword_argument_count = len(initializer.func_defaults)
+                initializer_code = initializer.func_code
+                mandatory_argument_count = (
+                    initializer_code.co_argcount - keyword_argument_count - 1)
+                start_index = 1 + mandatory_argument_count
+                stop_index = start_index + keyword_argument_count
+                return initializer_code.co_varnames[start_index:stop_index]
+            else:
+                return ()
+        return ()
