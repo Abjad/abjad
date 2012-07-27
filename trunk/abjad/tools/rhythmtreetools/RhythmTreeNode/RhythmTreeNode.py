a1f9f36411b31e1aaae001938fd04e87eac0e96f
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2012-07-27 18:37:02 +0000

    Reversed order of items returned by RhythmTreeNode.parentage_ratios, thereby modeling a "root to leaf" ordering rather than "leaf to root".
    This makes it simpler to map RhythmTree structures against the SearchTree structures used by quantizationtools for determining subtree divisibility.

--- trunk/abjad/tools/rhythmtreetools/RhythmTreeNode/RhythmTreeNode.py
+++ trunk/abjad/tools/rhythmtreetools/RhythmTreeNode/RhythmTreeNode.py
@@ -113,6 +113,16 @@ class RhythmTreeNode(abctools.AbjadObject):
         return self._parent
 
     @property
+    def parentage_ratios(self):
+        result = []
+        node = self
+        while node.parent is not None:
+            result.append((node.duration, node.parent.contents_duration))
+            node = node.parent
+        result.append(node.duration)
+        return tuple(reversed(result))
+
+    @property
     def prolated_duration(self):
         '''The prolated duration of the node.'''
         prolation = durationtools.Duration(1)
@@ -124,16 +134,6 @@ class RhythmTreeNode(abctools.AbjadObject):
             node = node.parent
         prolation *= node.duration
         return prolation
-
-    @property
-    def parentage_ratios(self):
-        result = []
-        node = self
-        while node.parent is not None:
-            result.append((node.duration, node.parent.contents_duration))
-            node = node.parent
-        result.append(node.duration)
-        return tuple(result)
 
     @property
     def proper_parentage(self):
--- trunk/abjad/tools/rhythmtreetools/RhythmTreeNode/test/test_RhythmTreeNode_parentage_ratios.py
+++ trunk/abjad/tools/rhythmtreetools/RhythmTreeNode/test/test_RhythmTreeNode_parentage_ratios.py
@@ -7,8 +7,8 @@ def test_RhythmTreeNode_parentage_ratios_01():
     tree = rhythmtreetools.RhythmTreeParser()(string)[0]
 
     assert tree.parentage_ratios == (1,)
-    assert tree[0].parentage_ratios == ((1, 5), 1)
-    assert tree[1].parentage_ratios == ((2, 5), 1)
-    assert tree[1][0].parentage_ratios == ((3, 7), (2, 5), 1)
-    assert tree[1][1].parentage_ratios == ((4, 7), (2, 5), 1)
-    assert tree[2].parentage_ratios == ((2, 5), 1)
+    assert tree[0].parentage_ratios == (1, (1, 5))
+    assert tree[1].parentage_ratios == (1, (2, 5))
+    assert tree[1][0].parentage_ratios == (1, (2, 5), (3, 7))
+    assert tree[1][1].parentage_ratios == (1, (2, 5), (4, 7))
+    assert tree[2].parentage_ratios == (1, (2, 5))
