3d29576c1b4c08dd5377c34e4c775c89ac7d1018
Trevor Bača <trevorbaca@gmail.com>
2012-08-04 20:25:52 +0000

    Added SegmentSpecification select test.
    Test for SegmentSpecification.select_background_measures().

--- experimental/specificationtools/SegmentSpecification/test/test_SegmentSpecification_select_background_measures.py
+++ experimental/specificationtools/SegmentSpecification/test/test_SegmentSpecification_select_background_measures.py
@@ -38,3 +38,22 @@ def test_SegmentSpecification_select_background_measures_02():
     current_function_name = introspectiontools.get_current_function_name()
     helpertools.write_test_output(score, __file__, current_function_name)
     assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
+
+
+def test_SegmentSpecification_select_background_measures_03():
+    '''Negative start and stop.
+    '''
+
+    score_template = scoretemplatetools.GroupedRhythmicStavesScoreTemplate(staff_count=1)
+    score_specification = specificationtools.ScoreSpecification(score_template)
+    segment = score_specification.append_segment('red')
+    segment.set_time_signatures([(2, 8), (3, 8), (4, 8)])
+    middle_measure = segment.select_background_measures(start=1, stop=-1)
+    segment.set_divisions_new([(2, 32)])
+    segment.set_divisions_new([(3, 32)], timespan=middle_measure)
+    segment.set_rhythm(library.thirty_seconds)
+    score = score_specification.interpret()
+
+    current_function_name = introspectiontools.get_current_function_name()
+    helpertools.write_test_output(score, __file__, current_function_name)
+    assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
