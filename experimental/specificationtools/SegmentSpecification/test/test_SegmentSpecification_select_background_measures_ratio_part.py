d11bee10875490a488d661cdc8bede2fba716d61
Trevor Bača <trevorbaca@gmail.com>
2012-08-08 15:35:52 +0000

    Authored CountRatioPartSelector test.

--- experimental/specificationtools/SegmentSpecification/test/test_SegmentSpecification_select_background_measures_ratio_part.py
+++ experimental/specificationtools/SegmentSpecification/test/test_SegmentSpecification_select_background_measures_ratio_part.py
@@ -3,6 +3,8 @@ from experimental import *
 
 
 def test_SegmentSpecification_select_background_measures_ratio_part_01():
+    '''Count ratio part.
+    '''
     
     template = scoretemplatetools.GroupedRhythmicStavesScoreTemplate(staff_count=1)
     score_specification = specificationtools.ScoreSpecification(template)
@@ -23,4 +25,23 @@ def test_SegmentSpecification_select_background_measures_ratio_part_01():
     current_function_name = introspectiontools.get_current_function_name()
     helpertools.write_test_output(score, __file__, current_function_name)
 
+    assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
+
+
+def test_SegmentSpecification_select_background_measures_ratio_part_02():
+    '''Count ratio part.
+    '''
+
+    score_template = scoretemplatetools.GroupedRhythmicStavesScoreTemplate(staff_count=1)
+    score_specification = specificationtools.ScoreSpecification(score_template)
+    segment = score_specification.append_segment('red')
+    segment.set_time_signatures([(1, 8), (1, 8), (1, 8), (3, 8)])
+    last_two_measures = segment.select_background_measures_ratio_part((1, 1), -1)
+    segment.set_divisions([(2, 32)])
+    segment.set_divisions([(3, 32)], selector=last_two_measures)
+    segment.set_rhythm(library.thirty_seconds)
+    score = score_specification.interpret()
+
+    current_function_name = introspectiontools.get_current_function_name()
+    helpertools.write_test_output(score, __file__, current_function_name)
     assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
