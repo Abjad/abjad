670a54f4190fa4ea26b76e5d290dd8cd4b105c0f
Trevor Bača <trevorbaca@gmail.com>
2012-08-13 02:26:45 +0000

    Renamed componenttools.number_is_between_prolated_start_and_stop_offsets_of_component(). The new nmae is componenttools.number_is_between_start_and_stop_offsets_of_component().
    NOTE: this change affects end-user syntax and must be noted in the 2.10 release notes.

---
+++ trunk/abjad/docs/chapters/api/tools/componenttools/number_is_between_start_and_stop_offsets_of_component.rst
@@ -0,0 +1,4 @@
+componenttools.number_is_between_start_and_stop_offsets_of_component
+====================================================================
+
+.. autofunction:: abjad.tools.componenttools.number_is_between_start_and_stop_offsets_of_component.number_is_between_start_and_stop_offsets_of_component
---
+++ trunk/abjad/tools/componenttools/test/test_componenttools_number_is_between_start_and_stop_offsets_of_component.py
@@ -0,0 +1,10 @@
+from abjad import *
+
+
+def test_componenttools_number_is_between_start_and_stop_offsets_of_component_01():
+    '''True when split point is within prolated duration of component.'''
+
+    assert componenttools.number_is_between_start_and_stop_offsets_of_component(Duration(0), Note("c'4"))
+    assert componenttools.number_is_between_start_and_stop_offsets_of_component(Duration(1, 16), Note("c'4"))
+    assert componenttools.number_is_between_start_and_stop_offsets_of_component(Duration(1, 12), Note("c'4"))
+    assert not componenttools.number_is_between_start_and_stop_offsets_of_component(Duration(1, 4), Note("c'4"))
