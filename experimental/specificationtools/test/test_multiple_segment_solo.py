d897a39b563520df8d75cf4d91467c40cdbb1612
Trevor Bača <trevorbaca@gmail.com>
2012-08-13 17:54:01 +0000

    Renamed one specificationtools test file:
        test_multisegment_solo.py ==>
        test_multiple_segment_solo.py
    
    The use of 'multi-' as a prefix is discouraged.
    
    Use 'multiple' instead.
    
    This parallels the of 'single'. Examples include 'multiple_segment' / 'single_segment'
    and 'multiple_context' / 'single_context'.

--- experimental/specificationtools/test/test_multiple_segment_solo.py
+++ experimental/specificationtools/test/test_multiple_segment_solo.py
@@ -1,31 +1,27 @@
-from abjad.tools import *
+from abjad import *
 from experimental import *
 
 
-def test_multisegment_solo_01():
+def test_multiple_segment_solo_01():
     '''Single division interprets cyclically over two segments.
     Division does not truncate at segment boundary.
     '''
 
     score_template = scoretemplatetools.GroupedRhythmicStavesScoreTemplate(staff_count=1)
     score_specification = specificationtools.ScoreSpecification(score_template)
-
     segment = score_specification.append_segment('red')
     segment.set_time_signatures([(4, 8), (3, 8)])
     segment.set_divisions([(3, 16)], contexts=segment.v1)
     segment.set_rhythm(library.thirty_seconds)
-
     segment = score_specification.append_segment('blue')
-
     score = score_specification.interpret()
 
     current_function_name = introspectiontools.get_current_function_name()
     helpertools.write_test_output(score, __file__, current_function_name)
-
     assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
 
 
-def test_multisegment_solo_02():
+def test_multiple_segment_solo_02():
     '''Single division interprets cyclically over two segments.
     Division truncates at segment boundary because of truncate keyword.
     Division starts over at beginning of second segment.
@@ -33,45 +29,37 @@ def test_multisegment_solo_02():
 
     score_template = scoretemplatetools.GroupedRhythmicStavesScoreTemplate(staff_count=1)
     score_specification = specificationtools.ScoreSpecification(score_template)
-    
     segment = score_specification.append_segment('red')
     segment.set_time_signatures([(4, 8), (3, 8)])
     segment.set_divisions([(3, 16)], contexts=segment.v1, truncate=True)
     segment.set_rhythm(library.thirty_seconds)
-
     segment = score_specification.append_segment('blue')
-
     score = score_specification.interpret()
     
     current_function_name = introspectiontools.get_current_function_name()
     helpertools.write_test_output(score, __file__, current_function_name)
-
     assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
 
 
-def test_multisegment_solo_03():
+def test_multiple_segment_solo_03():
     '''Single division exactly equal to duration of single segment.
     '''
 
     score_template = scoretemplatetools.GroupedRhythmicStavesScoreTemplate(staff_count=1)
     score_specification = specificationtools.ScoreSpecification(score_template)
-
     segment = score_specification.append_segment('red')
     segment.set_time_signatures([(4, 8), (3, 8)])
     segment.set_divisions([(14, 16)], contexts=segment.v1)
     segment.set_rhythm(library.thirty_seconds)
-
     segment = score_specification.append_segment('blue')
-
     score = score_specification.interpret()
 
     current_function_name = introspectiontools.get_current_function_name()
     helpertools.write_test_output(score, __file__, current_function_name)
-
     assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
 
 
-def test_multisegment_solo_04():
+def test_multiple_segment_solo_04():
     '''Single division exactly equal to duration of single segment.
     Division truncates because truncate keyword is set to true.
     Division starts over at beginning of second segment.
@@ -79,46 +67,38 @@ def test_multisegment_solo_04():
 
     score_template = scoretemplatetools.GroupedRhythmicStavesScoreTemplate(staff_count=1)
     score_specification = specificationtools.ScoreSpecification(score_template)
-
     segment = score_specification.append_segment('red')
     segment.set_time_signatures([(4, 8), (3, 8)])
     segment.set_divisions([(14, 16)], contexts=segment.v1, truncate=True)
     segment.set_rhythm(library.thirty_seconds)
-
     segment = score_specification.append_segment('blue')
-
     score = score_specification.interpret()
 
     current_function_name = introspectiontools.get_current_function_name()
     helpertools.write_test_output(score, __file__, current_function_name)
-
     assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
 
 
-def test_multisegment_solo_05():
+def test_multiple_segment_solo_05():
     '''Single division greater in duration than single segment but lesser in duration than two segments.
     Division does not truncate at segment boundary.
     '''
 
     score_template = scoretemplatetools.GroupedRhythmicStavesScoreTemplate(staff_count=1)
     score_specification = specificationtools.ScoreSpecification(score_template)
-
     segment = score_specification.append_segment('red')
     segment.set_time_signatures([(4, 8), (3, 8)])
     segment.set_divisions([(20, 16)], contexts=segment.v1)
     segment.set_rhythm(library.thirty_seconds)
-
     segment = score_specification.append_segment('blue')
-
     score = score_specification.interpret()
 
     current_function_name = introspectiontools.get_current_function_name()
     helpertools.write_test_output(score, __file__, current_function_name)
-
     assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
 
 
-def test_multisegment_solo_06():
+def test_multiple_segment_solo_06():
     '''Single division greater in duration than single segment but lesser in duration than two segments.
     Division truncates at segment boundary because truncate keyword is set to true.
     Division starts over from beginning of second segment.
@@ -126,41 +106,33 @@ def test_multisegment_solo_06():
 
     score_template = scoretemplatetools.GroupedRhythmicStavesScoreTemplate(staff_count=1)
     score_specification = specificationtools.ScoreSpecification(score_template)
-    
     segment = score_specification.append_segment('red')
     segment.set_time_signatures([(4, 8), (3, 8)])
     segment.set_divisions([(20, 16)], contexts=segment.v1, truncate=True)
     segment.set_rhythm(library.thirty_seconds)
-
     segment = score_specification.append_segment('blue')
-
     score = score_specification.interpret()
     
     current_function_name = introspectiontools.get_current_function_name()
     helpertools.write_test_output(score, __file__, current_function_name)
-
     assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
 
 
-def test_multisegment_solo_07():
+def test_multiple_segment_solo_07():
     '''Large division greater in duration than two segments.
     Division does not truncate at segment boundaries.
     '''
 
     score_template = scoretemplatetools.GroupedRhythmicStavesScoreTemplate(staff_count=1)
     score_specification = specificationtools.ScoreSpecification(score_template)
-    
     segment = score_specification.append_segment('red')
     segment.set_time_signatures([(4, 8), (3, 8)])
     segment.set_divisions([(15, 8)], contexts=segment.v1)
     segment.set_rhythm(library.thirty_seconds)
-
     segment = score_specification.append_segment('blue')
     segment = score_specification.append_segment('green')
-
     score = score_specification.interpret()
     
     current_function_name = introspectiontools.get_current_function_name()
     helpertools.write_test_output(score, __file__, current_function_name)
-
     assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
