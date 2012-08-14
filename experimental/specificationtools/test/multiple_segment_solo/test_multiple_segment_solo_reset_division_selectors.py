419a19593e76993a32ff0782fda021fbbb644b02
Trevor Bača <trevorbaca@gmail.com>
2012-08-14 18:30:03 +0000

    Authored division selector tests.
    Tests document selector reset behavior.
    
    Commit includes PDFs.

---
+++ experimental/specificationtools/test/multiple_segment_solo/test_multiple_segment_solo_reset_division_selectors.py
@@ -0,0 +1,46 @@
+from abjad import *
+from experimental import *
+
+
+def test_multiple_segment_solo_reset_division_selectors_01():
+    '''Reset persistent selectors.
+    '''
+
+    score_template = scoretemplatetools.GroupedRhythmicStavesScoreTemplate(staff_count=1)
+    score_specification = specificationtools.ScoreSpecification(score_template)
+    segment = score_specification.append_segment('red')
+    segment.set_time_signatures(4 * [(3, 16)])
+    segment.set_divisions([(1, 16)], truncate=True)
+    middle_two_measures = segment.select_segment_ratio_part((1, 2, 1), 1)
+    segment.set_divisions([(2, 16)], selector=middle_two_measures, persist=True)
+    segment.set_rhythm(library.thirty_seconds)
+    segment = score_specification.append_segment('blue')
+    segment.set_time_signatures(4 * [(2, 8)])
+    segment.set_divisions([(3, 16)])
+    score = score_specification.interpret()
+
+    current_function_name = introspectiontools.get_current_function_name()
+    helpertools.write_test_output(score, __file__, current_function_name)
+    assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
+
+
+def test_multiple_segment_solo_reset_division_selectors_02():
+    '''Reset nonpersistent selectors.
+    '''
+
+    score_template = scoretemplatetools.GroupedRhythmicStavesScoreTemplate(staff_count=1)
+    score_specification = specificationtools.ScoreSpecification(score_template)
+    segment = score_specification.append_segment('red')
+    segment.set_time_signatures(4 * [(3, 16)])
+    segment.set_divisions([(1, 16)], truncate=False)
+    middle_two_measures = segment.select_segment_ratio_part((1, 2, 1), 1)
+    segment.set_divisions([(2, 16)], selector=middle_two_measures, persist=True)
+    segment.set_rhythm(library.thirty_seconds)
+    segment = score_specification.append_segment('blue')
+    segment.set_time_signatures(4 * [(2, 8)])
+    segment.set_divisions([(3, 16)])
+    score = score_specification.interpret()
+
+    current_function_name = introspectiontools.get_current_function_name()
+    helpertools.write_test_output(score, __file__, current_function_name)
+    assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
