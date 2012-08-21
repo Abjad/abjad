6320d72d320d7b4aa8bba9cd9a92555cae8b53cf
Trevor Bača <trevorbaca@gmail.com>
2012-08-21 16:16:05 +0000

    Updated doctest for previous commit.

--- trunk/abjad/tools/sequencetools/partition_sequence_by_weights_exactly.py
+++ trunk/abjad/tools/sequencetools/partition_sequence_by_weights_exactly.py
@@ -3,7 +3,7 @@ def partition_sequence_by_weights_exactly(sequence, weights, cyclic=False, overh
 
     Partition `sequence` by `weights` exactly.
 
-        >>> sequence = [3, 3, 3, 3, 4, 4, 4, 4, 5, 5]
+        >>> sequence = [3, 3, 3, 3, 4, 4, 4, 4, 5]
 
     Example 1. Partition sequence once by weights exactly without overhang::
 
@@ -15,7 +15,7 @@ def partition_sequence_by_weights_exactly(sequence, weights, cyclic=False, overh
 
         >>> sequencetools.partition_sequence_by_weights_exactly(
         ...     sequence, [3, 9], cyclic=False, overhang=True)
-        [[3], [3, 3, 3], [4, 4, 4, 4, 5, 5]]
+        [[3], [3, 3, 3], [4, 4, 4, 4, 5]]
 
     Example 3. Partition sequence cyclically by weights exactly without overhang::
 
