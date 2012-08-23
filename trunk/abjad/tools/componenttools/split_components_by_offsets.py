4263eec29e820d5883931d14115a762663ae2dd0
Trevor Bača <trevorbaca@gmail.com>
2012-08-23 18:22:16 +0000

    Cleaned up componenttools.split_components_by_offsets().
    Preparataion for debugging.

--- trunk/abjad/tools/componenttools/split_components_by_offsets.py
+++ trunk/abjad/tools/componenttools/split_components_by_offsets.py
@@ -2,7 +2,7 @@ from abjad.tools import durationtools
 
 
 # TODO: fix bug that unintentionally fractures ties.
-def split_components_by_offsets(components, durations, fracture_spanners=False, cyclic=False, tie_after=False):
+def split_components_by_offsets(components, offsets, fracture_spanners=False, cyclic=False, tie_after=False):
     r'''.. versionadded:: 2.0
     
     Example 1. Split components cyclically and do not fracture crossing spanners::
@@ -147,12 +147,12 @@ def split_components_by_offsets(components, durations, fracture_spanners=False, 
 
     ::
 
-        >>> durations = [Duration(1, 32), Duration(3, 32), Duration(5, 32)]
+        >>> offsets = [Duration(1, 32), Duration(3, 32), Duration(5, 32)]
 
     ::
 
-        >>> parts = componenttools.split_components_by_offsets(
-        ... staff[:1], durations, cyclic=False, fracture_spanners=False)
+        >>> shards = componenttools.split_components_by_offsets(
+        ... staff[:1], offsets, cyclic=False, fracture_spanners=False)
 
     ::
 
@@ -207,9 +207,9 @@ def split_components_by_offsets(components, durations, fracture_spanners=False, 
 
     ::
 
-        >>> durations = [Duration(1, 32), Duration(3, 32), Duration(5, 32)]
-        >>> parts = componenttools.split_components_by_offsets(
-        ... staff[:1], durations, cyclic=False, fracture_spanners=True)
+        >>> offsets = [Duration(1, 32), Duration(3, 32), Duration(5, 32)]
+        >>> shards = componenttools.split_components_by_offsets(
+        ... staff[:1], offsets, cyclic=False, fracture_spanners=True)
 
     ::
 
@@ -234,80 +234,83 @@ def split_components_by_offsets(components, durations, fracture_spanners=False, 
             }
         }
 
-    Return list of newly split parts.
+    Return list of newly split shards.
     '''
     from abjad.tools import componenttools
 
     # check input
     assert componenttools.all_are_components(components)
-    assert durationtools.all_are_duration_tokens(durations)
+    assert durationtools.all_are_duration_tokens(offsets)
 
     # initialize loop variables
-    result = []
-    part = []
-    duration_index = 0
-    len_durations = len(durations)
-    cum_duration = durationtools.Duration(0)
-    xx = list(components[:])
+    result, shard = [], []
+    offset_index, offset_count = 0, len(offsets)
+    current_shard_duration = durationtools.Duration(0)
+    remaining_components = list(components[:])
 
-    # loop and build partition parts
-    # grab next component from input stack every time through loop
-    # grab size of current part every time, though part may still be filling
+    # loop and build shards
+    # grab next component and next offset each time through loop
     while True:
-        #print 'xx are now %s' % xx
-        try:
-            if cyclic:
-                next_split_point = durations[duration_index % len_durations]
-            else:
-                next_split_point = durations[duration_index]
-        except IndexError:
-            break
-        # grab next component from input stack of components
-        try:
-            x = xx.pop(0)
-        except IndexError:
-            break
-        # find where end point of current component will position us
-        next_cum_duration = cum_duration + x.prolated_duration
-        #print x, duration_index, cum_duration, next_split_point
-        # if current component fills duration of current part exactly
-        if next_cum_duration == next_split_point:
-            #print 'exactly equal %s' % x
-            part.append(x)
-            result.append(part)
-            part = []
-            cum_duration = durationtools.Duration(0)
-            duration_index += 1
-        # if current component exceeds duration of current part
-        elif next_split_point < next_cum_duration:
-            #print 'must split %s' % x
-            local_split_duration = next_split_point - cum_duration
-            #print cum_duration, next_split_point, x, part, local_split_duration
-            left_list, right_list = componenttools.split_component_at_offset(x, local_split_duration,
-                fracture_spanners=fracture_spanners, tie_after=tie_after)
-            #print 'left_list, right_list %s, %s' % (left_list, right_list)
-            part.extend(left_list)
-            result.append(part)
-            part = []
-            # to avoid slice assignment pychecker errors
-            #xx[0:0] = right_list
-            xx.__setitem__(slice(0, 0), right_list)
-            duration_index += 1
-            cum_duration = durationtools.Duration(0)
-        # if current component does not fill duration of current part
+        #print 'remaining_components are now %s' % remaining_components
+
+        # grab next split point
+        if cyclic:
+            next_split_point = offsets[offset_index % offset_count]
+        elif offset_index < offset_count:
+            next_split_point = offsets[offset_index]
         else:
-            #print 'simple append %s' % x
-            part.append(x)
-            cum_duration += x.prolated_duration
+            break
+
+        # grab next component from input stack of components
+        if remaining_components:
+            current_component = remaining_components.pop(0)
+        else:
+            break
+
+        # find where current component endpoint will position us
+        candidate_shard_duration = current_shard_duration + current_component.prolated_duration
+        #print current_component, offset_index, current_shard_duration, next_split_point
+
+        # if current component would fill current shard exactly
+        if candidate_shard_duration == next_split_point:
+            #print 'exactly equal %s' % current_component
+            shard.append(current_component)
+            result.append(shard)
+            shard = []
+            current_shard_duration = durationtools.Duration(0)
+            offset_index += 1
+        # if current component would exceed current shard
+        elif next_split_point < candidate_shard_duration:
+            #print 'must split %s' % current_component
+            local_split_duration = next_split_point - current_shard_duration
+            #print current_shard_duration, next_split_point, current_component, shard, local_split_duration
+            left_list, right_list = componenttools.split_component_at_offset(
+                current_component, local_split_duration, fracture_spanners=fracture_spanners, tie_after=tie_after)
+            #print 'left_list, right_list %s, %s' % (left_list, right_list)
+            shard.extend(left_list)
+            result.append(shard)
+            shard = []
+            # to avoid slice assignment pychecker errors
+            #remaining_components[0:0] = right_list
+            remaining_components.__setitem__(slice(0, 0), right_list)
+            offset_index += 1
+            current_shard_duration = durationtools.Duration(0)
+        # if current component would not fill current shard
+        elif candidate_shard_duration < next_split_point:
+            #print 'simple append %s' % current_component
+            shard.append(current_component)
+            current_shard_duration += current_component.prolated_duration
+        else:
+            raise ValueError
         #print ''
 
-    # append stub part, if any
-    if len(part):
-        result.append(part)
+    # append any stub shard
+    if len(shard):
+        result.append(shard)
 
-    # append unexamined components, if any
-    if len(xx):
-        result.append(xx)
+    # append any unexamined components
+    if len(remaining_components):
+        result.append(remaining_components)
 
-    # return list of parts, each of which is a list of components
+    # return list of shards
     return result
