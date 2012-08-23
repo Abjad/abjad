c3f578b000ce17bab18fd123e76d768ddecb42f5
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2012-08-23 01:58:43 +0000

    (Re)added iotools._run_abjad() function.
    abjad/scr/abjad and abjad/scr/abj now rely on this function.
    
    This should also make the setup.py console scripts definitions happy.

---
+++ trunk/abjad/tools/iotools/_run_abjad.py
@@ -0,0 +1,22 @@
+import os
+import sys
+
+
+def _run_abjad():
+    from abjad.tools import iotools
+
+    try:
+        file = sys.argv[1]
+    except IndexError:
+        file = ''
+
+    commands = [
+        "from abjad import *;",
+        "from abjad.tools import configurationtools;",
+        "print configurationtools.get_abjad_startup_string();",
+        "del configurationtools",
+    ]
+
+    command = '''python -i {} -c "{}"'''.format(file, ' '.join(commands))
+
+    iotools.spawn_subprocess(command)
