d19b4ff796d1b033f0ee48acb062db8c6373bfbb
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2012-08-24 01:02:37 +0000

    Fixed extension paths in docs/source/conf.py

--- trunk/abjad/docs/source/conf.py
+++ trunk/abjad/docs/source/conf.py
@@ -32,8 +32,8 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.intersphinx',
     'sphinx.ext.viewcode',
-    'abjad.newdocs.source._ext.inheritance_diagram',
-    'abjad.newdocs.source._ext.autodoc',
+    'abjad.docs.source._ext.inheritance_diagram',
+    'abjad.docs.source._ext.autodoc',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
