110a87bd2a804682caa3c2096429be8f332ef3c3
Trevor Bača <trevorbaca@gmail.com>
2012-09-03 23:13:12 +0000

    Migrated test collateral out of four deprecated tuplettools functions:
        tuplettools.make_augmented_tuplet_from_duration_and_proportions_and_avoid_dots
        tuplettools.make_augmented_tuplet_from_duration_and_proportions_and_encourage_dots
        tuplettools.make_diminished_tuplet_from_duration_and_proportions_and_avoid_dots
        tuplettools.make_diminished_tuplet_from_duration_and_proportions_and_encourage_dots

--- trunk/abjad/tools/tuplettools/make_tuplet_from_duration_and_proportions.py
+++ trunk/abjad/tools/tuplettools/make_tuplet_from_duration_and_proportions.py
@@ -11,6 +11,76 @@ def make_tuplet_from_duration_and_proportions(duration, proportions,
     r'''.. versionadded:: 2.10
 
     Make tuplet from `duration` and `proportions`.
+
+    Example set 1. Make augmented tuplet from `duration` and `proportions` and avoid dots.
+
+    Return tupletted leaves strictly without dots when all `proportions` equal ``1``::
+
+        >>> print tuplettools.make_tuplet_from_duration_and_proportions(
+        ... Fraction(3, 16), [1, 1, 1, -1, -1], avoid_dots=True, is_diminution=False)
+        {@ 5:6 c'32, c'32, c'32, r32, r32 @}
+
+    Allow tupletted leaves to return with dots when some `proportions` do not equal ``1``::
+
+        >>> print tuplettools.make_tuplet_from_duration_and_proportions(
+        ... Fraction(3, 16), [1, -2, -2, 3, 3], avoid_dots=True, is_diminution=False)
+        {@ 11:12 c'64, r32, r32, c'32., c'32. @}
+
+    Interpret nonassignable `proportions` according to `big_endian`::
+
+        >>> print tuplettools.make_tuplet_from_duration_and_proportions(
+        ... Fraction(3, 16), [5, -1, 5], avoid_dots=True, big_endian=False, is_diminution=False)
+        {@ 11:12 c'64, c'16, r64, c'64, c'16 @}
+
+    Example set 2. Make augmented tuplet from `duration` and `proportions` and encourage dots::
+
+        >>> tuplettools.make_tuplet_from_duration_and_proportions(
+        ... Fraction(3, 16), [1, 1, 1, -1, -1], avoid_dots=False, is_diminution=False)
+        FixedDurationTuplet(3/16, [c'64., c'64., c'64., r64., r64.])
+
+    Interpret nonassignable `proportions` according to `big_endian`::
+
+        >>> tuplettools.make_tuplet_from_duration_and_proportions(
+        ... Fraction(3, 16), [5, -1, 5], avoid_dots=False, big_endian=False, is_diminution=False)
+        FixedDurationTuplet(3/16, [c'32..., r128., c'32...])
+
+    Example set 3. Make diminished tuplet from `duration` and nonzero integer `proportions`.
+
+    Return tupletted leaves strictly without dots when all `proportions` equal ``1``::
+
+        >>> print tuplettools.make_tuplet_from_duration_and_proportions(
+        ... Fraction(3, 16), [1, 1, 1, -1, -1], avoid_dots=True, is_diminution=True)
+        {@ 5:3 c'16, c'16, c'16, r16, r16 @}
+
+    Allow tupletted leaves to return with dots when some `proportions` do not equal ``1``::
+
+        >>> print tuplettools.make_tuplet_from_duration_and_proportions(
+        ... Fraction(3, 16), [1, -2, -2, 3, 3], avoid_dots=True, is_diminution=True)
+        {@ 11:6 c'32, r16, r16, c'16., c'16. @}
+
+    Interpret nonassignable `proportions` according to `big_endian`::
+
+        >>> print tuplettools.make_tuplet_from_duration_and_proportions(
+        ... Fraction(3, 16), [5, -1, 5], avoid_dots=True, big_endian=False, is_diminution=True)
+        {@ 11:6 c'32, c'8, r32, c'32, c'8 @}
+
+    Example set 4. Make diminished tuplet from `duration` and `proportions` and encourage dots::
+
+        >>> tuplettools.make_tuplet_from_duration_and_proportions(
+        ... Fraction(3, 16), [1, 1, 1, -1, -1], avoid_dots=False, is_diminution=True)
+        FixedDurationTuplet(3/16, [c'32., c'32., c'32., r32., r32.])
+
+    Interpret nonassignable `proportions` according to `direction`::
+
+        >>> tuplettools.make_tuplet_from_duration_and_proportions(
+        ... Fraction(3, 16), [5, -1, 5], avoid_dots=False, big_endian=False, is_diminution=True)
+        FixedDurationTuplet(3/16, [c'16..., r64., c'16...])
+
+    Reduce `proportions` relative to each other.
+
+    Interpret negative `proportions` as rests.
+
+    Return fixed-duration tuplet.
     '''
     from abjad.tools import tuplettools
 
--- trunk/abjad/tools/tuplettools/test/test_tuplettools_make_tuplet_from_duration_and_proportions.py
+++ trunk/abjad/tools/tuplettools/test/test_tuplettools_make_tuplet_from_duration_and_proportions.py
@@ -1,4 +1,291 @@
 from abjad import *
 
 
+def test_tuplettools_make_tuplet_from_duration_and_proportions_01():
 
+    duration = Fraction(3, 16)
+
+    t = tuplettools.make_tuplet_from_duration_and_proportions(
+        duration, [1], avoid_dots=True, is_diminution=False)
+    assert t.lilypond_format == "\\fraction \\times 3/2 {\n\tc'8\n}"
+
+    t = tuplettools.make_tuplet_from_duration_and_proportions(
+        duration, [1, 1], avoid_dots=True, is_diminution=False)
+    assert t.lilypond_format == "\\fraction \\times 3/2 {\n\tc'16\n\tc'16\n}"
+
+    t = tuplettools.make_tuplet_from_duration_and_proportions(
+        duration, [1, 1, 1], avoid_dots=True, is_diminution=False)
+    assert t.lilypond_format == "{\n\tc'16\n\tc'16\n\tc'16\n}"
+
+    t = tuplettools.make_tuplet_from_duration_and_proportions(
+        duration, [1, 1, 1, 1], avoid_dots=True, is_diminution=False)
+    assert t.lilypond_format == "\\fraction \\times 3/2 {\n\tc'32\n\tc'32\n\tc'32\n\tc'32\n}"
+
+    t = tuplettools.make_tuplet_from_duration_and_proportions(
+        duration, [1, 1, 1, 1, 1], avoid_dots=True, is_diminution=False)
+    assert t.lilypond_format == "\\fraction \\times 6/5 {\n\tc'32\n\tc'32\n\tc'32\n\tc'32\n\tc'32\n}"
+
+
+def test_tuplettools_make_tuplet_from_duration_and_proportions_02():
+
+    duration = Fraction(3, 16)
+
+    t = tuplettools.make_tuplet_from_duration_and_proportions(
+        duration, [1], avoid_dots=True, is_diminution=False)
+    assert t.lilypond_format == "\\fraction \\times 3/2 {\n\tc'8\n}"
+
+    t = tuplettools.make_tuplet_from_duration_and_proportions(
+        duration, [1, 2], avoid_dots=True, is_diminution=False)
+    assert t.lilypond_format == "{\n\tc'16\n\tc'8\n}"
+
+    t = tuplettools.make_tuplet_from_duration_and_proportions(
+        duration, [1, 2, 2], avoid_dots=True, is_diminution=False)
+    assert t.lilypond_format == "\\fraction \\times 6/5 {\n\tc'32\n\tc'16\n\tc'16\n}"
+
+    t = tuplettools.make_tuplet_from_duration_and_proportions(
+        duration, [1, 2, 2, 3], avoid_dots=True, is_diminution=False)
+    assert t.lilypond_format == "\\fraction \\times 3/2 {\n\tc'64\n\tc'32\n\tc'32\n\tc'32.\n}"
+
+    t = tuplettools.make_tuplet_from_duration_and_proportions(
+        duration, [1, 2, 2, 3, 3], avoid_dots=True, is_diminution=False)
+    assert t.lilypond_format == "\\fraction \\times 12/11 {\n\tc'64\n\tc'32\n\tc'32\n\tc'32.\n\tc'32.\n}"
+
+
+def test_tuplettools_make_tuplet_from_duration_and_proportions_03():
+    '''Interpret negative proportions as rests.
+    '''
+
+    duration = Fraction(3, 16)
+
+    t = tuplettools.make_tuplet_from_duration_and_proportions(
+        duration, [1, -2, -2, 3, 3], avoid_dots=True, is_diminution=False)
+    assert t.lilypond_format == "\\fraction \\times 12/11 {\n\tc'64\n\tr32\n\tr32\n\tc'32.\n\tc'32.\n}"
+
+
+def test_tuplettools_make_tuplet_from_duration_and_proportions_04():
+    '''Reduce proportions relative to each other.
+    '''
+
+    duration = Fraction(3, 16)
+
+    t1 = tuplettools.make_tuplet_from_duration_and_proportions(
+        duration, [1, -2, -2, 3, 3], avoid_dots=True, is_diminution=False)
+    t2 = tuplettools.make_tuplet_from_duration_and_proportions(
+        duration, [2, -4, -4, 6, 6], avoid_dots=True, is_diminution=False)
+    assert t1.lilypond_format == t2.lilypond_format
+
+    t = tuplettools.make_tuplet_from_duration_and_proportions(
+        Fraction(1, 8), [27], avoid_dots=True, is_diminution=False)
+    assert t.lilypond_format == "{\n\tc'8\n}"
+
+
+def test_tuplettools_make_tuplet_from_duration_and_proportions_05():
+
+    duration = Fraction(3, 16)
+
+    t = tuplettools.make_tuplet_from_duration_and_proportions(
+        duration, [1], avoid_dots=False, is_diminution=False)
+    assert t.lilypond_format == "{\n\tc'8.\n}"
+
+    t = tuplettools.make_tuplet_from_duration_and_proportions(
+        duration, [1, 1], avoid_dots=False, is_diminution=False)
+    assert t.lilypond_format == "{\n\tc'16.\n\tc'16.\n}"
+
+    t = tuplettools.make_tuplet_from_duration_and_proportions(
+        duration, [1, 1, 1], avoid_dots=False, is_diminution=False)
+    assert t.lilypond_format == "{\n\tc'16\n\tc'16\n\tc'16\n}"
+
+    t = tuplettools.make_tuplet_from_duration_and_proportions(
+        duration, [1, 1, 1, 1], avoid_dots=False, is_diminution=False)
+    assert t.lilypond_format == "{\n\tc'32.\n\tc'32.\n\tc'32.\n\tc'32.\n}"
+
+    t = tuplettools.make_tuplet_from_duration_and_proportions(
+        duration, [1, 1, 1, 1, 1], avoid_dots=False, is_diminution=False)
+    assert t.lilypond_format == "\\fraction \\times 8/5 {\n\tc'64.\n\tc'64.\n\tc'64.\n\tc'64.\n\tc'64.\n}"
+
+
+def test_tuplettools_make_tuplet_from_duration_and_proportions_06():
+
+    duration = Fraction(3, 16)
+
+    t = tuplettools.make_tuplet_from_duration_and_proportions(
+        duration, [1], avoid_dots=False, is_diminution=False)
+    assert t.lilypond_format == "{\n\tc'8.\n}"
+
+    t = tuplettools.make_tuplet_from_duration_and_proportions(
+        duration, [1, 2], avoid_dots=False, is_diminution=False)
+    assert t.lilypond_format == "{\n\tc'16\n\tc'8\n}"
+
+    t = tuplettools.make_tuplet_from_duration_and_proportions(
+        duration, [1, 2, 2], avoid_dots=False, is_diminution=False)
+    assert t.lilypond_format == "\\fraction \\times 8/5 {\n\tc'64.\n\tc'32.\n\tc'32.\n}"
+
+    t = tuplettools.make_tuplet_from_duration_and_proportions(
+        duration, [1, 2, 2, 3], avoid_dots=False, is_diminution=False)
+    assert t.lilypond_format == "\\fraction \\times 3/2 {\n\tc'64\n\tc'32\n\tc'32\n\tc'32.\n}"
+
+    t = tuplettools.make_tuplet_from_duration_and_proportions(
+        duration, [1, 2, 2, 3, 3], avoid_dots=False, is_diminution=False)
+    assert t.lilypond_format == "\\fraction \\times 12/11 {\n\tc'64\n\tc'32\n\tc'32\n\tc'32.\n\tc'32.\n}"
+
+
+def test_tuplettools_make_tuplet_from_duration_and_proportions_07():
+    '''Reduce proportions relative to each other.
+    '''
+
+    duration = Fraction(3, 16)
+
+    t1 = tuplettools.make_tuplet_from_duration_and_proportions(
+        duration, [1, 2, 2, 3, 3], avoid_dots=False, is_diminution=False)
+    t2 = tuplettools.make_tuplet_from_duration_and_proportions(
+        duration, [2, 4, 4, 6, 6], avoid_dots=False, is_diminution=False)
+    assert t1.lilypond_format == t2.lilypond_format
+
+    t = tuplettools.make_tuplet_from_duration_and_proportions(
+        Fraction(1, 8), [27], avoid_dots=False, is_diminution=False)
+    assert t.lilypond_format == "{\n\tc'8\n}"
+
+
+def test_tuplettools_make_tuplet_from_duration_and_proportions_08():
+
+    duration = Fraction(3, 16)
+
+    t = tuplettools.make_tuplet_from_duration_and_proportions(
+        duration, [1], avoid_dots=True, is_diminution=True)
+    assert t.lilypond_format == "\\fraction \\times 3/4 {\n\tc'4\n}"
+
+    t = tuplettools.make_tuplet_from_duration_and_proportions(
+        duration, [1, 1], avoid_dots=True, is_diminution=True)
+    assert t.lilypond_format == "\\fraction \\times 3/4 {\n\tc'8\n\tc'8\n}"
+
+    t = tuplettools.make_tuplet_from_duration_and_proportions(
+        duration, [1, 1, 1], avoid_dots=True, is_diminution=True)
+    assert t.lilypond_format == "{\n\tc'16\n\tc'16\n\tc'16\n}"
+
+    t = tuplettools.make_tuplet_from_duration_and_proportions(
+        duration, [1, 1, 1, 1], avoid_dots=True, is_diminution=True)
+    assert t.lilypond_format == "\\fraction \\times 3/4 {\n\tc'16\n\tc'16\n\tc'16\n\tc'16\n}"
+
+    t = tuplettools.make_tuplet_from_duration_and_proportions(
+        duration, [1, 1, 1, 1, 1], avoid_dots=True, is_diminution=True)
+    assert t.lilypond_format == "\\fraction \\times 3/5 {\n\tc'16\n\tc'16\n\tc'16\n\tc'16\n\tc'16\n}"
+
+
+def test_tuplettools_make_tuplet_from_duration_and_proportions_09():
+
+    duration = Fraction(3, 16)
+
+    t = tuplettools.make_tuplet_from_duration_and_proportions(
+        duration, [1], avoid_dots=True, is_diminution=True)
+    assert t.lilypond_format == "\\fraction \\times 3/4 {\n\tc'4\n}"
+
+    t = tuplettools.make_tuplet_from_duration_and_proportions(
+        duration, [1, 2], avoid_dots=True, is_diminution=True)
+    assert t.lilypond_format == "{\n\tc'16\n\tc'8\n}"
+
+    t = tuplettools.make_tuplet_from_duration_and_proportions(
+        duration, [1, 2, 2], avoid_dots=True, is_diminution=True)
+    assert t.lilypond_format == "\\fraction \\times 3/5 {\n\tc'16\n\tc'8\n\tc'8\n}"
+
+    t = tuplettools.make_tuplet_from_duration_and_proportions(
+        duration, [1, 2, 2, 3], avoid_dots=True, is_diminution=True)
+    assert t.lilypond_format == "\\fraction \\times 3/4 {\n\tc'32\n\tc'16\n\tc'16\n\tc'16.\n}"
+
+    t = tuplettools.make_tuplet_from_duration_and_proportions(
+        duration, [1, 2, 2, 3, 3], avoid_dots=True, is_diminution=True)
+    assert t.lilypond_format == "\\fraction \\times 6/11 {\n\tc'32\n\tc'16\n\tc'16\n\tc'16.\n\tc'16.\n}"
+
+
+def test_tuplettools_make_tuplet_from_duration_and_proportions_10():
+    '''Interpret negative proportions as rests.
+    '''
+
+    duration = Fraction(3, 16)
+
+    t = tuplettools.make_tuplet_from_duration_and_proportions(
+        duration, [1, -2, -2, 3, 3], avoid_dots=True, is_diminution=True)
+    assert t.lilypond_format == "\\fraction \\times 6/11 {\n\tc'32\n\tr16\n\tr16\n\tc'16.\n\tc'16.\n}"
+
+
+def test_tuplettools_make_tuplet_from_duration_and_proportions_11():
+    '''Reduce propotions relative to each other.
+    '''
+
+    duration = Fraction(3, 16)
+
+    t1 = tuplettools.make_tuplet_from_duration_and_proportions(
+        duration, [1, -2, -2, 3, 3], avoid_dots=True, is_diminution=True)
+    t2 = tuplettools.make_tuplet_from_duration_and_proportions(
+        duration, [2, -4, -4, 6, 6], avoid_dots=True, is_diminution=True)
+    assert t1.lilypond_format == t2.lilypond_format
+
+    t = tuplettools.make_tuplet_from_duration_and_proportions(
+        Fraction(1, 8), [27])
+    assert t.lilypond_format == "{\n\tc'8\n}"
+
+
+def test_tuplettools_make_tuplet_from_duration_and_proportions_12():
+
+    duration = Fraction(3, 16)
+
+    t = tuplettools.make_tuplet_from_duration_and_proportions(
+        duration, [1], avoid_dots=False, is_diminution=True)
+    assert t.lilypond_format == "{\n\tc'8.\n}"
+
+    t = tuplettools.make_tuplet_from_duration_and_proportions(
+        duration, [1, 1], avoid_dots=False, is_diminution=True)
+    assert t.lilypond_format == "{\n\tc'16.\n\tc'16.\n}"
+
+    t = tuplettools.make_tuplet_from_duration_and_proportions(
+        duration, [1, 1, 1], avoid_dots=False, is_diminution=True)
+    assert t.lilypond_format == "{\n\tc'16\n\tc'16\n\tc'16\n}"
+
+    t = tuplettools.make_tuplet_from_duration_and_proportions(
+        duration, [1, 1, 1, 1], avoid_dots=False, is_diminution=True)
+    assert t.lilypond_format == "{\n\tc'32.\n\tc'32.\n\tc'32.\n\tc'32.\n}"
+
+    t = tuplettools.make_tuplet_from_duration_and_proportions(
+        duration, [1, 1, 1, 1, 1], avoid_dots=False, is_diminution=True)
+    assert t.lilypond_format == "\\times 4/5 {\n\tc'32.\n\tc'32.\n\tc'32.\n\tc'32.\n\tc'32.\n}"
+
+
+def test_tuplettools_make_tuplet_from_duration_and_proportions_13():
+
+    duration = Fraction(3, 16)
+
+    t = tuplettools.make_tuplet_from_duration_and_proportions(
+        duration, [1], avoid_dots=False, is_diminution=True)
+    assert t.lilypond_format == "{\n\tc'8.\n}"
+
+    t = tuplettools.make_tuplet_from_duration_and_proportions(
+        duration, [1, 2], avoid_dots=False, is_diminution=True)
+    assert t.lilypond_format == "{\n\tc'16\n\tc'8\n}"
+
+    t = tuplettools.make_tuplet_from_duration_and_proportions(
+        duration, [1, 2, 2], avoid_dots=False, is_diminution=True)
+    assert t.lilypond_format == "\\times 4/5 {\n\tc'32.\n\tc'16.\n\tc'16.\n}"
+
+    t = tuplettools.make_tuplet_from_duration_and_proportions(
+        duration, [1, 2, 2, 3], avoid_dots=False, is_diminution=True)
+    assert t.lilypond_format == "\\fraction \\times 3/4 {\n\tc'32\n\tc'16\n\tc'16\n\tc'16.\n}"
+
+    t = tuplettools.make_tuplet_from_duration_and_proportions(
+        duration, [1, 2, 2, 3, 3], avoid_dots=False, is_diminution=True)
+    assert t.lilypond_format == "\\fraction \\times 6/11 {\n\tc'32\n\tc'16\n\tc'16\n\tc'16.\n\tc'16.\n}"
+
+
+def test_tuplettools_make_tuplet_from_duration_and_proportions_14():
+    '''Reduce proportions relative to each other.
+    '''
+
+    duration = Fraction(3, 16)
+
+    t1 = tuplettools.make_tuplet_from_duration_and_proportions(
+        duration, [1, -2, -2, 3, 3], avoid_dots=False, is_diminution=True)
+    t2 = tuplettools.make_tuplet_from_duration_and_proportions(
+        duration, [2, -4, -4, 6, 6], avoid_dots=False, is_diminution=True)
+    assert t1.lilypond_format == t2.lilypond_format
+
+    t = tuplettools.make_tuplet_from_duration_and_proportions(
+        Fraction(1, 8), [27], avoid_dots=False, is_diminution=True)
+    assert t.lilypond_format == "{\n\tc'8\n}"
