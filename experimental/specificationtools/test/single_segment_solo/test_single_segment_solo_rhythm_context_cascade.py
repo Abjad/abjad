37c8b9de29e36e7e2c416aa546419534b3d1bf48
Trevor Bača <trevorbaca@gmail.com>
2012-09-04 19:06:35 +0000

    Updated doctest in ReducedLyParser.

--- experimental/specificationtools/test/single_segment_solo/test_single_segment_solo_rhythm_context_cascade.py
+++ experimental/specificationtools/test/single_segment_solo/test_single_segment_solo_rhythm_context_cascade.py
@@ -82,3 +82,22 @@ def test_single_segment_solo_rhythm_context_cascade_04():
     current_function_name = introspectiontools.get_current_function_name()
     helpertools.write_test_output(score, __file__, current_function_name)
     assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
+
+
+def test_single_segment_solo_rhythm_context_cascade_05():
+    '''Selector and contexts work together.
+    '''
+
+    score_template = scoretemplatetools.GroupedRhythmicStavesScoreTemplate(staff_count=1)
+    score_specification = specificationtools.ScoreSpecification(score_template)
+    segment = score_specification.append_segment(name='red')
+    segment.set_time_signatures([(4, 8), (3, 8), (2, 8)])
+    segment.set_divisions([(3, 16)])
+    segment.set_rhythm(library.sixteenths)
+    selector = segment.select_background_measure(1)
+    segment.set_rhythm(library.thirty_seconds, contexts=['Voice 1'], selector=selector)
+    score = score_specification.interpret()
+
+    current_function_name = introspectiontools.get_current_function_name()
+    helpertools.write_test_output(score, __file__, current_function_name)
+    assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
