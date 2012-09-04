60b61e113cf0b945a4ce36dfa0639c020a38a158
Trevor Bača <trevorbaca@gmail.com>
2012-09-04 19:46:26 +0000

    Authored more rhythm interpretation tests.
    More incomplete rhythm coverage tests.

--- experimental/specificationtools/test/single_segment_solo/test_single_segment_solo_incomplete_rhythm_coverage.py
+++ experimental/specificationtools/test/single_segment_solo/test_single_segment_solo_incomplete_rhythm_coverage.py
@@ -77,3 +77,63 @@ def test_single_segment_solo_incomplete_rhythm_coverage_04():
     current_function_name = introspectiontools.get_current_function_name()
     helpertools.write_test_output(score, __file__, current_function_name)
     assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
+
+
+def test_single_segment_solo_incomplete_rhythm_coverage_05():
+    '''One selector partially covers the other.
+    '''
+
+    score_template = scoretemplatetools.GroupedRhythmicStavesScoreTemplate(staff_count=1)
+    score_specification = specificationtools.ScoreSpecification(score_template)
+    segment = score_specification.append_segment(name='red')
+    segment.set_time_signatures([(4, 8), (3, 8), (2, 8)])
+    segment.set_divisions([(3, 16)])
+    selector = segment.select_background_measures(start=-2)
+    segment.set_rhythm(library.sixteenths, selector=selector)
+    selector = segment.select_background_measures(start=-1)
+    segment.set_rhythm(library.thirty_seconds, selector=selector)
+    score = score_specification.interpret()
+
+    current_function_name = introspectiontools.get_current_function_name()
+    helpertools.write_test_output(score, __file__, current_function_name)
+    assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
+
+
+def test_single_segment_solo_incomplete_rhythm_coverage_06():
+    '''One selector partially covers the other. Works with contexts.
+    '''
+
+    score_template = scoretemplatetools.GroupedRhythmicStavesScoreTemplate(staff_count=1)
+    score_specification = specificationtools.ScoreSpecification(score_template)
+    segment = score_specification.append_segment(name='red')
+    segment.set_time_signatures([(4, 8), (3, 8), (2, 8)])
+    segment.set_divisions([(3, 16)])
+    selector = segment.select_background_measures(start=-2)
+    segment.set_rhythm(library.sixteenths, selector=selector)
+    selector = segment.select_background_measures(start=-1)
+    segment.set_rhythm(library.thirty_seconds, contexts=['Voice 1'], selector=selector)
+    score = score_specification.interpret()
+
+    current_function_name = introspectiontools.get_current_function_name()
+    helpertools.write_test_output(score, __file__, current_function_name)
+    assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
+
+
+def test_single_segment_solo_incomplete_rhythm_coverage_07():
+    '''One selector more important than the other.
+    '''
+
+    score_template = scoretemplatetools.GroupedRhythmicStavesScoreTemplate(staff_count=1)
+    score_specification = specificationtools.ScoreSpecification(score_template)
+    segment = score_specification.append_segment(name='red')
+    segment.set_time_signatures([(4, 8), (3, 8), (2, 8)])
+    segment.set_divisions([(3, 16)])
+    selector = segment.select_background_measures(start=-2)
+    segment.set_rhythm(library.sixteenths, contexts=['Voice 1'], selector=selector)
+    selector = segment.select_background_measures(start=-1)
+    segment.set_rhythm(library.thirty_seconds, selector=selector)
+    score = score_specification.interpret()
+
+    current_function_name = introspectiontools.get_current_function_name()
+    helpertools.write_test_output(score, __file__, current_function_name)
+    assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
