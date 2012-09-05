6445920dcae68995b82039a6a5647bd9c89ee6b1
Trevor Bača <trevorbaca@gmail.com>
2012-09-05 19:30:26 +0000

    Authored another rhythm / division interaction tests.
    Tests demonstrates several different types of selector
    all overlapping and all working correctly.

--- experimental/specificationtools/test/single_segment_solo/test_single_segment_solo_fancy_rhythm_and_divisions.py
+++ experimental/specificationtools/test/single_segment_solo/test_single_segment_solo_fancy_rhythm_and_divisions.py
@@ -37,3 +37,26 @@ def test_single_segment_solo_fancy_rhythm_and_divisions_02():
     current_function_name = introspectiontools.get_current_function_name()
     helpertools.write_test_output(score, __file__, current_function_name)
     assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
+
+
+def test_single_segment_solo_fancy_rhythm_and_divisions_03():
+    '''Several different types of selector.
+    '''
+
+    score_template = scoretemplatetools.GroupedRhythmicStavesScoreTemplate(staff_count=1)
+    score_specification = specificationtools.ScoreSpecification(score_template)
+    segment = score_specification.append_segment(name='red')
+    segment.set_time_signatures([(4, 8), (3, 8), (2, 8)])
+    selector = segment.select_segment_ratio_part((1, 1), 0)
+    segment.set_divisions([(3, 16)], selector=selector)
+    selector = segment.select_segment_ratio_part((1, 1), -1)
+    segment.set_divisions([(2, 16)], selector=selector)
+    selector = segment.select_divisions(stop=2)
+    segment.set_rhythm(library.sixteenths, selector=selector)
+    selector = segment.select_divisions(start=2)
+    segment.set_rhythm(library.thirty_seconds, selector=selector)
+    score = score_specification.interpret()
+
+    current_function_name = introspectiontools.get_current_function_name()
+    helpertools.write_test_output(score, __file__, current_function_name)
+    assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
