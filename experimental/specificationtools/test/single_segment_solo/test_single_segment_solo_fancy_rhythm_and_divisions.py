4580ea378c4cd9e8e4cb4f16b673ebdff687f6a8
Trevor Bača <trevorbaca@gmail.com>
2012-09-05 19:18:56 +0000

    Authored rhythm / division interaction tests.
    The tests lock in different way that rhythm and division selectors can overlap.

---
+++ experimental/specificationtools/test/single_segment_solo/test_single_segment_solo_fancy_rhythm_and_divisions.py
@@ -0,0 +1,39 @@
+from abjad import *
+from experimental import *
+
+
+def test_single_segment_solo_fancy_rhythm_and_divisions_01():
+    '''Rhythm and divisions carve out same partial time signature selector.
+    '''
+
+    score_template = scoretemplatetools.GroupedRhythmicStavesScoreTemplate(staff_count=1)
+    score_specification = specificationtools.ScoreSpecification(score_template)
+    segment = score_specification.append_segment(name='red')
+    segment.set_time_signatures(3 * [(4, 8)])
+    selector = segment.select_segment_offsets(start=(10, 16), stop=(13, 16))
+    segment.set_divisions([(2, 32)], selector=selector)
+    segment.set_rhythm(library.thirty_seconds, selector=selector)
+    score = score_specification.interpret()
+
+    current_function_name = introspectiontools.get_current_function_name()
+    helpertools.write_test_output(score, __file__, current_function_name)
+    assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
+
+
+def test_single_segment_solo_fancy_rhythm_and_divisions_02():
+    '''Incomplete rhythm and division selectors overlap.
+    '''
+
+    score_template = scoretemplatetools.GroupedRhythmicStavesScoreTemplate(staff_count=1)
+    score_specification = specificationtools.ScoreSpecification(score_template)
+    segment = score_specification.append_segment(name='red')
+    segment.set_time_signatures(3 * [(4, 8)])
+    selector = segment.select_segment_offsets(start=(10, 16), stop=(13, 16))
+    segment.set_divisions([(2, 32)], selector=selector)
+    selector = segment.select_segment_ratio_part((1, 2), -1)
+    segment.set_rhythm(library.thirty_seconds, selector=selector)
+    score = score_specification.interpret()
+
+    current_function_name = introspectiontools.get_current_function_name()
+    helpertools.write_test_output(score, __file__, current_function_name)
+    assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
