db040609734e221c0faafc13559f886e62aa7f8c
Trevor Bača <trevorbaca@gmail.com>
2012-09-07 00:00:05 +0000

    Implemented stub TimespanInventory methods.

--- experimental/timespantools/TimespanInventory/TimespanInventory.py
+++ experimental/timespantools/TimespanInventory/TimespanInventory.py
@@ -1,5 +1,4 @@
 from abjad.tools.datastructuretools.ObjectInventory import ObjectInventory
-from experimental import helpertools
 
 
 class TimespanInventory(ObjectInventory):
@@ -16,10 +15,21 @@ class TimespanInventory(ObjectInventory):
     ### PUBLIC METHODS ###
 
     def get_timespan_active_at_offset(self, offset):
-        pass
+        timespans = self.get_timespans_active_at_offset(offset)
+        if len(timespans) == 1:
+            return timespans[0]
+        elif 1 < len(timespans):
+            raise Exception('extra timespan error.')
+        else:
+            raise Exception('missing timespan error.')
 
     def get_timespans_active_at_offset(self, offset):
-        pass
+        from experimental import timespantools
+        result = []
+        for timespan in self:
+            if timespantools.expr_happens_during_timespan(timespan, offset):
+                result.append(timespan)
+        return result
 
     def has_timespan_active_at_offset(self, offset):
-        pass
+        return bool(self.get_timespans_active_at_offset(offset))
