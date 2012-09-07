15db3ee2687eec587954bba44d013c43557445a3
Trevor Bača <trevorbaca@gmail.com>
2012-09-07 21:50:23 +0000

    Did more work on Turkish text percussion example.

--- trunk/abjad/docs/source/examples/turkish_text/outline.py
+++ trunk/abjad/docs/source/examples/turkish_text/outline.py
@@ -1,5 +1,6 @@
 # -*- encoding: utf-8 -*-
 from abjad import *
+import os
 
 
 input_text = u"""Taraf'ın Türkiye'nin saygın gazetelerinden olduğunun anımsatıldığı dilekçede."""
@@ -59,6 +60,110 @@ consonant_treatments = {
 }
 
 
+def preprocess_input_text(input_text):
+    input_text = input_text.lower()
+    result = []
+    alphabet = consonant_treatments.keys() + vowel_treatments.keys()
+    permissible_characters = alphabet + [',', '.', ' ']
+    for input_character in input_text:
+        if input_character in permissible_characters:
+            result.append(input_character)
+    result = ''.join(result)
+    return result
 
+def count_vowels(word):
+    vowel_count = 0
+    for character in word:
+        if character in vowel_treatments:
+            vowel_count += 1
+    return vowel_count
 
-def make_score(input_text):
+def count_consonants(word):
+    consonant_count = 0
+    for character in word:
+        if character in consonant_treatments:
+            consonant_count += 1
+    return consonant_count
+
+class WordAnalysis(object):
+    
+    def __init__(self, word):
+        self.word = word
+        self.word_length = len(word)
+        self.vowel_count = 0
+        self.consonant_count = 0
+        self.vowel_pitches = []
+        self.vowel_duration_numerators = []
+        self.consonant_pitches = []
+        self.consonant_duration_numerators = []
+        self.analyze_word()
+
+    def __repr__(self):
+        return 'WordAnalysis({!r})'.format(self.word)
+
+    def analyze_word(self):
+        for character in self.word:
+            if character in vowel_treatments:
+                self.vowel_count += 1
+                duration_numerator, pitch = vowel_treatments[character]
+                self.vowel_duration_numerators.append(duration_numerator)
+                self.vowel_pitches.append(pitch)
+            elif character in consonant_treatments:
+                duration_numerator, pitch = consonant_treatments[character]
+                # account for silent g
+                if duration_numerator:
+                    self.consonant_count += 1
+                    self.consonant_duration_numerators.append(duration_numerator)
+                    self.consonant_pitches.append(pitch)
+            else:
+                raise ValueError('what is character {!r}?'.format(character))
+    
+def find_first_phrase(input_text):
+    input_text = preprocess_input_text(input_text)
+    first_phrase_index = min(input_text.find(','), input_text.find('.'))
+    first_phrase = input_text[:first_phrase_index]
+    return first_phrase
+
+def analyze_phrase(phrase):
+    phrase_words = phrase.split()
+    word_analyses = []
+    for phrase_word in phrase_words:
+        word_analyses.append(WordAnalysis(phrase_word))
+    return word_analyses
+
+def make_score(input_text, base_duration=Duration(1, 16)):
+    first_phrase = find_first_phrase(input_text)
+    word_analyses = analyze_phrase(first_phrase)
+    upper_music, lower_music = make_music_for_word_analyses(word_analyses, base_duration)
+    score_template = scoretemplatetools.GroupedStavesScoreTemplate(staff_count=2)
+    score = score_template()
+    #score['Staff 1'].extend(upper_music)
+    #score['Staff 2'].extend(upper_music)
+    componenttools.get_first_component_in_expr_with_name(score, 'Staff 1').extend(upper_music)
+    componenttools.get_first_component_in_expr_with_name(score, 'Staff 2').extend(lower_music)
+    return score
+    
+def make_music_for_word_analyses(word_analyses, base_duration):
+    vowel_counts = [x.vowel_count for x in word_analyses]
+    upper_durations = [base_duration * x for x in vowel_counts]
+    lower_durations = sequencetools.rotate_sequence(upper_durations, -1)
+    upper_pitches = [x.vowel_pitches for x in word_analyses]
+    lower_pitches = [x.consonant_pitches for x in word_analyses]
+    upper_proportions = [x.vowel_duration_numerators for x in word_analyses]
+    lower_proportions = [x.consonant_duration_numerators for x in word_analyses]
+    upper_music = make_tuplets(upper_durations, upper_proportions, upper_pitches)
+    lower_music = make_tuplets(lower_durations, lower_proportions, lower_pitches)
+    return upper_music, lower_music
+
+def make_tuplets(durations, proportions, pitch_lists):
+    tuplets = []
+    for duration, proportion, pitch_list in zip(
+        durations, proportions, pitch_lists):
+        tuplet = tuplettools.make_tuplet_from_duration_and_proportions(duration, proportion)
+        tuplets.append(tuplet)
+    return tuplets
+
+    
+if __name__ == '__main__':
+    os.system('clear')
+    score = make_score(input_text)
