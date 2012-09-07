b0b120216a9dc6eb214e49205306212af344ce68
Trevor Bača <trevorbaca@gmail.com>
2012-09-07 00:08:56 +0000

    Changed timespantools.expr_happens_during_timespan() to allow for zero-duration expressions.
    This makes the function work for timepoints as well as timespans.

--- experimental/timespantools/expr_happens_during_timespan.py
+++ experimental/timespantools/expr_happens_during_timespan.py
@@ -1,20 +1,37 @@
 def expr_happens_during_timespan(timespan=None):
     r'''.. versionadded:: 1.0
 
-    Make timespan inequality template indicating that expression happens during timespan::
+    ::
 
         >>> from experimental import *
 
-    ::
+    Make timespan inequality template indicating that expression happens during timespan::
 
         >>> timespantools.expr_happens_during_timespan()
-        TimespanInequalityTemplate('t.start <= expr.start < expr.stop <= t.stop')
+        TimespanInequalityTemplate('t.start <= expr.start <= expr.stop <= t.stop')
 
-    Return timespan inequality or timespan inequality template.
+    Make timespan inequality indicating that expression happens during segment ``'red'``::
+
+        >>> selector = selectortools.SingleSegmentSelector(identifier='red')
+        >>> timespan_inequality = timespantools.expr_happens_during_timespan(timespan=selector)
+
+    ::
+
+        >>> z(timespan_inequality)
+        timespantools.TimespanInequality(
+            timespantools.TimespanInequalityTemplate('t.start <= expr.start <= expr.stop <= t.stop'),
+            timespantools.SingleSourceTimespan(
+                selector=selectortools.SingleSegmentSelector(
+                    identifier='red'
+                    )
+                )
+            )
+
+    Return timespan inequality template or timespan inequality.
     '''
     from experimental import timespantools
 
-    template = timespantools.TimespanInequalityTemplate('t.start <= expr.start < expr.stop <= t.stop')
+    template = timespantools.TimespanInequalityTemplate('t.start <= expr.start <= expr.stop <= t.stop')
 
     if timespan is None:
         return template
