dea803c9eaa4ad0bf47d2de2339d690353072239
Trevor Bača <trevorbaca@gmail.com>
2012-09-08 23:16:07 +0000

    Extended timespantools.expr_to_timespan() to work with individual score components.

--- experimental/timespantools/expr_to_timespan.py
+++ experimental/timespantools/expr_to_timespan.py
@@ -9,6 +9,25 @@ def expr_to_timespan(expr):
 
     Return ``expr.timespan`` when `expr` has timespan.
 
+    Return timespan constant when `expr` has start- and stop-offsets::
+
+        >>> staff = Staff("c'8 [ d'8 e'8 f'8 ]")
+
+    ::
+
+        >>> f(staff)
+        \new Staff {
+            c'8 [
+            d'8
+            e'8
+            f'8 ]
+        }
+
+    ::
+
+        >>> timespantools.expr_to_timespan(staff[1])
+        TimespanConstant(start_offset=Offset(1, 8), stop_offset=Offset(1, 4))
+
     Return timespan constant when `expr` is a number::
 
         >>> timespantools.expr_to_timespan(Fraction(7, 8))
@@ -32,6 +51,8 @@ def expr_to_timespan(expr):
         return expr
     if hasattr(expr, 'timespan'):
         return expr.timespan
+    elif hasattr(expr, 'start_offset') and hasattr(expr, 'stop_offset'):
+        return timespantools.TimespanConstant(start_offset=expr.start_offset, stop_offset=expr.stop_offset)
     elif isinstance(expr, numbers.Number):
         return timespantools.TimespanConstant(start_offset=expr, stop_offset=expr)
     elif sequencetools.is_pair(expr):
