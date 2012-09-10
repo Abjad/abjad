3a2203a1349fbce73215b941abfd30648d4ed520
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2012-09-10 19:46:40 +0000

    Wrote Container named __getitem__ tests.

--- trunk/abjad/tools/containertools/Container/test/test_Container___getitem__.py
+++ trunk/abjad/tools/containertools/Container/test/test_Container___getitem__.py
@@ -44,3 +44,40 @@ def test_Container___getitem___04():
     t = Voice("c'8 d'8 e'8 f'8")
 
     assert py.test.raises(IndexError, 't[99]')
+
+
+def test_Container___getitem___05():
+    '''Get one named component in subtree rooted at container.'''
+
+    template = scoretemplatetools.StringQuartetScoreTemplate()
+    score = template()
+
+    assert score['First Violin Staff'].name == 'First Violin Staff'
+    assert score['First Violin Voice'].name == 'First Violin Voice'
+
+
+def test_Container___getitem___06():
+    '''Bad name raises MissingNamedComponentError.'''
+
+    template = scoretemplatetools.StringQuartetScoreTemplate()
+    score = template()
+
+    assert py.test.raises(MissingNamedComponentError, "score['Foo']")
+
+
+def test_Container___getitem___07():
+    '''Duplicate named contexts raise ExtraNamedComponentError.'''
+
+    template = scoretemplatetools.StringQuartetScoreTemplate()
+    score = template()
+
+    assert score['First Violin Voice'].name == 'First Violin Voice'
+
+    score['Cello Staff'].append(Voice(name='First Violin Voice'))
+
+    assert py.test.raises(ExtraNamedComponentError, "score['First Violin Voice']")
+
+    extra_first_violin_voice = score['Cello Staff'].pop()
+
+    assert score['First Violin Voice'].name == 'First Violin Voice'
+    assert score['First Violin Voice'] is not extra_first_violin_voice
