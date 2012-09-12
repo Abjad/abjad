a862be17612d5f2fed6e3d1ace4f279fedff2bc4
Trevor Bača <trevorbaca@gmail.com>
2012-09-12 15:28:03 +0000

    Added test for division setter 'reverse' keyword.
    Test is current area of work and does not pass yet.

--- experimental/specificationtools/SegmentSpecification/test/test_SegmentSpecification_set_divisions.py
+++ experimental/specificationtools/SegmentSpecification/test/test_SegmentSpecification_set_divisions.py
@@ -1,5 +1,6 @@
-from abjad.tools import *
+from abjad import *
 from experimental import *
+import py
 
 
 def test_SegmentSpecification_set_divisions_01():
@@ -36,5 +37,20 @@ def test_SegmentSpecification_set_divisions_01():
 
     current_function_name = introspectiontools.get_current_function_name()
     helpertools.write_test_output(score, __file__, current_function_name)
-
     assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
+
+
+def test_SegmentSpecification_set_divisions_02():
+    '''Reverse keyword works on list input.
+    '''
+    py.test.skip('working on this one now.')
+
+    score_template = scoretemplatetools.GroupedRhythmicStavesScoreTemplate(staff_count=1)
+    score_specification = specificationtools.ScoreSpecification(score_template)
+
+    segment = score_specification.append_segment(name='red')
+    segment.set_time_signatures([(4, 8), (3, 8), (2, 8)])
+    segment.set_divisions([(5, 16), (3, 16)], reverse=True)
+    segment.set_rhythm(library.sixteenths)
+
+    score = score_specification.interpret()
