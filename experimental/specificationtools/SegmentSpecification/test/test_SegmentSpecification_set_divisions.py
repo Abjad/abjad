1004965b1972962276d03afdf1a8a42665f3a54b
Trevor Bača <trevorbaca@gmail.com>
2012-09-12 19:59:07 +0000

    Added division interpretation test.
    Tests checks that request-time 'reverse' and set-time 'reverse' undo each other.
    
    Test is currently under construction.

--- experimental/specificationtools/SegmentSpecification/test/test_SegmentSpecification_set_divisions.py
+++ experimental/specificationtools/SegmentSpecification/test/test_SegmentSpecification_set_divisions.py
@@ -1,5 +1,6 @@
 from abjad import *
 from experimental import *
+import py
 
 
 def test_SegmentSpecification_set_divisions_01():
@@ -56,3 +57,26 @@ def test_SegmentSpecification_set_divisions_02():
     current_function_name = introspectiontools.get_current_function_name()
     helpertools.write_test_output(score, __file__, current_function_name)
     assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
+
+
+def test_SegmentSpecification_set_divisions_03():
+    '''Reverse keywords work on both request and set methods.
+    Reverse keywords effectively undo each other.
+    '''
+    py.test.skip('working on this one now.')
+
+    score_template = scoretemplatetools.GroupedRhythmicStavesScoreTemplate(staff_count=1)
+    score_specification = specificationtools.ScoreSpecification(score_template)
+
+    segment = score_specification.append_segment(name='red')
+    segment.set_time_signatures([(4, 8), (3, 8), (2, 8)])
+    segment.set_divisions([(5, 16), (3, 16)], contexts=['Voice 1'])
+    voice_1_division_command = segment.request_division_command('Voice 1', reverse=True)
+    segment.set_divisions(voice_1_division_command, contexts=['Voice 2'], reverse=True)
+    segment.set_rhythm(library.sixteenths)
+
+    score = score_specification.interpret()
+
+    current_function_name = introspectiontools.get_current_function_name()
+    helpertools.write_test_output(score, __file__, current_function_name, render_pdf=True)
+    #assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
