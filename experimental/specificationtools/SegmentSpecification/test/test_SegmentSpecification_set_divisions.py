a102ad53279efc0e7fc753386245e447e6650229
Trevor Bača <trevorbaca@gmail.com>
2012-09-14 12:14:26 +0000

    Deprecated SegmentSpecification.set_retrograde_divisions().
    Use SegmentSpecification.set_divisions(reverse=True) instead.
    
    Use of the 'reverse' boolean is now recommended everywhere
    in both the main and experimental lines.

--- experimental/specificationtools/SegmentSpecification/test/test_SegmentSpecification_set_divisions.py
+++ experimental/specificationtools/SegmentSpecification/test/test_SegmentSpecification_set_divisions.py
@@ -77,3 +77,28 @@ def test_SegmentSpecification_set_divisions_03():
     current_function_name = introspectiontools.get_current_function_name()
     helpertools.write_test_output(score, __file__, current_function_name)
     assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
+
+
+def test_SegmentSpecification_set_divisions_04():
+    '''V3, V4 divisions equal V1, V2 divisions reversed.
+    '''
+
+    score_template = scoretemplatetools.GroupedRhythmicStavesScoreTemplate(staff_count=4)
+    score_specification = specificationtools.ScoreSpecification(score_template)
+
+    segment = score_specification.append_segment('red')
+    segment.set_time_signatures([(4, 8), (3, 8)])
+    segment.set_divisions([(3, 16)], contexts='Voice 1', truncate=True)
+    segment.set_divisions([(4, 16)], contexts='Voice 2', truncate=True)
+    source_1 = score_specification.request_divisions('Voice 1', 'red', segment_count=1)
+    source_2 = score_specification.request_divisions('Voice 2', 'red', segment_count=1)
+
+    segment.set_divisions(source_1, contexts=['Voice 3'], reverse=True, truncate=True)
+    segment.set_divisions(source_2, contexts=['Voice 4'], reverse=True, truncate=True)
+    segment.set_rhythm(library.thirty_seconds)
+    segment = score_specification.append_segment('blue')
+    score = score_specification.interpret()
+
+    current_function_name = introspectiontools.get_current_function_name()
+    helpertools.write_test_output(score, __file__, current_function_name)
+    assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
