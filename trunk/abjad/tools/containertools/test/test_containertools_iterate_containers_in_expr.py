4ddeae19a5ecaacf7fb3eaf6027de23d44e4f3c2
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2012-09-18 23:40:52 +0000

    Added tests for containertools.iterate_containers_in_expr().

---
+++ trunk/abjad/tools/containertools/test/test_containertools_iterate_containers_in_expr.py
@@ -0,0 +1,30 @@
+from abjad import *
+
+
+def test_containertools_iterate_containers_in_expr_01():
+
+    staff = Staff([Voice("c'8 d'8"), Voice("e'8 f'8 g'8")])
+    tuplet = Tuplet(Fraction(2, 3), staff[1][:])
+    staff.is_parallel = True
+
+    containers = containertools.iterate_containers_in_expr(staff, reverse=True)
+    containers = list(containers)
+
+    assert containers[0] is staff
+    assert containers[1] is staff[1]
+    assert containers[2] is tuplet
+    assert containers[3] is staff[0]
+
+def test_containertools_iterate_containers_in_expr_02():
+
+    staff = Staff([Voice("c'8 d'8"), Voice("e'8 f'8 g'8")])
+    tuplet = Tuplet(Fraction(2, 3), staff[1][:])
+    staff.is_parallel = True
+
+    containers = containertools.iterate_containers_in_expr(staff)
+    containers = list(containers)
+
+    assert containers[0] is staff
+    assert containers[1] is staff[0]
+    assert containers[2] is staff[1]
+    assert containers[3] is tuplet
