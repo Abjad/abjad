3b2b8b65adeffdebf24768d56c8f7686b6d36f1f
Trevor Bača <trevorbaca@gmail.com>
2012-09-20 17:52:10 +0000

    Added test to show from-future division command request.
    Example:
    
        score_template = scoretemplatetools.GroupedRhythmicStavesScoreTemplate(staff_count=1)
        score_specification = specificationtools.ScoreSpecification(score_template)
    
        red_segment = score_specification.make_segment(name='red')
        red_segment.set_time_signatures([(2, 8), (2, 8)])
        red_segment.set_divisions([(3, 16)])
        red_segment.set_rhythm(library.sixteenths)
    
        blue_segment = score_specification.make_segment(name='blue')
        blue_segment.set_time_signatures([(4, 8), (4, 8)])
        blue_segment.set_divisions([(5, 16)])
    
        green_segment = score_specification.make_segment(name='green', index=1)
        green_segment.set_time_signatures([(3, 8), (3, 8)])
        blue_division_command = blue_segment.request_division_command('Voice 1')
        green_segment.set_divisions(blue_division_command)
    
        score = score_specification.interpret()
    
    The score above defines a three-segment score.
    
    The red and blue segments are defined first. The green segment is
    then defined to come between the red and blue segments.
    The green segment then requests the blue segment's division command.

--- experimental/specificationtools/SegmentSpecification/test/test_SegmentSpecification_request_division_command.py
+++ experimental/specificationtools/SegmentSpecification/test/test_SegmentSpecification_request_division_command.py
@@ -28,3 +28,31 @@ def test_SegmentSpecification_request_division_command_01():
     current_function_name = introspectiontools.get_current_function_name()
     helpertools.write_test_output(score, __file__, current_function_name)
     assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
+
+
+def test_SegmentSpecification_request_division_command_02():
+    '''Request division command from future segment.
+    '''
+
+    score_template = scoretemplatetools.GroupedRhythmicStavesScoreTemplate(staff_count=1)
+    score_specification = specificationtools.ScoreSpecification(score_template)
+
+    red_segment = score_specification.make_segment(name='red')
+    red_segment.set_time_signatures([(2, 8), (2, 8)])
+    red_segment.set_divisions([(3, 16)])
+    red_segment.set_rhythm(library.sixteenths)
+
+    blue_segment = score_specification.make_segment(name='blue')
+    blue_segment.set_time_signatures([(4, 8), (4, 8)])
+    blue_segment.set_divisions([(5, 16)])
+
+    green_segment = score_specification.make_segment(name='green', index=1)
+    green_segment.set_time_signatures([(3, 8), (3, 8)])
+    blue_division_command = blue_segment.request_division_command('Voice 1')
+    green_segment.set_divisions(blue_division_command)
+
+    score = score_specification.interpret()
+
+    current_function_name = introspectiontools.get_current_function_name()
+    helpertools.write_test_output(score, __file__, current_function_name)
+    assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
--- experimental/specificationtools/SegmentSpecification/test/test_SegmentSpecification_request_divisions.py
+++ experimental/specificationtools/SegmentSpecification/test/test_SegmentSpecification_request_divisions.py
@@ -1,9 +1,10 @@
 from abjad import *
 from experimental import *
+import py
 
 
 def test_SegmentSpecification_request_divisions_01():
-    '''Request all divisions from earlier segment. Reverse divisions.
+    '''Request divisions from earlier segment. Reverse divisions at set-time.
     '''
 
     score_template = scoretemplatetools.GroupedRhythmicStavesScoreTemplate(staff_count=1)
@@ -23,3 +24,32 @@ def test_SegmentSpecification_request_divisions_01():
     current_function_name = introspectiontools.get_current_function_name()
     helpertools.write_test_output(score, __file__, current_function_name)
     assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
+
+
+def test_SegmentSpecification_request_divisions_02():
+    '''Request divisions from later segment. Reverse divisions at set-time.
+    '''
+    py.test.skip('working on this one now.')
+
+    score_template = scoretemplatetools.GroupedRhythmicStavesScoreTemplate(staff_count=1)
+    score_specification = specificationtools.ScoreSpecification(score_template)
+    
+    red_segment = score_specification.make_segment(name='red')
+    red_segment.set_time_signatures([((2, 8), (2, 8)])
+    red_segment.set_divisions([(3, 16)])
+    red_segment.set_rhythm(library.sixteenths)
+    
+    blue_segment = score_specification.make_segment(name='blue')
+    blue_segment.set_time_signatures([(4, 8), (4, 8)]
+    blue_segment.set_divisions([(5, 16)])
+
+    green_segment = score_specification.make_segment(name='green', index=1)
+    green_segment.set_time_signatures([(3, 8), (3, 8)])
+    blue_divisions = blue_segment.request_divisions('Voice 1')
+    green_segment.set_divisions(blue_divisions, reverse=True)
+
+    score = score_specification.interpret()
+    
+    current_function_name = introspectiontools.get_current_function_name()
+    helpertools.write_test_output(score, __file__, current_function_name, render_pdf=True)
+    #assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
