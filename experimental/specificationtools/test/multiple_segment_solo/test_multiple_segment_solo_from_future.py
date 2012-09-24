6865aa4476a8ac19749f5910b2bd377d451268d8
Trevor Bača <trevorbaca@gmail.com>
2012-09-24 11:51:13 +0000

    Added from-future time signature tests.

---
+++ experimental/specificationtools/test/multiple_segment_solo/test_multiple_segment_solo_from_future.py
@@ -0,0 +1,73 @@
+from abjad import *
+from experimental import *
+import py
+
+
+def test_multiple_segment_solo_from_future_01():
+    '''First segment defined after second.
+    Absolute time signature request.
+    '''
+
+    score_template = scoretemplatetools.GroupedRhythmicStavesScoreTemplate(staff_count=1)
+    score_specification = specificationtools.ScoreSpecification(score_template)
+
+    blue_segment = score_specification.make_segment(name='blue')
+    blue_segment.set_time_signatures([(3, 8), (3, 8)])
+    blue_segment.set_divisions([(2, 16), (4, 16)])
+    blue_segment.set_rhythm(library.sixteenths)
+
+    red_segment = score_specification.make_segment(name='red', index=0)
+    red_segment.set_time_signatures([(2, 8), (2, 8)])
+
+    score = score_specification.interpret()
+
+    current_function_name = introspectiontools.get_current_function_name()
+    helpertools.write_test_output(score, __file__, current_function_name)
+    assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
+
+
+def test_multiple_segment_solo_from_future_02():
+    '''First segment defined after second.
+    Time signature material request.
+    '''
+
+    score_template = scoretemplatetools.GroupedRhythmicStavesScoreTemplate(staff_count=1)
+    score_specification = specificationtools.ScoreSpecification(score_template)
+
+    blue_segment = score_specification.make_segment(name='blue')
+    blue_segment.set_time_signatures([(3, 8), (3, 8)])
+    blue_segment.set_divisions([(2, 16), (4, 16)])
+    blue_segment.set_rhythm(library.sixteenths)
+
+    red_segment = score_specification.make_segment(name='red', index=0)
+    red_segment.set_time_signatures(blue_segment.request_time_signatures())
+
+    score = score_specification.interpret()
+
+    current_function_name = introspectiontools.get_current_function_name()
+    helpertools.write_test_output(score, __file__, current_function_name)
+    assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
+
+
+def test_multiple_segment_solo_from_future_03():
+    '''First segment defined after second.
+    Time signature command request.
+    '''
+    py.test.skip('working on this one now.')
+
+    score_template = scoretemplatetools.GroupedRhythmicStavesScoreTemplate(staff_count=1)
+    score_specification = specificationtools.ScoreSpecification(score_template)
+
+    blue_segment = score_specification.make_segment(name='blue')
+    blue_segment.set_time_signatures([(3, 8), (3, 8)])
+    blue_segment.set_divisions([(2, 16), (4, 16)])
+    blue_segment.set_rhythm(library.sixteenths)
+
+    red_segment = score_specification.make_segment(name='red', index=0)
+    red_segment.set_time_signatures(blue_segment.request_time_signature_command())
+
+    score = score_specification.interpret()
+
+    current_function_name = introspectiontools.get_current_function_name()
+    helpertools.write_test_output(score, __file__, current_function_name, render_pdf=True)
+    #assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
--- experimental/specificationtools/test/single_segment_solo/test_single_segment_solo.py
+++ experimental/specificationtools/test/single_segment_solo/test_single_segment_solo.py
@@ -10,17 +10,14 @@ def test_single_segment_solo_01():
 
     score_template = scoretemplatetools.GroupedRhythmicStavesScoreTemplate(staff_count=1)
     score_specification = specificationtools.ScoreSpecification(score_template)
-
     segment = score_specification.make_segment('red')
     segment.set_time_signatures([(4, 8), (3, 8)])
     segment.set_divisions([(3, 16)], contexts=segment.v1)
     segment.set_rhythm(library.thirty_seconds)
-
     score = score_specification.interpret()
 
     current_function_name = introspectiontools.get_current_function_name()
     helpertools.write_test_output(score, __file__, current_function_name)
-
     assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
 
 
@@ -30,17 +27,14 @@ def test_single_segment_solo_02():
 
     score_template = scoretemplatetools.GroupedRhythmicStavesScoreTemplate(staff_count=1)
     score_specification = specificationtools.ScoreSpecification(score_template)
-    
     segment = score_specification.make_segment('red')
     segment.set_time_signatures([(4, 8), (3, 8)])
     segment.set_divisions([(14, 16)], contexts=segment.v1)
     segment.set_rhythm(library.thirty_seconds)
-
     score = score_specification.interpret()
 
     current_function_name = introspectiontools.get_current_function_name()
     helpertools.write_test_output(score, __file__, current_function_name)
-
     assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
 
 
@@ -51,15 +45,12 @@ def test_single_segment_solo_03():
 
     score_template = scoretemplatetools.GroupedRhythmicStavesScoreTemplate(staff_count=1)
     score_specification = specificationtools.ScoreSpecification(score_template)
-    
     segment = score_specification.make_segment('red')
     segment.set_time_signatures([(4, 8), (3, 8)])
     segment.set_divisions([(20, 16)], contexts=segment.v1)
     segment.set_rhythm(library.thirty_seconds)
-
     score = score_specification.interpret()
 
     current_function_name = introspectiontools.get_current_function_name()
     helpertools.write_test_output(score, __file__, current_function_name)
-
     assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
