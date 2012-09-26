bca4bcab71bc66526dc8eb787981861be32204ef
Trevor Bača <trevorbaca@gmail.com>
2012-09-26 00:06:57 +0000

    Completed from-future time signature material request tests.

--- experimental/specificationtools/test/multiple_segment_solo/test_multiple_segment_solo_from_future.py
+++ experimental/specificationtools/test/multiple_segment_solo/test_multiple_segment_solo_from_future.py
@@ -141,3 +141,76 @@ def test_multiple_segment_solo_from_future_06():
     current_function_name = introspectiontools.get_current_function_name()
     helpertools.write_test_output(score, __file__, current_function_name)
     assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
+
+
+def test_multiple_segment_solo_from_future_07():
+    '''First segment defined after second.
+    Time signature material request with request-time reverse.
+    '''
+
+    score_template = scoretemplatetools.GroupedRhythmicStavesScoreTemplate(staff_count=1)
+    score_specification = specificationtools.ScoreSpecification(score_template)
+
+    blue_segment = score_specification.make_segment(name='blue')
+    blue_segment.set_time_signatures([(3, 8), (4, 8)])
+    blue_segment.set_divisions([(2, 16), (6, 16)])
+    blue_segment.set_rhythm(library.sixteenths)
+
+    red_segment = score_specification.make_segment(name='red', index=0)
+    blue_time_signatures = blue_segment.request_time_signatures(reverse=True)
+    red_segment.set_time_signatures(blue_time_signatures)
+
+    score = score_specification.interpret()
+
+    current_function_name = introspectiontools.get_current_function_name()
+    helpertools.write_test_output(score, __file__, current_function_name)
+    assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
+
+
+def test_multiple_segment_solo_from_future_08():
+    '''First segment defined after second.
+    Time signature material request with set-time reverse.
+    '''
+
+    score_template = scoretemplatetools.GroupedRhythmicStavesScoreTemplate(staff_count=1)
+    score_specification = specificationtools.ScoreSpecification(score_template)
+
+    blue_segment = score_specification.make_segment(name='blue')
+    blue_segment.set_time_signatures([(3, 8), (4, 8)])
+    blue_segment.set_divisions([(2, 16), (6, 16)])
+    blue_segment.set_rhythm(library.sixteenths)
+
+    red_segment = score_specification.make_segment(name='red', index=0)
+    blue_time_signatures = blue_segment.request_time_signatures()
+    red_segment.set_time_signatures(blue_time_signatures, reverse=True)
+
+    score = score_specification.interpret()
+
+    current_function_name = introspectiontools.get_current_function_name()
+    helpertools.write_test_output(score, __file__, current_function_name)
+    assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
+
+
+def test_multiple_segment_solo_from_future_09():
+    '''First segment defined after second.
+    Time signature material request with both request- and set-time reverse.
+    The reverse indications effectively undo each other.
+    '''
+
+    score_template = scoretemplatetools.GroupedRhythmicStavesScoreTemplate(staff_count=1)
+    score_specification = specificationtools.ScoreSpecification(score_template)
+
+    blue_segment = score_specification.make_segment(name='blue')
+    blue_segment.set_time_signatures([(3, 8), (4, 8)])
+    blue_segment.set_divisions([(2, 16), (6, 16)])
+    blue_segment.set_rhythm(library.sixteenths)
+
+    red_segment = score_specification.make_segment(name='red', index=0)
+    blue_time_signatures = blue_segment.request_time_signatures(reverse=True)
+    red_segment.set_time_signatures(blue_time_signatures, reverse=True)
+
+    score = score_specification.interpret()
+
+    current_function_name = introspectiontools.get_current_function_name()
+    helpertools.write_test_output(score, __file__, current_function_name)
+    assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
