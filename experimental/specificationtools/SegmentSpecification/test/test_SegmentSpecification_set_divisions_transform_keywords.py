b343123ef538565ed8c72a44bb90eb73895c8fff
Trevor Bača <trevorbaca@gmail.com>
2012-09-27 18:39:33 +0000

    Added division transform tests.

---
+++ experimental/specificationtools/SegmentSpecification/test/test_SegmentSpecification_set_divisions_transform_keywords.py
@@ -0,0 +1,70 @@
+from abjad import *
+from experimental import *
+
+
+def test_SegmentSpecification_set_divisions_transform_keywords_01():
+    '''Division 'index' transform.
+    '''
+
+    score_template = scoretemplatetools.GroupedRhythmicStavesScoreTemplate(staff_count=1)
+    score_specification = specificationtools.ScoreSpecification(score_template)
+    red_segment = score_specification.make_segment('red')
+    red_segment.set_time_signatures([(4, 8), (4, 8)])
+    red_segment.set_divisions([(1, 16), (2, 16), (3, 16), (4, 16)], index=-2)
+    red_segment.set_rhythm(library.sixteenths)
+    score = score_specification.interpret()
+
+    current_function_name = introspectiontools.get_current_function_name()
+    helpertools.write_test_output(score, __file__, current_function_name)
+    assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
+
+
+def test_SegmentSpecification_set_divisions_transform_keywords_02():
+    '''Division 'count' transform.
+    '''
+
+    score_template = scoretemplatetools.GroupedRhythmicStavesScoreTemplate(staff_count=1)
+    score_specification = specificationtools.ScoreSpecification(score_template)
+    red_segment = score_specification.make_segment('red')
+    red_segment.set_time_signatures([(4, 8), (4, 8)])
+    red_segment.set_divisions([(1, 16), (2, 16), (3, 16), (4, 16)], count=6)
+    red_segment.set_rhythm(library.sixteenths)
+    score = score_specification.interpret()
+
+    current_function_name = introspectiontools.get_current_function_name()
+    helpertools.write_test_output(score, __file__, current_function_name)
+    assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
+
+
+def test_SegmentSpecification_set_divisions_transform_keywords_03():
+    '''Division 'reverse' transform.
+    '''
+
+    score_template = scoretemplatetools.GroupedRhythmicStavesScoreTemplate(staff_count=1)
+    score_specification = specificationtools.ScoreSpecification(score_template)
+    red_segment = score_specification.make_segment('red')
+    red_segment.set_time_signatures([(4, 8), (4, 8)])
+    red_segment.set_divisions([(1, 16), (2, 16), (3, 16), (4, 16)], reverse=True)
+    red_segment.set_rhythm(library.sixteenths)
+    score = score_specification.interpret()
+
+    current_function_name = introspectiontools.get_current_function_name()
+    helpertools.write_test_output(score, __file__, current_function_name)
+    assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
+
+
+def test_SegmentSpecification_set_divisions_transform_keywords_04():
+    '''Division 'rotation' transform.
+    '''
+
+    score_template = scoretemplatetools.GroupedRhythmicStavesScoreTemplate(staff_count=1)
+    score_specification = specificationtools.ScoreSpecification(score_template)
+    red_segment = score_specification.make_segment('red')
+    red_segment.set_time_signatures([(4, 8), (4, 8)])
+    red_segment.set_divisions([(1, 16), (2, 16), (3, 16), (4, 16)], rotation=-2)
+    red_segment.set_rhythm(library.sixteenths)
+    score = score_specification.interpret()
+
+    current_function_name = introspectiontools.get_current_function_name()
+    helpertools.write_test_output(score, __file__, current_function_name)
+    assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
