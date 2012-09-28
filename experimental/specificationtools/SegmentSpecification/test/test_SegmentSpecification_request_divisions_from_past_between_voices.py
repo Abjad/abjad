d120c0ceebe867a84db64c30d0b837b05005c3b8
Trevor Bača <trevorbaca@gmail.com>
2012-09-28 13:09:36 +0000

    Wrote from-past between-voice division material request tests.

---
+++ experimental/specificationtools/SegmentSpecification/test/test_SegmentSpecification_request_divisions_from_past_between_voices.py
@@ -0,0 +1,106 @@
+from abjad import *
+from experimental import *
+
+
+def test_SegmentSpecification_request_divisions_from_past_between_voices_01():
+    '''From-past division material request between voices.
+    '''
+
+    score_template = scoretemplatetools.GroupedRhythmicStavesScoreTemplate(staff_count=2)
+    score_specification = specificationtools.ScoreSpecification(score_template)
+
+    red_segment = score_specification.make_segment(name='red')
+    blue_segment = score_specification.make_segment(name='blue')
+
+    red_segment.set_time_signatures([(2, 8), (2, 8)])
+    red_segment.set_divisions([(2, 16), (3, 16)], contexts=['Voice 1'])
+    red_segment.set_divisions([(2, 16), (5, 16)], contexts=['Voice 2'])
+    red_segment.set_rhythm(library.sixteenths)
+
+    blue_segment.set_time_signatures([(3, 8), (3, 8)])
+    red_voice_2_divisions = red_segment.request_divisions('Voice 2')
+    blue_segment.set_divisions(red_voice_2_divisions, contexts=['Voice 1'])
+
+    score = score_specification.interpret()
+
+    current_function_name = introspectiontools.get_current_function_name()
+    helpertools.write_test_output(score, __file__, current_function_name)
+    assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
+
+
+def test_SegmentSpecification_request_divisions_from_past_between_voices_02():
+    '''From-past division material request between voices with request-time reverse.
+    '''
+
+    score_template = scoretemplatetools.GroupedRhythmicStavesScoreTemplate(staff_count=2)
+    score_specification = specificationtools.ScoreSpecification(score_template)
+
+    red_segment = score_specification.make_segment(name='red')
+    blue_segment = score_specification.make_segment(name='blue')
+
+    red_segment.set_time_signatures([(2, 8), (2, 8)])
+    red_segment.set_divisions([(2, 16), (3, 16)], contexts=['Voice 1'])
+    red_segment.set_divisions([(2, 16), (5, 16)], contexts=['Voice 2'])
+    red_segment.set_rhythm(library.sixteenths)
+
+    blue_segment.set_time_signatures([(3, 8), (3, 8)])
+    red_voice_2_divisions = red_segment.request_divisions('Voice 2', reverse=True)
+    blue_segment.set_divisions(red_voice_2_divisions, contexts=['Voice 1'])
+
+    score = score_specification.interpret()
+
+    current_function_name = introspectiontools.get_current_function_name()
+    helpertools.write_test_output(score, __file__, current_function_name)
+    assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
+
+
+def test_SegmentSpecification_request_divisions_from_past_between_voices_03():
+    '''From-past division material request between voices with set-time reverse.
+    '''
+
+    score_template = scoretemplatetools.GroupedRhythmicStavesScoreTemplate(staff_count=2)
+    score_specification = specificationtools.ScoreSpecification(score_template)
+
+    red_segment = score_specification.make_segment(name='red')
+    blue_segment = score_specification.make_segment(name='blue')
+
+    red_segment.set_time_signatures([(2, 8), (2, 8)])
+    red_segment.set_divisions([(2, 16), (3, 16)], contexts=['Voice 1'])
+    red_segment.set_divisions([(2, 16), (5, 16)], contexts=['Voice 2'])
+    red_segment.set_rhythm(library.sixteenths)
+
+    blue_segment.set_time_signatures([(3, 8), (3, 8)])
+    red_voice_2_divisions = red_segment.request_divisions('Voice 2')
+    blue_segment.set_divisions(red_voice_2_divisions, contexts=['Voice 1'], reverse=True)
+
+    score = score_specification.interpret()
+
+    current_function_name = introspectiontools.get_current_function_name()
+    helpertools.write_test_output(score, __file__, current_function_name)
+    assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
+
+
+def test_SegmentSpecification_request_divisions_from_past_between_voices_04():
+    '''From-past division material request between voices with both request- and set-time reverse.
+    '''
+
+    score_template = scoretemplatetools.GroupedRhythmicStavesScoreTemplate(staff_count=2)
+    score_specification = specificationtools.ScoreSpecification(score_template)
+
+    red_segment = score_specification.make_segment(name='red')
+    blue_segment = score_specification.make_segment(name='blue')
+
+    red_segment.set_time_signatures([(2, 8), (2, 8)])
+    red_segment.set_divisions([(2, 16), (3, 16)], contexts=['Voice 1'])
+    red_segment.set_divisions([(2, 16), (5, 16)], contexts=['Voice 2'])
+    red_segment.set_rhythm(library.sixteenths)
+
+    blue_segment.set_time_signatures([(3, 8), (3, 8)])
+    red_voice_2_divisions = red_segment.request_divisions('Voice 2', reverse=True)
+    blue_segment.set_divisions(red_voice_2_divisions, contexts=['Voice 1'], reverse=True)
+
+    score = score_specification.interpret()
+
+    current_function_name = introspectiontools.get_current_function_name()
+    helpertools.write_test_output(score, __file__, current_function_name)
+    assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
