5600c510d91f13122860ce0c5a898cc234ec1868
Trevor Bača <trevorbaca@gmail.com>
2012-10-03 01:32:32 +0000

    Unskipped developertools test.
    This leaves only 1 skipped test in the system.

--- trunk/abjad/tools/developerscripttools/CountToolsScript/test/test_CountToolsScript___init__.py
+++ trunk/abjad/tools/developerscripttools/CountToolsScript/test/test_CountToolsScript___init__.py
@@ -1,9 +1,6 @@
 from abjad.tools import developerscripttools
-import py.test
 
 
-@py.test.skip('Script not yet implemented.')
 def test_CountToolsScript___init___01():
 
     script = developerscripttools.CountToolsScript()
-
