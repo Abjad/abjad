4da2559cf95a31205d8d1b22bd29aa8e3a1621a1
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2012-10-24 18:23:42 +0000

    Added new tie tools:
        * tietools.get_tie_chains_masked_by_components()
    
    This function behaves the same as tietools.get_nontrivial_tie_chains_masked_by_components(), except
    that it also return trivial tie chains:
    
    ::
    
        >>> staff = Staff("abj: | 2/4 c'4 d'4 ~ || 2/4 d'4 e'4 ~ || 2/4 e'4 f'4 |")
        >>> f(staff)
        \new Staff {
            {
                \time 2/4
                c'4
                d'4 ~
            }
            {
                d'4
                e'4 ~
            }
            {
                e'4
                f'4
            }
        }
    
        ::
    
        >>> for tie_chain in tietools.get_tie_chains_masked_by_components(staff.leaves):
        ...     tie_chain
        ... 
        TieChain((Note("c'4"),))
        TieChain((Note("d'4"), Note("d'4")))
        TieChain((Note("e'4"), Note("e'4")))
        TieChain((Note("f'4"),))
    
        ::
    
        >>> for tie_chain in tietools.get_tie_chains_masked_by_components(staff[1].leaves):
        ...     tie_chain
        ... 
        TieChain((Note("d'4"),))
        TieChain((Note("e'4"),))
    
        ::
    
        >>> for tie_chain in tietools.get_tie_chains_masked_by_components(staff[1:]):
        ...     tie_chain
        ... 
        TieChain((Note("d'4"),))
        TieChain((Note("e'4"), Note("e'4")))
        TieChain((Note("f'4"),))
    
    NOTE: This should be noted in 2.11 release notes.

---
+++ trunk/abjad/docs/source/api/tools/tietools/get_tie_chains_masked_by_components.rst
@@ -0,0 +1,4 @@
+tietools.get_tie_chains_masked_by_components
+============================================
+
+.. autofunction:: abjad.tools.tietools.get_tie_chains_masked_by_components.get_tie_chains_masked_by_components
---
+++ trunk/abjad/tools/tietools/get_tie_chains_masked_by_components.py
@@ -0,0 +1,78 @@
+from abjad.tools import componenttools
+
+
+def get_tie_chains_masked_by_components(components):
+    r'''Get tie chains masked by `components`:
+
+    ::
+
+        >>> staff = Staff("abj: | 2/4 c'4 d'4 ~ || 2/4 d'4 e'4 ~ || 2/4 e'4 f'4 |")
+        >>> f(staff)
+        \new Staff {
+            {
+                \time 2/4
+                c'4
+                d'4 ~
+            }
+            {
+                d'4
+                e'4 ~
+            }
+            {
+                e'4
+                f'4
+            }
+        }
+
+    ::
+
+        >>> for tie_chain in tietools.get_tie_chains_masked_by_components(staff.leaves):
+        ...     tie_chain
+        ... 
+        TieChain((Note("c'4"),))
+        TieChain((Note("d'4"), Note("d'4")))
+        TieChain((Note("e'4"), Note("e'4")))
+        TieChain((Note("f'4"),))
+
+    ::
+
+        >>> for tie_chain in tietools.get_tie_chains_masked_by_components(staff[1].leaves):
+        ...     tie_chain
+        ... 
+        TieChain((Note("d'4"),))
+        TieChain((Note("e'4"),))
+
+    ::
+
+        >>> for tie_chain in tietools.get_tie_chains_masked_by_components(staff[1:]):
+        ...     tie_chain
+        ... 
+        TieChain((Note("d'4"),))
+        TieChain((Note("e'4"), Note("e'4")))
+        TieChain((Note("f'4"),))
+
+    Return list of zero or more (possibly masked) tie chains.
+    '''
+    from abjad.tools import iterationtools
+    from abjad.tools import tietools
+
+    assert componenttools.all_are_components(components)
+
+    leaves_in_components = set(iterationtools.iterate_leaves_in_expr(components))
+
+    tie_chains = []
+    for leaf in leaves_in_components:
+        tie_chain = tietools.get_tie_chain(leaf)
+        if tie_chain not in tie_chains:
+            tie_chains.append(tie_chain)
+
+    result = []
+    for tie_chain in tie_chains:
+        leaves_in_tie_chain = set(iterationtools.iterate_leaves_in_expr(tie_chain))
+        leaves_intersecting = leaves_in_components.intersection(leaves_in_tie_chain)
+        result.append(tietools.TieChain(tuple(sorted(leaves_intersecting,
+            key=lambda x: x.start_offset))))
+    result.sort(key=lambda x: x[0].start_offset)
+
+    return result
+
