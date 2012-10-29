15387b5c7a158b71500f26e8d97e6de19c18ebb6
Trevor Bača <trevorbaca@gmail.com>
2012-10-29 21:47:07 +0000

    Extended heleprtools.write_test_output() to format multiple title lines where necessary.

--- experimental/helpertools/write_test_output.py
+++ experimental/helpertools/write_test_output.py
@@ -9,11 +9,9 @@ def write_test_output(score, full_file_name, test_function_name,
     if go: cache_ly = cache_pdf = render_pdf = True
     if not any([cache_ly, cache_pdf, render_pdf]): return
     lilypond_file = lilypondfiletools.make_floating_time_signature_lilypond_file(score)
-    parts = test_function_name.split('_')
-    test_number = int(parts[-1])
-    title_words = ' '.join(parts[1:-1])
-    title = '{} {}'.format(title_words, test_number)
-    lilypond_file.header_block.title = markuptools.make_centered_title_markup(title, font_size=6)
+    title_lines = test_function_name_to_title_lines(test_function_name)
+    lilypond_file.header_block.title = markuptools.make_centered_title_markup(
+        title_lines, font_size=6, vspace_before=2, vspace_after=4)
     lilypond_file.score.set.proportionalNotationDuration = schemetools.SchemeMoment((1, 48))
     parent_directory_name = os.path.dirname(full_file_name)
     if render_pdf:
@@ -26,3 +24,29 @@ def write_test_output(score, full_file_name, test_function_name,
         file_name = '{}.ly'.format(test_function_name)
         ly_path_name = os.path.join(parent_directory_name, file_name)
         file(ly_path_name, 'w').write(score.lilypond_format)
+
+
+def test_function_name_to_title_lines(test_function_name):
+    title_lines = []
+    parts = test_function_name.split('_')
+    test_number = int(parts[-1])
+    parts.pop(0)
+    parts.pop(-1)
+    if parts[0][0].isupper():
+        title_lines.append(parts.pop(0))
+    lengths = [len(part) for part in parts]
+    if 35 < sum(lengths):
+        halves = sequencetools.partition_sequence_by_ratio_of_weights(lengths, [1, 1])
+        left_count = len(halves[0])
+        right_count = len(halves[-1])
+        assert left_count + right_count == len(lengths)
+        left_parts = parts[:left_count]
+        title_lines.append(' '.join(left_parts))
+        right_parts = parts[-right_count:]
+        right_parts.append(str(test_number))
+        title_lines.append(' '.join(right_parts))
+    else:
+        title_words = ' '.join(parts)
+        title = '{} {}'.format(title_words, test_number)
+        title_lines.append(title)
+    return title_lines
