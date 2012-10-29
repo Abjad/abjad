9ae3c49231e040e7ff532f614b3db4ff6112c8a6
Trevor Bača <trevorbaca@gmail.com>
2012-10-29 16:09:18 +0000

    Extended interpreter to allow divisions to be set from time signature request.

---
+++ experimental/specificationtools/SegmentSpecification/test/test_SegmentSpecification_set_divisions.py
@@ -0,0 +1,21 @@
+from abjad import *
+from experimental import *
+
+
+def test_SegmentSpecification_set_divisions_01():
+    '''Set divisions from time signature request.
+    '''
+
+    score_template = scoretemplatetools.GroupedRhythmicStavesScoreTemplate(staff_count=2)
+    score_specification = specificationtools.ScoreSpecification(score_template)
+
+    red_segment = score_specification.make_segment(name='red')
+    red_segment.set_time_signatures([(4, 8), (3, 8), (2, 8)])
+    divisions = red_segment.request_time_signatures()
+    red_segment.set_divisions(divisions, contexts=['Voice 1'], reverse=True)
+    red_segment.set_rhythm(library.sixteenths)
+    score = score_specification.interpret()
+
+    current_function_name = introspectiontools.get_current_function_name()
+    helpertools.write_test_output(score, __file__, current_function_name)
+    assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
