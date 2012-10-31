3aec91bc504e65f7b3882faf81bbbb5db086f2af
Trevor Bača <trevorbaca@gmail.com>
2012-10-31 02:29:00 +0000

    Reworked schematic example to implement fully semantic rotation.
    Done using new rotation indicator class.

--- experimental/specificationtools/test/schematic_examples/test_schematic_examples__X_series.py
+++ experimental/specificationtools/test/schematic_examples/test_schematic_examples__X_series.py
@@ -153,19 +153,13 @@ def test_schematic_examples__X_series_04():
     red_segment.set_rhythm(library.equal_divisions(8), selector=middle, contexts=['Voice 1'])
     red_segment.set_rhythm(library.equal_divisions(4), selector=right, contexts=['Voice 1'])
     voice_1_rhythm = red_segment.request_rhythm('Voice 1')
-
-    # TODO: implement rotation specifier
-    #indicator = sequencetools.RotationIndicator(-1, 1)
-    #red_segment.set_rhythm(voice_1_rhythm, contexts=['Voice 2'], rotation=indicator)
-    red_segment.set_rhythm(voice_1_rhythm, contexts=['Voice 2'], rotation=-16)
-
-    # TODO: implement rotation specifier
-    #red_segment.set_rhythm(voice_1_rhythm, contexts=['Voice 3'], rotation=(index=-2, level=-2))
-    red_segment.set_rhythm(voice_1_rhythm, contexts=['Voice 3'], rotation=-24)
+    indicator = sequencetools.RotationIndicator(-1, 1)
+    red_segment.set_rhythm(voice_1_rhythm, contexts=['Voice 2'], rotation=indicator)
+    indicator = sequencetools.RotationIndicator(-2, 1)
+    red_segment.set_rhythm(voice_1_rhythm, contexts=['Voice 3'], rotation=indicator)
     naive_beats = red_segment.request_naive_beats()
     red_segment.set_divisions(naive_beats, contexts=['Voice 4'])
     red_segment.set_rhythm(library.note_filled_tokens)
-
     blue_segment = score_specification.make_segment(name='blue')
     red_voice_1_rhythm = red_segment.request_rhythm('Voice 1')
     red_voice_2_rhythm = red_segment.request_rhythm('Voice 2')
