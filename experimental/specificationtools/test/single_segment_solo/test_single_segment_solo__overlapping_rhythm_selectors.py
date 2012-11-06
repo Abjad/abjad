406af1e7cc017e647aa238569c9628a22de4ca89
Trevor Bača <trevorbaca@gmail.com>
2012-11-06 17:17:05 +0000

    Added test to capture overlapping rhythm selectors working with material request.
    PDF included.

--- experimental/specificationtools/test/single_segment_solo/test_single_segment_solo__overlapping_rhythm_selectors.py
+++ experimental/specificationtools/test/single_segment_solo/test_single_segment_solo__overlapping_rhythm_selectors.py
@@ -75,3 +75,23 @@ def test_single_segment_solo__overlapping_rhythm_selectors_04():
     current_function_name = introspectiontools.get_current_function_name()
     helpertools.write_test_output(score, __file__, current_function_name)
     assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
+
+
+def test_single_segment_solo__overlapping_rhythm_selectors_05():
+    '''With rhythm material request.
+    '''
+
+    score_template = scoretemplatetools.GroupedRhythmicStavesScoreTemplate(staff_count=1)
+    score_specification = specificationtools.ScoreSpecification(score_template)
+    red_segment = score_specification.make_segment('red')
+    red_segment.set_time_signatures([(3, 8), (4, 8), (3, 8)])
+    red_segment.set_rhythm("{ c'4 }")
+    first_measure = red_segment.select_background_measure(0)
+    rhythmic_cell = score_specification.request_rhythm('Voice 1', selector=first_measure)
+    last_measure = red_segment.select_background_measure(-1)
+    red_segment.set_rhythm(rhythmic_cell, selector=last_measure)
+    score = score_specification.interpret()
+
+    current_function_name = introspectiontools.get_current_function_name()
+    helpertools.write_test_output(score, __file__, current_function_name)
+    assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
