c4fdc51150e3438907bd136a6f531aa6e17ec236
Trevor Bača <trevorbaca@gmail.com>
2012-11-10 00:07:58 +0000

    Updated time relation.

--- trunk/abjad/tools/timerelationtools/TimeInequality/TimeInequality.py
+++ trunk/abjad/tools/timerelationtools/TimeInequality/TimeInequality.py
@@ -2,6 +2,7 @@ import abc
 from abjad.tools.abctools.AbjadObject import AbjadObject
 
 
+# TODO: change name to TimeRelation
 class TimeInequality(AbjadObject):
     r'''.. versionadded:: 2.11
 
@@ -21,19 +22,25 @@ class TimeInequality(AbjadObject):
 
     ### PRIVATE METHODS ###
 
+    # TODO: maybe replace with self._get_expr_offsets()
     def _get_expr_start(self, expr, score_specification=None, context_name=None):
         if hasattr(expr, 'start_offset'):
             return expr.start_offset
-        elif hasattr(expr, 'get_score_start_offset'):
-            return expr.get_score_start_offset(score_specification, context_name)
+        #elif hasattr(expr, 'get_start_offset'):
+        #    return expr.get_start_offset(score_specification, context_name)
+        elif hasattr(expr, 'get_offsets'):
+            return expr.get_offsets(score_specification, context_name)[0]
         else:
             raise ValueError
 
+    # TODO: maybe replace with self._get_expr_offsets()
     def _get_expr_stop(self, expr, score_specification=None, context_name=None):
         if hasattr(expr, 'stop_offset'):
             return expr.stop_offset
-        elif hasattr(expr, 'get_score_stop_offset'):
-            return expr.get_score_stop_offset(score_specification, context_name)
+        #elif hasattr(expr, 'get_stop_offset'):
+        #    return expr.get_stop_offset(score_specification, context_name)
+        elif hasattr(expr, 'get_offsets'):
+            return expr.get_offsets(score_specification, context_name)[-1]
         else:
             raise ValueError
 
