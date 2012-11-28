468c3c3b28951925bdde45c56045868878e6e61c
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2012-11-28 22:32:31 +0000

    The abjad-book Sphinx extension now renders its images directly into the build directory.
    This means that no API images need ever be added to the repository.
    
    On the flipside, it makes building the docs slower.
    
    There may still be ways of bringing things back to speed, or avoiding unnecessary processing.

--- experimental/docs/source/_ext/abjadbook.py
+++ experimental/docs/source/_ext/abjadbook.py
@@ -1,18 +1,32 @@
 import docutils
 import os
+import posixpath
 import shutil
 import subprocess
 import tempfile
 from abjad.tools import documentationtools
-
+from sphinx.util.osutil import relative_uri
 
 # app.env.images is a dict:
 # {u'appendices/history/images/index-5.png': (set(['appendices/history/index']), u'index-5.png')}
 # etc.
 
+
 def add_image_block(literal_block, uri):
-    image = docutils.nodes.image(uri=uri)
+    image = docutils.nodes.image(
+        candidates={'*': uri},
+        uri=uri,
+        )
     literal_block.replace_self([literal_block, image])
+
+
+def builder_inited(app):
+    tmp_directory = os.path.abspath(tempfile.mkdtemp(dir=app.builder.outdir))
+    app.builder._abjadbook_tempdir = tmp_directory
+
+
+def build_finished(app, exc):
+    shutil.rmtree(app.builder._abjadbook_tempdir)
 
 
 def collect_literal_block_pairs(doctree):
@@ -32,9 +46,16 @@ def collect_literal_block_pairs(doctree):
     return None
 
 
-def process_doctree(app, doctree):
+def process_doctree(app, doctree, docname):
 
-    docname = doctree['source'].partition(app.srcdir)[-1][1:-4]
+    #print ''
+    #print docname
+    abs_imgpath = os.path.join(app.builder.outdir, '_images')
+    rel_imgpath = relative_uri(app.builder.get_target_uri(docname), '_images')
+    #print abs_imgpath
+    #print rel_imgpath
+
+    #docname = doctree['source'].partition(app.srcdir)[-1][1:-4]
     if not (app.config.abjadbook_should_process and \
         docname.startswith(app.config.abjadbook_transform_path)):
         return
@@ -48,44 +69,32 @@ def process_doctree(app, doctree):
     # setup
     docname_parts = docname.split('/')
     doctree_directory = os.path.join(*([app.srcdir] + docname_parts[:-1]))
-    png_directory = os.path.join(doctree_directory, 'images')
-    if not os.path.exists(png_directory):
-        os.mkdir(png_directory)
+    tmp_directory = app.builder._abjadbook_tempdir
     literal_block_images = {}
     image_prefix = docname_parts[-1]
     image_count = 0
-    tmp_directory = os.path.abspath(tempfile.mkdtemp(dir=doctree_directory))
+
+    assert os.path.exists(tmp_directory)
 
     # create tmp python file, create .ly files
-    f = open(os.path.join(tmp_directory, 'tmp.py'), 'w')
-    f.write('from abjad import *\n')
-    for literal_block, stripped_lines in pairs:
-        for line in stripped_lines:
-            if line.startswith('show('):
-                image_count += 1
-                file_name = '{}-{}'.format(image_prefix, image_count)
-                object_name = line.partition(')')[0][5:]
-                command = "iotools.write_expr_to_ly({}, {!r}, docs=True)".format(
-                    object_name, os.path.join(tmp_directory, file_name))
-                f.write(command + '\n')
-
-                # 
-                if literal_block not in literal_block_images:
-                    literal_block_images[literal_block] = []
-                literal_block_images[literal_block].append(file_name)
-
-                # 
-                #sphinx_image_name = file_name + '.png'
-                #sphinx_image_path = os.path.join(*(docname_parts[:-1] + [sphinx_image_name]))
-                #app.env.images[sphinx_image_path] = (
-                #    set((docname,)),
-                #    sphinx_image_name
-                #    )
-
-            else:
-                f.write(line + '\n')
-    f.close()
-    command = 'python {}'.format(os.path.join(tmp_directory, 'tmp.py'))
+    script_path = os.path.join(tmp_directory, 'tmp__{}.py'.format(image_prefix))
+    with open(script_path, 'w') as f:
+        f.write('from abjad import *\n')
+        for literal_block, stripped_lines in pairs:
+            for line in stripped_lines:
+                if line.startswith('show('):
+                    image_count += 1
+                    file_name = '{}-{}'.format(image_prefix, image_count)
+                    object_name = line.partition(')')[0][5:]
+                    command = "iotools.write_expr_to_ly({}, {!r}, docs=True)".format(
+                        object_name, os.path.join(tmp_directory, file_name))
+                    f.write(command + '\n')
+                    if literal_block not in literal_block_images:
+                        literal_block_images[literal_block] = []
+                    literal_block_images[literal_block].append(file_name)
+                else:
+                    f.write(line + '\n')
+    command = 'python {}'.format(script_path)
     subprocess.call(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     # run LilyPond on generated .ly files, move 
@@ -93,38 +102,24 @@ def process_doctree(app, doctree):
         for file_name in sorted(file_names):
             lilypond_file_name = os.path.join(tmp_directory, file_name + '.ly')
             tmp_png_file_name = os.path.join(tmp_directory, file_name + '.png')
-            final_png_file_name = os.path.join(png_directory, file_name + '.png')
-
-            # create png
+            final_png_file_name = os.path.join(abs_imgpath, file_name + '.png')
             command = 'lilypond --png -dresolution=300 -o {} {}'.format(
                 os.path.join(tmp_directory, file_name), lilypond_file_name)
-            subprocess.call(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-            # crop png
+            #subprocess.call(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            subprocess.call(command, shell=True)
             command = 'convert {} -trim -resample 40%% {}'.format(
                 tmp_png_file_name, tmp_png_file_name)
-            subprocess.call(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-            # only move to images/ if no original exists, or if it differs
-            if not os.path.exists(final_png_file_name) or \
-                not documentationtools.compare_images(tmp_png_file_name, final_png_file_name):
-                os.rename(tmp_png_file_name, final_png_file_name)
-                print 'OVERWRITING:', tmp_png_file_name, final_png_file_name
-
-            uri = '/'.join(docname_parts[:-1] + ['images', file_name + '.png'])
-            #print 'URI:', uri
-            #print 'FILENAME:', file_name
-            add_image_block(literal_block, 'images/{}.png'.format(file_name))
-        
-    #print 'DOCNAME:', docname
-    app.env.process_images(docname, doctree)
-
-    # cleanup
-    #shutil.rmtree(tmp_directory)
+            #subprocess.call(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            subprocess.call(command, shell=True)
+            os.rename(tmp_png_file_name, final_png_file_name)
+            uri = os.path.join(rel_imgpath, file_name + '.png')
+            add_image_block(literal_block, uri)
 
 
 def setup(app):
     app.add_config_value('abjadbook_should_process', False, 'env')
     app.add_config_value('abjadbook_transform_path', 'api/tools/', 'env')
-    app.connect('doctree-read', process_doctree)
+    app.connect('builder-inited', builder_inited)
+    app.connect('doctree-resolved', process_doctree)
+    app.connect('build-finished', build_finished)
 
