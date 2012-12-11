f20d5fba762db82de2cc1f5a827d7361f1e0ebbe
Trevor Bača <trevorbaca@gmail.com>
2012-12-11 21:15:21 +0000

    Added test file for BackroundMeasureSelector.partition_by_ratio_of_durations().
    PDF included.

--- experimental/symbolictimetools/BackgroundMeasureSelector/test/test_BackgroundMeasureSelector__partition_by_ratio.py
+++ experimental/symbolictimetools/BackgroundMeasureSelector/test/test_BackgroundMeasureSelector__partition_by_ratio.py
@@ -40,23 +40,3 @@ def test_BackgroundMeasureSelector__partition_by_ratio_02():
     current_function_name = introspectiontools.get_current_function_name()
     helpertools.write_test_output(score, __file__, current_function_name)
     assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
-
-
-def test_BackgroundMeasureSelector__partition_by_ratio_03():
-    '''Time ratio part.
-    '''
-
-    score_template = scoretemplatetools.GroupedRhythmicStavesScoreTemplate(staff_count=1)
-    score_specification = specificationtools.ScoreSpecification(score_template)
-    red_segment = score_specification.append_segment(name='red')
-    red_segment.set_time_signatures([(1, 8), (1, 8), (1, 8), (3, 8)])
-    measures = red_segment.select_background_measures()
-    last_measure = measures.partition_by_ratio_of_durations((1, 1))[-1]
-    red_segment.set_divisions([(2, 32)])
-    red_segment.set_divisions([(3, 32)], selector=last_measure)
-    red_segment.set_rhythm(library.thirty_seconds)
-    score = score_specification.interpret()
-
-    current_function_name = introspectiontools.get_current_function_name()
-    helpertools.write_test_output(score, __file__, current_function_name)
-    assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
