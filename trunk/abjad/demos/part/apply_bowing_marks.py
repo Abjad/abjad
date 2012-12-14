5f74eaaeabf3db5bfbcbd8a021d838a6316c1ad4
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2012-12-14 18:19:13 +0000

    Simplified Part Cantus demo, using the new markuptools.MusicGlyph class.

--- trunk/abjad/demos/part/apply_bowing_marks.py
+++ trunk/abjad/demos/part/apply_bowing_marks.py
@@ -17,24 +17,9 @@ def apply_bowing_marks(score):
     rebow_markup = markuptools.Markup(
         markuptools.MarkupCommand(
             'concat', [
-            markuptools.MarkupCommand(
-                'musicglyph',
-                schemetools.Scheme(
-                    'scripts.downbow',
-                    force_quotes=True,
-                    ),
-                ),
-            markuptools.MarkupCommand(
-                'hspace',
-                1,
-                ),
-            markuptools.MarkupCommand(
-                'musicglyph',
-                schemetools.Scheme(
-                    'scripts.upbow',
-                    force_quotes=True,
-                    ),
-                ),
+                markuptools.MusicGlyph('scripts.downbow'),
+                markuptools.MarkupCommand('hspace', 1),
+                markuptools.MusicGlyph('scripts.upbow'),
             ]))
     copy.copy(rebow_markup)(score['First Violin Voice'][64][0]) 
     copy.copy(rebow_markup)(score['Second Violin Voice'][75][0]) 
