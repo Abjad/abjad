e228ae3ae8e9b93dfcfc3f2c77e2733e94e6f762
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2012-12-24 21:27:17 +0000

    `ajv clean` can now remove Vim *.swp files.

--- trunk/abjad/tools/developerscripttools/CleanScript/CleanScript.py
+++ trunk/abjad/tools/developerscripttools/CleanScript/CleanScript.py
@@ -5,14 +5,14 @@ import shutil
 
 
 class CleanScript(DirectoryScript):
-    '''Remove *.pyc files and __pycache__ and tmp* directories recursively in a path:
+    '''Remove *.pyc, *.swp files and __pycache__ and tmp* directories recursively in a path:
 
     ::
 
         bash$ ajv clean -h
-        usage: clean [-h] [--version] [--pyc] [--pycache] [--tmp] path
+        usage: clean [-h] [--version] [--pyc] [--pycache] [--swp] [--tmp] path
 
-        Clean .pyc, __pycache__ and tmp* files and folders from PATH.
+        Clean *.pyc, *.swp, __pycache__ and tmp* files and folders from PATH.
 
         positional arguments:
           path        directory tree to be recursed over
@@ -20,9 +20,11 @@ class CleanScript(DirectoryScript):
         optional arguments:
           -h, --help  show this help message and exit
           --version   show program's version number and exit
-          --pyc       delete .pyc files
+          --pyc       delete *.pyc files
           --pycache   delete __pycache__ folders
+          --swp       delete Vim *.swp file
           --tmp       delete tmp* folders
+
 
     Return `CleanScript` instance.
     '''
@@ -43,7 +45,7 @@ class CleanScript(DirectoryScript):
 
     @property
     def short_description(self):
-        return 'Clean .pyc, __pycache__ and tmp* files and folders from PATH.'
+        return 'Clean *.pyc, *.swp, __pycache__ and tmp* files and folders from PATH.'
 
     @property
     def version(self):
@@ -52,29 +54,33 @@ class CleanScript(DirectoryScript):
     ### PUBLIC METHODS ###
 
     def process_args(self, args):
-        if not args.pyc and not args.pycache and not args.tmp:
-            args.pyc, args.pycache, args.tmp = True, True, True
+        if not args.pyc and not args.pycache and not args.swp and not args.tmp:
+            args.pyc, args.pycache, args.swp, args.tmp = True, True, True, True
 
         print 'Cleaning...'
         if args.pyc:
             print '\t*.pyc files'
+        if args.swp:
+            print '\t*.swp files'
         if args.pycache:
             print '\t__pycache__ directories'
         if args.tmp:
             print '\ttmp* directories'
 
         for root, dirs, files in os.walk(args.path):
-
             if '.svn' in dirs:
                 dirs.remove('.svn')
 
+            extensions = ()
             if args.pyc:
-                for file in files:
-                    if file.endswith('.pyc'):
-                        os.remove(os.path.join(root, file))
+                extensions += ('.pyc',)
+            if args.swp:
+                extensions += ('.swp',)
+            for file in files:
+                if file.endswith(extensions):
+                    os.remove(os.path.join(root, file))
 
-            to_remove = []
-
+            directories_to_remove = []
             predicate = lambda x: False
             if args.pycache and args.tmp:
                 predicate = lambda x: x == '__pycache__' or x.startswith('tmp')
@@ -82,13 +88,11 @@ class CleanScript(DirectoryScript):
                 predicate = lambda x: x == '__pycache__'
             elif args.tmp:
                 predicate = lambda x: x.startswith('tmp')
-
             for dir in dirs:
                 if predicate(dir):
                     shutil.rmtree(os.path.join(root, dir))
-                    to_remove.append(dir)
-                
-            for dir in to_remove:
+                    directories_to_remove.append(dir)
+            for dir in directories_to_remove:
                 dirs.remove(dir)
 
     def setup_argument_parser(self, parser):
@@ -100,12 +104,17 @@ class CleanScript(DirectoryScript):
 
         parser.add_argument('--pyc',
             action='store_true',
-            help='delete .pyc files',
+            help='delete *.pyc files',
             )
 
         parser.add_argument('--pycache',
             action='store_true',
             help='delete __pycache__ folders',
+            )
+
+        parser.add_argument('--swp',
+            action='store_true',
+            help='delete Vim *.swp file',
             )
 
         parser.add_argument('--tmp',
