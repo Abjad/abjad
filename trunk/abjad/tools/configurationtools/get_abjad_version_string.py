60ed191a62f6c83465debc048dde7f8a0387f0ff
Trevor Bača <trevorbaca@gmail.com>
2012-12-27 22:00:07 +0000

    Incremented __init__.py to 2.12 for current development cycle.

--- trunk/abjad/tools/configurationtools/get_abjad_version_string.py
+++ trunk/abjad/tools/configurationtools/get_abjad_version_string.py
@@ -4,7 +4,7 @@ def get_abjad_version_string():
     Get Abjad version string::
 
         >>> configurationtools.get_abjad_version_string()
-        '2.11'
+        '2.12'
 
     Return string.
     '''
