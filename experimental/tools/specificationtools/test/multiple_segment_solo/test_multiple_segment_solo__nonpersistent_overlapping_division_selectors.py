b7f0e1a0ac8c1166ef214b96c7afc63dd605d910
Trevor Bača <trevorbaca@gmail.com>
2012-12-29 03:13:05 +0000

    Changed SegmentSpecification.set_offsets() to SegmentSpecification.timespan.set_offsets().

--- experimental/tools/specificationtools/test/multiple_segment_solo/test_multiple_segment_solo__nonpersistent_overlapping_division_selectors.py
+++ experimental/tools/specificationtools/test/multiple_segment_solo/test_multiple_segment_solo__nonpersistent_overlapping_division_selectors.py
@@ -54,7 +54,7 @@ def test_multiple_segment_solo__nonpersistent_overlapping_division_selectors_03(
     red_segment = score_specification.append_segment(name='red')
     red_segment.set_time_signatures(4 * [(3, 16)])
     red_segment.set_divisions([(1, 16)], truncate=True)
-    middle_two_measures = red_segment.set_offsets((3, 16), (9, 16))
+    middle_two_measures = red_segment.timespan.set_offsets((3, 16), (9, 16))
     middle_two_measures.set_divisions([(2, 16)], persist=False)
     red_segment.set_rhythm(library.thirty_seconds)
     blue_segment = score_specification.append_segment(name='blue')
