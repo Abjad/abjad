68fb76c915c8d07d3ea829106673a77749f13d03
Trevor Bača <trevorbaca@gmail.com>
2012-12-30 21:09:50 +0000

    Streamlined BackgroundMeasureSelector.
    Cleaned up evaluation logic.

--- experimental/tools/selectortools/BackgroundMeasureSelector/BackgroundMeasureSelector.py
+++ experimental/tools/selectortools/BackgroundMeasureSelector/BackgroundMeasureSelector.py
@@ -65,33 +65,26 @@ class BackgroundMeasureSelector(Selector):
 
     ### PRIVATE METHODS ###
 
-    def _get_timespan(self, score_specification, context_name):
-        r'''Evaluate start and stop offsets when selector is applied
-        to `score_specification`.
-
-        Ignore `context_name`. 
-
-        Then apply any request_modifiers in selector request_modifiers stack.
-
-        Return pair.
-        '''
-        segment_specification = score_specification.get_start_segment_specification(self.anchor)
-        time_signatures = segment_specification.time_signatures[:]
+    def _get_time_signatures(self, score_specification, voice_name=None, start_offset=None, stop_offset=None):
+        start_segment_specification = score_specification.get_start_segment_specification(self)
+        time_signatures = start_segment_specification.time_signatures[:]
         time_signatures = [mathtools.NonreducedFraction(x) for x in time_signatures]
-        segment_name = segment_specification.segment_name
-        start_offset = score_specification[segment_name].timespan.start_offset
+        time_signatures, dummy = self._apply_request_modifiers(time_signatures, None)
+        return time_signatures
+
+    def _get_timespan(self, score_specification, context_name):
+        timespan, time_signatures = self._get_timespan_and_selected_objects(score_specification, context_name)
+        timespan = self._apply_timespan_modifiers(timespan)
+        return timespan
+
+    def _get_timespan_and_selected_objects(self, score_specification, context_name):
+        start_segment_specification = score_specification.get_start_segment_specification(self)
+        time_signatures = start_segment_specification.time_signatures[:]
+        time_signatures = [mathtools.NonreducedFraction(x) for x in time_signatures]
+        start_offset = start_segment_specification.timespan.start_offset
         time_signatures, start_offset = self._apply_request_modifiers(time_signatures, start_offset)
         durations = [durationtools.Duration(x) for x in time_signatures]
         duration = sum(durations)
         stop_offset = start_offset + duration
         timespan = timespantools.Timespan(start_offset, stop_offset)
-        timespan = self._apply_timespan_modifiers(timespan)
-        return timespan
-
-    def _get_time_signatures(self, score_specification, voice_name=None, start_offset=None, stop_offset=None):
-        start_segment_identifier = self.start_segment_identifier
-        segment_specification = score_specification.get_start_segment_specification(start_segment_identifier)
-        time_signatures = segment_specification.time_signatures[:]
-        time_signatures = [mathtools.NonreducedFraction(x) for x in time_signatures]
-        time_signatures, dummy = self._apply_request_modifiers(time_signatures, None)
-        return time_signatures
+        return timespan, time_signatures
