02ec6a9cf72661a686afac7a5e631f2cd0da49ff
Trevor Bača <trevorbaca@gmail.com>
2012-12-31 02:36:14 +0000

    Renamed test collateral.

--- experimental/tools/specificationtools/SegmentSpecification/test/test_SegmentSpecification__select_leaves.py
+++ experimental/tools/specificationtools/SegmentSpecification/test/test_SegmentSpecification__select_leaves.py
@@ -3,6 +3,8 @@ from experimental.tools import *
 
 
 def test_SegmentSpecification__select_leaves_01():
+    '''Select leaves composited on leaf selector.
+    '''
 
     score_template = scoretemplatetools.GroupedRhythmicStavesScoreTemplate(staff_count=2)
     score_specification = specificationtools.ScoreSpecification(score_template)
@@ -20,6 +22,8 @@ def test_SegmentSpecification__select_leaves_01():
 
 
 def test_SegmentSpecification__select_leaves_02():
+    '''Rotate leaf selector.
+    '''
 
     score_template = scoretemplatetools.GroupedRhythmicStavesScoreTemplate(staff_count=1)
     score_specification = specificationtools.ScoreSpecification(score_template)
@@ -60,6 +64,29 @@ def test_SegmentSpecification__select_leaves_03():
     timespan = red_segment.select_leaves('Voice 1')[4:8]
     voice_1_rhythm = timespan.select_leaves('Voice 1')
     blue_segment.set_rhythm(voice_1_rhythm, contexts=['Voice 1'])
+    score = score_specification.interpret()
+
+    current_function_name = introspectiontools.get_current_function_name()
+    helpertools.write_test_output(score, __file__, current_function_name)
+    assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
+
+
+def test_SegmentSpecification__select_leaves_04():
+    '''Select one division's leaves.
+    '''
+
+    score_template = scoretemplatetools.GroupedRhythmicStavesScoreTemplate(staff_count=2)
+    score_specification = specificationtools.ScoreSpecification(score_template)
+    red_segment = score_specification.append_segment(name='red')
+    red_segment.set_time_signatures([(3, 8), (3, 8)])
+    red_segment.set_divisions([(6, 32)], contexts=['Voice 1'])
+    maker = rhythmmakertools.TaleaRhythmMaker([1, 2, 3], 32)
+    maker.beam_cells_together = True
+    red_segment.set_rhythm(maker, contexts=['Voice 1'])
+    first_division = red_segment.select_divisions('Voice 1')[:1]
+    red_rhythm_cell = first_division.select_leaves('Voice 1')
+    red_rhythm_cell = red_rhythm_cell.reverse()
+    red_segment.set_rhythm(red_rhythm_cell, contexts=['Voice 2'])
     score = score_specification.interpret()
 
     current_function_name = introspectiontools.get_current_function_name()
