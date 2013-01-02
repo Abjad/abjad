7c3a954b75432817028cb95b283217cdee4d246b
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2013-01-02 16:42:26 +0000

    Implemented documentationtools.ReSTAutodocDirective.

--- trunk/abjad/tools/documentationtools/ReSTAutodocDirective/ReSTAutodocDirective.py
+++ trunk/abjad/tools/documentationtools/ReSTAutodocDirective/ReSTAutodocDirective.py
@@ -1,12 +1,43 @@
+import types
 from abjad.tools.documentationtools.ReSTDirective import ReSTDirective
 
 
 class ReSTAutodocDirective(ReSTDirective):
+    '''An ReST autodoc directive:
+
+    ::
+
+        >>> autodoc = documentationtools.ReSTAutodocDirective(
+        ...     argument=beamtools.BeamSpanner,
+        ...     directive='autoclass',
+        ...     )
+        >>> autodoc.options['noindex'] = True
+        >>> autodoc
+        ReSTAutodocDirective(
+            argument='abjad.tools.beamtools.BeamSpanner.BeamSpanner.BeamSpanner',
+            directive='autoclass',
+            options={
+                'noindex': True
+                }
+            )
+
+    ::
+
+        >>> print autodoc.rest_format
+        .. autoclass:: abjad.tools.beamtools.BeamSpanner.BeamSpanner.BeamSpanner
+           :noindex:
+
+    Return `ReSTAutodocDirective` instance.
+    '''
 
     ### INITIALIZER ###
 
-    def __init__(self, children=None, directive=None, name=None, options=None):
-        ReSTDirective.__init__(self, children=children, name=name, options=options)
+    def __init__(self, argument=None, children=None, directive=None, name=None, options=None):
+        if isinstance(argument, types.InstanceType):
+            argument = argument.__class__
+        if isinstance(argument, types.TypeType):
+            argument=argument.__module__ + '.' + argument.__name__
+        ReSTDirective.__init__(self, argument=argument, children=children, name=name, options=options)
         self.directive = directive
 
     ### READ/WRITE PUBLIC PROPERTIES ###
@@ -14,7 +45,7 @@ class ReSTAutodocDirective(ReSTDirective):
     @apply
     def directive():
         def fget(self):
-            return self.directive
+            return self._directive
         def fset(self, arg):
             assert arg in (
                 'autoattribute',
