b0ce4308248f7dbe65692aed1fd4abe13e05db03
Trevor Bača <trevorbaca@gmail.com>
2013-01-06 23:44:49 +0000

    Added missing experimental tools packages to experimental/tools/__init__.py file.
    The missing packages were these:
    
        breakpointtools
        parsertools
        selectortools

--- experimental/tools/__init__.py
+++ experimental/tools/__init__.py
@@ -1,5 +1,6 @@
 from abjad import *
 
+import breakpointtools
 import constrainttools
 import handlertools
 import helpertools
@@ -7,7 +8,9 @@ import interpretertools
 import library
 import lyrictools
 import metricmodulationtools
+import parsertools
 import requesttools
+import selectortools
 import settingtools
 import specificationtools
 import timeexpressiontools
