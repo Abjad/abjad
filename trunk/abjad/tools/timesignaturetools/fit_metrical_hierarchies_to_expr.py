ecfb1f280b5c0fbb180ee0d5a511f88d912caf48
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2013-01-08 04:25:53 +0000

    Implemented the first draft of timesignaturetools.fit_metrical_hierarchies_to_expr():
    Commit includes two trivial examples:
    
    Example 1. Matching a series of hypothetical 4/4 measures:
    
    ::
    
        >>> expr = [(0, 4), (4, 4), (8, 4), (12, 4), (16, 4)]
        >>> for x in timesignaturetools.fit_metrical_hierarchies_to_expr(
        ...     expr, metrical_hierarchies):
        ...     print x.implied_time_signature
        ...
        4/4
        4/4
        4/4
        4/4
    
    Example 2. Matching a series of hypothetical 5/4 measures:
    
    ::
    
        >>> expr = [(0, 4), (3, 4), (5, 4), (10, 4), (15, 4), (20, 4)]
        >>> for x in timesignaturetools.fit_metrical_hierarchies_to_expr(
        ...     expr, metrical_hierarchies):
        ...     print x.implied_time_signature
        ...
        5/4
        5/4
        5/4
        5/4

--- trunk/abjad/tools/timesignaturetools/fit_metrical_hierarchies_to_expr.py
+++ trunk/abjad/tools/timesignaturetools/fit_metrical_hierarchies_to_expr.py
@@ -1,12 +1,56 @@
 from abjad.tools import durationtools
 
 
-def fit_metrical_hierarchies_to_expr(expr, metrical_hierarchies, starting_offset=None, depth=32):
+def fit_metrical_hierarchies_to_expr(expr, metrical_hierarchies,
+    discard_final_orphan_downbeat=True, starting_offset=None, denominator=32):
+    r'''Find the best-matching sequence of metrical hierarchies for the offsets
+    contained in `expr`.
+
+    ::
+
+        >>> metrical_hierarchies = timesignaturetools.MetricalHierarchyInventory(
+        ...     [(3, 4), (4, 4), (5, 4)])
+
+    Example 1. Matching a series of hypothetical 4/4 measures:
+
+    ::
+
+        >>> expr = [(0, 4), (4, 4), (8, 4), (12, 4), (16, 4)]
+        >>> for x in timesignaturetools.fit_metrical_hierarchies_to_expr(
+        ...     expr, metrical_hierarchies):
+        ...     print x.implied_time_signature
+        ...
+        4/4
+        4/4
+        4/4
+        4/4
+
+    Example 2. Matching a series of hypothetical 5/4 measures:
+
+    ::
+
+        >>> expr = [(0, 4), (3, 4), (5, 4), (10, 4), (15, 4), (20, 4)]
+        >>> for x in timesignaturetools.fit_metrical_hierarchies_to_expr(
+        ...     expr, metrical_hierarchies):
+        ...     print x.implied_time_signature
+        ...
+        5/4
+        5/4
+        5/4
+        5/4
+
+    Offsets are coerced from `expr` via `durationtools.count_offsets_in_expr()`.
+
+    MetricalHierarchies are coerced from `metrical_hierarchies` via `MetricalHierarchyInventory`.
+
+    Return list.
+    '''
     from abjad.tools import timesignaturetools
 
     offset_counter = durationtools.count_offsets_in_expr(expr)
     ordered_offsets = sorted(offset_counter, reverse=True)
-    assert len(offset_count)
+    if not ordered_offsets:
+        return []
 
     if starting_offset is None:
         start_offset = durationtools.Offset(0)
@@ -14,25 +58,21 @@ def fit_metrical_hierarchies_to_expr(expr, metrical_hierarchies, starting_offset
         start_offset = durationtools.Offset(starting_offset)
 
     metrical_hierarchy_inventory = timesignaturetools.MetricalHierarchyInventory(metrical_hierarchies)
-    longest_hierarchy = sorted(metrical_hierarchy_inventory, lambda x: x.duration)[0]
+    longest_hierarchy = sorted(metrical_hierarchy_inventory, key=lambda x: x.duration, reverse=True)[0]
     longest_kernel_duration = max(x.duration for x in metrical_hierarchy_inventory)
-    kernels = [x.generate_offset_kernel_to_depth(depth=depth) for x in metrical_hierarchy_inventory]
+    kernels = [x.generate_offset_kernel_to_denominator(denominator) for x in metrical_hierarchy_inventory]
     
     current_start_offset = start_offset
     selected_hierarchies = []
-    while current_start_offset < ordered_offsets[-1]:            
-        # PROCEDURE:
-        # pop offsets less than current_start_offset
-        # then collect all offsets such that current_start_offset <= x <= current_stop_offset
-        # if no offsets, make a best guess selection (how?) and continue to loop
-        # otherwise, shift the collected offsets, taking current_start_offset as the origin
-        # compare each kernel against the shifted offsets, caching the response as a pair
-        # then sort the kernels by their responses, breaking ties somehow (fewest number of nodes?)
-        # append the winner to selected_hierarchies
-        # add the duration of the winner to current_start_offset
-        # continue to loop
-        while len(ordered_offsets) and ordered_offsets[-1] < start_offset:
-            ordered_offsets.pop()
+
+    while len(ordered_offsets) and ordered_offsets[-1] < current_start_offset:
+        ordered_offsets.pop()
+
+    while len(ordered_offsets) and current_start_offset <= ordered_offsets[-1]:
+        if len(ordered_offsets) == 1:
+            if discard_final_orphan_downbeat:
+                if ordered_offsets[0] == current_start_offset:
+                    break
         current_stop_offset = current_start_offset + longest_kernel_duration
         current_offset_counter = {}
         for offset in reversed(ordered_offsets):
@@ -43,14 +83,16 @@ def fit_metrical_hierarchies_to_expr(expr, metrical_hierarchies, starting_offset
         if not current_offset_counter:
             winner = longest_hierarchy
         else: 
-            # this is where we do the comparisons
             candidates = []
             for i, kernel in enumerate(kernels):
                 response = kernel(current_offset_counter)
                 candidates.append((response, i))
-            candidates.sort(key=lambda x: x[0], reversed=True)
-            winner = metrical_hierarchy_inventory[candidates[0]]
+            candidates.sort(key=lambda x: x[0], reverse=True)
+            response, index = candidates[0]
+            winner = metrical_hierarchy_inventory[index]
         selected_hierarchies.append(winner)
         current_start_offset += winner.duration 
+        while len(ordered_offsets) and ordered_offsets[-1] < current_start_offset:
+            ordered_offsets.pop()
 
     return selected_hierarchies
