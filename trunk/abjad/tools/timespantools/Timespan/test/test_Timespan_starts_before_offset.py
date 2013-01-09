c80431e8c642e618b6483964638f944b9d9d4a7e
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2013-01-09 17:31:36 +0000

    Added tests for Timespan vs Offset comparison predicates.

---
+++ trunk/abjad/tools/timespantools/Timespan/test/test_Timespan_starts_before_offset.py
@@ -0,0 +1,29 @@
+from abjad import *
+
+
+def test_Timespan_starts_before_offset_01():
+    timespan = timespantools.Timespan(0, 10)
+    offset = durationtools.Offset(-5)
+    assert not timespan.starts_before_offset(offset)
+
+def test_Timespan_starts_before_offset_02():
+    timespan = timespantools.Timespan(0, 10)
+    offset = durationtools.Offset(0)
+    assert not timespan.starts_before_offset(offset)
+
+def test_Timespan_starts_before_offset_03():
+    timespan = timespantools.Timespan(0, 10)
+    offset = durationtools.Offset(5)
+    assert timespan.starts_before_offset(offset)
+
+def test_Timespan_starts_before_offset_04():
+    timespan = timespantools.Timespan(0, 10)
+    offset = durationtools.Offset(10)
+    assert timespan.starts_before_offset(offset)
+
+def test_Timespan_starts_before_offset_05():
+    timespan = timespantools.Timespan(0, 10)
+    offset = durationtools.Offset(15)
+    assert timespan.starts_before_offset(offset)
+
+
---
+++ trunk/abjad/tools/timespantools/Timespan/test/test_Timespan_stops_at_or_after_offset.py
@@ -0,0 +1,29 @@
+from abjad import *
+
+
+def test_Timespan_stops_at_or_after_offset_01():
+    timespan = timespantools.Timespan(0, 10)
+    offset = durationtools.Offset(-5)
+    assert timespan.stops_at_or_after_offset(offset)
+
+def test_Timespan_stops_at_or_after_offset_02():
+    timespan = timespantools.Timespan(0, 10)
+    offset = durationtools.Offset(0)
+    assert timespan.stops_at_or_after_offset(offset)
+
+def test_Timespan_stops_at_or_after_offset_03():
+    timespan = timespantools.Timespan(0, 10)
+    offset = durationtools.Offset(5)
+    assert timespan.stops_at_or_after_offset(offset)
+
+def test_Timespan_stops_at_or_after_offset_04():
+    timespan = timespantools.Timespan(0, 10)
+    offset = durationtools.Offset(10)
+    assert timespan.stops_at_or_after_offset(offset)
+
+def test_Timespan_stops_at_or_after_offset_05():
+    timespan = timespantools.Timespan(0, 10)
+    offset = durationtools.Offset(15)
+    assert not timespan.stops_at_or_after_offset(offset)
+
+
