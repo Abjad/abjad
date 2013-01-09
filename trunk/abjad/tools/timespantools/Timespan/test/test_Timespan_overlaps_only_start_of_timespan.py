9c2a94be831a25461d9dbce63692257220113550
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2013-01-09 18:08:21 +0000

    Added tests for all Timespan.overlaps_*() predicates.

---
+++ trunk/abjad/tools/timespantools/Timespan/test/test_Timespan_overlaps_only_start_of_timespan.py
@@ -0,0 +1,69 @@
+from abjad import *
+
+
+def test_Timespan_overlaps_only_start_of_timespan_01():
+    a = timespantools.Timespan(0, 15)
+    b = timespantools.Timespan(-10, -5)
+    assert not a.overlaps_only_start_of_timespan(b)
+
+def test_Timespan_overlaps_only_start_of_timespan_02():
+    a = timespantools.Timespan(0, 15)
+    b = timespantools.Timespan(-10, 0)
+    assert not a.overlaps_only_start_of_timespan(b)
+
+def test_Timespan_overlaps_only_start_of_timespan_03():
+    a = timespantools.Timespan(0, 15)
+    b = timespantools.Timespan(-10, 5)
+    assert not a.overlaps_only_start_of_timespan(b)
+
+def test_Timespan_overlaps_only_start_of_timespan_04():
+    a = timespantools.Timespan(0, 15)
+    b = timespantools.Timespan(-10, 15)
+    assert not a.overlaps_only_start_of_timespan(b)
+
+def test_Timespan_overlaps_only_start_of_timespan_05():
+    a = timespantools.Timespan(0, 15)
+    b = timespantools.Timespan(-10, 25)
+    assert not a.overlaps_only_start_of_timespan(b)
+
+def test_Timespan_overlaps_only_start_of_timespan_06():
+    a = timespantools.Timespan(0, 15)
+    b = timespantools.Timespan(0, 10)
+    assert not a.overlaps_only_start_of_timespan(b)
+
+def test_Timespan_overlaps_only_start_of_timespan_07():
+    a = timespantools.Timespan(0, 15)
+    b = timespantools.Timespan(0, 15)
+    assert not a.overlaps_only_start_of_timespan(b)
+
+def test_Timespan_overlaps_only_start_of_timespan_08():
+    a = timespantools.Timespan(0, 15)
+    b = timespantools.Timespan(5, 10)
+    assert not a.overlaps_only_start_of_timespan(b)
+
+def test_Timespan_overlaps_only_start_of_timespan_09():
+    a = timespantools.Timespan(0, 15)
+    b = timespantools.Timespan(5, 15)
+    assert a.overlaps_only_start_of_timespan(b)
+
+def test_Timespan_overlaps_only_start_of_timespan_10():
+    a = timespantools.Timespan(0, 15)
+    b = timespantools.Timespan(0, 25)
+    assert not a.overlaps_only_start_of_timespan(b)
+
+def test_Timespan_overlaps_only_start_of_timespan_11():
+    a = timespantools.Timespan(0, 15)
+    b = timespantools.Timespan(5, 25)
+    assert a.overlaps_only_start_of_timespan(b)
+
+def test_Timespan_overlaps_only_start_of_timespan_12():
+    a = timespantools.Timespan(0, 15)
+    b = timespantools.Timespan(15, 25)
+    assert not a.overlaps_only_start_of_timespan(b)
+
+def test_Timespan_overlaps_only_start_of_timespan_13():
+    a = timespantools.Timespan(0, 15)
+    b = timespantools.Timespan(20, 25)
+    assert not a.overlaps_only_start_of_timespan(b)
+
+
