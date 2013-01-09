6dff9dca9bb586f9e546460db8f9d49a489cdd1d
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2013-01-09 17:54:05 +0000

    Corrected one time relation predicate.

--- trunk/abjad/tools/timerelationtools/timespan_2_stops_after_timespan_1_starts.py
+++ trunk/abjad/tools/timerelationtools/timespan_2_stops_after_timespan_1_starts.py
@@ -4,14 +4,14 @@ def timespan_2_stops_after_timespan_1_starts(timespan_1=None, timespan_2=None, h
     Make time relation template indicating that expression stops after timespan starts::
 
         >>> timerelationtools.timespan_2_stops_after_timespan_1_starts()
-        TimespanTimespanTimeRelation('timespan_1.stop <= timespan_2.start')
+        TimespanTimespanTimeRelation('timespan_1.start < timespan_2.stop')
 
     Return boolean or time relation.
     '''
     from abjad.tools import timerelationtools
 
     time_relation = timerelationtools.TimespanTimespanTimeRelation(
-        'timespan_1.stop <= timespan_2.start',
+        'timespan_1.start < timespan_2.stop',
         timespan_1=timespan_1,
         timespan_2=timespan_2)
 
