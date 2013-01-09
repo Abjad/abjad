bc5fbe0f4b45c901fcb574370dfa8178a73d59cb
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2013-01-09 18:15:41 +0000

    Added tests for two more Timespan predicates.

---
+++ trunk/abjad/tools/timespantools/Timespan/test/test_Timespan_happens_during_timespan.py
@@ -0,0 +1,69 @@
+from abjad import *
+
+
+def test_Timespan_happens_during_timespan_01():
+    a = timespantools.Timespan(0, 15)
+    b = timespantools.Timespan(-10, -5)
+    assert not a.happens_during_timespan(b)
+
+def test_Timespan_happens_during_timespan_02():
+    a = timespantools.Timespan(0, 15)
+    b = timespantools.Timespan(-10, 0)
+    assert not a.happens_during_timespan(b)
+
+def test_Timespan_happens_during_timespan_03():
+    a = timespantools.Timespan(0, 15)
+    b = timespantools.Timespan(-10, 5)
+    assert not a.happens_during_timespan(b)
+
+def test_Timespan_happens_during_timespan_04():
+    a = timespantools.Timespan(0, 15)
+    b = timespantools.Timespan(-10, 15)
+    assert a.happens_during_timespan(b)
+
+def test_Timespan_happens_during_timespan_05():
+    a = timespantools.Timespan(0, 15)
+    b = timespantools.Timespan(-10, 25)
+    assert a.happens_during_timespan(b)
+
+def test_Timespan_happens_during_timespan_06():
+    a = timespantools.Timespan(0, 15)
+    b = timespantools.Timespan(0, 10)
+    assert not a.happens_during_timespan(b)
+
+def test_Timespan_happens_during_timespan_07():
+    a = timespantools.Timespan(0, 15)
+    b = timespantools.Timespan(0, 15)
+    assert a.happens_during_timespan(b)
+
+def test_Timespan_happens_during_timespan_08():
+    a = timespantools.Timespan(0, 15)
+    b = timespantools.Timespan(5, 10)
+    assert not a.happens_during_timespan(b)
+
+def test_Timespan_happens_during_timespan_09():
+    a = timespantools.Timespan(0, 15)
+    b = timespantools.Timespan(5, 15)
+    assert not a.happens_during_timespan(b)
+
+def test_Timespan_happens_during_timespan_10():
+    a = timespantools.Timespan(0, 15)
+    b = timespantools.Timespan(0, 25)
+    assert a.happens_during_timespan(b)
+
+def test_Timespan_happens_during_timespan_11():
+    a = timespantools.Timespan(0, 15)
+    b = timespantools.Timespan(5, 25)
+    assert not a.happens_during_timespan(b)
+
+def test_Timespan_happens_during_timespan_12():
+    a = timespantools.Timespan(0, 15)
+    b = timespantools.Timespan(15, 25)
+    assert not a.happens_during_timespan(b)
+
+def test_Timespan_happens_during_timespan_13():
+    a = timespantools.Timespan(0, 15)
+    b = timespantools.Timespan(20, 25)
+    assert not a.happens_during_timespan(b)
+
+
