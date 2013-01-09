83dc71be7b6bd1a34ead11af3359760651c7f217
Trevor Bača <trevorbaca@gmail.com>
2013-01-09 14:01:46 +0000

    Corrected definition of timerelationtools.timespan_2_overlaps_only_start_of_timespan_1() function.

--- trunk/abjad/tools/timerelationtools/timespan_2_overlaps_only_start_of_timespan_1.py
+++ trunk/abjad/tools/timerelationtools/timespan_2_overlaps_only_start_of_timespan_1.py
@@ -4,14 +4,14 @@ def timespan_2_overlaps_only_start_of_timespan_1(timespan_1=None, timespan_2=Non
     Make time relation template indicating that expression happens during timespan::
 
         >>> timerelationtools.timespan_2_overlaps_only_start_of_timespan_1()
-        TimespanTimespanTimeRelation('timespan_2.start < timespan_1.start < timespan_2.start <= timespan_1.stop')
+        TimespanTimespanTimeRelation('timespan_2.start < timespan_1.stop < timespan_2.start <= timespan_1.stop')
 
     Return boolean or time relation.
     '''
     from abjad.tools import timerelationtools
 
     time_relation = timerelationtools.TimespanTimespanTimeRelation(
-        'timespan_2.start < timespan_1.start < timespan_2.start <= timespan_1.stop',
+        'timespan_2.start < timespan_1.start < timespan_2.stop <= timespan_1.stop',
         timespan_1=timespan_1, 
         timespan_2=timespan_2)
     
