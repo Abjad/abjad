e3aeab956bee05327b4ce71d7c42becb3baf5242
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2013-01-09 17:54:56 +0000

    Added tests for all *starts* and *stops* Timespan vs Timespan comparisons.

---
+++ trunk/abjad/tools/timespantools/Timespan/test/test_Timespan_starts_after_timespan_starts.py
@@ -0,0 +1,69 @@
+from abjad import *
+
+
+def test_Timespan_starts_after_timespan_starts_01():
+    a = timespantools.Timespan(0, 15)
+    b = timespantools.Timespan(-10, -5)
+    assert a.starts_after_timespan_starts(b)
+
+def test_Timespan_starts_after_timespan_starts_02():
+    a = timespantools.Timespan(0, 15)
+    b = timespantools.Timespan(-10, 0)
+    assert a.starts_after_timespan_starts(b)
+
+def test_Timespan_starts_after_timespan_starts_03():
+    a = timespantools.Timespan(0, 15)
+    b = timespantools.Timespan(-10, 5)
+    assert a.starts_after_timespan_starts(b)
+
+def test_Timespan_starts_after_timespan_starts_04():
+    a = timespantools.Timespan(0, 15)
+    b = timespantools.Timespan(-10, 15)
+    assert a.starts_after_timespan_starts(b)
+
+def test_Timespan_starts_after_timespan_starts_05():
+    a = timespantools.Timespan(0, 15)
+    b = timespantools.Timespan(-10, 25)
+    assert a.starts_after_timespan_starts(b)
+
+def test_Timespan_starts_after_timespan_starts_06():
+    a = timespantools.Timespan(0, 15)
+    b = timespantools.Timespan(0, 10)
+    assert not a.starts_after_timespan_starts(b)
+
+def test_Timespan_starts_after_timespan_starts_07():
+    a = timespantools.Timespan(0, 15)
+    b = timespantools.Timespan(0, 15)
+    assert not a.starts_after_timespan_starts(b)
+
+def test_Timespan_starts_after_timespan_starts_08():
+    a = timespantools.Timespan(0, 15)
+    b = timespantools.Timespan(5, 10)
+    assert not a.starts_after_timespan_starts(b)
+
+def test_Timespan_starts_after_timespan_starts_09():
+    a = timespantools.Timespan(0, 15)
+    b = timespantools.Timespan(5, 15)
+    assert not a.starts_after_timespan_starts(b)
+
+def test_Timespan_starts_after_timespan_starts_10():
+    a = timespantools.Timespan(0, 15)
+    b = timespantools.Timespan(0, 25)
+    assert not a.starts_after_timespan_starts(b)
+
+def test_Timespan_starts_after_timespan_starts_11():
+    a = timespantools.Timespan(0, 15)
+    b = timespantools.Timespan(5, 25)
+    assert not a.starts_after_timespan_starts(b)
+
+def test_Timespan_starts_after_timespan_starts_12():
+    a = timespantools.Timespan(0, 15)
+    b = timespantools.Timespan(15, 25)
+    assert not a.starts_after_timespan_starts(b)
+
+def test_Timespan_starts_after_timespan_starts_13():
+    a = timespantools.Timespan(0, 15)
+    b = timespantools.Timespan(20, 25)
+    assert not a.starts_after_timespan_starts(b)
+
+
---
+++ trunk/abjad/tools/timespantools/Timespan/test/test_Timespan_starts_after_timespan_stops.py
@@ -0,0 +1,69 @@
+from abjad import *
+
+
+def test_Timespan_starts_after_timespan_stops_01():
+    a = timespantools.Timespan(0, 15)
+    b = timespantools.Timespan(-10, -5)
+    assert a.starts_after_timespan_stops(b)
+
+def test_Timespan_starts_after_timespan_stops_02():
+    a = timespantools.Timespan(0, 15)
+    b = timespantools.Timespan(-10, 0)
+    assert a.starts_after_timespan_stops(b)
+
+def test_Timespan_starts_after_timespan_stops_03():
+    a = timespantools.Timespan(0, 15)
+    b = timespantools.Timespan(-10, 5)
+    assert not a.starts_after_timespan_stops(b)
+
+def test_Timespan_starts_after_timespan_stops_04():
+    a = timespantools.Timespan(0, 15)
+    b = timespantools.Timespan(-10, 15)
+    assert not a.starts_after_timespan_stops(b)
+
+def test_Timespan_starts_after_timespan_stops_05():
+    a = timespantools.Timespan(0, 15)
+    b = timespantools.Timespan(-10, 25)
+    assert not a.starts_after_timespan_stops(b)
+
+def test_Timespan_starts_after_timespan_stops_06():
+    a = timespantools.Timespan(0, 15)
+    b = timespantools.Timespan(0, 10)
+    assert not a.starts_after_timespan_stops(b)
+
+def test_Timespan_starts_after_timespan_stops_07():
+    a = timespantools.Timespan(0, 15)
+    b = timespantools.Timespan(0, 15)
+    assert not a.starts_after_timespan_stops(b)
+
+def test_Timespan_starts_after_timespan_stops_08():
+    a = timespantools.Timespan(0, 15)
+    b = timespantools.Timespan(5, 10)
+    assert not a.starts_after_timespan_stops(b)
+
+def test_Timespan_starts_after_timespan_stops_09():
+    a = timespantools.Timespan(0, 15)
+    b = timespantools.Timespan(5, 15)
+    assert not a.starts_after_timespan_stops(b)
+
+def test_Timespan_starts_after_timespan_stops_10():
+    a = timespantools.Timespan(0, 15)
+    b = timespantools.Timespan(0, 25)
+    assert not a.starts_after_timespan_stops(b)
+
+def test_Timespan_starts_after_timespan_stops_11():
+    a = timespantools.Timespan(0, 15)
+    b = timespantools.Timespan(5, 25)
+    assert not a.starts_after_timespan_stops(b)
+
+def test_Timespan_starts_after_timespan_stops_12():
+    a = timespantools.Timespan(0, 15)
+    b = timespantools.Timespan(15, 25)
+    assert not a.starts_after_timespan_stops(b)
+
+def test_Timespan_starts_after_timespan_stops_13():
+    a = timespantools.Timespan(0, 15)
+    b = timespantools.Timespan(20, 25)
+    assert not a.starts_after_timespan_stops(b)
+
+
