1bd6f4bdb63b5d6dc9cc6576b4f440af3c4a3e2a
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2013-01-11 01:57:46 +0000

    Reimplemented RegionProduct.trim_to_timespan() on top of RegionProduct.__and__().

--- experimental/tools/settingtools/RegionProduct/RegionProduct.py
+++ experimental/tools/settingtools/RegionProduct/RegionProduct.py
@@ -179,11 +179,13 @@ class RegionProduct(AbjadObject):
     # TODO: implement Timespan.trim_to_timespan(); or not
     # TODO: write tests
     def trim_to_timespan(self, timespan):
-        result = self
-        if timespan.start_offset is not None:
-            knife = timespantools.Timespan(self.start_offset, timespan.start_offset)
-            result = self - knife
-        if timespan.stop_offset is not None:
-            knife = timespantools.Timespan(timespan.stop_offset, self.stop_offset)
-            result = result - knife
-        return result
+        return self & timespan
+        #result = self
+        #if timespan.start_offset is not None:
+        #    knife = timespantools.Timespan(self.start_offset, timespan.start_offset)
+        #    result = self - knife
+        #if timespan.stop_offset is not None:
+        #    knife = timespantools.Timespan(timespan.stop_offset, self.stop_offset)
+        #    result = result - knife
+        #return result
+
