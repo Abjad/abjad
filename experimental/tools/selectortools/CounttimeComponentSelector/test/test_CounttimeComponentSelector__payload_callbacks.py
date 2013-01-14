82e5d16ac97515c8f9bad40f8183d903a414c980
Trevor Bača <trevorbaca@gmail.com>
2013-01-14 16:51:50 +0000

    Implement CounttimeComponentSelector.rotate() method.
    Example: rotate by count. This now works:
    
        red_segment.set_rhythm("{ c'16 [ c'8 c'8. ] }")
        red_leaves = red_segment.select_leaves('Voice 1')
        red_leaves = red_leaves.rotate(-1)
        blue_segment.set_rhythm(red_leaves)
    
    
    Example: rotate by duration This now works, too:
    
        red_segment.set_rhythm("{ c'16 [ c'8 c'8. ] }")
        red_leaves = red_segment.select_leaves('Voice 1')
        red_leaves = red_leaves.rotate(-Duration(1, 32))
        blue_segment.set_rhythm(red_leaves)
    
    
    Tests and PDFs included.

--- experimental/tools/selectortools/CounttimeComponentSelector/test/test_CounttimeComponentSelector__payload_callbacks.py
+++ experimental/tools/selectortools/CounttimeComponentSelector/test/test_CounttimeComponentSelector__payload_callbacks.py
@@ -132,7 +132,6 @@ def test_CounttimeComponentSelector__payload_callbacks_06():
 def test_CounttimeComponentSelector__payload_callbacks_07():
     '''Rotate rhythm by count.
     '''
-    py.test.skip('working on this one.')
 
     score_template = scoretemplatetools.GroupedRhythmicStavesScoreTemplate(staff_count=1)
     score_specification = specificationtools.ScoreSpecification(score_template)
@@ -146,5 +145,25 @@ def test_CounttimeComponentSelector__payload_callbacks_07():
     score = score_specification.interpret()
 
     current_function_name = introspectiontools.get_current_function_name()
-    helpertools.write_test_output(score, __file__, current_function_name, render_pdf=True)
-    #assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
+    helpertools.write_test_output(score, __file__, current_function_name)
+    assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
+
+
+def test_CounttimeComponentSelector__payload_callbacks_08():
+    '''Rotate rhythm by duration.
+    '''
+
+    score_template = scoretemplatetools.GroupedRhythmicStavesScoreTemplate(staff_count=1)
+    score_specification = specificationtools.ScoreSpecification(score_template)
+    red_segment = score_specification.append_segment(name='red')
+    red_segment.set_time_signatures([(2, 4), (3, 8), (3, 4)])
+    red_segment.set_rhythm("{ c'16 [ c'8 c'8. ] }")
+    red_leaves = red_segment.select_leaves('Voice 1')
+    red_leaves = red_leaves.rotate(-Duration(1, 32))
+    blue_segment = score_specification.append_segment(name='blue')
+    blue_segment.set_rhythm(red_leaves)
+    score = score_specification.interpret()
+
+    current_function_name = introspectiontools.get_current_function_name()
+    helpertools.write_test_output(score, __file__, current_function_name)
+    assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
---
+++ experimental/tools/selectortools/CounttimeComponentSelector/test/test_CounttimeComponentSelector__payload_callbacks_07.ly
@@ -0,0 +1,84 @@
+\context Score = "Grouped Rhythmic Staves Score" <<
+	\context TimeSignatureContext = "TimeSignatureContext" {
+		{
+			\time 2/4
+			s1 * 1/2
+		}
+		{
+			\time 3/8
+			s1 * 3/8
+		}
+		{
+			\time 3/4
+			s1 * 3/4
+		}
+		{
+			\time 2/4
+			s1 * 1/2
+		}
+		{
+			\time 3/8
+			s1 * 3/8
+		}
+		{
+			\time 3/4
+			s1 * 3/4
+		}
+	}
+	\context StaffGroup = "Grouped Rhythmic Staves Staff Group" <<
+		\context RhythmicStaff = "Staff 1" {
+			\context Voice = "Voice 1" {
+				{
+					c'16 [
+					c'8
+					c'8. ]
+				}
+				{
+					c'16 [
+					c'8
+					c'8. ]
+				}
+				{
+					c'16 [
+					c'8
+					c'8. ]
+				}
+				{
+					c'16 [
+					c'8
+					c'8. ]
+				}
+				{
+					c'16 [
+					c'16 ]
+				}
+				{
+					c'8 [
+					c'8. ]
+				}
+				{
+					c'16 [
+					c'8
+					c'8. ]
+				}
+				{
+					c'16 [
+					c'8
+					c'8. ]
+				}
+				{
+					c'16 [
+					c'8
+					c'8. ]
+				}
+				{
+					c'16 [
+					c'16 ]
+				}
+				{
+					c'16 [ ]
+				}
+			}
+		}
+	>>
+>>
