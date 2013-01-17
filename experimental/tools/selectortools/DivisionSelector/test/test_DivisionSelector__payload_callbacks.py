ee6e2d88fbbf52c4db72f986b214c6181791079b
Trevor Bača <trevorbaca@gmail.com>
2013-01-17 18:31:57 +0000

    Implemented DivisionSelector.__and__(timespan) and MeasureSelector.__and__(timespan) callbacks.
    Tests and PDFs included.

--- experimental/tools/selectortools/DivisionSelector/test/test_DivisionSelector__payload_callbacks.py
+++ experimental/tools/selectortools/DivisionSelector/test/test_DivisionSelector__payload_callbacks.py
@@ -146,3 +146,25 @@ def test_DivisionSelector__payload_callbacks_07():
     current_function_name = introspectiontools.get_current_function_name()
     helpertools.write_test_output(score, __file__, current_function_name)
     assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
+
+
+def test_DivisionSelector__payload_callbacks_08():
+    '''Logical AND of divisions and timespan.
+    '''
+
+    score_template = scoretemplatetools.GroupedRhythmicStavesScoreTemplate(staff_count=1)
+    score_specification = specificationtools.ScoreSpecification(score_template)
+    red_segment = score_specification.append_segment(name='red')
+    red_segment.set_time_signatures([(1, 8), (2, 8), (3, 8), (4, 8)])
+    red_segment.set_divisions([(2, 16), (3, 16), (4, 16)])
+    red_segment.set_rhythm(library.sixteenths)
+    blue_segment = score_specification.append_segment(name='blue')
+    red_divisions = red_segment.select_divisions('Voice 1')
+    timespan = timespantools.Timespan(Offset(1, 16), Offset(9, 16))
+    red_divisions = red_divisions & timespan
+    blue_segment.set_divisions(red_divisions)
+    score = score_specification.interpret()
+
+    current_function_name = introspectiontools.get_current_function_name()
+    helpertools.write_test_output(score, __file__, current_function_name)
+    assert score.lilypond_format == helpertools.read_test_output(__file__, current_function_name)
