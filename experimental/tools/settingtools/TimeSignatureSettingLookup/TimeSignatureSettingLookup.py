7b09ed844a4355014a1773db38f49e3bfbc88805
Trevor Bača <trevorbaca@gmail.com>
2013-01-21 21:25:43 +0000

    Regularizing expression evaluation.
    TimeSignatureSettingLookup now applies callbacks only to expression objects (and not to lists or tuples).

--- experimental/tools/settingtools/TimeSignatureSettingLookup/TimeSignatureSettingLookup.py
+++ experimental/tools/settingtools/TimeSignatureSettingLookup/TimeSignatureSettingLookup.py
@@ -14,11 +14,15 @@ class TimeSignatureSettingLookup(SettingLookup):
     ### PUBLIC METHODS ###
 
     def _evaluate(self, score_specification, voice_name=None):
+        from experimental.tools import settingtools
         # ignore voice_name input parameter
         voice_name = None
         segment_specification = score_specification.get_start_segment_specification(self.offset)
         time_signatures = segment_specification.time_signatures[:]
+        result = settingtools.MeasureRegionProduct(
+            time_signatures, voice_name='dummy voice name', start_offset=0)
         # TODO: eventually change to result = self._apply_callbacks(time_signatures)
-        time_signatures, dummy = self._apply_callbacks(time_signatures, None)
+        result, dummy = self._apply_callbacks(result, None)
         # TODO: eventually return TimeSignatureRegionProduct instead of tuple of time signatures
+        time_signatures = result.payload
         return time_signatures
