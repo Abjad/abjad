d1de2c7ecaa9f55b3944fa66a63c72603c10b1bd
Trevor Bača <trevorbaca@gmail.com>
2013-01-27 20:44:43 +0000

    Removed unused input parameters.
    Removed from two methods:
    
        SetExpressionInventory.get_set_expression()
        SetExpressionInventory.get_set_expressions()

--- experimental/tools/expressiontools/SetExpressionInventory/SetExpressionInventory.py
+++ experimental/tools/expressiontools/SetExpressionInventory/SetExpressionInventory.py
@@ -13,29 +13,27 @@ class SetExpressionInventory(ObjectInventory):
     ### PUBLIC METHODS ###
 
     # TODO: reorder parameters attribute, timespan, target_context_name, persist
-    # TODO: maybe remove segment_name parameter?
-    def get_set_expression(self, attribute=None, target_context_name=None, persist=None, timespan=None, segment_name=None):
+    def get_set_expression(self, attribute=None, target_context_name=None, persist=None, timespan=None):
         set_expressions = self.get_set_expressions(attribute=attribute, 
-            target_context_name=target_context_name, persist=persist, timespan=timespan, segment_name=segment_name)
+            target_context_name=target_context_name, persist=persist, timespan=timespan)
         if not set_expressions:
-            error ='no set expressions for {!r} found in segment {!r}.'.format(attribute, segment_name)
+            error ='no set expressions for {!r} found.'.format(attribute)
             raise Exception(error)
         elif 1 < len(set_expressions):
-            error = 'multiple set expressions for {!r} found in segment {!r}.'.format(attribute, segment_name)
+            error = 'multiple set expressions for {!r} found.'.format(attribute)
             raise Exception(error)
         assert len(set_expressions) == 1
         return set_expressions[0]
 
     # TODO: reorder input parameters
-    def get_set_expressions(self, attribute=None, target_context_name=None, persist=None, timespan=None, target=None):
+    def get_set_expressions(self, attribute=None, target_context_name=None, persist=None, timespan=None):
         assert attribute in self.attributes, repr(attribute)
         set_expressions = []
         for set_expression in self:
             if (
                 (attribute is None or set_expression.attribute == attribute) and
-                (target is None or set_expression.target == target) and
-                (target_context_name is None or set_expression.target.target_context_name == target_context_name) and
-                (timespan is None or set_expression.target.timespan == timespan) and
+                (target_context_name is None or set_expression.target_context_name == target_context_name) and
+                (timespan is None or set_expression.target_timespan == timespan) and
                 (persist is None or set_expression.persist == persist)
                 ):
                 set_expressions.append(set_expression)
