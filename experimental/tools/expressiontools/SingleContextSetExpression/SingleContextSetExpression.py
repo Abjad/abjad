b6338d9243715b70ed04fc6bd3fb485e517a0c8c
Trevor Bača <trevorbaca@gmail.com>
2013-02-03 20:26:47 +0000

    Integrating score-rooted set expressions.
    Extended SingleContextSetExpression.store_in_segment_by_context_and_attribute() again.

--- experimental/tools/expressiontools/SingleContextSetExpression/SingleContextSetExpression.py
+++ experimental/tools/expressiontools/SingleContextSetExpression/SingleContextSetExpression.py
@@ -133,4 +133,8 @@ class SingleContextSetExpression(InputSetExpression):
         expressions.append(self)
         if self.persist:
             target_context_proxy = self.score_specification.context_proxies[target_context_name]
-            target_context_proxy.single_context_set_expressions_by_attribute[self.attribute].append(self)
+            expressions = target_context_proxy.single_context_set_expressions_by_attribute[self.attribute]
+            for expression in expressions[:]:
+                if expression.target_timespan == self.target_timespan:
+                    expressions.remove(expression)
+            expressions.append(self)
