7d23ec7d539b181d33832dc2e354b2147e6bfe8b
Trevor Bača <trevorbaca@gmail.com>
2013-02-06 20:00:47 +0000

    Integrating score-anchored set expressions.
    Streamlined Interpreter.copy_persistent_segment_rooted_set_expressions_into_segment_specifications().

--- experimental/tools/interpretertools/Interpreter/Interpreter.py
+++ experimental/tools/interpretertools/Interpreter/Interpreter.py
@@ -45,36 +45,25 @@ class Interpreter(AbjadObject):
             # store persistent single-context set expressions in current segment specification
             for persistent_single_context_set_expression in persistent_single_context_set_expressions:
                 persistent_single_context_set_expression = \
-                    persistent_single_context_set_expression.copy_and_set_root(segment_specification.segment_name)
+                    persistent_single_context_set_expression.copy_and_set_root(
+                    segment_specification.segment_name)
                 persistent_single_context_set_expression.store_in_segment_by_context_and_attribute()
             # store fresh single-context-set expressions in current segment specification
             for attribute in self.attributes:
                 fresh_single_context_set_expressions = \
                     segment_specification.fresh_single_context_set_expressions_by_attribute[attribute]
-                #fresh_context_names = [x.target_context_name for x in fresh_single_context_set_expressions]
-                assert all([x.fresh for x in fresh_single_context_set_expressions])
-                #persistent_single_context_set_expressions = []
-                #for context_proxy in self.score_specification.context_proxies.itervalues():
-                #    set_expressions = context_proxy.single_context_set_expressions_by_attribute.get(attribute, [])
-                #    persistent_single_context_set_expressions.extend(set_expressions)
-                #assert all([x.persist for x in persistent_single_context_set_expressions])
-                set_expressions_to_store = fresh_single_context_set_expressions
-                #for persistent_single_context_set_expression in persistent_single_context_set_expressions[:]:
-                #    if persistent_single_context_set_expression.target_context_name not in fresh_context_names:
-                #        set_expression_to_store = persistent_single_context_set_expression.copy_and_set_root(
-                #            segment_specification.segment_name)
-                #        set_expressions_to_store.append(set_expression_to_store)
-                for set_expression_to_store in set_expressions_to_store:
-                    set_expression_to_store.store_in_segment_by_context_and_attribute()
-                    if set_expression_to_store.persist:
-                        target_context_name = set_expression_to_store.target_context_name
+                fresh_single_context_set_expressions = fresh_single_context_set_expressions
+                for fresh_single_context_set_expression in fresh_single_context_set_expressions:
+                    fresh_single_context_set_expression.store_in_segment_by_context_and_attribute()
+                    if fresh_single_context_set_expression.persist:
+                        target_context_name = fresh_single_context_set_expression.target_context_name
                         expressions = persistent_single_context_set_expressions_by_context[
                             target_context_name].single_context_set_expressions_by_attribute[attribute]
-                        # TODO: maybe also hash expressions by timespan?
                         for expression in expressions[:]:
-                            if expression.target_timespan == set_expression_to_store.target_timespan:
+                            if expression.target_timespan == \
+                                fresh_single_context_set_expression.target_timespan:
                                 expressions.remove(expression)
-                        expressions.append(set_expression_to_store)
+                        expressions.append(fresh_single_context_set_expression)
 
     def evaluate_multiple_context_set_expressions(self):
         for multiple_context_set_expression in self.score_specification.multiple_context_set_expressions:
@@ -82,10 +71,11 @@ class Interpreter(AbjadObject):
             attribute = multiple_context_set_expression.attribute
             fresh_single_context_set_expressions = multiple_context_set_expression.evaluate()
             assert all([x.fresh for x in fresh_single_context_set_expressions])
+            # TODO: make these two branches exactly parallel and see if branches can collapse into one
             if multiple_context_set_expression.is_segment_rooted:
                 root_specification.fresh_single_context_set_expressions_by_attribute[
                     attribute].extend(fresh_single_context_set_expressions)
-            elif multiple_context_set_expression.is_score_rooted:
+            else:
                 root_specification.fresh_single_context_set_expressions_by_attribute[
                     attribute].extend(fresh_single_context_set_expressions)
                 for fresh_single_context_set_expression in fresh_single_context_set_expressions:
@@ -93,8 +83,6 @@ class Interpreter(AbjadObject):
                     root_specification.fresh_single_context_set_expressions_by_context[
                         target_context_name].single_context_set_expressions_by_attribute[
                         attribute].append(fresh_single_context_set_expression)
-            else:
-                raise ValueError(multiple_context_set_expression)
                 
     def instantiate_score(self):
         '''Instantiate score.
