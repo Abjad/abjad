9ea642d9b49653275c127545b0ca508f132a6f9d
Trevor Bača <trevorbaca@gmail.com>
2013-02-08 03:22:23 +0000

    Authored incomplete division coverage test.
    Example. This now works:
    
        red_segment.set_time_signatures([(6, 8), (3, 8)])
        left_measure = red_segment.select_measures('Voice 1')[:1]
        left_measure.timespan.set_divisions([(4, 16)], persist=False)
        red_segment.set_rhythm(library.sixteenths)
        blue_segment = score_specification.append_segment(name='blue')
    
    Divisions are specified for one measure and one measure only.
    
    Other regions default to full-measure divisions.

--- experimental/tools/specificationtools/etc/formatting_boilerplate.py
+++ experimental/tools/specificationtools/etc/formatting_boilerplate.py
@@ -8,6 +8,6 @@ show(lilypond_file)
 
 # use this in committed test files while working on specificationtools tests
 
-current_function_name = introspectiontools.get_current_function_name()
-testtools.write_test_output(score, __file__, current_function_name)
-assert score.lilypond_format == testtools.read_test_output(__file__, current_function_name)
+    current_function_name = introspectiontools.get_current_function_name()
+    testtools.write_test_output(score, __file__, current_function_name)
+    assert score.lilypond_format == testtools.read_test_output(__file__, current_function_name)
--- experimental/tools/specificationtools/test/multiple_segment_solo/test_multiple_segment_solo__incomplete_division_coverage.py
+++ experimental/tools/specificationtools/test/multiple_segment_solo/test_multiple_segment_solo__incomplete_division_coverage.py
@@ -3,6 +3,26 @@ import py
 
 
 def test_multiple_segment_solo__incomplete_division_coverage_01():
+    '''Divisions specified for one measure and one measure only.
+    Other regions default to full-measure divisions.
+    '''
+
+    score_template = scoretemplatetools.GroupedRhythmicStavesScoreTemplate(staff_count=1)
+    score_specification = specificationtools.ScoreSpecificationInterface(score_template)
+    red_segment = score_specification.append_segment(name='red')
+    red_segment.set_time_signatures([(6, 8), (3, 8)])
+    left_measure = red_segment.select_measures('Voice 1')[:1]
+    left_measure.timespan.set_divisions([(4, 16)], persist=False)
+    red_segment.set_rhythm(library.sixteenths)
+    blue_segment = score_specification.append_segment(name='blue')
+    score = score_specification.interpret()
+
+    current_function_name = introspectiontools.get_current_function_name()
+    testtools.write_test_output(score, __file__, current_function_name)
+    assert score.lilypond_format == testtools.read_test_output(__file__, current_function_name)
+
+
+def test_multiple_segment_solo__incomplete_division_coverage_02():
     py.test.skip('working on this one now.')
 
     score_template = scoretemplatetools.GroupedRhythmicStavesScoreTemplate(staff_count=1)
@@ -16,7 +36,7 @@ def test_multiple_segment_solo__incomplete_division_coverage_01():
     blue_segment = score_specification.append_segment(name='blue')
 
 
-def test_multiple_segment_solo__incomplete_division_coverage_02():
+def test_multiple_segment_solo__incomplete_division_coverage_03():
     py.test.skip('working on this one now.')
 
     score_template = scoretemplatetools.GroupedRhythmicStavesScoreTemplate(staff_count=1)
