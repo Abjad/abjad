dca6831410ba3b28806fc5d37d2454b99cd2474b
Trevor Bača <trevorbaca@gmail.com>
2013-02-20 13:03:54 +0000

    Extending pitch interpretation.
    Authored test to one cursor read three times. Cursors maintain state.
    
    Example:
    
        cursor = library.example_pitches_1()
        score_specification.select_leaves('Voice 1')[:3].set_pitches(cursor)
        score_specification.select_leaves('Voice 1')[6:9].set_pitches(cursor)
        score_specification.select_leaves('Voice 1')[12:15].set_pitches(cursor)
    
    This produces the same results as:
    
        leaves = score_specification.select_leaves('Voice 1')[:3]
        leaves += score_specification.select_leaves('Voice 1')[6:9]
        leaves += score_specification.select_leaves('Voice 1')[12:15]
        leaves.set_pitches(cursor)
    
    PDF included.

--- experimental/tools/specificationtools/ScoreSpecification/test/test_ScoreSpecification__set_pitches.py
+++ experimental/tools/specificationtools/ScoreSpecification/test/test_ScoreSpecification__set_pitches.py
@@ -142,3 +142,45 @@ def test_ScoreSpecification__set_pitches_07():
     current_function_name = introspectiontools.get_current_function_name()
     testtools.write_test_output(score, __file__, current_function_name)
     assert score.lilypond_format == testtools.read_test_output(__file__, current_function_name)
+
+
+def test_ScoreSpecification__set_pitches_08():
+    '''Two cursors open against the same server.
+    '''
+
+    score_template = scoretemplatetools.GroupedStavesScoreTemplate(staff_count=2)
+    score_specification = specificationtools.ScoreSpecificationInterface(score_template)
+    score_specification.set_time_signatures(6 * [(2, 8)])
+    score_specification.set_divisions([(3, 16)])
+    rhythm = library.sixteenths.new(beam_cells_together=False, beam_each_cell=True)
+    score_specification.set_rhythm(rhythm)
+    cursor_1 = library.example_pitches_1()
+    cursor_2 = library.example_pitches_1()
+    score_specification.select_leaves('Voice 1')[:9].set_pitches(cursor_1)
+    score_specification.select_leaves('Voice 2')[:9].set_pitches(cursor_2)
+    score = score_specification.interpret()
+
+    current_function_name = introspectiontools.get_current_function_name()
+    testtools.write_test_output(score, __file__, current_function_name)
+    assert score.lilypond_format == testtools.read_test_output(__file__, current_function_name)
+
+
+def test_ScoreSpecification__set_pitches_09():
+    '''One cursor read three times.
+    '''
+
+    score_template = scoretemplatetools.GroupedStavesScoreTemplate(staff_count=1)
+    score_specification = specificationtools.ScoreSpecificationInterface(score_template)
+    score_specification.set_time_signatures(6 * [(2, 8)])
+    score_specification.set_divisions([(3, 16)])
+    rhythm = library.sixteenths.new(beam_cells_together=False, beam_each_cell=True)
+    score_specification.set_rhythm(rhythm)
+    cursor = library.example_pitches_1()
+    score_specification.select_leaves('Voice 1')[:3].set_pitches(cursor)
+    score_specification.select_leaves('Voice 1')[6:9].set_pitches(cursor)
+    score_specification.select_leaves('Voice 1')[12:15].set_pitches(cursor)
+    score = score_specification.interpret()
+
+    current_function_name = introspectiontools.get_current_function_name()
+    testtools.write_test_output(score, __file__, current_function_name)
+    assert score.lilypond_format == testtools.read_test_output(__file__, current_function_name)
