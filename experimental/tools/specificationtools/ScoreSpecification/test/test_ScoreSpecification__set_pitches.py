e0bb746d36edf76ddebcc94af599d349672f483d
Trevor Bača <trevorbaca@gmail.com>
2013-02-20 11:36:26 +0000

    Extending pitch interpretation.
    Authored test to show overlaid reassignment of pitch.
    
    Example:
    
        first_leaves = score_specification.select_leaves('Voice 1')[:9]
        last_leaves = score_specification.select_leaves('Voice 1')[3:12]
        leaves = first_leaves + last_leaves
        leaves.set_pitches(library.example_pitches_1())
    
    Earlier pitch assignments are simply overwritten by later pitch assignments.
    
    PDF included.

--- experimental/tools/specificationtools/ScoreSpecification/test/test_ScoreSpecification__set_pitches.py
+++ experimental/tools/specificationtools/ScoreSpecification/test/test_ScoreSpecification__set_pitches.py
@@ -79,3 +79,25 @@ def test_ScoreSpecification__set_pitches_04():
     current_function_name = introspectiontools.get_current_function_name()
     testtools.write_test_output(score, __file__, current_function_name)
     assert score.lilypond_format == testtools.read_test_output(__file__, current_function_name)
+
+
+def test_ScoreSpecification__set_pitches_05():
+    '''Read from server over overlaid pitches in discontiguous select expression.
+    Earlier pitch assignments are simply overwritten by later pitch assignments.
+    '''
+
+    score_template = scoretemplatetools.GroupedStavesScoreTemplate(staff_count=1)
+    score_specification = specificationtools.ScoreSpecificationInterface(score_template)
+    score_specification.set_time_signatures(6 * [(2, 8)])
+    score_specification.set_divisions([(3, 16)])
+    rhythm = library.sixteenths.new(beam_cells_together=False, beam_each_cell=True)
+    score_specification.set_rhythm(rhythm)
+    first_leaves = score_specification.select_leaves('Voice 1')[:9]
+    last_leaves = score_specification.select_leaves('Voice 1')[3:12]
+    leaves = first_leaves + last_leaves
+    leaves.set_pitches(library.example_pitches_1())
+    score = score_specification.interpret()
+
+    current_function_name = introspectiontools.get_current_function_name()
+    testtools.write_test_output(score, __file__, current_function_name)
+    assert score.lilypond_format == testtools.read_test_output(__file__, current_function_name)
