25af713ec974625d6f1311ba3aac74636c1ca6ab
Trevor Bača <trevorbaca@gmail.com>
2013-02-20 13:17:27 +0000

    Extending pitch interpretation.
    Authored test to show one cursor initialized from the position of another.
    
    Example:
    
        cursor_1 = library.example_pitches_1()
        score_specification.select_leaves('Voice 1')[:9].set_pitches(cursor_1)
        cursor_2 = library.example_pitches_1(position=cursor_1.position)
        score_specification.select_leaves('Voice 2')[-9:].set_pitches(cursor_2)
    
    PDF included.

--- experimental/tools/specificationtools/ScoreSpecification/test/test_ScoreSpecification__set_pitches.py
+++ experimental/tools/specificationtools/ScoreSpecification/test/test_ScoreSpecification__set_pitches.py
@@ -1,4 +1,5 @@
 from experimental import *
+import copy
 
 
 def test_ScoreSpecification__set_pitches_01():
@@ -179,6 +180,48 @@ def test_ScoreSpecification__set_pitches_09():
     score_specification.select_leaves('Voice 1')[:3].set_pitches(cursor)
     score_specification.select_leaves('Voice 1')[6:9].set_pitches(cursor)
     score_specification.select_leaves('Voice 1')[12:15].set_pitches(cursor)
+    score = score_specification.interpret()
+
+    current_function_name = introspectiontools.get_current_function_name()
+    testtools.write_test_output(score, __file__, current_function_name)
+    assert score.lilypond_format == testtools.read_test_output(__file__, current_function_name)
+
+
+def test_ScoreSpecification__set_pitches_10():
+    '''One cursor initialized from the position of another.
+    '''
+
+    score_template = scoretemplatetools.GroupedStavesScoreTemplate(staff_count=2)
+    score_specification = specificationtools.ScoreSpecificationInterface(score_template)
+    score_specification.set_time_signatures(6 * [(2, 8)])
+    score_specification.set_divisions([(3, 16)])
+    rhythm = library.sixteenths.new(beam_cells_together=False, beam_each_cell=True)
+    score_specification.set_rhythm(rhythm)
+    cursor_1 = library.example_pitches_1()
+    score_specification.select_leaves('Voice 1')[:9].set_pitches(cursor_1)
+    cursor_2 = library.example_pitches_1(position=cursor_1.position)
+    score_specification.select_leaves('Voice 2')[-9:].set_pitches(cursor_2)
+    score = score_specification.interpret()
+
+    current_function_name = introspectiontools.get_current_function_name()
+    testtools.write_test_output(score, __file__, current_function_name)
+    assert score.lilypond_format == testtools.read_test_output(__file__, current_function_name)
+
+
+def test_ScoreSpecification__set_pitches_11():
+    '''Copied cursor preserves state.
+    '''
+
+    score_template = scoretemplatetools.GroupedStavesScoreTemplate(staff_count=2)
+    score_specification = specificationtools.ScoreSpecificationInterface(score_template)
+    score_specification.set_time_signatures(6 * [(2, 8)])
+    score_specification.set_divisions([(3, 16)])
+    rhythm = library.sixteenths.new(beam_cells_together=False, beam_each_cell=True)
+    score_specification.set_rhythm(rhythm)
+    cursor_1 = library.example_pitches_1()
+    score_specification.select_leaves('Voice 1')[:9].set_pitches(cursor_1)
+    cursor_2 = copy.deepcopy(cursor_1)
+    score_specification.select_leaves('Voice 2')[-9:].set_pitches(cursor_2)
     score = score_specification.interpret()
 
     current_function_name = introspectiontools.get_current_function_name()
