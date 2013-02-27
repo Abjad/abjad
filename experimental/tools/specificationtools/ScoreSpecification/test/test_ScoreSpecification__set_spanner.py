3ec493ec1cb6d1166743300d964223a6dd2e3d53
Trevor Bača <trevorbaca@gmail.com>
2013-02-27 15:12:51 +0000

    Added test to show generalized grob override (and revert) specification.
    This comes for free because spanners have a grob override proxy.
    
    Example:
    
        divisions = score_specification.select_divisions('Voice 1')[2:4]
        leaves = divisions.timespan.select_leaves('Voice 1')
        spanner = spannertools.SlurSpanner()
        spanner.override.note_head.color = 'red'
        spanner.override.stem.color = 'red'
        spanner.override.beam.color = 'red'
        spanner.override.accidental.color = 'red'
        leaves.set_spanner(spanner)
    
    The example sets a slur on the leaves in divisions 2 - 4 of voice 1
    and also apply the overrides listed.
    
    Test and PDF included.
    
    The limitation is that you have to instantiate an actual spanner to get this work.
    
    The mainline previously had an OverrideSpanner that was designed to do exactly
    this with no further typographic output.
    
    We should either add the OverrideSpanner back into the mainline or else make Spanner concrete.
    
    It seems better to make Spanner concrete because all spanners have a grob override proxy
    (which means that the appearance of an OverrideSpanner class would suggest that some
    spanners do not have the ability to override grobs, which is false).

--- experimental/tools/specificationtools/ScoreSpecification/test/test_ScoreSpecification__set_spanner.py
+++ experimental/tools/specificationtools/ScoreSpecification/test/test_ScoreSpecification__set_spanner.py
@@ -21,3 +21,30 @@ def test_ScoreSpecification__set_spanner_01():
     current_function_name = introspectiontools.get_current_function_name()
     testtools.write_test_output(score, __file__, current_function_name)
     assert score.lilypond_format == testtools.read_test_output(__file__, current_function_name)
+
+
+def test_ScoreSpecification__set_spanner_02():
+    '''Use spanner to set grob overrides and subsequent reverts.
+    '''
+
+    score_template = scoretemplatetools.GroupedStavesScoreTemplate(staff_count=1)
+    score_specification = specificationtools.ScoreSpecificationInterface(score_template)
+    score_specification.set_time_signatures(6 * [(2, 8)])
+    score_specification.set_divisions([(3, 16)])
+    rhythm = library.sixteenths.new(beam_cells_together=False, beam_each_cell=True)
+    score_specification.set_rhythm(rhythm)
+    leaves = score_specification.select_leaves('Voice 1')
+    leaves.set_pitch(library.example_pitches_1())
+    divisions = score_specification.select_divisions('Voice 1')[2:4]
+    leaves = divisions.timespan.select_leaves('Voice 1')
+    spanner = spannertools.SlurSpanner()
+    spanner.override.note_head.color = 'red'
+    spanner.override.stem.color = 'red'
+    spanner.override.beam.color = 'red'
+    spanner.override.accidental.color = 'red'
+    leaves.set_spanner(spanner)
+    score = score_specification.interpret()
+
+    current_function_name = introspectiontools.get_current_function_name()
+    testtools.write_test_output(score, __file__, current_function_name)
+    assert score.lilypond_format == testtools.read_test_output(__file__, current_function_name)
