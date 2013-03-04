42f41f02dbbe9483b62481f54b4370cc562b5a29
Trevor Bača <trevorbaca@gmail.com>
2013-03-04 03:37:15 +0000

    Updated DiatonicClusterHandler.
    The class now calls chord[:] = [] instead of chord.clear().

--- experimental/tools/handlertools/pitch/DiatonicClusterHandler/DiatonicClusterHandler.py
+++ experimental/tools/handlertools/pitch/DiatonicClusterHandler/DiatonicClusterHandler.py
@@ -22,6 +22,6 @@ class DiatonicClusterHandler(PitchHandler):
             diatonic_pitch_numbers = range(start, start + cluster_width)
             chord_pitches = [pitchtools.NamedDiatonicPitch(x) for x in diatonic_pitch_numbers]
             chord = chordtools.Chord(note)
-            chord.clear()
+            chord[:] = []
             chord.extend(chord_pitches)
             componenttools.move_parentage_and_spanners_from_components_to_components([note], [chord])
