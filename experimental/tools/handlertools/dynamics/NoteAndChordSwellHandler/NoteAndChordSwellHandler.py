8b3b6d1554606beb59397fc380bf9e7db688f20f
Trevor Bača <trevorbaca@gmail.com>
2013-03-06 21:06:14 +0000

    Generalized NoteAndChordSwellHandler.
    The class now handles any two-stage pairing of hairpins.
    
    Examples:
    
        p < f > p
       
        fff > f < fff
    
        p < mf < f
    
    All of these are now possible. 
    
    The unifying factor is the presence of 2 hairpins and 2 + 1 = 3 dynamic endpoints.

--- experimental/tools/handlertools/dynamics/NoteAndChordSwellHandler/NoteAndChordSwellHandler.py
+++ experimental/tools/handlertools/dynamics/NoteAndChordSwellHandler/NoteAndChordSwellHandler.py
@@ -21,9 +21,9 @@ class NoteAndChordSwellHandler(DynamicHandler):
     ### SPECIAL METHODS ###
 
     def __call__(self, expr):
-        assert len(self.swell_dynamics) == 3, repr(self.swell_dynamics)
+        assert len(self.swell_dynamics) == 5, repr(self.swell_dynamics)
         assert leaftools.all_are_leaves(expr), repr(expr)
-        start_dynamic, peak_dynamic, stop_dynamic = self.swell_dynamics
+        start_dynamic, left_hairpin, peak_dynamic, right_hairpin, stop_dynamic = self.swell_dynamics
         #leaves = list(iterationtools.iterate_leaves_in_expr(expr))
         #leaves = leaftools.remove_outer_rests_from_sequence(leaves)
         leaves = expr
@@ -42,8 +42,14 @@ class NoteAndChordSwellHandler(DynamicHandler):
                 right_leaves = leaves[-middle_index:]
             else:
                 right_leaves = leaves[-(middle_index+1):]
-            spannertools.CrescendoSpanner(left_leaves)
-            spannertools.DecrescendoSpanner(right_leaves)
+            if left_hairpin == '<':
+                spannertools.CrescendoSpanner(left_leaves)
+            else:
+                spannertools.DecrescendoSpanner(left_leaves)
+            if right_hairpin == '<':
+                spannertools.CrescendoSpanner(right_leaves)
+            else:
+                spannertools.DecrescendoSpanner(right_leaves)
             return leaves
 
     ### READ / WRITE PUBLIC PROPERTIES ###
@@ -53,6 +59,6 @@ class NoteAndChordSwellHandler(DynamicHandler):
         def fget(self):
             return self._swell_dynamics
         def fset(self, swell_dynamics):
-            assert isinstance(swell_dynamics, (tuple, type(None))), repr(swell_dynamics)
+            assert isinstance(swell_dynamics, (tuple, list)), repr(swell_dynamics)
             self._swell_dynamics = swell_dynamics
         return property(**locals())
