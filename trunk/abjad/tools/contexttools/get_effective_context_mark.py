c94ec0a65181b318c8cae8b006b5e7e9d2021561
Trevor Bača <trevorbaca@gmail.com>
2013-03-13 02:47:12 +0000

    Streamlined part of contexttools.get_effective_context_marks().
    Description: The last part of the function searches all context marks
    that govern a component and elects the most recent governing context
    mark as the effective context mark. This is perfect for bisect.
    So this commit replaces the manual search for most recent context mark
    with a call to one of the search methods of SortedCollection.
    
    The speed increase is negligible but the code reads considerably easier
    and is now easier to maintain.

--- trunk/abjad/tools/contexttools/get_effective_context_mark.py
+++ trunk/abjad/tools/contexttools/get_effective_context_mark.py
@@ -1,4 +1,5 @@
 from abjad.tools import componenttools
+from abjad.tools import datastructuretools
 
 
 def get_effective_context_mark(component, klass):
@@ -28,15 +29,15 @@ def get_effective_context_mark(component, klass):
         >>> contexttools.get_effective_context_mark(staff[0], contexttools.TimeSignatureMark)
         TimeSignatureMark((4, 8))(Staff{4})
 
-
     Return context mark or none.
     '''
     from abjad.tools import contexttools
-    # TODO: make contexttools able to import measuretools at top level
     from abjad.tools import measuretools
 
+    # check input
     assert isinstance(component, componenttools.Component)
 
+    # do special things for time signature marks
     if klass == contexttools.TimeSignatureMark:
         if isinstance(component, measuretools.Measure):
             if not getattr(component, '_time_signature_is_current', True):
@@ -44,41 +45,24 @@ def get_effective_context_mark(component, klass):
             if contexttools.is_component_with_time_signature_mark_attached(component):
                 return contexttools.get_time_signature_mark_attached_to_component(component)
 
-    #print 'getting effective context mark mark ...'
-    # following line was tested to be completely unnecessary; remove after statal bug fix:
-    #component._update_prolated_offset_values_of_entire_score_tree_if_necessary()
+    # updating marks of entire score tree if necessary
     component._update_marks_of_entire_score_tree_if_necessary()
 
-    #print 'gathering candidate marks ...'
-    candidate_marks = []
+    # gathering candidate marks
+    candidate_marks = datastructuretools.SortedCollection(key=lambda x: x.start_component.start_offset)
     for parent in componenttools.get_improper_parentage_of_component(component):
         parent_marks = parent._marks_for_which_component_functions_as_effective_context
-        #print 'parent marks %s ...' % str(parent_marks)
         for mark in parent_marks:
-            #print 'now checking %s ...' % mark
             if isinstance(mark, klass):
-                #print 'mark.effective_context is %s ...' % mark.effective_context
                 if mark.effective_context is not None:
-                    candidate_marks.append(mark)
+                    candidate_marks.insert(mark)
                 elif isinstance(mark, contexttools.TimeSignatureMark):
                     if isinstance(mark.start_component, measuretools.Measure):
-                        candidate_marks.append(mark)
-    #print 'unsorted canddiate marks %s ...' % candidate_marks
-    #candidate_marks = sorted(candidate_marks,
-    candidate_marks.sort(key=lambda x: x.start_component.start_offset)
-    #print candidate_marks
-    #for x in candidate_marks:
-    #    print x, x.start_component.start
-    first_winner = None
-    for candidate_mark in reversed(candidate_marks):
-        if candidate_mark.start_component.start_offset <= component.start_offset:
-            if first_winner is None:
-                first_winner = candidate_mark
-            elif candidate_mark.start_component.start_offset == \
-                first_winner.start_component.start_offset:
-                raise ExtraMarkError('%s and %s start at the same time.' % (
-                    first_winner, candidate_mark))
-            else:
-                break
-    #print 'first winner is ', first_winner, '\n'
-    return first_winner
+                        candidate_marks.insert(mark)
+
+    # elect most recent candidate mark
+    if candidate_marks:
+        try:
+            return candidate_marks.find_le(component.start_offset)
+        except ValueError:
+            pass
