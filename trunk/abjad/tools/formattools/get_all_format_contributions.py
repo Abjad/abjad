3a496475cfc4f783f8066abd0bea4cb4aab3a354
Trevor Bača <trevorbaca@gmail.com>
2013-03-13 21:12:50 +0000

    Streamlined spannertools.get_all_format_contributions().

--- trunk/abjad/tools/formattools/get_all_format_contributions.py
+++ trunk/abjad/tools/formattools/get_all_format_contributions.py
@@ -1,32 +1,26 @@
-from abjad.tools.formattools.get_all_mark_format_contributions import get_all_mark_format_contributions
-from abjad.tools.formattools.get_context_setting_format_contributions import get_context_setting_format_contributions
-from abjad.tools.formattools.get_grob_override_format_contributions import get_grob_override_format_contributions
-from abjad.tools.formattools.get_grob_revert_format_contributions import get_grob_revert_format_contributions
-from abjad.tools.formattools.get_spanner_format_contributions import get_spanner_format_contributions
-
-
 def get_all_format_contributions(component):
-    '''Get all format contributions for `component` as a nested dictionary structure.
+    '''Get all format contributions for `component`.
 
-    Return dict.
+    Return nested dictionary.
     '''
+    from abjad.tools import formattools
 
-    result = get_all_mark_format_contributions(component)
+    result = formattools.get_all_mark_format_contributions(component)
 
-    for slot, contributions in get_spanner_format_contributions(component).iteritems():
+    for slot, contributions in formattools.get_spanner_format_contributions(component).iteritems():
         if slot not in result:
-            result[slot] = { }
+            result[slot] = {}
         result[slot]['spanners'] = contributions
 
-    settings = get_context_setting_format_contributions(component)[1]
+    settings = formattools.get_context_setting_format_contributions(component)[1]
     if settings:
         result['context settings'] = settings
 
-    overrides = get_grob_override_format_contributions(component)[1]
+    overrides = formattools.get_grob_override_format_contributions(component)[1]
     if overrides:
         result['grob overrides'] = overrides
 
-    reverts = get_grob_revert_format_contributions(component)[1]
+    reverts = formattools.get_grob_revert_format_contributions(component)[1]
     if reverts:
         result['grob reverts'] = reverts
 
