8f26dea9b3b53590efdfde32348336ddfb149a9a
Trevor Bača <trevorbaca@gmail.com>
2013-03-18 14:43:18 +0000

    Backing out spanner format changes.
    The performance increases are only incremental (10 - 20%) in only certain cases.
    
    The increase doesn't warrant the risk involved in maintaining another statal part of the system.

--- trunk/abjad/tools/formattools/get_spanner_format_contributions.py
+++ trunk/abjad/tools/formattools/get_spanner_format_contributions.py
@@ -17,10 +17,10 @@ def get_spanner_format_contributions(component):
     spanners = spannertools.get_spanners_attached_to_any_improper_parent_of_component(component)
     #spanners = list(spanners)
     #spanners.sort(key=lambda x: x.__class__.__name__)
-    spanners_that_know_how_to_deposit_format_contributions = (
-        spannertools.GlissandoSpanner,
-        spannertools.SlurSpanner,
-        )
+    #spanners_that_know_how_to_deposit_format_contributions = (
+    #    spannertools.GlissandoSpanner,
+    #    spannertools.SlurSpanner,
+    #    )
 
     if isinstance(component, containertools.Container):
         before_contributions = result['before']
@@ -32,8 +32,8 @@ def get_spanner_format_contributions(component):
     other_contributions = []
 
     for spanner in spanners:
-        if isinstance(spanner, spanners_that_know_how_to_deposit_format_contributions):
-            continue
+        #if isinstance(spanner, spanners_that_know_how_to_deposit_format_contributions):
+        #    continue
 
         # override contributions (in before slot)
         if spanner._is_my_first_leaf(component):
@@ -75,42 +75,45 @@ def get_spanner_format_contributions(component):
 
     result['right'] = stop_contributions + other_contributions
 
-    for format_slot_name, pairs in component._spanner_format_contributions.iteritems():
-        result[format_slot_name].extend(pairs)
+    #for format_slot_name, pairs in component._spanner_format_contributions.iteritems():
+    #    result[format_slot_name].extend(pairs)
 
-    # sort first by spanner class name;
-    # sort next by spanner start offset; 
-    # sort last by position of component within spanner
-    def compare_contributions(contribution_1, contribution_2):
-        left_class_name = contribution_1[0].__class__.__name__
-        right_class_name = contribution_2[0].__class__.__name__
-        if left_class_name == right_class_name:
-            left_start_offset = getattr(contribution_1[0], 'start_offset', None)
-            right_start_offset = getattr(contribution_2[0], 'start_offset', None)
-            if left_start_offset == right_start_offset:
-                if contribution_1[2] == contribution_2[2]:
-                    return 0
-                elif contribution_1[2] == 'first leaf in spanner':
-                    return -1
-                elif contribution_1[2] == 'last leaf in spanner':
-                    return 1
-                elif contribution_2[2] == 'first leaf in spanner':
-                    return 1
-                elif contribution_2[2] == 'last leaf in spanner':
-                    return -1
-                else:
-                    return 0
-            else:
-                return cmp(left_start_offset, right_start_offset)
-        else:
-            return cmp(left_class_name, right_class_name)
+#    # sort first by spanner class name;
+#    # sort next by spanner start offset; 
+#    # sort last by position of component within spanner
+#    def compare_contributions(contribution_1, contribution_2):
+#        left_class_name = contribution_1[0].__class__.__name__
+#        right_class_name = contribution_2[0].__class__.__name__
+#        if left_class_name == right_class_name:
+#            left_start_offset = getattr(contribution_1[0], 'start_offset', None)
+#            right_start_offset = getattr(contribution_2[0], 'start_offset', None)
+#            if left_start_offset == right_start_offset:
+#                if contribution_1[2] == contribution_2[2]:
+#                    return 0
+#                elif contribution_1[2] == 'first leaf in spanner':
+#                    return -1
+#                elif contribution_1[2] == 'last leaf in spanner':
+#                    return 1
+#                elif contribution_2[2] == 'first leaf in spanner':
+#                    return 1
+#                elif contribution_2[2] == 'last leaf in spanner':
+#                    return -1
+#                else:
+#                    return 0
+#            else:
+#                return cmp(left_start_offset, right_start_offset)
+#        else:
+#            return cmp(left_class_name, right_class_name)
 
-    for key in result.keys():
-        result[key].sort(cmp=compare_contributions)
-        result[key] = [x[1] for x in result[key]]
+#    for key in result.keys():
+#        result[key].sort(cmp=compare_contributions)
+#        result[key] = [x[1] for x in result[key]]
 
     for key in result.keys():
         if not result[key]:
             del(result[key])
-
+        else:
+            result[key].sort(key=lambda x: x[0].__class__.__name__)
+            result[key] = [x[1] for x in result[key]]
+            
     return result
--- trunk/abjad/tools/spannertools/SlurSpanner/SlurSpanner.py
+++ trunk/abjad/tools/spannertools/SlurSpanner/SlurSpanner.py
@@ -41,31 +41,31 @@ class SlurSpanner(DirectedSpanner):
     def _copy_keyword_args(self, new):
         DirectedSpanner._copy_keyword_args(self, new)
 
-    def _deposit_format_contributions(self):
-        self._deposit_override_format_contributions()
-        # deposit open slur
-        first_leaf = self._get_my_first_leaf()
-        if self.direction is not None:
-            string = '{} ('.format(stringtools.arg_to_tridirectional_lilypond_symbol(self.direction))
-        else:
-            string = '('
-        first_leaf_right = first_leaf._spanner_format_contributions.setdefault('right', [])
-        first_leaf_right.append((self, string, 'first leaf in spanner'))
-        # deposit close slur
-        last_leaf = self._get_my_last_leaf()
-        last_leaf_right = last_leaf._spanner_format_contributions.setdefault('right', [])
-        string = ')'
-        last_leaf_right.append((self, string, 'last leaf in spanner'))
-        self._deposit_revert_format_contributions()
+#    def _deposit_format_contributions(self):
+#        self._deposit_override_format_contributions()
+#        # deposit open slur
+#        first_leaf = self._get_my_first_leaf()
+#        if self.direction is not None:
+#            string = '{} ('.format(stringtools.arg_to_tridirectional_lilypond_symbol(self.direction))
+#        else:
+#            string = '('
+#        first_leaf_right = first_leaf._spanner_format_contributions.setdefault('right', [])
+#        first_leaf_right.append((self, string, 'first leaf in spanner'))
+#        # deposit close slur
+#        last_leaf = self._get_my_last_leaf()
+#        last_leaf_right = last_leaf._spanner_format_contributions.setdefault('right', [])
+#        string = ')'
+#        last_leaf_right.append((self, string, 'last leaf in spanner'))
+#        self._deposit_revert_format_contributions()
 
-    #def _format_right_of_leaf(self, leaf):
-    #    result = []
-    #    if self._is_my_first_leaf(leaf):
-    #        direction = self.direction
-    #        if direction is not None:
-    #            result.append('{} ('.format(stringtools.arg_to_tridirectional_lilypond_symbol(direction)))
-    #        else:
-    #            result.append('(')
-    #    if self._is_my_last_leaf(leaf):
-    #        result.append(')')
-    #    return result
+    def _format_right_of_leaf(self, leaf):
+        result = []
+        if self._is_my_first_leaf(leaf):
+            direction = self.direction
+            if direction is not None:
+                result.append('{} ('.format(stringtools.arg_to_tridirectional_lilypond_symbol(direction)))
+            else:
+                result.append('(')
+        if self._is_my_last_leaf(leaf):
+            result.append(')')
+        return result
