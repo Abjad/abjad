467d923057ff326821a14dbaa6723e95751855c7
Trevor Bača <trevorbaca@gmail.com>
2013-03-19 17:11:10 +0000

    Removed private _repeat_sequence_to_weight.py file.
    These three functions remain in the public API just as before:
    
        sequencetools.repeat_sequence_to_weight_exactly()
        sequencetools.repeat_sequence_to_weight_at_least()
        sequencetools.repeat_sequence_to_weight_at_most()
    
    Doesn't need to be noted in the 2.12 release notes or change log.

--- trunk/abjad/tools/sequencetools/repeat_sequence_to_weight_at_most.py
+++ trunk/abjad/tools/sequencetools/repeat_sequence_to_weight_at_most.py
@@ -1,10 +1,13 @@
-from abjad.tools.sequencetools._repeat_sequence_to_weight import _repeat_sequence_to_weight
+import numbers
+from abjad.tools import mathtools
 
 
 def repeat_sequence_to_weight_at_most(sequence, weight):
     '''.. versionadded:: 1.1
 
-    Repeat `sequence` to `weight` at most::
+    Repeat `sequence` to `weight` at most:
+
+    ::
 
         >>> sequencetools.repeat_sequence_to_weight_at_most((5, -5, -5), 23)
         (5, -5, -5, 5)
@@ -12,4 +15,23 @@ def repeat_sequence_to_weight_at_most(sequence, weight):
     Return newly constructed `sequence` object.
     '''
 
-    return _repeat_sequence_to_weight(sequence, weight, remainder='less')
+    # check input
+    assert isinstance(weight, numbers.Number)
+    assert 0 <= weight
+
+    
+    # initialize result
+    result = [sequence[0]]
+
+    # iterate input
+    i = 1
+    while mathtools.weight(result) < weight:
+        result.append(sequence[i % len(sequence)])
+        i += 1
+
+    # remove overage
+    if weight < mathtools.weight(result):
+        result = result[:-1]
+
+    # return result
+    return type(sequence)(result)
--- trunk/abjad/tools/sequencetools/repeat_sequence_to_weight_exactly.py
+++ trunk/abjad/tools/sequencetools/repeat_sequence_to_weight_exactly.py
@@ -1,10 +1,13 @@
-from abjad.tools.sequencetools._repeat_sequence_to_weight import _repeat_sequence_to_weight
+import numbers
+from abjad.tools import mathtools
 
 
 def repeat_sequence_to_weight_exactly(sequence, weight):
     '''.. versionadded:: 1.1
 
-    Repeat `sequence` to `weight` exactly::
+    Repeat `sequence` to `weight` exactly:
+
+    ::
 
         >>> sequencetools.repeat_sequence_to_weight_exactly((5, -5, -5), 23)
         (5, -5, -5, 5, -3)
@@ -12,4 +15,24 @@ def repeat_sequence_to_weight_exactly(sequence, weight):
     Return newly constructed `sequence` object.
     '''
 
-    return _repeat_sequence_to_weight(sequence, weight, remainder='chop')
+    # check input
+    assert isinstance(weight, numbers.Number)
+    assert 0 <= weight
+
+    # initialize result
+    result = [sequence[0]]
+
+    # iterate input
+    i = 1
+    while mathtools.weight(result) < weight:
+        result.append(sequence[i % len(sequence)])
+        i += 1
+
+    # chop overage
+    if weight < mathtools.weight(result):
+        last_sign = mathtools.sign(result[-1])
+        needed_weight = weight - mathtools.weight(result[:-1])
+        result = result[:-1] + [last_sign * needed_weight]
+
+    # return result
+    return type(sequence)(result)
