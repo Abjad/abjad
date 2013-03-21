b0e5be6e85130d31b689b04e90ba0be2eddd3f55
Trevor Bača <trevorbaca@gmail.com>
2013-03-21 14:34:54 +0000

    Extended iotools.count_function_calls() with fixed_point=True keyword.
    When set the function repeatedly profiles input until fixed point function call count is achieved.
    
    When not set the function profiles input a single time and returns that first function count received.

--- trunk/abjad/tools/iotools/count_function_calls.py
+++ trunk/abjad/tools/iotools/count_function_calls.py
@@ -1,4 +1,4 @@
-def count_function_calls(expr, global_context=None, local_context=None):
+def count_function_calls(expr, global_context=None, local_context=None, fixed_point=True):
     '''.. versionadded:: 2.12
 
     Count function calls returned by ``iotools.profile_expr(expr)``.
@@ -21,15 +21,20 @@ def count_function_calls(expr, global_context=None, local_context=None):
     '''
     from abjad.tools import iotools
 
-    last_result, current_result = 'foo', 'bar'
-    
-    # profile at least twice to ensure consist results from profiler;
-    # not sure why but profiler eventually levels off to consistent output
-    while current_result != last_result:
-        last_result = current_result
-        current_result = iotools.profile_expr(expr, print_to_terminal=False, 
-            global_context=global_context,
-            local_context=local_context)
-        current_result = int(current_result.split()[6])
+    if fixed_point:
+        # profile at least twice to ensure consist results from profiler;
+        # not sure why but profiler eventually levels off to consistent output
+        last_result, current_result = 'foo', 'bar'
+        while current_result != last_result:
+            last_result = current_result
+            current_result = iotools.profile_expr(expr, print_to_terminal=False, 
+                global_context=global_context,
+                local_context=local_context)
+            current_result = int(current_result.split()[6])
+        return current_result
 
-    return current_result
+    result = iotools.profile_expr(expr, print_to_terminal=False, 
+        global_context=global_context,
+        local_context=local_context)
+    result = int(result.split()[6])
+    return result
