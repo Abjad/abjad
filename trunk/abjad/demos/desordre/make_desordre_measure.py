6275b8bcf232ec281a78fb856653bdcb5a9d8eba
Trevor Bača <trevorbaca@gmail.com>
2013-03-25 01:57:57 +0000

    Updated Désordre demo again.

--- trunk/abjad/demos/desordre/make_desordre_measure.py
+++ trunk/abjad/demos/desordre/make_desordre_measure.py
@@ -5,15 +5,17 @@ from abjad.demos.desordre.make_desordre_cell import make_desordre_cell
 
 def make_desordre_measure(pitches):
     '''Constructs a measure composed of *Désordre cells*.
+
     `pitches` is a list of lists of number (e.g., [[1, 2, 3], [2, 3, 4]])
-    The function returns a DynamicMeasure.
+
+    The function returns a measure.
     '''
-    measure = measuretools.DynamicMeasure([ ])
+
     for sequence in pitches:
         container = make_desordre_cell(sequence)
         time_signature = container.duration
         time_signature = mathtools.NonreducedFraction(time_signature)
         time_signature = time_signature.with_denominator(8)
-        measure.append(time_signature, container)
+        measure = Measure(time_signature, [container])
 
     return measure
