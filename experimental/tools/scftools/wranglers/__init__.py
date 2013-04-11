a9253820a00635b91846d7376fd41d719bf521bb
Trevor Bača <trevorbaca@gmail.com>
2013-04-11 23:07:27 +0000

    Updated scftools/wranglers/__init__.py file.
    The file is now autoloading.

--- experimental/tools/scftools/wranglers/__init__.py
+++ experimental/tools/scftools/wranglers/__init__.py
@@ -1,11 +1,5 @@
-from AssetWrangler import AssetWrangler
-from ChunkPackageWrangler import ChunkPackageWrangler
-from FileWrangler import FileWrangler
-from ImportableAssetWrangler import ImportableAssetWrangler
-from MaterialPackageMakerWrangler import MaterialPackageMakerWrangler
-from MaterialPackageWrangler import MaterialPackageWrangler
-from ModuleWrangler import ModuleWrangler
-from MusicSpecifierModuleWrangler import MusicSpecifierModuleWrangler
-from PackageWrangler import PackageWrangler
-from ScorePackageWrangler import ScorePackageWrangler
-from StylesheetFileWrangler import StylesheetFileWrangler
+from abjad.tools import importtools
+
+importtools.import_structured_package(__path__[0], globals(), package_root_name='experimental')
+
+_documentation_section = 'unstable'
