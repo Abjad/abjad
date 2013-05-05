b877694f65bb7c24ecab502fe10043fcaa83a7a5
Trevor Bača <trevorbaca@gmail.com>
2013-05-05 20:16:23 +0000

    Updated score manager tools to recognize score/distribution directory.

--- experimental/tools/scoremanagertools/proxies/DistributionDirectoryProxy/test/test_DistributionDirectoryProxy.py
+++ experimental/tools/scoremanagertools/proxies/DistributionDirectoryProxy/test/test_DistributionDirectoryProxy.py
@@ -7,5 +7,5 @@ def test_DistributionDirectoryProxy_01():
     dist_proxy = scoremanagertools.proxies.DistributionDirectoryProxy('example_score_1')
 
     assert dist_proxy.directory_path == os.path.join(
-        dist_proxy.configuration.scores_directory_path, 'example_score_1', 'dist')
+        dist_proxy.configuration.scores_directory_path, 'example_score_1', 'distribution')
     assert dist_proxy._spaced_class_name == 'distribution directory proxy'
