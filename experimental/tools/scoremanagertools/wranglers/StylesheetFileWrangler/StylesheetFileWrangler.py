1f10c9f4777cc327b95366571010eecbd9bb9fec
Trevor Bača <trevorbaca@gmail.com>
2013-05-08 18:15:45 +0000

    Streamlined StylesheetFileWrangler initializer.

--- experimental/tools/scoremanagertools/wranglers/StylesheetFileWrangler/StylesheetFileWrangler.py
+++ experimental/tools/scoremanagertools/wranglers/StylesheetFileWrangler/StylesheetFileWrangler.py
@@ -1,7 +1,6 @@
 import os
 from abjad.tools import stringtools
 from experimental.tools.scoremanagertools.wranglers.PackageWrangler import PackageWrangler
-from experimental.tools.scoremanagertools.proxies.StylesheetFileProxy import StylesheetFileProxy
 
 
 class StylesheetFileWrangler(PackageWrangler):
@@ -9,20 +8,17 @@ class StylesheetFileWrangler(PackageWrangler):
     ### INITIALIZER ###
 
     def __init__(self, session=None):
-#        PackageWrangler.__init__(self,
-#            system_asset_container_package_paths=[self.configuration.system_stylesheets_package_path],
-#            score_internal_asset_container_package_path_infix=None,
-#            session=session)
-        PackageWrangler.__init__(self, session=session)
-        self._system_asset_container_package_paths = [
-            self.configuration.system_stylesheets_package_path]
-        self._score_internal_asset_container_package_path_infix = None
+        PackageWrangler.__init__(self,
+            system_asset_container_package_paths=[self.configuration.system_stylesheets_package_path],
+            score_internal_asset_container_package_path_infix=None,
+            session=session)
 
     ### READ-ONLY PUBLIC PROPERTIES ###
 
     @property
     def asset_class(self):
-        return StylesheetFileProxy
+        from experimental.tools import scoremanagertools
+        return scoremanagertools.proxies.StylesheetFileProxy
 
     @property
     def breadcrumb(self):
@@ -40,26 +36,31 @@ class StylesheetFileWrangler(PackageWrangler):
     ### PUBLIC METHODS ###
 
     def handle_main_menu_result(self, result):
+        from experimental.tools import scoremanagertools
         if result == 'new':
             self.make_asset_interactively()
         else:
             stylesheet_file_name = os.path.join(self.configuration.system_stylesheets_directory_path, result)
-            stylesheet_proxy = StylesheetFileProxy(stylesheet_file_name, session=self.session)
+            stylesheet_proxy = scoremanagertools.proxies.StylesheetFileProxy(
+                stylesheet_file_name, session=self.session)
             stylesheet_proxy.run()
 
     # TODO: write test
     def make_asset_interactively(self):
+        from experimental.tools import scoremanagertools
         getter = self.io.make_getter(where=self.where())
         getter.append_string('stylesheet name')
         stylesheet_file_name = getter.run()
         if self.session.backtrack():
             return
-        stylesheet_file_name = stringtools.string_to_accent_free_underscored_delimited_lowercase(stylesheet_file_name)
+        stylesheet_file_name = stringtools.string_to_accent_free_underscored_delimited_lowercase(
+            stylesheet_file_name)
         if not stylesheet_file_name.endswith('.ly'):
             stylesheet_file_name = stylesheet_file_name + '.ly'
         stylesheet_file_name = os.path.join(
             self.configuration.system_stylesheets_directory_path, stylesheet_file_name)
-        stylesheet_proxy = StylesheetFileProxy(stylesheet_file_name, session=self.session)
+        stylesheet_proxy = scoremanagertools.proxies.StylesheetFileProxy(
+            stylesheet_file_name, session=self.session)
         stylesheet_proxy.edit()
 
     def make_main_menu(self):
