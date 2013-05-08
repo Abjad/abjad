ed0616a6a430cb7f3d69009a377947dff53ea0dd
Trevor Bača <trevorbaca@gmail.com>
2013-05-08 22:07:41 +0000

    Updated two tests.

--- experimental/tools/packagepathtools/test/test_package_path_to_directory_path.py
+++ experimental/tools/packagepathtools/test/test_package_path_to_directory_path.py
@@ -10,6 +10,6 @@ def test_package_path_to_directory_path_01():
         configuration.system_materials_directory_path
     assert packagepathtools.package_path_to_directory_path('sketches') == \
         configuration.user_sketches_directory_path
-    assert packagepathtools.package_path_to_directory_path('specifiers') == \
+    assert packagepathtools.package_path_to_directory_path('system_specifiers') == \
         configuration.system_specifiers_directory_path
 
