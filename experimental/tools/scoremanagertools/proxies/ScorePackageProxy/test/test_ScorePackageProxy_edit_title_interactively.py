bfe5b159fb3b14d6e382a718f8a7ae1bf5519d29
Trevor Bača <trevorbaca@gmail.com>
2013-05-15 20:25:12 +0000

    Updated test case for score title editing.

--- experimental/tools/scoremanagertools/proxies/ScorePackageProxy/test/test_ScorePackageProxy_edit_title_interactively.py
+++ experimental/tools/scoremanagertools/proxies/ScorePackageProxy/test/test_ScorePackageProxy_edit_title_interactively.py
@@ -2,17 +2,16 @@
 from experimental import *
 
 
-# TODO: remove test from experimental branch or change to something that runs on all systems
 def test_ScorePackageProxy_edit_title_interactively_01():
 
     try:
         score_manager = scoremanagertools.scoremanager.ScoreManager()
-        score_manager._run(user_input='betorung setup title Foo q')
+        score_manager._run(user_input='green~example~score setup title Foo q')
         assert score_manager._session.transcript.signature == (9,)
-        assert score_manager._session.transcript[-5][1][0] == 'Betörung (2012) - setup'
-        assert score_manager._session.transcript[-2][1][0] == 'Foo (2012) - setup'
+        assert score_manager._session.transcript[-5][1][0] == 'Green Example Score (2013) - setup'
+        assert score_manager._session.transcript[-2][1][0] == 'Foo (2013) - setup'
     finally:
-        score_manager._run(user_input='foo setup title Betörung q')
+        score_manager._run(user_input='foo setup title Green~Example~Score q')
         assert score_manager._session.transcript.signature == (9,)
-        assert score_manager._session.transcript[-5][1][0] == 'Foo (2012) - setup'
-        assert score_manager._session.transcript[-2][1][0] == 'Betörung (2012) - setup'
+        assert score_manager._session.transcript[-5][1][0] == 'Foo (2013) - setup'
+        assert score_manager._session.transcript[-2][1][0] == 'Green Example Score (2013) - setup'
