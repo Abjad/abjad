bc7ba3c6033a4e82f74124aaf969e663410e2e9d
Trevor Bača <trevorbaca@gmail.com>
2013-05-17 03:19:06 +0000

    Updated scoremanagertools/built_in_materials package __init__.py file.

--- experimental/tools/scoremanagertools/built_in_materials/__init__.py
+++ experimental/tools/scoremanagertools/built_in_materials/__init__.py
@@ -1,5 +1,1 @@
 from experimental.tools.scoremanagertools.helpers import safe_import
-
-
-safe_import(globals(), 'red_directives', 'red_directives')
-safe_import(globals(), 'testarticulationhandler', 'testarticulationhandler')
