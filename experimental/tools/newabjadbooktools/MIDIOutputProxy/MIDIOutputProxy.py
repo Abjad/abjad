223ddd657b0380675cdf4524944c0372fc3d2391
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2013-06-12 14:56:59 +0000

    Wrote doctests for newabjadbooktools.MIDIOutputProxy.

--- experimental/tools/newabjadbooktools/MIDIOutputProxy/MIDIOutputProxy.py
+++ experimental/tools/newabjadbooktools/MIDIOutputProxy/MIDIOutputProxy.py
@@ -7,6 +7,17 @@ from experimental.tools.newabjadbooktools.AssetOutputProxy import AssetOutputPro
 
 
 class MIDIOutputProxy(AssetOutputProxy):
+    '''Output proxy for LilyPond MIDI files"
+
+    ::
+
+        >>> payload = Staff("c'4 d'4 e'4 f'4")
+        >>> output_proxy = newabjadbooktools.MIDIOutputProxy(payload)
+        >>> print output_proxy
+        MIDIOutputProxy()
+
+    Return output_proxy.
+    '''
 
     ### INITIALIZER ###
 
@@ -27,7 +38,6 @@ class MIDIOutputProxy(AssetOutputProxy):
              
     ### PUBLIC METHODS ###
 
-
     def get_absolute_asset_output_path(self, document_handler):
         return os.path.join(
             document_handler.asset_output_directory_name,
@@ -40,3 +50,52 @@ class MIDIOutputProxy(AssetOutputProxy):
             '{}.{}'.format(self.file_name_without_extension, 'mid'),
             )
 
+    def handle_html_document_environment(self, document_handler):
+        '''Handle an HTML document environment:
+
+        ::
+
+            >>> document_handler = newabjadbooktools.HTMLDocumentHandler([])
+            >>> result = output_proxy.handle_html_document_environment(
+            ...     document_handler)
+            >>> for x in result:
+            ...     x
+            '<audio controls="controls">'
+            '\tYour browser does not support the <code>audio</code> element.'
+            '\t<source src="assets/midi-841714d6c9f05853df8b09fdff45f27c.mid">'
+            '</audio>'
+            ''
+
+        Return list.
+        '''
+        asset_path = self.get_relative_asset_output_path(document_handler)
+        result = [
+            '<audio controls="controls">',
+            '\tYour browser does not support the <code>audio</code> element.',
+            '\t<source src="{}">'.format(asset_path),
+            '</audio>',
+            ''
+            ]
+        return result
+
+    def handle_rest_document_environment(self, document_handler):
+        '''Handle an ReST document_environment:
+
+        ::
+
+            >>> document_handler = newabjadbooktools.ReSTDocumentHandler([])
+            >>> result = output_proxy.handle_rest_document_environment(
+            ...     document_handler)
+            >>> for x in result:
+            ...     x
+            ':download:`MIDI <assets/midi-841714d6c9f05853df8b09fdff45f27c.mid>`'
+            ''
+
+        Return list.
+        '''
+        asset_path = self.get_relative_asset_output_path(document_handler)
+        result = [
+            ':download:`MIDI <{}>`'.format(asset_path),
+            ''
+            ]
+        return result
