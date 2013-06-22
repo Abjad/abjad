980f1ae2c14aa9f6cff9fdb97f6e8778b303efd6
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2013-06-22 04:04:23 +0000

    Wrote TextualDocumentHandler filesystem test.

--- experimental/tools/newabjadbooktools/TextualDocumentHandler/TextualDocumentHandler.py
+++ experimental/tools/newabjadbooktools/TextualDocumentHandler/TextualDocumentHandler.py
@@ -1,4 +1,5 @@
 import collections
+import os
 from experimental.tools.newabjadbooktools.DocumentHandler import DocumentHandler
 
 
---
+++ experimental/tools/newabjadbooktools/TextualDocumentHandler/test/test_TextualDocumentHandler.py
@@ -0,0 +1,54 @@
+import os
+from experimental import *
+
+
+def test_TextualDocumentHandler_01():
+
+    document = '''Let's print something:
+
+    <abjad>
+    print "hello, world!"
+    </abjad>
+
+    This is just a simple Python string:
+
+    <abjad>
+    just_a_string = \'\'\'
+    show(Nothing!)
+    \'\'\'
+    </abjad>
+
+    And let's show some music too:
+
+    <abjad>
+    show(Note("c'4"))
+    </abjad>
+
+    That's it!
+    '''
+
+    test_directory_path = os.path.abspath(os.path.dirname(__file__))
+    test_document_file_name = 'document.rst'
+    test_document_file_path = os.path.join(
+        test_directory_path,
+        test_document_file_name,
+        )
+    if os.path.exists(test_document_file_path):
+        os.remove(test_document_file_path)
+
+    document_handler = newabjadbooktools.ReSTDocumentHandler(
+        document,
+        document_file_name=test_document_file_name,
+        output_directory_path=test_directory_path,
+        )
+    document_handler.extract_code_blocks()
+    document_handler.execute_code_blocks()
+    rebuilt_document = '\n'.join(document_handler.rebuild_document())
+    document_handler.write_rebuilt_document_to_disk()
+
+    assert os.path.exists(test_document_file_path)
+    with open(test_document_file_path, 'r') as f:
+        written_document = f.read()
+
+    os.remove(test_document_file_path)
+    assert rebuilt_document == written_document
