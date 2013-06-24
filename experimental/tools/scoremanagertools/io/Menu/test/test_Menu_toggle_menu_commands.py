409e0adeef79eefb50204b707789417086b267b1
Trevor Bača <trevorbaca@gmail.com>
2013-06-24 15:23:45 +0000

    Added Menu.toggle_menu_commands() test.

---
+++ experimental/tools/scoremanagertools/io/Menu/test/test_Menu_toggle_menu_commands.py
@@ -0,0 +1,14 @@
+from experimental import *
+
+
+def test_ScoreManager_toggle_menu_commands_01():
+
+    score_manager = scoremanagertools.scoremanager.ScoreManager()
+    score_manager._run(user_input='tmc q')
+
+    starting_menu_lines = score_manager._session.transcript[0][1]
+    modified_menu_lines = score_manager._session.transcript[2][1]
+
+    new_score_menu_line = '     new score (new)'
+    assert new_score_menu_line in starting_menu_lines
+    assert not new_score_menu_line in modified_menu_lines
