5bada407789ee7bbb17e7043fa7c32ac1e186008
Trevor Bača <trevorbaca@gmail.com>
2013-06-26 12:39:03 +0000

    Renamed UserInputGetter test files.

--- experimental/tools/scoremanagertools/io/UserInputGetter/test/test_UserInputGetter_where.py
+++ experimental/tools/scoremanagertools/io/UserInputGetter/test/test_UserInputGetter_where.py
@@ -3,7 +3,7 @@ import py
 py.test.skip('unskip after where automatically toggles where-tracking.')
 
 
-def test_UserInputGetterMenu_where_01():
+def test_UserInputGetter_where_01():
 
     score_manager = scoremanagertools.scoremanager.ScoreManager()
     score_manager._run(pending_user_input='red~example~score setup performers move where q')
