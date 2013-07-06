ecc41f4a342337cbef03d683d9a423cb91831d4f
Trevor Bača <trevorbaca@gmail.com>
2013-07-06 19:30:52 +0000

    Removed private componenttools function.
        * componenttools._cut_components_from_parentage()
    
    The function was unused.

--- trunk/abjad/tools/iotools/count_function_calls.py
+++ trunk/abjad/tools/iotools/count_function_calls.py
@@ -13,7 +13,7 @@ def count_function_calls(
     ::
 
         >>> iotools.count_function_calls("Note('c4')", globals())
-        11274
+        11286
 
     Example 2. Function calls required to initialize note from integers:
 
