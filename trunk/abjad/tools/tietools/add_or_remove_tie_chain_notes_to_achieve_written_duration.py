df58d79587fbc2eba166cc9a08dcfa93aab05fd1
Trevor Bača <trevorbaca@gmail.com>
2013-07-08 13:42:45 +0000

    Removed another private spannertools function:
        _withdraw_components_from_attached_spanners()
    
    The function is unnecessary.

--- trunk/abjad/tools/tietools/add_or_remove_tie_chain_notes_to_achieve_written_duration.py
+++ trunk/abjad/tools/tietools/add_or_remove_tie_chain_notes_to_achieve_written_duration.py
@@ -5,10 +5,12 @@ from abjad.tools import spannertools
 from abjad.tools import tuplettools
 
 
-# TODO: Inspect tietools.add_or_remove_tie_chain_notes_to_achieve_written_duration() carefully.
-#       Determine whether behavior is correct with LilyPond multipliers.
-def add_or_remove_tie_chain_notes_to_achieve_written_duration(tie_chain, new_written_duration):
-    r'''Add or remove `tie_chain` notes to achieve `written_duration`::
+# TODO: determine whether behavior is correct with LilyPond multipliers.
+def add_or_remove_tie_chain_notes_to_achieve_written_duration(
+    tie_chain, new_written_duration):
+    r'''Add or remove `tie_chain` notes to achieve `written_duration`:
+
+    ::
 
         >>> staff = Staff("c'8 [ ]")
 
@@ -37,8 +39,6 @@ def add_or_remove_tie_chain_notes_to_achieve_written_duration(tie_chain, new_wri
     Return `tie_chain`.
     '''
     from abjad.tools import tietools
-    from abjad.tools.spannertools._withdraw_components_from_attached_spanners import \
-        _withdraw_components_from_attached_spanners
 
     assert isinstance(tie_chain, tietools.TieChain)
     new_written_duration = durationtools.Duration(new_written_duration)
@@ -54,24 +54,32 @@ def add_or_remove_tie_chain_notes_to_achieve_written_duration(tie_chain, new_wri
             pass
         elif len(durations) < len(tie_chain):
             for leaf in tie_chain[len(durations):]:
-                componenttools.remove_component_subtree_from_score_and_spanners([leaf])
+                componenttools.remove_component_subtree_from_score_and_spanners(
+                    [leaf])
         elif len(tie_chain) < len(durations):
-            spannertools.destroy_spanners_attached_to_component(tie_chain[0], tietools.TieSpanner)
+            spannertools.destroy_spanners_attached_to_component(
+                tie_chain[0], tietools.TieSpanner)
             difference = len(durations) - len(tie_chain)
             extra_leaves = tie_chain[0] * difference
-            _withdraw_components_from_attached_spanners(extra_leaves)
+            for extra_leaf in extra_leaves:
+                for spanner in extra_leaf.spanners:
+                    spanner._remove(extra_leaf)
             extra_tokens = durations[len(tie_chain):]
             for leaf, token in zip(extra_leaves, extra_tokens):
                 leaf.written_duration = token.written_duration
-            if not spannertools.is_component_with_spanner_attached(tie_chain[-1], tietools.TieSpanner):
+            if not spannertools.is_component_with_spanner_attached(
+                tie_chain[-1], tietools.TieSpanner):
                 tietools.TieSpanner(list(tie_chain))
-            componenttools.extend_in_parent_of_component(tie_chain[-1], extra_leaves, grow_spanners=True)
+            componenttools.extend_in_parent_of_component(
+                tie_chain[-1], extra_leaves, grow_spanners=True)
     else:
         durations = notetools.make_notes(0, new_written_duration)
         assert isinstance(durations[0], tuplettools.Tuplet)
         fmtuplet = durations[0]
-        new_chain_written = tietools.get_tie_chain(fmtuplet[0]).preprolated_duration
-        add_or_remove_tie_chain_notes_to_achieve_written_duration(tie_chain, new_chain_written)
+        new_chain_written = \
+            tietools.get_tie_chain(fmtuplet[0]).preprolated_duration
+        add_or_remove_tie_chain_notes_to_achieve_written_duration(
+            tie_chain, new_chain_written)
         multiplier = fmtuplet.multiplier
         tuplettools.Tuplet(multiplier, tie_chain.leaves)
 
