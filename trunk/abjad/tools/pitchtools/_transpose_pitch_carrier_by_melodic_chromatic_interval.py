f6dccc0aecb55fed282b222551034f0b73ba0fcf
Trevor Bača <trevorbaca@gmail.com>
2013-07-08 17:17:09 +0000

    Beautified two private pitchtools modules.
    Preparing the modules for removal.

--- trunk/abjad/tools/pitchtools/_transpose_pitch_carrier_by_melodic_chromatic_interval.py
+++ trunk/abjad/tools/pitchtools/_transpose_pitch_carrier_by_melodic_chromatic_interval.py
@@ -2,13 +2,15 @@ import numbers
 
 
 # TODO: make public?
-def _transpose_pitch_carrier_by_melodic_chromatic_interval(pitch_carrier, melodic_chromatic_interval):
+def _transpose_pitch_carrier_by_melodic_chromatic_interval(
+    pitch_carrier, melodic_chromatic_interval):
     '''.. versionadded:: 2.0
 
-    Transpose `pitch_carrier` by `melodic_chromatic_interval`::
+    Transpose `pitch_carrier` by `melodic_chromatic_interval`:
+
+    ::
 
         >>> from abjad.tools.pitchtools._transpose_pitch_carrier_by_melodic_chromatic_interval import _transpose_pitch_carrier_by_melodic_chromatic_interval
-
         >>> pitch = pitchtools.NamedChromaticPitch(12)
         >>> mci = pitchtools.MelodicChromaticInterval(-3)
         >>> _transpose_pitch_carrier_by_melodic_chromatic_interval(pitch, mci)
@@ -21,30 +23,31 @@ def _transpose_pitch_carrier_by_melodic_chromatic_interval(pitch_carrier, melodi
     from abjad.tools import notetools
     from abjad.tools import pitchtools
 
-    # TODO: remove try / except block
-    try:
-        mci = pitchtools.MelodicChromaticInterval(melodic_chromatic_interval)
-    except (TypeError, ValueError):
-        raise TypeError('must be melodic chromatic interval.')
-
-    # works for named & numbered pitches both chromatic & diatonic
+    mci = pitchtools.MelodicChromaticInterval(melodic_chromatic_interval)
     if isinstance(pitch_carrier, pitchtools.Pitch):
-        return type(pitch_carrier)(pitch_carrier.chromatic_pitch_number + mci.semitones)
+        number = pitch_carrier.chromatic_pitch_number + mci.semitones
+        return type(pitch_carrier)(number)
     elif isinstance(pitch_carrier, numbers.Number):
         pitch_carrier = pitchtools.NumberedChromaticPitch(pitch_carrier)
-        result = _transpose_pitch_carrier_by_melodic_chromatic_interval(pitch_carrier, mci)
+        result = _transpose_pitch_carrier_by_melodic_chromatic_interval(
+            pitch_carrier, mci)
         return result.chromatic_pitch_number
     elif isinstance(pitch_carrier, notetools.Note):
-        new_note = componenttools.copy_components_and_remove_spanners([pitch_carrier])[0]
-        new_pitch = pitchtools.NamedChromaticPitch(
-            abs(pitch_carrier.written_pitch.numbered_chromatic_pitch) + mci.number)
+        new_note = componenttools.copy_components_and_remove_spanners(
+            [pitch_carrier])[0]
+        number = abs(pitch_carrier.written_pitch.numbered_chromatic_pitch)
+        number += mci.number
+        new_pitch = pitchtools.NamedChromaticPitch(number)
         new_note.written_pitch = new_pitch
         return new_note
     elif isinstance(pitch_carrier, chordtools.Chord):
-        new_chord = componenttools.copy_components_and_remove_spanners([pitch_carrier])[0]
-        for new_nh, old_nh in zip(new_chord.note_heads, pitch_carrier.note_heads):
-            new_pitch = pitchtools.NamedChromaticPitch(
-                abs(old_nh.written_pitch.numbered_chromatic_pitch) + mci.number)
+        new_chord = componenttools.copy_components_and_remove_spanners(
+            [pitch_carrier])[0]
+        for new_nh, old_nh in \
+            zip(new_chord.note_heads, pitch_carrier.note_heads):
+            number = abs(old_nh.written_pitch.numbered_chromatic_pitch)
+            number += mci.number
+            new_pitch = pitchtools.NamedChromaticPitch(number)
             new_nh.written_pitch = new_pitch
         return new_chord
     else:
--- trunk/abjad/tools/pitchtools/_transpose_pitch_carrier_by_melodic_diatonic_interval.py
+++ trunk/abjad/tools/pitchtools/_transpose_pitch_carrier_by_melodic_diatonic_interval.py
@@ -1,14 +1,18 @@
 # TODO: make public?
-def _transpose_pitch_carrier_by_melodic_diatonic_interval(pitch_carrier, melodic_diatonic_interval):
+def _transpose_pitch_carrier_by_melodic_diatonic_interval(
+    pitch_carrier, melodic_diatonic_interval):
     '''.. versionadded:: 2.0
 
-    Transpose `pitch_carrier` by `melodic_diatonic_interval`::
+    Transpose `pitch_carrier` by `melodic_diatonic_interval`:
 
-        >>> from abjad.tools.pitchtools._transpose_pitch_carrier_by_melodic_diatonic_interval import _transpose_pitch_carrier_by_melodic_diatonic_interval
+    ::
 
+        >>> from abjad.tools.pitchtools._transpose_pitch_carrier_by_melodic_diatonic_interval \
+        ...     import _transpose_pitch_carrier_by_melodic_diatonic_interval
         >>> pitch_carrier = pitchtools.NamedChromaticPitch("c''")
         >>> mdi = pitchtools.MelodicDiatonicInterval('minor', -3)
-        >>> _transpose_pitch_carrier_by_melodic_diatonic_interval(pitch_carrier, mdi)
+        >>> _transpose_pitch_carrier_by_melodic_diatonic_interval(
+        ...     pitch_carrier, mdi)
         NamedChromaticPitch("a'")
 
     Return named chromatic pitch.
@@ -18,36 +22,38 @@ def _transpose_pitch_carrier_by_melodic_diatonic_interval(pitch_carrier, melodic
     from abjad.tools import notetools
     from abjad.tools import pitchtools
 
-    # TODO: remove try / except block
-    try:
-        mdi = pitchtools.MelodicDiatonicInterval(melodic_diatonic_interval)
-    except (TypeError, ValueError):
-        raise TypeError('must be melodic diatonic interval.')
-
+    mdi = pitchtools.MelodicDiatonicInterval(melodic_diatonic_interval)
     if isinstance(pitch_carrier, pitchtools.Pitch):
-        return _transpose_pitch_by_melodic_diatonic_interval(pitch_carrier, mdi)
+        return _transpose_pitch_by_melodic_diatonic_interval(
+            pitch_carrier, mdi)
     elif isinstance(pitch_carrier, notetools.Note):
-        new_note = componenttools.copy_components_and_remove_spanners([pitch_carrier])[0]
-        new_pitch = _transpose_pitch_by_melodic_diatonic_interval(pitch_carrier.written_pitch, mdi)
+        new_note = componenttools.copy_components_and_remove_spanners(
+            [pitch_carrier])[0]
+        new_pitch = _transpose_pitch_by_melodic_diatonic_interval(
+            pitch_carrier.written_pitch, mdi)
         new_note.written_pitch = new_pitch
         return new_note
     elif isinstance(pitch_carrier, chordtools.Chord):
-        new_chord = componenttools.copy_components_and_remove_spanners([pitch_carrier])[0]
-        for new_nh, old_nh in zip(new_chord.note_heads, pitch_carrier.note_heads):
-            new_pitch = _transpose_pitch_by_melodic_diatonic_interval(old_nh.written_pitch, mdi)
+        new_chord = componenttools.copy_components_and_remove_spanners(
+            [pitch_carrier])[0]
+        for new_nh, old_nh in \
+            zip(new_chord.note_heads, pitch_carrier.note_heads):
+            new_pitch = _transpose_pitch_by_melodic_diatonic_interval(
+                old_nh.written_pitch, mdi)
             new_nh.written_pitch = new_pitch
         return new_chord
     else:
-        #raise TypeError('must be pitch, note or chord: %s' % str(pitch_carrier))
         return pitch_carrier
 
 
 def _transpose_pitch_by_melodic_diatonic_interval(pitch, mdi):
     from abjad.tools import pitchtools
-
     chromatic_pitch_number = pitch.chromatic_pitch_number + mdi.semitones
-    diatonic_pitch_class_number = (pitch.diatonic_pitch_class_number + mdi.staff_spaces) % 7
-    diatonic_pitch_class_name = pitchtools.diatonic_pitch_class_number_to_diatonic_pitch_class_name(
+    diatonic_pitch_class_number = \
+        (pitch.diatonic_pitch_class_number + mdi.staff_spaces) % 7
+    diatonic_pitch_class_name = \
+        pitchtools.diatonic_pitch_class_number_to_diatonic_pitch_class_name(
         diatonic_pitch_class_number)
-    named_chromatic_pitch = pitchtools.NamedChromaticPitch(chromatic_pitch_number, diatonic_pitch_class_name)
+    named_chromatic_pitch = pitchtools.NamedChromaticPitch(
+        chromatic_pitch_number, diatonic_pitch_class_name)
     return type(pitch)(named_chromatic_pitch)
