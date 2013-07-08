4ace36dbe81af7b3a7139cce68e587e760542869
Trevor Bača <trevorbaca@gmail.com>
2013-07-08 19:29:43 +0000

    Removed two private lilypondproxytools modules.
        * lilypondproxytools._get_grob_revert_format_contributions.py
        * lilypondproxytools._get_grob_override_format_contributions.py
    
    The modules were unused.
    
    No private lilypondproxytools modules remain.

--- trunk/abjad/tools/lilypondproxytools/LilyPondGrobOverrideComponentPlugIn/_get_grob_revert_format_contributions.py
+++
@@ -1,9 +0,0 @@
-def _get_grob_revert_format_contributions(component):
-    '''Alphabetized list of LilyPond grob reverts.
-    '''
-    from abjad.tools.leaftools.Leaf import Leaf
-
-    result = []
-    if not isinstance(component, Leaf):
-        result.extend(component.override._list_format_contributions('revert'))
-    return ['grob reverts', result]
