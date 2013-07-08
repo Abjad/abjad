fa02ab3f08b7b2d0e2356a2e07149a0977539ec7
Trevor Bača <trevorbaca@gmail.com>
2013-07-08 19:46:41 +0000

    Removed three private lilypondfiletools modules.
        * lilypondfiletools._parse_note_entry_token.py
        * lilypondfiletools._parse_chord_entry_token.py
        * lilypondfiletools._lilypond_leaf_regex.py
    
    The modules were long ago superseded by the parser.

--- trunk/abjad/tools/lilypondfiletools/_lilypond_leaf_regex.py
+++
@@ -1,6 +0,0 @@
-# TODO: make public
-numeric_body_strings = [str(2 ** n) for n in range(8)]
-other_body_strings = [r'\\breve', r'\\longa', r'\\maxima']
-body_strings = numeric_body_strings + other_body_strings
-body_strings = '|'.join(body_strings)
-_lilypond_leaf_regex = '^([Ra-z]+)(\,*|\'*)\s*(%s)(\\.*)$' % body_strings
