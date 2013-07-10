3ccf099ed306f4831b901bc609645cfd5851974c
Trevor Bača <trevorbaca@gmail.com>
2013-07-10 03:52:26 +0000

    Modernized tietools.tie_chain_to_tuplet_with_ratio() API entry.

--- trunk/abjad/tools/tietools/tie_chain_to_tuplet_with_ratio.py
+++ trunk/abjad/tools/tietools/tie_chain_to_tuplet_with_ratio.py
@@ -6,112 +6,133 @@ from abjad.tools import spannertools
 from abjad.tools import tuplettools
 
 
-def tie_chain_to_tuplet_with_ratio(tie_chain, proportions, is_diminution=True, dotted=True):
-    r'''.. versionadded:: 2.0
+def tie_chain_to_tuplet_with_ratio(
+    tie_chain,
+    proportions,
+    dotted=True,
+    is_diminution=True,
+    ):
+    r'''.. versionadded:: 2.9
 
-    Example 1a. Change `tie_chain` to augmented tuplet with proportions ``[1]``.
-    Avoid dots:
+    Example 1. Change `tie_chain` to diminished tuplet
+    and avoid dots:
 
     ::
 
-        >>> staff = Staff("c'8 [ ~ c'16 c'16 ]")
-
+        >>> staff = Staff(r"c'8 ~ c'16 cqs''4")
+        >>> crescendo = spannertools.HairpinSpanner(staff[:], 'p < f')
+        >>> staff.override.dynamic_line_spanner.staff_padding = 3
+        >>> time_signature = contexttools.TimeSignatureMark((7, 16))
+        >>> time_signature.attach(staff)
+        TimeSignatureMark((7, 16))(Staff{3})
+        
     ::
 
         >>> f(staff)
-        \new Staff {
-            c'8 [ ~
+        \new Staff \with {
+            \override DynamicLineSpanner #'staff-padding = #3
+        } {
+            \time 7/16
+            c'8 \< \p ~
             c'16
-            c'16 ]
+            cqs''4 \f
         }
+
+    ::
+
+        >>> show(staff) # doctest: +SKIP
 
     ::
 
         >>> tie_chain = tietools.get_tie_chain(staff[0])
         >>> tietools.tie_chain_to_tuplet_with_ratio(
-        ...     tie_chain, [1], is_diminution=False, dotted=False)
-        FixedDurationTuplet(3/16, [c'8])
+        ...     tie_chain, 
+        ...     [2, 1, 1, 1], 
+        ...     dotted=False,
+        ...     is_diminution=True,
+        ...     )
+        FixedDurationTuplet(3/16, [c'8, c'16, c'16, c'16])
 
     ::
 
         >>> f(staff)
-        \new Staff {
+        \new Staff \with {
+            \override DynamicLineSpanner #'staff-padding = #3
+        } {
+            \time 7/16
             \tweak #'text #tuplet-number::calc-fraction-text
-            \times 3/2 {
-                c'8 [
+            \times 3/5 {
+                c'8 \< \p
+                c'16
+                c'16
+                c'16
             }
-            c'16 ]
+            cqs''4 \f
         }
-
-    Exampl 1b. Change `tie_chain` to augment tuplet with proportions ``[1, 2]``.
-    Avoid dots:
 
     ::
 
-        >>> staff = Staff("c'8 [ ~ c'16 c'16 ]")
+        >>> show(staff) # doctest: +SKIP
 
+    Example 2. Change `tie_chain` to augmented tuplet
+    and avoid dots:
+
+    ::
+
+        >>> staff = Staff(r"c'8 ~ c'16 cqs''4")
+        >>> crescendo = spannertools.HairpinSpanner(staff[:], 'p < f')
+        >>> staff.override.dynamic_line_spanner.staff_padding = 3
+        >>> time_signature = contexttools.TimeSignatureMark((7, 16))
+        >>> time_signature.attach(staff)
+        TimeSignatureMark((7, 16))(Staff{3})
+        
     ::
 
         >>> f(staff)
-        \new Staff {
-            c'8 [ ~
+        \new Staff \with {
+            \override DynamicLineSpanner #'staff-padding = #3
+        } {
+            \time 7/16
+            c'8 \< \p ~
             c'16
-            c'16 ]
+            cqs''4 \f
         }
+
+    ::
+
+        >>> show(staff) # doctest: +SKIP
 
     ::
 
         >>> tie_chain = tietools.get_tie_chain(staff[0])
         >>> tietools.tie_chain_to_tuplet_with_ratio(
-        ...     tie_chain, [1, 2], is_diminution=False, dotted=False)
-        FixedDurationTuplet(3/16, [c'16, c'8])
+        ...     tie_chain, 
+        ...     [2, 1, 1, 1], 
+        ...     dotted=False,
+        ...     is_diminution=False, 
+        ...     )
+        FixedDurationTuplet(3/16, [c'16, c'32, c'32, c'32])
 
     ::
 
         >>> f(staff)
-        \new Staff {
-            {
-                c'16 [
-                c'8
-            }
-            c'16 ]
-        }
-
-    Examle 1c. Change `tie_chain` to augmented tuplet with proportions ``[1, 2, 2]``.
-    Avoid dots:
-
-    ::
-
-        >>> staff = Staff("c'8 [ ~ c'16 c'16 ]")
-
-    ::
-
-        >>> f(staff)
-        \new Staff {
-            c'8 [ ~
-            c'16
-            c'16 ]
-        }
-
-    ::
-
-        >>> tie_chain = tietools.get_tie_chain(staff[0])
-        >>> tietools.tie_chain_to_tuplet_with_ratio(
-        ...     tie_chain, [1, 2, 2], is_diminution=False, dotted=False)
-        FixedDurationTuplet(3/16, [c'32, c'16, c'16])
-
-    ::
-
-        >>> f(staff)
-        \new Staff {
+        \new Staff \with {
+            \override DynamicLineSpanner #'staff-padding = #3
+        } {
+            \time 7/16
             \tweak #'text #tuplet-number::calc-fraction-text
             \times 6/5 {
-                c'32 [
-                c'16
-                c'16
+                c'16 \< \p
+                c'32
+                c'32
+                c'32
             }
-            c'16 ]
+            cqs''4 \f
         }
+
+    ::
+
+        >>> show(staff) # doctest: +SKIP
 
     Return tuplet.
     '''
@@ -130,14 +151,18 @@ def tie_chain_to_tuplet_with_ratio(tie_chain, proportions, is_diminution=True, d
     # find written duration of each notes in tuplet
     if is_diminution:
         if dotted:
-            basic_written_duration = prolated_duration.equal_or_greater_assignable
+            basic_written_duration = \
+                prolated_duration.equal_or_greater_assignable
         else:
-            basic_written_duration = prolated_duration.equal_or_greater_power_of_two
+            basic_written_duration = \
+                prolated_duration.equal_or_greater_power_of_two
     else:
         if dotted:
-            basic_written_duration = prolated_Duration.equal_or_lesser_assignable
+            basic_written_duration = \
+                prolated_duration.equal_or_lesser_assignable
         else:
-            basic_written_duration = prolated_duration.equal_or_lesser_power_of_two
+            basic_written_duration = \
+                prolated_duration.equal_or_lesser_power_of_two
 
     # find written duration of each note in tuplet
     written_durations = [x * basic_written_duration for x in proportions]
@@ -147,17 +172,20 @@ def tie_chain_to_tuplet_with_ratio(tie_chain, proportions, is_diminution=True, d
         notes = [notetools.Note(0, x) for x in written_durations]
     except AssignabilityError:
         denominator = target_duration._denominator
-        note_durations = [durationtools.Duration(x, denominator) for x in proportions]
+        note_durations = [durationtools.Duration(x, denominator) 
+            for x in proportions]
         notes = notetools.make_notes(0, note_durations)
 
     # make tuplet
     tuplet = tuplettools.FixedDurationTuplet(target_duration, notes)
 
     # replace tie chain with tuplet
-    componenttools.move_parentage_and_spanners_from_components_to_components(list(tie_chain), [tuplet])
+    componenttools.move_parentage_and_spanners_from_components_to_components(
+        list(tie_chain), [tuplet])
 
     # untie tuplet
-    spannertools.destroy_spanners_attached_to_component(tuplet, tietools.TieSpanner)
+    spannertools.destroy_spanners_attached_to_component(
+        tuplet, tietools.TieSpanner)
 
     # return tuplet
     return tuplet
