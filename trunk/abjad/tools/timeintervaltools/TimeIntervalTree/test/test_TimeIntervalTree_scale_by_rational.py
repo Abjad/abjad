490cd1e9d7dda6e5bac2884b93c0ca8982010d21
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2013-07-21 17:35:37 +0000

    Shifted timeintervaltools test collateral.

--- trunk/abjad/tools/timeintervaltools/TimeIntervalTree/test/test_TimeIntervalTree_scale_by_rational.py
+++ trunk/abjad/tools/timeintervaltools/TimeIntervalTree/test/test_TimeIntervalTree_scale_by_rational.py
@@ -1,15 +1,14 @@
 from abjad import *
-from abjad.tools.timeintervaltools import TimeInterval
-from abjad.tools.timeintervaltools import TimeIntervalTree
+import py.test
 
 
 def test_TimeIntervalTree_scale_by_rational_01():
-    t1 = TimeInterval(0, 4)
-    t2 = TimeInterval(2, 6)
-    t3 = TimeInterval(1, 8)
+    t1 = timeintervaltools.TimeInterval(0, 4)
+    t2 = timeintervaltools.TimeInterval(2, 6)
+    t3 = timeintervaltools.TimeInterval(1, 8)
 
-    tree = TimeIntervalTree([t1, t2, t3])
-    scalar = Fraction(1, 2)
+    tree = timeintervaltools.TimeIntervalTree([t1, t2, t3])
+    scalar = durationtools.Multiplier(1, 2)
     result = tree.scale_by_rational(scalar)
 
     assert type(result) == type(tree)
@@ -23,13 +22,13 @@ def test_TimeIntervalTree_scale_by_rational_01():
 
 
 def test_TimeIntervalTree_scale_by_rational_02():
-    t0 = TimeInterval(-3, -1)
-    t1 = TimeInterval(0, 4)
-    t2 = TimeInterval(2, 6)
-    t3 = TimeInterval(1, 8)
+    t0 = timeintervaltools.TimeInterval(-3, -1)
+    t1 = timeintervaltools.TimeInterval(0, 4)
+    t2 = timeintervaltools.TimeInterval(2, 6)
+    t3 = timeintervaltools.TimeInterval(1, 8)
 
-    tree = TimeIntervalTree([t0, t1, t2, t3])
-    scalar = Fraction(1, 2)
+    tree = timeintervaltools.TimeIntervalTree([t0, t1, t2, t3])
+    scalar = durationtools.Multiplier(1, 2)
     result = tree.scale_by_rational(scalar)
 
     assert type(result) == type(tree)
@@ -41,3 +40,33 @@ def test_TimeIntervalTree_scale_by_rational_02():
     assert result.duration == tree.duration * scalar
     for old, new in zip(tree, result):
         assert new.duration == old.duration * scalar
+
+
+def test_TimeIntervalTree_scale_by_rational_03():
+    a = timeintervaltools.TimeInterval(durationtools.Multiplier(-1, 2), 1)
+    b = timeintervaltools.TimeInterval(2, durationtools.Multiplier(7, 3))
+    tree = timeintervaltools.TimeIntervalTree([a, b])
+    rational = 2
+    result = tree.scale_by_rational(rational)
+    assert result.duration == tree.duration * rational
+    assert [x.signature for x in result] == \
+        [(Offset(-1, 2), Offset(5, 2)), 
+        (Offset(9, 2), Offset(31, 6))]
+
+
+def test_TimeIntervalTree_scale_by_rational_04():
+    a = timeintervaltools.TimeInterval(durationtools.Multiplier(-1, 2), 1)
+    b = timeintervaltools.TimeInterval(2, durationtools.Multiplier(7, 3))
+    tree = timeintervaltools.TimeIntervalTree([a, b])
+    rational = -1
+    py.test.raises(AssertionError,
+        "result = tree.scale_by_rational(rational)")
+
+
+def test_TimeIntervalTree_scale_by_rational_05():
+    a = timeintervaltools.TimeInterval(durationtools.Multiplier(-1, 2), 1)
+    b = timeintervaltools.TimeInterval(2, durationtools.Multiplier(7, 3))
+    tree = timeintervaltools.TimeIntervalTree([a, b])
+    rational = 0
+    py.test.raises(AssertionError,
+        "result = tree.scale_by_rational(rational)")
--- trunk/abjad/tools/timeintervaltools/TimeIntervalTree/test/test_TimeIntervalTree_scale_to_rational.py
+++ trunk/abjad/tools/timeintervaltools/TimeIntervalTree/test/test_TimeIntervalTree_scale_to_rational.py
@@ -1,16 +1,14 @@
-import fractions
-from abjad.tools.durationtools import Offset
-from abjad.tools.timeintervaltools import TimeInterval
-from abjad.tools.timeintervaltools import TimeIntervalTree
+from abjad import *
+import py.test
 
 
 def test_TimeIntervalTree_scale_to_rational_01():
-    t1 = TimeInterval(0, 4)
-    t2 = TimeInterval(1, 8)
-    t3 = TimeInterval(2, 6)
+    t1 = timeintervaltools.TimeInterval(0, 4)
+    t2 = timeintervaltools.TimeInterval(1, 8)
+    t3 = timeintervaltools.TimeInterval(2, 6)
 
-    tree = TimeIntervalTree([t1, t2, t3])
-    scalar = fractions.Fraction(1, 4)
+    tree = timeintervaltools.TimeIntervalTree([t1, t2, t3])
+    scalar = durationtools.Duration(1, 4)
     result = tree.scale_to_rational(scalar)
 
     assert type(result) == type(tree)
@@ -19,3 +17,33 @@ def test_TimeIntervalTree_scale_to_rational_01():
         (Offset(1, 32), Offset(1, 4)),
         (Offset(1, 16), Offset(3, 16))]
     assert result.duration == scalar
+
+
+def test_TimeIntervalTree_scale_to_rational_02():
+    a = timeintervaltools.TimeInterval(durationtools.Duration(-1, 2), 1)
+    b = timeintervaltools.TimeInterval(2, durationtools.Duration(7, 3))
+    tree = timeintervaltools.TimeIntervalTree([a, b])
+    rational = 3
+    result = tree.scale_to_rational(rational)
+    assert result.duration == 3
+    assert [x.signature for x in result] == \
+        [(Offset(-1, 2), Offset(37, 34)), 
+        (Offset(73, 34), Offset(5, 2))]
+
+
+def test_TimeIntervalTree_scale_to_rational_03():
+    a = timeintervaltools.TimeInterval(durationtools.Duration(-1, 2), 1)
+    b = timeintervaltools.TimeInterval(2, durationtools.Duration(7, 3))
+    tree = timeintervaltools.TimeIntervalTree([a, b])
+    rational = -1
+    py.test.raises(AssertionError,
+        "result = tree.scale_to_rational(rational)")
+
+
+def test_TimeIntervalTree_scale_to_rational_04():
+    a = timeintervaltools.TimeInterval(durationtools.Duration(-1, 2), 1)
+    b = timeintervaltools.TimeInterval(2, durationtools.Duration(7, 3))
+    tree = timeintervaltools.TimeIntervalTree([a, b])
+    rational = 0
+    py.test.raises(AssertionError,
+        "result = tree.scale_to_rational(rational)")
