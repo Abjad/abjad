a17310769ac19474c0b945d3381096cb6cb3e955
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2013-07-23 13:22:30 +0000

    Fixed test case names in timeintervaltools.

--- trunk/abjad/tools/timeintervaltools/TimeIntervalTree/test/test_TimeIntervalTree_clip_interval_durations_to_range.py
+++ trunk/abjad/tools/timeintervaltools/TimeIntervalTree/test/test_TimeIntervalTree_clip_interval_durations_to_range.py
@@ -2,7 +2,7 @@ from abjad import *
 import py.test
 
 
-def test_timeintervaltools_clip_interval_durations_to_range_01():
+def test_TimeIntervalTree_clip_interval_durations_to_range_01():
     start_offset = None
     stop_offset = None
     tree = timeintervaltools.TimeIntervalTree(timeintervaltools.make_test_intervals())
@@ -11,7 +11,7 @@ def test_timeintervaltools_clip_interval_durations_to_range_01():
     assert sorted([x.start_offset for x in tree]) == sorted([x.start_offset for x in clipped])
 
 
-def test_timeintervaltools_clip_interval_durations_to_range_02():
+def test_TimeIntervalTree_clip_interval_durations_to_range_02():
     start_offset = Fraction(3, 4)
     stop_offset = None
     tree = timeintervaltools.TimeIntervalTree(timeintervaltools.make_test_intervals())
@@ -20,7 +20,7 @@ def test_timeintervaltools_clip_interval_durations_to_range_02():
     assert sorted([x.start_offset for x in tree]) == sorted([x.start_offset for x in clipped])
 
 
-def test_timeintervaltools_clip_interval_durations_to_range_03():
+def test_TimeIntervalTree_clip_interval_durations_to_range_03():
     start_offset = None
     stop_offset = Fraction(1, 5)
     tree = timeintervaltools.TimeIntervalTree(timeintervaltools.make_test_intervals())
@@ -29,7 +29,7 @@ def test_timeintervaltools_clip_interval_durations_to_range_03():
     assert sorted([x.start_offset for x in tree]) == sorted([x.start_offset for x in clipped])
 
 
-def test_timeintervaltools_clip_interval_durations_to_range_04():
+def test_TimeIntervalTree_clip_interval_durations_to_range_04():
     start_offset = Fraction(1, 7)
     stop_offset = Fraction(1, 3)
     tree = timeintervaltools.TimeIntervalTree(timeintervaltools.make_test_intervals())
@@ -38,7 +38,7 @@ def test_timeintervaltools_clip_interval_durations_to_range_04():
     assert sorted([x.start_offset for x in tree]) == sorted([x.start_offset for x in clipped])
 
 
-def test_timeintervaltools_clip_interval_durations_to_range_05():
+def test_TimeIntervalTree_clip_interval_durations_to_range_05():
     start_offset = Fraction(1, 3)
     stop_offset = Fraction(1, 7)
     tree = timeintervaltools.TimeIntervalTree(timeintervaltools.make_test_intervals())
