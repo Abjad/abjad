207057f94875c2cea98c229f797fd0b8ae57b19d
Trevor Bača <trevorbaca@gmail.com>
2013-07-24 04:41:43 +0000

    Moved durationtools functionality.
    Old function: durationtools.yield_durations().
    
    New static method: Duration.yield_durations().

--- trunk/abjad/tools/durationtools/Duration/Duration.py
+++ trunk/abjad/tools/durationtools/Duration/Duration.py
@@ -708,7 +708,7 @@ class Duration(ImmutableAbjadObject, fractions.Fraction):
 
         ::
 
-            >>> generator = durationtools.yield_durations(unique=True)
+            >>> generator = Duration.yield_durations(unique=True)
             >>> for n in range(16):
             ...     rational = generator.next()
             ...     duration = Duration(rational)
@@ -801,3 +801,70 @@ class Duration(ImmutableAbjadObject, fractions.Fraction):
         '''
         nonreduced_fraction = mathtools.NonreducedFraction(self)
         return nonreduced_fraction.with_denominator(denominator)
+
+    @staticmethod
+    def yield_durations(unique=False):
+        r'''Yield durations.
+
+        Example 1. Yield all positive durations in 
+        Cantor diagonalized order:
+
+        ::
+
+            >>> generator = Duration.yield_durations()
+            >>> for n in range(16):
+            ...     generator.next()
+            ...
+            Duration(1, 1)
+            Duration(2, 1)
+            Duration(1, 2)
+            Duration(1, 3)
+            Duration(1, 1)
+            Duration(3, 1)
+            Duration(4, 1)
+            Duration(3, 2)
+            Duration(2, 3)
+            Duration(1, 4)
+            Duration(1, 5)
+            Duration(1, 2)
+            Duration(1, 1)
+            Duration(2, 1)
+            Duration(5, 1)
+            Duration(6, 1)
+
+        Example 2. Yield all positive durations in 
+        Cantor diagonalized order uniquely:
+
+        ::
+
+            >>> generator = Duration.yield_durations(unique=True)
+            >>> for n in range(16):
+            ...     generator.next()
+            ...
+            Duration(1, 1)
+            Duration(2, 1)
+            Duration(1, 2)
+            Duration(1, 3)
+            Duration(3, 1)
+            Duration(4, 1)
+            Duration(3, 2)
+            Duration(2, 3)
+            Duration(1, 4)
+            Duration(1, 5)
+            Duration(5, 1)
+            Duration(6, 1)
+            Duration(5, 2)
+            Duration(4, 3)
+            Duration(3, 4)
+            Duration(2, 5)
+
+        Return generator.
+        '''
+        generator = mathtools.yield_nonreduced_fractions()
+        while True:
+            integer_pair = generator.next()
+            duration = Duration(integer_pair)
+            if not unique:
+                yield duration
+            elif duration.pair == integer_pair:
+                yield duration
