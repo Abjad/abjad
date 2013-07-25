5a10500fea6c94ea7e1977fbaa8f367f72282320
Trevor Bača <trevorbaca@gmail.com>
2013-07-25 15:12:41 +0000

    Updated two iterationtools tests.

--- trunk/abjad/tools/iterationtools/test/test_iterationtools_iterate_pitched_tie_chains_in_expr.py
+++ trunk/abjad/tools/iterationtools/test/test_iterationtools_iterate_pitched_tie_chains_in_expr.py
@@ -5,17 +5,19 @@ def test_iterationtools_iterate_pitched_tie_chains_in_expr_01():
 
     staff = Staff('c ~ c r d ~ d r')
 
-    tie_chains = list(iterationtools.iterate_pitched_tie_chains_in_expr(staff, reverse=True))
+    tie_chains = list(iterationtools.iterate_pitched_tie_chains_in_expr(
+        staff, reverse=True))
 
-    assert tie_chains[0] == leaftools.TieChain(staff[3:5])
-    assert tie_chains[1] == leaftools.TieChain(staff[:2])
+    assert tie_chains[0].music == staff[3:5].music
+    assert tie_chains[1].music == staff[:2].music
 
 
 def test_iterationtools_iterate_pitched_tie_chains_in_expr_02():
 
     staff = Staff('c ~ c r d ~ d r')
 
-    tie_chains = list(iterationtools.iterate_pitched_tie_chains_in_expr(staff))
+    tie_chains = list(iterationtools.iterate_pitched_tie_chains_in_expr(
+        staff))
 
-    assert tie_chains[0] == leaftools.TieChain(staff[:2])
-    assert tie_chains[1] == leaftools.TieChain(staff[3:5])
+    assert tie_chains[0].music == staff[:2].music
+    assert tie_chains[1].music == staff[3:5].music
