26234c0e5e9c93d5c81ced3e86abfbcce998d83e
Trevor Bača <trevorbaca@gmail.com>
2013-07-26 18:13:24 +0000

    Removed resttools function.
        * resttools.make_repeated_rests_from_time_signature (singular)
    
    Use ...
    
        * resttools.make_repeated_rests_from_time_signatures (plural)
    
    ... instead.
    
    CHANGE: should be noted in the 2.13 change log.
    
    NOTE: the resttools package now houses the correct classes
    and make methods for the 2.13 public release:
    
        resttools.MultimeasureRest
        resttools.Rest
    
        resttools.make_multimeasure_rests
        resttools.make_repeated_rests_from_time_signatures
        resttools.make_rests

--- trunk/abjad/tools/resttools/make_repeated_rests_from_time_signatures.py
+++ trunk/abjad/tools/resttools/make_repeated_rests_from_time_signatures.py
@@ -1,7 +1,9 @@
 def make_repeated_rests_from_time_signatures(time_signatures):
     '''.. versionadded 1.1.2
 
-    Make repated rests from `time_signatures`::
+    Make repated rests from `time_signatures`:
+
+    ::
 
         resttools.make_repeated_rests_from_time_signatures([(2, 8), (3, 32)])
         [[Rest('r8'), Rest('r8')], [Rest('r32'), Rest('r32'), Rest('r32')]]
@@ -12,13 +14,30 @@ def make_repeated_rests_from_time_signatures(time_signatures):
     '''
     from abjad.tools import resttools
 
-    # init result
+    # initialize result
     result = []
 
     # iterate time signatures and make rests
     for time_signature in time_signatures:
-        rests = resttools.make_repeated_rests_from_time_signature(time_signature)
+        rests = _make_repeated_rests_from_time_signature(time_signature)
         result.append(rests)
 
     # return result
     return result
+
+def _make_repeated_rests_from_time_signature(time_signature):
+    from abjad.tools import contexttools
+    from abjad.tools import resttools
+
+    # afford basic input polymorphism
+    time_signature = contexttools.TimeSignatureMark(time_signature)
+
+    # check input
+    if time_signature.has_non_power_of_two_denominator:
+        message = 'TODO: extend this function for time signatures'
+        message += ' with non-power-of-two denominators.'
+        raise NotImplementedError(message)
+
+    # make and return repeated rests
+    rest = resttools.Rest((1, time_signature.denominator))
+    return time_signature.numerator * rest
