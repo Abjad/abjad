8fd62affd64b9e1a580e710864db918914a02c8c
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2013-07-31 16:55:59 +0000

    Added new selectiontools function:
        selectiontools.select_leaves()
    
    NEW: Should be noted in 2.13 release notes.

---
+++ trunk/abjad/tools/selectiontools/select_leaves.py
@@ -0,0 +1,7 @@
+def select_leaves(expr=None):
+    from abjad.tools import iterationtools
+    from abjad.tools import selectiontools
+    expr = iterationtools.iterate_leaves_in_expr(expr)
+    selection = selectiontools.LeafSelection(music=expr)
+    return selection
+
