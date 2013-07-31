774284659c9ab3f3b305ba82b39208a7f6f11394
Trevor Bača <trevorbaca@gmail.com>
2013-07-31 01:27:23 +0000

    Migrating tuplet function.
    Old function:
    
        tuplettools.leaf_to_tuplet_with_ratio().
    
    The new method will be bound to LeafSelection:
    
        LeafSelection.leaves_to_tuplets_with_ratio()
    
    A few preparatory steps remain on LeafSelection before this
    will happen.

--- trunk/abjad/tools/leaftools/Leaf/Leaf.py
+++ trunk/abjad/tools/leaftools/Leaf/Leaf.py
@@ -3,6 +3,7 @@ import copy
 import operator
 from abjad.tools import durationtools
 from abjad.tools import formattools
+from abjad.tools import mathtools
 from abjad.tools import sequencetools
 from abjad.tools.componenttools.Component import Component
 
@@ -292,6 +293,50 @@ class Leaf(Component):
         report += self._process_contribution_packet(
             self._format_after_slot(format_contributions))
         return report
+
+    def _to_tuplet_with_ratio(self, proportions, is_diminution=True):
+        from abjad.tools import componenttools
+        from abjad.tools import leaftools
+        from abjad.tools import notetools
+        from abjad.tools import tuplettools
+        # check input
+        proportions = mathtools.Ratio(proportions)
+        # find target duration of fixed-duration tuplet
+        target_duration = self.written_duration
+        # find basic prolated duration of note in tuplet
+        basic_prolated_duration = target_duration / sum(proportions)
+        # find basic written duration of note in tuplet
+        basic_written_duration = \
+            basic_prolated_duration.equal_or_greater_assignable
+        # find written duration of each note in tuplet
+        written_durations = [x * basic_written_duration for x in proportions]
+        # make tuplet notes
+        try:
+            notes = [notetools.Note(0, x) for x in written_durations]
+        except AssignabilityError:
+            denominator = target_duration._denominator
+            note_durations = [durationtools.Duration(x, denominator) 
+                for x in proportions]
+            notes = notetools.make_notes(0, note_durations)
+        # make tuplet
+        tuplet = tuplettools.FixedDurationTuplet(target_duration, notes)
+        # fix tuplet contents if necessary
+        tuplettools.fix_contents_of_tuplets_in_expr(tuplet)
+        # change prolation if necessary
+        if not tuplet.multiplier == 1:
+            if is_diminution:
+                if not tuplet.is_diminution:
+                    tuplettools.change_augmented_tuplets_in_expr_to_diminished(
+                        tuplet)
+            else:
+                if tuplet.is_diminution:
+                    tuplettools.change_diminished_tuplets_in_expr_to_augmented(
+                        tuplet)
+        # give leaf position in score structure to tuplet
+        componenttools.move_parentage_and_spanners_from_components_to_components(
+            [self], [tuplet])
+        # return tuplet
+        return tuplet
 
     ### PUBLIC PROPERTIES ###
 
