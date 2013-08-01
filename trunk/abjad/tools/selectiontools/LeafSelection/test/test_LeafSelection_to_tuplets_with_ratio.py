d2ffe44b038cdc3b1ce18d65eef733b39d86fa43
Trevor Bača <trevorbaca@gmail.com>
2013-08-01 14:21:32 +0000

    Removed tuplet function.
        * tuplettools.leaf_to_tuplet_with_n_notes_of_equal_written_duration(n)
    
    Use ...
    
        >>> ratio = n * [1]
        >>> LeafSelection.to_tuplets_with_ratio(ratio)
    
    ... instead.
    
    Doesn't need to be noted in the 2.13 change log.

--- trunk/abjad/tools/selectiontools/LeafSelection/test/test_LeafSelection_to_tuplets_with_ratio.py
+++ trunk/abjad/tools/selectiontools/LeafSelection/test/test_LeafSelection_to_tuplets_with_ratio.py
@@ -29,7 +29,7 @@ def test_LeafSelection_to_tuplets_with_ratio_01():
 
 def test_LeafSelection_to_tuplets_with_ratio_02():
 
-    note = Note(0, (3, 16))
+    note = Note("c'8.")
     selection = selectiontools.select_leaves(note)
 
     tuplets = selection.to_tuplets_with_ratio([1], is_diminution=True)
@@ -50,3 +50,46 @@ def test_LeafSelection_to_tuplets_with_ratio_02():
 
     tuplets = selection.to_tuplets_with_ratio([1, 2, 2, 3, 3, 4], is_diminution=True)
     assert tuplets[0].lilypond_format == "\\times 4/5 {\n\tc'64\n\tc'32\n\tc'32\n\tc'32.\n\tc'32.\n\tc'16\n}"
+
+
+def test_LeafSelection_to_tuplets_with_ratio_03():
+    r'''Divide note into 1, ..., 5 parts.
+    '''
+
+    note = Note("c'8.")
+    selection = selectiontools.select_leaves(note)
+
+    tuplets =selection.to_tuplets_with_ratio(1 * [1], is_diminution=False)
+    assert tuplets[0].lilypond_format == "{\n\tc'8.\n}"
+
+    tuplets =selection.to_tuplets_with_ratio(2 * [1], is_diminution=False)
+    assert tuplets[0].lilypond_format == "{\n\tc'16.\n\tc'16.\n}"
+
+    tuplets =selection.to_tuplets_with_ratio(3 * [1], is_diminution=False)
+    assert tuplets[0].lilypond_format == "{\n\tc'16\n\tc'16\n\tc'16\n}"
+
+    tuplets =selection.to_tuplets_with_ratio(4 * [1], is_diminution=False)
+    assert tuplets[0].lilypond_format == "{\n\tc'32.\n\tc'32.\n\tc'32.\n\tc'32.\n}"
+
+    tuplets =selection.to_tuplets_with_ratio(5 * [1], is_diminution=False)
+    assert tuplets[0].lilypond_format == "\\tweak #'text #tuplet-number::calc-fraction-text\n\\times 8/5 {\n\tc'64.\n\tc'64.\n\tc'64.\n\tc'64.\n\tc'64.\n}"
+
+
+def test_LeafSelection_to_tuplets_with_ratio_04():
+    r'''Divide note into 1, ..., 5 parts.
+    '''
+
+    note = Note("c'8.")
+    selection = selectiontools.select_leaves(note)
+
+    tuplets =selection.to_tuplets_with_ratio(1 * [1], is_diminution=True)
+    assert tuplets[0].lilypond_format == "{\n\tc'8.\n}"
+
+    tuplets =selection.to_tuplets_with_ratio(2 * [1], is_diminution=True)
+    assert tuplets[0].lilypond_format == "{\n\tc'16.\n\tc'16.\n}"
+
+    tuplets =selection.to_tuplets_with_ratio(3 * [1], is_diminution=True)
+    assert tuplets[0].lilypond_format == "{\n\tc'16\n\tc'16\n\tc'16\n}"
+
+    tuplets =selection.to_tuplets_with_ratio(4 * [1], is_diminution=True)
+    assert tuplets[0].lilypond_format == "{\n\tc'32.\n\tc'32.\n\tc'32.\n\tc'32.\n}"
