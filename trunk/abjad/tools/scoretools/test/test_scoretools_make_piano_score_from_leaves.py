150418f04bd7d003d2d344bc65746f40fa1336dc
Trevor Bača <trevorbaca@gmail.com>
2013-08-05 19:55:59 +0000

    Removed containertools function.
        * containertools.eject_contents_of_container()
    
    Function no longer supported.
    
    Use this recipe ...
    
        >>> contents = container[:]
        >>> containertools.delete_contents_of_container(container)
    
    ... instead.
    
    Doesn't need to be noted in the change log.

--- trunk/abjad/tools/scoretools/test/test_scoretools_make_piano_score_from_leaves.py
+++ trunk/abjad/tools/scoretools/test/test_scoretools_make_piano_score_from_leaves.py
@@ -45,7 +45,8 @@ def test_scoretools_make_piano_score_from_leaves_02():
     '''
 
     container = iotools.p("{ g4 a4 b4 c'4 d'4 r4 a4 g4 }")
-    container_contents = containertools.eject_contents_of_container(container)
+    container_contents = container[:]
+    containertools.delete_contents_of_container(container)
     lowest_treble_pitch = pitchtools.NamedChromaticPitch('a')
     score, treble_staff, bass_staff = \
         scoretools.make_piano_score_from_leaves(
