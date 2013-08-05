618c267b791b2312dd5724ce70c4062512414dcd
Trevor Bača <trevorbaca@gmail.com>
2013-08-05 19:40:38 +0000

    Moved container functionality.
    Old function: containertools.reverse_contents_of_container().
    New method: Container.reverse().
    
    CHANGE: should be noted in the 2.13 change log.

--- trunk/abjad/tools/containertools/Container/Container.py
+++ trunk/abjad/tools/containertools/Container/Container.py
@@ -864,6 +864,59 @@ class Container(Component):
         i = self.index(component)
         del(self[i])
 
+    def reverse(self):
+        r'''Reverse contents of container.
+
+        **Example**:
+
+        ::
+
+            >>> staff = Staff("c'8 [ d'8 ] e'8 ( f'8 )")
+            >>> show(staff) # doctest: +SKIP
+
+        ..  doctest::
+
+            >>> f(staff)
+            \new Staff {
+                c'8 [
+                d'8 ]
+                e'8 (
+                f'8 )
+            }
+
+        ::
+
+            >>> staff.reverse()
+            >>> show(staff) # doctest: +SKIP
+
+        ..  doctest::
+
+            >>> f(staff) # doctest: +SKIP
+            \new Staff {
+                f'8 (
+                e'8 )
+                d'8 [
+                c'8 ]
+            }
+
+        Return none.
+        '''
+        from abjad.tools import containertools
+        from abjad.tools import spannertools
+        def _offset(x, y):
+            if x.get_timespan().start_offset < y.get_timespan().start_offset:
+                return -1
+            elif y.get_timespan().start_offset < x.get_timespan().start_offset:
+                return 1
+            else:
+                return 0
+        self._music.reverse()
+        self._mark_entire_score_tree_for_later_update('prolated')
+        spanners = spannertools.get_spanners_attached_to_any_improper_child_of_component(
+            self)
+        for s in spanners:
+            s._components.sort(_offset)
+
     def select_leaves(self):
         r'''Select leaves in container.
 
--- trunk/abjad/tools/containertools/Container/test/test_Container_reverse.py
+++ trunk/abjad/tools/containertools/Container/test/test_Container_reverse.py
@@ -3,33 +3,34 @@ from abjad import *
 import py.test
 
 
-def test_containertools_reverse_contents_of_container_01():
+def test_Container_reverse_01():
     r'''Retrograde works on a depth-0 Container with no spanners and no parent.
     '''
 
     t = Staff("c'8 d'8 e'8 f'8 g'8 a'8 b'8 c''8")
     leaves_rev = reversed(t.select_leaves())
-    containertools.reverse_contents_of_container(t)
+    t.reverse()
 
     assert list(leaves_rev) == list(t.select_leaves())
     assert select(t).is_well_formed()
 
 
-def test_containertools_reverse_contents_of_container_02():
-    r'''Retrograde works on a depth-0 Container with one spanner attached and no parent.
+def test_Container_reverse_02():
+    r'''Retrograde works on a depth-0 Container with one spanner 
+    attached and no parent.
     '''
 
     t = Staff("c'8 d'8 e'8 f'8 g'8 a'8 b'8 c''8")
     beam = spannertools.BeamSpanner(t)
     leaves_rev = reversed(t.select_leaves())
-    containertools.reverse_contents_of_container(t)
+    t.reverse()
 
     assert list(leaves_rev) == list(t.select_leaves())
     assert beam.components == (t, )
     assert select(t).is_well_formed()
 
 
-def test_containertools_reverse_contents_of_container_03():
+def test_Container_reverse_03():
     r'''Retrograde works on a depth-0 Container with one spanner attached
     to its leaves and with no parent.
     '''
@@ -37,14 +38,14 @@ def test_containertools_reverse_contents_of_container_03():
     t = Staff("c'8 d'8 e'8 f'8 g'8 a'8 b'8 c''8")
     beam = spannertools.BeamSpanner(t.select_leaves())
     leaves_rev = reversed(t.select_leaves())
-    containertools.reverse_contents_of_container(t)
+    t.reverse()
 
     assert list(leaves_rev) == list(t.select_leaves())
     assert beam.components == tuple(t.select_leaves())
     assert select(t).is_well_formed()
 
 
-def test_containertools_reverse_contents_of_container_04():
+def test_Container_reverse_04():
     r'''Retrograde works on a depth-0 Container with one spanner
     attached to itself and with a parent.
     '''
@@ -52,41 +53,43 @@ def test_containertools_reverse_contents_of_container_04():
     t = Staff([Measure((4, 4), "c'8 d'8 e'8 f'8 g'8 a'8 b'8 c''8")] + notetools.make_repeated_notes(2))
     beam = spannertools.BeamSpanner(t[0])
     leaves_rev = reversed(t[0].select_leaves())
-    containertools.reverse_contents_of_container(t[0])
+    t[0].reverse()
     assert list(leaves_rev) == list(t[0].select_leaves())
     assert beam.components == (t[0], )
     assert select(t).is_well_formed()
 
 
-def test_containertools_reverse_contents_of_container_05():
+def test_Container_reverse_05():
     r'''Retrograde works on a depth-0 Container with one spanner
     attached to its leaves and with a parent.
     '''
 
-    t = Staff([Measure((4, 4), "c'8 d'8 e'8 f'8 g'8 a'8 b'8 c''8")] + notetools.make_repeated_notes(2))
+    t = Staff([Measure((4, 4), "c'8 d'8 e'8 f'8 g'8 a'8 b'8 c''8")] + 
+        notetools.make_repeated_notes(2))
     beam = spannertools.BeamSpanner(t[0].select_leaves())
     leaves_rev = reversed(t[0].select_leaves())
-    containertools.reverse_contents_of_container(t[0])
+    t[0].reverse()
     assert list(leaves_rev) == list(t[0].select_leaves())
     assert beam.components == tuple(t[0].select_leaves())
     assert select(t).is_well_formed()
 
 
-def test_containertools_reverse_contents_of_container_06():
-    r'''Retrograde works on a depth-0 Container with one spanner attached to its parent.
+def test_Container_reverse_06():
+    r'''Retrograde works on a depth-0 Container with one spanner 
+    attached to its parent.
     '''
 
     notes = [Note("c'8"), Note("d'8")]
     t = Staff([Measure((4, 4), "c'8 d'8 e'8 f'8 g'8 a'8 b'8 c''8")] + notes)
     beam = spannertools.BeamSpanner(t)
     leaves_rev = reversed(t[0].select_leaves())
-    containertools.reverse_contents_of_container(t[0])
+    t[0].reverse()
     assert list(leaves_rev) == list(t[0].select_leaves())
     assert beam.components == tuple([t])
     assert select(t).is_well_formed()
 
 
-def test_containertools_reverse_contents_of_container_07():
+def test_Container_reverse_07():
     r'''Retrograde works on a depth-0 Container with one spanner
     attached to its parent's contents.
     '''
@@ -96,14 +99,14 @@ def test_containertools_reverse_contents_of_container_07():
     t = Staff([measure] + notes)
     beam = spannertools.BeamSpanner(t[:])
     leaves_rev = reversed(t[0].select_leaves())
-    containertools.reverse_contents_of_container(t[0])
+    t[0].reverse()
     assert list(leaves_rev) == list(t[0].select_leaves())
     assert beam.components == tuple([measure] + notes)
     assert select(t).is_well_formed()
 
 
 # TODO: Add well-formedness check for measure contiguity
-def test_containertools_reverse_contents_of_container_08():
+def test_Container_reverse_08():
     r'''Retrograde unable to apply because of measure contiguity.
     '''
 
@@ -129,13 +132,12 @@ def test_containertools_reverse_contents_of_container_08():
     '''
 
     # TODO: Make MeasureContiguityError raise here #
-
-#   assert py.test.raises(MeasureContiguityError,
-#      'containertools.reverse_contents_of_container(t)')
+    #assert py.test.raises(MeasureContiguityError, 't.reverse()')
 
 
-def test_containertools_reverse_contents_of_container_09():
-    r'''Retrograde works on a depth-2 Container with no parent and with spanners at all levels.
+def test_Container_reverse_09():
+    r'''Retrograde works on a depth-2 Container with no parent 
+    and with spanners at all levels.
     '''
 
     m1 = Measure((4, 8), "c'8 d'8 e'8 f'8")
@@ -146,7 +148,7 @@ def test_containertools_reverse_contents_of_container_09():
     beam1 = spannertools.BeamSpanner(staff[0])
     beam2 = spannertools.BeamSpanner(staff[1])
     gliss = spannertools.GlissandoSpanner(staff.select_leaves())
-    containertools.reverse_contents_of_container(staff)
+    staff.reverse()
     assert staff[0] is m2
     assert staff[1] is m1
     assert len(m2) == 3
