5626c973fc6a43da71308b704fa9029540229ac8
Trevor Bača <trevorbaca@gmail.com>
2013-08-07 04:20:25 +0000

    Cleaned up test files.
    Replaced "a = ..." and "b = ..." in timespantools test files.

--- trunk/abjad/tools/timespantools/Timespan/test/test_Timespan_contains_timespan_improperly.py
+++ trunk/abjad/tools/timespantools/Timespan/test/test_Timespan_contains_timespan_improperly.py
@@ -3,66 +3,66 @@ from abjad import *
 
 
 def test_Timespan_contains_timespan_improperly_01():
-    a = timespantools.Timespan(0, 15)
-    b = timespantools.Timespan(-10, -5)
-    assert not a.contains_timespan_improperly(b)
+    timespan_1 = timespantools.Timespan(0, 15)
+    timespan_2 = timespantools.Timespan(-10, -5)
+    assert not timespan_1.contains_timespan_improperly(timespan_2)
 
 def test_Timespan_contains_timespan_improperly_02():
-    a = timespantools.Timespan(0, 15)
-    b = timespantools.Timespan(-10, 0)
-    assert not a.contains_timespan_improperly(b)
+    timespan_1 = timespantools.Timespan(0, 15)
+    timespan_2 = timespantools.Timespan(-10, 0)
+    assert not timespan_1.contains_timespan_improperly(timespan_2)
 
 def test_Timespan_contains_timespan_improperly_03():
-    a = timespantools.Timespan(0, 15)
-    b = timespantools.Timespan(-10, 5)
-    assert not a.contains_timespan_improperly(b)
+    timespan_1 = timespantools.Timespan(0, 15)
+    timespan_2 = timespantools.Timespan(-10, 5)
+    assert not timespan_1.contains_timespan_improperly(timespan_2)
 
 def test_Timespan_contains_timespan_improperly_04():
-    a = timespantools.Timespan(0, 15)
-    b = timespantools.Timespan(-10, 15)
-    assert not a.contains_timespan_improperly(b)
+    timespan_1 = timespantools.Timespan(0, 15)
+    timespan_2 = timespantools.Timespan(-10, 15)
+    assert not timespan_1.contains_timespan_improperly(timespan_2)
 
 def test_Timespan_contains_timespan_improperly_05():
-    a = timespantools.Timespan(0, 15)
-    b = timespantools.Timespan(-10, 25)
-    assert not a.contains_timespan_improperly(b)
+    timespan_1 = timespantools.Timespan(0, 15)
+    timespan_2 = timespantools.Timespan(-10, 25)
+    assert not timespan_1.contains_timespan_improperly(timespan_2)
 
 def test_Timespan_contains_timespan_improperly_06():
-    a = timespantools.Timespan(0, 15)
-    b = timespantools.Timespan(0, 10)
-    assert a.contains_timespan_improperly(b)
+    timespan_1 = timespantools.Timespan(0, 15)
+    timespan_2 = timespantools.Timespan(0, 10)
+    assert timespan_1.contains_timespan_improperly(timespan_2)
 
 def test_Timespan_contains_timespan_improperly_07():
-    a = timespantools.Timespan(0, 15)
-    b = timespantools.Timespan(0, 15)
-    assert a.contains_timespan_improperly(b)
+    timespan_1 = timespantools.Timespan(0, 15)
+    timespan_2 = timespantools.Timespan(0, 15)
+    assert timespan_1.contains_timespan_improperly(timespan_2)
 
 def test_Timespan_contains_timespan_improperly_08():
-    a = timespantools.Timespan(0, 15)
-    b = timespantools.Timespan(5, 10)
-    assert a.contains_timespan_improperly(b)
+    timespan_1 = timespantools.Timespan(0, 15)
+    timespan_2 = timespantools.Timespan(5, 10)
+    assert timespan_1.contains_timespan_improperly(timespan_2)
 
 def test_Timespan_contains_timespan_improperly_09():
-    a = timespantools.Timespan(0, 15)
-    b = timespantools.Timespan(5, 15)
-    assert a.contains_timespan_improperly(b)
+    timespan_1 = timespantools.Timespan(0, 15)
+    timespan_2 = timespantools.Timespan(5, 15)
+    assert timespan_1.contains_timespan_improperly(timespan_2)
 
 def test_Timespan_contains_timespan_improperly_10():
-    a = timespantools.Timespan(0, 15)
-    b = timespantools.Timespan(0, 25)
-    assert not a.contains_timespan_improperly(b)
+    timespan_1 = timespantools.Timespan(0, 15)
+    timespan_2 = timespantools.Timespan(0, 25)
+    assert not timespan_1.contains_timespan_improperly(timespan_2)
 
 def test_Timespan_contains_timespan_improperly_11():
-    a = timespantools.Timespan(0, 15)
-    b = timespantools.Timespan(5, 25)
-    assert not a.contains_timespan_improperly(b)
+    timespan_1 = timespantools.Timespan(0, 15)
+    timespan_2 = timespantools.Timespan(5, 25)
+    assert not timespan_1.contains_timespan_improperly(timespan_2)
 
 def test_Timespan_contains_timespan_improperly_12():
-    a = timespantools.Timespan(0, 15)
-    b = timespantools.Timespan(15, 25)
-    assert not a.contains_timespan_improperly(b)
+    timespan_1 = timespantools.Timespan(0, 15)
+    timespan_2 = timespantools.Timespan(15, 25)
+    assert not timespan_1.contains_timespan_improperly(timespan_2)
 
 def test_Timespan_contains_timespan_improperly_13():
-    a = timespantools.Timespan(0, 15)
-    b = timespantools.Timespan(20, 25)
-    assert not a.contains_timespan_improperly(b)
+    timespan_1 = timespantools.Timespan(0, 15)
+    timespan_2 = timespantools.Timespan(20, 25)
+    assert not timespan_1.contains_timespan_improperly(timespan_2)
--- trunk/abjad/tools/timespantools/Timespan/test/test_Timespan_stops_before_timespan_starts.py
+++ trunk/abjad/tools/timespantools/Timespan/test/test_Timespan_stops_before_timespan_starts.py
@@ -3,66 +3,66 @@ from abjad import *
 
 
 def test_Timespan_stops_before_timespan_starts_01():
-    a = timespantools.Timespan(0, 15)
-    b = timespantools.Timespan(-10, -5)
-    assert not a.stops_before_timespan_starts(b)
+    timespan_1 = timespantools.Timespan(0, 15)
+    timespan_2 = timespantools.Timespan(-10, -5)
+    assert not timespan_1.stops_before_timespan_starts(timespan_2)
 
 def test_Timespan_stops_before_timespan_starts_02():
-    a = timespantools.Timespan(0, 15)
-    b = timespantools.Timespan(-10, 0)
-    assert not a.stops_before_timespan_starts(b)
+    timespan_1 = timespantools.Timespan(0, 15)
+    timespan_2 = timespantools.Timespan(-10, 0)
+    assert not timespan_1.stops_before_timespan_starts(timespan_2)
 
 def test_Timespan_stops_before_timespan_starts_03():
-    a = timespantools.Timespan(0, 15)
-    b = timespantools.Timespan(-10, 5)
-    assert not a.stops_before_timespan_starts(b)
+    timespan_1 = timespantools.Timespan(0, 15)
+    timespan_2 = timespantools.Timespan(-10, 5)
+    assert not timespan_1.stops_before_timespan_starts(timespan_2)
 
 def test_Timespan_stops_before_timespan_starts_04():
-    a = timespantools.Timespan(0, 15)
-    b = timespantools.Timespan(-10, 15)
-    assert not a.stops_before_timespan_starts(b)
+    timespan_1 = timespantools.Timespan(0, 15)
+    timespan_2 = timespantools.Timespan(-10, 15)
+    assert not timespan_1.stops_before_timespan_starts(timespan_2)
 
 def test_Timespan_stops_before_timespan_starts_05():
-    a = timespantools.Timespan(0, 15)
-    b = timespantools.Timespan(-10, 25)
-    assert not a.stops_before_timespan_starts(b)
+    timespan_1 = timespantools.Timespan(0, 15)
+    timespan_2 = timespantools.Timespan(-10, 25)
+    assert not timespan_1.stops_before_timespan_starts(timespan_2)
 
 def test_Timespan_stops_before_timespan_starts_06():
-    a = timespantools.Timespan(0, 15)
-    b = timespantools.Timespan(0, 10)
-    assert not a.stops_before_timespan_starts(b)
+    timespan_1 = timespantools.Timespan(0, 15)
+    timespan_2 = timespantools.Timespan(0, 10)
+    assert not timespan_1.stops_before_timespan_starts(timespan_2)
 
 def test_Timespan_stops_before_timespan_starts_07():
-    a = timespantools.Timespan(0, 15)
-    b = timespantools.Timespan(0, 15)
-    assert not a.stops_before_timespan_starts(b)
+    timespan_1 = timespantools.Timespan(0, 15)
+    timespan_2 = timespantools.Timespan(0, 15)
+    assert not timespan_1.stops_before_timespan_starts(timespan_2)
 
 def test_Timespan_stops_before_timespan_starts_08():
-    a = timespantools.Timespan(0, 15)
-    b = timespantools.Timespan(5, 10)
-    assert not a.stops_before_timespan_starts(b)
+    timespan_1 = timespantools.Timespan(0, 15)
+    timespan_2 = timespantools.Timespan(5, 10)
+    assert not timespan_1.stops_before_timespan_starts(timespan_2)
 
 def test_Timespan_stops_before_timespan_starts_09():
-    a = timespantools.Timespan(0, 15)
-    b = timespantools.Timespan(5, 15)
-    assert not a.stops_before_timespan_starts(b)
+    timespan_1 = timespantools.Timespan(0, 15)
+    timespan_2 = timespantools.Timespan(5, 15)
+    assert not timespan_1.stops_before_timespan_starts(timespan_2)
 
 def test_Timespan_stops_before_timespan_starts_10():
-    a = timespantools.Timespan(0, 15)
-    b = timespantools.Timespan(0, 25)
-    assert not a.stops_before_timespan_starts(b)
+    timespan_1 = timespantools.Timespan(0, 15)
+    timespan_2 = timespantools.Timespan(0, 25)
+    assert not timespan_1.stops_before_timespan_starts(timespan_2)
 
 def test_Timespan_stops_before_timespan_starts_11():
-    a = timespantools.Timespan(0, 15)
-    b = timespantools.Timespan(5, 25)
-    assert not a.stops_before_timespan_starts(b)
+    timespan_1 = timespantools.Timespan(0, 15)
+    timespan_2 = timespantools.Timespan(5, 25)
+    assert not timespan_1.stops_before_timespan_starts(timespan_2)
 
 def test_Timespan_stops_before_timespan_starts_12():
-    a = timespantools.Timespan(0, 15)
-    b = timespantools.Timespan(15, 25)
-    assert not a.stops_before_timespan_starts(b)
+    timespan_1 = timespantools.Timespan(0, 15)
+    timespan_2 = timespantools.Timespan(15, 25)
+    assert not timespan_1.stops_before_timespan_starts(timespan_2)
 
 def test_Timespan_stops_before_timespan_starts_13():
-    a = timespantools.Timespan(0, 15)
-    b = timespantools.Timespan(20, 25)
-    assert a.stops_before_timespan_starts(b)
+    timespan_1 = timespantools.Timespan(0, 15)
+    timespan_2 = timespantools.Timespan(20, 25)
+    assert timespan_1.stops_before_timespan_starts(timespan_2)
