748445728b891eaa057edd58cadd4f6a6c7a3aae
Trevor Bača <trevorbaca@gmail.com>
2013-08-09 20:13:25 +0000

    Added MutationInterface to selectiontools.
    MutationInterface is currently a stub.
    
    MutationInterface will house the Abjad mutator methods
    defined against a single component.

---
+++ trunk/abjad/tools/selectiontools/MutationInterface/MutationInterface.py
@@ -0,0 +1,9 @@
+# -*- encoding: utf-8 -*-
+from abjad.tools.selectiontools.Selection import Selection
+
+
+class MutationInterface(Selection):
+    r'''The Abjad mutators defined against a single component.
+    '''
+
+    pass
