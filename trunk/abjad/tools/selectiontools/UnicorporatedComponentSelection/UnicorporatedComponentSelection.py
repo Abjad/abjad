623fbd858d945aa16cf1ed8ffe6efec44ba71d1e
Trevor Bača <trevorbaca@gmail.com>
2013-08-14 11:25:28 +0000

    Added UnicorporatedComponentSelection to selectiontools.
    The class is currently a stub.
    
    The class will be used as the return type of many or most maker functions:
    
        * leaftools.make_leaves()
        * leaftools.make_leaves_from_talea()
        * leaftools.make_tied_leaf()
        * measuretools.make_measures_with_full_measure_spacing_skips()
        * notetools.make_accelerating_notes_with_lilypond_multipliers()
        * notetools.make_notes()
        * notetools.make_notes_with_multiplied_durations()
        * notetools.make_percussion_note()
        * notetools.make_quarter_notes_with_lilypond_duration_multiplier()
        * notetools.make_repeated_notes()
        * notetools.make_repeated_notes_from_time_signature()
        * notetools.make_repeated_notes_from_time_signatures()
        * notetools.make_repeated_notes_with_shorter_notes_at_end()
        * resttools.make_multimeasure_rests()
        * resttools.make_repeated_rests_from_time_signatures()
        * resttools.make_rests()
        * skiptools.make_repeated_skips_from_time_signatures()
        * skiptools.make_skips_with_multiplied_durations()
    
    These functions will probably just continue to return Abjad containers:
    
        * scoretools.make_empty_piano_score()
        * scoretools.make_piano_score_from_leaves()
        * scoretools.make_piano_sketch_score_from_leaves()
    
    The funcitons in the first list all produce counttime components.
    
    So UnicorporatedCounttimeComponentSelection might be an even
    more accurate name for this new class.

---
+++ trunk/abjad/tools/selectiontools/UnicorporatedComponentSelection/UnicorporatedComponentSelection.py
@@ -0,0 +1,9 @@
+from abjad.tools.selectiontools.FreeComponentSelection \
+    import FreeComponentSelection
+
+
+class UnicorporatedComponentSelection(FreeComponentSelection):
+    '''A selection of components not yet incorporated in any score.
+    '''
+
+    pass
