3a7d61bdc8d5cec9207ef7726eb7a218222a5742
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2013-08-15 22:07:18 +0000

    Removed all importtools functions.
    Use importtools.ImportManager instead.
    
    IGNORE: Does not need to be noted in 2.13 change log.

--- trunk/abjad/tools/importtools/__init__.py
+++ trunk/abjad/tools/importtools/__init__.py
@@ -1,5 +1,4 @@
 # -*- encoding: utf-8 -*-
 from ImportManager import ImportManager
-from import_structured_package import import_structured_package
 
 _documentation_section = 'internals'
--- trunk/abjad/tools/importtools/remove_modules_from_namespace.py
+++
@@ -1,8 +0,0 @@
-# -*- encoding: utf-8 -*-
-import types
-
-
-def remove_modules_from_namespace(namespace):
-    for key, value in namespace.items():
-        if isinstance(value, types.ModuleType) and not key.startswith('_'):
-            del(namespace[key])
