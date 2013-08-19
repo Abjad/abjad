d57a17233e284d9d649038ebf3b02cdd9a931d3d
Trevor Bača <trevorbaca@gmail.com>
2013-08-19 23:02:49 +0000

    Migrated split tests.

--- trunk/abjad/tools/componenttools/test/test_componenttools_split.py
+++ trunk/abjad/tools/componenttools/test/test_componenttools_split.py
@@ -1,4 +1,5 @@
 # -*- encoding: utf-8 -*-
+import py
 from abjad import *
 
 
@@ -1497,3 +1498,870 @@ def test_componenttools_split_25():
         )
 
     assert select(staff).is_well_formed()
+
+
+def test_componenttools_split_26():
+    r'''Split tuplet in score and do not fracture spanners.
+    '''
+
+    voice = Voice()
+    voice.append(Tuplet((2, 3), "c'8 d'8 e'8"))
+    voice.append(Tuplet((2, 3), "f'8 g'8 a'8"))
+    beam = spannertools.BeamSpanner(voice[:])
+
+    componenttools.split(
+        voice[1:2],
+        [Duration(1, 12)],
+        fracture_spanners=False,
+        )
+
+    assert testtools.compare(
+        voice,
+        r'''
+        \new Voice {
+            \times 2/3 {
+                c'8 [
+                d'8
+                e'8
+            }
+            \times 2/3 {
+                f'8
+            }
+            \times 2/3 {
+                g'8
+                a'8 ]
+            }
+        }
+        '''
+        )
+
+    assert select(voice).is_well_formed()
+
+
+def test_componenttools_split_27():
+    r'''Split in-score measure with power-of-two denominator and 
+    do not fracture spanners.
+    '''
+
+    voice = Voice()
+    voice.append(Measure((3, 8), "c'8 d'8 e'8"))
+    voice.append(Measure((3, 8), "f'8 g'8 a'8"))
+    beam = spannertools.BeamSpanner(voice[:])
+
+    componenttools.split(
+        voice[1:2], 
+        [Duration(1, 8)], 
+        fracture_spanners=False,
+        )
+
+    assert testtools.compare(
+        voice,
+        r'''
+        \new Voice {
+            {
+                \time 3/8
+                c'8 [
+                d'8
+                e'8
+            }
+            {
+                \time 1/8
+                f'8
+            }
+            {
+                \time 2/8
+                g'8
+                a'8 ]
+            }
+        }
+        '''
+        )
+
+    assert select(voice).is_well_formed()
+
+
+def test_componenttools_split_28():
+    r'''Split in-score measure without power-of-two denominator 
+    and do not frature spanners.
+    '''
+
+    voice = Voice()
+    voice.append(Measure((3, 9), "c'8 d'8 e'8"))
+    voice.append(Measure((3, 9), "f'8 g'8 a'8"))
+    beam = spannertools.BeamSpanner(voice[:])
+
+    componenttools.split(
+        voice[1:2], 
+        [Duration(1, 9)], 
+        fracture_spanners=False,
+        )
+
+    assert testtools.compare(
+        voice,
+        r'''
+        \new Voice {
+            {
+                \time 3/9
+                \scaleDurations #'(8 . 9) {
+                    c'8 [
+                    d'8
+                    e'8
+                }
+            }
+            {
+                \time 1/9
+                \scaleDurations #'(8 . 9) {
+                    f'8
+                }
+            }
+            {
+                \time 2/9
+                \scaleDurations #'(8 . 9) {
+                    g'8
+                    a'8 ]
+                }
+            }
+        }
+        '''
+        )
+
+    assert select(voice).is_well_formed()
+
+
+def test_componenttools_split_29():
+    r'''A single container can be index split in two by the middle; no parent.
+    '''
+
+    voice = Voice("c'8 d'8 e'8 f'8")
+
+    result = componenttools.split(
+        [voice], 
+        [Duration(1, 4)], 
+        fracture_spanners=False,
+        )
+
+    assert select(voice).is_well_formed()
+
+    voice_1 = result[0][0]
+    voice_2 = result[1][0]
+
+    assert testtools.compare(
+        voice_1,
+        r'''
+        \new Voice {
+            c'8
+            d'8
+        }
+        '''
+        )
+
+    assert select(voice_1).is_well_formed()
+
+    assert testtools.compare(
+        voice_2,
+        r'''
+        \new Voice {
+            e'8
+            f'8
+        }
+        '''
+        )
+
+    assert select(voice_2).is_well_formed()
+
+
+def test_componenttools_split_30():
+    r'''Split voice at negative index.
+    '''
+
+    staff = Staff([Voice("c'8 d'8 e'8 f'8")])
+    voice = staff[0]
+
+    result = componenttools.split(
+        [voice], 
+        #-2, 
+        [Duration(1, 4)],
+        fracture_spanners=False,
+        )
+
+    left = result[0][0]
+    right = result[1][0]
+
+    assert testtools.compare(
+        left,
+        r'''
+        \new Voice {
+            c'8
+            d'8
+        }
+        '''
+        )
+
+    assert testtools.compare(
+        right,
+        r'''
+        \new Voice {
+            e'8
+            f'8
+        }
+        '''
+        )
+
+    assert testtools.compare(
+        voice,
+        r'''
+        \new Voice {
+        }
+        '''
+        )
+
+    assert testtools.compare(
+        staff,
+        r'''
+        \new Staff {
+            \new Voice {
+                c'8
+                d'8
+            }
+            \new Voice {
+                e'8
+                f'8
+            }
+        }
+        '''
+        )
+
+    assert select(staff).is_well_formed()
+
+
+def test_componenttools_split_31():
+    r'''Slpit container in score and do not fracture spanners.
+    '''
+
+    staff = Staff([Container("c'8 d'8 e'8 f'8")])
+    voice = staff[0]
+    spannertools.BeamSpanner(voice)
+
+    result = componenttools.split(
+        [voice], 
+        #2, 
+        [Duration(1, 4)],
+        fracture_spanners=False,
+        )
+
+    left = result[0][0]
+    right = result[1][0]
+
+    assert testtools.compare(
+        staff,
+        r'''
+        \new Staff {
+            {
+                c'8 [
+                d'8
+            }
+            {
+                e'8
+                f'8 ]
+            }
+        }
+        '''
+        )
+
+    assert testtools.compare(
+        left,
+        r'''
+        {
+            c'8 [
+            d'8
+        }
+        '''
+        )
+
+    assert testtools.compare(
+        right,
+        r'''
+        {
+            e'8
+            f'8 ]
+        }
+        '''
+        )
+
+    assert testtools.compare(
+        voice,
+        r'''
+        {
+        }
+        '''
+        )
+
+    assert testtools.compare(
+        staff,
+        r'''
+        \new Staff {
+            {
+                c'8 [
+                d'8
+            }
+            {
+                e'8
+                f'8 ]
+            }
+        }
+        '''
+        )
+
+    assert select(staff).is_well_formed()
+
+
+def test_componenttools_split_32():
+    r'''Split tuplet in score and do not fracture spanners.
+    '''
+
+    tuplet = Tuplet((4, 5), "c'8 c'8 c'8 c'8 c'8")
+    voice = Voice([tuplet])
+    staff = Staff([voice])
+    spannertools.BeamSpanner(tuplet)
+
+    result = componenttools.split(
+        [tuplet], 
+        #2, 
+        [Duration(1, 5)],
+        fracture_spanners=False,
+        )
+
+    left = result[0][0]
+    right = result[1][0]
+
+    assert testtools.compare(
+        staff,
+        r'''
+        \new Staff {
+            \new Voice {
+                \times 4/5 {
+                    c'8 [
+                    c'8
+                }
+                \times 4/5 {
+                    c'8
+                    c'8
+                    c'8 ]
+                }
+            }
+        }
+        '''
+        )
+
+    assert testtools.compare(
+        left,
+        r'''
+        \times 4/5 {
+            c'8 [
+            c'8
+        }
+        '''
+        )
+
+    assert testtools.compare(
+        right,
+        r'''
+        \times 4/5 {
+            c'8
+            c'8
+            c'8 ]
+        }
+        '''
+        )
+
+    assert testtools.compare(
+        tuplet,
+        r'''
+        \times 4/5 {
+        }
+        '''
+        )
+
+    assert testtools.compare(
+        voice,
+        r'''
+        \new Voice {
+            \times 4/5 {
+                c'8 [
+                c'8
+            }
+            \times 4/5 {
+                c'8
+                c'8
+                c'8 ]
+            }
+        }
+        '''
+        )
+
+    assert testtools.compare(
+        staff,
+        r'''
+        \new Staff {
+            \new Voice {
+                \times 4/5 {
+                    c'8 [
+                    c'8
+                }
+                \times 4/5 {
+                    c'8
+                    c'8
+                    c'8 ]
+                }
+            }
+        }
+        '''
+        )
+
+    assert select(staff).is_well_formed()
+
+
+def test_componenttools_split_33():
+    r'''Split triplet, and fracture spanners.
+    '''
+
+    voice = Voice()
+    voice.append(Tuplet((2, 3), "c'8 d'8 e'8"))
+    voice.append(Tuplet((2, 3), "f'8 g'8 a'8"))
+    spannertools.BeamSpanner(voice[:])
+    tuplet = voice[1]
+
+    assert testtools.compare(
+        voice,
+        r'''
+        \new Voice {
+            \times 2/3 {
+                c'8 [
+                d'8
+                e'8
+            }
+            \times 2/3 {
+                f'8
+                g'8
+                a'8 ]
+            }
+        }
+        '''
+        )
+
+    result = componenttools.split(
+        [tuplet], 
+        #1, 
+        [Duration(1, 12)],
+        fracture_spanners=True,
+        )
+
+    left = result[0][0]
+    right = result[1][0]
+
+    assert testtools.compare(
+        left,
+        r'''
+        \times 2/3 {
+            f'8 ]
+        }
+        '''
+        )
+
+    assert testtools.compare(
+        right,
+        r'''
+        \times 2/3 {
+            g'8 [
+            a'8 ]
+        }
+        '''
+        )
+
+    assert tuplet.lilypond_format == '\\times 2/3 {\n}'
+
+    assert testtools.compare(
+        voice,
+        r'''
+        \new Voice {
+            \times 2/3 {
+                c'8 [
+                d'8
+                e'8
+            }
+            \times 2/3 {
+                f'8 ]
+            }
+            \times 2/3 {
+                g'8 [
+                a'8 ]
+            }
+        }
+        '''
+        )
+
+    assert select(voice).is_well_formed()
+
+
+def test_componenttools_split_34():
+    r'''Split measure with power-of-two time signature denominator.
+    Fracture spanners.
+    '''
+
+    voice = Voice()
+    voice.append(Measure((3, 8), "c'8 d'8 e'8"))
+    voice.append(Measure((3, 8), "f'8 g'8 a'8"))
+    spannertools.BeamSpanner(voice[:])
+    measure = voice[1]
+
+    assert testtools.compare(
+        voice,
+        r'''
+        \new Voice {
+            {
+                \time 3/8
+                c'8 [
+                d'8
+                e'8
+            }
+            {
+                f'8
+                g'8
+                a'8 ]
+            }
+        }
+        '''
+        )
+
+    result = componenttools.split(
+        [measure], 
+        #1, 
+        [Duration(1, 8)],
+        fracture_spanners=True,
+        )
+
+    left = result[0][0]
+    right = result[1][0]
+
+    assert testtools.compare(
+        left,
+        r'''
+        {
+            \time 1/8
+            f'8 ]
+        }
+        '''
+        )
+
+    assert testtools.compare(
+        right,
+        r'''
+        {
+            \time 2/8
+            g'8 [
+            a'8 ]
+        }
+        '''
+        )
+
+    assert py.test.raises(UnderfullContainerError, 'measure.lilypond_format')
+
+    assert testtools.compare(
+        voice,
+        r'''
+        \new Voice {
+            {
+                \time 3/8
+                c'8 [
+                d'8
+                e'8
+            }
+            {
+                \time 1/8
+                f'8 ]
+            }
+            {
+                \time 2/8
+                g'8 [
+                a'8 ]
+            }
+        }
+        '''
+        )
+
+    assert select(voice).is_well_formed()
+
+
+def test_componenttools_split_35():
+    r'''Split measure without power-of-two time signature denominator.
+    Fracture spanners.
+    '''
+
+    voice = Voice()
+    voice.append(Measure((3, 9), "c'8 d'8 e'8"))
+    voice.append(Measure((3, 9), "f'8 g'8 a'8"))
+    spannertools.BeamSpanner(voice[:])
+    measure = voice[1]
+
+    assert testtools.compare(
+        voice,
+        r'''
+        \new Voice {
+            {
+                \time 3/9
+                \scaleDurations #'(8 . 9) {
+                    c'8 [
+                    d'8
+                    e'8
+                }
+            }
+            {
+                \scaleDurations #'(8 . 9) {
+                    f'8
+                    g'8
+                    a'8 ]
+                }
+            }
+        }
+        '''
+        )
+
+    result = componenttools.split(
+        [measure], 
+        #1, 
+        [Duration(1, 9)],
+        fracture_spanners=True,
+        )
+
+    left = result[0][0]
+    right = result[1][0]
+
+    assert testtools.compare(
+        left,
+        r'''
+        {
+            \time 1/9
+            \scaleDurations #'(8 . 9) {
+                f'8 ]
+            }
+        }
+        '''
+        )
+
+    assert testtools.compare(
+        right,
+        r'''
+        {
+            \time 2/9
+            \scaleDurations #'(8 . 9) {
+                g'8 [
+                a'8 ]
+            }
+        }
+        '''
+        )
+
+    assert py.test.raises(UnderfullContainerError, 'measure.lilypond_format')
+
+    assert testtools.compare(
+        voice,
+        r'''
+        \new Voice {
+            {
+                \time 3/9
+                \scaleDurations #'(8 . 9) {
+                    c'8 [
+                    d'8
+                    e'8
+                }
+            }
+            {
+                \time 1/9
+                \scaleDurations #'(8 . 9) {
+                    f'8 ]
+                }
+            }
+            {
+                \time 2/9
+                \scaleDurations #'(8 . 9) {
+                    g'8 [
+                    a'8 ]
+                }
+            }
+        }
+        '''
+        )
+
+    assert select(voice).is_well_formed()
+
+
+def test_componenttools_split_36():
+    r'''Split voice outside of score.
+    Fracture spanners.
+    '''
+
+    voice = Voice("c'8 d'8 e'8 f'8")
+    spannertools.BeamSpanner(voice[:])
+
+    assert testtools.compare(
+        voice,
+        r'''
+        \new Voice {
+            c'8 [
+            d'8
+            e'8
+            f'8 ]
+        }
+        '''
+        )
+
+    result = componenttools.split(
+        [voice], 
+        #2, 
+        [Duration(1, 4)],
+        fracture_spanners=True,
+        )
+
+    left = result[0][0]
+    right = result[1][0]
+
+    assert testtools.compare(
+        left,
+        r'''
+        \new Voice {
+            c'8 [
+            d'8 ]
+        }
+        '''
+        )
+
+    assert testtools.compare(
+        right,
+        r'''
+        \new Voice {
+            e'8 [
+            f'8 ]
+        }
+        '''
+        )
+
+    assert testtools.compare(
+        voice,
+        r'''
+        \new Voice {
+        }
+        '''
+        )
+
+
+def test_componenttools_split_37():
+    r'''Split measure in score and fracture spanners.
+    '''
+
+    staff = Staff()
+    staff.append(Measure((2, 8), "c'8 d'8"))
+    staff.append(Measure((2, 8), "e'8 f'8"))
+    spannertools.BeamSpanner(staff[0])
+    spannertools.BeamSpanner(staff[1])
+    slur = spannertools.SlurSpanner(staff.select_leaves())
+
+    assert testtools.compare(
+        staff,
+        r'''
+        \new Staff {
+            {
+                \time 2/8
+                c'8 [ (
+                d'8 ]
+            }
+            {
+                e'8 [
+                f'8 ] )
+            }
+        }
+        '''
+        )
+
+    result = componenttools.split(
+        staff[:1], 
+        #1, 
+        [Duration(1, 8)],
+        fracture_spanners=True,
+        )
+
+    left = result[0][0]
+    right = result[1][0]
+
+    assert testtools.compare(
+        staff,
+        r'''
+        \new Staff {
+            {
+                \time 1/8
+                c'8 [ ] ( )
+            }
+            {
+                d'8 [ ] (
+            }
+            {
+                \time 2/8
+                e'8 [
+                f'8 ] )
+            }
+        }
+        '''
+        )
+
+    assert select(staff).is_well_formed()
+
+
+
+
+
+def test_componenttools_split_38():
+    r'''Split in-score measure with power-of-two time signature denominator.
+    Fractured spanners but do not tie over split locus.
+    Measure contents necessitate denominator change.
+    '''
+
+    staff = Staff([Measure((3, 8), "c'8. d'8.")])
+    spannertools.BeamSpanner(staff[0])
+    spannertools.SlurSpanner(staff.select_leaves())
+
+    assert testtools.compare(
+        staff,
+        r'''
+        \new Staff {
+            {
+                \time 3/8
+                c'8. [ (
+                d'8. ] )
+            }
+        }
+        '''
+        )
+
+    halves = componenttools.split(
+        staff[:1], 
+        #1, 
+        [Duration(3, 16)],
+        fracture_spanners=True,
+        )
+
+    assert testtools.compare(
+        staff,
+        r'''
+        \new Staff {
+            {
+                \time 3/16
+                c'8. [ ] ( )
+            }
+            {
+                d'8. [ ] ( )
+            }
+        }
+        '''
+        )
+
+    assert select(staff).is_well_formed()
+    assert len(halves) == 2
