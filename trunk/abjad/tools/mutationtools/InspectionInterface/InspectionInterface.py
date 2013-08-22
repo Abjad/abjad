67371733973245ef2abd09704cc83a3eb1937985
Trevor Bača <trevorbaca@gmail.com>
2013-08-22 04:19:13 +0000

    Deprecating FreeComponentSelection.
    Removed remaining FreeComponentSelection methods.
    
    Use inspect() instead.

--- trunk/abjad/tools/mutationtools/InspectionInterface/InspectionInterface.py
+++ trunk/abjad/tools/mutationtools/InspectionInterface/InspectionInterface.py
@@ -49,6 +49,43 @@ class InspectionInterface(AbjadObject):
         annotation_value = with_correct_name[0].value
         return annotation_value
 
+    def get_badly_formed_components(self):
+        r'''Gets badly formed components in component.
+
+        ..  container:: example
+
+            ::
+
+                >>> staff = Staff("c'8 d'8 e'8 f'8")
+                >>> staff[1].written_duration = Duration(1, 4)
+                >>> spannertools.BeamSpanner(staff[:])
+                BeamSpanner(c'8, d'4, e'8, f'8)
+
+            ..  doctest::
+
+                >>> f(staff)
+                \new Staff {
+                    c'8 [
+                    d'4
+                    e'8
+                    f'8 ]
+                }
+
+            ::
+
+                >>> inspect(staff).get_badly_formed_components()
+                [Note("d'4")]
+
+            (Beamed quarter notes are not well formed.)
+
+        Returns list.
+        '''
+        from abjad.tools import wellformednesstools
+        badly_formed_components = []
+        for checker in wellformednesstools.Check.list_checks():
+            badly_formed_components.extend(checker.violators(self._component))
+        return badly_formed_components
+
     def get_duration(self, in_seconds=False):
         r'''Gets duration of component.
 
@@ -341,6 +378,129 @@ class InspectionInterface(AbjadObject):
                     continue
             results.append(checker.check(self._component))
         return all(results)
+
+    def report_modifications(self):
+        r'''Report modifications of component.
+
+        ..  container:: example
+
+            **Example.** Report modifications of container in selection:
+
+            ::
+
+                >>> container = Container("c'8 d'8 e'8 f'8")
+                >>> container.override.note_head.color = 'red'
+                >>> container.override.note_head.style = 'harmonic'
+                >>> show(container) # doctest: +SKIP
+
+            ..  doctest::
+
+                >>> f(container)
+                {
+                    \override NoteHead #'color = #red
+                    \override NoteHead #'style = #'harmonic
+                    c'8
+                    d'8
+                    e'8
+                    f'8
+                    \revert NoteHead #'color
+                    \revert NoteHead #'style
+                }
+
+            ::
+
+                >>> report = inspect(container).report_modifications()
+
+            ::
+
+                >>> print report
+                {
+                    \override NoteHead #'color = #red
+                    \override NoteHead #'style = #'harmonic
+                    %%% 4 components omitted %%%
+                    \revert NoteHead #'color
+                    \revert NoteHead #'style
+                }
+
+        Returns string.
+        '''
+        from abjad.tools import containertools
+        from abjad.tools import formattools
+        component = self._component
+        format_contributions = formattools.get_all_format_contributions(
+            component)
+        result = []
+        result.extend(component._get_format_contributions_for_slot(
+            'before', format_contributions))
+        result.extend(component._get_format_contributions_for_slot(
+            'open brackets', format_contributions))
+        result.extend(component._get_format_contributions_for_slot(
+            'opening', format_contributions))
+        result.append('\t%%%%%% %s components omitted %%%%%%' % len(component))
+        result.extend(component._get_format_contributions_for_slot(
+            'closing', format_contributions))
+        result.extend(component._get_format_contributions_for_slot(
+            'close brackets', format_contributions))
+        result.extend(component._get_format_contributions_for_slot(
+            'after', format_contributions))
+        result = '\n'.join(result)
+        return result
+
+    def tabulate_well_formedness_violations(self):
+        r'''Tabulates well-formedness violations in component.
+
+        ..  container:: example
+
+            ::
+
+                >>> staff = Staff("c'8 d'8 e'8 f'8")
+                >>> staff[1].written_duration = Duration(1, 4)
+                >>> spannertools.BeamSpanner(staff[:])
+                BeamSpanner(c'8, d'4, e'8, f'8)
+
+            ..  doctest::
+
+                >>> f(staff)
+                \new Staff {
+                    c'8 [
+                    d'4
+                    e'8
+                    f'8 ]
+                }
+
+            ::
+
+                >>> result = inspect(staff).tabulate_well_formedness_violations()
+
+            ::
+
+                >>> print result
+                1 /    4 beamed quarter note
+                0 /    1 discontiguous spanner
+                0 /    5 duplicate id
+                0 /    1 empty container
+                0 /    0 intermarked hairpin
+                0 /    0 misdurated measure
+                0 /    0 misfilled measure
+                0 /    4 mispitched tie
+                0 /    4 misrepresented flag
+                0 /    5 missing parent
+                0 /    0 nested measure
+                0 /    1 overlapping beam
+                0 /    0 overlapping glissando
+                0 /    0 overlapping octavation
+                0 /    0 short hairpin
+
+            Beamed quarter notes are not well formed.
+
+        Returns string.
+        '''
+        from abjad.tools import wellformednesstools
+        lines = []
+        for checker in wellformednesstools.Check.list_checks():
+            lines.append(checker.report(self._component))
+        result = '\n'.join(lines)
+        return result
 
 
 def inspect(component):
---
+++ trunk/abjad/tools/mutationtools/InspectionInterface/test/test_InspectionInterface_report_modifications.py
@@ -0,0 +1,88 @@
+# -*- encoding: utf-8 -*-
+from abjad import *
+import py.test
+
+
+def test_InspectionInterface_report_modifications_01():
+
+    t = Voice("c'8 d'8 e'8 f'8")
+    marktools.LilyPondComment('Example voice', 'before')(t)
+    t.override.note_head.color = 'red'
+    marktools.LilyPondCommandMark("#(set-accidental-style 'forget)")(t)
+    beam = spannertools.BeamSpanner(t[:])
+    beam.override.beam.thickness = 3
+
+    assert testtools.compare(
+        t,
+        r'''
+        % Example voice
+        \new Voice \with {
+            \override NoteHead #'color = #red
+        } {
+            #(set-accidental-style 'forget)
+            \override Beam #'thickness = #3
+            c'8 [
+            d'8
+            e'8
+            f'8 ]
+            \revert Beam #'thickness
+        }
+        '''
+        )
+
+    result = inspect(t).report_modifications()
+
+    assert testtools.compare(
+        result,
+        r'''
+        % Example voice
+        \new Voice \with {
+            \override NoteHead #'color = #red
+        } {
+            #(set-accidental-style 'forget)
+            %%% 4 components omitted %%%
+        }
+        '''
+        )
+
+
+def test_InspectionInterface_report_modifications_02():
+
+    t = tuplettools.FixedDurationTuplet(Duration(2, 8), "c'8 d'8 e'8")
+    marktools.LilyPondComment('Example tuplet', 'before')(t)
+    t.override.note_head.color = 'red'
+    marktools.LilyPondCommandMark("#(set-accidental-style 'forget)")(t)
+    beam = spannertools.BeamSpanner(t[:])
+    beam.override.beam.thickness = 3
+
+    assert testtools.compare(
+        t,
+        r'''
+        % Example tuplet
+        \override NoteHead #'color = #red
+        \times 2/3 {
+            #(set-accidental-style 'forget)
+            \override Beam #'thickness = #3
+            c'8 [
+            d'8
+            e'8 ]
+            \revert Beam #'thickness
+        }
+        \revert NoteHead #'color
+        '''
+        )
+
+    result = inspect(t).report_modifications()
+
+    assert testtools.compare(
+        result,
+        r'''
+        % Example tuplet
+        \override NoteHead #'color = #red
+        \times 2/3 {
+            #(set-accidental-style 'forget)
+            %%% 3 components omitted %%%
+        }
+        \revert NoteHead #'color
+        '''
+        )
