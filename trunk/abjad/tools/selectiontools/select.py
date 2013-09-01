33b3cf7c7777f242cfa7385e8437db021b6e745b
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2013-09-01 10:49:51 +0000

    selectiontools.select() can now select the components in a Spanner.

--- trunk/abjad/tools/selectiontools/select.py
+++ trunk/abjad/tools/selectiontools/select.py
@@ -8,15 +8,19 @@ def select(expr=None, contiguous=False):
     '''
     from abjad.tools import componenttools
     from abjad.tools import selectiontools
+    from abjad.tools import spannertools
     Selection = selectiontools.Selection
     if contiguous:
-        if isinstance(expr, list):
+        if isinstance(expr, (list, tuple)):
             assert Selection._all_are_contiguous_components_in_same_logical_voice(expr)
         return selectiontools.ContiguousSelection(expr)
     elif isinstance(expr, componenttools.Component):
         return selectiontools.Selection(expr)
     elif hasattr(expr, '_music'):
         music = expr._music
+        return selectiontools.Selection(music)
+    elif isinstance(expr, spannertools.Spanner):
+        music = expr._components
         return selectiontools.Selection(music)
     elif expr is None:
         return selectiontools.Selection()
