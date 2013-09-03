be5d05003b2c146a98cdf6c032d22769115ba7f2
Trevor Bača <trevorbaca@gmail.com>
2013-09-03 13:23:08 +0000

    Removed leaftools function.
        * leaftools.force_leaf_written_duration()
    
    Just set leaf.lilypond_duration_multiplier values by hand.

--- trunk/abjad/tools/leaftools/Leaf/test/test_Leaf__multiplied_duration.py
+++ trunk/abjad/tools/leaftools/Leaf/test/test_Leaf__multiplied_duration.py
@@ -5,6 +5,7 @@ from abjad import *
 def test_Leaf__multiplied_duration_01():
     r'''Mulplied leaf duration == written * multiplier.
     '''
+
     note = Note("c'4")
     note.lilypond_duration_multiplier = Duration(1, 2)
     assert note._multiplied_duration == Duration(1, 8)
@@ -12,7 +13,9 @@ def test_Leaf__multiplied_duration_01():
 
 def test_Leaf__multiplied_duration_02():
     r'''Mulplied leaf duration == written,
-        when multiplier is None.'''
+    when multiplier is none.
+    '''
+
     note = Note("c'4")
     assert note._multiplied_duration == Duration(1, 4)
 
@@ -20,14 +23,18 @@ def test_Leaf__multiplied_duration_02():
 def test_Leaf__multiplied_duration_03():
     r'''Mulplied leaf duration can be set and then unset.
     '''
+
     note = Note("c'4")
-    leaftools.force_leaf_written_duration(
-        note, Duration(3, 8))
+    note.written_duration = Duration(3, 8)
+    note.lilypond_duration_multiplier = Multiplier(2, 3)
+
     assert note.written_duration == Duration(3, 8)
     assert note.lilypond_duration_multiplier == Duration(2, 3)
     assert note._multiplied_duration == Duration(1, 4)
-    leaftools.force_leaf_written_duration(
-        note, Duration(1, 4))
+
+    note.written_duration = Duration(1, 4)
+    note.lilypond_duration_multiplier = None
+
     assert note.written_duration == Duration(1, 4)
     assert note.lilypond_duration_multiplier is None
     assert note._multiplied_duration == Duration(1, 4)
