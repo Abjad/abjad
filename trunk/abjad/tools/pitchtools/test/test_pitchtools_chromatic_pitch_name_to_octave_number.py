7d7aa22b0d99e3e03287e799c54d6d76d94602bf
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2013-09-04 16:30:37 +0000

    Removed two pitchtools functions:
        pitchtools.chromatic_pitch_name_to_octave_number()
        pitchtools.chromatic_pitch_number_to_octave_number()
    
    Use the following recipes instead:
    
        int(pitchtools.OctaveIndication.from_pitch_name(pitch_name))
        int(pitchtools.OctaveIndication.from_pitch_number(pitch_number))
    
    CHANGE: Should be noted in 2.13 change log.

--- trunk/abjad/tools/pitchtools/test/test_pitchtools_chromatic_pitch_name_to_octave_number.py
+++
@@ -1,9 +0,0 @@
-# -*- encoding: utf-8 -*-
-from abjad import *
-
-
-def test_pitchtools_chromatic_pitch_name_to_octave_number_01():
-
-    assert pitchtools.chromatic_pitch_name_to_octave_number("cs'") == 4
-    assert pitchtools.chromatic_pitch_name_to_octave_number('cs') == 3
-    assert pitchtools.chromatic_pitch_name_to_octave_number('cs,') == 2
