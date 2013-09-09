a6df643d647106a14e1aa9eb7a239b4bd9f04364
Trevor Bača <trevorbaca@gmail.com>
2013-09-09 14:07:50 +0000

    Streamlined two iotools functions.
    NOTE: the standard library function ...
    
        os.path.splitext()
    
    ... separates a path from any final extension (like .ly or .pdf or similar).

--- trunk/abjad/tools/iotools/save_last_ly_as.py
+++ trunk/abjad/tools/iotools/save_last_ly_as.py
@@ -15,17 +15,8 @@ def save_last_ly_as(file_name):
     from abjad.tools import iotools
     ABJADOUTPUT = abjad_configuration['abjad_output']
     last_output_file_name = iotools.get_last_output_file_name()
-    if last_output_file_name.endswith('.ly'):
-        basename = last_output_file_name[:-3]
-    elif last_output_file_name.endswith('.pdf'):
-        basename = last_output_file_name[:-4]
-    elif last_output_file_name.endswith('.mid'):
-        basename = last_output_file_name[:-4]
-    elif last_output_file_name.endswith('.midi'):
-        basename = last_output_file_name[:-5]
-    else:
-        raise Exception(last_output_file_name)
-    last_ly = basename + '.ly'
+    without_extension, extension = os.path.splitext(last_output_file_name)
+    last_ly = without_extension + '.ly'
     last_ly_full_name = os.path.join(ABJADOUTPUT, last_ly)
     old = open(last_ly_full_name, 'r')
     new = open(file_name, 'w')
