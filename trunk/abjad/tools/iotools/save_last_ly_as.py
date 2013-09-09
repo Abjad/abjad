895dfbc4a64acd48c395bc903ecf05d5ed20fa09
Trevor Bača <trevorbaca@gmail.com>
2013-09-09 13:48:14 +0000

    Fixed bug in iotools.save_last_pdf_as() and iotools.save_last_ly_as().

--- trunk/abjad/tools/iotools/save_last_ly_as.py
+++ trunk/abjad/tools/iotools/save_last_ly_as.py
@@ -14,7 +14,18 @@ def save_last_ly_as(file_name):
     from abjad import abjad_configuration
     from abjad.tools import iotools
     ABJADOUTPUT = abjad_configuration['abjad_output']
-    last_ly = iotools.get_last_output_file_name()
+    last_output_file_name = iotools.get_last_output_file_name()
+    if last_output_file_name.endswith('.ly'):
+        basename = last_output_file_name[:-3]
+    elif last_output_file_name.endswith('.pdf'):
+        basename = last_output_file_name[:-4]
+    elif last_output_file_name.endswith('.mid'):
+        basename = last_output_file_name[:-4]
+    elif last_output_file_name.endswith('.midi'):
+        basename = last_output_file_name[:-5]
+    else:
+        raise Exception(last_output_file_name)
+    last_ly = basename + '.ly'
     last_ly_full_name = os.path.join(ABJADOUTPUT, last_ly)
     old = open(last_ly_full_name, 'r')
     new = open(file_name, 'w')
--- trunk/abjad/tools/iotools/save_last_pdf_as.py
+++ trunk/abjad/tools/iotools/save_last_pdf_as.py
@@ -14,8 +14,18 @@ def save_last_pdf_as(file_name):
     from abjad import abjad_configuration
     from abjad.tools import iotools
     ABJADOUTPUT = abjad_configuration['abjad_output']
-    last_ly = iotools.get_last_output_file_name()
-    last_pdf = last_ly[:-3] + '.pdf'
+    last_output_file_name = iotools.get_last_output_file_name()
+    if last_output_file_name.endswith('.ly'):
+        basename = last_output_file_name[:-3]
+    elif last_output_file_name.endswith('.pdf'):
+        basename = last_output_file_name[:-4]
+    elif last_output_file_name.endswith('.mid'):
+        basename = last_output_file_name[:-4]
+    elif last_output_file_name.endswith('.midi'):
+        basename = last_output_file_name[:-5]
+    else:
+        raise Exception(last_output_file_name)
+    last_pdf = basename + '.pdf'
     last_pdf_full_name = os.path.join(ABJADOUTPUT, last_pdf)
     old = open(last_pdf_full_name, 'r')
     new = open(file_name, 'w')
