5c84ab507cd9beccdf1719c05547855315cc0aae
Trevor Bača <trevorbaca@gmail.com>
2013-09-15 19:52:49 +0000

    Extended two iotools functions.
        * iotools.run_lilypond()
        * iotools.log_render_lilypond_input()
    
    The functions now implement an output_directory_path=None keyword.
    
    Output writes to the output directory specified when the keyword is set.
    
    Output writes to the Abjad output directory when the keyword is none.

--- trunk/abjad/tools/iotools/log_render_lilypond_input.py
+++ trunk/abjad/tools/iotools/log_render_lilypond_input.py
@@ -4,23 +4,36 @@ import time
 from abjad.tools import documentationtools
 
 
-def log_render_lilypond_input(expr, tagline=False, docs=False):
-    r'''Write both .ly and .pdf files to the ``abjad_output`` directory.
+def log_render_lilypond_input(
+    expr, 
+    output_directory_path=None, 
+    output_file_name_root=None,
+    tagline=False, 
+    docs=False,
+    ):
+    r'''Writes both .ly and .pdf files to `output_directory`.
+
+    Writes to Abjad output directory when `output_directory` is none.
+
+    Writes to next 4-digit numeric file name 
+    when `output_file_name_root` is none.
+
+    Returns none.
     '''
     from abjad import abjad_configuration
     from abjad.tools import iotools
 
+    # set timing thresholds
     lily_time = 2
     format_time = 2
-
-    # log score
+    # change to output directory
     current_directory = os.path.abspath('.')
-    ABJADOUTPUT = abjad_configuration['abjad_output']
-    iotools.verify_output_directory(ABJADOUTPUT)
-    os.chdir(ABJADOUTPUT)
-    name = iotools.get_next_output_file_name()
+    output_directory_path = \
+        output_directory_path or abjad_configuration['abjad_output']
+    iotools.verify_output_directory(output_directory_path)
+    os.chdir(output_directory_path)
+    name = output_file_name_root or iotools.get_next_output_file_name()
     outfile = open(name, 'w')
-
     # catch Abjad tight loops that result in excessive format time
     start_format_time = time.time()
     if docs:
@@ -31,29 +44,28 @@ def log_render_lilypond_input(expr, tagline=False, docs=False):
     stop_format_time = time.time()
     actual_format_time = int(stop_format_time - start_format_time)
     if format_time <= actual_format_time:
-        print 'Abjad format time equal to %s seconds ...' % actual_format_time
+        message = 'Abjad format time equal to {} seconds ...'
+        message = message.format(actual_format_time)
+        print message
     outfile.write(formatted_lilypond_file)
     outfile.close()
-
     if getattr(lilypond_file, '_is_temporary', False):
-        # TODO: eliminate this exception handler? #
+        # TODO: eliminate this exception handler?
         try:
             music = lilypond_file.score_block.pop()
             delattr(music, '_lilypond_file')
         except (IndexError, AttributeError):
             pass
         del(lilypond_file)
-
     # render
     start_time = time.time()
     iotools.run_lilypond(name, abjad_configuration['lilypond_path'])
     stop_time = time.time()
     actual_lily_time = int(stop_time - start_time)
-
     os.chdir(current_directory)
-
     # catch LilyPond taking a long time to render
     if lily_time <= actual_lily_time:
-        print 'LilyPond processing time equal to %s seconds ...' % actual_lily_time
-
+        message = 'LilyPond processing time equal to {} seconds ...'
+        message = message.format(actual_lily_time)
+        print message
     return name, actual_format_time, actual_lily_time
--- trunk/abjad/tools/iotools/run_lilypond.py
+++ trunk/abjad/tools/iotools/run_lilypond.py
@@ -3,11 +3,14 @@ import os
 
 
 def run_lilypond(lilypond_file_name, lilypond_path):
+    from abjad import abjad_configuration
     from abjad.tools import iotools
+    abjad_output_directory_path = abjad_configuration['abjad_output']
     if not lilypond_path:
         lilypond_path = 'lilypond'
-    command = '{} -dno-point-and-click {} > lily.log 2>&1'
-    command = command.format(lilypond_path, lilypond_file_name)
+    log_file_path = os.path.join(abjad_output_directory_path, 'lily.log')
+    command = '{} -dno-point-and-click {} > {} 2>&1'
+    command = command.format(lilypond_path, lilypond_file_name, log_file_path)
     iotools.spawn_subprocess(command)
     postscript_file_name = lilypond_file_name.replace('.ly', '.ps')
     try:
