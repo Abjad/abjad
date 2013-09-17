6650740439506bcfe6fdff4a117b9fc36a009618
Trevor Bača <trevorbaca@gmail.com>
2013-09-17 19:05:12 +0000

    Removed the term 'exergue' from codebase.
    Use 'build directory' instead.

--- experimental/tools/scoremanagertools/proxies/BuildDirectoryManager/BuildDirectoryManager.py
+++ experimental/tools/scoremanagertools/proxies/BuildDirectoryManager/BuildDirectoryManager.py
@@ -15,12 +15,18 @@ class BuildDirectoryManager(DirectoryProxy):
         score_directory_path = \
             self.configuration.packagesystem_path_to_filesystem_path(
             score_package_path)
-        filesystem_path = os.path.join(score_directory_path, 'exergue')
+        filesystem_path = os.path.join(score_directory_path, 'build')
         DirectoryProxy.__init__(
             self,
             filesystem_path=filesystem_path,
             session=session,
             )
+
+    ### PRIVATE PROPERTIES ###
+
+    @property
+    def _breadcrumb(self):
+        return 'build directory'
 
     ### PRIVATE METHODS ###
 
--- experimental/tools/scoremanagertools/proxies/BuildDirectoryManager/test/test_BuildDirectoryManager.py
+++ experimental/tools/scoremanagertools/proxies/BuildDirectoryManager/test/test_BuildDirectoryManager.py
@@ -12,6 +12,6 @@ def test_BuildDirectoryManager_01():
         manager.configuration.score_manager_tools_directory_path,
         'scorepackages', 
         'red_example_score', 
-        'exergue',
+        'build',
         )
     assert manager._spaced_class_name == 'build directory manager'
