227b25ffd480e833eaee3f5ffc54e3ae00a5b1eb
Trevor Bača <trevorbaca@gmail.com>
2013-09-25 18:39:29 +0000

    Removed ParameterEditorClassNameSelector.

--- experimental/tools/scoremanagertools/selectors/ParameterEditorClassNameSelector/test/test_ParameterEditorClassNameSelector_run.py
+++
@@ -1,9 +0,0 @@
-# -*- encoding: utf-8 -*-
-from experimental import *
-
-
-def test_ParameterEditorClassNameSelector_run_01():
-
-    selector = scoremanagertools.selectors.ParameterEditorClassNameSelector()
-
-    assert selector._run(pending_user_input='trop') == 'TropingSpecifierEditor'
