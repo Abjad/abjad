d33c8b169709d7fe03e383f9599564fc8fc450f0
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2013-10-15 15:16:29 +0000

    Move one pitchtools function:
        pitchtools.set_default_accidental_spelling()
    
        ==>
    
        configurationtools.AbjadConfiguration.set_default_accidental_spelling()
    
    CHANGE: Should be noted in 2.13 change log.

--- trunk/abjad/tools/configurationtools/AbjadConfiguration/AbjadConfiguration.py
+++ trunk/abjad/tools/configurationtools/AbjadConfiguration/AbjadConfiguration.py
@@ -344,6 +344,52 @@ class AbjadConfiguration(Configuration):
             pass
         return dependencies
         
+    @staticmethod
+    def set_default_accidental_spelling(spelling='mixed'):
+        '''Set default accidental spelling to sharps:
+
+        ::
+
+            >>> pitchtools.set_default_accidental_spelling('sharps')
+
+        ::
+
+            >>> [Note(13, (1, 4)), Note(15, (1, 4))]
+            [Note("cs''4"), Note("ds''4")]
+
+        Set default accidental spelling to flats:
+
+        ::
+
+            >>> pitchtools.set_default_accidental_spelling('flats')
+
+        ::
+
+            >>> [Note(13, (1, 4)), Note(15, (1, 4))]
+            [Note("df''4"), Note("ef''4")]
+
+        Set default accidental spelling to mixed:
+
+        ::
+
+            >>> pitchtools.set_default_accidental_spelling()
+
+        ::
+
+            >>> [Note(13, (1, 4)), Note(15, (1, 4))]
+            [Note("cs''4"), Note("ef''4")]
+
+        Mixed is system default.
+
+        Mixed test case must appear last here for doc tests to check correctly.
+
+        Return none.
+        '''
+        from abjad import abjad_configuration
+        if spelling not in ('mixed', 'sharps', 'flats'):
+            raise ValueError
+        abjad_configuration['accidental_spelling'] = spelling
+
     ### PUBLIC PROPERTIES ###
 
     @property
--- trunk/abjad/tools/configurationtools/AbjadConfiguration/test/test_AbjadConfiguration_set_default_accidental_spelling.py
+++ trunk/abjad/tools/configurationtools/AbjadConfiguration/test/test_AbjadConfiguration_set_default_accidental_spelling.py
@@ -2,26 +2,9 @@
 from abjad import *
 
 
-def test_pitchtools_set_default_accidental_spelling_01():
+def test_AbjadConfiguration_set_default_accidental_spelling_01():
 
     staff = Staff([Note(n, (1, 8)) for n in range(12)])
-
-    r'''
-    \new Staff {
-        c'8
-        cs'8
-        d'8
-        ef'8
-        e'8
-        f'8
-        fs'8
-        g'8
-        af'8
-        a'8
-        bf'8
-        b'8
-    }
-    '''
 
     assert testtools.compare(
         staff,
@@ -44,27 +27,10 @@ def test_pitchtools_set_default_accidental_spelling_01():
         )
 
 
-def test_pitchtools_set_default_accidental_spelling_02():
+def test_AbjadConfiguration_set_default_accidental_spelling_02():
 
-    pitchtools.set_default_accidental_spelling('sharps')
+    abjad_configuration.set_default_accidental_spelling('sharps')
     staff = Staff([Note(n, (1, 8)) for n in range(12)])
-
-    r'''
-    \new Staff {
-        c'8
-        cs'8
-        d'8
-        ds'8
-        e'8
-        f'8
-        fs'8
-        g'8
-        gs'8
-        a'8
-        as'8
-        b'8
-    }
-    '''
 
     assert testtools.compare(
         staff,
@@ -86,30 +52,13 @@ def test_pitchtools_set_default_accidental_spelling_02():
         '''
         )
 
-    pitchtools.set_default_accidental_spelling('mixed')
+    abjad_configuration.set_default_accidental_spelling('mixed')
 
 
-def test_pitchtools_set_default_accidental_spelling_03():
+def test_AbjadConfiguration_set_default_accidental_spelling_03():
 
-    pitchtools.set_default_accidental_spelling('flats')
+    abjad_configuration.set_default_accidental_spelling('flats')
     staff = Staff([Note(n, (1, 8)) for n in range(12)])
-
-    r'''
-    \new Staff {
-        c'8
-        df'8
-        d'8
-        ef'8
-        e'8
-        f'8
-        gf'8
-        g'8
-        af'8
-        a'8
-        bf'8
-        b'8
-    }
-    '''
 
     assert testtools.compare(
         staff,
@@ -131,32 +80,15 @@ def test_pitchtools_set_default_accidental_spelling_03():
         '''
         )
 
-    pitchtools.set_default_accidental_spelling('mixed')
+    abjad_configuration.set_default_accidental_spelling('mixed')
 
 
-def test_pitchtools_set_default_accidental_spelling_04():
+def test_AbjadConfiguration_set_default_accidental_spelling_04():
     r'''Revert back to default mixed spelling.
     '''
 
-    pitchtools.set_default_accidental_spelling('mixed')
+    abjad_configuration.set_default_accidental_spelling('mixed')
     staff = Staff([Note(n, (1, 8)) for n in range(12)])
-
-    r'''
-    \new Staff {
-        c'8
-        cs'8
-        d'8
-        ef'8
-        e'8
-        f'8
-        fs'8
-        g'8
-        af'8
-        a'8
-        bf'8
-        b'8
-    }
-    '''
 
     assert testtools.compare(
         staff,
