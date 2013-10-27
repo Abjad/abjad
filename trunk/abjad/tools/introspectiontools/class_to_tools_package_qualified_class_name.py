c1312d724c0bee22c450308b4d9fdd00c5ab2931
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2013-10-27 20:26:59 +0000

    Flattened more tools packages.

--- trunk/abjad/tools/introspectiontools/class_to_tools_package_qualified_class_name.py
+++ trunk/abjad/tools/introspectiontools/class_to_tools_package_qualified_class_name.py
@@ -11,8 +11,10 @@ def class_to_tools_package_qualified_class_name(current_class):
 
     Returns string.
     '''
-
     module_parts = current_class.__module__.split('.')
-    tools_package_qualified_class_name = '.'.join(module_parts[-3:-1])
-
+    unique_parts = [module_parts[0]]
+    for part in module_parts[1:]:
+        if part != unique_parts[-1]:
+            unique_parts.append(part)
+    tools_package_qualified_class_name = '.'.join(unique_parts[-2:])
     return tools_package_qualified_class_name
