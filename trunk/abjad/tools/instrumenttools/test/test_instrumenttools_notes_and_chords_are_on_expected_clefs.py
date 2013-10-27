85646953cc21c9a0c742977d481646eabc16fc0d
Trevor Bača <trevorbaca@gmail.com>
2013-10-27 21:15:48 +0000

    Porting mainline forward to use explicit mark and spanner attachment.

--- trunk/abjad/tools/instrumenttools/test/test_instrumenttools_notes_and_chords_are_on_expected_clefs.py
+++ trunk/abjad/tools/instrumenttools/test/test_instrumenttools_notes_and_chords_are_on_expected_clefs.py
@@ -5,14 +5,18 @@ from abjad import *
 def test_instrumenttools_notes_and_chords_are_on_expected_clefs_01():
 
     staff = Staff("c'8 d'8 e'8 f'8")
-    contexttools.ClefMark('treble')(staff)
-    instrumenttools.Violin()(staff)
+    clef = contexttools.ClefMark('treble')
+    clef.attach(staff)
+    violin = instrumenttools.Violin()
+    violin.attach(staff)
 
     assert instrumenttools.notes_and_chords_are_on_expected_clefs(staff)
 
     staff = Staff("c'8 d'8 e'8 f'8")
-    contexttools.ClefMark('alto')(staff)
-    instrumenttools.Violin()(staff)
+    clef = contexttools.ClefMark('alto')
+    clef.attach(staff)
+    violin = instrumenttools.Violin()
+    violin.attach(staff)
 
     assert not instrumenttools.notes_and_chords_are_on_expected_clefs(staff)
 
@@ -20,14 +24,18 @@ def test_instrumenttools_notes_and_chords_are_on_expected_clefs_01():
 def test_instrumenttools_notes_and_chords_are_on_expected_clefs_02():
 
     staff = Staff("c'8 d'8 e'8 f'8")
-    contexttools.ClefMark('treble')(staff)
-    instrumenttools.Bassoon()(staff)
+    clef = contexttools.ClefMark('treble')
+    clef.attach(staff)
+    bassoon = instrumenttools.Bassoon()
+    bassoon.attach(staff)
 
     assert not instrumenttools.notes_and_chords_are_on_expected_clefs(staff)
 
     staff = Staff("c'8 d'8 e'8 f'8")
-    contexttools.ClefMark('alto')(staff)
-    instrumenttools.Bassoon()(staff)
+    clef = contexttools.ClefMark('alto')
+    clef.attach(staff)
+    bassoon = instrumenttools.Bassoon()
+    bassoon.attach(staff)
 
     assert not instrumenttools.notes_and_chords_are_on_expected_clefs(staff)
 
@@ -35,8 +43,10 @@ def test_instrumenttools_notes_and_chords_are_on_expected_clefs_02():
 def test_instrumenttools_notes_and_chords_are_on_expected_clefs_03():
 
     staff = Staff("c'8 d'8 e'8 f'8")
-    contexttools.ClefMark('percussion')(staff)
-    instrumenttools.Violin()(staff)
+    clef = contexttools.ClefMark('percussion')
+    clef.attach(staff)
+    violin = instrumenttools.Violin()
+    violin.attach(staff)
 
     assert instrumenttools.notes_and_chords_are_on_expected_clefs(
         staff, percussion_clef_is_allowed = True)
