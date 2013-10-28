b94ed4d255f9d4aa03fb18bed4a1f53afcae6183
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2013-10-28 18:27:09 +0000

    Flattened experimental Windungen demo.

--- experimental/demos/windungen/WindungenScoreTemplate/test/test_WindungenScoreTemplate.py
+++
@@ -1,9 +0,0 @@
-from experimental.demos.windungen.WindungenScoreTemplate \
-    import WindungenScoreTemplate
-
-
-def test_WindungenScoreTemplate_01():
-
-    score_template = WindungenScoreTemplate()
-    score = score_template()
-    result = score.lilypond_format
