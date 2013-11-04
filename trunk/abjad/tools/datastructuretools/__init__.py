6d30531347546aa400489420d1270959b4b472ea
Trevor Bača <trevorbaca@gmail.com>
2013-11-04 10:30:10 -0500

    Moved datastructuretools to main section of API.

--- trunk/abjad/tools/datastructuretools/__init__.py
+++ trunk/abjad/tools/datastructuretools/__init__.py
@@ -4,6 +4,7 @@ from abjad.tools import importtools
 importtools.ImportManager.import_structured_package(
 	__path__[0],
 	globals(),
-	package_root_name='abjad')
+	package_root_name='abjad',
+    )
 
-_documentation_section = 'internals'
+_documentation_section = 'core'
