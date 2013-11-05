0d2a03786f37d8d369d8e191706f973ea4ecfaa6
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2013-11-05 13:19:31 -0500

    Added proof-of-concept parameterized test.

---
+++ abjad/tools/test/test_repr.py
@@ -0,0 +1,13 @@
+from abjad import *
+import pytest
+
+
+class_crawler = documentationtools.ClassCrawler(
+    abjad_configuration.abjad_directory_path,
+    root_package_name='abjad',
+    )
+all_classes = class_crawler()
+
+@pytest.mark.parametrize('klass', all_classes)
+def test_eval(klass): 
+    assert '__repr__' in dir(klass)
