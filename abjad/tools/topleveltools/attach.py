d6ad24def2a7a3f2f8295ba48453c2104b223ce3
Trevor Bača <trevorbaca@gmail.com>
2013-11-10 13:41:28 -0500

    Added target_context=None keyword to attach().
    The keyword is currently a stub.
    
    The keyword will eventually replace all _target_context keywords currently
    implemented in the initializers of the different context mark classes.

--- abjad/tools/topleveltools/attach.py
+++ abjad/tools/topleveltools/attach.py
@@ -1,5 +1,10 @@
-def attach(attachable, component_expression):
+def attach(attachable, component_expression, target_context=None):
     r'''Attaches `attachable` to `component_expression`.
+
+    Creates attachment expression effective at `target_context`.
+
+    Derives `target_context` from the default target context of `attachable`
+    when `target_context` is none.
 
     Returns none.
     '''
