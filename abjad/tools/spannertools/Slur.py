7fb33513cd4720ad0ab7eb40110f596055024542
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2013-11-10 21:14:17 -0500

    Rewired PhrasingSlur, Slur and Tie.
    They now inherit directly from Spanner, rather than DirectedSpanner.

--- abjad/tools/spannertools/Slur.py
+++ abjad/tools/spannertools/Slur.py
@@ -1,9 +1,9 @@
 # -*- encoding: utf-8 -*-
 from abjad.tools import stringtools
-from abjad.tools.spannertools.DirectedSpanner import DirectedSpanner
+from abjad.tools.spannertools.Spanner import Spanner
 
 
-class Slur(DirectedSpanner):
+class Slur(Spanner):
     r'''A slur spanner.
 
     ::
@@ -28,22 +28,33 @@ class Slur(DirectedSpanner):
     ### INITIALIZER ###
 
     def __init__(
-        self, 
-        components=None, 
+        self,
+        components=None,
         direction=None,
         overrides=None,
         ):
-        DirectedSpanner.__init__(
-            self, 
-            components, 
-            direction,
+        Spanner.__init__(
+            self,
+            components,
             overrides=overrides,
             )
+        self.direction = direction
+
+    ### PUBLIC PROPERTIES ###
+
+    @apply
+    def direction():
+        def fget(self):
+            return self._direction
+        def fset(self, arg):
+            self._direction = \
+                stringtools.arg_to_tridirectional_lilypond_symbol(arg)
+        return property(**locals())
 
     ### PRIVATE METHODS ###
 
     def _copy_keyword_args(self, new):
-        DirectedSpanner._copy_keyword_args(self, new)
+        new.direction = self.direction
 
     def _format_right_of_leaf(self, leaf):
         result = []
--- abjad/tools/spannertools/Tie.py
+++ abjad/tools/spannertools/Tie.py
@@ -1,9 +1,9 @@
 # -*- encoding: utf-8 -*-
 from abjad.tools import stringtools
-from abjad.tools.spannertools.DirectedSpanner import DirectedSpanner
+from abjad.tools.spannertools.Spanner import Spanner
 
 
-class Tie(DirectedSpanner):
+class Tie(Spanner):
     r'''A tie spanner.
 
     ::
@@ -28,22 +28,33 @@ class Tie(DirectedSpanner):
     ### INITIALIZER ###
 
     def __init__(
-        self, 
-        music=None, 
+        self,
+        music=None,
         direction=None,
         overrides=None,
         ):
-        DirectedSpanner.__init__(
-            self, 
-            music, 
-            direction,
+        Spanner.__init__(
+            self,
+            music,
             overrides=overrides,
             )
+        self.direction = direction
+
+    ### PUBLIC PROPERTIES ###
+
+    @apply
+    def direction():
+        def fget(self):
+            return self._direction
+        def fset(self, arg):
+            self._direction = \
+                stringtools.arg_to_tridirectional_lilypond_symbol(arg)
+        return property(**locals())
 
     ### PRIVATE METHODS ###
 
     def _copy_keyword_args(self, new):
-        DirectedSpanner._copy_keyword_args(self, new)
+        new.direction = self.direction
 
     def _format_right_of_leaf(self, leaf):
         result = []
