94876a8e83c6a5005dde6ac13361feb38a49bcea
Trevor Bača <trevorbaca@gmail.com>
2013-11-13 16:41:44 -0500

    Updated function call count.

--- abjad/tools/systemtools/IOManager.py
+++ abjad/tools/systemtools/IOManager.py
@@ -78,7 +78,7 @@ class IOManager(object):
                 ...     "Note('c4')",
                 ...     globals(),
                 ...     )
-                10496
+                10454
 
         ..  container:: example
 
