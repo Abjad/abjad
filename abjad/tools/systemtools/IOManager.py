607c389330ca6b77d29e15d58203e65f82852187
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2013-11-13 18:29:05 -0500

    Migrated more functionality from AbjadObject into StorageFormatManager.

--- abjad/tools/systemtools/IOManager.py
+++ abjad/tools/systemtools/IOManager.py
@@ -78,7 +78,7 @@ class IOManager(object):
                 ...     "Note('c4')",
                 ...     globals(),
                 ...     )
-                10538
+                10556
 
         ..  container:: example
 
--- abjad/tools/systemtools/StorageFormatManager.py
+++ abjad/tools/systemtools/StorageFormatManager.py
@@ -1,4 +1,5 @@
 # -*- encoding: utf-8 -*-
+import abc
 import types
 
 
@@ -17,6 +18,32 @@ class StorageFormatManager(object):
             StorageFormatManager.get_keyword_argument_values(object_two):
             return False
         return True
+
+    @staticmethod
+    def format_one_value(value, is_indented=True):
+        prefix, suffix = '', ', '
+        if is_indented:
+            prefix, suffix = '\t', ','
+        result = []
+        if isinstance(value, types.MethodType):
+            return result
+        if type(value) is abc.ABCMeta:
+            value = \
+                StorageFormatManager.get_tools_package_qualified_class_name(
+                    value)
+            result.append('{}{}{}'.format(prefix, value, suffix))
+        elif hasattr(value, '_get_tools_package_qualified_repr_pieces'):
+            pieces = value._get_tools_package_qualified_repr_pieces(
+                is_indented=is_indented)
+            for piece in pieces[:-1]:
+                result.append('{}{}'.format(prefix, piece))
+            result.append('{}{}{}'.format(prefix, pieces[-1], suffix))
+        elif hasattr(value, '_tools_package_name'):
+            result.append('{}{}.{!r}{}'.format(
+                prefix, value._tools_package_name, value, suffix))
+        else:
+            result.append('{}{!r}{}'.format(prefix, value, suffix))
+        return result
 
     @staticmethod
     def get_input_argument_values(object_):
@@ -167,7 +194,38 @@ class StorageFormatManager(object):
         positional_argument_values=None,
         tools_package_name=None,
         ):
-        pass
+        result = []
+        argument_pieces = []
+        argument_pieces.extend(
+            StorageFormatManager.get_storage_format_positional_pieces(
+                object_,
+                is_indented=is_indented,
+                positional_argument_values=positional_argument_values,
+                ))
+        argument_pieces.extend(
+            StorageFormatManager.get_storage_format_keyword_pieces(
+                object_,
+                is_indented=is_indented,
+                keyword_argument_names=keyword_argument_names,
+                ))
+        if argument_pieces:
+            argument_pieces[-1] = argument_pieces[-1].rstrip(' ')
+            argument_pieces[-1] = argument_pieces[-1].rstrip(',')
+        class_name = type(object_).__name__
+        tools_package_name = tools_package_name or \
+            StorageFormatManager.get_tools_package_name(object_)
+        tools_package_qualified_class_name = '{}.{}'.format(
+            tools_package_name, class_name)
+        if len(argument_pieces) == 0:
+            result.append('{}()'.format(tools_package_qualified_class_name))
+        else:
+            result.append('{}('.format(tools_package_qualified_class_name))
+            result.extend(argument_pieces)
+            if is_indented:
+                result.append('\t)')
+            else:
+                result.append(')')
+        return tuple(result)
 
     @staticmethod
     def get_storage_format_keyword_argument_pieces(
@@ -175,7 +233,9 @@ class StorageFormatManager(object):
         is_indented=True,
         keyword_argument_names=None,
         ):
-        pass
+        prefix, suffix = '', ', '
+        if is_indented:
+            prefix, suffix = '\t', ','
 
     @staticmethod
     def get_storage_format_positional_argument_pieces(
@@ -183,4 +243,6 @@ class StorageFormatManager(object):
         is_indented=True,
         positional_argument_values=None,
         ):
-        pass
+        prefix, suffix = '', ', '
+        if is_indented:
+            prefix, suffix = '\t', ','
