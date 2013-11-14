8f6718dcfc3f1d9284b945528263b848327c98c4
Trevor Bača <trevorbaca@gmail.com>
2013-11-14 15:28:43 -0500

    Removed metricmodulationtools.

--- experimental/__init__.py
+++ experimental/__init__.py
@@ -5,7 +5,6 @@ import library
 from tools import constrainttools
 from tools import handlertools
 from tools import interpolationtools
-from tools import metricmodulationtools
 from tools import musicexpressiontools
 from tools import newabjadbooktools
 from tools import scoremanagertools
--- experimental/tools/__init__.py
+++ experimental/tools/__init__.py
@@ -4,7 +4,6 @@ from abjad import *
 import constrainttools
 import handlertools
 import interpolationtools
-import metricmodulationtools
 import musicexpressiontools
 import newabjadbooktools
 import scoremanagertools
