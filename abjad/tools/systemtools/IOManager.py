bcd705049c6ed83de828d1113fa4e8a9d4dbd565
Trevor Bača <trevorbaca@gmail.com>
2013-11-21 13:26:17 -0500

    Removed context mark code from detach().

--- abjad/tools/systemtools/IOManager.py
+++ abjad/tools/systemtools/IOManager.py
@@ -78,7 +78,7 @@ class IOManager(object):
                 ...     "Note('c4')",
                 ...     globals(),
                 ...     )
-                9767
+                9763
 
         ..  container:: example
 
--- abjad/tools/topleveltools/detach.py
+++ abjad/tools/topleveltools/detach.py
@@ -1,64 +1,60 @@
 import types
 
 
-def detach(indicator, component_expression):
-    r'''Detaches all matching indicators from `component_expression`.
+def detach(prototype, component_expression):
+    r'''Detaches from `component_expression` all items matching `prototype`.
 
-    Returns tuple of zero detached indicators.
+    Returns tuple of zero or more detached items.
     '''
     from abjad.tools import indicatortools
     from abjad.tools import scoretools
     from abjad.tools import spannertools
     from abjad.tools.agenttools.InspectionAgent import inspect
 
-    marks, spanners, grace_containers = [], [], []
+    spanners = []
+    grace_containers = []
     inspector = inspect(component_expression)
-    if isinstance(indicator, types.TypeType):
-        if issubclass(indicator, indicatortools.ContextMark):
-            marks = inspector.get_context_marks(indicator)
-        elif issubclass(indicator, spannertools.Spanner):
-            spanners = inspector.get_spanners(indicator)
-        elif issubclass(indicator, scoretools.GraceContainer):
-            grace_containers = inspector.get_grace_containers(indicator)
+    if isinstance(prototype, types.TypeType):
+        if issubclass(prototype, spannertools.Spanner):
+            spanners = inspector.get_spanners(prototype)
+        elif issubclass(prototype, scoretools.GraceContainer):
+            grace_containers = inspector.get_grace_containers(prototype)
         else:
             assert hasattr(component_expression, '_indicators')
             result = []
             for x in component_expression._indicators[:]:
-                if isinstance(x, indicator):
+                if isinstance(x, prototype):
                     component_expression._indicators.remove(x)
                     result.append(x)
                 # indicator is a wrapper
                 elif hasattr(x, 'indicator') and \
-                    isinstance(x.indicator, indicator):
+                    isinstance(x.indicator, prototype):
                     x._detach()
                     result.append(x.indicator)
             result = tuple(result)
             return result
     else:
-        if isinstance(indicator, indicatortools.ContextMark):
-            marks = inspector.get_context_marks(indicator)
-        elif isinstance(indicator, spannertools.Spanner):
-            spanners = inspector.get_spanners(indicator)
-        elif isinstance(indicator, scoretools.GraceContainer):
-            grace_containers = inspector.get_grace_containers(indicator)
+        if isinstance(prototype, spannertools.Spanner):
+            spanners = inspector.get_spanners(prototype)
+        elif isinstance(prototype, scoretools.GraceContainer):
+            grace_containers = inspector.get_grace_containers(prototype)
         else:
             assert hasattr(component_expression, '_indicators')
             result = []
             for x in component_expression._indicators[:]:
-                if x == indicator:
+                if x == prototype:
                     component_expression._indicators.remove(x)
                     result.append(x)
                 # indicator is a wrapper
-                elif hasattr(x, 'indicator') and x.indicator == indicator:
+                elif hasattr(x, 'indicator') and x.indicator == prototype:
                     x._detach()
                     result.append(x.indicator)
             result = tuple(result)
             return result
-    indicators = []
-    indicators.extend(marks)
-    indicators.extend(spanners)
-    indicators.extend(grace_containers)
-    for indicator in indicators:
-        indicator._detach()
-    indicators = tuple(indicators)
-    return indicators
+    items = []
+    items.extend(spanners)
+    items.extend(grace_containers)
+    for item in items:
+        item._detach()
+    items = tuple(items)
+    return items
