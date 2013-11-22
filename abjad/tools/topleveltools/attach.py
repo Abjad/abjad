23399b6dc9d623886485105f9ba9db9989dee2f0
Trevor Bača <trevorbaca@gmail.com>
2013-11-22 16:01:41 -0500

    Simplifying attach().

--- abjad/tools/topleveltools/attach.py
+++ abjad/tools/topleveltools/attach.py
@@ -4,56 +4,36 @@ import types
 def attach(indicator, component_expression, scope=None):
     r'''Attaches `indicator` to `component_expression`.
 
-    Derives `scope` from the default scope of `indicator` 
+    Derives scope from the default scope of `indicator` 
     when `scope` is none.
 
     Returns none.
     '''
     from abjad.tools import indicatortools
     from abjad.tools import scoretools
+    from abjad.tools import spannertools
+
+    if hasattr(indicator, '_attach'):
+        prototype = (spannertools.Spanner, scoretools.GraceContainer)
+        assert isinstance(indicator, prototype), repr(indicator)
+        assert scope is None
+        indicator._attach(component_expression)
+        return
 
     if isinstance(indicator, indicatortools.IndicatorWrapper):
+        scope = scope or indicator.scope
+        if isinstance(scope, types.TypeType):
+            assert issubclass(scope, scoretools.Context), repr(scope)
+        else:
+            assert isinstance(scope, scoretools.Context), repr(scope)
         indicator._detach()
-        scope = scope or indicator.scope
-        wrapper = indicatortools.IndicatorWrapper(
-            indicator.indicator, 
-            component_expression, 
-            scope,
-            )
-        indicator = wrapper
-    elif scope is not None and hasattr(indicator, '_attach'):
-        assert hasattr(indicator, 'scope')
-        if isinstance(scope, types.TypeType):
-            assert issubclass(scope, scoretools.Context)
-        else:
-            assert isinstance(scope, scoretools.Context)
-        indicator._scope = scope
-    elif scope is not None and not hasattr(indicator, '_attach'):
-        scope = scope or indicator.scope
-        if isinstance(scope, types.TypeType):
-            assert issubclass(scope, scoretools.Context), repr(scope)
-        else:
-            assert isinstance(scope, scoretools.Context), repr(scope)
+        indicator = indicator.indicator
         wrapper = indicatortools.IndicatorWrapper(
             indicator, 
             component_expression, 
             scope,
             )
         indicator = wrapper
-    elif not hasattr(indicator, '_attach') and hasattr(indicator, 'scope'):
-        scope = scope or indicator.scope
-        if isinstance(scope, types.TypeType):
-            assert issubclass(scope, scoretools.Context), repr(scope)
-        else:
-            assert isinstance(scope, scoretools.Context), repr(scope)
-        wrapper = indicatortools.IndicatorWrapper(
-            indicator, 
-            component_expression, 
-            scope,
-            )
-        indicator = wrapper
-    elif scope is None and hasattr(indicator, '_attach'):
-        pass
     elif hasattr(indicator, '_default_scope'):
         scope = scope or indicator._default_scope
         if isinstance(scope, types.TypeType):
@@ -67,30 +47,26 @@ def attach(indicator, component_expression, scope=None):
             )
         indicator = wrapper
 
-    if hasattr(indicator, '_attach'):
-        indicator._attach(component_expression)
-    elif hasattr(component_expression, '_indicators'):
-        if isinstance(indicator, indicatortools.IndicatorWrapper):
-            classes = (type(indicator.indicator),)
-        else:
-            classes = (type(indicator),)
-        effective_indicator = component_expression._get_effective_indicator(
-            classes,
-            unwrap=False,
-            )
-        if effective_indicator is not None:
-            timespan = effective_indicator.component._get_timespan()
-            mark_start_offset = timespan.start_offset
-            timespan = component_expression._get_timespan()
-            component_start_offset = timespan.start_offset
-            if mark_start_offset == component_start_offset:
-                message = 'effective indicator already attached'
-                message += ' to some component starting at same time.'
-                raise ValueError(message)
-        if isinstance(indicator, indicatortools.IndicatorWrapper):
-            indicator._bind_to_component(component_expression)
-        component_expression._indicators.append(indicator)
-    else:
-        message = 'can not attach {!r} to {!r}.'
-        message = message.format(indicator, component_expression)
-        raise TypeError(message)
+    if isinstance(indicator, indicatortools.IndicatorWrapper):
+        assert hasattr(indicator.indicator, '_default_scope')
+
+    assert hasattr(component_expression, '_indicators')
+    prototype = (type(indicator),)
+    if isinstance(indicator, indicatortools.IndicatorWrapper):
+        prototype = (type(indicator.indicator),)
+    effective_indicator = component_expression._get_effective_indicator(
+        prototype,
+        unwrap=False,
+        )
+    if effective_indicator is not None:
+        timespan = effective_indicator.component._get_timespan()
+        mark_start_offset = timespan.start_offset
+        timespan = component_expression._get_timespan()
+        component_start_offset = timespan.start_offset
+        if mark_start_offset == component_start_offset:
+            message = 'effective indicator already attached'
+            message += ' to some component starting at same time.'
+            raise ValueError(message)
+    if isinstance(indicator, indicatortools.IndicatorWrapper):
+        indicator._bind_to_component(component_expression)
+    component_expression._indicators.append(indicator)
