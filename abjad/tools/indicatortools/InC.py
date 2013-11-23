c4f05f647a62a7544c26f63f6277bdcbdfa47e73
Trevor Bača <trevorbaca@gmail.com>
2013-11-23 12:29:17 -0500

    Added InC indicator.
    Attach to score or selection to indicate music written in C instead
    of at sounding pitch.

---
+++ abjad/tools/indicatortools/InC.py
@@ -0,0 +1,19 @@
+# -*- encoding: utf-8 -*-
+from abjad.tools.abctools import AbjadObject
+
+
+class InC(AbjadObject):
+    r'''In C indicator.
+
+    ::
+
+        >>> in_c = indicatortools.InC()
+
+    Attach to score or selection to denote music written in C
+    instead of at sounding pitch.
+    '''
+
+    ### CLASS VARIABLES ###
+
+    __slots__ = (
+        )
