ef9789779a203d829f32f9d41ed8ebcfb6203d59
Trevor Bača <trevorbaca@gmail.com>
2013-12-03 19:17:38 -0500

    Change _init_by to _initialize_by in tonalanalysistools test file names.

--- abjad/tools/abctools/__init__.py
+++ abjad/tools/abctools/__init__.py
@@ -1,7 +1,6 @@
 # -*- encoding: utf-8 -*-
 from AbjadObject import AbjadObject
 from ContextManager import ContextManager
-from Maker import Maker
 from Parser import Parser
 
 _documentation_section = 'internals'
