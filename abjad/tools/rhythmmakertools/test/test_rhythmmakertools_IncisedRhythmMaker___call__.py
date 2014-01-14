1efd572e2ac2d556470aaf2d52110a5fd789fde7
Trevor Bača <trevorbaca@gmail.com>
2014-01-14 12:31:17 -0500

    Removed DivisionIncisedRestRhythmMaker.
    Use IncisedRhythmMaker(..., fill_with_notes=False, incise_divisions=True)
    instead.

--- abjad/tools/rhythmmakertools/test/test_rhythmmakertools_IncisedRhythmMaker___call__.py
+++ abjad/tools/rhythmmakertools/test/test_rhythmmakertools_IncisedRhythmMaker___call__.py
@@ -3,6 +3,8 @@ from abjad import *
 
 
 def test_rhythmmakertools_IncisedRhythmMaker___call___01():
+    r'''Division-incised notes.
+    '''
 
     maker = rhythmmakertools.IncisedRhythmMaker(
         prefix_talea=(-8,), 
@@ -54,6 +56,8 @@ def test_rhythmmakertools_IncisedRhythmMaker___call___01():
 
 
 def test_rhythmmakertools_IncisedRhythmMaker___call___02():
+    r'''Division-incised notes.
+    '''
 
     maker = rhythmmakertools.IncisedRhythmMaker(
         prefix_talea=(-8,), 
@@ -105,6 +109,8 @@ def test_rhythmmakertools_IncisedRhythmMaker___call___02():
 
 
 def test_rhythmmakertools_IncisedRhythmMaker___call___03():
+    r'''Division-incised notes.
+    '''
 
     maker = rhythmmakertools.IncisedRhythmMaker(
         prefix_talea=(-1,), 
@@ -152,6 +158,8 @@ def test_rhythmmakertools_IncisedRhythmMaker___call___03():
 
 
 def test_rhythmmakertools_IncisedRhythmMaker___call___04():
+    r'''Division-incised notes.
+    '''
 
     maker = rhythmmakertools.IncisedRhythmMaker(
         prefix_talea=(), 
@@ -194,6 +202,8 @@ def test_rhythmmakertools_IncisedRhythmMaker___call___04():
 
 
 def test_rhythmmakertools_IncisedRhythmMaker___call___05():
+    r'''Division-incised notes.
+    '''
 
     maker = rhythmmakertools.IncisedRhythmMaker(
         prefix_talea=(-1,), 
@@ -247,6 +257,8 @@ def test_rhythmmakertools_IncisedRhythmMaker___call___05():
 
 
 def test_rhythmmakertools_IncisedRhythmMaker___call___06():
+    r'''Division-incised notes.
+    '''
 
     maker = rhythmmakertools.IncisedRhythmMaker(
         prefix_talea=(-1,),
@@ -301,6 +313,327 @@ def test_rhythmmakertools_IncisedRhythmMaker___call___06():
                     r32
                     c'4 ~
                     c'32
+                }
+            }
+        }
+        '''
+        )
+
+
+def test_rhythmmakertools_IncisedRhythmMaker___call___07():
+    r'''Division-incised rests.
+    '''
+
+    maker = rhythmmakertools.IncisedRhythmMaker(
+        prefix_talea=(8,), 
+        prefix_lengths=(0, 1), 
+        suffix_talea=(1,), 
+        suffix_lengths=(1,), 
+        talea_denominator=32,
+        fill_with_notes=False,
+        incise_divisions=True,
+        )
+
+    divisions = [(5, 8), (5, 8), (5, 8), (5, 8)]
+    leaf_lists = maker(divisions)
+    leaves = sequencetools.flatten_sequence(leaf_lists)
+
+    measures = scoretools.make_spacer_skip_measures(divisions)
+    staff = Staff(measures)
+    mutate(staff).replace_measure_contents(leaves)
+
+    assert systemtools.TestManager.compare(
+        staff,
+        r'''
+        \new Staff {
+            {
+                \time 5/8
+                r2
+                r16.
+                c'32
+            }
+            {
+                c'4
+                r4
+                r16.
+                c'32
+            }
+            {
+                r2
+                r16.
+                c'32
+            }
+            {
+                c'4
+                r4
+                r16.
+                c'32
+            }
+        }
+        '''
+        )
+
+
+def test_rhythmmakertools_IncisedRhythmMaker___call___08():
+    r'''Division-incised rests.
+    '''
+
+    maker = rhythmmakertools.IncisedRhythmMaker(
+        prefix_talea=(8,), 
+        prefix_lengths=(1, 2, 3, 4), 
+        suffix_talea=(1,), 
+        suffix_lengths=(1,), 
+        talea_denominator=32,
+        fill_with_notes=False,
+        incise_divisions=True,
+        )
+
+    divisions = [(5, 8), (5, 8), (5, 8), (5, 8)]
+    leaf_lists = maker(divisions)
+    leaves = sequencetools.flatten_sequence(leaf_lists)
+
+    measures = scoretools.make_spacer_skip_measures(divisions)
+    staff = Staff(measures)
+    mutate(staff).replace_measure_contents(leaves)
+
+    assert systemtools.TestManager.compare(
+        staff,
+        r'''
+        \new Staff {
+            {
+                \time 5/8
+                c'4
+                r4
+                r16.
+                c'32
+            }
+            {
+                c'4
+                c'4
+                r16.
+                c'32
+            }
+            {
+                c'4
+                c'4
+                c'8
+            }
+            {
+                c'4
+                c'4
+                c'8
+            }
+        }
+        '''
+        )
+
+
+def test_rhythmmakertools_IncisedRhythmMaker___call___09():
+    r'''Division-incised rests.
+    '''
+
+    maker = rhythmmakertools.IncisedRhythmMaker(
+        prefix_talea=(1,), 
+        prefix_lengths=(1,), 
+        suffix_talea=(8,), 
+        suffix_lengths=(1, 2, 3), 
+        talea_denominator=32,
+        fill_with_notes=False,
+        incise_divisions=True,
+        )
+
+    divisions = [(5, 8), (5, 8), (5, 8)]
+    leaf_lists = maker(divisions)
+    leaves = sequencetools.flatten_sequence(leaf_lists)
+
+    measures = scoretools.make_spacer_skip_measures(divisions)
+    staff = Staff(measures)
+    mutate(staff).replace_measure_contents(leaves)
+
+    assert systemtools.TestManager.compare(
+        staff,
+        r'''
+        \new Staff {
+            {
+                \time 5/8
+                c'32
+                r4
+                r16.
+                c'4
+            }
+            {
+                c'32
+                r16.
+                c'4
+                c'4
+            }
+            {
+                c'32
+                c'4
+                c'4
+                c'16.
+            }
+        }
+        '''
+        )
+
+
+def test_rhythmmakertools_IncisedRhythmMaker___call___10():
+    r'''Division-incised rests.
+    '''
+
+    maker = rhythmmakertools.IncisedRhythmMaker(
+        prefix_talea=(), 
+        prefix_lengths=(0,), 
+        suffix_talea=(), 
+        suffix_lengths=(0,), 
+        talea_denominator=8,
+        fill_with_notes=False,
+        incise_divisions=True,
+        )
+
+    divisions = [(5, 8), (5, 8), (5, 8)]
+    leaf_lists = maker(divisions)
+    leaves = sequencetools.flatten_sequence(leaf_lists)
+
+    measures = scoretools.make_spacer_skip_measures(divisions)
+    staff = Staff(measures)
+    mutate(staff).replace_measure_contents(leaves)
+
+    assert systemtools.TestManager.compare(
+        staff,
+        r'''
+        \new Staff {
+            {
+                \time 5/8
+                r2
+                r8
+            }
+            {
+                r2
+                r8
+            }
+            {
+                r2
+                r8
+            }
+        }
+        '''
+        )
+
+
+def test_rhythmmakertools_IncisedRhythmMaker___call___11():
+    r'''Division-incised rests.
+    '''
+
+    maker = rhythmmakertools.IncisedRhythmMaker(
+        prefix_talea=(1,), 
+        prefix_lengths=(1,), 
+        suffix_talea=(1,), 
+        suffix_lengths=(1,), 
+        talea_denominator=8,
+        prolation_addenda=(1, 0, 3),
+        fill_with_notes=False,
+        incise_divisions=True,
+        )
+
+    divisions = [(4, 8), (4, 8), (4, 8)]
+    leaf_lists = maker(divisions)
+    leaves = sequencetools.flatten_sequence(leaf_lists)
+
+    measures = scoretools.make_spacer_skip_measures(divisions)
+    staff = Staff(measures)
+    mutate(staff).replace_measure_contents(leaves)
+
+    assert systemtools.TestManager.compare(
+        staff,
+        r'''
+        \new Staff {
+            {
+                \time 4/8
+                \times 4/5 {
+                    c'8
+                    r4.
+                    c'8
+                }
+            }
+            {
+                {
+                    c'8
+                    r4
+                    c'8
+                }
+            }
+            {
+                \times 4/7 {
+                    c'8
+                    r2
+                    r8
+                    c'8
+                }
+            }
+        }
+        '''
+        )
+
+
+def test_rhythmmakertools_IncisedRhythmMaker___call___12():
+    r'''Division-incised rests.
+    '''
+
+    maker = rhythmmakertools.IncisedRhythmMaker(
+        prefix_talea=(1,),
+        prefix_lengths=(1,),
+        suffix_talea=(),
+        suffix_lengths=(0,),
+        talea_denominator=32,
+        prolation_addenda=(2, 0),
+        secondary_divisions=(20,),
+        fill_with_notes=False,
+        incise_divisions=True,
+        )
+
+    divisions = [(4, 8), (4, 8), (4, 8)]
+    leaf_lists = maker(divisions)
+    leaves = sequencetools.flatten_sequence(leaf_lists)
+
+    measures = scoretools.make_spacer_skip_measures(divisions)
+    staff = Staff(measures)
+    mutate(staff).replace_measure_contents(leaves)
+
+    assert systemtools.TestManager.compare(
+        staff,
+        r'''
+        \new Staff {
+            {
+                \time 4/8
+                \times 8/9 {
+                    c'32
+                    r2
+                    r32
+                }
+            }
+            {
+                {
+                    c'32
+                    r16.
+                }
+                \tweak #'text #tuplet-number::calc-fraction-text
+                \times 6/7 {
+                    c'32
+                    r4.
+                    r32
+                }
+            }
+            {
+                {
+                    c'32
+                    r8..
+                }
+                \times 4/5 {
+                    c'32
+                    r4
+                    r32
                 }
             }
         }
