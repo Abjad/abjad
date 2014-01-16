16faf32841175b7b8e99abd0c13215750f068d31
Trevor Bača <trevorbaca@gmail.com>
2014-01-16 16:44:17 -0500

    Updated OrdinalConstant __format__ and __repr__.
    The methods now handle the case where OrdinalConstant is empty initialized.

--- abjad/tools/test/test___repr__.py
+++ abjad/tools/test/test___repr__.py
@@ -1,14 +1,8 @@
 # -*- encoding: utf-8 -*-
 import inspect
 import pytest
-from abjad.tools import datastructuretools
 from abjad.tools import documentationtools
-from abjad.tools import markuptools
 
-
-# TODO: eventually make these work
-_classes_to_skip = (
-    )
 
 classes = documentationtools.list_all_abjad_classes()
 @pytest.mark.parametrize('class_', classes)
@@ -17,7 +11,7 @@ def test___repr___01(class_):
     '''
 
     if not inspect.isabstract(class_):
-        if class_ not in _classes_to_skip:
-            instance = class_()
-            string = repr(instance)
-            assert string is not None
+        instance = class_()
+        string = repr(instance)
+        assert string is not None
+        assert string != ''
