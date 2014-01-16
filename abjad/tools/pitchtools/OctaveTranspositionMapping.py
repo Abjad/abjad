c1c28bc22ca7913e6d0e26fe38046cb149416b9a
Trevor Bača <trevorbaca@gmail.com>
2014-01-16 12:28:31 -0500

    Merged master.

--- abjad/tools/pitchtools/OctaveTranspositionMapping.py
+++ abjad/tools/pitchtools/OctaveTranspositionMapping.py
@@ -25,10 +25,25 @@ class OctaveTranspositionMapping(TypedList):
     are mutable.
     '''
 
-    ### CLASS VARIABLES ###
+#    ### CLASS VARIABLES ###
+#
+#    __slots__ = (
+#        )
 
-    __slots__ = (
-        )
+    def __init__(
+        self,
+        tokens=None,
+        item_class=None,
+        keep_sorted=None,
+        custom_identifier=None,
+        ):
+        TypedList.__init__(
+            self,
+            tokens=tokens,
+            item_class=item_class,
+            keep_sorted=keep_sorted,
+            custom_identifier=custom_identifier,
+            )
 
     ### SPECIAL METHODS ###
 
