c5b176af3ae464b8c8a51592e8224179bdb7431f
Trevor Bača <trevorbaca@gmail.com>
2014-01-17 20:08:37 -0500

    Hoisted gallery-making code up to rhythm-maker base class.

--- abjad/tools/rhythmmakertools/EvenRunRhythmMaker.py
+++ abjad/tools/rhythmmakertools/EvenRunRhythmMaker.py
@@ -1,20 +1,12 @@
 # -*- encoding: utf-8 -*-
-import os
 from abjad.tools import durationtools
-from abjad.tools import indicatortools
-from abjad.tools import markuptools
 from abjad.tools import mathtools
 from abjad.tools import scoretools
 from abjad.tools import selectiontools
 from abjad.tools import spannertools
-from abjad.tools import stringtools
 from abjad.tools import systemtools
-from abjad.tools.agenttools.InspectionAgent import inspect
 from abjad.tools.rhythmmakertools.RhythmMaker import RhythmMaker
 from abjad.tools.topleveltools import attach
-from abjad.tools.topleveltools import mutate
-from abjad.tools.topleveltools import override
-from abjad.tools.topleveltools import persist
 
 
 class EvenRunRhythmMaker(RhythmMaker):
@@ -213,75 +205,6 @@ class EvenRunRhythmMaker(RhythmMaker):
 
     ### PRIVATE METHODS ###
 
-    def _gallery_input_block_to_score(self, block):
-        from abjad.tools import sequencetools
-        maker = type(self)(**block.input_)
-        lists = maker(block.divisions)
-        music = sequencetools.flatten_sequence(lists)
-        measures = scoretools.make_spacer_skip_measures(block.divisions)
-        time_signature_context = scoretools.Context(
-            measures,
-            context_name='TimeSignatureContext',
-            name='TimeSignatureContext',
-            )
-        measures = scoretools.make_spacer_skip_measures(block.divisions)
-        staff = scoretools.RhythmicStaff(measures)
-        measures = mutate(staff).replace_measure_contents(music)
-        score = scoretools.Score()
-        score.append(time_signature_context)
-        score.append(staff)
-        return score
-
-    def _make_gallery_title_markup(self):
-        string = self._human_readable_class_name 
-        string = stringtools.capitalize_string_start(string)
-        markup = markuptools.Markup(string)
-        return markup
-
-    def _gallery_input_to_lilypond_file(self):
-        from abjad.tools import lilypondfiletools
-        from abjad.tools import markuptools
-        lilypond_file = lilypondfiletools.make_basic_lilypond_file()
-        lilypond_file.items.remove(lilypond_file.score_block)
-        title_markup = self._make_gallery_title_markup()
-        lilypond_file.header_block.title = title_markup
-        markups = []
-        scores = []
-        for block in self._gallery_input_blocks:
-            score = self._gallery_input_block_to_score(block)
-            score.add_final_bar_line()
-            selection = score.select_leaves(start=-1)
-            last_leaf = selection[0]
-            string = "override Staff.BarLine #'extra-offset = #'(1.6 . 0)"
-            command = indicatortools.LilyPondCommand(
-                string,
-                'after',
-                )
-            attach(command, last_leaf)
-            if not inspect(score).is_well_formed():
-                message = 'score is not well-formed: {!r}.'
-                message = message.format(score)
-                message += '\n'
-                message += inspect(score).tabulate_well_formedness_violations()
-                raise Exception(message)
-            scores.append(score)
-            markup = block._to_markup(type(self))
-            markups.append(markup)
-        for markup, score in zip(markups, scores):
-            lilypond_file.items.append(markup)
-            lilypond_file.items.append(score)
-        lilypond_file.default_paper_size = ('letter', 'portrait')
-        lilypond_file.global_staff_size = 10
-        lilypond_file.use_relative_includes = True
-        stylesheet_path = os.path.join(
-            '..', '..', '..', 
-            'stylesheets', 
-            'gallery-layout.ly',
-            )
-        lilypond_file.file_initial_user_includes.append(stylesheet_path)
-        lilypond_file.paper_block.tagline = markuptools.Markup('')
-        return lilypond_file
-
     def _make_container(self, division):
         numerator, denominator = division
         # eventually allow for non-power-of-two divisions
@@ -296,15 +219,6 @@ class EvenRunRhythmMaker(RhythmMaker):
             beam = spannertools.MultipartBeam()
             attach(beam, container)
         return container
-
-    def _write_gallery_to_disk(self):
-        lilypond_file = self._gallery_input_to_lilypond_file()
-        file_path = __file__
-        directory_path = os.path.dirname(file_path)
-        class_name = type(self).__name__
-        file_name = '{}.pdf'.format(class_name)
-        file_path = os.path.join(directory_path, 'gallery', file_name)
-        persist(lilypond_file).as_pdf(file_path, remove_ly=False)
 
     ### PUBLIC PROPERTIES ###
 
