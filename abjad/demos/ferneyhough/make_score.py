25462558190a324337a4ca41c0ffa9b9bd6517cf
Trevor Bača <trevorbaca@gmail.com>
2014-01-18 15:05:18 -0500

    Removed scoretools.RhythmicStaff.
    Set staff.context_name = 'RhythmicStaff' instead.
    
    CHANGE: should be noted in the 2.15 change log.

--- abjad/demos/ferneyhough/make_score.py
+++ abjad/demos/ferneyhough/make_score.py
@@ -12,7 +12,8 @@ def make_score(tuplet_duration, row_count, column_count):
     rows_of_nested_tuplets = make_rows_of_nested_tuplets(
         tuplet_duration, row_count, column_count)
     for row_of_nested_tuplets in rows_of_nested_tuplets:
-        staff = scoretools.RhythmicStaff(row_of_nested_tuplets)
+        staff = Staff(row_of_nested_tuplets)
+        staff.context_name = 'RhythmicStaff'
         time_signature = indicatortools.TimeSignature((1, 4))
         attach(time_signature, staff)
         score.append(staff)
