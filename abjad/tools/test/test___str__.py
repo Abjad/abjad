a79be8f68efe858c1ea53adc3416e9f89fc0d266
Trevor Bača <trevorbaca@gmail.com>
2014-01-17 19:26:20 -0500

    Added parameterized string tests.
    The tests are currently skipped because 21 tests fail.
    
    The tests will be activated when all tests pass.

---
+++ abjad/tools/test/test___str__.py
@@ -0,0 +1,18 @@
+# -*- encoding: utf-8 -*-
+import inspect
+import pytest
+from abjad.tools import documentationtools
+pytest.skip('working on these now.')
+
+
+classes = documentationtools.list_all_abjad_classes()
+@pytest.mark.parametrize('class_', classes)
+def test___repr___01(class_):
+    r'''All concrete classes have a string representation.
+    '''
+
+    if not inspect.isabstract(class_):
+        instance = class_()
+        string = str(instance)
+        assert string is not None
+        assert string != ''
