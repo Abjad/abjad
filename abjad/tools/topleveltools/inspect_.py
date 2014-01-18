1fd8b0ba8a51da20f1fddaf97206c21387b4b09c
Trevor Bača <trevorbaca@gmail.com>
2014-01-17 21:32:16 -0500

    Added new inspect_() function to topleveltools.
    The function is an exact synonym of the inspect() function currently
    implemented in the InspectionAgent module.
    
    Use of inspect() is now deprecated. Use inspect_() instead.
    
    CHANGE: should be noted in the 2.15 change log and release notes.

---
+++ abjad/tools/topleveltools/inspect_.py
@@ -0,0 +1,12 @@
+# -*- encoding: utf-8 -*-
+
+
+def inspect_(expr):
+    r'''Inspects `expr`.
+
+    Factory function.
+
+    Returns inspect agent.
+    '''
+    from abjad.tools import agenttools
+    return agenttools.InspectionAgent(expr)
