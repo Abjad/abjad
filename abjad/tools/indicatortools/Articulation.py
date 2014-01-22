87cb06b17a6d75f8dc6ed7296380fe0c1e03a765
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-01-22 14:55:45 -0500

    Added private Articulation._lilypond_format_bundle property.
    All indicators will eventually receive a _lilypond_format_bundle property,
    which LilyPondFormatManager will consult when formatting score components.

--- abjad/tools/indicatortools/Articulation.py
+++ abjad/tools/indicatortools/Articulation.py
@@ -65,118 +65,6 @@ class Articulation(AbjadObject):
         '_format_slot',
         )
 
-    ### INITIALIZER ###
-
-    def __init__(self, *args):
-        from abjad.tools import datastructuretools
-        assert len(args) in range(3), repr(args)
-        if 2 <= len(args):
-            assert isinstance(args[0], (str, type(None))), repr(args[0])
-            assert isinstance(args[1],
-                (str, type(None), datastructuretools.OrdinalConstant)), \
-                repr(args[1])
-            name, direction = args
-        elif len(args) == 1 and isinstance(args[0], type(self)):
-            name = args[0].name
-            direction = args[0].direction
-        elif len(args) == 1:
-            assert isinstance(args[0],
-                (str, type(None), datastructuretools.OrdinalConstant)), \
-                repr(args[0])
-            if args[0]:
-                splits = args[0].split('\\')
-                assert len(splits) in (1, 2), repr(splits)
-                if len(splits) == 1:
-                    name, direction = args[0], None
-                elif len(splits) == 2:
-                    name = splits[1]
-                    if splits[0]:
-                        direction = splits[0]
-                    else:
-                        direction = None
-            else:
-                name, direction = None, None
-        else:
-            name, direction = None, None
-        self._name = name
-        direction = \
-            stringtools.arg_to_tridirectional_ordinal_constant(direction)
-        self._direction = direction
-        self._format_slot = 'right'
-
-    ### SPECIAL METHODS ###
-
-    def __copy__(self, *args):
-        r'''Copies articulation.
-
-        Returns new articulation.
-        '''
-        return type(self)(self.name, self.direction)
-
-    def __eq__(self, expr):
-        r'''Is true when `expr` is an articulation with name and direction 
-        equal to that of this articulation. Otherwise false.
-
-        Returns boolean.
-        '''
-        if isinstance(expr, type(self)):
-            if expr.name == self.name:
-                if self.direction == expr.direction:
-                    return True
-        return False
-
-    def __format__(self, format_specification=''):
-        r'''Formats articulation.
-
-        Set `format_specification` to `''`, `'lilypond`' or `'storage'`.
-        Interprets `''` equal to `'storage'`.
-
-        Returns string.
-        '''
-        from abjad.tools import systemtools
-        if format_specification in ('', 'storage'):
-            return systemtools.StorageFormatManager.get_storage_format(self)
-        elif format_specification == 'lilypond':
-            return self._lilypond_format
-        return str(self)
-
-    def __illustrate__(self):
-        r'''Illustrates articulation.
-
-        Returns LilyPond file.
-        '''
-        from abjad.tools import lilypondfiletools
-        from abjad.tools import markuptools
-        from abjad.tools import scoretools
-        from abjad.tools import topleveltools
-        note = scoretools.Note("c'4")
-        articulation = copy.copy(self)
-        topleveltools.attach(articulation, note)
-        lilypond_file = lilypondfiletools.make_basic_lilypond_file(note)
-        lilypond_file.header_block.tagline = markuptools.Markup('""')
-        return lilypond_file
-
-    def __str__(self):
-        r'''String representation of articulation.
-
-        Returns string.
-        '''
-        if self.name:
-            string = self._shortcut_to_word.get(self.name)
-            if not string:
-                string = self.name
-            if self.direction is None:
-                direction = '-'
-            else:
-                direction = \
-                    stringtools.arg_to_tridirectional_lilypond_symbol(
-                    self.direction)
-            return '%s\%s' % (direction, string)
-        else:
-            return ''
-
-    ### PRIVATE PROPERTIES ###
-
     # this causes unnecessary coupling to changeable lilypond codebase
     # and is discouraged
     _articulations_supported = (
@@ -240,6 +128,118 @@ class Articulation(AbjadObject):
         '_': 'portato',
         }
 
+    ### INITIALIZER ###
+
+    def __init__(self, *args):
+        from abjad.tools import datastructuretools
+        assert len(args) in range(3), repr(args)
+        if 2 <= len(args):
+            assert isinstance(args[0], (str, type(None))), repr(args[0])
+            assert isinstance(args[1],
+                (str, type(None), datastructuretools.OrdinalConstant)), \
+                repr(args[1])
+            name, direction = args
+        elif len(args) == 1 and isinstance(args[0], type(self)):
+            name = args[0].name
+            direction = args[0].direction
+        elif len(args) == 1:
+            assert isinstance(args[0],
+                (str, type(None), datastructuretools.OrdinalConstant)), \
+                repr(args[0])
+            if args[0]:
+                splits = args[0].split('\\')
+                assert len(splits) in (1, 2), repr(splits)
+                if len(splits) == 1:
+                    name, direction = args[0], None
+                elif len(splits) == 2:
+                    name = splits[1]
+                    if splits[0]:
+                        direction = splits[0]
+                    else:
+                        direction = None
+            else:
+                name, direction = None, None
+        else:
+            name, direction = None, None
+        self._name = name
+        direction = \
+            stringtools.arg_to_tridirectional_ordinal_constant(direction)
+        self._direction = direction
+        self._format_slot = 'right'
+
+    ### SPECIAL METHODS ###
+
+    def __copy__(self, *args):
+        r'''Copies articulation.
+
+        Returns new articulation.
+        '''
+        return type(self)(self.name, self.direction)
+
+    def __eq__(self, expr):
+        r'''Is true when `expr` is an articulation with name and direction
+        equal to that of this articulation. Otherwise false.
+
+        Returns boolean.
+        '''
+        if isinstance(expr, type(self)):
+            if expr.name == self.name:
+                if self.direction == expr.direction:
+                    return True
+        return False
+
+    def __format__(self, format_specification=''):
+        r'''Formats articulation.
+
+        Set `format_specification` to `''`, `'lilypond`' or `'storage'`.
+        Interprets `''` equal to `'storage'`.
+
+        Returns string.
+        '''
+        from abjad.tools import systemtools
+        if format_specification in ('', 'storage'):
+            return systemtools.StorageFormatManager.get_storage_format(self)
+        elif format_specification == 'lilypond':
+            return self._lilypond_format
+        return str(self)
+
+    def __illustrate__(self):
+        r'''Illustrates articulation.
+
+        Returns LilyPond file.
+        '''
+        from abjad.tools import lilypondfiletools
+        from abjad.tools import markuptools
+        from abjad.tools import scoretools
+        from abjad.tools import topleveltools
+        note = scoretools.Note("c'4")
+        articulation = copy.copy(self)
+        topleveltools.attach(articulation, note)
+        lilypond_file = lilypondfiletools.make_basic_lilypond_file(note)
+        lilypond_file.header_block.tagline = markuptools.Markup('""')
+        return lilypond_file
+
+    def __str__(self):
+        r'''String representation of articulation.
+
+        Returns string.
+        '''
+        if self.name:
+            string = self._shortcut_to_word.get(self.name)
+            if not string:
+                string = self.name
+            if self.direction is None:
+                direction = '-'
+            else:
+                direction = \
+                    stringtools.arg_to_tridirectional_lilypond_symbol(
+                        self.direction)
+            return '%s\%s' % (direction, string)
+        else:
+            return ''
+
+    ### PRIVATE PROPERTIES ###
+
     @property
     def _contents_repr_string(self):
         if self.direction is not None:
@@ -250,6 +250,13 @@ class Articulation(AbjadObject):
     @property
     def _lilypond_format(self):
         return str(self)
+
+    @property
+    def _lilypond_format_bundle(self):
+        from abjad.tools import systemtools
+        lilypond_format_bundle = systemtools.LilyPondFormatBundle()
+        lilypond_format_bundle.right.articulations.append(str(self))
+        return lilypond_format_bundle
 
     @property
     def _repr_specification(self):
