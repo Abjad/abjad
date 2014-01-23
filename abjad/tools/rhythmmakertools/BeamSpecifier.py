93341ebdacda550b700cc206f8e02eaac1fe0882
Trevor Bača <trevorbaca@gmail.com>
2014-01-22 21:03:22 -0500

    Added BeamSpecifier to rhythmmakertools.
    The class is currently a stub.
    
    The class will replace the beam_each_cell=True and beam_cells_together=False
    keywords.
    
    The beam_each_cell=True and beam_cells_together=False keywords are currently
    supplied to all rhythm-makers. This means that BeamSpecifier will be supplied
    to all rhythm-makers.

---
+++ abjad/tools/rhythmmakertools/BeamSpecifier.py
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8 -*-
+from abjad.tools.abctools import AbjadObject
+
+
+class BeamSpecifier(AbjadObject):
+    r'''A beam specifier.
+    '''
+
+    ### CLASS VARIABLES ###
+
+    __slots__ = (
+        )
+
+    ### INITIALIZER ###
+
+    def __init__(self):
+        pass
