477d6a361fd65d418e484077dbd28ca0d15b448c
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-01-23 12:08:22 -0500

    Improved systemtools.Timer.
    You can now access the Timer from within the `with` block, like so:
    
        with systemtools.Timer() as timer:
            for _ in range(5):
                for _ in xrange(1000000):
                    x = 1 + 1
                print timer.elapsed_time
    
    All context managers can be accessed from within their `with` block so long as
    they return a reference to themself from their `__enter__()` method.

--- abjad/tools/systemtools/Timer.py
+++ abjad/tools/systemtools/Timer.py
@@ -16,6 +16,24 @@ class Timer(ContextManager):
         >>> timer.elapsed_time # doctest: +SKIP
         0.092742919921875
 
+    The timer can also be accessed from within the `with` block:
+
+    ::
+
+        >>> with systemtools.Timer() as timer: # doctest: +SKIP
+        ...     for _ in range(5):
+        ...         for _ in xrange(1000000):
+        ...             x = 1 + 1
+        ...         print timer.elapsed_time
+        ...
+        0.101150989532
+        0.203935861588
+        0.304930925369
+        0.4057970047
+        0.50649189949
+
+    Timers can be reused between `with` blocks. They will reset their clock on
+    entering any `with` block.
     '''
 
     ### CLASS VARIABLES ###
@@ -40,6 +58,7 @@ class Timer(ContextManager):
         '''
         self._stop_time = None
         self._start_time = time.time()
+        return self
 
     def __exit__(self, exc_type, exc_value, traceback):
         r'''Exist context manager.
@@ -56,8 +75,10 @@ class Timer(ContextManager):
 
         Return float or none.
         '''
-        if self.stop_time is not None and self.start_time is not None:
-            return self.stop_time - self.start_time
+        if self.start_time is not None:
+            if self.stop_time is not None:
+                return self.stop_time - self.start_time
+            return time.time() - self.start_time
         return None
 
     @property
