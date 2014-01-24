ba8c1465712f4204da1c188a49c66926157f97d9
Trevor Bača <trevorbaca@gmail.com>
2014-01-24 10:32:41 -0500

    Introduced DurationSpellingSpecifier to rhythmmakertools.
    The class is currently a stub.
    
    The class will bundle the decrease_durations_monotonically=True and the
    forbidden_written_duration=None rhythm-maker input parameters.
    
    Those parameters are now deprecated.
    
    Importantly the class will also provide an object-modeled namespace for future
    duration-spelling parameters. We will almost certainly want to incorporate
    options for invoking rewrite_meter() on all rhythm-makers at some point. The
    parameters specifying how that will happen should be implemented as properites
    on DurationSpellingSpecifier.

---
+++ abjad/tools/rhythmmakertools/DurationSpellingSpecifier.py
@@ -0,0 +1,9 @@
+# -*- encoding: utf-8 -*-
+from abjad.tools.abctools import AbjadObject
+
+
+class DurationSpellingSpecifier(AbjadObject):
+    r'''Duration spelling speficifier.
+    '''
+
+    pass
