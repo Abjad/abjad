99ba6e755f4086ba860715ed63c1f1f510d56e3b
Trevor Bača <trevorbaca@gmail.com>
2014-02-02 15:35:58 -0500

    Added SegmentMaker baseclass to experimental segmentmakertools package.
    The class is currently a stub.
    
    All segment-makers should inherit from SegmentMaker analogously to the way that
    all rhythm-makers inherit from RhythmMaker.
    
    PACKAGE STRUCTURE: we'll probably leave segmentmakertools in experimental for a
    couple of point releases before moving to mainline. Seems like a couple of
    complete scores should be done with segment-makers before making public.

---
+++ experimental/tools/segmentmakertools/SegmentMaker.py
@@ -0,0 +1,12 @@
+# -*- encoding: utf-8 -*-
+from abjad.tools.abctools import AbjadObject
+
+
+class SegmentMaker(AbjadObject):
+    r'''Segment-maker baseclass.
+    '''
+
+    ### CLASS VARIABLES ###
+
+    __slots__ = (
+        )
