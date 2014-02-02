6e1c2d03b0e20e25414c696d6f2dd456335692c5
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-02-02 16:49:58 -0500

    Added stub special methods for SegmentMaker.
    These include `__eq__`, `__hash__`, `__illustrate__` and `__makenew__`.

--- experimental/tools/segmentmakertools/SegmentMaker.py
+++ experimental/tools/segmentmakertools/SegmentMaker.py
@@ -9,4 +9,80 @@ class SegmentMaker(AbjadObject):
     ### CLASS VARIABLES ###
 
     __slots__ = (
+        '_name',
         )
+
+    ### INITIALIZER ###
+
+    def __init__(
+        self,
+        name=None,
+        ):
+        self._name = name
+
+    ### SPECIAL METHODS ###
+
+    def __call__(self):
+        r'''Calls segment-maker.
+
+        Returns LilyPond file.
+        '''
+        raise NotImplementedError
+
+    def __eq__(self, expr):
+        r'''Is true if `expr` is a segment-maker with equivalent properties.
+        '''
+        from abjad.tools import systemtools
+        return systemtools.StorageFormatManager.compare(self, expr)
+
+    def __hash__(self):
+        r'''Hashes segment-maker.
+        '''
+        from abjad.tools import systemtools
+        hash_values = systemtools.StorageFormatManager.get_hash_values(self)
+        return hash(hash_values)
+
+    def __illustrate__(self):
+        r'''Illustrates segment-maker.
+
+        Returns LilyPond file.
+        '''
+        raise NotImplementedError
+
+    def __makenew__(self, *args, **kwargs):
+        r'''Make new segment-maker with `args` and `kwargs`.
+        '''
+        from abjad.tools import systemtools
+        manager = systemtools.StorageFormatManager
+        keyword_argument_dictionary = \
+            manager.get_keyword_argument_dictionary(self)
+        positional_argument_dictionary = \
+            manager.get_positional_argument_dictionary(self)
+        for key, value in kwargs.iteritems():
+            if key in positional_argument_dictionary:
+                positional_argument_dictionary[key] = value
+            elif key in keyword_argument_dictionary:
+                keyword_argument_dictionary[key] = value
+            else:
+                raise KeyError(key)
+        positional_argument_values = []
+        positional_argument_names = getattr(
+            self, '_positional_argument_names', None) or \
+            manager.get_positional_argument_names(self)
+        for positional_argument_name in positional_argument_names:
+            positional_argument_value = positional_argument_dictionary[
+                positional_argument_name]
+            positional_argument_values.append(positional_argument_value)
+        result = type(self)(
+            *positional_argument_values, **keyword_argument_dictionary)
+        return result
+
+    ### PUBLIC PROPERTIES ###
+
+    @property
+    def name(self):
+        r'''Gets segment name.
+
+        Returns string.
+        '''
+        return self._name
