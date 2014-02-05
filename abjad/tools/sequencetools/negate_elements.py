3e52d9e7c6463d77cc276faa2f68048a5cff2171
Trevor Bača <trevorbaca@gmail.com>
2014-02-05 15:20:17 -0500

    Collapsed four sequencetools functions.
        * sequencetools.negate_elements()
        * sequencetools.negate_elements_cyclically()
        * sequencetools.negate_absolute_value_of_elements_at_indices()
        * sequencetools.negate_absolute_value_of_elements_cyclically()
    
    Use sequencetools.negate_elements() for everything.
    
    CHANGE: should be included in the 2.15 change log.

---
+++ abjad/tools/sequencetools/negate_elements.py
@@ -0,0 +1,103 @@
+# -*- encoding: utf-8 -*-
+
+
+def negate_elements(sequence, absolute=False, indices=None, period=None):
+    '''Negates `sequence` elements.
+
+    ..  container:: example
+
+        Negates all elements:
+
+        ::
+
+            >>> sequence = [1, 2, 3, 4, 5, -6, -7, -8, -9, -10]
+            >>> sequencetools.negate_elements(sequence)
+            [-1, -2, -3, -4, -5, 6, 7, 8, 9, 10]
+
+    ..  container:: example
+
+        Negates elements at indices 0, 1 and 2:
+
+        ::
+
+            >>> sequence = [1, 2, 3, 4, 5, -6, -7, -8, -9, -10]
+            >>> sequencetools.negate_elements(sequence, indices=[0, 1, 2])
+            [-1, -2, -3, 4, 5, -6, -7, -8, -9, -10]
+
+    ..  container:: example
+
+        Negates elements at indices congruent to 0, 1 or 2 mod 5:
+
+        ::
+
+
+            >>> sequence = [1, 2, 3, 4, 5, -6, -7, -8, -9, -10]
+            >>> sequencetools.negate_elements(
+            ...     sequence, 
+            ...     indices=[0, 1, 2], 
+            ...     period=5,
+            ...     )
+            [-1, -2, -3, 4, 5, 6, 7, 8, -9, -10]
+
+    ..  container:: example
+
+        Negates the absolute value of all elements:
+
+        ::
+
+            >>> sequence = [1, 2, 3, 4, 5, -6, -7, -8, -9, -10]
+            >>> sequencetools.negate_elements(sequence, absolute=True)
+            [-1, -2, -3, -4, -5, -6, -7, -8, -9, -10]
+
+    ..  container:: example
+
+        Negates the absolute value elements at indices 0, 1 and 2:
+
+        ::
+
+            >>> sequence = [1, 2, 3, 4, 5, -6, -7, -8, -9, -10]
+            >>> sequencetools.negate_elements(
+            ...     sequence, 
+            ...     absolute=True,
+            ...     indices=[0, 1, 2],
+            ...     )
+            [-1, -2, -3, 4, 5, -6, -7, -8, -9, -10]
+
+    ..  container:: example
+
+        Negates the absolute value elements at indices congruent to 0, 1 or 2
+        mod 5:
+
+        ::
+
+            >>> sequence = [1, 2, 3, 4, 5, -6, -7, -8, -9, -10]
+            >>> sequencetools.negate_elements(
+            ...     sequence, 
+            ...     absolute=True,
+            ...     indices=[0, 1, 2],
+            ...     period=5,
+            ...     )
+            [-1, -2, -3, 4, 5, -6, -7, -8, -9, -10]
+
+    Returns newly constructed list.
+    '''
+
+    indices = indices or range(len(sequence))
+
+    if not isinstance(sequence, list):
+        raise TypeError
+
+    period = period or len(sequence)
+
+    result = []
+
+    for i, element in enumerate(sequence):
+        if (i in indices) or (period and i % period in indices):
+            if absolute:
+                result.append(-abs(element))
+            else:
+                result.append(-element)
+        else:
+            result.append(element)
+
+    return result
