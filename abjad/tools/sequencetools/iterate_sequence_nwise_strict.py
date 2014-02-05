fcf42bda0118a3f45618adbb00c32d195ffcfb82
Trevor Bača <trevorbaca@gmail.com>
2014-02-05 12:03:41 -0500

    Update to previous commit.
    CHANGE: should be noted in the 2.15 change log.

--- abjad/tools/sequencetools/iterate_sequence_nwise_strict.py
+++ abjad/tools/sequencetools/iterate_sequence_nwise_strict.py
@@ -4,18 +4,41 @@
 def iterate_sequence_nwise_strict(sequence, n=2):
     '''Iterates elements in `sequence` `n` at a time.
 
-    ::
+    ..  container:: example
 
-        >>> for x in sequencetools.iterate_sequence_nwise_strict(range(10), n=4):
-        ...     x
-        ...
-        (0, 1, 2, 3)
-        (1, 2, 3, 4)
-        (2, 3, 4, 5)
-        (3, 4, 5, 6)
-        (4, 5, 6, 7)
-        (5, 6, 7, 8)
-        (6, 7, 8, 9)
+        Iterates sequence by quadruples:
+
+        ::
+
+            >>> for x in sequencetools.iterate_sequence_nwise_strict(range(10), n=4):
+            ...     x
+            ...
+            (0, 1, 2, 3)
+            (1, 2, 3, 4)
+            (2, 3, 4, 5)
+            (3, 4, 5, 6)
+            (4, 5, 6, 7)
+            (5, 6, 7, 8)
+            (6, 7, 8, 9)
+
+    ..  container:: example
+
+        Iterates sequence by pairs:
+
+        ::
+
+            >>> for x in sequencetools.iterate_sequence_nwise_strict(range(10), n=2):
+            ...     x
+            ...
+            (0, 1)
+            (1, 2)
+            (2, 3)
+            (3, 4)
+            (4, 5)
+            (5, 6)
+            (6, 7)
+            (7, 8)
+            (8, 9)
 
     Returns generator.
     '''
--- abjad/tools/sequencetools/test/test_sequencetools_iterate_sequence_pairwise_wrapped.py
+++
@@ -1,9 +0,0 @@
-# -*- encoding: utf-8 -*-
-from abjad import *
-
-
-def test_sequencetools_iterate_sequence_pairwise_wrapped_01():
-
-    sequence = range(6)
-    pairs = sequencetools.iterate_sequence_pairwise_wrapped(sequence)
-    assert list(pairs) == [(0, 1), (1, 2), (2, 3), (3, 4), (4, 5), (5, 0)]
