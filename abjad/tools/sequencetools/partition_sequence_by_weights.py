a4468f8e052b297862de34e07d25153db0f97f7c
Trevor Bača <trevorbaca@gmail.com>
2014-02-05 16:37:40 -0500

    Removed two sequencetools functions.
        * sequencetools.partition_sequence_by_weights_at_most()
        * sequencetools.partition_sequence_by_weights_at_least()
    
    Use sequencetools.partition_sequence_by_weights() instead.
    
    CHANGE: should be noted in the 2.15 change log.

--- abjad/tools/sequencetools/partition_sequence_by_weights.py
+++ abjad/tools/sequencetools/partition_sequence_by_weights.py
@@ -1,4 +1,5 @@
 # -*- encoding: utf-8 -*-
+from abjad.tools import mathtools
 
 
 def partition_sequence_by_weights(
@@ -6,6 +7,7 @@ def partition_sequence_by_weights(
     weights, 
     cyclic=False, 
     overhang=False,
+    allow_part_weights=Exact,
     ):
     r'''Partitions `sequence` by `weights` exactly.
 
@@ -73,19 +75,319 @@ def partition_sequence_by_weights(
             ...     )
             [[3, 3, 3, 3], [4, 4, 4], [4, 5]]
 
+    ::
+
+        >>> sequence = [3, 3, 3, 3, 4, 4, 4, 4, 5, 5]
+
+    ..  container:: example
+    
+        **Example 1.** Partition sequence once by weights at most without 
+        overhang:
+
+        ::
+
+            >>> sequencetools.partition_sequence_by_weights(
+            ...     sequence, 
+            ...     [10, 4], 
+            ...     cyclic=False, 
+            ...     overhang=False,
+            ...     allow_part_weights=Less,
+            ...     )
+            [[3, 3, 3], [3]]
+
+    ..  container:: example
+    
+        **Example 2.** Partition sequence once by weights at most with 
+        overhang:
+
+        ::
+
+            >>> sequencetools.partition_sequence_by_weights(
+            ...     sequence, 
+            ...     [10, 4], 
+            ...     cyclic=False, 
+            ...     overhang=True,
+            ...     allow_part_weights=Less,
+            ...     )
+            [[3, 3, 3], [3], [4, 4, 4, 4, 5, 5]]
+
+    ..  container:: example
+    
+        **Example 3.** Partition sequence cyclically by weights at most 
+        without overhang:
+
+        ::
+
+            >>> sequencetools.partition_sequence_by_weights(
+            ...     sequence, 
+            ...     [10, 5], 
+            ...     cyclic=True, 
+            ...     overhang=False,
+            ...     allow_part_weights=Less,
+            ...     )
+            [[3, 3, 3], [3], [4, 4], [4], [4, 5], [5]]
+
+    ..  container:: example
+    
+        **Example 4.** Partition sequence cyclically by weights at most 
+        with overhang:
+
+        ::
+
+            >>> sequencetools.partition_sequence_by_weights(
+            ...     sequence, 
+            ...     [10, 5], 
+            ...     cyclic=True, 
+            ...     overhang=True,
+            ...     allow_part_weights=Less,
+            ...     )
+            [[3, 3, 3], [3], [4, 4], [4], [4, 5], [5]]
+
+    ::
+
+        >>> sequence = [3, 3, 3, 3, 4, 4, 4, 4, 5, 5]
+    
+    ..  container:: example
+
+        **Example 1.** Partition sequence once by weights at least without 
+        overhang:
+
+        ::
+
+            >>> sequencetools.partition_sequence_by_weights(
+            ...     sequence, 
+            ...     [10, 4], 
+            ...     cyclic=False, 
+            ...     overhang=False,
+            ...     allow_part_weights=More,
+            ...     )
+            [[3, 3, 3, 3], [4]]
+
+    ..  container:: example
+
+        **Example 2.** Partition sequence once by weights at least with 
+        overhang:
+
+        ::
+
+            >>> sequencetools.partition_sequence_by_weights(
+            ...     sequence, 
+            ...     [10, 4], 
+            ...     cyclic=False, 
+            ...     overhang=True,
+            ...     allow_part_weights=More,
+            ...     )
+            [[3, 3, 3, 3], [4], [4, 4, 4, 5, 5]]
+
+    ..  container:: example
+
+        **Example 3.** Partition sequence cyclically by weights at least 
+        without overhang:
+
+        ::
+
+            >>> sequencetools.partition_sequence_by_weights(
+            ...     sequence, 
+            ...     [10, 4], 
+            ...     cyclic=True, 
+            ...     overhang=False,
+            ...     allow_part_weights=More,
+            ...     )
+            [[3, 3, 3, 3], [4], [4, 4, 4], [5]]
+
+    ..  container:: example
+
+        **Example 4.** Partition sequence cyclically by weights at least with 
+        overhang:
+
+        ::
+
+            >>> sequencetools.partition_sequence_by_weights(
+            ...     sequence, 
+            ...     [10, 4], 
+            ...     cyclic=True, 
+            ...     overhang=True,
+            ...     allow_part_weights=More,
+            ...     )
+            [[3, 3, 3, 3], [4], [4, 4, 4], [5], [5]]
+
     Returns list sequence objects.
     '''
     from abjad.tools import sequencetools
 
-    candidate = sequencetools.split_sequence_by_weights(
-        sequence, 
-        weights, 
-        cyclic=cyclic, 
-        overhang=overhang,
-        )
-    flattened_candidate = sequencetools.flatten_sequence(candidate)
-    if flattened_candidate == sequence[:len(flattened_candidate)]:
-        return candidate
+    if allow_part_weights == Exact:
+        candidate = sequencetools.split_sequence_by_weights(
+            sequence, 
+            weights, 
+            cyclic=cyclic, 
+            overhang=overhang,
+            )
+        flattened_candidate = sequencetools.flatten_sequence(candidate)
+        if flattened_candidate == sequence[:len(flattened_candidate)]:
+            return candidate
+        else:
+            message = 'can not partition exactly.'
+            raise PartitionError(message)
+    elif allow_part_weights == More:
+        if not cyclic:
+            return _partition_sequence_once_by_weights_at_least(
+                sequence, weights, overhang=overhang)
+        else:
+            return _partition_sequence_cyclically_by_weights_at_least(
+                sequence, weights, overhang=overhang)
+    elif allow_part_weights == Less:
+        if not cyclic:
+            return _partition_sequence_once_by_weights_at_most(
+                sequence, 
+                weights, 
+                overhang=overhang,
+                )
+        else:
+            return _partition_sequence_cyclically_by_weights_at_most(
+                sequence, 
+                weights, 
+                overhang=overhang,
+                )
     else:
-        message = 'can not partition exactly.'
-        raise PartitionError(message)
+        message = 'not an ordinal value constant: {!r}.'
+        message = message.format(allow_part_weights)
+        raise ValueError(message)
+
+
+def _partition_sequence_once_by_weights_at_least(
+    sequence, 
+    weights, 
+    overhang=False,
+    ):
+    result = []
+    current_part = []
+    l_copy = sequence[:]
+    for num_weight, target_weight in enumerate(weights):
+        while True:
+            try:
+                x = l_copy.pop(0)
+            except IndexError:
+                if num_weight + 1 == len(weights):
+                    if current_part:
+                        result.append(current_part)
+                        break
+                message = 'too few elements in sequence.'
+                raise PartitionError(message)
+            current_part.append(x)
+            if target_weight <= mathtools.weight(current_part):
+                result.append(current_part)
+                current_part = []
+                break
+    if l_copy:
+        if overhang:
+            result.append(l_copy)
+    return result
+
+
+def _partition_sequence_cyclically_by_weights_at_least(
+    sequence, 
+    weights, 
+    overhang=False,
+    ):
+    l_copy = sequence[:]
+    result = []
+    current_part = []
+    target_weight_index = 0
+    len_weights = len(weights)
+    while l_copy:
+        target_weight = weights[target_weight_index % len_weights]
+        x = l_copy.pop(0)
+        current_part.append(x)
+        if target_weight <= mathtools.weight(current_part):
+            result.append(current_part)
+            current_part = []
+            target_weight_index += 1
+    assert not l_copy
+    if current_part:
+        if overhang:
+            result.append(current_part)
+    return result
+
+
+def _partition_sequence_once_by_weights_at_most(
+    sequence, 
+    weights, 
+    overhang=False,
+    ):
+    l_copy = sequence[:]
+    result = []
+    current_part = []
+    for target_weight in weights:
+        while True:
+            try:
+                x = l_copy.pop(0)
+            except IndexError:
+                message = 'too few elements in sequence.'
+                raise PartitionError(message)
+            current_weight = mathtools.weight(current_part)
+            candidate_weight = current_weight + mathtools.weight([x])
+            if candidate_weight < target_weight:
+                current_part.append(x)
+            elif candidate_weight == target_weight:
+                current_part.append(x)
+                result.append(current_part)
+                current_part = []
+                break
+            elif target_weight < candidate_weight:
+                if current_part:
+                    result.append(current_part)
+                    current_part = []
+                    l_copy.insert(0, x)
+                    break
+                else:
+                    message = 'elements in sequence too big.'
+                    raise PartitionError(message)
+            else:
+                message = 'candidate and target weights must compare.'
+                raise ValueError(message)
+    if overhang:
+        left_over = current_part + l_copy
+        if left_over:
+            result.append(left_over)
+    return result
+
+
+def _partition_sequence_cyclically_by_weights_at_most(
+    sequence, 
+    weights, 
+    overhang=False,
+    ):
+    result = []
+    current_part = []
+    current_target_weight_index = 0
+    current_target_weight = weights[current_target_weight_index]
+    l_copy = sequence[:]
+    while l_copy:
+        current_target_weight = \
+            weights[current_target_weight_index % len(weights)]
+        x = l_copy.pop(0)
+        current_part_weight = mathtools.weight(current_part)
+        candidate_part_weight = current_part_weight + mathtools.weight([x])
+        if candidate_part_weight < current_target_weight:
+            current_part.append(x)
+        elif candidate_part_weight == current_target_weight:
+            current_part.append(x)
+            result.append(current_part)
+            current_part = []
+            current_target_weight_index += 1
+        elif current_target_weight < candidate_part_weight:
+            if current_part:
+                l_copy.insert(0, x)
+                result.append(current_part)
+                current_part = []
+                current_target_weight_index += 1
+            else:
+                message = 'elements in sequence too big.'
+                raise PartitionError(message)
+        else:
+            message = 'candidate and target rates must compare.'
+            raise ValueError(message)
+    if current_part:
+        if overhang:
+            result.append(current_part)
+    return result
--- abjad/tools/sequencetools/test/test_sequencetools_partition_sequence_by_weights.py
+++ abjad/tools/sequencetools/test/test_sequencetools_partition_sequence_by_weights.py
@@ -48,3 +48,107 @@ def test_sequencetools_partition_sequence_by_weights_04():
         overhang=False,
         )
     assert groups == [[3, 3, 3, 3], [4, 4, 4]]
+
+
+def test_sequencetools_partition_sequence_by_weights_05():
+
+    sequence = [3, 3, 3, 3, 4, 4, 4, 4, 5, 5]
+    groups = sequencetools.partition_sequence_by_weights(
+        sequence, 
+        [10, 4], 
+        cyclic=False, 
+        overhang=True,
+        allow_part_weights=Less,
+        )
+    assert groups == [[3, 3, 3], [3], [4, 4, 4, 4, 5, 5]]
+
+
+def test_sequencetools_partition_sequence_by_weights_06():
+
+    sequence = [3, 3, 3, 3, 4, 4, 4, 4, 5, 5]
+    groups = sequencetools.partition_sequence_by_weights(
+        sequence, 
+        [10, 4], 
+        cyclic=False, 
+        overhang=False,
+        allow_part_weights=Less,
+        )
+    assert groups == [[3, 3, 3], [3]]
+
+
+def test_sequencetools_partition_sequence_by_weights_07():
+
+    sequence = [3, 3, 3, 3, 4, 4, 4, 4, 5, 5]
+    groups = sequencetools.partition_sequence_by_weights(
+        sequence, 
+        [10, 5], 
+        cyclic=True, 
+        overhang=True,
+        allow_part_weights=Less,
+        )
+    assert groups == [[3, 3, 3], [3], [4, 4], [4], [4, 5], [5]]
+
+
+def test_sequencetools_partition_sequence_by_weights_08():
+
+    sequence = [3, 3, 3, 3, 4, 4, 4, 4, 5]
+    groups = sequencetools.partition_sequence_by_weights(
+        sequence, 
+        [10, 5], 
+        cyclic=True, 
+        overhang=False,
+        allow_part_weights=Less,
+        )
+    assert groups == [[3, 3, 3], [3], [4, 4], [4]]
+
+
+def test_sequencetools_partition_sequence_by_weights_09():
+
+    sequence = [3, 3, 3, 3, 4, 4, 4, 4, 5, 5]
+    groups = sequencetools.partition_sequence_by_weights(
+        sequence, 
+        [10, 4], 
+        cyclic=False, 
+        overhang=True,
+        allow_part_weights=More,
+        )
+    assert groups == [[3, 3, 3, 3], [4], [4, 4, 4, 5, 5]]
+
+
+def test_sequencetools_partition_sequence_by_weights_10():
+
+    sequence = [3, 3, 3, 3, 4, 4, 4, 4, 5, 5]
+    groups = sequencetools.partition_sequence_by_weights(
+        sequence, 
+        [10, 4], 
+        cyclic=False, 
+        overhang=False,
+        allow_part_weights=More,
+        )
+    assert groups == [[3, 3, 3, 3], [4]]
+
+
+def test_sequencetools_partition_sequence_by_weights_11():
+
+    sequence = [3, 3, 3, 3, 4, 4, 4, 4, 5, 5]
+    groups = sequencetools.partition_sequence_by_weights(
+        sequence, 
+        [10, 4], 
+        cyclic=True, 
+        overhang=True,
+        allow_part_weights=More,
+        )
+    assert groups == [[3, 3, 3, 3], [4], [4, 4, 4], [5], [5]]
+
+
+def test_sequencetools_partition_sequence_by_weights_12():
+
+    sequence = [3, 3, 3, 3, 4, 4, 4, 4, 5, 5]
+    groups = sequencetools.partition_sequence_by_weights(
+        sequence, 
+        [10, 4], 
+        cyclic=True, 
+        overhang=False,
+        allow_part_weights=More,
+        )
+    assert groups == [[3, 3, 3, 3], [4], [4, 4, 4], [5]]
