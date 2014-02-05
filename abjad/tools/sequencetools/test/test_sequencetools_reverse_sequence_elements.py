028f71bab573c17bc30d2f55295605f16c63aebd
Trevor Bača <trevorbaca@gmail.com>
2014-02-05 11:30:49 -0500

    Removed sequencetools.reverse_sequence_elements().
    Use the recipe ...
    
        >>> [sequencetools.reverse_sequence(x) for x in sequence]
    
    ... instead.

--- abjad/tools/sequencetools/test/test_sequencetools_reverse_sequence_elements.py
+++
@@ -1,8 +0,0 @@
-# -*- encoding: utf-8 -*-
-from abjad import *
-
-
-def test_sequencetools_reverse_sequence_elements_01():
-
-    result = sequencetools.reverse_sequence_elements([1, (2, 3, 4), 5, (6, 7)])
-    assert result == [1, (4, 3, 2), 5, (7, 6)]
