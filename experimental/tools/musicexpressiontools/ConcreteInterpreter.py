b5204233fc34f6cfd717dc10d466ce14ca552f1d
Trevor Bača <trevorbaca@gmail.com>
2014-02-05 14:31:53 -0500

    Removed sequencetools function.
        * sequencetools.pair_duration_sequence_elements_with_input_pair_values()
    
    The functionality is now a private static method on the ConcreteInterpreter in
    experimental (which is the only place the functionality is used).
    
    Doesn't need to be included in the change log.

--- experimental/tools/musicexpressiontools/ConcreteInterpreter.py
+++ experimental/tools/musicexpressiontools/ConcreteInterpreter.py
@@ -36,6 +36,68 @@ class ConcreteInterpreter(Interpreter):
             print message.format(int(total_time))
         return self.score
 
+    ### PRIVATE METHODS ###
+
+    @staticmethod
+    def _pair_duration_sequence_elements_with_input_pair_values(
+        duration_sequence, 
+        input_pairs,
+        ):
+        r'''Pairs `duration_sequence` elements with the values of 
+        `input_pairs`.
+
+        ::
+
+            >>> from experimental import *
+
+        ::
+
+            >>> duration_sequence = [10, 10, 10, 10]
+            >>> input_pairs = [('red', 1), ('orange', 18), ('yellow', 200)]
+
+        ::
+
+            >>> interpreter = musicexpressiontools.ConcreteInterpreter
+            >>> interpreter._pair_duration_sequence_elements_with_input_pair_values(
+            ... duration_sequence, input_pairs)
+            [(10, 'red'), (10, 'orange'), (10, 'yellow'), (10, 'yellow')]
+
+        Returns a list of ``(element, value)`` output pairs.
+
+        The `input_pairs` argument must be a list of ``(value, duration)`` pairs.
+
+        The basic idea behind the function is model which input pair
+        value is in effect at the start of each element in `duration_sequence`.
+        '''
+        from abjad.tools import sequencetools
+
+        assert mathtools.all_are_numbers(duration_sequence)
+        assert mathtools.all_are_pairs(input_pairs)
+
+        output_pairs = []
+        current_element_start = 0
+        current_input_pair_index = 0
+        current_input_pair = input_pairs[current_input_pair_index]
+        current_input_pair_value = current_input_pair[0]
+        current_input_pair_duration = current_input_pair[-1]
+        current_input_pair_start = 0
+        current_input_pair_stop = \
+            current_input_pair_start + current_input_pair_duration
+
+        for element in duration_sequence:
+            while current_input_pair_stop <= current_element_start:
+                current_input_pair_index += 1
+                current_input_pair = input_pairs[current_input_pair_index]
+                current_input_pair_value = current_input_pair[0]
+                current_input_pair_duration = current_input_pair[-1]
+                current_input_pair_start = current_input_pair_stop
+                current_input_pair_stop += current_input_pair_duration
+            output_pair = (element, current_input_pair_value)
+            output_pairs.append(output_pair)
+            current_element_start += element
+
+        return output_pairs
+
     ### PUBLIC PROPERTIES ###
 
     @property
@@ -291,7 +353,7 @@ class ConcreteInterpreter(Interpreter):
         #self._debug_values(timespan_delimited_single_context_rhythm_set_expression_duration_pairs,
         #    'rhythm expression / duration pairs')
         merged_duration_timespan_delimited_single_context_rhythm_set_expression_pairs = \
-            sequencetools.pair_duration_sequence_elements_with_input_pair_values(
+            self._pair_duration_sequence_elements_with_input_pair_values(
             timespan_delimited_single_context_rhythm_set_expression_merged_durations,
             timespan_delimited_single_context_rhythm_set_expression_duration_pairs)
         # the first column in pairs is not used for anything further at all is discarded
--- experimental/tools/musicexpressiontools/test/test_ConcreteInterpreter__pair_duration_sequence_elements_with_input_pair_values.py
+++ experimental/tools/musicexpressiontools/test/test_ConcreteInterpreter__pair_duration_sequence_elements_with_input_pair_values.py
@@ -1,22 +1,24 @@
 # -*- encoding: utf-8 -*-
-from abjad import *
+from experimental import *
 
 
 def test_sequencetools_pair_duration_sequence_elements_with_input_pair_values_01():
 
-    duration_sequence = [10, 10, 10, 10]
 
-    result = sequencetools.pair_duration_sequence_elements_with_input_pair_values(
+    duration_sequence = [10, 10, 10, 10]
+    interpreter = musicexpressiontools.ConcreteInterpreter
+
+    result = interpreter._pair_duration_sequence_elements_with_input_pair_values(
         duration_sequence, [('red', 100)])
     assert result == [(10, 'red'), (10, 'red'), (10, 'red'), (10, 'red')]
 
-    result = sequencetools.pair_duration_sequence_elements_with_input_pair_values(
+    result = interpreter._pair_duration_sequence_elements_with_input_pair_values(
         duration_sequence, [('red', 1), ('blue', 99)])
 
-    result = sequencetools.pair_duration_sequence_elements_with_input_pair_values(
+    result = interpreter._pair_duration_sequence_elements_with_input_pair_values(
         duration_sequence, [('red', 1), ('blue', 1), ('green', 98)])
     assert result == [(10, 'red'), (10, 'green'), (10, 'green'), (10, 'green')]
 
-    result = sequencetools.pair_duration_sequence_elements_with_input_pair_values(
+    result = interpreter._pair_duration_sequence_elements_with_input_pair_values(
         duration_sequence, [('red', 100), ('blue', 100), ('green', 100)])
     assert result == [(10, 'red'), (10, 'red'), (10, 'red'), (10, 'red')]
