bead644ba9021a90649dd0a309b0514d6edd3817
Trevor Bača <trevorbaca@gmail.com>
2014-02-05 13:18:28 -0500

    Deprecated sequencetools.truncate_runs_in_sequence().
    Use sequencetools.remove_repeated_elements() instead.

--- abjad/tools/sequencetools/truncate_runs_in_sequence.py
+++ abjad/tools/sequencetools/truncate_runs_in_sequence.py
@@ -2,8 +2,7 @@
 import numbers
 
 
-# TODO: this duplicates sequencetools.remove_duplicate_elemens()
-#       sequencetools.remove_duplciate_elements() is a better name.
+# DEPRECATED: use sequencetools.remove_repeated_elements() instead
 def truncate_runs_in_sequence(sequence):
     '''Truncates subruns of like elements in `sequence` to length ``1``.
 
@@ -24,17 +23,20 @@ def truncate_runs_in_sequence(sequence):
     Returns new list.
     '''
 
-    if not isinstance(sequence, list):
-        raise TypeError
+#    if not isinstance(sequence, list):
+#        raise TypeError
+#
+#    assert all(isinstance(x, numbers.Number) for x in sequence)
+#
+#    result = []
+#
+#    if sequence:
+#        result.append(sequence[0])
+#        for element in sequence[1:]:
+#            if not element == result[-1]:
+#                result.append(element)
+#
+#    return result
 
-    assert all(isinstance(x, numbers.Number) for x in sequence)
-
-    result = []
-
-    if sequence:
-        result.append(sequence[0])
-        for element in sequence[1:]:
-            if not element == result[-1]:
-                result.append(element)
-
-    return result
+    from abjad.tools import sequencetools
+    return sequencetools.remove_repeated_elements(sequence)
