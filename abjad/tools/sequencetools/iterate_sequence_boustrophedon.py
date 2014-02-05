b27d62be039c8bed25ab9dd4db21350ac14a9c3f
Trevor Bača <trevorbaca@gmail.com>
2014-02-05 12:45:34 -0500

    Removed sequencetools.iterate_sequence_forward_and_backward_nonoverlapping().
    Use ...
    
        >>> sequencetools.iterate_sequence_boustrophedon(..., duplicates=True)
    
    ... instead.

--- abjad/tools/sequencetools/iterate_sequence_boustrophedon.py
+++ abjad/tools/sequencetools/iterate_sequence_boustrophedon.py
@@ -2,20 +2,48 @@
 
 
 def iterate_sequence_boustrophedon(sequence, duplicates=False):
-    '''Iterates `sequence` first forward and then backward,
-    with first and last elements appearing only once.
+    '''Iterates `sequence` boustrophedon.
 
-    ::
+    ..  container:: example
 
-        >>> list(sequencetools.iterate_sequence_boustrophedon([1, 2, 3, 4, 5]))
-        [1, 2, 3, 4, 5, 4, 3, 2]
+        Iterates `sequence` first forward and then backward.
+        Duplicates neither first nor last elements:
+
+        ::
+
+            >>> sequence = [1, 2, 3, 4, 5]
+            >>> generator = sequencetools.iterate_sequence_boustrophedon(
+            ...     sequence, duplicates=False)
+            >>> list(generator)
+            [1, 2, 3, 4, 5, 4, 3, 2]
+
+    ..  container:: example
+
+        Iterates `sequence` first forward and then backward.
+        Duplicates both first and last elements:
+
+        ::
+
+            >>> sequence = [1, 2, 3, 4, 5]
+            >>> generator = sequencetools.iterate_sequence_boustrophedon(
+            ...     sequence, duplicates=True)
+            >>> list(generator)
+            [1, 2, 3, 4, 5, 5, 4, 3, 2, 1]
 
     Returns generator.
     '''
 
-    sequence_copy = []
-    for x in sequence:
-        yield x
-        sequence_copy.append(x)
-    for x in reversed(sequence_copy[1:-1]):
-        yield x
+    if duplicates:
+        sequence_copy = []
+        for x in sequence:
+            yield x
+            sequence_copy.append(x)
+        for x in reversed(sequence_copy):
+            yield x
+    else:
+        sequence_copy = []
+        for x in sequence:
+            yield x
+            sequence_copy.append(x)
+        for x in reversed(sequence_copy[1:-1]):
+            yield x
--- abjad/tools/sequencetools/test/test_sequencetools_iterate_sequence_forward_and_backward_nonoverlapping.py
+++
@@ -1,9 +0,0 @@
-# -*- encoding: utf-8 -*-
-from abjad import *
-
-
-def test_sequencetools_iterate_sequence_forward_and_backward_nonoverlapping_01():
-
-    sequence_2 = list(sequencetools.iterate_sequence_forward_and_backward_nonoverlapping(xrange(1, 6)))
-
-    assert sequence_2 == [1, 2, 3, 4, 5, 5, 4, 3, 2, 1]
