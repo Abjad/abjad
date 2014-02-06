603ab33aee51b0ea10b2cf49c41fa8048ea001eb
Trevor Bača <trevorbaca@gmail.com>
2014-02-06 10:43:45 -0500

    Removed sequencetools.repeat_elements_cyclically().
    Use sequencetools.repeat_elements() instead.
    
    CHNAGE: should be noted in the change log.

--- abjad/tools/sequencetools/repeat_elements.py
+++ abjad/tools/sequencetools/repeat_elements.py
@@ -1,12 +1,16 @@
 # -*- encoding: utf-8 -*-
 
 
-def repeat_elements(sequence, indices, total):
+def repeat_elements(sequence, indices=None, period=None, total=1):
     '''Repeats `sequence` elements at `indices` to `total` length.
 
     ::
 
-        >>> sequencetools.repeat_elements(range(10), [6, 7, 8], 3)
+        >>> sequencetools.repeat_elements(
+        ...     range(10), 
+        ...     indices=[6, 7, 8], 
+        ...     total=3,
+        ...     )
         [0, 1, 2, 3, 4, 5, [6, 6, 6], [7, 7, 7], [8, 8, 8], 9]
 
     Returns list.
