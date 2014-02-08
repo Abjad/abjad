affc0eee2117c14725bb1da349cf1ecd72183ef1
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-02-08 16:42:00 -0500

    Modernized TextSpanner format contributions.

--- abjad/tools/spannertools/TextSpanner.py
+++ abjad/tools/spannertools/TextSpanner.py
@@ -1,5 +1,6 @@
 # -*- encoding: utf-8 -*-
 from abjad.tools.spannertools.Spanner import Spanner
+from abjad.tools.topleveltools import override
 
 
 class TextSpanner(Spanner):
@@ -55,20 +56,32 @@ class TextSpanner(Spanner):
     ### INITIALIZER ###
 
     def __init__(
-        self, 
+        self,
         overrides=None,
         ):
         Spanner.__init__(
-            self, 
+            self,
             overrides=overrides,
             )
 
     ### PRIVATE METHODS ###
 
-    def _format_right_of_leaf(self, leaf):
-        result = []
+    def _get_lilypond_format_bundle(self, leaf):
+        from abjad.tools import systemtools
+        lilypond_format_bundle = systemtools.LilyPondFormatBundle()
         if self._is_my_first_leaf(leaf):
-            result.append(r'\startTextSpan')
+            contributions = override(self)._list_format_contributions(
+                'override',
+                is_once=False,
+                )
+            lilypond_format_bundle.grob_overrides.extend(contributions)
+            string = r'\startTextSpan'
+            lilypond_format_bundle.right.spanner_starts.append(string)
         if self._is_my_last_leaf(leaf):
-            result.append(r'\stopTextSpan')
-        return result
+            contributions = override(self)._list_format_contributions(
+                'revert',
+                )
+            lilypond_format_bundle.grob_reverts.extend(contributions)
+            string = r'\stopTextSpan'
+            lilypond_format_bundle.right.spanner_stops.append(string)
+        return lilypond_format_bundle
