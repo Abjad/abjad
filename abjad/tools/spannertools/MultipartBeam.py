1c7110c19f67ca62d10ccb4b695fef1dfc69ab53
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-02-08 18:39:49 -0500

    Modernized MultipartBeam format contributions.

--- abjad/tools/spannertools/MultipartBeam.py
+++ abjad/tools/spannertools/MultipartBeam.py
@@ -46,52 +46,50 @@ class MultipartBeam(Beam):
 
     ### PRIVATE METHODS ###
 
-    def _format_right_of_leaf(self, leaf):
-        result = []
+    def _get_lilypond_format_bundle(self, leaf):
+        lilypond_format_bundle = self._get_basic_lilypond_format_bundle(leaf)
+        if not self._is_beamable_component(leaf):
+            return lilypond_format_bundle
         direction_string = ''
         if self.direction is not None:
             direction_string = '{} '.format(self.direction)
-        if self._is_beamable_component(leaf):
-            beamable_leaf_count = 0
-            for _ in self._leaves:
-                if self._is_beamable_component(_):
-                    beamable_leaf_count += 1
-            if 2 <= beamable_leaf_count:
-                previous_leaf = leaf._get_leaf(-1)
-                if previous_leaf not in self._leaves:
-                    previous_leaf = None
-                next_leaf = leaf._get_leaf(1)
-                if next_leaf not in self._leaves:
-                    next_leaf = None
-                if self._is_my_first_leaf(leaf):
-                    if next_leaf is not None:
+        beamable_leaf_count = 0
+        for _ in self._leaves:
+            if self._is_beamable_component(_):
+                beamable_leaf_count += 1
+        if 2 <= beamable_leaf_count:
+            previous_leaf = leaf._get_leaf(-1)
+            if previous_leaf not in self._leaves:
+                previous_leaf = None
+            next_leaf = leaf._get_leaf(1)
+            if next_leaf not in self._leaves:
+                next_leaf = None
+            start_piece = None
+            stop_piece = None
+            if self._is_my_first_leaf(leaf):
+                if next_leaf is not None:
+                    if self._is_beamable_component(next_leaf):
+                        start_piece = '{}['.format(direction_string)
+            else:
+                if previous_leaf is not None:
+                    if not self._is_beamable_component(previous_leaf):
                         if self._is_beamable_component(next_leaf):
-                            string = '{}['.format(direction_string)
-                            result.append(string)
-                else:
-                    if previous_leaf is not None:
-                        if not self._is_beamable_component(previous_leaf):
-                            if self._is_beamable_component(next_leaf):
-                                string = '{}['.format(direction_string)
-                                result.append(string)
-                if self._is_my_last_leaf(leaf):
-                    if previous_leaf is not None:
-                        if self._is_beamable_component(previous_leaf):
-                            result.append(']')
-                else:
+                            start_piece = '{}['.format(direction_string)
+            if self._is_my_last_leaf(leaf):
+                if previous_leaf is not None:
                     if self._is_beamable_component(previous_leaf):
-                        next_leaf = leaf._get_leaf(1)
-                        if next_leaf is not None:
-                            if not self._is_beamable_component(next_leaf):
-                                result.append(']')
-        return result
-
-    def _get_lilypond_format_bundle(self, leaf):
-        lilypond_format_bundle = self._get_basic_lilypond_format_bundle(leaf)
-        lilypond_format_bundle.get('before').spanners.extend(
-            self._format_before_leaf(leaf))
-        lilypond_format_bundle.get('right').spanners.extend(
-            self._format_right_of_leaf(leaf))
-        lilypond_format_bundle.get('after').spanners.extend(
-            self._format_after_leaf(leaf))
+                        stop_piece = ']'
+            else:
+                if self._is_beamable_component(previous_leaf):
+                    next_leaf = leaf._get_leaf(1)
+                    if next_leaf is not None:
+                        if not self._is_beamable_component(next_leaf):
+                            stop_piece = ']'
+            if start_piece and stop_piece:
+                lilypond_format_bundle.right.spanner_starts.extend([
+                    start_piece, stop_piece])
+            elif start_piece:
+                lilypond_format_bundle.right.spanner_starts.append(start_piece)
+            elif stop_piece:
+                lilypond_format_bundle.right.spanner_stops.append(stop_piece)
         return lilypond_format_bundle
