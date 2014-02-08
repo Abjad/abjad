8fa0cb4a4fd759e4bae7329736b2425eb5e5a699
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-02-08 16:41:45 -0500

    Modernized TrillSpanner format contributions.

--- abjad/tools/spannertools/TrillSpanner.py
+++ abjad/tools/spannertools/TrillSpanner.py
@@ -1,6 +1,7 @@
 # -*- encoding: utf-8 -*-
 from abjad.tools import pitchtools
 from abjad.tools.spannertools.Spanner import Spanner
+from abjad.tools.topleveltools import override
 
 
 class TrillSpanner(Spanner):
@@ -56,22 +57,30 @@ class TrillSpanner(Spanner):
     def _copy_keyword_args(self, new):
         new._pitch = self.pitch
 
-    def _format_before_leaf(self, leaf):
-        result = []
-        if self.pitch is not None:
-            if self._is_my_first_leaf(leaf):
-                result.append(r'\pitchedTrill')
-        return result
-
-    def _format_right_of_leaf(self, leaf):
-        result = []
+    def _get_lilypond_format_bundle(self, leaf):
+        from abjad.tools import systemtools
+        lilypond_format_bundle = systemtools.LilyPondFormatBundle()
         if self._is_my_first_leaf(leaf):
-            result.append(r'\startTrillSpan')
+            contributions = override(self)._list_format_contributions(
+                'override',
+                is_once=False,
+                )
+            lilypond_format_bundle.grob_overrides.extend(contributions)
+            string = r'\startTrillSpan'
+            lilypond_format_bundle.right.spanner_starts.append(string)
             if self.pitch is not None:
-                result.append(str(self.pitch))
+                string = r'\pitchedTrill'
+                lilypond_format_bundle.before.commands.append(string)
+                string = str(self.pitch)
+                lilypond_format_bundle.right.spanner_starts.append(string)
         if self._is_my_last_leaf(leaf):
-            result.append(r'\stopTrillSpan')
-        return result
+            contributions = override(self)._list_format_contributions(
+                'revert',
+                )
+            lilypond_format_bundle.grob_reverts.extend(contributions)
+            string = r'\stopTrillSpan'
+            lilypond_format_bundle.right.spanner_stops.append(string)
+        return lilypond_format_bundle
 
     ### PUBLIC PROPERTIES ###
 
