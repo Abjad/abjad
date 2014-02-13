9219a54056a8da96a29b0fc887f70e0115beafe1
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-02-12 20:32:53 -0500

    Added systemtools.ForbidUpdating context manager.
    ForbidUpdating is a convenience for preventing offset updates within a logical
    block of code, but returning to normal once completed.
    
        with systemtools.ForbidUpdating():
            mutate(my_score).rewrite_meter((4, 4))

---
+++ abjad/tools/systemtools/ForbidUpdating.py
@@ -0,0 +1,57 @@
+# -*- encoding: utf-8 -*-
+import time
+from abjad.tools.abctools import ContextManager
+
+
+class Timer(ContextManager):
+    r'''A context manager for forbidding score updates.
+    '''
+
+    ### CLASS VARIABLES ###
+
+    __slots__ = (
+        '_component',
+        )
+
+    ### INITIALIZER ###
+
+    def __init__(
+        self,
+        component=None,
+        ):
+        from abjad.tools import scoretools
+        prototype = (scoretools.Component, type(None))
+        assert isinstance(component, prototype)
+        self._component = component
+
+    ### SPECIAL METHODS ###
+
+    def __enter__(self):
+        r'''Enters context manager.
+
+        Returns context manager.
+        '''
+        if self.component is not None:
+            self.component._update_now(
+                offsets=True,
+                )
+            self.component._is_forbidden_to_update = True
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        r'''Exist context manager.
+
+        Returns none.
+        '''
+        if self.component is not None:
+            self.component._is_forbidden_to_update = False
+
+    ### PUBLIC PROPERTIES ###
+
+    @property
+    def component(self):
+        r'''Component of context manager.
+
+        Return component.
+        '''
+        return self._component
