f79de31e27cfceb78e4c6f70aebd5a94642599c1
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-02-17 14:13:15 -0500

    Added Tuning to indicatortools.

---
+++ abjad/tools/indicatortools/Tuning.py
@@ -0,0 +1,69 @@
+# -*- encoding: utf-8 -*-
+from abjad.tools.abctools import AbjadObject
+
+
+class Tuning(AbjadObject):
+    r'''Tuning indicator.
+
+    ::
+
+        >>> indicator = indicatortools.Tuning(
+        ...     pitches=('G3', 'D4', 'A4', 'E5'),
+        ...     )
+        >>> print format(indicator)
+        indicatortools.Tuning(
+            pitches=pitchtools.PitchSegment(
+                (
+                    pitchtools.NamedPitch('g'),
+                    pitchtools.NamedPitch("d'"),
+                    pitchtools.NamedPitch("a'"),
+                    pitchtools.NamedPitch("e''"),
+                    ),
+                item_class=pitchtools.NamedPitch,
+                ),
+            )
+
+    '''
+
+    ### CLASS VARIABLES ###
+
+    __slots__ = (
+        '_pitches',
+        )
+
+    ### INITIALIZER ###
+
+    def __init__(self,
+        pitches=None,
+        ):
+        from abjad.tools import pitchtools
+        if pitches is not None:
+            pitches = pitchtools.PitchSegment(
+                items=pitches,
+                item_class=pitchtools.NamedPitch,
+                )
+        self._pitches = pitches
+
+    ### PUBLIC PROPERTIES ###
+
+    @property
+    def pitches(self):
+        r'''Gets tuning pitches.
+
+        ::
+
+            >>> pitches = indicator.pitches
+            >>> print format(pitches)
+            pitchtools.PitchSegment(
+                (
+                    pitchtools.NamedPitch('g'),
+                    pitchtools.NamedPitch("d'"),
+                    pitchtools.NamedPitch("a'"),
+                    pitchtools.NamedPitch("e''"),
+                    ),
+                item_class=pitchtools.NamedPitch,
+                )
+
+        Return pitch segment.
+        '''
+        return self._pitches
