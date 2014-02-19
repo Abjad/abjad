2f1d31e0ad6ade4150022da61a797793f9c1ce9b
Trevor Bača <trevorbaca@gmail.com>
2014-02-19 09:32:19 -0500

    Updated instrumentation test.

--- scoremanager/editors/test/test_PerformerEditor_move_instrument.py
+++ scoremanager/editors/test/test_PerformerEditor_move_instrument.py
@@ -1,33 +1,31 @@
 # -*- encoding: utf-8 -*-
-import pytest
 from abjad import *
-from abjad.tools.instrumenttools import *
 import scoremanager
 
 
 def test_PerformerEditor_move_instrument_01():
     r'''Quit, back, home, score & junk all work.
     '''
-    pytest.skip('remove custom score name.')
 
     score_manager = scoremanager.core.ScoreManager()
-    string = "l'arch score~setup instrumentation flutist move q"
+    string = 'green~example~score score~setup instrumentation flutist move q'
     score_manager._run(pending_user_input=string)
     assert score_manager.session.io_transcript.signature == (11,)
 
-    string = "l'arch score~setup instrumentation flutist move b q"
+    string = 'green~example~score score~setup instrumentation flutist move b q'
     score_manager._run(pending_user_input=string)
     assert score_manager.session.io_transcript.signature == (13, (8, 11))
 
-    string = "l'arch score~setup instrumentation flutist move h q"
+    string = 'green~example~score score~setup instrumentation flutist move h q'
     score_manager._run(pending_user_input=string)
     assert score_manager.session.io_transcript.signature == (13, (0, 11))
 
-    string = "l'arch score~setup instrumentation flutist move s q"
+    string = 'green~example~score score~setup instrumentation flutist move s q'
     score_manager._run(pending_user_input=string)
     assert score_manager.session.io_transcript.signature == (13, (2, 11))
 
-    string = "l'arch score~setup instrumentation flutist move foo q"
+    string = 'green~example~score score~setup instrumentation'
+    string += ' flutist move foo q'
     score_manager._run(pending_user_input=string)
     assert score_manager.session.io_transcript.signature == (13,)
 
@@ -38,4 +36,9 @@ def test_PerformerEditor_move_instrument_02():
 
     editor = scoremanager.editors.PerformerEditor()
     editor._run(pending_user_input='add 1 add 2 move 1 2 q')
-    assert editor.target == Performer(instruments=[AltoVoice(), Accordion()])
+    assert editor.target == instrumenttools.Performer(
+        instruments=[
+            instrumenttools.AltoVoice(), 
+            instrumenttools.Accordion(),
+            ],
+        )
--- scoremanager/scorepackages/green_example_score/instrumentation.py
+++ scoremanager/scorepackages/green_example_score/instrumentation.py
@@ -3,18 +3,98 @@ from abjad import *
 
 
 instrumentation=instrumenttools.InstrumentationSpecifier(
-	performers=instrumenttools.PerformerInventory([
-		instrumenttools.Performer(
-			name='flutist',
-			instruments=instrumenttools.InstrumentInventory([
-				instrumenttools.AltoFlute()
-				])
-			),
-		instrumenttools.Performer(
-			name='guitarist',
-			instruments=instrumenttools.InstrumentInventory([
-				instrumenttools.Guitar()
-				])
-			)
-		])
-	)
+    performers=instrumenttools.PerformerInventory(
+        [
+            instrumenttools.Performer(
+                name='flutist',
+                instruments=instrumenttools.InstrumentInventory(
+                    [
+                        instrumenttools.AltoFlute(
+                            instrument_name='alto flute',
+                            short_instrument_name='alt. fl.',
+                            instrument_name_markup=markuptools.Markup(
+                                contents=('Alto flute',),
+                                ),
+                            short_instrument_name_markup=markuptools.Markup(
+                                contents=('Alt. fl.',),
+                                ),
+                            allowable_clefs=indicatortools.ClefInventory(
+                                [
+                                    indicatortools.Clef(
+                                        name='treble',
+                                        ),
+                                    ]
+                                ),
+                            pitch_range=pitchtools.PitchRange(
+                                '[G3, G6]'
+                                ),
+                            sounding_pitch_of_written_middle_c=pitchtools.NamedPitch('g'),
+                            ),
+                        instrumenttools.Flute(
+                            instrument_name='flute',
+                            short_instrument_name='fl.',
+                            instrument_name_markup=markuptools.Markup(
+                                contents=('Flute',),
+                                ),
+                            short_instrument_name_markup=markuptools.Markup(
+                                contents=('Fl.',),
+                                ),
+                            allowable_clefs=indicatortools.ClefInventory(
+                                [
+                                    indicatortools.Clef(
+                                        name='treble',
+                                        ),
+                                    ]
+                                ),
+                            pitch_range=pitchtools.PitchRange(
+                                '[C4, D7]'
+                                ),
+                            sounding_pitch_of_written_middle_c=pitchtools.NamedPitch("c'"),
+                            ),
+                        ]
+                    ),
+                ),
+            instrumenttools.Performer(
+                name='guitarist',
+                instruments=instrumenttools.InstrumentInventory(
+                    [
+                        instrumenttools.Guitar(
+                            instrument_name='guitar',
+                            short_instrument_name='gt.',
+                            instrument_name_markup=markuptools.Markup(
+                                contents=('Guitar',),
+                                ),
+                            short_instrument_name_markup=markuptools.Markup(
+                                contents=('Gt.',),
+                                ),
+                            allowable_clefs=indicatortools.ClefInventory(
+                                [
+                                    indicatortools.Clef(
+                                        name='treble',
+                                        ),
+                                    ]
+                                ),
+                            default_tuning=indicatortools.Tuning(
+                                pitches=pitchtools.PitchSegment(
+                                    (
+                                        pitchtools.NamedPitch('e,'),
+                                        pitchtools.NamedPitch('a,'),
+                                        pitchtools.NamedPitch('d'),
+                                        pitchtools.NamedPitch('g'),
+                                        pitchtools.NamedPitch('b'),
+                                        pitchtools.NamedPitch("e'"),
+                                        ),
+                                    item_class=pitchtools.NamedPitch,
+                                    ),
+                                ),
+                            pitch_range=pitchtools.PitchRange(
+                                '[E2, E5]'
+                                ),
+                            sounding_pitch_of_written_middle_c=pitchtools.NamedPitch('c'),
+                            ),
+                        ]
+                    ),
+                ),
+            ]
+        ),
+    )
