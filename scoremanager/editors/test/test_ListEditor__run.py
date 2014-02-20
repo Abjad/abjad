06fff4a6221fa918eec2d7ed900df3f0d099079b
Trevor Bača <trevorbaca@gmail.com>
2014-02-20 10:15:42 -0500

    Renamed test file.
    I don't understand why the editors/test/ directory appears as editors/TEST/ in
    the commit message.

--- scoremanager/editors/test/test_ListEditor__run.py
+++ scoremanager/editors/test/test_ListEditor__run.py
@@ -3,7 +3,7 @@ from abjad import *
 import scoremanager
 
 
-def test_ListEditor_run_01():
+def test_ListEditor__run_01():
 
     editor = scoremanager.editors.ListEditor()
     editor._run(pending_user_input="17 99 'foo' done q", is_autoadding=True)
