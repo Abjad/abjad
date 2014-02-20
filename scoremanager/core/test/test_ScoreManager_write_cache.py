602f365eea563df1e5734f60bcbd8ff7a3d0d82b
Trevor Bača <trevorbaca@gmail.com>
2014-02-20 17:01:07 -0500

    Wrote cache test.

--- scoremanager/core/test/test_ScoreManager_write_cache.py
+++ scoremanager/core/test/test_ScoreManager_write_cache.py
@@ -1,13 +1,13 @@
 # -*- encoding: utf-8 -*-
-import pytest
 from abjad import *
 import scoremanager
 
 
 def test_ScoreManager_write_cache_01():
-    pytest.skip('figure out good assert')
     
     score_manager = scoremanager.core.ScoreManager()
     score_manager._run(pending_user_input='cw default q')
-    string = 'Score manager - all scores'
-    assert score_manager._session.io_transcript.last_menu_title == string
+
+    string = 'Cache written.'
+    transcript = score_manager._session.io_transcript
+    assert transcript[-3].title.startswith(string)
