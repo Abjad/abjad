86a70b410e5d943d063fca6341c0860a4b9c3b9f
Trevor Bača <trevorbaca@gmail.com>
2014-02-25 12:58:20 -0500

    Renamed material package.
    OLD: 'red_notes'.
    
    NEW: 'example_notes'.

--- scoremanager/materialpackages/example_notes/material_definition.py
+++ scoremanager/materialpackages/example_notes/material_definition.py
@@ -3,4 +3,4 @@ from abjad import *
 output_material_module_import_statements = ['from abjad import *']
 
 
-red_notes = scoretools.make_notes(6 * [2, 4, 5], [(1, 16), (1, 8)])
+example_notes = scoretools.make_notes(6 * [2, 4, 5], [(1, 16), (1, 8)])
