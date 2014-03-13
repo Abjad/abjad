535880a2c9c8209b7c62fe48183c2e10fd8b19ac
Trevor Bača <trevorbaca@gmail.com>
2014-03-13 13:12:13 -0400

    Updated Red Example Score setup.

--- scoremanager/scores/red_example_score/__metadata__.py
+++ scoremanager/scores/red_example_score/__metadata__.py
@@ -3,6 +3,7 @@ import collections
 
 
 metadata = collections.OrderedDict([
+    ('catalog_number', 'Op. 12'),
     ('forces_tagline', 'for six players'),
     ('is_example', True),
     ('title', 'Red Example Score'),
