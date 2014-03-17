d47b303bed2c04f0ca9c7c3ee56058ee42265b73
Trevor Bača <trevorbaca@gmail.com>
2014-03-17 15:20:52 -0700

    Wrote score creation test.

--- scoremanager/wranglers/test/test_ScorePackageWrangler_make_new_score.py
+++ scoremanager/wranglers/test/test_ScorePackageWrangler_make_new_score.py
@@ -1,22 +1,30 @@
 # -*- encoding: utf-8 -*-
 import os
-import pytest
 import shutil
 from abjad import *
 import scoremanager
 
 
-def test_ScorePackageWrangler__run_01():
-    r'''Create score package. Remove score package.
+def test_ScorePackageWrangler_make_new_score_01():
+    r'''Create score package.
     '''
-    pytest.skip('unskip after deciding about cache.')
 
     score_manager = scoremanager.core.ScoreManager()
     path = os.path.join(
-        score_manager._configuration.abjad_score_packages_directory_path,
-        'testscore',
+        score_manager._configuration.user_score_packages_directory_path,
+        'test_score',
         )
-    input_ = 'new test~score testscore 2012 q'
+    input_ = 'new test~score q'
+    directory_entries = [
+        '__init__.py',
+        '__metadata__.py',
+        'build',
+        'distribution',
+        'makers',
+        'materials',
+        'segments',
+        'stylesheets',
+        ]
 
     assert not os.path.exists(path)
     try:
@@ -25,11 +33,7 @@ def test_ScorePackageWrangler__run_01():
         session = scoremanager.core.Session()
         manager = scoremanager.managers.ScorePackageManager
         manager = manager(path=path, session=session)
-        assert manager.annotated_title == 'Test score (2012)'
-        assert manager.composer is None
-        assert manager.instrumentation is None
-        input_ = 'test removescore clobberscore remove default q'
-        score_manager._run(pending_user_input=input_, is_test=True)
+        assert manager._list() == directory_entries
     finally:
         if os.path.exists(path):
             shutil.rmtree(path)
