c49f96f92b38c17fc27e745e4a16577465f8117a
Trevor Bača <trevorbaca@gmail.com>
2014-03-17 18:37:41 -0700

    Reactivated ScorePackageManager.edit_title() test.

--- scoremanager/managers/test/test_ScorePackageManager_edit_title.py
+++ scoremanager/managers/test/test_ScorePackageManager_edit_title.py
@@ -1,26 +1,22 @@
 # -*- encoding: utf-8 -*-
-import pytest
 from abjad import *
 import scoremanager
-pytest.skip('unskip me after making decision about cache.')
 
 
 def test_ScorePackageManager_edit_title_01():
 
-    try:
-        score_manager = scoremanager.core.ScoreManager()
-        input_ = 'green~example~score setup title Foo q'
-        score_manager._run(pending_user_input=input_, is_test=True)
-        assert score_manager._transcript.signature == (9,)
-        string = 'Green Example Score (2013) - setup'
-        assert score_manager._transcript[-5].title == string
-        string = 'Foo (2013) - setup'
-        assert score_manager._transcript.last_title == string
-    finally:
-        input_ = 'foo setup title Green~Example~Score q'
-        score_manager._run(pending_user_input=input_, is_test=True)
-        assert score_manager._transcript.signature == (9,)
-        string = 'Foo (2013) - setup'
-        assert score_manager._transcript[-5].title == string
-        string = 'Green Example Score (2013) - setup'
-        assert score_manager._transcript.last_title == string
+    score_manager = scoremanager.core.ScoreManager()
+    input_ = 'étude~example~score setup title Foo q'
+    score_manager._run(pending_user_input=input_, is_test=True)
+    string = 'Étude Example Score (2013) - setup'
+    assert score_manager._transcript[-5].title == string
+    string = 'Foo (2013) - setup'
+    assert score_manager._transcript.last_title == string
+
+    input_ = 'foo setup title Étude~Example~Score q'
+    score_manager._run(pending_user_input=input_, is_test=True)
+    assert score_manager._transcript.signature == (9,)
+    string = 'Foo (2013) - setup'
+    assert score_manager._transcript[-5].title == string
+    string = 'Étude Example Score (2013) - setup'
+    assert score_manager._transcript.last_title == string
