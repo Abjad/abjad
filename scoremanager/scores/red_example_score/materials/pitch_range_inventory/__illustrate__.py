d6e5f960a424dc321b9d9425aa0d8b87b4272629
Trevor Bača <trevorbaca@gmail.com>
2014-03-19 08:39:42 -0400

    Removed two unused illustrate modules.

--- scoremanager/scores/red_example_score/materials/pitch_range_inventory/__illustrate__.py
+++
@@ -1,5 +0,0 @@
-from abjad import *
-from red_example_score.materials.pitch_range_inventory.output import pitch_range_inventory
-
-
-#illustration = lilypondfiletools.make_basic_lilypond_file(score)
--- scoremanager/scores/red_example_score/materials/tempo_inventory/__illustrate__.py
+++
@@ -1,5 +0,0 @@
-from abjad import *
-from red_example_score.materials.tempo_inventory.output import tempo_inventory
-
-
-#illustration = lilypondfiletools.make_basic_lilypond_file(score)
