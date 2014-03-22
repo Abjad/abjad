59b0f395afa04f2b1f37e657fedc4fdb2aa2c93b
Trevor Bača <trevorbaca@gmail.com>
2014-03-22 11:47:48 -0400

    Hoisted metadata methods from PackageManager to DirectoryManager.
    Any directory may house a __metadata__ module.

--- scoremanager/managers/DirectoryManager.py
+++ scoremanager/managers/DirectoryManager.py
@@ -1,7 +1,10 @@
 # -*- encoding: utf-8 -*-
+import collections
 import os
 import subprocess
+import traceback
 from abjad.tools import sequencetools
+from abjad.tools import stringtools
 from scoremanager.managers.Manager import Manager
 
 
@@ -23,16 +26,33 @@ class DirectoryManager(Manager):
     ### PRIVATE PROPERTIES ###
 
     @property
+    def _metadata_module_path(self):
+        file_path = os.path.join(self._path, '__metadata__.py')
+        return file_path
+
+    @property
     def _user_input_to_action(self):
         superclass = super(DirectoryManager, self)
         result = superclass._user_input_to_action
         result = result.copy()
         result.update({
+            'mda': self.add_metadatum,
+            'mdg': self.get_metadatum,
+            'mdrm': self.remove_metadatum,
+            'mdmro': self.view_metadata_module,
+            'mdmrm': self.remove_metadata_module,
+            'mdmrw': self.rewrite_metadata_module,
             'pwd': self.pwd,
             })
         return result
 
     ### PRIVATE METHODS ###
+
+    def _add_metadatum(self, metadatum_name, metadatum_value):
+        assert stringtools.is_snake_case_string(metadatum_name)
+        metadata = self._get_metadata()
+        metadata[metadatum_name] = metadatum_value
+        self.rewrite_metadata_module(metadata, prompt=False)
 
     def _get_file_manager(self, file_path):
         from scoremanager import managers
@@ -41,6 +61,27 @@ class DirectoryManager(Manager):
             session=self._session,
             )
         return file_manager
+
+    def _get_metadata(self):
+        metadata = None
+        if os.path.isfile(self._metadata_module_path):
+            file_pointer = open(self._metadata_module_path, 'r')
+            file_contents_string = file_pointer.read()
+            file_pointer.close()
+            try:
+                exec(file_contents_string)
+            except:
+                message = 'Can not interpret metadata module: {!r}.'
+                message = message.format(self)
+                print message
+            metadata = locals().get('metadata')
+        metadata = metadata or collections.OrderedDict()
+        return metadata
+
+    def _get_metadatum(self, metadatum_name):
+        metadata = self._get_metadata()
+        metadatum = metadata.get(metadatum_name, None)
+        return metadatum
 
     def _handle_main_menu_result(self, result):
         if result in self._user_input_to_action:
@@ -87,6 +128,54 @@ class DirectoryManager(Manager):
         self._make_asset_menu_section(menu)
         return menu
 
+    @staticmethod
+    def _make_metadata_lines(metadata):
+        if metadata:
+            lines = []
+            for key, value in sorted(metadata.iteritems()):
+                key = repr(key)
+                if hasattr(value, '_get_multiline_repr'):
+                    repr_lines = \
+                        value._get_multiline_repr(include_tools_package=True)
+                    value = '\n    '.join(repr_lines)
+                    lines.append('({}, {})'.format(key, value))
+                else:
+                    if hasattr(value, '_storage_format_specification'):
+                        string = format(value)
+                    else:
+                        string = repr(value)
+                    lines.append('({}, {})'.format(key, string))
+            lines = ',\n    '.join(lines)
+            result = 'metadata = collections.OrderedDict([\n    {},\n    ])'
+            result = result.format(lines)
+        else:
+            result = 'metadata = collections.OrderedDict([])'
+        return result
+
+    def _make_metadata_menu_entries(self):
+        result = []
+        metadata = self._get_metadata()
+        for key in sorted(metadata):
+            display_string = key.replace('_', ' ')
+            result.append((display_string, None, metadata[key], key))
+        return result
+
+    def _remove_metadatum(self, metadatum_name):
+        metadata = self._get_metadata()
+        was_removed = False
+        try:
+            del(metadata[metadatum_name])
+            was_removed = True
+        except KeyError:
+            message = 'metadatum not found: {!r}.'
+            message = message.format(metadatum_name)
+            self._io_manager.proceed(message)
+        if was_removed:
+            self.rewrite_metadata_module(metadata, prompt=False)
+            message = 'metadatum removed: {!r}.'
+            message = message.format(metadatum_name)
+            self._io_manager.proceed(message)
+
     def _run_asset_manager(
         self,
         path,
@@ -97,7 +186,57 @@ class DirectoryManager(Manager):
             )
         manager._run()
 
+    # TODO: change to self._write_metadata_module()
+    def _write_metadata(self, metadata):
+        lines = []
+        lines.append(self._unicode_directive + '\n')
+        lines.append('import collections\n')
+        lines.append('\n\n')
+        metadata_lines = self._make_metadata_lines(metadata) 
+        lines.extend(metadata_lines)
+        lines = ''.join(lines)
+        with file(self._metadata_module_path, 'w') as file_pointer:
+            file_pointer.write(lines)
+
     ### PUBLIC METHODS ###
+
+    def add_metadatum(self):
+        r'''Adds metadatum to metadata module.
+
+        Returns none.
+        '''
+        getter = self._io_manager.make_getter(where=self._where)
+        getter.append_snake_case_string('metadatum name', allow_empty=False)
+        getter.append_expr('metadatum value')
+        result = getter._run()
+        if self._session._backtrack():
+            return
+        if result:
+            metadatum_name, metadatum_value = result
+            self._add_metadatum(metadatum_name, metadatum_value)
+
+    def edit_metadata_module(self):
+        r'''Edits metadata module.
+
+        Returns none.
+        '''
+        file_path = self._metadata_module_path
+        if os.path.isfile(file_path):
+            self._io_manager.edit(file_path)
+
+    def get_metadatum(self):
+        r'''Gets metadatum from metadata module.
+
+        Returns none.
+        '''
+        getter = self._io_manager.make_getter(where=self._where)
+        getter.append_string('metadatum name')
+        result = getter._run()
+        if self._session._backtrack():
+            return
+        metadatum = self._get_metadatum(result)
+        message = '{!r}'.format(metadatum)
+        self._io_manager.proceed(message=message)
 
     def pwd(self):
         '''Displays path of current working directory.
@@ -109,3 +248,59 @@ class DirectoryManager(Manager):
         lines.append('')
         self._io_manager.display(lines)
         self._session._hide_next_redraw = True
+
+    def remove_metadata_module(self, prompt=True):
+        r'''Removes metadata module.
+
+        Returns none.
+        '''
+        if os.path.isfile(self._metadata_module_path):
+            if prompt:
+                message = 'remove metadata module?'
+                if not self._io_manager.confirm(message):
+                    return
+            os.remove(self._metadata_module_path)
+            line = 'metadata module removed.'
+            self._io_manager.proceed(
+                line, 
+                prompt=prompt,
+                )
+
+    def remove_metadatum(self):
+        r'''Removes metadatum from meatdata module.
+
+        Returns none.
+        '''
+        getter = self._io_manager.make_getter(where=self._where)
+        getter.append_string('metadatum name')
+        result = getter._run()
+        if self._session._backtrack():
+            return
+        if result:
+            metadatum_name = result
+            self._remove_metadatum(metadatum_name)
+
+    def rewrite_metadata_module(
+        self, 
+        metadata=None, 
+        prompt=True,
+        ):
+        r'''Rewrites metadata module.
+
+        Returns none.
+        '''
+        if metadata is None:
+            metadata = self._get_metadata()
+        self._write_metadata(metadata)
+        if prompt:
+            message = 'rewrote metadata module.'
+            self._io_manager.proceed(message)
+
+    def view_metadata_module(self):
+        r'''Views metadata module.
+
+        Returns none.
+        '''
+        file_path = self._metadata_module_path
+        if os.path.isfile(file_path):
+            self._io_manager.view(file_path)
--- scoremanager/managers/PackageManager.py
+++ scoremanager/managers/PackageManager.py
@@ -40,10 +40,10 @@ class PackageManager(DirectoryManager):
         if self._path is not None:
             return os.path.join(self._path, '__init__.py')
 
-    @property
-    def _metadata_module_path(self):
-        file_path = os.path.join(self._path, '__metadata__.py')
-        return file_path
+#    @property
+#    def _metadata_module_path(self):
+#        file_path = os.path.join(self._path, '__metadata__.py')
+#        return file_path
 
     @property
     def _space_delimited_lowercase_name(self):
@@ -61,12 +61,6 @@ class PackageManager(DirectoryManager):
             'inrm': self.remove_initializer,
             'ins': self.write_initializer_stub,
             'inro': self.view_initializer,
-            'mda': self.add_metadatum,
-            'mdg': self.get_metadatum,
-            'mdrm': self.remove_metadatum,
-            'mdmro': self.view_metadata_module,
-            'mdmrm': self.remove_metadata_module,
-            'mdmrw': self.rewrite_metadata_module,
             'ren': self.rename,
             'rm': self.remove,
             })
@@ -79,33 +73,6 @@ class PackageManager(DirectoryManager):
 
     ### PRIVATE METHODS ###
 
-    def _add_metadatum(self, metadatum_name, metadatum_value):
-        assert stringtools.is_snake_case_string(metadatum_name)
-        metadata = self._get_metadata()
-        metadata[metadatum_name] = metadatum_value
-        self.rewrite_metadata_module(metadata, prompt=False)
-
-    def _get_metadata(self):
-        metadata = None
-        if os.path.isfile(self._metadata_module_path):
-            file_pointer = open(self._metadata_module_path, 'r')
-            file_contents_string = file_pointer.read()
-            file_pointer.close()
-            try:
-                exec(file_contents_string)
-            except:
-                message = 'Can not interpret metadata module: {!r}.'
-                message = message.format(self)
-                print message
-            metadata = locals().get('metadata')
-        metadata = metadata or collections.OrderedDict()
-        return metadata
-
-    def _get_metadatum(self, metadatum_name):
-        metadata = self._get_metadata()
-        metadatum = metadata.get(metadatum_name, None)
-        return metadatum
-
     def _make_main_menu(self, where=None, name='package manager'):
         where = where or self._where
         menu = self._io_manager.make_menu(
@@ -114,107 +81,10 @@ class PackageManager(DirectoryManager):
             )
         return menu
 
-    @staticmethod
-    def _make_metadata_lines(metadata):
-        if metadata:
-            lines = []
-            for key, value in sorted(metadata.iteritems()):
-                key = repr(key)
-                if hasattr(value, '_get_multiline_repr'):
-                    repr_lines = \
-                        value._get_multiline_repr(include_tools_package=True)
-                    value = '\n    '.join(repr_lines)
-                    lines.append('({}, {})'.format(key, value))
-                else:
-                    if hasattr(value, '_storage_format_specification'):
-                        string = format(value)
-                    else:
-                        string = repr(value)
-                    lines.append('({}, {})'.format(key, string))
-            lines = ',\n    '.join(lines)
-            result = 'metadata = collections.OrderedDict([\n    {},\n    ])'
-            result = result.format(lines)
-        else:
-            result = 'metadata = collections.OrderedDict([])'
-        return result
-
-    def _make_metadata_menu_entries(self):
-        result = []
-        metadata = self._get_metadata()
-        for key in sorted(metadata):
-            display_string = key.replace('_', ' ')
-            result.append((display_string, None, metadata[key], key))
-        return result
-
-    def _remove_metadatum(self, metadatum_name):
-        metadata = self._get_metadata()
-        was_removed = False
-        try:
-            del(metadata[metadatum_name])
-            was_removed = True
-        except KeyError:
-            message = 'metadatum not found: {!r}.'
-            message = message.format(metadatum_name)
-            self._io_manager.proceed(message)
-        if was_removed:
-            self.rewrite_metadata_module(metadata, prompt=False)
-            message = 'metadatum removed: {!r}.'
-            message = message.format(metadatum_name)
-            self._io_manager.proceed(message)
-
     def _run_first_time(self, **kwargs):
         self._run(**kwargs)
 
-    def _write_metadata(self, metadata):
-        lines = []
-        lines.append(self._unicode_directive + '\n')
-        lines.append('import collections\n')
-        lines.append('\n\n')
-        metadata_lines = self._make_metadata_lines(metadata) 
-        lines.extend(metadata_lines)
-        lines = ''.join(lines)
-        with file(self._metadata_module_path, 'w') as file_pointer:
-            file_pointer.write(lines)
-
     ### PUBLIC METHODS ###
-
-    def add_metadatum(self):
-        r'''Adds metadatum to metadata module.
-
-        Returns none.
-        '''
-        getter = self._io_manager.make_getter(where=self._where)
-        getter.append_snake_case_string('metadatum name', allow_empty=False)
-        getter.append_expr('metadatum value')
-        result = getter._run()
-        if self._session._backtrack():
-            return
-        if result:
-            metadatum_name, metadatum_value = result
-            self._add_metadatum(metadatum_name, metadatum_value)
-
-    def edit_metadata_module(self):
-        r'''Edits metadata module.
-
-        Returns none.
-        '''
-        file_path = self._metadata_module_path
-        if os.path.isfile(file_path):
-            self._io_manager.edit(file_path)
-
-    def get_metadatum(self):
-        r'''Gets metadatum from metadata module.
-
-        Returns none.
-        '''
-        getter = self._io_manager.make_getter(where=self._where)
-        getter.append_string('metadatum name')
-        result = getter._run()
-        if self._session._backtrack():
-            return
-        metadatum = self._get_metadatum(result)
-        message = '{!r}'.format(metadatum)
-        self._io_manager.proceed(message=message)
 
     def remove_initializer(self, prompt=True):
         r'''Removes initializer module.
@@ -228,37 +98,6 @@ class PackageManager(DirectoryManager):
                 line, 
                 prompt=prompt,
                 )
-
-    def remove_metadata_module(self, prompt=True):
-        r'''Removes metadata module.
-
-        Returns none.
-        '''
-        if os.path.isfile(self._metadata_module_path):
-            if prompt:
-                message = 'remove metadata module?'
-                if not self._io_manager.confirm(message):
-                    return
-            os.remove(self._metadata_module_path)
-            line = 'metadata module removed.'
-            self._io_manager.proceed(
-                line, 
-                prompt=prompt,
-                )
-
-    def remove_metadatum(self):
-        r'''Removes metadatum from meatdata module.
-
-        Returns none.
-        '''
-        getter = self._io_manager.make_getter(where=self._where)
-        getter.append_string('metadatum name')
-        result = getter._run()
-        if self._session._backtrack():
-            return
-        if result:
-            metadatum_name = result
-            self._remove_metadatum(metadatum_name)
 
     def remove_views_module(self, prompt=True):
         r'''Removes views module.
@@ -330,22 +169,6 @@ class PackageManager(DirectoryManager):
         self._path = new_directory_path
         self._session._is_backtracking_locally = True
 
-    def rewrite_metadata_module(
-        self, 
-        metadata=None, 
-        prompt=True,
-        ):
-        r'''Rewrites metadata module.
-
-        Returns none.
-        '''
-        if metadata is None:
-            metadata = self._get_metadata()
-        self._write_metadata(metadata)
-        if prompt:
-            message = 'rewrote metadata module.'
-            self._io_manager.proceed(message)
-
     def view_initializer(self):
         r'''Views initializer module.
 
@@ -357,15 +180,6 @@ class PackageManager(DirectoryManager):
             session=self._session,
             )
         manager.view()
-
-    def view_metadata_module(self):
-        r'''Views metadata module.
-
-        Returns none.
-        '''
-        file_path = self._metadata_module_path
-        if os.path.isfile(file_path):
-            self._io_manager.view(file_path)
 
     def write_initializer_boilerplate(self, prompt=True):
         r'''Writes boilerplate initializer module.
