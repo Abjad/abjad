1bb903090d5300e7d22d8d49591de3ae2a9127c7
Trevor Bača <trevorbaca@gmail.com>
2014-03-23 12:08:56 -0400

    Maded Memoize decorator empty-initializable.

--- abjad/tools/systemtools/Memoize.py
+++ abjad/tools/systemtools/Memoize.py
@@ -15,8 +15,8 @@ class Memoize(AbjadObject):
 
     ### INITIALIZER ###
 
-    def __init__(self, func):
-       self.func = func
+    def __init__(self, function=None):
+       self.function = function
        self.cache = {}
 
     ### SPECIAL METHODS ###
@@ -29,20 +29,13 @@ class Memoize(AbjadObject):
         if not isinstance(args, collections.Hashable):
             # mutable objects can not cache;
             # reevaluates function instead of blowing up
-            return self.func(*args)
+            return self.function(*args)
         if args in self.cache:
             return self.cache[args]
         else:
-            value = self.func(*args)
+            value = self.function(*args)
             self.cache[args] = value
             return value
-
-    def __repr__(self):
-        r'''Return string representation of dectorator.
-
-        Uses function docstring.
-        '''
-        return self.func.__doc__
 
     def __get__(self, obj, objtype):
         r'''Supports instance methods.
