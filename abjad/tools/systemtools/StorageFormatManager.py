03b69ebdfbea468bcc6bd2b24c3d87fbf3c41114
Tiago Antao <tra@popgen.net>
2014-03-24 14:32:17 +0000

    getting constructor defaults

--- abjad/tools/systemtools/StorageFormatManager.py
+++ abjad/tools/systemtools/StorageFormatManager.py
@@ -339,17 +339,21 @@ class StorageFormatManager(object):
         '''
         if hasattr(subject.__init__, '__func__'):
             initializer = subject.__init__.__func__
-            if initializer.func_defaults:
-                keyword_argument_count = len(initializer.func_defaults)
-                initializer_code = initializer.func_code
-                positional_argument_count = (
-                    initializer_code.co_argcount - keyword_argument_count - 1)
-                start_index = 1 + positional_argument_count
-                stop_index = start_index + keyword_argument_count
-                return initializer_code.co_varnames[start_index:stop_index]
-            else:
-                return ()
-        return ()
+            defaults = initializer.func_defaults
+        elif hasattr(subject.__init__, '__defaults__'):
+            defaults = subject.__init__.__defaults__
+        else:
+            return ()
+        if defaults:
+            keyword_argument_count = len(initializer.func_defaults)
+            initializer_code = initializer.func_code
+            positional_argument_count = (
+                initializer_code.co_argcount - keyword_argument_count - 1)
+            start_index = 1 + positional_argument_count
+            stop_index = start_index + keyword_argument_count
+            return initializer_code.co_varnames[start_index:stop_index]
+        else:
+            return ()
 
     @staticmethod
     def get_signature_positional_argument_names(subject):
