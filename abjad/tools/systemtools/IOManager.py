393876f067c0b231a3458114023f2b5e5f50554d
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-03-26 14:37:51 -0400

    Fixed various doctests.

--- abjad/tools/systemtools/IOManager.py
+++ abjad/tools/systemtools/IOManager.py
@@ -109,7 +109,7 @@ class IOManager(object):
                 ...     "Note('c4')",
                 ...     globals(),
                 ...     )
-                10194
+                10195
 
         ..  container:: example
 
