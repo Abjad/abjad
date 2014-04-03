e78e7be90e7fce6a2eb7d5e552ac4106b30c0764
Trevor Bača <trevorbaca@gmail.com>
2014-04-03 09:47:57 -0400

    Cleaned up ScorePackageManager._get_tempo_inventory().

--- scoremanager/managers/ScorePackageManager.py
+++ scoremanager/managers/ScorePackageManager.py
@@ -209,8 +209,8 @@ class ScorePackageManager(PackageManager):
         paths = wrangler._list_asset_paths()
         for path in paths:
             manager = wrangler._initialize_asset_manager(path)
-            output_class = manager._get_metadatum('output_class')
-            if output_class is indicatortools.TempoInventory:
+            output_class_name = manager._get_metadatum('output_class_name')
+            if output_class_name == 'TempoInventory':
                 output_material = manager._execute_output_module()
                 return output_material
 
--- scoremanager/scores/red_example_score/materials/tempo_inventory/__metadata__.py
+++ scoremanager/scores/red_example_score/materials/tempo_inventory/__metadata__.py
@@ -1,10 +1,9 @@
 # -*- encoding: utf-8 -*-
-from abjad import *
 import collections
 
 
 metadata = collections.OrderedDict([
     ('generic_output_name', 'tempo inventory'),
     ('material_manager_class_name', 'TempoInventoryMaterialManager'),
-    ('output_class', indicatortools.TempoInventory), 
+    ('output_class_name', 'TempoInventory'), 
     ])
