bf028eae4c13cc55a41bdc2febe2ef60ace76336
Trevor Bača <trevorbaca@gmail.com>
2014-04-04 18:20:40 -0400

    Rebuild materials.example_sargasso_measures package.
    Now implemented around SargassoMeasureMaker.

--- scoremanager/materials/example_sargasso_measures/output.py
+++ scoremanager/materials/example_sargasso_measures/output.py
@@ -1,24 +1,15 @@
 # -*- encoding: utf-8 -*-
-from abjad.tools.scoretools.Measure import Measure
+from abjad import *
+from scoremanager import makers
 
 
-example_sargasso_measures = [
-	Measure((4, 16), "c'16 c'16 c'8"),
-	Measure((2, 10), "c'8 c'8"),
-	Measure((3, 20), "c'8 c'16"),
-	Measure((4, 16), "c'8. c'16"),
-	Measure((4, 16), "c'8. c'16"),
-	Measure((11, 30), "c'16 c'16 c'8 c'8. c'4"),
-	Measure((15, 30), "c'8 c'16 c'8 c'8. c'4 c'16 c'16 c'16"),
-	Measure((2, 8), "c'8 c'8"),
-	Measure((10, 26), "c'8 c'8. c'4 c'16"),
-	Measure((4, 30), "c'16 c'16 c'16 c'16"),
-	Measure((15, 30), "c'16 c'4 c'16 c'16 c'8 c'8. c'16 c'8"),
-	Measure((7, 26), "c'16 c'4 c'16 c'16"),
-	Measure((3, 26), "c'16 c'16 c'16"),
-	Measure((1, 4), "c'4"),
-	Measure((10, 19), "c'8. c'4 c'16 c'16 c'16"),
-	Measure((6, 26), "c'16 c'16 c'4"),
-	Measure((6, 20), "c'4 c'16 c'16"),
-	Measure((2, 20), "c'16 c'16"),
-	Measure((9, 19), "c'16 c'4 c'16 c'16 c'8")]
+example_sargasso_measures = makers.SargassoMeasureMaker(
+    measure_denominator=4,
+    measure_numerator_talea=(2, 2, 2, 2, 1, 1, 4, 4),
+    measure_division_denominator=16,
+    measure_division_talea=(1, 1, 2, 3, 1, 2, 3, 4, 1, 1, 1, 1, 4),
+    total_duration=durationtools.Duration(11, 2),
+    measures_are_scaled=True,
+    measures_are_split=True,
+    measures_are_shuffled=True,
+    )
