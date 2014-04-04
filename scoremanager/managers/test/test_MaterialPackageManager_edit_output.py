5392b122c3df665c28f18087c625d5510cd86254
Trevor Bača <trevorbaca@gmail.com>
2014-04-04 16:19:28 -0400

    Updated MaterialPackageManager test file.

--- scoremanager/managers/test/test_MaterialPackageManager_edit_output.py
+++ scoremanager/managers/test/test_MaterialPackageManager_edit_output.py
@@ -172,7 +172,7 @@ def test_MaterialPackageManager_edit_output_05():
         score_manager._run(pending_user_input=input_)
         assert os.path.exists(path)
         session = scoremanager.core.Session(is_test=True)
-        manager = scoremanager.managers.ArticulationHandlerMaterialManager
+        manager = scoremanager.managers.MaterialPackageManager
         manager = manager(path=path, session=session)
         assert manager._list() == directory_entries
         output_material = manager._execute_output_module()
@@ -380,6 +380,52 @@ def test_MaterialPackageManager_edit_output_10():
         output_material = manager._execute_output_module()
         assert output_material == maker
         input_ = 'm testrhythmmaker rm remove q'
+        score_manager._run(pending_user_input=input_)
+    finally:
+        if os.path.exists(path):
+            shutil.rmtree(path)
+    assert not os.path.exists(path)
+
+
+def test_ArticulationHandlerMaterialManager_edit_output_material_01():
+    r'''Edits retierated articulation handler.
+    '''
+    pytest.skip('port me forward.')
+
+    score_manager = scoremanager.core.ScoreManager(is_test=True)
+    configuration = score_manager._configuration
+    path = os.path.join(
+        configuration.user_library_material_packages_directory_path,
+        'testarticulationhandler',
+        )
+    directory_entries = [
+        '__init__.py',
+        '__metadata__.py',
+        'output.py',
+        ]
+    handler = handlertools.ReiteratedArticulationHandler(
+        articulation_list=['^', '.'],
+        minimum_duration=Duration(1, 64),
+        maximum_duration=Duration(1, 4),
+        minimum_written_pitch=NamedPitch('c'),
+        maximum_written_pitch=NamedPitch("c''''"),
+        )
+
+    assert not os.path.exists(path)
+    try:
+        input_ = 'm nmc articulation testarticulationhandler default'
+        input_ += ' reiterated'
+        input_ += " ['^', '.'] (1, 64) (1, 4) c c'''' done default"
+        input_ += ' q'
+        score_manager._run(pending_user_input=input_)
+        assert os.path.exists(path)
+        session = scoremanager.core.Session(is_test=True)
+        manager = scoremanager.managers.MaterialPackageManager
+        manager = manager(path=path, session=session)
+        assert manager._list() == directory_entries
+        output_material = manager._execute_output_module()
+        assert output_material == handler
+        input_ = 'm testarticulationhandler rm remove q'
         score_manager._run(pending_user_input=input_)
     finally:
         if os.path.exists(path):
