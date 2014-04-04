9818d3eb4da8518874736f24ff5bfacf72b7b01c
Trevor Bača <trevorbaca@gmail.com>
2014-04-04 17:36:07 -0400

    Added SargassoMeasureMaker class to makers package.
    The class is currently a stub.
    
    The class will house logic migrated out of SargassoMeasureMaterialManager.

---
+++ scoremanager/makers/SargassoMeasureMaker.py
@@ -0,0 +1,27 @@
+# -*- encoding: utf-8 -*-
+from abjad import *
+from abjad.tools.abctools.AbjadObject import AbjadObject
+
+
+class SargassoMeasureMaker(AbjadObject):
+    r'''Sargasso measure maker.
+    '''
+
+    ### CLASS VARIABLES ###
+
+    __slots__ = (
+        )
+
+    ### INITIALIZER ###
+
+    def __init__(self):
+        pass
+
+    ### SPECIAL METHODS ###
+
+    def __illustrate__(self, **kwargs):
+        r'''Illustrates sargaso measures.
+
+        Returns LilyPond file.
+        '''
+        pass
