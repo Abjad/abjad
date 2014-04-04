8c45e36297601e99c8c76a796ec591bfcdbb9905
Trevor Bača <trevorbaca@gmail.com>
2014-04-04 18:27:08 -0400

    Removed SargassoMeasureMaterialManager.
    Use SargassoMeasureMaker instead.

--- scoremanager/wranglers/test/test_MaterialPackageWrangler__make_material_package.py
+++ scoremanager/wranglers/test/test_MaterialPackageWrangler__make_material_package.py
@@ -68,37 +68,3 @@ def test_MaterialPackageWrangler__make_material_package_02():
         if os.path.exists(path):
             shutil.rmtree(path)
     assert not os.path.exists(path)
-
-
-def test_MaterialPackageWrangler__make_material_package_03():
-
-    session = scoremanager.core.Session(is_test=True)
-    wrangler = scoremanager.wranglers.MaterialPackageWrangler(session=session)
-    path = os.path.join(
-        wrangler._configuration.user_library_material_packages_directory_path,
-        'testsargasso',
-        )
-    directory_entries = [
-        '__init__.py',
-        '__metadata__.py',
-        ]
-
-    assert not os.path.exists(path)
-    try:
-        key = 'material_manager_class_name'
-        metadata = {key: 'SargassoMeasureMaterialManager'}
-        wrangler._make_material_package(
-            path,
-            metadata=metadata,
-            )
-
-        assert os.path.exists(path)
-        session = scoremanager.core.Session(is_test=True)
-        manager = scoremanager.managers.SargassoMeasureMaterialManager
-        manager = manager(path=path, session=session)
-        assert manager._list() == directory_entries
-        manager._remove()
-    finally:
-        if os.path.exists(path):
-            shutil.rmtree(path)
-    assert not os.path.exists(path)
