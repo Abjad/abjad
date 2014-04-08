4bae8d4a9e81228bc33eeaf2991e948a169de61d
Trevor Bača <trevorbaca@gmail.com>
2014-04-08 08:23:34 -0400

    Wrote UserInputGetter help tests.

---
+++ scoremanager/iotools/test/test_UserInputGetter__display_help.py
@@ -0,0 +1,44 @@
+# -*- encoding: utf-8 -*-
+from abjad import *
+import scoremanager
+score_manager = scoremanager.core.ScoreManager(is_test=True)
+
+
+def test_UserInputGetter__display_help_01():
+    r'''Question mark displays help.
+    '''
+
+    input_ = 'red~example~score m new ? q'
+    score_manager._run(pending_user_input=input_)
+    lines = [
+        'Score manager - example scores',
+        '> red example score',
+        'Red Example Score (2013)',
+        '> m',
+        'Red Example Score (2013) - materials',
+        '> new',
+        'Enter material package name> ?',
+        'Value  must be space-delimited lowercase string.',
+        'Enter material package name> q',
+        ]
+    assert score_manager._transcript.first_lines == lines
+
+
+def test_UserInputGetter__display_help_02():
+    r'''Help string displays help.
+    '''
+
+    input_ = 'red~example~score m new help q'
+    score_manager._run(pending_user_input=input_)
+    lines = [
+        'Score manager - example scores',
+        '> red example score',
+        'Red Example Score (2013)',
+        '> m',
+        'Red Example Score (2013) - materials',
+        '> new',
+        'Enter material package name> help',
+        'Value  must be space-delimited lowercase string.',
+        'Enter material package name> q',
+        ]
+    assert score_manager._transcript.first_lines == lines
