32c37bd24ef5a596bd4d46989087a0a212622847
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-04-15 19:43:22 -0400

    Added new indicator tool: BowPressure.
    This class accompanies BowHairContactPoint, StringContactPoint, StringNumber
    and Tuning in describing various parameters for string instrument writing.

---
+++ abjad/tools/indicatortools/BowPressure.py
@@ -0,0 +1,44 @@
+# -*- encoding: utf-8 -*-
+from abjad.tools.abctools import AbjadObject
+
+
+class BowPressure(AbjadObject):
+    r'''Bow pressure indicator.
+
+    ::
+
+        >>> indicator = indicatortools.BowPressure('overpressure')
+        >>> print format(indicator)
+        indicatortools.BowPressure(
+            pressure='overpressure',
+            )
+
+    '''
+
+    ### CLASS VARIABLES ###
+
+    __slots__ = (
+        '_pressure',
+        )
+
+    ### INITIALIZER ###
+
+    def __init__(self,
+        pressure=None,
+        ):
+        self._pressure = pressure
+
+    ### PUBLIC PROPERTIES ###
+
+    @property
+    def pressure(self):
+        r'''Gets pressure.
+
+        ::
+
+            >>> indicator = indicatortools.BowPressure('underpressure')
+            >>> indicator.pressure
+            'underpressure'
+
+        '''
+        return self._pressure
