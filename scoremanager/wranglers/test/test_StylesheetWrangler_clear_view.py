4aa40651d23d796921ca6b1a36a5f5009cfc197e
Trevor Bača <trevorbaca@gmail.com>
2014-04-15 17:41:34 -0400

    Wrote StylesheetWrangler.clear_view() tests.

--- scoremanager/wranglers/test/test_StylesheetWrangler_clear_view.py
+++ scoremanager/wranglers/test/test_StylesheetWrangler_clear_view.py
@@ -4,14 +4,15 @@ import scoremanager
 score_manager = scoremanager.core.ScoreManager(is_test=False)
 
 
-def test_StylesheetWrangler_apply_view_01():
-    r'''Makes sure only one stylesheet is visible with view.
+def test_StylesheetWrangler_clear_view_01():
+    r'''Makes sure more than one stylesheet is visible without view.
     '''
     
     input_ = 'y vnew _test rm all add clean-letter-14.ily done default'
-    input_ += ' va _test vrm _test default q'
+    input_ += ' va _test V vrm _test default q'
     score_manager._run(pending_user_input=input_)
-    applied_view = score_manager._transcript[-8]
+    with_view = score_manager._transcript[-10]
+    without_view = score_manager._transcript[-8]
 
     lines = [
         'Score manager - stylesheet library (_test view)',
@@ -24,4 +25,13 @@ def test_StylesheetWrangler_apply_view_01():
         '    stylesheets - rename (ren)',
         '',
         ]
-    assert applied_view.lines == lines
+    assert with_view.lines == lines
+
+    title = 'Score manager - stylesheet library'
+    assert without_view.title == title
+
+    string = 'clean-letter-14.ily (Abjad)'
+    assert string in without_view.contents
+
+    string = 'clean-letter-16.ily (Abjad)'
+    assert string in without_view.contents
