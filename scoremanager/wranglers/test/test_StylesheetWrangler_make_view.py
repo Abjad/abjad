598b4628ab14941caaf9d3db30bc2c030196c882
Trevor Bača <trevorbaca@gmail.com>
2014-04-17 12:52:35 -0400

    Wrote StylesheetWrangler.remove_view() tests.

--- scoremanager/wranglers/test/test_StylesheetWrangler_make_view.py
+++ scoremanager/wranglers/test/test_StylesheetWrangler_make_view.py
@@ -17,7 +17,7 @@ def test_StylesheetWrangler_make_view_01():
 
 
 def test_StylesheetWrangler_make_view_02():
-    r'''Checks to make sure at least one Abjad stylesheet appears in
+    r'''Makes sure at least one Abjad stylesheet appears in 
     view creation menu.
     '''
 
@@ -27,3 +27,23 @@ def test_StylesheetWrangler_make_view_02():
 
     string = 'clean-letter-14.ily (Abjad)'
     assert string in transcript.contents
+
+
+def test_StylesheetWrangler_make_view_03():
+    r'''Makes view. Removes view.
+    '''
+
+    input_ = 'y vnew _test rm all add clean-letter-14.ily done default q' 
+    score_manager._run(pending_user_input=input_)
+
+    input_ = 'y vls vrm _test default q'
+    score_manager._run(pending_user_input=input_)
+    contents = score_manager._transcript.contents
+    assert 'views found:' in contents
+    assert '_test' in contents
+
+    input_ = 'y vls q'
+    score_manager._run(pending_user_input=input_)
+    contents = score_manager._transcript.contents
+    assert 'views found:' in contents
+    assert '_test' not in contents
--- scoremanager/wranglers/test/test_StylesheetWrangler_remove_view.py
+++ scoremanager/wranglers/test/test_StylesheetWrangler_remove_view.py
@@ -5,6 +5,20 @@ score_manager = scoremanager.core.ScoreManager(is_test=True)
 
 
 def test_StylesheetWrangler_remove_view_01():
-    
-    input_ = 'q'
+    r'''Makes view. Removes view.
+    '''
+
+    input_ = 'y vnew _test rm all add clean-letter-14.ily done default q' 
     score_manager._run(pending_user_input=input_)
+
+    input_ = 'y vls vrm _test default q'
+    score_manager._run(pending_user_input=input_)
+    contents = score_manager._transcript.contents
+    assert 'views found:' in contents
+    assert '_test' in contents
+
+    input_ = 'y vls q'
+    score_manager._run(pending_user_input=input_)
+    contents = score_manager._transcript.contents
+    assert 'views found:' in contents
+    assert '_test' not in contents
