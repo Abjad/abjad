3c9113da2cfa68d66ad75e9b2077f083b0458c10
Trevor Bača <trevorbaca@gmail.com>
2014-04-19 19:30:31 -0400

    Built Red Example Score instrumentation material package.
    You can do this:
    
        * enter a score
        * create a material package
        * configure the material package for autoediting
        * autoedit a PerformerInventory
    
    This persists a perfect instrumentation object to the score in question.
    
    MODEL: this means that it will be possible to eliminate the special (instr)
    command that allows ScorePackageManager to manager an elected instrumentation
    module by hand. No special handling is needed.

---
+++ scoremanager/scores/red_example_score/materials/instrumentation/__metadata__.py
@@ -0,0 +1,8 @@
+# -*- encoding: utf-8 -*-
+import collections
+
+
+metadata = collections.OrderedDict([
+    ('output_material_class_name', 'PerformerInventory'),
+    ('use_autoeditor', True),
+    ])
