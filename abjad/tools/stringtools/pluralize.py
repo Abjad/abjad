edd662851c36ed2fa70ed6b76586ef892d200408
Trevor Bača <trevorbaca@gmail.com>
2014-04-18 20:04:32 -0400

    Added count=None keyowrd to stringtools.pluralize().
    Example:
    
        >>> stringtools.pluralize(identifier, count=0)
        'cherries'
    
        >>> stringtools.pluralize(identifier, count=1)
        'cherry'
    
        >>> stringtools.pluralize(identifier, count=2)
        'cherries'
    
        >>> stringtools.pluralize(identifier, count=3)
        'cherries'
    
        >>> stringtools.pluralize(identifier, count=99)
        'cherries'
    
    SYNTAX: note that grammatical number in English distinguishes not between
    'singular' and 'plural' but between the open-closed range (0, 1] and all other
    numbers. That is, (0, 1] of a noun receives SING morphology while any other
    number of nouns receive PL morphology. Specifically this means that the idea
    that "more than 1 of something receives PL" is absolutely wrong: 0 of
    something always recevies PL: "no cherries". (And 0 is less than 1.)
    
    The commit here accounts for integer numbers of a noun. Set the count=None
    keyword to the number concerned and pluralization will happen as it should.

--- abjad/tools/stringtools/pluralize.py
+++ abjad/tools/stringtools/pluralize.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 
 
-def pluralize(string):
+def pluralize(string, count=None):
     r'''Pluralize English `string`. Change terminal `-y` to `-ies`:
 
     ::
@@ -25,6 +25,8 @@ def pluralize(string):
 
     Returns string.
     '''
+    if count == 1:
+        return string
     if string.endswith('y'):
         return string[:-1] + 'ies'
     elif string.endswith(('s', 'sh', 'x', 'z')):
