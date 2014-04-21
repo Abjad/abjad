eca4ba574272a5a291d61220004b99bdedc387d1
Trevor Bača <trevorbaca@gmail.com>
2014-04-21 14:48:41 -0400

    Wrote BuildFileWrangler.copy_segment_pdfs() tests.

--- scoremanager/wranglers/test/test_BuildFileWrangler_copy_segment_pdfs.py
+++ scoremanager/wranglers/test/test_BuildFileWrangler_copy_segment_pdfs.py
@@ -1,17 +1,14 @@
 # -*- encoding: utf-8 -*-
 import filecmp
 import os
-import pytest
-pytest.skip('working on this one now.')
-import shutil
 from abjad import *
 import scoremanager
+score_manager = scoremanager.core.ScoreManager(is_test=True)
 
 
-def test_BuildFileWrangler_copy_segment_lilypond_files_01():
+def test_BuildFileWrangler_copy_segment_pdfs_01():
 
-    # set up score manager and diretories
-    score_manager = scoremanager.core.ScoreManager(is_test=True)
+    # find build directory
     build_directory = os.path.join(
         score_manager._configuration.example_score_packages_directory_path,
         'red_example_score',
@@ -19,45 +16,62 @@ def test_BuildFileWrangler_copy_segment_lilypond_files_01():
         )
     temporary_directory = os.path.join(build_directory, 'tmp')
 
-    # set up file paths
-    file_names, file_paths, temporary_file_paths = [], [], []
+    # find source pdfs
+    source_pdfs = []
     for number in ('01', '02', '03'):
-        file_name = 'red-example-score-segment-{}.ly'
+        directory_name = 'segment_{}'.format(number)
+        path = os.path.join(
+            score_manager._configuration.example_score_packages_directory_path,
+            'red_example_score',
+            'segments',
+            directory_name,
+            'output.pdf',
+            )
+        source_pdfs.append(path)
+
+    # make sure source paths exist
+    for path in source_pdfs:
+        assert os.path.isfile(path)
+
+    # find destination paths
+    destination_pdfs = []
+    for number in ('01', '02', '03'):
+        file_name = 'red-example-score-segment-{}.pdf'
         file_name = file_name.format(number)
-        file_names.append(file_name)
-        file_path = os.path.join(build_directory, file_name)
-        file_paths.append(file_path)
-        temporary_file_path = os.path.join(temporary_directory, file_name)
-        temporary_file_paths.append(temporary_file_path)
+        path = os.path.join(
+            score_manager._configuration.example_score_packages_directory_path,
+            'red_example_score',
+            'build',
+            file_name,
+            )
+        destination_pdfs.append(path)
 
-    # make sure segment files are currently in build directory
-    for file_path in file_paths:
-        assert os.path.isfile(file_path)
+    # make sure destination paths don't exist
+    for path in destination_pdfs:
+        assert not os.path.exists(path)
 
-    if not os.path.isdir(temporary_directory):
-        os.mkdir(temporary_directory)
-
-    # move segment files from build directory to temporary directory
-    pairs = zip(file_paths, temporary_file_paths)
-    for file_path, temporary_file_path in pairs:
-        print(file_path, temporary_file_path)
-        shutil.move(file_path, temporary_file_path)
-
-    # make sure segment files are no longer in build directory
-    for file_path in file_paths:
-        assert not os.path.isfile(file_path)
-
-    # call (lycp)
-    input_ = 'red~example~score u lycp default q'
+    # run input
+    input_ = 'red~example~score u pdfcp default q'
     score_manager._run(pending_user_input=input_)
 
-    # make sure new segment files are currently in build directory
-    for file_path in file_paths:
-        assert os.path.isfile(file_path)
+    # make sure destination paths exist
+    for path in destination_pdfs:
+        assert os.path.isfile(path)
 
-    # make sure new and old segment files compare equal
-    for file_path, temporary_file_path in pairs:
-        assert filecmp.cmp(file_path, temporary_file_path), repr(file_path)
+    # make sure source and destination files compare equal
+    pairs = zip(source_pdfs, destination_pdfs)
+    for source_path, destination_path in pairs:
+        assert filecmp.cmp(source_path, destination_path)
 
-    # remove temporary directory
-    shutil.rmtree(temporary_directory)
+    # remove destination pdfs
+    for path in destination_pdfs:
+        if os.path.exists(path):
+            os.remove(path)
+
+    # make sure destination paths do not exist
+    for path in destination_pdfs:
+        assert not os.path.exists(path)
+
+    # make sure source paths do exist
+    for path in source_pdfs:
+        assert os.path.isfile(path)
