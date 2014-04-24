4ef6a2e7ecf19d6c2fb905e264ea55765eb52d71
Trevor Bača <trevorbaca@gmail.com>
2014-04-24 13:51:24 -0400

    Wrote SegmentPackageWrangler.clear_view() tests.

--- scoremanager/wranglers/test/test_SegmentPackageWrangler_clear_view.py
+++ scoremanager/wranglers/test/test_SegmentPackageWrangler_clear_view.py
@@ -6,34 +6,35 @@ import scoremanager
 score_manager = scoremanager.core.ScoreManager(is_test=False)
 
 
-def test_StylesheetWrangler_clear_view_01():
-    r'''Makes sure more than one stylesheet is visible without view.
+def test_SegmentPackageWrangler_clear_view_01():
+    r'''Makes sure more than one segment is visible without view.
     '''
     
-    input_ = 'y vnew _test rm all add clean-letter-14.ily done default'
+    input_ = 'g vnew _test rm all'
+    input_ += ' add segment~01~(Red~Example~Score) done default'
     input_ += ' va _test V vrm _test default q'
     score_manager._run(pending_user_input=input_)
     with_view = score_manager._transcript[-10]
     without_view = score_manager._transcript[-8]
 
     lines = [
-        'Score manager - stylesheets (_test)',
+        'Score manager - segments (_test)',
         '',
-        '   1: clean-letter-14.ily (Abjad)',
+        '   1: segment 01 (Red Example Score)',
         '',
-        '      stylesheets - copy (cp)',
-        '      stylesheets - new (new)',
-        '      stylesheets - remove (rm)',
-        '      stylesheets - rename (ren)',
+        '      segments - copy (cp)',
+        '      segments - new (new)',
+        '      segments - remove (rm)',
+        '      segments - rename (ren)',
         '',
         ]
     assert with_view.lines == lines
 
-    title = 'Score manager - stylesheets'
+    title = 'Score manager - segments'
     assert without_view.title == title
 
-    string = 'clean-letter-14.ily (Abjad)'
+    string = 'segment 01 (Red Example Score)'
     assert string in without_view.contents
 
-    string = 'clean-letter-16.ily (Abjad)'
+    string = 'segment 02 (Red Example Score)'
     assert string in without_view.contents
