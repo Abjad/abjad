39c83b3d20da636169495eda19a104c2fd5e2f1e
Trevor Bača <trevorbaca@gmail.com>
2014-04-29 14:20:11 -0400

    Wrote BuildFileWrangler.list_views() tests.

---
+++ scoremanager/wranglers/test/test_BuildFileWrangler_list_views.py
@@ -0,0 +1,25 @@
+# -*- encoding: utf-8 -*-
+from abjad import *
+import scoremanager
+score_manager = scoremanager.core.ScoreManager(is_test=False)
+
+
+def test_BuildFileWrangler_list_views_01():
+    r'''Makes sure only one build file is visible with view.
+    '''
+    
+    input_ = 'u vls vnew _test rm all'
+    input_ += ' add front-cover.pdf~(Red~Example~Score) done default'
+    input_ += ' vls vrm _test default vls q'
+    score_manager._run(pending_user_input=input_)
+    transcript = score_manager._transcript
+
+    view_list_entries = [
+        _ for _ in transcript
+        if ('view found' in _.contents or 'views found' in _.contents)
+        ]
+    assert len(view_list_entries) == 3
+
+    assert '_test' not in view_list_entries[0].contents
+    assert '_test' in view_list_entries[1].contents
+    assert '_test' not in view_list_entries[2].contents
