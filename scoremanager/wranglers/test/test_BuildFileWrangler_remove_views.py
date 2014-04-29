ef4e09b6cef65268eddd9da30630622907a64b53
Trevor Bača <trevorbaca@gmail.com>
2014-04-29 14:37:20 -0400

    Wrote BuildFileWrangler.remove_views() tests.

---
+++ scoremanager/wranglers/test/test_BuildFileWrangler_remove_views.py
@@ -0,0 +1,49 @@
+# -*- encoding: utf-8 -*-
+from abjad import *
+import scoremanager
+score_manager = scoremanager.core.ScoreManager(is_test=True)
+
+
+def test_BuildFileWrangler_remove_views_01():
+    r'''Makes two views. Removes two views at one time.
+    '''
+
+    input_ = 'u vnew _test_100 rm all'
+    input_ += ' add front-cover.pdf~(Red~Example~Score) done default' 
+    input_ += ' u vnew _test_101 rm all'
+    input_ += ' add back-cover.pdf~(Red~Example~Score) done default'
+    input_ += ' q' 
+    score_manager._run(pending_user_input=input_)
+
+    input_ = 'u vls q'
+    score_manager._run(pending_user_input=input_)
+    contents = score_manager._transcript.contents
+    assert 'views found' in contents
+    assert '_test_100' in contents
+    assert '_test_101' in contents
+
+    input_ = 'u vrm _test_100-_test_101 default q'
+    score_manager._run(pending_user_input=input_)
+
+    input_ = 'u vls q'
+    score_manager._run(pending_user_input=input_)
+    contents = score_manager._transcript.contents
+    assert 'view found' in contents or 'views found' in contents
+    assert '_test_100' not in contents
+    assert '_test_101' not in contents
+
+
+def test_BuildFileWrangler_remove_views_02():
+    r'''Makes sure selector backtracking works.
+    '''
+
+    input_ = 'u vrm b q'
+    score_manager._run(pending_user_input=input_)
+
+    titles = [
+        'Score manager - example scores',
+        'Score manager - build files',
+        'Score manager - build files - select view(s) to remove:',
+        'Score manager - build files',
+        ]
+    assert score_manager._transcript.titles == titles
