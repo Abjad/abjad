abbd8e789fe8313622244eb4bb5793710d7b4c3a
Trevor Bača <trevorbaca@gmail.com>
2014-04-30 14:26:36 -0400

    Wrote DistributionFileWrangler.remove_files() tests.

---
+++ scoremanager/wranglers/test/test_DistributionFileWrangler_remove_files.py
@@ -0,0 +1,29 @@
+# -*- encoding: utf-8 -*-
+import os
+import shutil
+from abjad import *
+import scoremanager
+score_manager = scoremanager.core.ScoreManager(is_test=True)
+
+
+def test_DistributionFileWrangler_remove_files_01():
+
+    path = os.path.join(
+        score_manager._configuration.example_score_packages_directory_path,
+        'red_example_score',
+        'distribution',
+        'foo-file.txt',
+        )
+
+    assert not os.path.exists(path)
+    try:
+        with file(path, 'w') as file_pointer:
+            file_pointer.write('This is a test file.')
+        assert os.path.exists(path)
+        input_ = 'red~example~score d rm foo-file.txt remove q'
+        score_manager._run(pending_user_input=input_)
+        assert not os.path.exists(path)
+    finally:
+        if os.path.exists(path):
+            os.remove(path)
+    assert not os.path.exists(path)
