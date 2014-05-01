ba83036bc16ac5561a6fce519f9beb25714fcc58
Trevor Bača <trevorbaca@gmail.com>
2014-05-01 10:22:04 -0400

    Wrote MakerModuleWrangler.remove_modules() tests.

---
+++ scoremanager/wranglers/test/test_MakerModuleWrangler_remove_modules.py
@@ -0,0 +1,29 @@
+# -*- encoding: utf-8 -*-
+import os
+import shutil
+from abjad import *
+import scoremanager
+score_manager = scoremanager.core.ScoreManager(is_test=True)
+
+
+def test_MakerModuleWrangler_remove_modules_01():
+
+    path = os.path.join(
+        score_manager._configuration.example_score_packages_directory_path,
+        'red_example_score',
+        'makers',
+        'Foo.py',
+        )
+
+    assert not os.path.exists(path)
+    try:
+        with file(path, 'w') as file_pointer:
+            file_pointer.write('This is a test file.')
+        assert os.path.exists(path)
+        input_ = 'red~example~score k rm Foo.py remove q'
+        score_manager._run(pending_user_input=input_)
+        assert not os.path.exists(path)
+    finally:
+        if os.path.exists(path):
+            os.remove(path)
+    assert not os.path.exists(path)
