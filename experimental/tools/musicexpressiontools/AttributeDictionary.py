bc40e1b9c9289a33e6f15800400e74935992634e
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-05-01 17:41:47 -0400

    AttributeDictionary now inherits from TypedOrderedDictionary.

--- experimental/tools/musicexpressiontools/AttributeDictionary.py
+++ experimental/tools/musicexpressiontools/AttributeDictionary.py
@@ -2,12 +2,11 @@
 import collections
 from abjad.tools import datastructuretools
 from abjad.tools import timespantools
-from abjad.tools.abctools.AbjadObject import AbjadObject
 from experimental.tools.musicexpressiontools.AttributeNameEnumeration \
     import AttributeNameEnumeration
 
 
-class AttributeDictionary(AbjadObject, collections.OrderedDict):
+class AttributeDictionary(datastructuretools.TypedOrderedDict):
     r'''Attribute dictionary.
     '''
 
@@ -18,7 +17,8 @@ class AttributeDictionary(AbjadObject, collections.OrderedDict):
     ### INITIALIZER ###
 
     def __init__(self):
-        collections.OrderedDict.__init__(self)
+        #collections.OrderedDict.__init__(self)
+        datastructuretools.TypedOrderedDict.__init__(self)
         for attribute in self.attributes:
             self[attribute] = timespantools.TimespanInventory()
         assert 'time_signatures' in self
@@ -32,4 +32,4 @@ class AttributeDictionary(AbjadObject, collections.OrderedDict):
         assert isinstance(key, str), repr(key)
         assert isinstance(value, (list, datastructuretools.TypedList)), \
             repr(value)
-        collections.OrderedDict.__setitem__(self, key, value)
+        datastructuretools.TypedOrderedDict.__setitem__(self, key, value)
