5282d8008f4f8d34eaa919b2b82bd6f20bdd72f4
Trevor Bača <trevorbaca@gmail.com>
2014-05-01 18:04:01 -0400

    Wrote MaterialPackageWrangler.rename_view() tests.

---
+++ scoremanager/wranglers/test/test_MaterialPackageWrangler_rename_view.py
@@ -0,0 +1,37 @@
+# -*- encoding: utf-8 -*-
+from abjad import *
+import scoremanager
+score_manager = scoremanager.core.ScoreManager(is_test=True)
+
+
+def test_MaterialPackageWrangler_rename_view_01():
+    r'''Works in library.
+    '''
+
+    input_ = 'm vnew _test_100 rm all'
+    input_ += ' add instrumentation~(Red~Example~Score) done default q' 
+    score_manager._run(pending_user_input=input_)
+        
+    input_ = 'm vls q'
+    score_manager._run(pending_user_input=input_)
+    contents = score_manager._transcript.contents
+    assert '_test_100' in contents
+    assert '_test_101' not in contents
+
+    input_ = 'm vren _test_100 _test_101 default q'
+    score_manager._run(pending_user_input=input_)
+    contents = score_manager._transcript.contents
+
+    input_ = 'm vls q'
+    score_manager._run(pending_user_input=input_)
+    contents = score_manager._transcript.contents
+    assert '_test_100' not in contents
+    assert '_test_101' in contents
+
+    input_ = 'm vrm _test_101 default q'
+    score_manager._run(pending_user_input=input_)
+
+    input_ = 'm vls q'
+    score_manager._run(pending_user_input=input_)
+    contents = score_manager._transcript.contents
+    assert '_test_101' not in contents
