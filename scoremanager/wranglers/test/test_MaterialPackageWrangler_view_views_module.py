e2979d24ab1c7b8a3123d802dfea1c1e612dc840
Trevor Bača <trevorbaca@gmail.com>
2014-05-01 18:05:03 -0400

    Wrote MaterialPackageWrangler.view_views_module() tests.

---
+++ scoremanager/wranglers/test/test_MaterialPackageWrangler_view_views_module.py
@@ -0,0 +1,22 @@
+# -*- encoding: utf-8 -*-
+from abjad import *
+import scoremanager
+score_manager = scoremanager.core.ScoreManager(is_test=True)
+
+
+def test_MaterialPackageWrangler_view_views_module_01():
+
+    input_ = 'm vmro q'
+    score_manager._run(pending_user_input=input_)
+
+    assert score_manager._session._attempted_to_open_file
+
+
+def test_MaterialPackageWrangler_view_views_module_02():
+
+    input_ = 'blue~example~score m vmro q'
+    score_manager._run(pending_user_input=input_)
+    contents = score_manager._transcript.contents
+
+    assert not score_manager._session._attempted_to_open_file
+    assert 'No views module found.' in contents
