045d8c16715d4f1ba7c9e1489c74f6eb4aaac452
Trevor Bača <trevorbaca@gmail.com>
2014-05-01 10:59:06 -0400

    Wrote MakerModuleWrangler.make_module() testes.

---
+++ scoremanager/wranglers/test/test_MakerModuleWrangler_make_module.py
@@ -0,0 +1,25 @@
+# -*- encoding: utf-8 -*-
+import os
+from abjad import *
+import scoremanager
+score_manager = scoremanager.core.ScoreManager(is_test=True)
+
+
+def test_MakerModuleWrangler_make_module_01():
+
+    path = os.path.join(
+        score_manager._configuration.example_score_packages_directory_path,
+        'red_example_score',
+        'makers',
+        'FooMaker.py',
+        )
+
+    assert not os.path.exists(path)
+    try:
+        input_ = 'red~example~score k new FooMaker.py q'
+        score_manager._run(pending_user_input=input_)
+        assert os.path.exists(path)
+    finally:
+        if os.path.exists(path):
+            os.remove(path)
+    assert not os.path.exists(path)
