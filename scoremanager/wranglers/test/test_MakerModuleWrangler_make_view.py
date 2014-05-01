e7e79300a398b342c914b5861d2a9da1e1758e3d
Trevor Bača <trevorbaca@gmail.com>
2014-05-01 10:15:31 -0400

    Wrote MakerModuleWrangler.make_view() tests.

---
+++ scoremanager/wranglers/test/test_MakerModuleWrangler_make_view.py
@@ -0,0 +1,62 @@
+# -*- encoding: utf-8 -*-
+from abjad import *
+import scoremanager
+
+# is_test=True is ok when testing the creation of views
+score_manager = scoremanager.core.ScoreManager(is_test=True)
+
+
+def test_MakerModuleWrangler_make_view_01():
+    r'''Makes sure view creation menu title is correct.
+    '''
+
+    input_ = 'k vnew _test q' 
+    score_manager._run(pending_user_input=input_)
+    transcript = score_manager._transcript
+
+    string = 'Score manager - maker modules - views - _test - edit:'
+    assert transcript.last_title == string
+
+
+def test_MakerModuleWrangler_make_view_02():
+    r'''Makes sure at least maker module appears in 
+    view creation menu.
+    '''
+
+    input_ = 'k vnew _test q' 
+    score_manager._run(pending_user_input=input_)
+    transcript = score_manager._transcript
+
+    string = 'RedExampleScoreTemplate.py (Red Example Score)'
+    assert string in transcript.contents
+
+
+def test_MakerModuleWrangler_make_view_03():
+    r'''Makes view in library. Removes view.
+
+    Makes sure no extra new lines appear before or after 
+    'written to disk' message.
+    '''
+
+    input_ = 'k vnew _test rm all'
+    input_ += ' add RedExampleScoreTemplate.py~(Red~Example~Score)'
+    input_ += ' done default q' 
+    score_manager._run(pending_user_input=input_)
+
+    lines =['> done', '']
+    assert score_manager._transcript[-5].lines == lines
+
+    lines = ['View inventory written to disk.', '']
+    assert score_manager._transcript[-4].lines == lines
+        
+    input_ = 'k vls vrm _test default q'
+    score_manager._run(pending_user_input=input_)
+    contents = score_manager._transcript.contents
+    assert 'view found' in contents or 'views found' in contents
+    assert '_test' in contents
+
+    input_ = 'k vls q'
+    score_manager._run(pending_user_input=input_)
+    contents = score_manager._transcript.contents
+    assert 'view found' in contents or 'views found' in contents
+    assert '_test' not in contents
