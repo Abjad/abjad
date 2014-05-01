a90964e3e2bff3e7417cdc64a657627e731bca93
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-05-01 12:10:20 -0400

    Made cross-version compatibility changes in experimental.

--- experimental/tools/constrainttools/GlobalReferenceConstraint.py
+++ experimental/tools/constrainttools/GlobalReferenceConstraint.py
@@ -1,4 +1,5 @@
 # -*- encoding: utf-8 -*-
+import sys
 from experimental.tools.constrainttools._GlobalConstraint._GlobalConstraint \
     import _GlobalConstraint
 
@@ -43,7 +44,10 @@ class GlobalReferenceConstraint(_GlobalConstraint):
         object.__setattr__(self, '_kind', 'global')
         object.__setattr__(self, '_reference', reference)
         assert isinstance(predicate, type(lambda: None))
-        assert predicate.func_code.co_argcount == 2
+        if sys.version_info[0] == 2:
+            assert predicate.func_code.co_argcount == 2
+        else:
+            assert predicate.__code__.co_argcount == 2
         object.__setattr__(self, '_predicate', predicate)
 
     ### SPECIAL METHODS ###
