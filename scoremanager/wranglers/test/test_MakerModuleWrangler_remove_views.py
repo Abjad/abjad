bec6f25d3ee088a2368748dc644ee5905fb7f4aa
Trevor Bača <trevorbaca@gmail.com>
2014-05-01 10:23:48 -0400

    Wrote MakerModuleWrangler.remove_views() tests.

---
+++ scoremanager/wranglers/test/test_MakerModuleWrangler_remove_views.py
@@ -0,0 +1,51 @@
+# -*- encoding: utf-8 -*-
+from abjad import *
+import scoremanager
+score_manager = scoremanager.core.ScoreManager(is_test=True)
+
+
+def test_MakerModuleWrangler_remove_views_01():
+    r'''Makes two views. Removes two views at one time.
+    '''
+
+    input_ = 'k vnew _test_100 rm all'
+    input_ += ' add RedExampleScoreTemplate.py~(Red~Example~Score)'
+    input_ += ' done default' 
+    input_ += ' k vnew _test_101 rm all'
+    input_ += ' add RedExampleScoreRhythmMaker.py~(Red~Example~Score)'
+    input_ += ' done default'
+    input_ += ' q' 
+    score_manager._run(pending_user_input=input_)
+
+    input_ = 'k vls q'
+    score_manager._run(pending_user_input=input_)
+    contents = score_manager._transcript.contents
+    assert 'views found' in contents
+    assert '_test_100' in contents
+    assert '_test_101' in contents
+
+    input_ = 'k vrm _test_100-_test_101 default q'
+    score_manager._run(pending_user_input=input_)
+
+    input_ = 'k vls q'
+    score_manager._run(pending_user_input=input_)
+    contents = score_manager._transcript.contents
+    assert 'view found' in contents or 'views found' in contents
+    assert '_test_100' not in contents
+    assert '_test_101' not in contents
+
+
+def test_MakerModuleWrangler_remove_views_02():
+    r'''Makes sure selector backtracking works.
+    '''
+
+    input_ = 'k vrm b q'
+    score_manager._run(pending_user_input=input_)
+
+    titles = [
+        'Score manager - example scores',
+        'Score manager - maker modules',
+        'Score manager - maker modules - select view(s) to remove:',
+        'Score manager - maker modules',
+        ]
+    assert score_manager._transcript.titles == titles
