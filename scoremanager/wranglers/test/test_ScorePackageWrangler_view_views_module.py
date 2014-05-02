2cbc6a7fe8178ef34e98843fd59c84cf45fd9855
Trevor Bača <trevorbaca@gmail.com>
2014-05-02 11:53:56 -0400

    Wrote ScorePackageWrangler.view_views_module() tests.

---
+++ scoremanager/wranglers/test/test_ScorePackageWrangler_view_views_module.py
@@ -0,0 +1,12 @@
+# -*- encoding: utf-8 -*-
+from abjad import *
+import scoremanager
+score_manager = scoremanager.core.ScoreManager(is_test=True)
+
+
+def test_ScorePackageWrangler_view_views_module_01():
+
+    input_ = 'vmro q'
+    score_manager._run(pending_user_input=input_)
+
+    assert score_manager._session._attempted_to_open_file
