db3a0f7e5b6c4c8bb5e0c6c90d419775a462200b
Trevor Bača <trevorbaca@gmail.com>
2014-05-06 14:25:11 -0400

    Wrote SegmentPackageManager.get_metadatum() tests.

---
+++ scoremanager/managers/test/test_SegmentPackageManager_get_metadatum.py
@@ -0,0 +1,30 @@
+# -*- encoding: utf-8 -*-
+from abjad import *
+import scoremanager
+score_manager = scoremanager.core.ScoreManager(is_test=True)
+
+
+def test_SegmentPackageManager_get_metadatum_01():
+
+    # make sure no flavor metadatum found
+    input_ = 'red~example~score g segment~01 mdg flavor default q'
+    score_manager._run(pending_user_input=input_)
+    assert score_manager._transcript.entries[-4].title == 'None'
+
+    # add flavor metadatum
+    input_ = 'red~example~score g segment~01 mda flavor cherry q'
+    score_manager._run(pending_user_input=input_)
+
+    # maker sure flavor metadatum now equal to 'cherry'
+    input_ = 'red~example~score g segment~01 mdg flavor default q'
+    score_manager._run(pending_user_input=input_)
+    assert score_manager._transcript.entries[-4].title == "'cherry'"
+
+    # remove flavor metadatum
+    input_ = 'red~example~score g segment~01 mdrm flavor default q'
+    score_manager._run(pending_user_input=input_)
+
+    # make sure no flavor metadatum found
+    input_ = 'red~example~score g segment~01 mdg flavor default q'
+    score_manager._run(pending_user_input=input_)
+    assert score_manager._transcript.entries[-4].title == 'None'
