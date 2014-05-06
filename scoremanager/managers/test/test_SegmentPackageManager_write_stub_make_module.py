14e37695c59708abd4ef0f1741815c8de4cda535
Trevor Bača <trevorbaca@gmail.com>
2014-05-06 18:23:58 -0400

    Wrote SegmentPackageManager.write_stub_make_module() tests. MILESTONE.
    MILESTONE: tests now exists for all SegmentPackageManager public methods.
    
    What remains are tests for ScorePackageManager public methods.

---
+++ scoremanager/managers/test/test_SegmentPackageManager_write_stub_make_module.py
@@ -0,0 +1,42 @@
+# -*- encoding: utf-8 -*-
+import filecmp
+import os
+import shutil
+from abjad import *
+import scoremanager
+score_manager = scoremanager.core.ScoreManager(is_test=True)
+
+
+def test_SegmentPackageManager_write_stub_make_module_01():
+
+    path = os.path.join(
+        score_manager._configuration.example_score_packages_directory_path,
+        'red_example_score',
+        'segments',
+        'segment_01',
+        '__make__.py',
+        )
+    backup_path = path + '.backup'
+
+    assert os.path.isfile(path)
+    assert not os.path.exists(backup_path)
+
+    try:
+        shutil.copyfile(path, backup_path)
+        assert filecmp.cmp(path, backup_path)
+        os.remove(path)
+        assert not os.path.exists(path)
+        input_ = 'red~example~score g segment~01 mmws y q'
+        score_manager._run(pending_user_input=input_)
+        assert os.path.isfile(path)
+        contents = score_manager._transcript.contents
+        assert 'Will write stub to' in contents
+        assert 'Wrote stub to' in contents
+    finally:
+        assert os.path.isfile(backup_path)
+        shutil.copyfile(backup_path, path)
+        assert filecmp.cmp(path, backup_path)
+        os.remove(backup_path)
+
+    assert os.path.isfile(path)
+    assert not os.path.exists(backup_path)
