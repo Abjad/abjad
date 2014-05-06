3c556d7a109c42490bbb694c9726e2022adc05d9
Trevor Bača <trevorbaca@gmail.com>
2014-05-06 19:56:18 -0400

    Wrote ScorePackageManager.pytest() tests.

---
+++ scoremanager/managers/test/test_ScorePackageManager_pytest.py
@@ -0,0 +1,19 @@
+# -*- encoding: utf-8 -*-
+from abjad import *
+import scoremanager
+score_manager = scoremanager.core.ScoreManager(is_test=True)
+
+
+def test_ScorePackageManager_pytest_01():
+
+    input_ = 'red~example~score pyt q'
+    score_manager._run(pending_user_input=input_)
+    transcript_contents = score_manager._transcript.contents
+
+    strings = [
+        'Running py.test ...',
+        '1 testable asset found ...',
+        ]
+
+    for string in strings:
+        assert string in transcript_contents
