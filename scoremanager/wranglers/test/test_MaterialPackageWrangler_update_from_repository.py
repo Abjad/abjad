7c28c3c79d310c301b08ab31c953b07511c82bf5
Trevor Bača <trevorbaca@gmail.com>
2014-05-06 10:17:50 -0400

    Wrote MaterialPackageManager.update_from_repository() tests.

--- scoremanager/wranglers/test/test_MaterialPackageWrangler_update_from_repository.py
+++ scoremanager/wranglers/test/test_MaterialPackageWrangler_update_from_repository.py
@@ -1,15 +1,14 @@
 # -*- encoding: utf-8 -*-
-import pytest
 from abjad import *
 import scoremanager
+score_manager = scoremanager.core.ScoreManager(is_test=True)
+score_manager._session._is_repository_test = True
 
 
 def test_MaterialPackageWrangler_update_from_repository_01():
     r'''Works in score.
     '''
 
-    score_manager = scoremanager.core.ScoreManager(is_test=True)
-    score_manager._session._is_repository_test = True
     input_ = 'red~example~score m rup default q'
     score_manager._run(pending_user_input=input_)
     assert score_manager._session._attempted_to_update_from_repository
@@ -19,8 +18,6 @@ def test_MaterialPackageWrangler_update_from_repository_02():
     r'''Works in library.
     '''
 
-    score_manager = scoremanager.core.ScoreManager(is_test=True)
-    score_manager._session._is_repository_test = True
     input_ = 'm rup default q'
     score_manager._run(pending_user_input=input_)
     assert score_manager._session._attempted_to_update_from_repository
