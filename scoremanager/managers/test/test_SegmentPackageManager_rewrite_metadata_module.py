c9f0d68175ed9501baacddfec48b24b717a18126
Trevor Bača <trevorbaca@gmail.com>
2014-05-06 17:59:20 -0400

    Wrote SegmentPackageManager metadata tests.

---
+++ scoremanager/managers/test/test_SegmentPackageManager_rewrite_metadata_module.py
@@ -0,0 +1,13 @@
+# -*- encoding: utf-8 -*-
+from abjad import *
+import scoremanager
+score_manager = scoremanager.core.ScoreManager(is_test=True)
+
+
+def test_SegmentPackageManager_rewrite_metadata_module_01():
+
+    input_ = 'red~example~score g segment~01 mdmrw default q'
+    score_manager._run(pending_user_input=input_)
+    contents = score_manager._transcript.contents
+
+    assert 'Rewrote metadata module.' in contents
