80c1aa9094fc4f6bb24fa1825497a0abdbe440b0
Trevor Bača <trevorbaca@gmail.com>
2014-05-06 17:56:39 -0400

    Wrote SegmentPackageManager.open_versioned_*() tests.

---
+++ scoremanager/managers/test/test_SegmentPackageManager_open_versioned_output_pdfs.py
@@ -0,0 +1,12 @@
+# -*- encoding: utf-8 -*-
+from abjad import *
+import scoremanager
+score_manager = scoremanager.core.ScoreManager(is_test=True)
+
+
+def test_SegmentPackageManager_open_versioned_output_pdfs_01():
+
+    input_ = 'red~example~score g segment~01 vpdfso 0 q'
+    score_manager._run(pending_user_input=input_)
+
+    assert score_manager._session._attempted_to_open_file
