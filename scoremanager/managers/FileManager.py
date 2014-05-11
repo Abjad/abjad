4533a9c1e1cf927fc0f83aad8a1cd08687ee17c4
Trevor Bača <trevorbaca@gmail.com>
2014-05-11 12:29:44 -0400

    Removed FileManager._execute().
    Use IOManager.execute_file() instead.

--- scoremanager/managers/FileManager.py
+++ scoremanager/managers/FileManager.py
@@ -1,6 +1,4 @@
 # -*- encoding: utf-8 -*-
-import os
-import traceback
 from scoremanager.managers.Manager import Manager
 
 
@@ -8,31 +6,4 @@ class FileManager(Manager):
     r'''File manager.
     '''
 
-    ### CLASS VARIABLES ###
-
-    __slots__ = (
-        )
-
-    ### PRIVATE METHODS ###
-
-    def _execute(self, path=None, attribute_names=None):
-        assert isinstance(attribute_names, tuple)
-        path = path or self._path
-        if not os.path.isfile(path):
-            return
-        with open(path, 'r') as file_pointer:
-            file_contents_string = file_pointer.read()
-        try:
-            exec(file_contents_string)
-        except:
-            traceback.print_exc()
-            self._io_manager.display('')
-            return 'corrupt'
-        result = []
-        for name in attribute_names:
-            if name in locals():
-                result.append(locals()[name])
-            else:
-                result.append(None)
-        result = tuple(result)
-        return result
+    pass
