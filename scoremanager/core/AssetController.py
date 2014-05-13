60f44d1b8d0e8dd8b7ed9c09201724f4fb833438
Trevor Bača <trevorbaca@gmail.com>
2014-05-13 10:12:25 -0400

    Consolidating global navigation commands.

--- scoremanager/core/AssetController.py
+++ scoremanager/core/AssetController.py
@@ -9,3 +9,64 @@ class AssetController(Controller):
 
     __slots__ = (
         )
+
+    ### PRIVATE PROPERTIES ###
+
+    @property
+    def _input_to_action(self):
+        superclass = super(AssetController, self)
+        result = superclass._input_to_action
+        result = result.copy()
+        result.update({
+            'd': self.go_to_distribution_files,
+            'g': self.go_to_segment_packages,
+            'k': self.go_to_maker_files,
+            'm': self.go_to_material_packages,
+            'u': self.go_to_build_files,
+            'y': self.go_to_stylesheets,
+            })
+        return result
+
+    ### PUBLIC METHODS ###
+
+    def go_to_build_files(self):
+        r'''Goes to build files.
+
+        Returns none.
+        '''
+        self._session._score_manager._build_file_wrangler._run()
+
+    def go_to_distribution_files(self):
+        r'''Goes to distribution files.
+
+        Returns none.
+        '''
+        self._session._score_manager._distribution_file_wrangler._run()
+
+    def go_to_maker_files(self):
+        r'''Goes to maker files.
+
+        Returns none.
+        '''
+        self._session._score_manager._maker_file_wrangler._run()
+
+    def go_to_material_packages(self):
+        r'''Goes to material packages.
+
+        Returns none.
+        '''
+        self._session._score_manager._material_package_wrangler._run()
+
+    def go_to_segment_packages(self):
+        r'''Goes to segment packages.
+
+        Returns none.
+        '''
+        self._session._score_manager._segment_package_wrangler._run()
+
+    def go_to_stylesheets(self):
+        r'''Goes to stylesheets.
+
+        Returns none.
+        '''
+        self._session._score_manager._stylesheet_wrangler._run()
