d37b853d3caf65f6dafbb17f8f4a2cb3e1eba419
Trevor Bača <trevorbaca@gmail.com>
2014-05-16 17:37:45 -0400

    Wrote (ess) tests.

---
+++ scoremanager/wranglers/test/test_MakerFileWrangler_edit_score_stylesheet.py
@@ -0,0 +1,12 @@
+# -*- encoding: utf-8 -*-
+from abjad import *
+import scoremanager
+score_manager = scoremanager.core.ScoreManager(is_test=True)
+
+
+def test_MakerFileWrangler_edit_score_stylesheet_01():
+
+    input_ = 'red~example~score k ess q'
+    score_manager._run(pending_input=input_)
+
+    assert score_manager._session._attempted_to_open_file
