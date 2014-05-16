e0fd9b2df3be764bffdff422e70d99ec488060e1
Trevor Bača <trevorbaca@gmail.com>
2014-05-16 17:11:27 -0400

    Wrote (<<) and (>>) tests for managers.
    These complement existing (<<) and (>>) tests for wranglers.

---
+++ scoremanager/managers/test/test_MaterialPackageManager_go_to_next_score.py
@@ -0,0 +1,18 @@
+# -*- encoding: utf-8 -*-
+from abjad import *
+import scoremanager
+score_manager = scoremanager.core.ScoreManager(is_test=True)
+
+
+def test_MaterialPackageManager_go_to_next_score_01():
+
+    input_ = 'red~example~score m tempo~inventory >> q'
+    score_manager._run(pending_input=input_)
+    titles = [
+        'Score manager - example scores',
+        'Red Example Score (2013)',
+        'Red Example Score (2013) - materials',
+        'Red Example Score (2013) - materials - tempo inventory (AE)',
+        'Blue Example Score (2013)',
+        ]
+    assert score_manager._transcript.titles == titles
---
+++ scoremanager/managers/test/test_ScorePackageManager_go_to_next_score.py
@@ -0,0 +1,16 @@
+# -*- encoding: utf-8 -*-
+from abjad import *
+import scoremanager
+score_manager = scoremanager.core.ScoreManager(is_test=True)
+
+
+def test_ScorePackageManager_go_to_next_score_01():
+
+    input_ = 'red~example~score >> q'
+    score_manager._run(pending_input=input_)
+    titles = [
+        'Score manager - example scores',
+        'Red Example Score (2013)',
+        'Blue Example Score (2013)',
+        ]
+    assert score_manager._transcript.titles == titles
