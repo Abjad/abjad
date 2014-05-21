31db716b14a71466e0cf67d8609ea5af85d1ea0a
Trevor Bača <trevorbaca@gmail.com>
2014-05-21 14:08:06 -0400

    Test milestone.
    Tests exist for all 48 MaterialPackageWrangler public methods.

--- scoremanager/wranglers/test/test_MaterialPackageWrangler_rewrite_every_metadata_py.py
+++ scoremanager/wranglers/test/test_MaterialPackageWrangler_rewrite_every_metadata_py.py
@@ -7,6 +7,23 @@ score_manager = scoremanager.core.ScoreManager(is_test=True)
 
 def test_MaterialPackageWrangler_rewrite_every_metadata_py_01():
 
+    input_ = 'red~example~score m mdrw default q'
+    score_manager._run(pending_input=input_)
+    contents = score_manager._transcript.contents
+
+    path = score_manager._configuration.example_score_packages_directory
+    materials_directory = os.path.join(
+        score_manager._configuration.example_score_packages_directory,
+        'red_example_score', 
+        'materials',
+        )
+    assert materials_directory in contents
+    assert 'Rewriting' in contents
+    assert '__metadata__.py files rewritten.' in contents
+
+
+def test_MaterialPackageWrangler_rewrite_every_metadata_py_02():
+
     input_ = 'm mdrw default q'
     score_manager._run(pending_input=input_)
     contents = score_manager._transcript.contents
