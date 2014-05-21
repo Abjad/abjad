0b7870e2a06764edd4c36683434b242cca2618c0
Trevor Bača <trevorbaca@gmail.com>
2014-05-21 14:05:36 -0400

    Fixed MaterialPackageWrangler (mdrw) test.

--- scoremanager/wranglers/test/test_MaterialPackageWrangler_rewrite_every_metadata_py.py
+++ scoremanager/wranglers/test/test_MaterialPackageWrangler_rewrite_every_metadata_py.py
@@ -5,20 +5,18 @@ import scoremanager
 score_manager = scoremanager.core.ScoreManager(is_test=True)
 
 
-def test_MaterialPackageWrangler_rewrite_every_metadata_py_02():
+def test_MaterialPackageWrangler_rewrite_every_metadata_py_01():
 
     input_ = 'm mdrw default q'
     score_manager._run(pending_input=input_)
     contents = score_manager._transcript.contents
 
     path = score_manager._configuration.example_score_packages_directory
-    paths = [
-        os.path.join(path, 'blue_example_score', '__metadata__.py'),
-        os.path.join(path, 'etude_example_score', '__metadata__.py'),
-        os.path.join(path, 'red_example_score', '__metadata__.py'),
-        ]
-    for path in paths:
-        assert path in contents
-
+    materials_directory = os.path.join(
+        score_manager._configuration.example_score_packages_directory,
+        'red_example_score', 
+        'materials',
+        )
+    assert materials_directory in contents
     assert 'Rewriting' in contents
     assert '__metadata__.py files rewritten.' in contents
