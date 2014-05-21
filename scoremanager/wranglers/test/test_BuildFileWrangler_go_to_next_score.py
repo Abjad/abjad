44205b94fd9c438e8944a66420f397e7d0ea3bb9
Trevor Bača <trevorbaca@gmail.com>
2014-05-21 09:17:37 -0400

    Wrote (<<), (>>) tests for wranglers.

---
+++ scoremanager/wranglers/test/test_BuildFileWrangler_go_to_next_score.py
@@ -0,0 +1,31 @@
+# -*- encoding: utf-8 -*-
+from abjad import *
+import scoremanager
+score_manager = scoremanager.core.ScoreManager(is_test=True)
+
+
+def test_BuildFileWrangler_go_to_next_score_01():
+
+    input_ = 'red~example~score u >> q'
+    score_manager._run(pending_input=input_)
+
+    titles = [
+        'Score manager - scores',
+        'Red Example Score (2013)',
+        'Red Example Score (2013) - build files',
+        'Blue Example Score (2013)',
+        ]
+    assert score_manager._transcript.titles == titles
+
+
+def test_BuildFileWrangler_go_to_next_score_02():
+
+    input_ = 'u >> q'
+    score_manager._run(pending_input=input_)
+
+    titles = [
+        'Score manager - scores',
+        'Score manager - build files',
+        'Blue Example Score (2013)',
+        ]
+    assert score_manager._transcript.titles == titles
---
+++ scoremanager/wranglers/test/test_StylesheetWrangler_go_to_previous_score.py
@@ -0,0 +1,31 @@
+# -*- encoding: utf-8 -*-
+from abjad import *
+import scoremanager
+score_manager = scoremanager.core.ScoreManager(is_test=True)
+
+
+def test_StylesheetWrangler_go_to_previous_score_01():
+
+    input_ = 'red~example~score y << q'
+    score_manager._run(pending_input=input_)
+
+    titles = [
+        'Score manager - scores',
+        'Red Example Score (2013)',
+        'Red Example Score (2013) - stylesheets',
+        'Étude Example Score (2013)',
+        ]
+    assert score_manager._transcript.titles == titles
+
+
+def test_StylesheetWrangler_go_to_previous_score_02():
+
+    input_ = 'y << q'
+    score_manager._run(pending_input=input_)
+
+    titles = [
+        'Score manager - scores',
+        'Score manager - stylesheets',
+        'Red Example Score (2013)',
+        ]
+    assert score_manager._transcript.titles == titles
