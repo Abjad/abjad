d9ab71cfeef8d7e054123580496bda62bb4e2db7
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-05-25 12:51:40 -0400

    Implemented persist(expr).as_png().
    `as_png()` handles multi-page output correctly.

--- abjad/tools/agenttools/PersistenceAgent.py
+++ abjad/tools/agenttools/PersistenceAgent.py
@@ -27,6 +27,8 @@ class PersistenceAgent(abctools.AbjadObject):
         '_client',
         )
 
+    _png_page_pattern = re.compile(r'.+page(\d+)\.png')
+
     ### INITIALIZER ###
 
     def __init__(self, client=None):
@@ -229,6 +231,117 @@ class PersistenceAgent(abctools.AbjadObject):
             os.remove(ly_file_path)
         return (
             pdf_file_path,
+            abjad_formatting_time,
+            lilypond_rendering_time,
+            success,
+            )
+
+    def as_png(
+        self,
+        png_file_path=None,
+        remove_ly=False,
+        illustrate_function=None,
+        **kwargs
+        ):
+        r'''Persists client as PNG.
+
+        ::
+
+            >>> staff = Staff()
+            >>> measure = Measure((4, 4), "c'4 d'4 e'4 f'4")
+            >>> command = indicatortools.LilyPondCommand('break', 'after')
+            >>> attach(command, measure[-1])
+            >>> staff.extend(measure * 200)
+
+        ::
+
+            >>> result = persist(staff).as_png() # doctest: +SKIP
+            >>> for x in result[0]: # doctest: +SKIP
+            ...     x
+            ...
+            '/Users/josiah/Desktop/test-page1.png'
+            '/Users/josiah/Desktop/test-page2.png'
+            '/Users/josiah/Desktop/test-page3.png'
+            '/Users/josiah/Desktop/test-page4.png'
+            '/Users/josiah/Desktop/test-page5.png'
+            '/Users/josiah/Desktop/test-page6.png'
+            '/Users/josiah/Desktop/test-page7.png'
+            '/Users/josiah/Desktop/test-page8.png'
+            '/Users/josiah/Desktop/test-page9.png'
+            '/Users/josiah/Desktop/test-page10.png'
+            '/Users/josiah/Desktop/test-page11.png'
+            '/Users/josiah/Desktop/test-page12.png'
+            '/Users/josiah/Desktop/test-page13.png'
+            '/Users/josiah/Desktop/test-page14.png'
+            '/Users/josiah/Desktop/test-page15.png'
+
+        Autogenerates file path when `png_file_path` is none.
+
+        Returns output path(s), elapsed formatting time and elapsed rendering
+        time.
+        '''
+        import shutil
+        import tempfile
+        from abjad.tools import systemtools
+        if illustrate_function is None:
+            assert '__illustrate__' in dir(self._client)
+        if png_file_path is not None:
+            png_file_path = os.path.expanduser(png_file_path)
+            without_extension = os.path.splitext(png_file_path)[0]
+            ly_file_path = '{}.ly'.format(without_extension)
+        else:
+            ly_file_path = None
+        result = self.as_ly(
+            ly_file_path,
+            illustrate_function=illustrate_function,
+            **kwargs
+            )
+
+        ly_file_path, abjad_formatting_time = result
+
+        original_directory = os.path.split(ly_file_path)[0]
+        original_ly_file_path = ly_file_path
+        temporary_directory = tempfile.mkdtemp()
+        temporary_ly_file_path = os.path.join(
+            temporary_directory,
+            os.path.split(ly_file_path)[1],
+            )
+        shutil.copy(original_ly_file_path, temporary_ly_file_path)
+
+        timer = systemtools.Timer()
+        with timer:
+            success = systemtools.IOManager.run_lilypond(
+                temporary_ly_file_path,
+                flags='--png',
+                )
+        lilypond_rendering_time = timer.elapsed_time
+
+        png_file_paths = []
+        for file_name in os.listdir(temporary_directory):
+            if not file_name.endswith('.png'):
+                continue
+            source_png_file_path = os.path.join(
+                temporary_directory,
+                file_name,
+                )
+            target_png_file_path = os.path.join(
+                original_directory,
+                file_name,
+                )
+            shutil.move(source_png_file_path, target_png_file_path)
+            png_file_paths.append(target_png_file_path)
+        shutil.rmtree(temporary_directory)
+
+        if remove_ly:
+            os.remove(ly_file_path)
+
+        if 1 < len(png_file_paths):
+            png_file_paths.sort(
+                key=lambda x: int(self._png_page_pattern.match(x).groups()[0]),
+                )
+
+        return (
+            tuple(png_file_paths),
             abjad_formatting_time,
             lilypond_rendering_time,
             success,
