d8ed86bdc38d57abd9718f3cb1cc36bc64630bcd
Trevor Bača <trevorbaca@gmail.com>
2014-05-30 14:49:18 -0400

    Wrote (oo*) tests.
    Description: the command lets you open (and subsequently edit) the output.py
    files in all visible material packages:
    
    Example:
    
        Red Example Score (2013) - materials
    
           1: instrumentation (AE)
           2: magic numbers
           3: pitch range inventory (AE)
           4: tempo inventory (AE)
           5: time signatures
    
              materials - copy (cp)
              materials - new (new)
              materials - remove (rm)
              materials - rename (ren)
    
        > oo*
        Will open ...
           /Users/trevorbaca/Documents/abjad/scoremanager/scores/red_example_score/materials/instrumentation/output.py
           /Users/trevorbaca/Documents/abjad/scoremanager/scores/red_example_score/materials/magic_numbers/output.py
           /Users/trevorbaca/Documents/abjad/scoremanager/scores/red_example_score/materials/pitch_range_inventory/output.py
           /Users/trevorbaca/Documents/abjad/scoremanager/scores/red_example_score/materials/tempo_inventory/output.py
           /Users/trevorbaca/Documents/abjad/scoremanager/scores/red_example_score/materials/time_signatures/output.py
        Ok? y
        5 files to edit
    
        > q

---
+++ scoremanager/wranglers/test/test_MaterialPackageWrangler_open_every_output.py
@@ -0,0 +1,38 @@
+# -*- encoding: utf-8 -*-
+import os
+from abjad import *
+import scoremanager
+score_manager = scoremanager.core.ScoreManager(is_test=True)
+
+
+def test_MaterialPackageWrangler_open_every_output_01():
+
+    input_ = 'red~example~score m oo* y q'
+    score_manager._run(input_=input_)
+    contents = score_manager._transcript.contents
+
+    assert score_manager._session._attempted_to_open_file
+
+    package_names = (
+        'instrumentation',
+        'magic_numbers',
+        'pitch_range_inventory',
+        'tempo_inventory',
+        )
+
+    paths = []
+    for package_name in package_names:
+        path = os.path.join(
+            score_manager._configuration.example_score_packages_directory,
+            'red_example_score',
+            'materials',
+            package_name,
+            'output.py',
+            )
+
+    lines = []
+    lines.append('Will open ...')
+    lines.extend(paths)
+
+    for line in lines:
+        assert line in contents
