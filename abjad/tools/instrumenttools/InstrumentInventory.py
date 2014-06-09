b4086762c9d7f408167e338abdcf5b4955a80824
Trevor Bača <trevorbaca@gmail.com>
2014-06-09 16:42:07 -0400

    Extended InstrumentInventory.
    Implemented nested class to handle item creation.

--- abjad/tools/instrumenttools/InstrumentInventory.py
+++ abjad/tools/instrumenttools/InstrumentInventory.py
@@ -1,5 +1,6 @@
 # -*- encoding: utf-8 -*-
 from abjad.tools.datastructuretools.TypedList import TypedList
+from abjad.tools.topleveltools import new
 
 
 class InstrumentInventory(TypedList):
@@ -47,12 +48,122 @@ class InstrumentInventory(TypedList):
         contents = ', '.join(contents)
         return '{}([{}])'.format(type(self).__name__, contents)
 
+    @staticmethod
+    def _change_instrument_name_to_instrument(instrument_name):
+        from abjad.tools import instrumenttools
+        if instrument_name in (
+            'alto',
+            'baritone',
+            'bass',
+            'soprano',
+            'tenor',
+            ):
+            instrument_name = instrument_name + ' Voice'
+        instrument_name = instrument_name.title()
+        instrument_name = instrument_name.replace(' ', '')
+        command = 'instrument = instrumenttools.{}()'.format(instrument_name)
+        exec(command)
+        return instrument
+
+    ### WIZARD ###
+
+    def _make_wizard(self):
+        from scoremanager.core.Controller import Controller
+        class InstrumentCreationWizard(Controller):
+            ### CLASS VARIABLES ###
+            __slots__ = ('_is_ranged', '_target',)
+            ### INITIALIZER ###
+            def __init__(
+                self,
+                is_ranged=False,
+                session=None,
+                target=None,
+                ):
+                Controller.__init__(self, session=session)
+                self._is_ranged = is_ranged
+                self._target = target
+            ### PRIVATE METHODS ###
+#            @staticmethod
+#            def _change_instrument_name_to_instrument(instrument_name):
+#                if instrument_name in (
+#                    'alto',
+#                    'baritone',
+#                    'bass',
+#                    'soprano',
+#                    'tenor',
+#                    ):
+#                    instrument_name = instrument_name + ' Voice'
+#                instrument_name = instrument_name.title()
+#                instrument_name = instrument_name.replace(' ', '')
+#                command = 'instrument = instrumenttools.{}()'.format(instrument_name)
+#                exec(command)
+#                return instrument
+            def _name_untuned_percussion(self, instrument):
+                from abjad.tools import instrumenttools
+                from abjad.tools.instrumenttools import UntunedPercussion
+                from scoremanager import iotools
+                if isinstance(instrument, instrumenttools.UntunedPercussion):
+                    items = UntunedPercussion.known_untuned_percussion[:]
+                    selector = iotools.Selector(
+                        session=self._session,
+                        items=items,
+                        )
+                    instrument_name = selector._run()
+                    if self._session.is_backtracking:
+                        return
+                    instrument = new(
+                        instrument,
+                        instrument_name=instrument_name,
+                        short_instrument_name=instrument_name,
+                        )
+                return instrument
+            def _run(self, input_=None):
+                from abjad.tools import instrumenttools
+                from scoremanager import iotools
+                if input_:
+                    self._session._pending_input = input_
+                controller = iotools.ControllerContext(controller=self)
+                with controller:
+                    items = instrumenttools.Instrument._list_instrument_names()
+                    selector = iotools.Selector(
+                        session=self._session,
+                        items=items,
+                        is_ranged=self._is_ranged,
+                        )
+                    result = selector._run()
+                    if self._session.is_backtracking:
+                        return
+                    if isinstance(result, list):
+                        instrument_names = result
+                    else:
+                        instrument_names = [result]
+                    instruments = []
+                    for instrument_name in instrument_names:
+                        #instrument = \
+                        #    self._change_instrument_name_to_instrument(instrument_name)
+                        instrument = \
+                            InstrumentInventory._change_instrument_name_to_instrument(instrument_name)
+                        instrument = self._name_untuned_percussion(instrument)
+                        instruments.append(instrument)
+                    if self._is_ranged:
+                        result = instruments[:]
+                    else:
+                        result = instruments[0]
+                    self._target = result
+                    return self.target
+            @property
+            def target(self):
+                return self._target
+        return InstrumentCreationWizard
+
     ### PRIVATE PROPERTIES ###
 
     @property
     def _item_creator_class(self):
-        from scoremanager import wizards
-        return wizards.InstrumentCreationWizard
+        #from scoremanager import wizards
+        #return wizards.InstrumentCreationWizard
+        wizard = self._make_wizard()
+        return wizard
 
     @property
     def _item_creator_class_kwargs(self):
--- scoremanager/wizards/test/test_InstrumentCreationWizard__run.py
+++ scoremanager/wizards/test/test_InstrumentCreationWizard__run.py
@@ -2,11 +2,14 @@
 from abjad import *
 import scoremanager
 session = scoremanager.core.Session()
+inventory = instrumenttools.InstrumentInventory()
+class_ = inventory._make_wizard()
 
 
 def test_InstrumentCreationWizard__run_01():
 
-    wizard = scoremanager.wizards.InstrumentCreationWizard(session=session)
+    #wizard = scoremanager.wizards.InstrumentCreationWizard(session=session)
+    wizard = class_(session=session)
     input_ = 'violin'
     wizard._run(input_=input_)
     assert wizard.target == instrumenttools.Violin()
@@ -14,7 +17,8 @@ def test_InstrumentCreationWizard__run_01():
 
 def test_InstrumentCreationWizard__run_02():
 
-    wizard = scoremanager.wizards.InstrumentCreationWizard(session=session)
+    #wizard = scoremanager.wizards.InstrumentCreationWizard(session=session)
+    wizard = class_(session=session)
     input_ = 'untuned vibraslap'
     wizard._run(input_=input_)
     vibraslap = instrumenttools.UntunedPercussion(
@@ -26,10 +30,11 @@ def test_InstrumentCreationWizard__run_02():
 
 def test_InstrumentCreationWizard__run_03():
 
-    wizard = scoremanager.wizards.InstrumentCreationWizard(
-        is_ranged=True,
-        session=session,
-        )
+    #wizard = scoremanager.wizards.InstrumentCreationWizard(
+    #    is_ranged=True,
+    #    session=session,
+    #    )
+    wizard = class_(is_ranged=True, session=session)
     input_ = 'violin, viola'
     wizard._run(input_=input_)
     instruments = [instrumenttools.Violin(), instrumenttools.Viola()]
@@ -38,10 +43,11 @@ def test_InstrumentCreationWizard__run_03():
 
 def test_InstrumentCreationWizard__run_04():
 
-    wizard = scoremanager.wizards.InstrumentCreationWizard(
-        is_ranged=True,
-        session=session,
-        )
+    #wizard = scoremanager.wizards.InstrumentCreationWizard(
+    #    is_ranged=True,
+    #    session=session,
+    #    )
+    wizard = class_(is_ranged=True, session=session)
     input_ = 'violin, viola, untuned vibraslap'
     wizard._run(input_=input_)
     instruments = [
