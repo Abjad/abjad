b05f80b2cf4cab3047fa841902a6aace61f896c6
Trevor Bača <trevorbaca@gmail.com>
2014-06-10 13:21:33 -0400

    Updated (ds) test.

--- scoremanager/idetools/test/test_MaterialPackageManager_write_stub_definition_py.py
+++ scoremanager/idetools/test/test_MaterialPackageManager_write_stub_definition_py.py
@@ -1,5 +1,4 @@
 # -*- encoding: utf-8 -*-
-import filecmp
 import os
 from abjad import *
 import scoremanager
@@ -8,16 +7,15 @@ score_manager = scoremanager.idetools.AbjadIDE(is_test=True)
 
 def test_MaterialPackageManager_write_stub_definition_py_01():
 
-    path = os.path.join(
+    package = os.path.join(
         score_manager._configuration.example_score_packages_directory,
         'red_example_score',
         'materials',
-        'magic_numbers',
-        'definition.py',
+        'test_package',
         )
+    definition_py = os.path.join(package, 'definition.py')
 
-    with systemtools.FilesystemState(keep=[path]):
-        input_ = 'red~example~score m magic~numbers ds y q'
+    with systemtools.FilesystemState(remove=[package]):
+        input_ = 'red~example~score m new test~package ds y q'
         score_manager._run(input_=input_)
-        assert os.path.isfile(path)
-        assert not filecmp.cmp(path, path + '.backup')
+        assert os.path.isfile(definition_py)
