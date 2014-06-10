6da58999a6e03d581f1a8af6f3a41ba7146a5cb5
Trevor Bača <trevorbaca@gmail.com>
2014-06-10 11:52:20 -0400

    Added sargasso measures package to Blue Example Score materials.
    The package is currently a stub.

---
+++ scoremanager/scores/blue_example_score/materials/__metadata__.py
@@ -0,0 +1,7 @@
+# -*- encoding: utf-8 -*-
+import collections
+
+
+metadata = collections.OrderedDict([
+    ('view_name', None),
+    ])
---
+++ scoremanager/scores/blue_example_score/materials/sargasso_measures/definition.py
@@ -0,0 +1,6 @@
+# -*- encoding: utf-8 -*-
+from abjad import *
+output_py_import_statements = []
+
+
+sargasso_measures = None
