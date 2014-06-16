ca1923a7b1b9b1f8aa8879f79d66fa1a57d25760
Trevor Bača <trevorbaca@gmail.com>
2014-06-16 17:09:16 -0500

    Wrote (U) tests.

--- scoremanager/idetools/test/test_DistributionFileWrangler_go_to_all_build_files.py
+++ scoremanager/idetools/test/test_DistributionFileWrangler_go_to_all_build_files.py
@@ -4,30 +4,30 @@ import scoremanager
 score_manager = scoremanager.idetools.AbjadIDE(is_test=True)
 
 
-def test_BuildFileWrangler_go_to_all_build_files_01():
-    r'''From score build files to all build files.
+def test_DistributionFileWrangler_go_to_all_build_files_01():
+    r'''From score distribution files to all build files.
     '''
 
-    input_ = 'red~example~score u U q'
+    input_ = 'red~example~score d U q'
     score_manager._run(input_=input_)
     titles = [
         'Abjad IDE - scores',
         'Red Example Score (2013)',
-        'Red Example Score (2013) - build files',
+        'Red Example Score (2013) - distribution files',
         'Abjad IDE - build files',
         ]
     assert score_manager._transcript.titles == titles
 
 
-def test_BuildFileWrangler_go_to_all_build_files_02():
-    r'''From all build files to all build files.
+def test_DistributionFileWrangler_go_to_all_build_files_02():
+    r'''From all distribution files to all build files.
     '''
 
-    input_ = 'U U q'
+    input_ = 'D U q'
     score_manager._run(input_=input_)
     titles = [
         'Abjad IDE - scores',
-        'Abjad IDE - build files',
+        'Abjad IDE - distribution files',
         'Abjad IDE - build files',
         ]
     assert score_manager._transcript.titles == titles
--- scoremanager/idetools/test/test_SegmentPackageWrangler_go_to_all_build_files.py
+++ scoremanager/idetools/test/test_SegmentPackageWrangler_go_to_all_build_files.py
@@ -4,30 +4,30 @@ import scoremanager
 score_manager = scoremanager.idetools.AbjadIDE(is_test=True)
 
 
-def test_BuildFileWrangler_go_to_all_build_files_01():
-    r'''From score build files to all build files.
+def test_SegmentPackageWrangler_go_to_all_build_files_01():
+    r'''From score segments to all build files.
     '''
 
-    input_ = 'red~example~score u U q'
+    input_ = 'red~example~score g U q'
     score_manager._run(input_=input_)
     titles = [
         'Abjad IDE - scores',
         'Red Example Score (2013)',
-        'Red Example Score (2013) - build files',
+        'Red Example Score (2013) - segments',
         'Abjad IDE - build files',
         ]
     assert score_manager._transcript.titles == titles
 
 
-def test_BuildFileWrangler_go_to_all_build_files_02():
-    r'''From all build files to all build files.
+def test_SegmentPackageWrangler_go_to_all_build_files_02():
+    r'''From all segments to all build files.
     '''
 
-    input_ = 'U U q'
+    input_ = 'G U q'
     score_manager._run(input_=input_)
     titles = [
         'Abjad IDE - scores',
-        'Abjad IDE - build files',
+        'Abjad IDE - segments',
         'Abjad IDE - build files',
         ]
     assert score_manager._transcript.titles == titles
