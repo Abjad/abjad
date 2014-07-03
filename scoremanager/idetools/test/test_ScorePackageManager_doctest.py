51efb6625d4348fa1c83834cfc2e054eea2c229d
Trevor Bača <trevorbaca@gmail.com>
2014-07-03 14:42:03 -0400

    Updated doctest tests.

--- scoremanager/idetools/test/test_ScorePackageManager_doctest.py
+++ scoremanager/idetools/test/test_ScorePackageManager_doctest.py
@@ -12,11 +12,11 @@ def test_ScorePackageManager_doctest_01():
 
     strings = [
         'Running doctest ...',
-        '60 testable assets found ...',
+        '61 testable assets found ...',
         '\n__init__.py OK\n',
         '\n__metadata__.py OK\n',
         '\nmakers/RedExampleScoreTemplate.py OK\n',
-        '0 of 0 tests passed in 60 modules.',
+        '0 of 0 tests passed in 61 modules.',
         ]
 
     for string in strings:
