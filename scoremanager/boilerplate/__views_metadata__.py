4cc159728c4015c2720bef02677e858fda1e9ddf
Trevor Bača <trevorbaca@gmail.com>
2014-07-07 17:49:38 -0400

    Added __views_metadata__.py boilerplate.

---
+++ scoremanager/boilerplate/__views_metadata__.py
@@ -0,0 +1,14 @@
+# -*- encoding: utf-8 -*-
+import collections
+
+
+metadata = collections.OrderedDict([
+    ('AbjadIDE_view_name', None),
+    ('BuildFileWrangler_view_name', None),
+    ('DistributionFileWrangler_view_name', None),
+    ('MakerFileWrangler_view_name', None),
+    ('MaterialPackageWrangler_view_name', None),
+    ('ScorePackageWrangler_view_name', None),
+    ('SegmentPackageWrangler_view_name', None),
+    ('StylesheetWrangler_view_name', None),
+    ])
