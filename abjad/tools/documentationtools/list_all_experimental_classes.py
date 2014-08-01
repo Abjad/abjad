8639993be0a82b4885b5a4415ce8ae0af6529807
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-08-01 16:51:49 -0400

    Reimplemented class listing functions.

--- abjad/tools/documentationtools/list_all_experimental_classes.py
+++ abjad/tools/documentationtools/list_all_experimental_classes.py
@@ -11,20 +11,5 @@ def list_all_experimental_classes(modules=None):
         >>> all_classes = documentationtools.list_all_experimental_classes()
 
     '''
-    from abjad import abjad_configuration
     from abjad.tools import documentationtools
-    all_classes = set()
-    paths = []
-    if modules is None:
-        paths.append(abjad_configuration.abjad_experimental_directory)
-    elif isinstance(modules, types.ModuleType):
-        paths.extend(modules.__path__)
-    elif isinstance(modules, collections.Iterable):
-        for module in modules:
-            if isinstance(module, types.ModuleType):
-                paths.extend(module.__path__)
-    for path in paths:
-        class_documenter = documentationtools.ClassCrawler(path)
-        for x in class_documenter():
-            all_classes.add(x)
-    return list(all_classes)
+    return documentationtools.list_all_classes('experimental')
