5d4808138002129cf2082e58fd7a34b02009b553
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-08-01 16:50:18 -0400

    Added `documentationtools.list_all_classes()`.
    This function generalized the previous three class listing functions:
    
    - `list_all_abjad_classes()`
    - `list_all_experimental_classes()`
    - `list_all_scoremanager_classes()`
    
    Those functions will be reimplemented in terms of `list_all_classes()`.

---
+++ abjad/tools/documentationtools/list_all_classes.py
@@ -0,0 +1,45 @@
+# -*- encoding: utf-8 -*-
+import collections
+import importlib
+import types
+
+
+def list_all_classes(modules=None):
+    r'''Lists all public classes defined in `path`.
+
+    ::
+
+        >>> all_classes = documentationtools.list_all_classes(
+        ...     modules='experimental',
+        ...     )
+
+    '''
+    from abjad import abjad_configuration
+    from abjad.tools import documentationtools
+    all_classes = set()
+    paths = []
+    if modules is None:
+        paths.append(abjad_configuration.score_manager_directory)
+    elif isinstance(modules, str):
+        module = importlib.import_module(modules)
+        paths.extend(module.__path__)
+    elif isinstance(modules, types.ModuleType):
+        paths.extend(modules.__path__)
+    elif isinstance(modules, collections.Iterable):
+        for module in modules:
+            if isinstance(module, types.ModuleType):
+                paths.extend(module.__path__)
+            elif isinstance(module, str):
+                module = importlib.import_module(module)
+                paths.extend(module.__path__)
+            else:
+                raise ValueError(module)
+    else:
+        raise ValueError(modules)
+    for path in paths:
+        class_documenter = documentationtools.ClassCrawler(
+            path,
+            )
+        for x in class_documenter():
+            all_classes.add(x)
+    return list(all_classes)
