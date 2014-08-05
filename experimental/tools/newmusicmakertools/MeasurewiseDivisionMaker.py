1550fa3b3feb7c90eb35f03f02abe5e020ded460
Trevor Bača <trevorbaca@gmail.com>
2014-08-05 11:21:38 -0400

    Implemented MeasurewiseDivisionMaker properties.

--- experimental/tools/newmusicmakertools/MeasurewiseDivisionMaker.py
+++ experimental/tools/newmusicmakertools/MeasurewiseDivisionMaker.py
@@ -12,9 +12,53 @@ class MeasurewiseDivisionMaker(AbjadValueObject):
     ### CLASS VARIABLES ###
 
     __slots__ = (
+        '_divisions',
+        '_hypermeasure_counts',
+        '_remainder_direction',
         )
 
     ### INITIALIZER ###
 
-    def __init__(self):
+    def __init__(
+        self,
+        divisions=None,
+        hypermeasure_counts=None,
+        remainder_direction=Right,
+        ):
         pass
+
+    ### SPECIAL METHODS ###
+
+    def __call__(self, measures=()):
+        r'''Calls measurewise division-maker.
+
+        Returns list of nonreduced fractions.
+        '''
+        divisions = []
+        return divisions
+
+    ### PUBLIC PROPERTIES ###
+
+    @property
+    def divisions(self):
+        r'''Gets divisions.
+
+        Returns tuple or cylic tuple of nonreduced fractions.
+        '''
+        return self._divisions
+
+    @property
+    def hypermeasure_counts(self):
+        r'''Gets hypermeasure counts.
+
+        Returns possibly empty tuple or cyclic tuple of positive integers.
+        '''
+        return self._hypermeasure _counts
+
+    @property
+    def remainder_direction(self):
+        r'''Gets direction of any remainder division.
+
+        Returns left, right or none.
+        '''
+        return self._remainder_direction
