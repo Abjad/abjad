34faed68dc825500415097e79ea85419c5c2d1b4
Trevor Bača <trevorbaca@gmail.com>
2014-08-08 13:40:15 -0400

    Incremented Abjad version number from 2.15 to 2.16.

--- abjad/tools/systemtools/AbjadConfiguration.py
+++ abjad/tools/systemtools/AbjadConfiguration.py
@@ -151,7 +151,7 @@ class AbjadConfiguration(Configuration):
             ::
 
                 >>> abjad_configuration.get_abjad_startup_string()
-                'Abjad 2.15 (development)'
+                'Abjad 2.16 (development)'
 
         Returns string.
         '''
@@ -171,7 +171,7 @@ class AbjadConfiguration(Configuration):
             ::
 
                 >>> abjad_configuration.get_abjad_version_string()
-                '2.15'
+                '2.16'
 
         Returns string.
         '''
