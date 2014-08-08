16d8621402de95e92872871bff3c5ec74a85b0e1
Trevor Bača <trevorbaca@gmail.com>
2014-08-08 16:45:59 -0400

    Added Division assert.

--- experimental/tools/musicexpressiontools/StartPositionedPayloadExpression.py
+++ experimental/tools/musicexpressiontools/StartPositionedPayloadExpression.py
@@ -324,6 +324,10 @@ class StartPositionedPayloadExpression(IterablePayloadExpression):
                 return
             callback_cache[key] = elements
         elements = callback_cache[key]
+        from experimental import musicexpressiontools
+        assert all(
+            isinstance(_, musicexpressiontools.Division) for _ in elements
+            ), repr(elements)
         start_offset = elements[0]._flamingo
         expression = new(
             self,
