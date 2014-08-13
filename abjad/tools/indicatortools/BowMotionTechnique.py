5aae5d68bd82991ec1941dab28752a25f8b3b1bd
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-08-13 16:27:55 -0400

    Added BowMotionTechnique to indicatortools.
    This indicator models the concept of bow motions like jeté, circular bowing and
    ordinario bowing, which are independent of bow contact point or string contact
    point.

---
+++ abjad/tools/indicatortools/BowMotionTechnique.py
@@ -0,0 +1,45 @@
+# -*- encoding: utf-8 -*-
+from abjad.tools.abctools.AbjadValueObject import AbjadValueObject
+
+
+class BowMotionTechnique(AbjadValueObject):
+    r'''Bow motion technique.
+
+    ..  container:: example
+
+        >>> bow_motion_technique = indicatortools.BowMotionTechnique('jete')
+        >>> print(format(bow_motion_technique))
+        indicatortools.BowMotionTechnique(
+            technique_name='jete',
+            )
+
+    Valid technique names include 'ordinario', 'jeté' and 'circular'.
+    '''
+
+    ### CLASS VARIABLES ###
+
+    __slots__ = (
+        '_technique_name',
+        )
+
+    _valid_technique_names = (
+        'circular',
+        'jete',
+        'ordinario',
+        )
+
+    ### INITIALIZER ###
+
+    def __init__(self, technique_name='ordinario'):
+        assert technique_name in self._valid_technique_names
+        self._technique_name = technique_name
+
+    ### PUBLIC PROPERTIES ###
+
+    @property
+    def technique_name(self):
+        r'''Gets technique name.
+
+        Returns string.
+        '''
+        return self._technique_name
