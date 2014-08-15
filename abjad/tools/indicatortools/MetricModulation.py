f0b9fc79f98bd96be6a02e618702a5350742c533
Trevor Bača <trevorbaca@gmail.com>
2014-08-15 10:32:00 -0400

    Updated MetricModulation initializer.

--- abjad/tools/indicatortools/MetricModulation.py
+++ abjad/tools/indicatortools/MetricModulation.py
@@ -1,4 +1,5 @@
 # -*- encoding: utf-8 -*-
+from abjad.tools import durationtools
 from abjad.tools.abctools.AbjadObject import AbjadObject
 
 
@@ -23,21 +24,23 @@ class MetricModulation(AbjadObject):
         ::
 
             >>> metric_modulation = indicatortools.MetricModulation(
-            ...     left_rhythm=tempo_1,
-            ...     right_rhythm=tempo_2,
+            ...     left_rhythm=Duration(1, 4),
+            ...     right_rhythm=Duration(3, 8),
             ...     )
 
         ::
 
             >>> print(format(metric_modulation))
             indicatortools.MetricModulation(
-                left_rhythm=indicatortools.Tempo(
-                    duration=durationtools.Duration(1, 4),
-                    units_per_minute=60,
+                left_rhythm=selectiontools.Selection(
+                    (
+                        scoretools.Note("c'4"),
+                        )
                     ),
-                right_rhythm=indicatortools.Tempo(
-                    duration=durationtools.Duration(1, 4),
-                    units_per_minute=90,
+                right_rhythm=selectiontools.Selection(
+                    (
+                        scoretools.Note("c'4."),
+                        )
                     ),
                 )
 
@@ -109,11 +112,10 @@ class MetricModulation(AbjadObject):
         from abjad.tools import scoretools
         # TODO: make default scope work
         #self._default_scope = scoretools.Score
-        if left_rhythm is not None:
-            assert isinstance(left_rhythm, indicatortools.Tempo)
+        left_rhythm = self._initialize_rhythm(left_rhythm)
         self._left_rhythm = left_rhythm
-        if right_rhythm is not None:
-            assert isinstance(right_rhythm, indicatortools.Tempo)
+        right_rhythm = self._initialize_rhythm(right_rhythm)
+        self._right_rhythm = right_rhythm
         self._right_rhythm = right_rhythm
         if left_markup is not None:
             assert isinstance(left_markup, markuptools.Markup)
@@ -121,6 +123,25 @@ class MetricModulation(AbjadObject):
         if right_markup is not None:
             assert isinstance(right_markup, markuptools.Markup)
         self._right_markup = right_markup
+
+    ### PRIVATE METHODS ###
+
+    def _initialize_rhythm(self, rhythm):
+        from abjad.tools import scoretools
+        from abjad.tools import selectiontools
+        if isinstance(rhythm, durationtools.Duration):
+            selection = scoretools.make_notes([0], [rhythm])
+        elif isinstance(rhythm, scoretools.Component):
+            rhythm = copy.copy(rhythm)
+            selection = selectiontools.Selection([rhythm])
+        elif isinstance(rhythm, selectiontools.Selection):
+            selection = copy.copy(selection)
+        else:
+            message = 'rhythm must be duration, component or selection: {!r}.'
+            message = message.format(rhythm)
+            raise TypeError(message)
+        assert isinstance(selection, selectiontools.Selection)
+        return selection
 
     ### PUBLIC PROPERTIES ###
 
