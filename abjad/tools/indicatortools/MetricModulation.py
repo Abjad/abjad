aa5383693719edf3622b791bb36f2f0c10f7e7f0
Trevor Bača <trevorbaca@gmail.com>
2014-08-14 20:25:13 -0400

    Added MetricModulation class to indicatortools.
    The class is currently a stub.
    
    The class will model the relationship between two different tempo indications.
    
    The class will provide two primary things:
    
        * a place to implement (and continually) extend tempo relation operations
        * markup handling to generate typographic quality markup for score
          inclusion

---
+++ abjad/tools/indicatortools/MetricModulation.py
@@ -0,0 +1,12 @@
+# -*- encoding: utf-8 -*-
+from abjad.tools.abctools.AbjadObject import AbjadObject
+
+
+class MetricModulation(AbjadObject):
+    r'''A metric modulation.
+    '''
+
+    ### CLASS ATTRIBUTES ###
+
+    __slots__ = (
+        )
