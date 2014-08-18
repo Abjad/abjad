c829071f9de27342960687aa85a81912292908b6
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-08-18 19:22:20 -0400

    Cleaned up Spanner.

--- abjad/tools/spannertools/Spanner.py
+++ abjad/tools/spannertools/Spanner.py
@@ -506,9 +506,6 @@ class Spanner(AbjadObject):
     def _is_my_only_leaf(self, leaf):
         return self._is_my_first_leaf(leaf) and self._is_my_last_leaf(leaf)
 
-    def _make_storage_format_with_overrides(self):
-        return format(self, 'storage')
-
     def _remove(self, component):
         r'''Not composer-safe.
         '''
