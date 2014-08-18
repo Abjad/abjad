60b14af1f1dc15e21f685a739cbbbbca2cd18e70
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-08-18 19:19:25 -0400

    Adjusted two schemetools classes.

--- abjad/tools/schemetools/SchemeVector.py
+++ abjad/tools/schemetools/SchemeVector.py
@@ -1,4 +1,5 @@
 # -*- encoding: utf-8 -*-
+from abjad.tools import stringtools
 from abjad.tools.schemetools.Scheme import Scheme
 
 
@@ -8,7 +9,7 @@ class SchemeVector(Scheme):
     ::
 
         >>> schemetools.SchemeVector(True, True, False)
-        SchemeVector(True, True, False, quoting="'")
+        SchemeVector(True, True, False)
 
     Scheme vectors and Scheme vector constants differ in only
     their LilyPond input format.
@@ -18,10 +19,23 @@ class SchemeVector(Scheme):
 
     ### CLASS VARIABLES ##
 
-    __slots__ = (
-        )
+    __slots__ = ()
 
     ### INITIALIZER ###
 
     def __init__(self, *args):
         Scheme.__init__(self, *args, quoting="'")
+
+    ### PRIVATE PROPERTIES ###
+
+    @property
+    def _storage_format_specification(self):
+        from abjad.tools import systemtools
+        if stringtools.is_string(self._value):
+            positional_argument_values = (self._value,)
+        else:
+            positional_argument_values = self._value
+        return systemtools.StorageFormatSpecification(
+            self,
+            positional_argument_values=positional_argument_values,
+            )
--- abjad/tools/schemetools/SchemeVectorConstant.py
+++ abjad/tools/schemetools/SchemeVectorConstant.py
@@ -1,4 +1,5 @@
 # -*- encoding: utf-8 -*-
+from abjad.tools import stringtools
 from abjad.tools.schemetools.Scheme import Scheme
 
 
@@ -8,7 +9,7 @@ class SchemeVectorConstant(Scheme):
     ::
 
         >>> schemetools.SchemeVectorConstant(True, True, False)
-        SchemeVectorConstant(True, True, False, quoting="'#")
+        SchemeVectorConstant(True, True, False)
 
     Scheme vectors and Scheme vector constants differ in
     only their LilyPond input format.
@@ -24,4 +25,18 @@ class SchemeVectorConstant(Scheme):
     ### INITIALIZER ###
 
     def __init__(self, *args):
-        Scheme.__init__(self, *args, **{'quoting': "'#"})
+        Scheme.__init__(self, *args, quoting="'#")
+
+    ### PRIVATE PROPERTIES ###
+
+    @property
+    def _storage_format_specification(self):
+        from abjad.tools import systemtools
+        if stringtools.is_string(self._value):
+            positional_argument_values = (self._value,)
+        else:
+            positional_argument_values = self._value
+        return systemtools.StorageFormatSpecification(
+            self,
+            positional_argument_values=positional_argument_values,
+            )
