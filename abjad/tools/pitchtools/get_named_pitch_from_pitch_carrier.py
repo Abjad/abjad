cd7bb3197ecc16456b3a7730cc8f2c68a6872e93
Trevor Bača <trevorbaca@gmail.com>
2014-09-01 12:25:15 -0400

    Extended pitchtools.get_named_pitch_from_pitch_carrier().
    Now works on numbered pitch-class input.

--- abjad/tools/pitchtools/get_named_pitch_from_pitch_carrier.py
+++ abjad/tools/pitchtools/get_named_pitch_from_pitch_carrier.py
@@ -5,44 +5,74 @@ import numbers
 def get_named_pitch_from_pitch_carrier(pitch_carrier):
     '''Gets named pitch from `pitch_carrier`.
 
-    ::
+    ..  container:: example
 
-        >>> pitch = NamedPitch('df', 5)
-        >>> pitch
-        NamedPitch("df''")
-        >>> pitchtools.get_named_pitch_from_pitch_carrier(pitch)
-        NamedPitch("df''")
+        **Example 1.** Gets named pitch from named pitch:
 
-    ::
+        ::
 
-        >>> note = Note(('df', 5), (1, 4))
-        >>> note
-        Note("df''4")
-        >>> pitchtools.get_named_pitch_from_pitch_carrier(note)
-        NamedPitch("df''")
+            >>> pitch = NamedPitch('df', 5)
+            >>> pitch
+            NamedPitch("df''")
+            >>> pitchtools.get_named_pitch_from_pitch_carrier(pitch)
+            NamedPitch("df''")
 
-    ::
+    ..  container:: example
 
-        >>> note = Note(('df', 5), (1, 4))
-        >>> note.note_head
-        NoteHead("df''")
-        >>> pitchtools.get_named_pitch_from_pitch_carrier(note.note_head)
-        NamedPitch("df''")
+        **Example 2.** Gets named pitch from note:
 
-    ::
+        ::
 
-        >>> chord = Chord([('df', 5)], (1, 4))
-        >>> chord
-        Chord("<df''>4")
-        >>> pitchtools.get_named_pitch_from_pitch_carrier(chord)
-        NamedPitch("df''")
+            >>> note = Note(('df', 5), (1, 4))
+            >>> note
+            Note("df''4")
+            >>> pitchtools.get_named_pitch_from_pitch_carrier(note)
+            NamedPitch("df''")
 
-    ::
+    ..  container:: example
 
-        >>> pitchtools.get_named_pitch_from_pitch_carrier(13)
-        NamedPitch("cs''")
+        **Example 3.** Gets named pitch from note head:
 
-    Raise value error when `pitch_carrier` carries no pitch.
+        ::
+
+            >>> note = Note(('df', 5), (1, 4))
+            >>> note.note_head
+            NoteHead("df''")
+            >>> pitchtools.get_named_pitch_from_pitch_carrier(note.note_head)
+            NamedPitch("df''")
+
+    ..  container:: example
+
+        **Example 4.** Gets named pitch from chord:
+
+        ::
+
+            >>> chord = Chord([('df', 5)], (1, 4))
+            >>> chord
+            Chord("<df''>4")
+            >>> pitchtools.get_named_pitch_from_pitch_carrier(chord)
+            NamedPitch("df''")
+
+    ..  container:: example
+
+        **Example 5.** Gets named pitch from integer:
+
+        ::
+
+            >>> pitchtools.get_named_pitch_from_pitch_carrier(13)
+            NamedPitch("cs''")
+
+    ..  container:: example
+
+        **Example 6.** Gets named pitch from numbered pitch-class:
+
+        ::
+
+            >>> pitch_class = pitchtools.NumberedPitchClass(7)
+            >>> pitchtools.get_named_pitch_from_pitch_carrier(pitch_class)
+            NamedPitch("g'")
+
+    Raises value error when `pitch_carrier` carries no pitch.
 
     Raises value error when `pitch_carrier` carries more than one pitch.
 
@@ -86,6 +116,10 @@ def get_named_pitch_from_pitch_carrier(pitch_carrier):
             message = 'multiple pitches found on {!r}.'
             message = message.format(pitch_carrier)
             raise ValueError(message)
+    elif isinstance(pitch_carrier, pitchtools.NumberedPitchClass):
+        named_pitch_class = pitch_carrier.named_pitch_class
+        named_pitch = pitchtools.NamedPitch(named_pitch_class)
+        return named_pitch
     else:
         message = 'pitch carrier {!r} must be pitch, note, note head or chord.'
         message = message.format(pitch_carrier)
