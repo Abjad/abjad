6619928d72ee696e2375844155a47e3c5b48fc4a
Trevor Bača <trevorbaca@gmail.com>
2014-09-03 15:43:44 -0400

    Implemented Glissando.include_tied_leaves property.
    OLD: the Glissando spanner always included leaves in the middle of a logical
    tie. This is almost certainly not the behavior normally desired.
    
    NEW: the Glissando spanner now glisses into the first leaf in a logical tie and
    out of the last leaf of a logical tie (omitting leaves internal to a logical
    tie), by default. The old behavior is available by initializing a glissando
    with include_tied_leaves=True.
    
    See the Glissando API entry for examples.

--- abjad/tools/spannertools/Glissando.py
+++ abjad/tools/spannertools/Glissando.py
@@ -1,6 +1,7 @@
 # -*- encoding: utf-8 -*-
 from abjad.tools import scoretools
 from abjad.tools.spannertools.Spanner import Spanner
+from abjad.tools.topleveltools.inspect_ import inspect_
 
 
 class Glissando(Spanner):
@@ -31,18 +32,22 @@ class Glissando(Spanner):
     ### CLASS VARIABLES ###
 
     __slots__ = (
+        '_include_tied_leaves',
         )
 
     ### INITIALIZER ###
 
     def __init__(
         self,
+        include_tied_leaves=False,
         overrides=None,
         ):
         Spanner.__init__(
             self,
             overrides=overrides,
             )
+        assert isinstance(include_tied_leaves, bool), repr(include_tied_leaves)
+        self._include_tied_leaves = include_tied_leaves
 
     ### PRIVATE METHODS ###
 
@@ -50,6 +55,82 @@ class Glissando(Spanner):
         lilypond_format_bundle = self._get_basic_lilypond_format_bundle(leaf)
         prototype = (scoretools.Chord, scoretools.Note)
         if not self._is_my_last_leaf(leaf) and isinstance(leaf, prototype):
-            string = r'\glissando'
-            lilypond_format_bundle.right.spanner_starts.append(string)
+            if self.include_tied_leaves:
+                string = r'\glissando'
+                lilypond_format_bundle.right.spanner_starts.append(string)
+            else:
+                logical_tie = inspect_(leaf).get_logical_tie()
+                if leaf is logical_tie[-1]:
+                    string = r'\glissando'
+                    lilypond_format_bundle.right.spanner_starts.append(string)
         return lilypond_format_bundle
+
+    ### PUBLIC PROPERTIES ###
+
+    @property
+    def include_tied_leaves(self):
+        r'''Is true when glissando should include tied leaves.
+        Otherwise false.
+
+        ..  container:: example
+
+            **Example 1.** Does not include tied leaves:
+
+            ::
+
+                >>> staff = Staff("c'8 cs'8 d'8 ~ d'8 ~ d'8 ~ d'8 e'4 f'4")
+                >>> glissando = Glissando(
+                ...     include_tied_leaves=False,
+                ...     )
+                >>> attach(glissando, staff[:])
+                >>> show(staff) # doctest: +SKIP 
+
+            ..  doctest::
+
+                >>> f(staff)
+                \new Staff {
+                    c'8 \glissando
+                    cs'8 \glissando
+                    d'8 ~
+                    d'8 ~
+                    d'8 ~
+                    d'8 \glissando
+                    e'4 \glissando
+                    f'4
+                }
+
+            Default behavior.
+
+        ..  container:: example
+
+            **Example 2.** Does include tied leaves:
+
+            ::
+
+                >>> staff = Staff("c'8 cs'8 d'8 ~ d'8 ~ d'8 ~ d'8 e'4 f'4")
+                >>> glissando = Glissando(
+                ...     include_tied_leaves=True,
+                ...     )
+                >>> attach(glissando, staff[:])
+                >>> show(staff) # doctest: +SKIP 
+
+            ..  doctest::
+
+                >>> f(staff)
+                \new Staff {
+                    c'8 \glissando
+                    cs'8 \glissando
+                    d'8 ~ \glissando
+                    d'8 ~ \glissando
+                    d'8 ~ \glissando
+                    d'8 \glissando
+                    e'4 \glissando
+                    f'4
+                }
+
+
+
+
+        Defaults to false.
+        '''
+        return self._include_tied_leaves
