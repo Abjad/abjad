cc3ad8042b798386a6dae963afd9ce546cf8b7a4
Trevor Bača <trevorbaca@gmail.com>
2014-09-03 15:42:55 -0400

    Removed four musicexpressiontools tests.
    The tests concerned ways of assigning a dynamic handler.
    
    Dynamic handler behavior has been changed and these tests are now deprecated.

--- experimental/tools/musicexpressiontools/test/test_SegmentSpecification__select_divisions.py
+++ experimental/tools/musicexpressiontools/test/test_SegmentSpecification__select_divisions.py
@@ -152,22 +152,3 @@ def test_SegmentSpecification__select_divisions_08():
     current_function_name = systemtools.TestManager.get_current_function_name()
     systemtools.TestManager.write_test_output(score, __file__, current_function_name)
     assert format(score) == systemtools.TestManager.read_test_output(__file__, current_function_name)
-
-
-def test_SegmentSpecification__select_divisions_09():
-    r'''Set handler on each division's leaves.
-    '''
-
-    score_template = templatetools.GroupedRhythmicStavesScoreTemplate(staff_count=1)
-    score_specification = musicexpressiontools.ScoreSpecificationInterface(score_template)
-    red_segment = score_specification.append_segment(name='red')
-    red_segment.set_time_signatures(4 * [(2, 8)])
-    red_segment.set_divisions([(3, 16)])
-    red_segment.set_rhythm(library.joined_thirty_seconds)
-    divisions = red_segment.select_divisions('Voice 1').map_to_each()
-    divisions.timespan.select_leaves('Voice 1').set_dynamic_handler(library.hairpins)
-    score = score_specification.interpret()
-
-    current_function_name = systemtools.TestManager.get_current_function_name()
-    systemtools.TestManager.write_test_output(score, __file__, current_function_name)
-    assert format(score) == systemtools.TestManager.read_test_output(__file__, current_function_name)
