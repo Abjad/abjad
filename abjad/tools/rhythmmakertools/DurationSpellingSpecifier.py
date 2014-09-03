c033a45238a49dbe45ae2002b088b0f51ccad064
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2014-09-03 14:14:17 -0400

    Added DurationSpellingSpecifier.permit_meter_rewriting property.

--- abjad/tools/rhythmmakertools/DurationSpellingSpecifier.py
+++ abjad/tools/rhythmmakertools/DurationSpellingSpecifier.py
@@ -4,7 +4,7 @@ from abjad.tools.abctools import AbjadValueObject
 
 
 class DurationSpellingSpecifier(AbjadValueObject):
-    r'''Duration spelling speficifier.
+    r'''Duration spelling specifier.
     '''
 
     ### CLASS VARIABLES ###
@@ -12,6 +12,7 @@ class DurationSpellingSpecifier(AbjadValueObject):
     __slots__ = (
         '_decrease_durations_monotonically',
         '_forbidden_written_duration',
+        '_permit_meter_rewriting',
         '_spell_metrically',
         )
 
@@ -21,6 +22,7 @@ class DurationSpellingSpecifier(AbjadValueObject):
         self,
         decrease_durations_monotonically=True,
         forbidden_written_duration=None,
+        permit_meter_rewriting=None,
         spell_metrically=None,
         ):
         assert isinstance(decrease_durations_monotonically, bool)
@@ -32,44 +34,11 @@ class DurationSpellingSpecifier(AbjadValueObject):
         if spell_metrically is not None and spell_metrically != 'unassignable':
             assert isinstance(spell_metrically, bool)
         self._spell_metrically = spell_metrically
+        if permit_meter_rewriting is not None:
+            permit_meter_rewriting = bool(permit_meter_rewriting)
+        self._permit_meter_rewriting = permit_meter_rewriting
 
     ### SPECIAL METHODS ###
-
-    def __eq__(self, arg):
-        r'''Is true when `arg` is a duration spelling specifier with
-        values of `decrease_durations_monotonically` and `forbidden_written_duration`
-        equal to those of this duration spelling specifier. Otherwise false.
-
-        ..  container:: example
-
-            ::
-
-                >>> specifier_1 = rhythmmakertools.DurationSpellingSpecifier(
-                ...     decrease_durations_monotonically=True,
-                ...     )
-                >>> specifier_2 = rhythmmakertools.DurationSpellingSpecifier(
-                ...     decrease_durations_monotonically=False,
-                ...     )
-
-            ::
-
-                >>> specifier_1 == specifier_1
-                True
-                >>> specifier_1 == specifier_2
-                False
-                >>> specifier_2 == specifier_1
-                False
-                >>> specifier_2 == specifier_2
-                True
-
-        Returns boolean.
-        '''
-        if isinstance(arg, type(self)):
-            if self.decrease_durations_monotonically == arg.decrease_durations_monotonically and \
-                self.forbidden_written_duration == \
-                arg.forbidden_written_duration:
-                return True
-        return False
 
     def __format__(self, format_specification=''):
         r'''Formats duration spelling specifier.
@@ -90,15 +59,6 @@ class DurationSpellingSpecifier(AbjadValueObject):
             self,
             format_specification=format_specification,
             )
-
-    def __hash__(self):
-        r'''Hashes duration spelling specifier.
-
-        Required to be explicitely re-defined on Python 3 if __eq__ changes.
-
-        Returns integer.
-        '''
-        return super(DurationSpellingSpecifier, self).__hash__()
 
     def __repr__(self):
         r'''Gets interpreter representation of duration spelling specifier.
@@ -171,6 +131,24 @@ class DurationSpellingSpecifier(AbjadValueObject):
         Returns duration or none.
         '''
         return self._forbidden_written_duration
+
+    @property
+    def permit_meter_rewriting(self):
+        r'''Is true when meter rewriting is permitted.
+
+        ..  container:: example
+
+            ::
+
+                >>> specifier = rhythmmakertools.DurationSpellingSpecifier()
+                >>> specifier.permit_meter_rewriting is None
+                True
+
+        Defaults to true.
+
+        Returns boolean or none.
+        '''
+        return self._permit_meter_rewriting
 
     @property
     def spell_metrically(self):
