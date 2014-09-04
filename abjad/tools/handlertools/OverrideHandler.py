e9464be19aec6e421b6ce226d9602188f879c733
Trevor Bača <trevorbaca@gmail.com>
2014-09-04 19:53:27 -0400

    Implement OverrideHandler.maximum_written_duration property.

--- abjad/tools/handlertools/OverrideHandler.py
+++ abjad/tools/handlertools/OverrideHandler.py
@@ -1,5 +1,6 @@
 # -*- encoding: utf-8 -*-
 from abjad.tools import datastructuretools
+from abjad.tools import durationtools
 from abjad.tools import indicatortools
 from abjad.tools import mathtools
 from abjad.tools import scoretools
@@ -17,8 +18,10 @@ class OverrideHandler(Handler):
 
     __slots__ = (
         '_attribute_name',
-        '_grob_name',
         '_attribute_value',
+        '_grob_name',
+        '_maximum_settings',
+        '_maximum_written_duration',
         )
 
     ### INITIALIZER ###
@@ -28,6 +31,8 @@ class OverrideHandler(Handler):
         grob_name=None, 
         attribute_name=None,
         attribute_value=None,
+        maximum_written_duration=None,
+        maximum_settings=None,
         ):
         assert isinstance(grob_name, str), repr(grob_name)
         self._grob_name = grob_name
@@ -35,6 +40,13 @@ class OverrideHandler(Handler):
         self._attribute_name = attribute_name
         assert isinstance(attribute_value, str), repr(attribute_value)
         self._attribute_value = attribute_value
+        if maximum_written_duration is not None:
+            maximum_written_duration = durationtools.Duration(
+                maximum_written_duration)
+        self._maximum_written_duration = maximum_written_duration
+        if maximum_settings is not None:
+            assert isinstance(maximum_settings, dict), maximum_settings
+        self._maximum_settings = maximum_settings
 
     ### SPECIAL METHODS ###
 
@@ -51,7 +63,19 @@ class OverrideHandler(Handler):
             self._attribute_name,
             self._attribute_value,
             )
+        if self.maximum_written_duration is not None:
+            maximum_statement = 'override(leaf).{}.{} = {}'
+            maximum_statement = maximum_statement.format(
+                self.maximum_settings['grob_name'],
+                self.maximum_settings['attribute_name'],
+                self.maximum_settings['attribute_value'],
+                )
         for leaf in leaves:
+            if self.maximum_written_duration is not None:
+                if self.maximum_written_duration <= leaf.written_duration:
+                    if maximum_statement is not None:
+                        exec(maximum_statement, globals(), locals())
+                    continue
             exec(statement, globals(), locals())
 
     ### PUBLIC PROPERTIES ###
@@ -79,3 +103,25 @@ class OverrideHandler(Handler):
         Set to string or none.
         '''
         return self._grob_name
+
+    @property
+    def maximum_settings(self):
+        r'''Gets maximum settings for leaves with written duration
+        greater than or equal to maximum written duration of handler.
+
+        ..  todo: Write examples and tests.
+
+        Set to dictionary or none.
+        '''
+        return self._maximum_settings
+
+    @property
+    def maximum_written_duration(self):
+        r'''Gets maximum written duration of override handler.
+
+        Written durations equal to or greater than this will
+        not be handled.
+
+        Set to duration or none.
+        '''
+        return self._maximum_written_duration
