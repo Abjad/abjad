1073be8289e300fe119bcf9cdebcb7bda26ea98f
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2014-10-16 14:10:25 -0700

    `ipythontools.Play()` now only displays a single MP3 <audio> tag.

--- abjad/tools/ipythontools/Play.py
+++ abjad/tools/ipythontools/Play.py
@@ -38,9 +38,15 @@ class Play(object):
         from abjad.tools import systemtools
         from abjad.tools import topleveltools
         if not systemtools.IOManager.find_executable('fluidsynth'):
-            print('fluidsynth is not available.')
+            message = 'WARNING: fluidsynth is not available; '
+            message += 'cannot render MIDI file to MP3.'
+            print(message)
+            return
         if not systemtools.IOManager.find_executable('ffmpeg'):
-            print('ffmpeg is not available.')
+            message = 'WARNING: ffmpeg is not available; '
+            message += 'cannot render MIDI file to MP3.'
+            print(message)
+            return
         assert '__illustrate__' in dir(expr)
         sound_font = self.sound_font
         if not sound_font:
@@ -55,37 +61,66 @@ class Play(object):
         midi_file_path, format_time, render_time = result
         ogg_file_path = os.path.join(temp_directory, 'out.ogg')
         mp3_file_path = os.path.join(temp_directory, 'out.mp3')
-        rendered_successfully = self._display_ogg(
+        encoded_audio = self._get_ogg_as_base64(
             self.midi_bank,
             midi_file_path,
             ogg_file_path,
             sound_font,
             )
-        if rendered_successfully:
-            self._display_mp3(mp3_file_path, ogg_file_path)
+        if encoded_audio is not None:
+            encoded_audio = self._get_mp3_as_base64(
+                mp3_file_path,
+                ogg_file_path,
+                )
+            if encoded_audio is not None:
+                self._display_mp3_audio_tag(encoded_audio)
         shutil.rmtree(temp_directory)
 
     ### PRIVATE METHODS ###
 
-    def _display_mp3(self, mp3_file_path, ogg_file_path):
-        from abjad.tools import systemtools
+    def _display_mp3_audio_tag(self, encoded_audio):
         from IPython.core.display import display_html
+        audio_tag = '<audio controls type="audio/mpeg" '
+        audio_tag += 'src="data:audio/mpeg;base64,{}">'
+        audio_tag = audio_tag.format(encoded_audio)
+        display_html(audio_tag, raw=True)
+
+    def _display_ogg_audio_tag(self, encoded_audio):
+        from IPython.core.display import display_html
+        audio_tag = '<audio controls type="audio/ogg" '
+        audio_tag += 'src="data:audio/ogg;base64,{}">'
+        audio_tag = audio_tag.format(encoded_audio)
+        display_html(audio_tag, raw=True)
+
+    def _get_base64_from_file(self, file_name):
+        '''Read the base64 representation of a file and encode for HTML.
+        '''
+        import base64
+        import sys
+        with open(file_name, 'rb') as file_pointer:
+            data = file_pointer.read()
+            if sys.version_info[0] == 2:
+                return base64.b64encode(data).decode('utf-8')
+            return base64.b64encode(data)
+
+    def _get_mp3_as_base64(
+        self,
+        mp3_file_path,
+        ogg_file_path,
+        ):
+        from abjad.tools import systemtools
         ffmpeg_command = 'ffmpeg -i {} {}'.format(ogg_file_path, mp3_file_path)
-        print(ffmpeg_command)
+        # print(ffmpeg_command)
         result = systemtools.IOManager.spawn_subprocess(ffmpeg_command)
         if result == 0:
             encoded_audio = self._get_base64_from_file(mp3_file_path)
-            audio_tag = '<audio controls type="audio/mpeg" '
-            audio_tag += 'src="data:audio/mpeg;base64,{}">'
-            audio_tag = audio_tag.format(encoded_audio)
-            display_html(audio_tag, raw=True)
-            return True
+            return encoded_audio
         message = 'ffmpeg failed to render OGG as MP3, result: {}'
         message = message.format(result)
         print(message)
-        return False
+        return None
 
-    def _display_ogg(
+    def _get_ogg_as_base64(
         self,
         midi_bank,
         midi_file_path,
@@ -93,7 +128,6 @@ class Play(object):
         sound_font,
         ):
         from abjad.tools import systemtools
-        from IPython.core.display import display_html
         fluidsynth_command = (
             'fluidsynth',
             '-T oga',
@@ -106,30 +140,15 @@ class Play(object):
             midi_file_path,
             )
         fluidsynth_command = ' '.join(fluidsynth_command)
-        print(fluidsynth_command)
+        # print(fluidsynth_command)
         result = systemtools.IOManager.spawn_subprocess(fluidsynth_command)
         if result == 0:
             encoded_audio = self._get_base64_from_file(ogg_file_path)
-            audio_tag = '<audio controls type="audio/ogg" '
-            audio_tag += 'src="data:audio/ogg;base64,{}">'
-            audio_tag = audio_tag.format(encoded_audio)
-            display_html(audio_tag, raw=True)
-            return True
+            return encoded_audio
         message = 'fluidsynth failed to render MIDI as OGG, result: {}'
         message = message.format(result)
-        print(message)
-        return False
-
-    def _get_base64_from_file(self, file_name):
-        '''Read the base64 representation of a file and encode for HTML.
-        '''
-        import base64
-        import sys
-        with open(file_name, 'rb') as file_pointer:
-            data = file_pointer.read()
-            if sys.version_info[0] == 2:
-                return base64.b64encode(data).decode('utf-8')
-            return base64.b64encode(data)
+        # print(message)
+        return None
 
     ### PUBLIC METHODS ###
 
