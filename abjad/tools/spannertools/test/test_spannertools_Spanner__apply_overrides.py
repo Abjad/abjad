020be155920e5766215321d76a904db47896f0c4
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2014-10-18 15:55:36 -0700

    Adjusted one test for Python 3.

--- abjad/tools/spannertools/test/test_spannertools_Spanner__apply_overrides.py
+++ abjad/tools/spannertools/test/test_spannertools_Spanner__apply_overrides.py
@@ -1,23 +1,30 @@
 # -*- encoding: utf-8 -*-
+import sys
 from abjad import *
 
 
 def test_spannertools_Spanner__apply_overrides_01():
 
-    white_flautando_spanner = spannertools.TextSpanner(
-        overrides = {
-            'text_spanner__dash_period': '1.5',
-            'text_spanner__bound_details__right_broken__padding': '0',
-            'text_spanner__bound_details__left_broken__text': "markuptools.Markup((markuptools.MarkupCommand('italic','(fl.)'),markuptools.MarkupCommand('hspace',1)))",
-            'text_spanner__bound_details__left__stencil_align_dir_y': '0',
-            'text_spanner__bound_details__right__text': "markuptools.Markup((markuptools.MarkupCommand('draw-line',schemetools.SchemePair(0, -1)),))",
-            'text_spanner__bound_details__right_broken__text': 'None',
-            'text_spanner__bound_details__left__text': "markuptools.Markup((markuptools.MarkupCommand('italic','\\xe2\\x80\\x9cwhite\\xe2\\x80\\x9d flautando'),markuptools.MarkupCommand('hspace',1)))",
-            'text_spanner__bound_details__right__padding': '1',
-            'text_spanner__bound_details__left__padding': '-1',
-            'text_spanner__dash_fraction': '0.25',
-        }
-    )
+    overrides = {
+        'text_spanner__dash_period': '1.5',
+        'text_spanner__bound_details__right_broken__padding': '0',
+        'text_spanner__bound_details__left_broken__text': "markuptools.Markup((markuptools.MarkupCommand('italic','(fl.)'),markuptools.MarkupCommand('hspace',1)))",
+        'text_spanner__bound_details__left__stencil_align_dir_y': '0',
+        'text_spanner__bound_details__right__text': "markuptools.Markup((markuptools.MarkupCommand('draw-line',schemetools.SchemePair(0, -1)),))",
+        'text_spanner__bound_details__right_broken__text': 'None',
+        'text_spanner__bound_details__right__padding': '1',
+        'text_spanner__bound_details__left__padding': '-1',
+        'text_spanner__dash_fraction': '0.25',
+    }
+
+    if sys.version_info[0] == 2:
+        overrides['text_spanner__bound_details__left__text'] = \
+            "markuptools.Markup((markuptools.MarkupCommand('italic','\\xe2\\x80\\x9cwhite\\xe2\\x80\\x9d flautando'),markuptools.MarkupCommand('hspace',1)))"
+    else:
+        overrides['text_spanner__bound_details__left__text'] = \
+            "markuptools.Markup((markuptools.MarkupCommand('italic','“white” flautando'),markuptools.MarkupCommand('hspace',1)))"
+
+    white_flautando_spanner = spannertools.TextSpanner(overrides=overrides)
 
     staff = Staff("c'4 d'4 e'4 f'4")
     attach(white_flautando_spanner, staff[:])
