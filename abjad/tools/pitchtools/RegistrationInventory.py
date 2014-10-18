215e5afe4178e5622b658b5916c111c4ff4d52f5
Trevor Bača <trevorbaca@gmail.com>
2014-10-18 14:00:23 -0400

    Updated RegistrationInventory docstring.

--- abjad/tools/pitchtools/RegistrationInventory.py
+++ abjad/tools/pitchtools/RegistrationInventory.py
@@ -1,57 +1,61 @@
 # -*- encoding: utf-8 -*-
 from abjad.tools.datastructuretools.TypedList import TypedList
-from abjad.tools.pitchtools.Registration \
-    import Registration
+from abjad.tools.pitchtools.Registration import Registration
 
 
 class RegistrationInventory(TypedList):
-    '''An ordered list of octave transposition mappings.
+    '''Registration inventory.
 
-    ::
+    ..  container:: example
 
-        >>> mapping_1 = pitchtools.Registration(
-        ...     [('[A0, C4)', 15), ('[C4, C8)', 27)])
-        >>> mapping_2 = pitchtools.Registration(
-        ...     [('[A0, C8]', -18)])
-        >>> inventory = pitchtools.RegistrationInventory(
-        ...     [mapping_1, mapping_2])
+        **Example.** Inventory with two registrations:
+        
+        ::
 
-    ::
+            >>> registration_1 = pitchtools.Registration(
+            ...     [('[A0, C4)', 15), ('[C4, C8)', 27)]
+            ...     )
+            >>> registration_2 = pitchtools.Registration(
+            ...     [('[A0, C8]', -18)]
+            ...     )
+            >>> inventory = pitchtools.RegistrationInventory(
+            ...     [registration_1, registration_2]
+            ...     )
 
-        >>> print(format(inventory))
-        pitchtools.RegistrationInventory(
-            [
-                pitchtools.Registration(
-                    [
-                        pitchtools.RegistrationComponent(
-                            source_pitch_range=pitchtools.PitchRange(
-                                range_string='[A0, C4)',
+        ::
+
+            >>> print(format(inventory))
+            pitchtools.RegistrationInventory(
+                [
+                    pitchtools.Registration(
+                        [
+                            pitchtools.RegistrationComponent(
+                                source_pitch_range=pitchtools.PitchRange(
+                                    range_string='[A0, C4)',
+                                    ),
+                                target_octave_start_pitch=pitchtools.NumberedPitch(15),
                                 ),
-                            target_octave_start_pitch=pitchtools.NumberedPitch(15),
-                            ),
-                        pitchtools.RegistrationComponent(
-                            source_pitch_range=pitchtools.PitchRange(
-                                range_string='[C4, C8)',
+                            pitchtools.RegistrationComponent(
+                                source_pitch_range=pitchtools.PitchRange(
+                                    range_string='[C4, C8)',
+                                    ),
+                                target_octave_start_pitch=pitchtools.NumberedPitch(27),
                                 ),
-                            target_octave_start_pitch=pitchtools.NumberedPitch(27),
-                            ),
-                        ]
-                    ),
-                pitchtools.Registration(
-                    [
-                        pitchtools.RegistrationComponent(
-                            source_pitch_range=pitchtools.PitchRange(
-                                range_string='[A0, C8]',
+                            ]
+                        ),
+                    pitchtools.Registration(
+                        [
+                            pitchtools.RegistrationComponent(
+                                source_pitch_range=pitchtools.PitchRange(
+                                    range_string='[A0, C8]',
+                                    ),
+                                target_octave_start_pitch=pitchtools.NumberedPitch(-18),
                                 ),
-                            target_octave_start_pitch=pitchtools.NumberedPitch(-18),
-                            ),
-                        ]
-                    ),
-                ]
-            )
+                            ]
+                        ),
+                    ]
+                )
 
-    Octave transposition mapping inventories implement list interface
-    and are mutable.
     '''
 
     ### CLASS VARIABLES ###
