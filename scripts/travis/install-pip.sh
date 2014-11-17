9d65d90744df19ad4012c88e3140ae2866b61bed
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2014-11-16 22:26:13 -0800

    Removed unused toplevel scripts/ directory.

--- scripts/travis/install-pip.sh
+++
@@ -1,6 +0,0 @@
-#
-# To ensure Abjad docs build
-#
-
-pip install sphinx
-
--- scripts/travis/install-texlive.sh
+++
@@ -1,7 +0,0 @@
-#
-# For Sphinx book building
-#
-
-sudo apt-get update -qq
-sudo apt-get install -qq help2man python-sphinx texlive-latex-base texlive-latex-recommended texlive-latex-extra texlive-fonts-recommended texinfo
-
