9b028d7d87ffa8d22b19f1269102736f991ddeeb
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2014-12-07 13:52:44 -0800

    Removed empty experimental packages.

--- experimental/__init__.py
+++ experimental/__init__.py
@@ -7,6 +7,3 @@ from experimental import library
 from experimental.tools import constrainttools
 from experimental.tools import interpolationtools
 from experimental.tools import makertools
-from experimental.tools import miditools
-from experimental.tools import newindicatortools
-from experimental.tools import newspannertools
