52c7a81ccfc3c2e687642e412b7b070e946d3ecc
Trevor Bača <trevorbaca@gmail.com>
2014-12-08 16:09:12 -0500

    Removed scoretools.make_rhythmic_sketch_staff().
    CHANGE: should be included in the 2.16 change log.

--- abjad/tools/scoretools/test/test_scoretools_Container_is_simultaneous.py
+++ abjad/tools/scoretools/test/test_scoretools_Container_is_simultaneous.py
@@ -11,7 +11,6 @@ def test_scoretools_Container_is_simultaneous_01():
     assert not Container().is_simultaneous
     assert not scoretools.FixedDurationTuplet().is_simultaneous
     assert not Tuplet().is_simultaneous
-    assert not scoretools.make_rhythmic_sketch_staff([]).is_simultaneous
     assert not Measure().is_simultaneous
     assert Score().is_simultaneous
     assert not Container().is_simultaneous
