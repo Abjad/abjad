22a9d90a14703178a449019acc9dcc8d4728b8d7
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2014-12-09 10:08:16 -0800

    Cleaned up scoremanager conf.

--- scoremanager/docs/source/conf.py
+++ scoremanager/docs/source/conf.py
@@ -24,12 +24,6 @@ class CustomLatexFormatter(LatexFormatter):
 
 PygmentsBridge.latex_formatter = CustomLatexFormatter
 
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-if not on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
