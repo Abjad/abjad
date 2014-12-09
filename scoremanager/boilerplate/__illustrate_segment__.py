194195ac3f1d7500cfbb425602af0b72ff3db415
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2014-12-08 17:28:54 -0800

    Wrapped executable boilerplate code in "if __name__ ...".

--- scoremanager/boilerplate/__illustrate_segment__.py
+++ scoremanager/boilerplate/__illustrate_segment__.py
@@ -1,10 +1,11 @@
 # -*- encoding: utf-8 -*-
 import os
-from abjad import *
+from abjad import persist
 from definition import segment_maker
 
 
-lilypond_file = segment_maker()
-current_directory = os.path.dirname(__file__)
-candidate_path = os.path.join(current_directory, 'illustration.candidate.pdf')
-persist(lilypond_file).as_pdf(candidate_path)
+if __name__ == '__main__':
+    lilypond_file = segment_maker()
+    current_directory = os.path.dirname(__file__)
+    candidate_path = os.path.join(current_directory, 'illustration.candidate.pdf')
+    persist(lilypond_file).as_pdf(candidate_path)
