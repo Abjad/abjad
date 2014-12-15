117dcc23b678f984b9ff9e0b0702ca1047c14340
Trevor Bača <trevorbaca@gmail.com>
2014-12-15 12:31:06 -0500

    Added 'include_repeated_pitches' property to Glissando.
    OLD: Glissando always included repeated pitches.
    NEW: Glissando includes repeated pitches when include_repeated_pitches=True.
    Glissando excludes repeated pitches when include_repeated_pitches=False.
    
    Glissando now defaults to include_repeated_pitches=False.
    
    Set include_repeated_pitches=True to recoup the old behavior.
    
    Doesn't need to be included in the change log.

--- abjad/tools/spannertools/Glissando.py
+++ abjad/tools/spannertools/Glissando.py
@@ -32,6 +32,7 @@ class Glissando(Spanner):
     ### CLASS VARIABLES ###
 
     __slots__ = (
+        '_include_repeated_pitches',
         '_include_tied_leaves',
         )
 
@@ -39,6 +40,7 @@ class Glissando(Spanner):
 
     def __init__(
         self,
+        include_repeated_pitches=False,
         include_tied_leaves=False,
         overrides=None,
         ):
@@ -48,25 +50,122 @@ class Glissando(Spanner):
             )
         assert isinstance(include_tied_leaves, bool), repr(include_tied_leaves)
         self._include_tied_leaves = include_tied_leaves
+        assert isinstance(include_repeated_pitches, bool), repr(
+            include_repeated_pitches)
+        self._include_repeated_pitches = include_repeated_pitches
 
     ### PRIVATE METHODS ###
 
     def _get_lilypond_format_bundle(self, leaf):
         lilypond_format_bundle = self._get_basic_lilypond_format_bundle(leaf)
         prototype = (scoretools.Chord, scoretools.Note)
-        if not self._is_my_last_leaf(leaf) and isinstance(leaf, prototype):
-            if self.include_tied_leaves:
-                string = r'\glissando'
-                lilypond_format_bundle.right.spanner_starts.append(string)
-            else:
-                logical_tie = inspect_(leaf).get_logical_tie()
-                if leaf is logical_tie[-1]:
-                    string = r'\glissando'
-                    lilypond_format_bundle.right.spanner_starts.append(string)
+        should_attach_glissando = False
+        if self._is_my_last_leaf(leaf):
+            pass
+        elif not isinstance(leaf, prototype):
+            pass
+        elif self.include_repeated_pitches and self.include_tied_leaves:
+            should_attach_glissando = True
+        elif self.include_repeated_pitches and not self.include_tied_leaves:
+            should_attach_glissando = self._is_last_in_tie_chain(leaf)
+        elif not self.include_repeated_pitches and self.include_tied_leaves:
+            if self._next_leaf_changes_current_pitch(leaf):
+                should_attach_glissando = True
+        elif (
+            not self.include_repeated_pitches and 
+            not self.include_tied_leaves):
+            if self._next_leaf_changes_current_pitch(leaf):
+                if self._is_last_in_tie_chain(leaf):
+                    should_attach_glissando = True
+        if should_attach_glissando:
+            lilypond_format_bundle.right.spanner_starts.append('\glissando')
         return lilypond_format_bundle
+
+    @staticmethod
+    def _is_last_in_tie_chain(leaf):
+        logical_tie = inspect_(leaf).get_logical_tie()
+        return leaf is logical_tie[-1]
+
+    @staticmethod
+    def _next_leaf_changes_current_pitch(leaf):
+        next_leaf = inspect_(leaf).get_leaf(n=1)
+        if (isinstance(leaf, scoretools.Note) and 
+            isinstance(next_leaf, scoretools.Note) and
+            leaf.written_pitch == next_leaf.written_pitch):
+            return False
+        elif (isinstance(leaf, scoretools.Chord) and 
+            isinstance(next_leaf, scoretools.Chord) and
+            leaf.written_pitches == next_leaf.written_pitches):
+            return False
+        return True
 
     ### PUBLIC PROPERTIES ###
 
+    @property
+    def include_repeated_pitches(self):
+        r'''Is true when glissando should include repeated pitches.
+        Otherwise false.
+
+        ..  container:: example
+
+            **Example 1.** Does not include repeated pitches:
+
+            ::
+
+                >>> staff = Staff("a8 a8 b8 ~ b8 c'8 c'8 d'8 ~ d'8")
+                >>> glissando = Glissando(
+                ...     include_repeated_pitches=False,
+                ...     )
+                >>> attach(glissando, staff[:])
+                >>> show(staff) # doctest: +SKIP 
+
+            ..  doctest::
+
+                >>> f(staff)
+                \new Staff {
+                    a8
+                    a8 \glissando
+                    b8 ~
+                    b8 \glissando
+                    c'8
+                    c'8 \glissando
+                    d'8 ~
+                    d'8
+                }
+
+            This is default behavior.
+
+        ..  container:: example
+
+            **Example 2.** Does include repeated pitches:
+
+            ::
+
+                >>> staff = Staff("a8 a8 b8 ~ b8 c'8 c'8 d'8 ~ d'8")
+                >>> glissando = Glissando(
+                ...     include_repeated_pitches=True,
+                ...     )
+                >>> attach(glissando, staff[:])
+                >>> show(staff) # doctest: +SKIP 
+
+            ..  doctest::
+
+                >>> f(staff)
+                \new Staff {
+                    a8 \glissando
+                    a8 \glissando
+                    b8 ~
+                    b8 \glissando
+                    c'8 \glissando
+                    c'8 \glissando
+                    d'8 ~
+                    d'8
+                }
+
+        Defaults to false.
+        '''
+        return self._include_repeated_pitches
+        
     @property
     def include_tied_leaves(self):
         r'''Is true when glissando should include tied leaves.
@@ -74,12 +173,13 @@ class Glissando(Spanner):
 
         ..  container:: example
 
-            **Example 1.** Does not include tied leaves:
+            **Example 1.** Includes neither repeated pitches nor ties:
 
             ::
 
-                >>> staff = Staff("c'8 cs'8 d'8 ~ d'8 ~ d'8 ~ d'8 e'4 f'4")
+                >>> staff = Staff("a8 a8 b8 ~ b8 c'8 c'8 d'8 ~ d'8")
                 >>> glissando = Glissando(
+                ...     include_repeated_pitches=False,
                 ...     include_tied_leaves=False,
                 ...     )
                 >>> attach(glissando, staff[:])
@@ -89,26 +189,27 @@ class Glissando(Spanner):
 
                 >>> f(staff)
                 \new Staff {
+                    a8
+                    a8 \glissando
+                    b8 ~
+                    b8 \glissando
+                    c'8
                     c'8 \glissando
-                    cs'8 \glissando
                     d'8 ~
-                    d'8 ~
-                    d'8 ~
-                    d'8 \glissando
-                    e'4 \glissando
-                    f'4
+                    d'8
                 }
 
-            Default behavior.
+            This is default behavior.
 
         ..  container:: example
 
-            **Example 2.** Does include tied leaves:
+            **Example 2.** Same as above:
 
             ::
 
-                >>> staff = Staff("c'8 cs'8 d'8 ~ d'8 ~ d'8 ~ d'8 e'4 f'4")
+                >>> staff = Staff("a8 a8 b8 ~ b8 c'8 c'8 d'8 ~ d'8")
                 >>> glissando = Glissando(
+                ...     include_repeated_pitches=False,
                 ...     include_tied_leaves=True,
                 ...     )
                 >>> attach(glissando, staff[:])
@@ -118,18 +219,74 @@ class Glissando(Spanner):
 
                 >>> f(staff)
                 \new Staff {
+                    a8
+                    a8 \glissando
+                    b8 ~
+                    b8 \glissando
+                    c'8
                     c'8 \glissando
-                    cs'8 \glissando
-                    d'8 ~ \glissando
-                    d'8 ~ \glissando
-                    d'8 ~ \glissando
-                    d'8 \glissando
-                    e'4 \glissando
-                    f'4
+                    d'8 ~
+                    d'8
                 }
 
+            Including ties is blocked when repeated pitches
+            are not included (because ties all repeat a preceding pitch).
 
+        ..  container:: example
 
+            **Example 3.** Includes repeated pitches but not ties:
+
+            ::
+
+                >>> staff = Staff("a8 a8 b8 ~ b8 c'8 c'8 d'8 ~ d'8")
+                >>> glissando = Glissando(
+                ...     include_repeated_pitches=True,
+                ...     include_tied_leaves=False,
+                ...     )
+                >>> attach(glissando, staff[:])
+                >>> show(staff) # doctest: +SKIP 
+
+            ..  doctest::
+
+                >>> f(staff)
+                \new Staff {
+                    a8 \glissando
+                    a8 \glissando
+                    b8 ~
+                    b8 \glissando
+                    c'8 \glissando
+                    c'8 \glissando
+                    d'8 ~
+                    d'8
+                }
+
+        ..  container:: example
+
+            **Example 4.** Includes both repeated pitches and ties:
+
+            ::
+
+                >>> staff = Staff("a8 a8 b8 ~ b8 c'8 c'8 d'8 ~ d'8")
+                >>> glissando = Glissando(
+                ...     include_repeated_pitches=True,
+                ...     include_tied_leaves=True,
+                ...     )
+                >>> attach(glissando, staff[:])
+                >>> show(staff) # doctest: +SKIP 
+
+            ..  doctest::
+
+                >>> f(staff)
+                \new Staff {
+                    a8 \glissando
+                    a8 \glissando
+                    b8 ~ \glissando
+                    b8 \glissando
+                    c'8 \glissando
+                    c'8 \glissando
+                    d'8 ~ \glissando
+                    d'8
+                }
 
         Defaults to false.
         '''
