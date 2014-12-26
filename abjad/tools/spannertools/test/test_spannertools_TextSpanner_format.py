dc9ee906af2467532960c25b306832002d2b3eaa
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2014-12-25 23:30:34 -0500

    Added fancy TextSpanner test.

---
+++ abjad/tools/spannertools/test/test_spannertools_TextSpanner_format.py
@@ -0,0 +1,44 @@
+# -*- encoding: utf-8 -*-
+from abjad import *
+
+
+def test_spannertools_TextSpanner_format_01():
+    staff = scoretools.Staff("c'4 d'4 e'4 f'4")
+    markup_one = markuptools.Markup('one')
+    markup_two = markuptools.Markup('two')
+    markup_three = markuptools.Markup('three')
+    line_segment = indicatortools.LineSegment()
+    arrow = indicatortools.Arrow()
+    text_spanner = spannertools.TextSpanner()
+    attach(markup_one, staff[0], is_annotation=True)
+    attach(line_segment, staff[0])
+    attach(markup_two, staff[1], is_annotation=True)
+    attach(arrow, staff[1])
+    attach(markup_three, staff[-1], is_annotation=True)
+    attach(text_spanner, staff[:])
+    assert format(staff) == systemtools.TestManager.clean_string(
+        r'''
+        \new Staff {
+            \once \override TextSpanner.bound-details.left.text = \markup { one }
+            c'4 \startTextSpan
+            \once \override TextSpanner.arrow-width = 0.25
+            \once \override TextSpanner.bound-details.left-broken.text = ##f
+            \once \override TextSpanner.bound-details.left.stencil-align-dir-y = #center
+            \once \override TextSpanner.bound-details.left.text = \markup {
+                \concat
+                    {
+                        two
+                        \hspace
+                            #0.25
+                    }
+                }
+            \once \override TextSpanner.bound-details.right-broken.padding = 0
+            \once \override TextSpanner.bound-details.right.arrow = ##t
+            \once \override TextSpanner.bound-details.right.padding = 1.5
+            \once \override TextSpanner.bound-details.right.stencil-align-dir-y = #center
+            \once \override TextSpanner.dash-fraction = 1
+            d'4 \stopTextSpan \startTextSpan
+            e'4
+            f'4 \stopTextSpan ^ \markup { three }
+        }
+        ''')
