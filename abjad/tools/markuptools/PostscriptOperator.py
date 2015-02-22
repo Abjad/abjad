b0844e7d6712b40cbe3a8c3b10428d378c2e4c1d
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-02-21 22:36:15 -0800

    Added PostscriptOperator to markuptools.

---
+++ abjad/tools/markuptools/PostscriptOperator.py
@@ -0,0 +1,142 @@
+# -*- encoding: utf-8 -*-
+import collections
+from abjad.tools.abctools import AbjadValueObject
+
+
+class PostscriptOperator(AbjadValueObject):
+    r'''A Postscript operator.
+
+    ..  container:: example
+
+        ::
+
+            >>> operator = markuptools.PostscriptOperator('rmoveto', 1, 1.5)
+            >>> print(format(operator))
+            markuptools.PostscriptOperator('rmoveto', 1, 1.5)
+
+    '''
+
+    ### CLASS VARIABLES ###
+
+    __slots__ = (
+        '_name',
+        '_arguments',
+        )
+
+    _operator_names = (
+        'add',
+        'arc',
+        'begin',
+        'bind',
+        'clip',
+        'charpath',
+        'closepath',
+        'curveto',
+        'def',
+        'div',
+        'dup',
+        'end',
+        'exch',
+        'fill',
+        'for',
+        'findfont',
+        'grestore',
+        'gsave',
+        'if',
+        'ifelse',
+        'index',
+        'lineto',
+        'moveto',
+        'mul',
+        'newpath',
+        'pop',
+        'restore',
+        'rlineto',
+        'rmoveto',
+        'rotate',
+        'save',
+        'scale',
+        'scalefont',
+        'setfont',
+        'setgray',
+        'setlinewidth',
+        'show',
+        'showpage',
+        'stroke',
+        'sub',
+        'translate',
+        )
+
+    ### INITIALIZER ###
+
+    def __init__(self, name='stroke', *arguments):
+        name = str(name)
+        assert name in self._operator_names
+        self._name = name
+        if arguments:
+            self._arguments = tuple(arguments)
+        else:
+            self._arguments = None
+
+    ### SPECIAL METHODS ###
+
+    def __str__(self):
+        r'''Gets string representation of Postscript operator.
+
+        ::
+
+            >>> operator = markuptools.PostscriptOperator('rmoveto', 1, 1.5)
+            >>> str(operator)
+            '1 1.5 rmoveto'
+
+        Returns string.
+        '''
+        def recurse(arguments):
+            parts = []
+            for argument in self.arguments:
+                if isinstance(argument, (int, float, str)):
+                    parts.append(str(argument))
+                elif isinstance(collections.Sequence):
+                    parts.append('[')
+                    parts.extend(recurse(argument))
+                    parts.append(']')
+                else:
+                    raise ValueError(argument)
+            return parts
+        parts = []
+        if self.arguments:
+            parts.extend(recurse(self.arguments))
+        parts.append(self.name)
+        return ' '.join(parts)
+
+    ### PRIVATE PROPERTIES ###
+
+    @property
+    def _storage_format_specification(self):
+        from abjad.tools import systemtools
+        arguments = self.arguments or ()
+        positional_argument_values = (self.name,) + arguments
+        return systemtools.StorageFormatSpecification(
+            self,
+            is_indented=False,
+            keyword_argument_names=(),
+            positional_argument_values=positional_argument_values,
+            )
+
+    ### PUBLIC PROPERTIES ###
+
+    @property
+    def name(self):
+        r'''Gets Postscript operator name.
+
+        Returns string.
+        '''
+        return self._name
+
+    @property
+    def arguments(self):
+        r'''Gets Postscript operator arguments.
+
+        Returns tuple or none.
+        '''
+        return self._arguments
