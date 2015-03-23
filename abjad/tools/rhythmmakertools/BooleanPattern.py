bd6bc1e42fb53d6efe1e82041a80517ee9ddfc8c
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-03-23 16:19:38 -0700

    Added start and stop properties to BooleanPattern.
    Set start and/or stop to an integer to specify a range to apply the mask
    within. Setting only the start or stop to an integer creates a half-open
    interval to apply the mask within, allowing periodic masks to continue. Mask
    indices start relative to the interval's start.
    
        >>> mask = rhythmmakertools.BooleanPattern(
        ...     indices=[1, 2],
        ...     period=3,
        ...     start=1,
        ...     stop=-1,
        ...     )
        >>> total_length = 10
        >>> for i in range(11):
        ...     matches = mask._matches_index(i, total_length)
        ...     print(i, matches)
        ...
        (0, False)
        (1, False)
        (2, True)
        (3, True)
        (4, False)
        (5, True)
        (6, True)
        (7, False)
        (8, True)
        (9, False)
        (10, False)
    
    Also:
    
        >>> mask = rhythmmakertools.BooleanPattern(
        ...     indices=[0],
        ...     period=2,
        ...     start=5,
        ...     )
        >>> total_length = 7
        >>> for i in range(11):
        ...     matches = mask._matches_index(i, total_length)
        ...     print(i, matches)
        ...
        (0, False)
        (1, False)
        (2, False)
        (3, False)
        (4, False)
        (5, True)
        (6, False)
        (7, True)
        (8, False)
        (9, True)
        (10, False)

--- abjad/tools/rhythmmakertools/BooleanPattern.py
+++ abjad/tools/rhythmmakertools/BooleanPattern.py
@@ -30,6 +30,8 @@ class BooleanPattern(AbjadValueObject):
     __slots__ = (
         '_indices',
         '_period',
+        '_start',
+        '_stop',
         )
 
     ### INITIALIZER ###
@@ -38,6 +40,8 @@ class BooleanPattern(AbjadValueObject):
         self,
         indices=None,
         period=None,
+        start=None,
+        stop=None,
         ):
         if indices is not None:
             assert all(isinstance(_, int) for _ in indices), repr(indices)
@@ -46,6 +50,12 @@ class BooleanPattern(AbjadValueObject):
         if period is not None:
             assert mathtools.is_positive_integer(period), repr(period)
         self._period = period
+        if start is not None:
+            start = int(start)
+        self._start = start
+        if stop is not None:
+            stop = int(stop)
+        self._stop = stop
 
     ### PRIVATE PROPERTIES ###
 
@@ -74,6 +84,15 @@ class BooleanPattern(AbjadValueObject):
             nonnegative_index = index
         else:
             nonnegative_index = total_length - abs(index)
+        if self.start is not None or self.stop is not None:
+            start, stop, _ = slice(self.start, self.stop).indices(total_length)
+            if stop <= start:
+                return False
+            if self.start is not None and nonnegative_index < start:
+                return False
+            if self.stop is not None and stop <= nonnegative_index:
+                return False
+            nonnegative_index -= start
         if self.period is None:
             for index in self.indices:
                 if 0 <= index:
@@ -170,3 +189,51 @@ class BooleanPattern(AbjadValueObject):
         Set to positive integer or none.
         '''
         return self._period
+
+    @property
+    def start(self):
+        r'''Gets start index of ouput mask.
+
+        ..  container:: example
+
+            ::
+
+                >>> mask = rhythmmakertools.BooleanPattern(
+                ...     indices=[0, 1, 7],
+                ...     period=16,
+                ...     start=1,
+                ...     stop=-1,
+                ...     )
+
+            ::
+
+                >>> mask.start
+                1
+
+        Set to integer or none.
+        '''
+        return self._start
+
+    @property
+    def stop(self):
+        r'''Gets stop index of ouput mask.
+
+        ..  container:: example
+
+            ::
+
+                >>> mask = rhythmmakertools.BooleanPattern(
+                ...     indices=[0, 1, 7],
+                ...     period=16,
+                ...     start=1,
+                ...     stop=-1,
+                ...     )
+
+            ::
+
+                >>> mask.stop
+                -1
+
+        Set to integer or none.
+        '''
+        return self._stop
