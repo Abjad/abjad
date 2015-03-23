d4cf0b88d9a15a6a1fa96c0801f17a3e59c70cc5
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-03-23 14:15:01 -0700

    Added SustainMask and SilenceMask to rhythmmakertools.

---
+++ abjad/tools/rhythmmakertools/SustainMask.py
@@ -0,0 +1,29 @@
+# -*- encoding: utf-8 -*-
+from abjad.tools.rhythmmakertools.BooleanPattern import BooleanPattern
+
+
+class SustainMask(BooleanPattern):
+    r'''A sustain mask.
+
+    ..  container:: example
+
+        ::
+
+            >>> mask = rhythmmakertools.SustainMask(
+            ...     indices=[0, 1, 7],
+            ...     period=16,
+            ...     )
+
+        ::
+
+            >>> print(format(mask))
+            rhythmmakertools.SustainMask(
+                indices=(0, 1, 7),
+                period=16,
+                )
+
+    '''
+
+    ### CLASS VARIABLES ###
+
+    __slots__ = ()
