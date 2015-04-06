42e0f8ef236aebc3f763e95fc2ef6be4c9e73def
Trevor Bača <trevorbaca@gmail.com>
2015-04-06 10:37:27 -0400

    Updated BooleanPatternInventory API entry.

--- abjad/tools/rhythmmakertools/BooleanPatternInventory.py
+++ abjad/tools/rhythmmakertools/BooleanPatternInventory.py
@@ -5,81 +5,103 @@ from abjad.tools.datastructuretools.TypedTuple import TypedTuple
 class BooleanPatternInventory(TypedTuple):
     r'''An ordered list of boolean patterns.
 
-    ::
+    ..  container:: example
 
-        >>> inventory = rhythmmakertools.BooleanPatternInventory([
-        ...     rhythmmakertools.BooleanPattern([0, 1, 7], 10),
-        ...     rhythmmakertools.BooleanPattern([-2, -1]),
-        ...     rhythmmakertools.BooleanPattern([2], 3, 1, -1),
-        ...     ])
+        **Example 1.** Inventory of three boolean patterns:
 
-    ::
+        ::
 
-        >>> print(format(inventory))
-        rhythmmakertools.BooleanPatternInventory(
-            (
-                rhythmmakertools.BooleanPattern(
-                    indices=(0, 1, 7),
-                    period=10,
-                    ),
-                rhythmmakertools.BooleanPattern(
-                    indices=(-2, -1),
-                    ),
-                rhythmmakertools.BooleanPattern(
-                    indices=(2,),
-                    period=3,
-                    start=1,
-                    stop=-1,
-                    ),
+            >>> inventory = rhythmmakertools.BooleanPatternInventory([
+            ...     rhythmmakertools.BooleanPattern([0, 1, 7], period=10),
+            ...     rhythmmakertools.BooleanPattern([-2, -1]),
+            ...     rhythmmakertools.BooleanPattern(
+            ...         indices=[2],
+            ...         period=3,
+            ...         start=1,
+            ...         stop=-1,
+            ...         ),
+            ...     ])
+
+        ::
+
+            >>> print(format(inventory))
+            rhythmmakertools.BooleanPatternInventory(
+                (
+                    rhythmmakertools.BooleanPattern(
+                        indices=(0, 1, 7),
+                        period=10,
+                        ),
+                    rhythmmakertools.BooleanPattern(
+                        indices=(-2, -1),
+                        ),
+                    rhythmmakertools.BooleanPattern(
+                        indices=(2,),
+                        period=3,
+                        start=1,
+                        stop=-1,
+                        ),
+                    )
                 )
-            )
 
     '''
 
     ### CLASS VARIABLES ###
 
-    __slots__ = ()
+    __slots__ = (
+        )
 
     ### PUBLIC METHODS ###
 
     def get_matching_pattern(self, index, total_length, seed=None):
         r'''Gets pattern matching `index`.
 
-        ::
+        ..  container:: example
 
-            >>> for i in range(11):
-            ...     match = inventory.get_matching_pattern(i, 10)
-            ...     print(i, match)
-            ...
-            0 BooleanPattern(indices=(0, 1, 7), period=10)
-            1 BooleanPattern(indices=(0, 1, 7), period=10)
-            2 None
-            3 BooleanPattern(indices=(2,), period=3, start=1, stop=-1)
-            4 None
-            5 None
-            6 BooleanPattern(indices=(2,), period=3, start=1, stop=-1)
-            7 BooleanPattern(indices=(0, 1, 7), period=10)
-            8 BooleanPattern(indices=(-2, -1))
-            9 BooleanPattern(indices=(-2, -1))
-            10 BooleanPattern(indices=(0, 1, 7), period=10)
+            **Example 1.** Gets pattern that matches each of the 10 indices of
+            the inventory take over a period of 10:
 
-        ::
+            ::
 
-            >>> for i in range(11):
-            ...     match = inventory.get_matching_pattern(i, 10, seed=1)
-            ...     print(i, match)
-            ...
-            0 BooleanPattern(indices=(0, 1, 7), period=10)
-            1 None
-            2 BooleanPattern(indices=(2,), period=3, start=1, stop=-1)
-            3 None
-            4 None
-            5 BooleanPattern(indices=(2,), period=3, start=1, stop=-1)
-            6 BooleanPattern(indices=(0, 1, 7), period=10)
-            7 None
-            8 BooleanPattern(indices=(2,), period=3, start=1, stop=-1)
-            9 BooleanPattern(indices=(-2, -1))
-            10 BooleanPattern(indices=(0, 1, 7), period=10)
+                >>> for i in range(11):
+                ...     match = inventory.get_matching_pattern(i, 10)
+                ...     print(i, match)
+                ...
+                0 BooleanPattern(indices=(0, 1, 7), period=10)
+                1 BooleanPattern(indices=(0, 1, 7), period=10)
+                2 None
+                3 BooleanPattern(indices=(2,), period=3, start=1, stop=-1)
+                4 None
+                5 None
+                6 BooleanPattern(indices=(2,), period=3, start=1, stop=-1)
+                7 BooleanPattern(indices=(0, 1, 7), period=10)
+                8 BooleanPattern(indices=(-2, -1))
+                9 BooleanPattern(indices=(-2, -1))
+                10 BooleanPattern(indices=(0, 1, 7), period=10)
+
+            The pattern at index ``10`` is the same as the pattern at index
+            ``0``.
+
+        ..  container:: example
+
+            **Examle 2.** What is going on here?
+
+            ::
+
+                >>> for i in range(11):
+                ...     match = inventory.get_matching_pattern(i, 10, seed=1)
+                ...     print(i, match)
+                ...
+                0 BooleanPattern(indices=(0, 1, 7), period=10)
+                1 None
+                2 BooleanPattern(indices=(2,), period=3, start=1, stop=-1)
+                3 None
+                4 None
+                5 BooleanPattern(indices=(2,), period=3, start=1, stop=-1)
+                6 BooleanPattern(indices=(0, 1, 7), period=10)
+                7 None
+                8 BooleanPattern(indices=(2,), period=3, start=1, stop=-1)
+                9 BooleanPattern(indices=(-2, -1))
+                10 BooleanPattern(indices=(0, 1, 7), period=10)
 
         Returns pattern or none.
         '''
