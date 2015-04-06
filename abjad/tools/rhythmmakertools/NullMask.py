88e452ee81287ea68ca00eb14365e1ea241d716d
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-04-06 15:41:43 -0700

    Added NullMask to rhythmmakertools.

---
+++ abjad/tools/rhythmmakertools/NullMask.py
@@ -0,0 +1,29 @@
+# -*- encoding: utf-8 -*-
+from abjad.tools.rhythmmakertools.BooleanPattern import BooleanPattern
+
+
+class NullMask(BooleanPattern):
+    r'''A sustain mask.
+
+    ..  container:: example
+
+        ::
+
+            >>> mask = rhythmmakertools.NullMask(
+            ...     indices=[0, 1, 7],
+            ...     period=16,
+            ...     )
+
+        ::
+
+            >>> print(format(mask))
+            rhythmmakertools.NullMask(
+                indices=(0, 1, 7),
+                period=16,
+                )
+
+    '''
+
+    ### CLASS VARIABLES ###
+
+    __slots__ = ()
