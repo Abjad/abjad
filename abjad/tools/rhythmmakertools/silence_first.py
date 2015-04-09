82de2a315bf180be312100b5565a8227326b61ee
Trevor Bača <trevorbaca@gmail.com>
2015-04-09 15:18:25 -0400

    Extended rhythmmakertools.silence_[first|last]().
    OLD: value passed to function had to be positive integer.
    
    NEW: value passed to function may be zero, indicating that no divisions are
    silenced.

--- abjad/tools/rhythmmakertools/silence_first.py
+++ abjad/tools/rhythmmakertools/silence_first.py
@@ -106,11 +106,62 @@ def silence_first(n=1, use_multimeasure_rests=None):
                 }
             }
 
+    ..  container:: example
+
+        **Example 3.** Silences no first divisions:
+
+        ::
+
+            >>> mask = rhythmmakertools.silence_first(n=0)
+
+        ::
+
+            >>> print(format(mask))
+            rhythmmakertools.SilenceMask(
+                indices=(),
+                )
+
+        ::
+
+            >>> maker = rhythmmakertools.NoteRhythmMaker(
+            ...     output_masks=[mask],
+            ...     )
+            >>> divisions = [(7, 16), (3, 8), (7, 16), (3, 8)]
+            >>> music = maker(divisions)
+            >>> lilypond_file = rhythmmakertools.make_lilypond_file(
+            ...     music,
+            ...     divisions,
+            ...     )
+            >>> show(lilypond_file) # doctest: +SKIP
+
+        ..  doctest::
+
+            >>> staff = maker._get_rhythmic_staff(lilypond_file)
+            >>> f(staff)
+            \new RhythmicStaff {
+                {
+                    \time 7/16
+                    c'4..
+                }
+                {
+                    \time 3/8
+                    c'4.
+                }
+                {
+                    \time 7/16
+                    c'4..
+                }
+                {
+                    \time 3/8
+                    c'4.
+                }
+            }
+
     Returns silence mask.
     '''
     from abjad.tools import rhythmmakertools
 
-    assert 0 < n, repr(n)
+    assert 0 <= n, repr(n)
     indices = list(range(n))
 
     return rhythmmakertools.SilenceMask(
--- abjad/tools/rhythmmakertools/silence_last.py
+++ abjad/tools/rhythmmakertools/silence_last.py
@@ -106,11 +106,62 @@ def silence_last(n=1, use_multimeasure_rests=None):
                 }
             }
 
+    ..  container:: example
+
+        **Example 3.** Silences no last divisions:
+
+        ::
+
+            >>> mask = rhythmmakertools.silence_last(n=0)
+
+        ::
+
+            >>> print(format(mask))
+            rhythmmakertools.SilenceMask(
+                indices=(),
+                )
+
+        ::
+
+            >>> maker = rhythmmakertools.NoteRhythmMaker(
+            ...     output_masks=[mask],
+            ...     )
+            >>> divisions = [(7, 16), (3, 8), (7, 16), (3, 8)]
+            >>> music = maker(divisions)
+            >>> lilypond_file = rhythmmakertools.make_lilypond_file(
+            ...     music,
+            ...     divisions,
+            ...     )
+            >>> show(lilypond_file) # doctest: +SKIP
+
+        ..  doctest::
+
+            >>> staff = maker._get_rhythmic_staff(lilypond_file)
+            >>> f(staff)
+            \new RhythmicStaff {
+                {
+                    \time 7/16
+                    c'4..
+                }
+                {
+                    \time 3/8
+                    c'4.
+                }
+                {
+                    \time 7/16
+                    c'4..
+                }
+                {
+                    \time 3/8
+                    c'4.
+                }
+            }
+
     Returns silence mask.
     '''
     from abjad.tools import rhythmmakertools
 
-    assert 0 < n, repr(n)
+    assert 0 <= n, repr(n)
     indices = list(reversed(range(-1, -n-1, -1)))
 
     return rhythmmakertools.SilenceMask(
