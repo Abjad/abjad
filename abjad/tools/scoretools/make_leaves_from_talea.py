bdb579051506d588e0ef635d75fdfb1a0c6d6881
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-04-08 17:03:57 -0700

    Added "spell_metrically" keyword to scoretools.make_leaves_from_talea().
    Set "spell_metrically" to true to spell unassignable talea values metrically:
    
        >>> leaves = scoretools.make_leaves_from_talea(
        ...     [3, -3, 5, -5], 16,
        ...     spell_metrically='unassignable',
        ...     )
        >>> staff = Staff(leaves)
        >>> staff.context_name = 'RhythmicStaff'
        >>> time_signature = TimeSignature((4, 4))
        >>> attach(time_signature, staff)
        >>> print(format(staff))
        \new RhythmicStaff {
            \time 4/4
            c'8.
            r8.
            c'8. ~
            c'8
            r8.
            r8
        }

--- abjad/tools/scoretools/make_leaves_from_talea.py
+++ abjad/tools/scoretools/make_leaves_from_talea.py
@@ -1,6 +1,8 @@
 # -*- encoding: utf-8 -*-
 from abjad.tools import durationtools
+from abjad.tools import mathtools
 from abjad.tools import selectiontools
+from abjad.tools.topleveltools import attach
 
 
 def make_leaves_from_talea(
@@ -8,6 +10,7 @@ def make_leaves_from_talea(
     talea_denominator,
     decrease_durations_monotonically=True,
     forbidden_written_duration=None,
+    spell_metrically=None,
     ):
     r'''Make leaves from `talea`.
 
@@ -51,7 +54,8 @@ def make_leaves_from_talea(
 
             >>> leaves = scoretools.make_leaves_from_talea(
             ...     [3, -3, 5, -5], 16,
-            ...     decrease_durations_monotonically=False)
+            ...     decrease_durations_monotonically=False,
+            ...     )
             >>> staff = Staff(leaves)
             >>> staff.context_name = 'RhythmicStaff'
             >>> time_signature = TimeSignature((4, 4))
@@ -80,7 +84,8 @@ def make_leaves_from_talea(
 
             >>> leaves = scoretools.make_leaves_from_talea(
             ...     [3, -3, 5, -5], 16,
-            ...     forbidden_written_duration=Duration(1, 4))
+            ...     forbidden_written_duration=Duration(1, 4),
+            ...     )
             >>> staff = Staff(leaves)
             >>> staff.context_name = 'RhythmicStaff'
             >>> time_signature = TimeSignature((4, 4))
@@ -102,9 +107,41 @@ def make_leaves_from_talea(
                 r16
             }
 
+    ..  container:: example
+
+        **Example 4.** Spell unassignable durations metrically:
+
+        ::
+
+            >>> leaves = scoretools.make_leaves_from_talea(
+            ...     [3, -3, 5, -5], 16,
+            ...     spell_metrically='unassignable',
+            ...     )
+            >>> staff = Staff(leaves)
+            >>> staff.context_name = 'RhythmicStaff'
+            >>> time_signature = TimeSignature((4, 4))
+            >>> attach(time_signature, staff)
+            >>> show(staff) # doctest: +SKIP
+
+        ..  doctest::
+
+            >>> print(format(staff))
+            \new RhythmicStaff {
+                \time 4/4
+                c'8.
+                r8.
+                c'8. ~
+                c'8
+                r8.
+                r8
+            }
+
+
     Returns selection.
     '''
+    from abjad.tools import metertools
     from abjad.tools import scoretools
+    from abjad.tools import spannertools
 
     assert all(x != 0 for x in talea), repr(talea)
 
@@ -114,13 +151,30 @@ def make_leaves_from_talea(
             pitches = [0]
         else:
             pitches = [None]
+        division = durationtools.Duration(
+            abs(note_value),
+            talea_denominator,
+            )
+        if (spell_metrically is True or
+            (spell_metrically == 'unassignable' and
+            not mathtools.is_assignable_integer(division.numerator))):
+            meter = metertools.Meter(division)
+            rhythm_tree_container = meter.root_node
+            durations = [_.duration for _ in rhythm_tree_container]
+        else:
+            durations = [division]
         leaves = scoretools.make_leaves(
             pitches,
-            [durationtools.Duration(abs(note_value), talea_denominator)],
+            durations,
             decrease_durations_monotonically=decrease_durations_monotonically,
             forbidden_written_duration=forbidden_written_duration,
             )
+        if (
+            1 < len(leaves) and
+            not leaves[0]._has_spanner(spannertools.Tie) and
+            not isinstance(leaves[0], scoretools.Rest)
+            ):
+            attach(spannertools.Tie(), leaves[:])
         result.extend(leaves)
-
     result = selectiontools.Selection(result)
     return result
