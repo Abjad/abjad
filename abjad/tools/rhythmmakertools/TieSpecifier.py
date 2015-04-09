656a1facef14be7f963762a9dffc819299a6754e
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-04-08 18:23:45 -0700

    Added TieSpecifier.strip_ties property.
    Set TieSpecifier.strip_ties to true to remove all ties from a rhythm-maker's
    output.

--- abjad/tools/rhythmmakertools/TieSpecifier.py
+++ abjad/tools/rhythmmakertools/TieSpecifier.py
@@ -18,6 +18,7 @@ class TieSpecifier(AbjadValueObject):
     ### CLASS VARIABLES ###
 
     __slots__ = (
+        '_strip_ties',
         '_tie_across_divisions',
         '_use_messiaen_style_ties',
         )
@@ -26,10 +27,14 @@ class TieSpecifier(AbjadValueObject):
 
     def __init__(
         self,
+        strip_ties=None,
         tie_across_divisions=None,
         use_messiaen_style_ties=None,
         ):
         from abjad.tools import rhythmmakertools
+        if strip_ties is not None:
+            strip_ties = bool(strip_ties)
+        self._strip_ties = strip_ties
         prototype = (
             type(None),
             bool,
@@ -39,32 +44,16 @@ class TieSpecifier(AbjadValueObject):
             )
         assert isinstance(tie_across_divisions, prototype)
         self._tie_across_divisions = tie_across_divisions
-        prototype = (type(None), bool)
-        assert isinstance(use_messiaen_style_ties, prototype)
+        if use_messiaen_style_ties is not None:
+            use_messiaen_style_ties = bool(use_messiaen_style_ties)
         self._use_messiaen_style_ties = use_messiaen_style_ties
 
     ### SPECIAL METHODS ###
 
-    def __eq__(self, arg):
-        r'''Is true when `arg` is a tie specifier with value of
-        `tie_across_divisions` equal to that of this tie
-        specifier. Otherwise false.
-
-        Returns boolean.
-        '''
-        if isinstance(arg, type(self)):
-            if self.tie_across_divisions == arg.tie_across_divisions:
-                return True
-        return False
-
-    def __hash__(self):
-        r'''Hashes tie specifier.
-
-        Required to be explicitely re-defined on Python 3 if __eq__ changes.
-
-        Returns integer.
-        '''
-        return super(TieSpecifier, self).__hash__()
+    def __call__(self, divisions):
+        if not self.strip_ties:
+            self._make_ties_across_divisions(divisions)
+        self._strip_ties_from_divisions(divisions)
 
     ### PRIVATE PROPERTIES ###
 
@@ -125,7 +114,23 @@ class TieSpecifier(AbjadValueObject):
             attach(tie_spanner, combined_logical_tie)
             tie_spanner._constrain_contiguity()
 
+    def _strip_ties_from_divisions(self, divisions):
+        if not self.strip_ties:
+            return
+        for division in divisions:
+            for leaf in iterate(division).by_class(scoretools.Leaf):
+                detach(spannertools.Tie, leaf)
+
     ### PUBLIC PROPERTIES ###
+
+    @property
+    def strip_ties(self):
+        r'''Is true when rhythm-maker should strip all ties from all leaves in
+        each division.
+
+        Set to true, false or none.
+        '''
+        return self._strip_ties
 
     @property
     def tie_across_divisions(self):
