1e08dc4ffcf0e5bad3b05f96c7780a308fb75572
Trevor Bača <trevorbaca@gmail.com>
2015-04-09 23:47:13 -0400

    Wrote rhythmtreetools.parse_rtm_syntax() API examples.

--- abjad/tools/rhythmtreetools/parse_rtm_syntax.py
+++ abjad/tools/rhythmtreetools/parse_rtm_syntax.py
@@ -5,34 +5,60 @@ from abjad.tools import scoretools
 def parse_rtm_syntax(rtm):
     r'''Parses RTM syntax.
 
-    ::
+    ..  container:: example
 
-        >>> rtm = '(1 (1 (1 (1 1)) 1))'
-        >>> rhythmtreetools.parse_rtm_syntax(rtm)
-        FixedDurationTuplet(Duration(1, 4), "c'8 c'16 c'16 c'8")
+        **Example 1.** Parses tuplet:
 
-    Also supports fractional durations:
+        ::
 
-    ::
+            >>> rtm = '(1 (1 (1 (1 1)) 1))'
+            >>> tuplet = rhythmtreetools.parse_rtm_syntax(rtm)
+            >>> tuplet
+            FixedDurationTuplet(Duration(1, 4), "c'8 c'16 c'16 c'8")
 
-        >>> rtm = '(3/4 (1 1/2 (4/3 (1 -1/2 1))))'
-        >>> rhythmtreetools.parse_rtm_syntax(rtm)
-        FixedDurationTuplet(Duration(3, 16), "c'8 c'16 { 8/15 c'8 r16 c'8 }")
+        ::
 
-    ::
+            >>> show(tuplet) # doctest: +SKIP
 
-        >>> print(format(_))
-        \tweak #'text #tuplet-number::calc-fraction-text
-        \times 9/17 {
-            c'8
-            c'16
-            \tweak #'edge-height #'(0.7 . 0)
-            \times 8/15 {
+        ..  doctest::
+
+            >>> print(format(tuplet))
+            \times 2/3 {
                 c'8
-                r16
+                c'16
+                c'16
                 c'8
             }
-        }
+
+    ..  container:: example
+
+        **Example 2.** Also supports fractional durations:
+
+        ::
+
+            >>> rtm = '(3/4 (1 1/2 (4/3 (1 -1/2 1))))'
+            >>> tuplet = rhythmtreetools.parse_rtm_syntax(rtm)
+            >>> tuplet
+            FixedDurationTuplet(Duration(3, 16), "c'8 c'16 { 8/15 c'8 r16 c'8 }")
+
+        ::
+
+            >>> show(tuplet) # doctest: +SKIP
+
+        ..  doctest::
+
+            >>> print(format(_))
+            \tweak #'text #tuplet-number::calc-fraction-text
+            \times 9/17 {
+                c'8
+                c'16
+                \tweak #'edge-height #'(0.7 . 0)
+                \times 8/15 {
+                    c'8
+                    r16
+                    c'8
+                }
+            }
 
     Returns fixed-duration tuplet or container.
     '''
