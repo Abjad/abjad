46d64b79275dbbaa4602b24a81028963fa85be12
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-04-10 17:25:37 -0700

    Extended new() and wrote new() tests.
    Given a class like so:
    
        class Aggregate(abctools.AbjadValueObject):
    
            __slots__ = ('_pitch_segment', '_ratio')
    
            def __init__(self, pitch_segment=None, ratio=None):
                if pitch_segment is not None:
                    pitch_segment = pitchtools.PitchSegment(pitch_segment)
                self._pitch_segment = pitch_segment
                if ratio is not None:
                    ratio = mathtools.Ratio(ratio)
                self._ratio = ratio
    
            @property
            def pitch_segment(self):
                return self._pitch_segment
    
            @property
            def ratio(self):
                return self._ratio
    
    ... you can now use new() to not only replace, but also call generating methods
    on subkeys as well:
    
        >>> old_aggregate = Aggregate(
        ...    pitch_segment=pitchtools.PitchSegment('c d e f'),
        ...    ratio=mathtools.Ratio([1, 2, 3])
        ...    )
        >>> new_aggregate = new(
        ...     old_aggregate
        ...     ratio=[4, 5],
        ...     pitch_segment__rotate=2,
        ...     )
        >>> print(format(new_aggregate))
        Aggregate(
            pitch_segment=pitchtools.PitchSegment(
                (
                    pitchtools.NamedPitch('e'),
                    pitchtools.NamedPitch('f'),
                    pitchtools.NamedPitch('c'),
                    pitchtools.NamedPitch('d'),
                    ),
                item_class=pitchtools.NamedPitch,
                ),
            ratio=mathtools.Ratio((4, 5)),
            )

--- abjad/tools/topleveltools/new.py
+++ abjad/tools/topleveltools/new.py
@@ -1,4 +1,5 @@
 # -*- encoding: utf-8 -*-
+import types
 
 
 def new(expr, **kwargs):
@@ -6,19 +7,37 @@ def new(expr, **kwargs):
 
     Returns new object with the same type as `expr`.
     '''
+    def update_dictionaries(
+        expr,
+        keyword_argument_dictionary,
+        positional_argument_dictionary,
+        ):
+        keyword_argument_dictionary.update(
+            manager.get_keyword_argument_dictionary(expr)
+            )
+        if hasattr(expr, '_storage_format_specification'):
+            specification = expr._storage_format_specification
+            keyword_argument_names = specification._keyword_argument_names or ()
+            for name in keyword_argument_names:
+                keyword_argument_dictionary[name] = getattr(expr, name)
+        positional_argument_dictionary.update(
+            manager.get_positional_argument_dictionary(expr)
+            )
+
     from abjad.tools import systemtools
     if expr is None:
         return expr
+
     manager = systemtools.StorageFormatManager
-    keyword_argument_dictionary = \
-        manager.get_keyword_argument_dictionary(expr)
-    if hasattr(expr, '_storage_format_specification'):
-        specification = expr._storage_format_specification
-        keyword_argument_names = specification._keyword_argument_names or ()
-        for name in keyword_argument_names:
-            keyword_argument_dictionary[name] = getattr(expr, name)
-    positional_argument_dictionary = \
-        manager.get_positional_argument_dictionary(expr)
+
+    keyword_argument_dictionary = {}
+    positional_argument_dictionary = {}
+    update_dictionaries(
+        expr,
+        keyword_argument_dictionary,
+        positional_argument_dictionary,
+        )
+
     recursive_arguments = {}
     for key, value in kwargs.items():
         if '__' in key:
@@ -34,9 +53,22 @@ def new(expr, **kwargs):
             keyword_argument_dictionary[key] = value
         elif manager.accepts_kwargs(expr):
             keyword_argument_dictionary[key] = value
+        elif isinstance(getattr(expr, key, None), types.MethodType):
+            method = getattr(expr, key)
+            result = method(value)
+            print('!!!', expr, key, value, result)
+            if isinstance(result, type(expr)):
+                print('???')
+                expr = result
+                update_dictionaries(
+                    expr,
+                    keyword_argument_dictionary,
+                    positional_argument_dictionary,
+                    )
         else:
             message = '{} has no key {!r}'.format(type(expr), key)
             raise KeyError(message)
+
     for key, pairs in recursive_arguments.items():
         recursed_object = getattr(expr, key)
         if recursed_object is None:
@@ -50,6 +82,7 @@ def new(expr, **kwargs):
             positional_argument_dictionary[key] = recursed_object
         elif key in keyword_argument_dictionary:
             keyword_argument_dictionary[key] = recursed_object
+
     positional_argument_values = []
     positional_argument_names = getattr(
         expr, '_positional_argument_names', None) or \
@@ -58,6 +91,10 @@ def new(expr, **kwargs):
         positional_argument_value = positional_argument_dictionary[
             positional_argument_name]
         positional_argument_values.append(positional_argument_value)
+
     result = type(expr)(
-        *positional_argument_values, **keyword_argument_dictionary)
+        *positional_argument_values,
+        **keyword_argument_dictionary
+        )
+
     return result
---
+++ abjad/tools/topleveltools/test/test_topleveltools_new.py
@@ -0,0 +1,261 @@
+# -*- encoding: utf-8 -*-
+from abjad import *
+
+
+class Aggregate(abctools.AbjadValueObject):
+
+    __slots__ = ('_pitch_segment', '_ratio')
+
+    def __init__(self, pitch_segment=None, ratio=None):
+        if pitch_segment is not None:
+            pitch_segment = pitchtools.PitchSegment(pitch_segment)
+        self._pitch_segment = pitch_segment
+        if ratio is not None:
+            ratio = mathtools.Ratio(ratio)
+        self._ratio = ratio
+
+    @property
+    def pitch_segment(self):
+        return self._pitch_segment
+
+    @property
+    def ratio(self):
+        return self._ratio
+
+
+def test_topleveltools_new_01():
+
+    old_aggregate = Aggregate(
+        pitch_segment=pitchtools.PitchSegment('c d e f'),
+        ratio=mathtools.Ratio([1, 2, 3])
+        )
+
+    assert format(old_aggregate) == systemtools.TestManager.clean_string(
+        r'''
+        test_topleveltools_new.Aggregate(
+            pitch_segment=pitchtools.PitchSegment(
+                (
+                    pitchtools.NamedPitch('c'),
+                    pitchtools.NamedPitch('d'),
+                    pitchtools.NamedPitch('e'),
+                    pitchtools.NamedPitch('f'),
+                    ),
+                item_class=pitchtools.NamedPitch,
+                ),
+            ratio=mathtools.Ratio((1, 2, 3)),
+            )
+        ''')
+
+    new_aggregate = new(old_aggregate)
+
+    assert new_aggregate is not old_aggregate
+    assert new_aggregate == old_aggregate
+    assert format(old_aggregate) == systemtools.TestManager.clean_string(
+        r'''
+        test_topleveltools_new.Aggregate(
+            pitch_segment=pitchtools.PitchSegment(
+                (
+                    pitchtools.NamedPitch('c'),
+                    pitchtools.NamedPitch('d'),
+                    pitchtools.NamedPitch('e'),
+                    pitchtools.NamedPitch('f'),
+                    ),
+                item_class=pitchtools.NamedPitch,
+                ),
+            ratio=mathtools.Ratio((1, 2, 3)),
+            )
+        ''')
+    assert format(new_aggregate) == systemtools.TestManager.clean_string(
+        r'''
+        test_topleveltools_new.Aggregate(
+            pitch_segment=pitchtools.PitchSegment(
+                (
+                    pitchtools.NamedPitch('c'),
+                    pitchtools.NamedPitch('d'),
+                    pitchtools.NamedPitch('e'),
+                    pitchtools.NamedPitch('f'),
+                    ),
+                item_class=pitchtools.NamedPitch,
+                ),
+            ratio=mathtools.Ratio((1, 2, 3)),
+            )
+        ''')
+
+
+def test_topleveltools_new_02():
+
+    old_aggregate = Aggregate(
+        pitch_segment=pitchtools.PitchSegment('c d e f'),
+        ratio=mathtools.Ratio([1, 2, 3])
+        )
+
+    assert format(old_aggregate) == systemtools.TestManager.clean_string(
+        r'''
+        test_topleveltools_new.Aggregate(
+            pitch_segment=pitchtools.PitchSegment(
+                (
+                    pitchtools.NamedPitch('c'),
+                    pitchtools.NamedPitch('d'),
+                    pitchtools.NamedPitch('e'),
+                    pitchtools.NamedPitch('f'),
+                    ),
+                item_class=pitchtools.NamedPitch,
+                ),
+            ratio=mathtools.Ratio((1, 2, 3)),
+            )
+        ''')
+
+    new_aggregate = new(
+        old_aggregate,
+        ratio=(4, 5),
+        )
+
+    assert new_aggregate is not old_aggregate
+    assert new_aggregate != old_aggregate
+    assert format(old_aggregate) == systemtools.TestManager.clean_string(
+        r'''
+        test_topleveltools_new.Aggregate(
+            pitch_segment=pitchtools.PitchSegment(
+                (
+                    pitchtools.NamedPitch('c'),
+                    pitchtools.NamedPitch('d'),
+                    pitchtools.NamedPitch('e'),
+                    pitchtools.NamedPitch('f'),
+                    ),
+                item_class=pitchtools.NamedPitch,
+                ),
+            ratio=mathtools.Ratio((1, 2, 3)),
+            )
+        ''')
+    assert format(new_aggregate) == systemtools.TestManager.clean_string(
+        r'''
+        test_topleveltools_new.Aggregate(
+            pitch_segment=pitchtools.PitchSegment(
+                (
+                    pitchtools.NamedPitch('c'),
+                    pitchtools.NamedPitch('d'),
+                    pitchtools.NamedPitch('e'),
+                    pitchtools.NamedPitch('f'),
+                    ),
+                item_class=pitchtools.NamedPitch,
+                ),
+            ratio=mathtools.Ratio((4, 5)),
+            )
+        ''')
+
+
+def test_topleveltools_new_03():
+
+    old_aggregate = Aggregate(
+        pitch_segment=pitchtools.PitchSegment('c d e f'),
+        )
+
+    assert format(old_aggregate) == systemtools.TestManager.clean_string(
+        r'''
+        test_topleveltools_new.Aggregate(
+            pitch_segment=pitchtools.PitchSegment(
+                (
+                    pitchtools.NamedPitch('c'),
+                    pitchtools.NamedPitch('d'),
+                    pitchtools.NamedPitch('e'),
+                    pitchtools.NamedPitch('f'),
+                    ),
+                item_class=pitchtools.NamedPitch,
+                ),
+            )
+        ''')
+
+    new_aggregate = new(
+        old_aggregate,
+        pitch_segment='af bf df',
+        ratio=(5, 4),
+        )
+
+    assert new_aggregate is not old_aggregate
+    assert new_aggregate != old_aggregate
+    assert format(old_aggregate) == systemtools.TestManager.clean_string(
+        r'''
+        test_topleveltools_new.Aggregate(
+            pitch_segment=pitchtools.PitchSegment(
+                (
+                    pitchtools.NamedPitch('c'),
+                    pitchtools.NamedPitch('d'),
+                    pitchtools.NamedPitch('e'),
+                    pitchtools.NamedPitch('f'),
+                    ),
+                item_class=pitchtools.NamedPitch,
+                ),
+            )
+        ''')
+    assert format(new_aggregate) == systemtools.TestManager.clean_string(
+        r'''
+        test_topleveltools_new.Aggregate(
+            pitch_segment=pitchtools.PitchSegment(
+                (
+                    pitchtools.NamedPitch('af'),
+                    pitchtools.NamedPitch('bf'),
+                    pitchtools.NamedPitch('df'),
+                    ),
+                item_class=pitchtools.NamedPitch,
+                ),
+            ratio=mathtools.Ratio((5, 4)),
+            )
+        ''')
+
+
+def test_topleveltools_new_04():
+
+    old_aggregate = Aggregate(
+        pitch_segment=pitchtools.PitchSegment('c d e f'),
+        )
+
+    assert format(old_aggregate) == systemtools.TestManager.clean_string(
+        r'''
+        test_topleveltools_new.Aggregate(
+            pitch_segment=pitchtools.PitchSegment(
+                (
+                    pitchtools.NamedPitch('c'),
+                    pitchtools.NamedPitch('d'),
+                    pitchtools.NamedPitch('e'),
+                    pitchtools.NamedPitch('f'),
+                    ),
+                item_class=pitchtools.NamedPitch,
+                ),
+            )
+        ''')
+
+    new_aggregate = new(
+        old_aggregate,
+        pitch_segment__rotate=2,
+        )
+
+    assert new_aggregate is not old_aggregate
+    assert new_aggregate != old_aggregate
+    assert format(old_aggregate) == systemtools.TestManager.clean_string(
+        r'''
+        test_topleveltools_new.Aggregate(
+            pitch_segment=pitchtools.PitchSegment(
+                (
+                    pitchtools.NamedPitch('c'),
+                    pitchtools.NamedPitch('d'),
+                    pitchtools.NamedPitch('e'),
+                    pitchtools.NamedPitch('f'),
+                    ),
+                item_class=pitchtools.NamedPitch,
+                ),
+            )
+        ''')
+    assert format(new_aggregate) == systemtools.TestManager.clean_string(
+        r'''
+        test_topleveltools_new.Aggregate(
+            pitch_segment=pitchtools.PitchSegment(
+                (
+                    pitchtools.NamedPitch('e'),
+                    pitchtools.NamedPitch('f'),
+                    pitchtools.NamedPitch('c'),
+                    pitchtools.NamedPitch('d'),
+                    ),
+                item_class=pitchtools.NamedPitch,
+                ),
+            )
+        ''')
