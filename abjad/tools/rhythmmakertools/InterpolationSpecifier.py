a87f1e548eddfe8cd9a8528eb4db59cf59af3a1e
Trevor Bača <trevorbaca@gmail.com>
2015-04-12 18:39:59 -0400

    Added InterpolationSpecifier.reverse().
    See the API entry for examples.

--- abjad/tools/rhythmmakertools/InterpolationSpecifier.py
+++ abjad/tools/rhythmmakertools/InterpolationSpecifier.py
@@ -64,3 +64,56 @@ class InterpolationSpecifier(AbjadValueObject):
         Returns position duration.
         '''
         return self._written_duration
+
+    ### PUBLIC METHODS ###
+
+    def reverse(self):
+        r'''Swaps start duration and stop duration of interpolation specifier.
+
+        ..  container:: example
+
+            **Example 1.** Changes accelerando specifier to ritardando
+            specifier:
+
+            ::
+
+                >>> accelerando_specifier = rhythmmakertools.InterpolationSpecifier(
+                ...     start_duration=Duration(1, 4),
+                ...     stop_duration=Duration(1, 16),
+                ...     written_duration=Duration(1, 16),
+                ...     )
+                >>> ritardando_specifier = accelerando_specifier.reverse()
+                >>> print(format(ritardando_specifier))
+                rhythmmakertools.InterpolationSpecifier(
+                    start_duration=durationtools.Duration(1, 16),
+                    stop_duration=durationtools.Duration(1, 4),
+                    written_duration=durationtools.Duration(1, 16),
+                    )
+
+        ..  container:: example
+
+            **Example 2.** Changes ritardando specifier to accelerando
+            specifier:
+
+            ::
+
+                >>> accelerando_specifier = rhythmmakertools.InterpolationSpecifier(
+                ...     start_duration=Duration(1, 16),
+                ...     stop_duration=Duration(1, 4),
+                ...     written_duration=Duration(1, 16),
+                ...     )
+                >>> ritardando_specifier = accelerando_specifier.reverse()
+                >>> print(format(ritardando_specifier))
+                rhythmmakertools.InterpolationSpecifier(
+                    start_duration=durationtools.Duration(1, 4),
+                    stop_duration=durationtools.Duration(1, 16),
+                    written_duration=durationtools.Duration(1, 16),
+                    )
+
+        Copies written duration from source.
+        '''
+        return type(self)(
+            start_duration=self.stop_duration,
+            stop_duration=self.start_duration,
+            written_duration=self.written_duration,
+            )
