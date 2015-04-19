c3dca4fb752627528504d25b559ff56954fa5e8b
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-04-18 20:05:40 -0700

    Cleaned up BreathMark.

--- abjad/tools/indicatortools/BreathMark.py
+++ abjad/tools/indicatortools/BreathMark.py
@@ -7,8 +7,6 @@ class BreathMark(AbjadValueObject):
 
     ..  container:: example
 
-        BreathMark:
-
         ::
 
             >>> note = Note("c'4")
@@ -21,12 +19,38 @@ class BreathMark(AbjadValueObject):
             >>> print(format(note))
             c'4 \breathe
 
+    ..  container:: example
+
+        ::
+
+            >>> staff = Staff("c'8 d' e' f' g' a' b' c''")
+            >>> attach(Beam(), staff[:4])
+            >>> attach(Beam(), staff[4:])
+            >>> attach(indicatortools.BreathMark(), staff[3])
+            >>> attach(indicatortools.BreathMark(), staff[7])
+            >>> show(staff) # doctest: +SKIP
+
+        ..  doctest::
+
+            >>> print(format(staff))
+            \new Staff {
+                c'8 [
+                d'8
+                e'8
+                f'8 ]
+                \breathe
+                g'8 [
+                a'8
+                b'8
+                c''8 ]
+                \breathe
+            }
+
     '''
 
     ### CLASS VARIABLES ###
 
-    __slots__ = (
-        )
+    __slots__ = ()
 
     _format_slot = 'after'
 
@@ -56,7 +80,7 @@ class BreathMark(AbjadValueObject):
     def _get_lilypond_format_bundle(self, component=None):
         from abjad.tools import systemtools
         lilypond_format_bundle = systemtools.LilyPondFormatBundle()
-        lilypond_format_bundle.right.articulations.append(str(self))
+        lilypond_format_bundle.after.commands.append(str(self))
         return lilypond_format_bundle
 
     ### PRIVATE PROPERTIES ###
