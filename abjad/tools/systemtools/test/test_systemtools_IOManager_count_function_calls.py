21182699ae9aeabea8d7f66a04e2351966ad07be
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-04-26 18:02:03 -0700

    Adjusted one test for Python 3.

--- abjad/tools/systemtools/test/test_systemtools_IOManager_count_function_calls.py
+++ abjad/tools/systemtools/test/test_systemtools_IOManager_count_function_calls.py
@@ -12,7 +12,7 @@ def test_systemtools_IOManager_count_function_calls_01():
     if sys.version_info[0] == 2:
         assert result < 14000
     else:
-        assert result < 17000
+        assert result < 19500
 
 
 def test_systemtools_IOManager_count_function_calls_02():
