c8931058c576e6346eda742dc9083a4e5644302c
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-07-25 21:54:30 -0700

    Made builtin-modifying code pypy compatible.

--- abjad/tools/__init__.py
+++ abjad/tools/__init__.py
@@ -5,17 +5,19 @@ from abjad.tools import mathtools
 
 
 # load constants into __builtins__ namespace
-__builtins__['Less'] = datastructuretools.OrdinalConstant('value', -1, 'Less')
-__builtins__['More'] = datastructuretools.OrdinalConstant('value', 1, 'More')
-__builtins__['Exact'] = datastructuretools.OrdinalConstant(
+import builtins
+builtins.Less = datastructuretools.OrdinalConstant('value', -1, 'Less')
+builtins.More = datastructuretools.OrdinalConstant('value', 1, 'More')
+builtins.Exact = datastructuretools.OrdinalConstant(
     'value', 0, 'Exact')
-__builtins__['Left'] = datastructuretools.OrdinalConstant('x', -1, 'Left')
-__builtins__['Right'] = datastructuretools.OrdinalConstant('x', 1, 'Right')
-__builtins__['Center'] = datastructuretools.OrdinalConstant('y', 0, 'Center')
-__builtins__['Up'] = datastructuretools.OrdinalConstant('y', 1, 'Up')
-__builtins__['Down'] = datastructuretools.OrdinalConstant('y', -1, 'Down')
-__builtins__['Infinity'] = mathtools.Infinity()
-__builtins__['NegativeInfinity'] = mathtools.NegativeInfinity()
+builtins.Left = datastructuretools.OrdinalConstant('x', -1, 'Left')
+builtins.Right = datastructuretools.OrdinalConstant('x', 1, 'Right')
+builtins.Center = datastructuretools.OrdinalConstant('y', 0, 'Center')
+builtins.Up = datastructuretools.OrdinalConstant('y', 1, 'Up')
+builtins.Down = datastructuretools.OrdinalConstant('y', -1, 'Down')
+builtins.Infinity = mathtools.Infinity()
+builtins.NegativeInfinity = mathtools.NegativeInfinity()
+del(builtins)
 
 systemtools.ImportManager.import_structured_package(
     __path__[0],
