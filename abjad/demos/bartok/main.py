c54804891c945950eede02dbaa86c3e2cabf2d3b
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-07-28 19:33:33 -0700

    Modernized demos.

--- abjad/demos/bartok/main.py
+++ abjad/demos/bartok/main.py
@@ -3,6 +3,6 @@
 
 if __name__ == '__main__':
     from abjad import show
-    from abjad.demos import desordre
-    lilypond_file = desordre.make_desordre_lilypond_file()
+    from abjad.demos import bartok
+    lilypond_file = bartok.make_bartok_score()
     show(lilypond_file)
