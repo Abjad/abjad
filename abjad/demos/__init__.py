f66d1d72e867e6c3a0f1e15481c1b07fc87b7be9
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-07-28 20:03:31 -0700

    Modernized demos.

--- abjad/demos/__init__.py
+++ abjad/demos/__init__.py
@@ -1,13 +1,6 @@
 # -*- encoding: utf-8 -*-
-import sys
-if sys.version_info[0] == 2:
-    import desordre
-    import ferneyhough
-    import mozart
-    import part
-else:
-    from abjad.demos import desordre
-    from abjad.demos import ferneyhough
-    from abjad.demos import mozart
-    from abjad.demos import part
-del sys
+from abjad.demos import bartok
+from abjad.demos import desordre
+from abjad.demos import ferneyhough
+from abjad.demos import mozart
+from abjad.demos import part
---
+++ abjad/demos/bartok/__init__.py
@@ -0,0 +1,11 @@
+# -*- encoding: utf-8 -*-
+from abjad.tools import systemtools
+
+systemtools.ImportManager.import_structured_package(
+    __path__[0],
+    globals(),
+    )
+
+_documentation_section = 'demos'
+
+del(main)
