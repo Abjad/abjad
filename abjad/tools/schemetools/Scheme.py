b9706ca21a3d774ed72622161b34952e75c7dac8
Oscar Dub <odub@users.noreply.github.com>
2015-08-02 20:54:00 +0200

    Fix character escaping in schemetools doctest

--- abjad/tools/schemetools/Scheme.py
+++ abjad/tools/schemetools/Scheme.py
@@ -295,7 +295,7 @@ class Scheme(AbjadValueObject):
                 >>> string = '(lambda (grob) (grob-interpret-markup grob'
                 >>> string += r' #{ \markup \musicglyph #"noteheads.s0harmonic" #}))'
                 >>> schemetools.Scheme.format_scheme_value(string, verbatim=True)
-                '(lambda (grob) (grob-interpret-markup grob #{ \\markup \\musicglyph #\\"noteheads.s0harmonic\\" #}))'
+                '(lambda (grob) (grob-interpret-markup grob #{ \\markup \\musicglyph #"noteheads.s0harmonic" #}))'
 
         Returns string.
         '''
