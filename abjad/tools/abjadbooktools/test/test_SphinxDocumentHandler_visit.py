d55aabaab37067c5e18d159249c99a343add36fb
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-08-02 16:47:55 -0700

    Writing docutils node-visiting tests in abjadbooktools.

--- abjad/tools/abjadbooktools/test/test_SphinxDocumentHandler_visit.py
+++ abjad/tools/abjadbooktools/test/test_SphinxDocumentHandler_visit.py
@@ -10,6 +10,8 @@ import unittest
 
 class SphinxDocumentHandlerTests(unittest.TestCase):
 
+    maxDiff = None
+
     class Namespace(object):
         pass
 
@@ -28,6 +30,10 @@ class SphinxDocumentHandlerTests(unittest.TestCase):
         self.images_directory = os.path.join(
             self.app.builder.outdir,
             self.app.builder.imagedir,
+            )
+        self.abjadbook_images_directory = os.path.join(
+            self.images_directory,
+            'abjadbook',
             )
         if os.path.exists(self.images_directory):
             shutil.rmtree(self.images_directory)
@@ -63,9 +69,46 @@ class SphinxDocumentHandlerTests(unittest.TestCase):
         </div>
         ''')
         self.assertEqual(actual, expected)
+        assert len(os.listdir(self.abjadbook_images_directory)) == 2
         for name in (
             'lilypond-cae93b80d6f6c70eab16244f682fce8f2f5a1821.ly',
             'lilypond-cae93b80d6f6c70eab16244f682fce8f2f5a1821.png',
+            ):
+            path = os.path.join(self.images_directory, 'abjadbook', name)
+            assert os.path.exists(path)
+
+    def test_02(self):
+        source = r'''
+        ..  abjad::
+            :hide:
+            :no-stylesheet:
+            :no-trim:
+
+            show(Staff("c'4 d'4 e'4 f'4"))
+        '''
+        source = systemtools.TestManager.clean_string(source)
+        handler = abjadbooktools.SphinxDocumentHandler()
+        document = handler.parse_rst(source)
+        handler.on_doctree_read(self.app, document)
+        node = document[0]
+        try:
+            abjadbooktools.SphinxDocumentHandler.visit_abjad_output_block_html(
+                self.app, node)
+        except docutils.nodes.SkipNode:
+            pass
+        actual = '\n'.join(self.app.body)
+        expected = systemtools.TestManager.clean_string(r'''
+        <div class="abjad-book-image">
+            <a href="../_images/abjadbook/abjadbook/lilypond-868cc561d54bf8b577ddf5fbc9ca6ab1a22a8c02.ly">
+                <img src="../_images/abjadbook/abjadbook/lilypond-868cc561d54bf8b577ddf5fbc9ca6ab1a22a8c02.png" alt="View source." title="View source." />
+            </a>
+        </div>
+        ''')
+        self.assertEqual(actual, expected)
+        assert len(os.listdir(self.abjadbook_images_directory)) == 2
+        for name in (
+            'lilypond-868cc561d54bf8b577ddf5fbc9ca6ab1a22a8c02.ly',
+            'lilypond-868cc561d54bf8b577ddf5fbc9ca6ab1a22a8c02.png',
             ):
             path = os.path.join(self.images_directory, 'abjadbook', name)
             assert os.path.exists(path)
