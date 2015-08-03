7d45f48fe8f9d9848999501a89940bf6c81d51d9
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-08-02 17:39:44 -0700

    Added a test for new(Articulation('.')) == Articulation('.').
    Closes #424.

---
+++ abjad/tools/indicatortools/test/test_indicatortools_Articulation_new.py
@@ -0,0 +1,8 @@
+# -*- encoding: utf-8 -*-
+from abjad import *
+
+
+def test_indicatortools_Articulation_new_01():
+    old_articulation = Articulation('.')
+    new_articulation = new(old_articulation)
+    assert repr(new_articulation) == "Articulation('.')"
