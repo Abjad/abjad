d3e32aba4e58e7ba8c010f22a768f5bfe886090e
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-08-04 11:05:30 -0700

    Extended GraphvizGraph deepcopy tests.

--- abjad/tools/documentationtools/test/test_documentationtools_GraphvizGraph_deepcopy.py
+++ abjad/tools/documentationtools/test/test_documentationtools_GraphvizGraph_deepcopy.py
@@ -1,9 +1,67 @@
 # -*- encoding: utf-8 -*-
 import copy
 from abjad.tools import documentationtools
+from abjad.tools import systemtools
 
 
 def test_documentationtools_GraphvizGraph_deepcopy_01():
+    graph = documentationtools.GraphvizGraph()
+    assert str(graph) == systemtools.TestManager.clean_string(
+        r'''
+        digraph G {
+        }
+        ''')
+    copied = copy.deepcopy(graph)
+    assert str(graph) == str(copied)
+
+
+def test_documentationtools_GraphvizGraph_deepcopy_02():
+    graph = documentationtools.GraphvizGraph()
+    graph.append(documentationtools.GraphvizSubgraph())
+    assert str(graph) == systemtools.TestManager.clean_string(
+        r'''
+        digraph G {
+            subgraph cluster_0 {
+            }
+        }
+        ''')
+    copied = copy.deepcopy(graph)
+    assert str(graph) == str(copied)
+
+
+def test_documentationtools_GraphvizGraph_deepcopy_03():
+    graph = documentationtools.GraphvizGraph()
+    graph.append(documentationtools.GraphvizNode())
+    graph.append(documentationtools.GraphvizNode())
+    assert str(graph) == systemtools.TestManager.clean_string(
+        r'''
+        digraph G {
+            node_0;
+            node_1;
+        }
+        ''')
+    copied = copy.deepcopy(graph)
+    assert str(graph) == str(copied)
+
+
+def test_documentationtools_GraphvizGraph_deepcopy_04():
+    graph = documentationtools.GraphvizGraph()
+    graph.append(documentationtools.GraphvizNode())
+    graph.append(documentationtools.GraphvizNode())
+    edge = documentationtools.GraphvizEdge()(graph[0], graph[1])
+    assert str(graph) == systemtools.TestManager.clean_string(
+        r'''
+        digraph G {
+            node_0;
+            node_1;
+            node_0 -> node_1;
+        }
+        ''')
+    copied = copy.deepcopy(graph)
+    assert str(graph) == str(copied)
+
+
+def test_documentationtools_GraphvizGraph_deepcopy_05():
     graph = documentationtools.GraphvizGraph()
     graph.append(documentationtools.GraphvizSubgraph())
     graph[0].append(documentationtools.GraphvizNode())
@@ -12,15 +70,24 @@ def test_documentationtools_GraphvizGraph_deepcopy_01():
     graph[0].append(documentationtools.GraphvizSubgraph())
     graph[0][-1].append(documentationtools.GraphvizNode())
     graph.append(documentationtools.GraphvizNode())
-    documentationtools.GraphvizEdge()(
-        graph[0][1],
-        graph[1],
+    edge = documentationtools.GraphvizEdge()(graph[0][1], graph[1])
+    edge = documentationtools.GraphvizEdge()(graph[0][0], graph[0][-1][0])
+    assert str(graph) == systemtools.TestManager.clean_string(
+        r'''
+        digraph G {
+            subgraph cluster_0 {
+                node_0_0;
+                node_0_1;
+                node_0_2;
+                subgraph cluster_0_3 {
+                    node_0_3_0;
+                }
+                node_0_0 -> node_0_3_0;
+            }
+            node_1;
+            node_0_1 -> node_1;
+        }
+        '''
         )
-    documentationtools.GraphvizEdge()(
-        graph[0][0],
-        graph[0][-1][0],
-        )
-
-    new_graph = copy.deepcopy(graph)
-
-    assert str(graph) == str(new_graph)
+    copied = copy.deepcopy(graph)
+    assert str(graph) == str(copied)
