601195b43089e284b1f172847b3399ce32955bb1
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-08-07 14:24:27 -0700

    Added parameterized test for co-existence of __mul__ and __rmul__.

---
+++ abjad/tools/test/test_abjad___rmul__.py
@@ -0,0 +1,22 @@
+# -*- encoding: utf-8 -*-
+import inspect
+import pytest
+from abjad.tools import documentationtools
+
+
+ignored_classes = (
+    )
+
+classes = documentationtools.list_all_abjad_classes(
+    ignored_classes=ignored_classes,
+    )
+
+
+@pytest.mark.parametrize('class_', classes)
+def test_abjad___rmul___01(class_):
+    r'''All classes implementing __mul__ also implement __rmul__.
+    '''
+    if inspect.isabstract(class_):
+        return
+    if hasattr(class_, '__mul__'):
+        assert hasattr(class_, '__rmul__')
