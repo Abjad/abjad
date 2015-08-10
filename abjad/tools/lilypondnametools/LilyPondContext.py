500d40bb3479d3d2b4cf28ec23595ff869e2f921
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-08-10 16:07:35 -0700

    Extended LilyPondContext with .register() and .unregister().
    These methods allow for the (un)registration of custom contexts.

--- abjad/tools/lilypondnametools/LilyPondContext.py
+++ abjad/tools/lilypondnametools/LilyPondContext.py
@@ -26,6 +26,8 @@ class LilyPondContext(abctools.AbjadValueObject):
     ### CONSTRUCTOR ###
 
     def __new__(cls, name='Voice'):
+        if isinstance(name, cls):
+            name = name.name
         if name in cls._identity_map:
             obj = cls._identity_map[name]
         else:
@@ -88,6 +90,220 @@ class LilyPondContext(abctools.AbjadValueObject):
         '''
         from abjad.ly import contexts
         return tuple(LilyPondContext(name=name) for name in sorted(contexts))
+
+    @classmethod
+    def register(
+        cls,
+        accepted_by=None,
+        accepts=None,
+        alias=None,
+        consists=None,
+        default_child=None,
+        denies=None,
+        name=None,
+        removes=None,
+        ):
+        r'''Registers a new context.
+
+        ::
+
+            >>> custom_context = lilypondnametools.LilyPondContext.register(
+            ...     accepted_by=['Score', 'StaffGroup'],
+            ...     alias='Staff',
+            ...     name='BowingStaff',
+            ...     removes=['Note_heads_engraver'],
+            ...     )
+            >>> print(format(custom_context))
+            lilypondnametools.LilyPondContext(
+                name='BowingStaff',
+                )
+
+        ::
+
+            >>> custom_context.is_custom
+            True
+
+        ::
+
+            >>> for engraver in custom_context.engravers:
+            ...     engraver
+            ...
+            LilyPondEngraver(name='Accidental_engraver')
+            LilyPondEngraver(name='Axis_group_engraver')
+            LilyPondEngraver(name='Bar_engraver')
+            LilyPondEngraver(name='Clef_engraver')
+            LilyPondEngraver(name='Collision_engraver')
+            LilyPondEngraver(name='Cue_clef_engraver')
+            LilyPondEngraver(name='Dot_column_engraver')
+            LilyPondEngraver(name='Figured_bass_engraver')
+            LilyPondEngraver(name='Figured_bass_position_engraver')
+            LilyPondEngraver(name='Fingering_column_engraver')
+            LilyPondEngraver(name='Font_size_engraver')
+            LilyPondEngraver(name='Grob_pq_engraver')
+            LilyPondEngraver(name='Instrument_name_engraver')
+            LilyPondEngraver(name='Key_engraver')
+            LilyPondEngraver(name='Ledger_line_engraver')
+            LilyPondEngraver(name='Ottava_spanner_engraver')
+            LilyPondEngraver(name='Output_property_engraver')
+            LilyPondEngraver(name='Piano_pedal_align_engraver')
+            LilyPondEngraver(name='Piano_pedal_engraver')
+            LilyPondEngraver(name='Pure_from_neighbor_engraver')
+            LilyPondEngraver(name='Rest_collision_engraver')
+            LilyPondEngraver(name='Script_row_engraver')
+            LilyPondEngraver(name='Separating_line_group_engraver')
+            LilyPondEngraver(name='Staff_collecting_engraver')
+            LilyPondEngraver(name='Staff_symbol_engraver')
+            LilyPondEngraver(name='Time_signature_engraver')
+
+        ::
+
+            >>> score_context = lilypondnametools.LilyPondContext('Score')
+            >>> custom_context in score_context.accepts
+            True
+
+        ::
+
+            >>> custom_context.unregister()
+        
+        '''
+        from abjad.ly import contexts
+        from abjad.tools import lilypondnametools
+        assert name not in contexts
+        context_entry = {
+            'accepts': set(),
+            'consists': set(),
+            'is_custom': True,
+            }
+        if alias is not None:
+            if not isinstance(alias, cls):
+                alias = cls(name=alias)
+            assert isinstance(alias, cls)
+            context_entry['accepts'].update(_.name for _ in alias.accepts)
+            context_entry['consists'].update(_.name for _ in alias.engravers)
+            context_entry['aliases'] = set([alias.name])
+        if accepts:
+            for x in accepts:
+                if not isinstance(x, cls):
+                    x = cls(name=x)
+                assert isinstance(x, cls)
+                context_entry['accepts'].add(x.name)
+        if denies:
+            for x in denies:
+                if not isinstance(x, cls):
+                    x = cls(name=x)
+                assert isinstance(x, cls)
+                if x.name in context_entry['accepts']:
+                    context_entry['accepts'].remove(x.name)
+        if consists:
+            for x in consists:
+                if not isinstance(x, lilypondnametools.LilyPondEngraver):
+                    x = lilypondnametools.LilyPondEngraver(name=x)
+                assert isinstance(x, lilypondnametools.LilyPondEngraver)
+                context_entry['consists'].add(x.name)
+        if removes:
+            for x in removes:
+                if not isinstance(x, lilypondnametools.LilyPondEngraver):
+                    x = lilypondnametools.LilyPondEngraver(name=x)
+                assert isinstance(x, lilypondnametools.LilyPondEngraver)
+                if x.name in context_entry['consists']:
+                    context_entry['consists'].remove(x.name)
+        if default_child is not None:
+            if not isinstance(default_child, cls):
+                default_child = cls(name=default_child)
+            assert isinstance(default_child, cls)
+            context_entry['default_child'] = default_child.name
+        accepting_contexts = set()
+        if accepted_by:
+            for x in accepted_by:
+                if not isinstance(x, cls):
+                    x = cls(name=x)
+                assert isinstance(x, cls)
+                accepting_contexts.add(x.name)
+        contexts[name] = context_entry
+        for accepting_context in accepting_contexts:
+            contexts[accepting_context]['accepts'].add(name)
+        custom_context = cls(name=name)
+        return custom_context
+
+    def unregister(
+        self,
+        context=None,
+        ):
+        r'''Unregisters custom context.
+
+        ::
+
+            >>> custom_context = lilypondnametools.LilyPondContext.register(
+            ...     accepted_by=['Score', 'StaffGroup'],
+            ...     alias='Staff',
+            ...     name='FingeringStaff',
+            ...     )
+
+        ::
+
+            >>> score_context = lilypondnametools.LilyPondContext('Score')
+            >>> for accepted_context in score_context.accepts:
+            ...     accepted_context
+            ...
+            LilyPondContext(name='ChoirStaff')
+            LilyPondContext(name='ChordNames')
+            LilyPondContext(name='Devnull')
+            LilyPondContext(name='DrumStaff')
+            LilyPondContext(name='FiguredBass')
+            LilyPondContext(name='FingeringStaff')
+            LilyPondContext(name='FretBoards')
+            LilyPondContext(name='GrandStaff')
+            LilyPondContext(name='GregorianTranscriptionStaff')
+            LilyPondContext(name='KievanStaff')
+            LilyPondContext(name='Lyrics')
+            LilyPondContext(name='MensuralStaff')
+            LilyPondContext(name='NoteNames')
+            LilyPondContext(name='PetrucciStaff')
+            LilyPondContext(name='PianoStaff')
+            LilyPondContext(name='RhythmicStaff')
+            LilyPondContext(name='Staff')
+            LilyPondContext(name='StaffGroup')
+            LilyPondContext(name='TabStaff')
+            LilyPondContext(name='VaticanaStaff')
+
+        ::
+
+            >>> custom_context.unregister()
+
+        ::
+
+            >>> score_context = lilypondnametools.LilyPondContext('Score')
+            >>> for accepted_context in score_context.accepts:
+            ...     accepted_context
+            ...
+            LilyPondContext(name='ChoirStaff')
+            LilyPondContext(name='ChordNames')
+            LilyPondContext(name='Devnull')
+            LilyPondContext(name='DrumStaff')
+            LilyPondContext(name='FiguredBass')
+            LilyPondContext(name='FretBoards')
+            LilyPondContext(name='GrandStaff')
+            LilyPondContext(name='GregorianTranscriptionStaff')
+            LilyPondContext(name='KievanStaff')
+            LilyPondContext(name='Lyrics')
+            LilyPondContext(name='MensuralStaff')
+            LilyPondContext(name='NoteNames')
+            LilyPondContext(name='PetrucciStaff')
+            LilyPondContext(name='PianoStaff')
+            LilyPondContext(name='RhythmicStaff')
+            LilyPondContext(name='Staff')
+            LilyPondContext(name='StaffGroup')
+            LilyPondContext(name='TabStaff')
+            LilyPondContext(name='VaticanaStaff')
+
+        '''
+        from abjad.ly import contexts
+        assert self.is_custom
+        del(contexts[self.name])
+        del(self._identity_map[self.name])
+        for context_name, context_info in contexts.items():
+            if self.name in context_info['accepts']:
+                context_info['accepts'].remove(self.name)
 
     ### PUBLIC PROPERTIES ###
 
@@ -266,6 +482,20 @@ class LilyPondContext(abctools.AbjadValueObject):
         for engraver in self.engravers:
             grobs.update(engraver.grobs)
         return tuple(sorted(grobs, key=lambda x: x.name))
+
+    @property
+    def is_custom(self):
+        r'''Is true if LilyPond context is user-created. Otherwise false.
+
+        ::
+
+            >>> context.is_custom
+            False
+
+        Returns boolean.
+        '''
+        from abjad.ly import contexts
+        return bool(contexts[self.name].get('is_custom', False))
 
     @property
     def name(self):
