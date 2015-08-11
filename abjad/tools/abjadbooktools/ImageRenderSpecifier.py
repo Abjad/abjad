737049e31c2fecb6db1e6357f806b11da6e1e0cf
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-08-11 14:33:43 -0700

    Added no_resize property to abjadbooktools.ImageRenderSpecifier.

--- abjad/tools/abjadbooktools/ImageRenderSpecifier.py
+++ abjad/tools/abjadbooktools/ImageRenderSpecifier.py
@@ -11,6 +11,7 @@ class ImageRenderSpecifier(abctools.AbjadValueObject):
     __documentation_section__ = 'Internals'
 
     __slots__ = (
+        '_no_resize',
         '_no_stylesheet',
         '_no_trim',
         '_stylesheet',
@@ -20,10 +21,12 @@ class ImageRenderSpecifier(abctools.AbjadValueObject):
 
     def __init__(
         self,
+        no_resize=None,
         no_stylesheet=None,
         no_trim=None,
         stylesheet=None,
         ):
+        self._no_resize = bool(no_resize) or None
         self._no_stylesheet = bool(no_stylesheet) or None
         self._no_trim = bool(no_trim) or None
         self._stylesheet = stylesheet
@@ -38,22 +41,34 @@ class ImageRenderSpecifier(abctools.AbjadValueObject):
 
         Returns image specifier.
         '''
+        no_resize = options.get('no_resize', None) or None
         no_stylesheet = options.get('no_stylesheet', None) or None
         no_trim = options.get('no_trim', None) or None
         stylesheet = options.get('stylesheet', None) or None
         if all(_ is None for _ in (
+            no_resize,
             no_stylesheet,
             no_trim,
             stylesheet,
             )):
             return None
         return cls(
+            no_resize=no_resize,
             no_stylesheet=no_stylesheet,
             no_trim=no_trim,
             stylesheet=stylesheet,
             )
 
     ### PUBLIC PROPERTIES ###
+
+    @property
+    def no_resize(self):
+        r'''Is true if image should not be resized.
+        Otherwise false.
+
+        Returns boolean.
+        '''
+        return self._no_resize
 
     @property
     def no_stylesheet(self):
