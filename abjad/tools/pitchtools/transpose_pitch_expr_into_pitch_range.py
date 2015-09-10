519a4fb0903d506dab0345953c67d4b9b60b49d0
Trevor Bača <trevorbaca@gmail.com>
2015-09-10 15:03:41 -0400

    Cleaned up one pitchtools function.

--- abjad/tools/pitchtools/transpose_pitch_expr_into_pitch_range.py
+++ abjad/tools/pitchtools/transpose_pitch_expr_into_pitch_range.py
@@ -2,7 +2,7 @@
 
 
 def transpose_pitch_expr_into_pitch_range(pitch_expr, pitch_range):
-    '''Transpose `pitch_expr` into `pitch_range`:
+    '''Transposes `pitch_expr` into `pitch_range`:
 
     ::
 
@@ -13,25 +13,30 @@ def transpose_pitch_expr_into_pitch_range(pitch_expr, pitch_range):
 
     Returns new `pitch_expr` object.
     '''
-
     try:
         transposed_pitch_carriers = [
-            _transpose_pitch_carrier_into_pitch_range(x, pitch_range) for x in pitch_expr]
+            _transpose_pitch_carrier_into_pitch_range(x, pitch_range)
+            for x in pitch_expr
+            ]
         return type(pitch_expr)(transposed_pitch_carriers)
     except TypeError:
-        return _transpose_pitch_carrier_into_pitch_range(pitch_expr, pitch_range)
+        return _transpose_pitch_carrier_into_pitch_range(
+            pitch_expr,
+            pitch_range,
+            )
 
 
 # TODO: make public? make nested helper?
 def _transpose_pitch_carrier_into_pitch_range(pitch_carrier, pitch_range):
     from abjad.tools import pitchtools
-
     while pitch_carrier < pitch_range:
-        pitch_carrier = pitchtools.transpose_pitch_carrier_by_interval(pitch_carrier, 12)
+        pitch_carrier = pitchtools.transpose_pitch_carrier_by_interval(
+            pitch_carrier, 12)
     while pitch_range < pitch_carrier:
-        pitch_carrier = pitchtools.transpose_pitch_carrier_by_interval(pitch_carrier, -12)
+        pitch_carrier = pitchtools.transpose_pitch_carrier_by_interval(
+            pitch_carrier, -12)
     if pitch_carrier not in pitch_range:
-        message = 'can not transpose pitch carrier {} into pitch range.'
+        message = 'can not transpose pitch carrier {!r} into pitch range.'
         message = message.format(pitch_carrier)
         raise ValueError(message)
     return pitch_carrier
