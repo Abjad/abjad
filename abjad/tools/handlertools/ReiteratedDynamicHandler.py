afc7ec484ea908cad60b783e60e0a3b388e92af4
Trevor Bača <trevorbaca@gmail.com>
2015-09-29 12:13:46 -0500

    Extended ReiteratedDynamicHandler.
    OLD: dynamic_name input only like 'f'.
    
    NEW: dynamic_name input possible like 'f p mf mp'. Applied cyclically.

--- abjad/tools/handlertools/ReiteratedDynamicHandler.py
+++ abjad/tools/handlertools/ReiteratedDynamicHandler.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from abjad.tools import datastructuretools
 from abjad.tools import indicatortools
 from abjad.tools import scoretools
 from abjad.tools.topleveltools import attach
@@ -11,6 +12,8 @@ class ReiteratedDynamicHandler(DynamicHandler):
     r'''Reiterated dynamic handler.
 
     ..  container:: example
+
+        **Example 1.** With one dynamic:
 
         ::
 
@@ -33,21 +36,53 @@ class ReiteratedDynamicHandler(DynamicHandler):
                 e'4 \f
             }
 
+    ..  container:: example
+
+        **Example 2.** With multiple dynamics:
+
+        ::
+
+            >>> handler = handlertools.ReiteratedDynamicHandler(
+            ...     dynamic_name='f p',
+            ...     )
+            >>> staff = Staff("c'4. ~ c'8 d'4 e'4")
+            >>> logical_ties = iterate(staff).by_logical_tie(pitched=True)
+            >>> logical_ties = list(logical_ties)
+            >>> logical_ties = handler(logical_ties)
+            >>> show(staff) # doctest: +SKIP
+
+        ..  doctest::
+
+            >>> print(format(staff))
+            \new Staff {
+                c'4. \f ~
+                c'8
+                d'4 \p
+                e'4 \f
+            }
+
     '''
 
     ### CLASS ATTRIBUTES ###
 
     __slots__ = (
         '_dynamic_name',
+        '_dynamic_names',
         )
 
     ### INITIALIZER ###
 
     def __init__(self, dynamic_name=None, minimum_duration=None):
         DynamicHandler.__init__(self, minimum_duration=minimum_duration)
-        if dynamic_name is not None:
-            assert indicatortools.Dynamic.is_dynamic_name(dynamic_name)
         self._dynamic_name = dynamic_name
+        dynamic_names = ()
+        if dynamic_name is not None:
+            assert isinstance(dynamic_name, str), repr(dynamic_name)
+            dynamic_names = dynamic_name.split()
+        for dynamic_name in dynamic_names:
+            assert indicatortools.Dynamic.is_dynamic_name(dynamic_name)
+        dynamic_names = datastructuretools.CyclicTuple(dynamic_names)
+        self._dynamic_names = dynamic_names
 
     ### SPECIAL METHODS ###
 
@@ -56,23 +91,26 @@ class ReiteratedDynamicHandler(DynamicHandler):
 
         Returns none.
         '''
+        note_or_chords = []
         prototype = (scoretools.Note, scoretools.Chord)
-        for note_or_chord in iterate(expr).by_class(prototype):
+        logical_ties = iterate(expr).by_logical_tie()
+        logical_ties = [
+            _ for _ in logical_ties if isinstance(_.head, prototype)]
+        for i, logical_tie in enumerate(logical_ties):
             #indicatortools.Dynamic(self.dynamic_name)(note_or_chord)
+            dynamic_name = self._dynamic_names[i]
             command = indicatortools.LilyPondCommand(
-                self.dynamic_name,
+                dynamic_name,
                 'right',
                 )
-            logical_tie = inspect_(note_or_chord).get_logical_tie()
-            if note_or_chord is logical_tie.head:
-                attach(command, note_or_chord)
+            attach(command, logical_tie.head)
         return expr
 
     ### PUBLIC PROPERTIES ###
 
     @property
     def dynamic_name(self):
-        r'''Gets dynamic name of handler.
+        r'''Gets dynamic name.
 
         Returns string or none.
         '''
