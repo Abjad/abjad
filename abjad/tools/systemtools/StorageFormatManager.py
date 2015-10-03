2238bdc3502ffbb7edd2272d97dc11b121d336bf
Trevor Bača <trevorbaca@gmail.com>
2015-10-03 19:03:03 -0400

    Harmonzied StorageFormatManager and ImageRenderSpecifier.
    Changed back to previous implementation.
    
    No idea what the underlying problem was. The _storage_format_specification of
    ImageRenderSpecifier was probably fine.
    
    The recent series of fixes to revive the experimental API had the collateral
    benefit of fixing the problem with ImageRenderSpecifier.

--- abjad/tools/systemtools/StorageFormatManager.py
+++ abjad/tools/systemtools/StorageFormatManager.py
@@ -192,13 +192,7 @@ class StorageFormatManager(AbjadObject):
 
         keyword_argument_pieces = []
         for name in specification.keyword_argument_names:
-            # TODO: temporary hack while debugging:
-            if (name == 'no_resize' and
-                specification.instance.__class__.__name__ ==
-                'ImageRenderSpecifier'):
-                value = None
-            else:
-                value = getattr(specification.instance, name)
+            value = getattr(specification.instance, name)
             if value is None or isinstance(value, types.MethodType):
                 continue
             if specification.keyword_argument_callables:
