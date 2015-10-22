ea04205d41aad2eb63722b4aff69bfa83aeb1d3b
Trevor Bača <trevorbaca@gmail.com>
2015-10-22 11:47:32 -0400

    Changed boilerplate score makers/ directory to tools/ directory.

--- abjad/boilerplate/example_score/example_score/__init__.py
+++ abjad/boilerplate/example_score/example_score/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
-from {score_package_name} import makers
 from {score_package_name} import materials
 from {score_package_name} import segments
+from {score_package_name} import tools
