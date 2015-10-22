5bb589731d27d1529e2c97d13b93d16090539e2b
Trevor Bača <trevorbaca@gmail.com>
2015-10-22 15:29:12 -0400

    Rewrote boilerplate score segment tests.

--- abjad/boilerplate/example_score/example_score/test/test_segments.py
+++ abjad/boilerplate/example_score/example_score/test/test_segments.py
@@ -8,34 +8,36 @@ import ide
 abjad_ide = ide.tools.idetools.AbjadIDE()
 
 
-this_file = os.path.abspath(__file__)
-test_directory = os.path.dirname(this_file)
-inner_score_directory = os.path.dirname(test_directory)
-outer_score_directory = os.path.dirname(inner_score_directory)
-composer_scores_directory = os.path.dirname(outer_score_directory)
-# Travis monkey patch
-abjad_ide._configuration._composer_scores_directory_override = \
-    composer_scores_directory
-segments_directory = abjad_ide._to_score_directory(this_file, 'segments')
-segment_directories = abjad_ide._list_visible_paths(segments_directory)
+if __name__ == '__main__':
+    this_file = os.path.abspath(__file__)
+    test_directory = os.path.dirname(this_file)
+    inner_score_directory = os.path.dirname(test_directory)
+    outer_score_directory = os.path.dirname(inner_score_directory)
+    composer_scores_directory = os.path.dirname(outer_score_directory)
+    # Travis monkey patch
+    abjad_ide._configuration._composer_scores_directory_override = \
+        composer_scores_directory
+    segments_directory = abjad_ide._to_score_directory(this_file, 'segments')
+    segment_directories = abjad_ide._list_visible_paths(segments_directory)
 
-@pytest.mark.parametrize('segment_directory', segment_directories)
-def test_segments_01(segment_directory):
-    r'''Checks segment definition files.
-    '''
-    try:
-        abjad_ide.check_definition_file(segment_directory)
-    except:
-        traceback.print_exc()
-        sys.exit(1)
+    # not parameterized to print keep-alive message to Travis log
+    for segment_directory in segment_directories:
+        message = 'Checking {} definition file ...'
+        message = message.format(abjad_ide._trim_path(segment_directory))
+        print(message)
+        try:
+            abjad_ide.check_definition_file(segment_directory)
+        except:
+            traceback.print_exc()
+            sys.exit(1)
 
-
-@pytest.mark.parametrize('segment_directory', segment_directories)
-def test_segments_02(segment_directory):
-    r'''Makes segment PDFs.
-    '''
-    try:
-        abjad_ide.make_pdf(segment_directory)
-    except:
-        traceback.print_exc()
-        sys.exit(1)
+    # not parameterized to print keep-alive message to Travis log
+    for segment_directory in segment_directories:
+        message = 'Making {} PDF ...'
+        message = message.format(abjad_ide._trim_path(segment_directory))
+        print(message)
+        try:
+            abjad_ide.make_pdf(segment_directory)
+        except:
+            traceback.print_exc()
+            sys.exit(1)
