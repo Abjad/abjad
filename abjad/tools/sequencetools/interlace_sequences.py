4622901b9e37d668ed3ee6a1f9c71bf804c2da84
Trevor Bača <trevorbaca@gmail.com>
2015-10-28 10:08:01 -0400

    Changed sequencetools.interlace_sequences() signature.
    OLD: sequencetools.interlace_sequences(*sequences).
    NEW: sequencetools.interlace_sequences(sequences).
    
    NOTE. The *args input signature is discouraged.

--- abjad/tools/sequencetools/interlace_sequences.py
+++ abjad/tools/sequencetools/interlace_sequences.py
@@ -1,23 +1,35 @@
 # -*- coding: utf-8 -*-
+import collections
 
 
-def interlace_sequences(*sequences):
+def interlace_sequences(sequences):
     '''Interlaces `sequences`.
 
-    ::
+    ..  container:: example
 
-        >>> k = range(100, 103)
-        >>> l = range(200, 201)
-        >>> m = range(300, 303)
-        >>> n = range(400, 408)
-        >>> sequencetools.interlace_sequences(k, l, m, n)
-        (100, 200, 300, 400, 101, 301, 401, 102, 302, 402, 403, 404, 405, 406, 407)
+        **Example 1.** Interlaces generators:
+
+        ::
+
+            >>> k = range(100, 103)
+            >>> l = range(200, 201)
+            >>> m = range(300, 303)
+            >>> n = range(400, 408)
+            >>> sequencetools.interlace_sequences([k, l, m, n])
+            [100, 200, 300, 400, 101, 301, 401, 102, 302, 402, 403, 404, 405, 406, 407]
 
     Returns list.
     '''
     from abjad.tools import sequencetools
 
+    for sequence in sequences:
+        if not isinstance(sequence, collections.Iterable):
+            message = 'must be iterable: {!r}.'
+            message = message.format(sequence)
+            raise Exception(message)
+
     result = sequencetools.zip_sequences(sequences, truncate=False)
     result = sequencetools.flatten_sequence(result, depth=1)
 
+    assert isinstance(result, list), repr(result)
     return result
--- abjad/tools/sequencetools/test/test_sequencetools_interlace_sequences.py
+++ abjad/tools/sequencetools/test/test_sequencetools_interlace_sequences.py
@@ -8,17 +8,19 @@ def test_sequencetools_interlace_sequences_01():
     l = range(200, 201)
     m = range(300, 303)
     n = range(400, 408)
-    t = sequencetools.interlace_sequences(k, l, m, n)
+    result = sequencetools.interlace_sequences([k, l, m, n])
 
-    assert t == (100, 200, 300, 400, 101, 301, 401, 102, 302, 402, 403, 404, 405, 406, 407)
+    assert result == [
+        100, 200, 300, 400, 101, 301, 401, 102, 302, 
+        402, 403, 404, 405, 406, 407]
 
 
 def test_sequencetools_interlace_sequences_02():
 
-    a = 'introductory'
-    b = 'text'
+    string_1 = 'introductory'
+    string_2 = 'text'
 
-    t = sequencetools.interlace_sequences(a, b)
-    t = ''.join(t)
+    result = sequencetools.interlace_sequences([string_1, string_2])
+    result = ''.join(result)
 
-    assert t == 'itnetxrtoductory'
+    assert result == 'itnetxrtoductory'
