c72f91e114ac5ec2b66cdb9b00b9a539ea58d72b
Trevor Bača <trevorbaca@gmail.com>
2015-10-28 10:14:45 -0400

    Wrote sequencetools.interlace_sequences() examples.

--- abjad/tools/sequencetools/interlace_sequences.py
+++ abjad/tools/sequencetools/interlace_sequences.py
@@ -11,12 +11,53 @@ def interlace_sequences(sequences):
 
         ::
 
-            >>> k = range(100, 103)
-            >>> l = range(200, 201)
-            >>> m = range(300, 303)
-            >>> n = range(400, 408)
-            >>> sequencetools.interlace_sequences([k, l, m, n])
+            >>> sequences = []
+            >>> sequences.append(range(100, 103))
+            >>> sequences.append(range(200, 201))
+            >>> sequences.append(range(300, 303))
+            >>> sequences.append(range(400, 408))
+            >>> sequencetools.interlace_sequences(sequences)
             [100, 200, 300, 400, 101, 301, 401, 102, 302, 402, 403, 404, 405, 406, 407]
+
+    ..  container:: example
+
+        **Example 2.** Interlaces tuples:
+
+        ::
+
+            >>> sequences = []
+            >>> sequences.append(tuple(range(100, 103)))
+            >>> sequences.append(tuple(range(200, 201)))
+            >>> sequences.append(tuple(range(300, 303)))
+            >>> sequences.append(tuple(range(400, 408)))
+            >>> sequencetools.interlace_sequences(sequences)
+            [100, 200, 300, 400, 101, 301, 401, 102, 302, 402, 403, 404, 405, 406, 407]
+
+    ..  container:: example
+
+        **Example 3.** Interlaces lists:
+
+        ::
+
+            >>> sequences = []
+            >>> sequences.append(list(range(100, 103)))
+            >>> sequences.append(list(range(200, 201)))
+            >>> sequences.append(list(range(300, 303)))
+            >>> sequences.append(list(range(400, 408)))
+            >>> sequencetools.interlace_sequences(sequences)
+            [100, 200, 300, 400, 101, 301, 401, 102, 302, 402, 403, 404, 405, 406, 407]
+
+    ..  container:: example
+
+        **Example 4.** Interlaces strings:
+
+        ::
+
+            >>> sequences = []
+            >>> sequences.append('first')
+            >>> sequences.append('second')
+            >>> sequencetools.interlace_sequences(sequences)
+            ['f', 's', 'i', 'e', 'r', 'c', 's', 'o', 't', 'n', 'd']
 
     Returns list.
     '''
