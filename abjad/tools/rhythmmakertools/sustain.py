3a2cc7a3487f2b729d83a5787ecde126d69d6a18
Trevor Bača <trevorbaca@gmail.com>
2015-12-15 13:31:35 -0500

    Added new rhythmmakertools.sustain() factory function.
    The function instantiates an acyclic SustainMask.

---
+++ abjad/tools/rhythmmakertools/sustain.py
@@ -0,0 +1,120 @@
+# -*- coding: utf-8 -*-
+
+
+def sustain(indices):
+    r'''Makes ayclic sustain mask.
+
+    ..  container:: example
+
+        **Example 1.** Sustains divisions 1 and 2:
+
+        ::
+
+            >>> mask = rhythmmakertools.sustain([1, 2])
+
+        ::
+
+            >>> print(format(mask))
+            rhythmmakertools.SustainMask(
+                indices=(1, 2),
+                )
+
+        ::
+
+            >>> maker = rhythmmakertools.NoteRhythmMaker(
+            ...     output_masks=[
+            ...         rhythmmakertools.silence_all(),
+            ...         mask,
+            ...         ],
+            ...     )
+            >>> divisions = [(7, 16), (3, 8), (7, 16), (3, 8)]
+            >>> music = maker(divisions)
+            >>> lilypond_file = rhythmmakertools.make_lilypond_file(
+            ...     music,
+            ...     divisions,
+            ...     )
+            >>> show(lilypond_file) # doctest: +SKIP
+
+        ..  doctest::
+
+            >>> staff = maker._get_rhythmic_staff(lilypond_file)
+            >>> print(format(staff))
+            \new RhythmicStaff {
+                {
+                    \time 7/16
+                    r4..
+                }
+                {
+                    \time 3/8
+                    c'4.
+                }
+                {
+                    \time 7/16
+                    c'4..
+                }
+                {
+                    \time 3/8
+                    r4.
+                }
+            }
+
+    ..  container:: example
+
+        **Example 2.** Sustains divisions -1 and -2:
+
+        ::
+
+            >>> mask = rhythmmakertools.sustain([-1, -2])
+
+        ::
+
+            >>> print(format(mask))
+            rhythmmakertools.SustainMask(
+                indices=(-1, -2),
+                )
+
+        ::
+
+            >>> maker = rhythmmakertools.NoteRhythmMaker(
+            ...     output_masks=[
+            ...         rhythmmakertools.silence_all(),
+            ...         mask,
+            ...         ],
+            ...     )
+            >>> divisions = [(7, 16), (3, 8), (7, 16), (3, 8)]
+            >>> music = maker(divisions)
+            >>> lilypond_file = rhythmmakertools.make_lilypond_file(
+            ...     music,
+            ...     divisions,
+            ...     )
+            >>> show(lilypond_file) # doctest: +SKIP
+
+        ..  doctest::
+
+            >>> staff = maker._get_rhythmic_staff(lilypond_file)
+            >>> print(format(staff))
+            \new RhythmicStaff {
+                {
+                    \time 7/16
+                    r4..
+                }
+                {
+                    \time 3/8
+                    r4.
+                }
+                {
+                    \time 7/16
+                    c'4..
+                }
+                {
+                    \time 3/8
+                    c'4.
+                }
+            }
+
+    Returns sustain mask.
+    '''
+    from abjad.tools import rhythmmakertools
+
+    indices = list(indices)
+    return rhythmmakertools.SustainMask(indices=indices)
