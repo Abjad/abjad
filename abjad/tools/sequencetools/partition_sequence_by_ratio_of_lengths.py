0fe066100cd8a8d4e68e4564598b3b1292c58425
Trevor Bača <trevorbaca@gmail.com>
2015-12-24 15:58:50 -0600

    Cleaned up sequencetools.partition_sequence_by_ratio_of_lengths.

--- abjad/tools/sequencetools/partition_sequence_by_ratio_of_lengths.py
+++ abjad/tools/sequencetools/partition_sequence_by_ratio_of_lengths.py
@@ -2,31 +2,47 @@
 from abjad.tools import mathtools
 
 
-def partition_sequence_by_ratio_of_lengths(sequence, lengths):
-    '''Partitions `sequence` by ratio of `lengths`.
+def partition_sequence_by_ratio_of_lengths(sequence, ratio):
+    '''Partitions `sequence` by `ratio` of lengths.
 
-    ::
+    ..  container:: example
 
-        >>> sequence = tuple(range(10))
+        **Example 1.** Partitions sequence by ``1:1:1`` ratio:
 
-    ::
+        ::
 
-        >>> sequencetools.partition_sequence_by_ratio_of_lengths(
-        ...     sequence,
-        ...     [1, 1, 2],
-        ...     )
-        [(0, 1, 2), (3, 4), (5, 6, 7, 8, 9)]
+            >>> sequence = tuple(range(10))
+            >>> sequencetools.partition_sequence_by_ratio_of_lengths(
+            ...     sequence,
+            ...     [1, 1, 1],
+            ...     )
+            [(0, 1, 2), (3, 4, 5, 6), (7, 8, 9)]
 
-    Uses rounding magic to avoid fractional part lengths.
+    ..  container:: example
+
+        **Example 2.** Partitions sequence by ``1:1:2`` ratio:
+
+        ::
+
+            >>> sequence = tuple(range(10))
+            >>> sequencetools.partition_sequence_by_ratio_of_lengths(
+            ...     sequence,
+            ...     [1, 1, 2],
+            ...     )
+            [(0, 1, 2), (3, 4), (5, 6, 7, 8, 9)]
+
+    Uses the rounding magic implemented in
+    ``mathtools.partition_integer_by_ratio()`` to avoid fractional part
+    lengths.
 
     Returns list of `sequence` objects.
     '''
     from abjad.tools import sequencetools
-
-    lengths = mathtools.partition_integer_by_ratio(len(sequence), lengths)
+    ratio = mathtools.Ratio(ratio)
+    counts = mathtools.partition_integer_by_ratio(len(sequence), ratio)
     return sequencetools.partition_sequence_by_counts(
         sequence,
-        lengths,
+        counts,
         cyclic=False,
-        overhang=False,
+        overhang=Exact,
         )
