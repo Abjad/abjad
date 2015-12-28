d649205322677effba6038dd6f422f6eb6ae818e
Trevor Bača <trevorbaca@gmail.com>
2015-12-27 21:01:32 -0600

    Extended rhythmmakertools.silence() and rhythmmakertools.sustain().
    You can now pass Pattern objects and CompoundObjects to silence() and sustain()
    as input.
    
    EXAMPLE. Selects everything except the first and last elements:
    
        >>> pattern_1 = rhythmmakertools.select_all()
        >>> pattern_2 = rhythmmakertools.select_first()
        >>> pattern_3 = rhythmmakertools.select_last()
        >>> pattern = pattern_1 ^ pattern_2 ^ pattern_3
        >>> mask = rhythmmakertools.silence(pattern)
    
    See the API entries for examples with notation.

--- abjad/tools/rhythmmakertools/silence.py
+++ abjad/tools/rhythmmakertools/silence.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from abjad.tools.topleveltools import new
 
 
 def silence(indices=None, invert=None):
@@ -112,12 +113,160 @@ def silence(indices=None, invert=None):
                 }
             }
 
+    ..  container:: example
+
+        **Example 3.** Works with pattern input:
+
+        ::
+
+            >>> pattern_1 = rhythmmakertools.select_all()
+            >>> pattern_2 = rhythmmakertools.select_first()
+            >>> pattern_3 = rhythmmakertools.select_last()
+            >>> pattern = pattern_1 ^ pattern_2 ^ pattern_3
+            >>> mask = rhythmmakertools.silence(pattern)
+
+        ::
+
+            >>> print(format(mask))
+            rhythmmakertools.SilenceMask(
+                pattern=rhythmmakertools.CompoundPattern(
+                    (
+                        rhythmmakertools.Pattern(
+                            indices=(0,),
+                            period=1,
+                            ),
+                        rhythmmakertools.Pattern(
+                            indices=(0,),
+                            ),
+                        rhythmmakertools.Pattern(
+                            indices=(-1,),
+                            ),
+                        ),
+                    operator='xor',
+                    ),
+                )
+
+        ::
+
+            >>> maker = rhythmmakertools.NoteRhythmMaker(
+            ...     division_masks=[
+            ...         mask,
+            ...         ],
+            ...     )
+            >>> divisions = [(7, 16), (3, 8), (7, 16), (3, 8)]
+            >>> music = maker(divisions)
+            >>> lilypond_file = rhythmmakertools.make_lilypond_file(
+            ...     music,
+            ...     divisions,
+            ...     )
+            >>> show(lilypond_file) # doctest: +SKIP
+
+        ..  doctest::
+
+            >>> staff = maker._get_rhythmic_staff(lilypond_file)
+            >>> print(format(staff))
+            \new RhythmicStaff {
+                {
+                    \time 7/16
+                    c'4..
+                }
+                {
+                    \time 3/8
+                    r4.
+                }
+                {
+                    \time 7/16
+                    r4..
+                }
+                {
+                    \time 3/8
+                    c'4.
+                }
+            }
+
+    ..  container:: example
+
+        **Example 4.** Works with pattern input and invert flag:
+
+        ::
+
+            >>> pattern_1 = rhythmmakertools.select_all()
+            >>> pattern_2 = rhythmmakertools.select_first()
+            >>> pattern_3 = rhythmmakertools.select_last()
+            >>> pattern = pattern_1 ^ pattern_2 ^ pattern_3
+            >>> mask = rhythmmakertools.silence(pattern, invert=True)
+
+        ::
+
+            >>> print(format(mask))
+            rhythmmakertools.SilenceMask(
+                pattern=rhythmmakertools.CompoundPattern(
+                    (
+                        rhythmmakertools.Pattern(
+                            indices=(0,),
+                            period=1,
+                            ),
+                        rhythmmakertools.Pattern(
+                            indices=(0,),
+                            ),
+                        rhythmmakertools.Pattern(
+                            indices=(-1,),
+                            ),
+                        ),
+                    invert=True,
+                    operator='xor',
+                    ),
+                )
+
+        ::
+
+            >>> maker = rhythmmakertools.NoteRhythmMaker(
+            ...     division_masks=[
+            ...         mask,
+            ...         ],
+            ...     )
+            >>> divisions = [(7, 16), (3, 8), (7, 16), (3, 8)]
+            >>> music = maker(divisions)
+            >>> lilypond_file = rhythmmakertools.make_lilypond_file(
+            ...     music,
+            ...     divisions,
+            ...     )
+            >>> show(lilypond_file) # doctest: +SKIP
+
+        ..  doctest::
+
+            >>> staff = maker._get_rhythmic_staff(lilypond_file)
+            >>> print(format(staff))
+            \new RhythmicStaff {
+                {
+                    \time 7/16
+                    r4..
+                }
+                {
+                    \time 3/8
+                    c'4.
+                }
+                {
+                    \time 7/16
+                    c'4..
+                }
+                {
+                    \time 3/8
+                    r4.
+                }
+            }
+
     Returns silence mask.
     '''
     from abjad.tools import rhythmmakertools
     indices = indices or []
-    pattern = rhythmmakertools.Pattern(
-        indices=indices,
-        invert=invert,
-        )
+    prototype = (rhythmmakertools.Pattern, rhythmmakertools.CompoundPattern)
+    if isinstance(indices, prototype):
+        pattern = indices
+    else:
+        pattern = rhythmmakertools.Pattern(
+            indices=indices,
+            invert=invert,
+            )
+    pattern = new(pattern, invert=invert)
     return rhythmmakertools.SilenceMask(pattern=pattern)
--- abjad/tools/rhythmmakertools/sustain.py
+++ abjad/tools/rhythmmakertools/sustain.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
+from abjad.tools.topleveltools import new
 
 
-def sustain(indices, invert=None):
+def sustain(indices=None, invert=None):
     r'''Makes sustain mask that matches `indices`.
 
     ..  container:: example
@@ -116,13 +117,162 @@ def sustain(indices, invert=None):
                 }
             }
 
+    ..  container:: example
+
+        **Example 3.** Works with pattern input:
+
+        ::
+
+            >>> pattern_1 = rhythmmakertools.select_all()
+            >>> pattern_2 = rhythmmakertools.select_first()
+            >>> pattern_3 = rhythmmakertools.select_last()
+            >>> pattern = pattern_1 ^ pattern_2 ^ pattern_3
+            >>> mask = rhythmmakertools.sustain(pattern)
+
+        ::
+
+            >>> print(format(mask))
+            rhythmmakertools.SustainMask(
+                pattern=rhythmmakertools.CompoundPattern(
+                    (
+                        rhythmmakertools.Pattern(
+                            indices=(0,),
+                            period=1,
+                            ),
+                        rhythmmakertools.Pattern(
+                            indices=(0,),
+                            ),
+                        rhythmmakertools.Pattern(
+                            indices=(-1,),
+                            ),
+                        ),
+                    operator='xor',
+                    ),
+                )
+
+        ::
+
+            >>> maker = rhythmmakertools.NoteRhythmMaker(
+            ...     division_masks=[
+            ...         rhythmmakertools.silence_all(),
+            ...         mask,
+            ...         ],
+            ...     )
+            >>> divisions = [(7, 16), (3, 8), (7, 16), (3, 8)]
+            >>> music = maker(divisions)
+            >>> lilypond_file = rhythmmakertools.make_lilypond_file(
+            ...     music,
+            ...     divisions,
+            ...     )
+            >>> show(lilypond_file) # doctest: +SKIP
+
+        ..  doctest::
+
+            >>> staff = maker._get_rhythmic_staff(lilypond_file)
+            >>> print(format(staff))
+            \new RhythmicStaff {
+                {
+                    \time 7/16
+                    r4..
+                }
+                {
+                    \time 3/8
+                    c'4.
+                }
+                {
+                    \time 7/16
+                    c'4..
+                }
+                {
+                    \time 3/8
+                    r4.
+                }
+            }
+
+    ..  container:: example
+
+        **Example 4.** Works with pattern input and invert flag:
+
+        ::
+
+            >>> pattern_1 = rhythmmakertools.select_all()
+            >>> pattern_2 = rhythmmakertools.select_first()
+            >>> pattern_3 = rhythmmakertools.select_last()
+            >>> pattern = pattern_1 ^ pattern_2 ^ pattern_3
+            >>> mask = rhythmmakertools.sustain(pattern, invert=True)
+
+        ::
+
+            >>> print(format(mask))
+            rhythmmakertools.SustainMask(
+                pattern=rhythmmakertools.CompoundPattern(
+                    (
+                        rhythmmakertools.Pattern(
+                            indices=(0,),
+                            period=1,
+                            ),
+                        rhythmmakertools.Pattern(
+                            indices=(0,),
+                            ),
+                        rhythmmakertools.Pattern(
+                            indices=(-1,),
+                            ),
+                        ),
+                    invert=True,
+                    operator='xor',
+                    ),
+                )
+
+        ::
+
+            >>> maker = rhythmmakertools.NoteRhythmMaker(
+            ...     division_masks=[
+            ...         rhythmmakertools.silence_all(),
+            ...         mask,
+            ...         ],
+            ...     )
+            >>> divisions = [(7, 16), (3, 8), (7, 16), (3, 8)]
+            >>> music = maker(divisions)
+            >>> lilypond_file = rhythmmakertools.make_lilypond_file(
+            ...     music,
+            ...     divisions,
+            ...     )
+            >>> show(lilypond_file) # doctest: +SKIP
+
+        ..  doctest::
+
+            >>> staff = maker._get_rhythmic_staff(lilypond_file)
+            >>> print(format(staff))
+            \new RhythmicStaff {
+                {
+                    \time 7/16
+                    c'4..
+                }
+                {
+                    \time 3/8
+                    r4.
+                }
+                {
+                    \time 7/16
+                    r4..
+                }
+                {
+                    \time 3/8
+                    c'4.
+                }
+            }
+
     Returns sustain mask.
     '''
     from abjad.tools import rhythmmakertools
-    indices = list(indices)
-    pattern = rhythmmakertools.Pattern(
-        indices=indices,
-        invert=invert,
-        )
-    mask = rhythmmakertools.SustainMask(pattern=pattern)
-    return mask
+    indices = indices or []
+    prototype = (rhythmmakertools.Pattern, rhythmmakertools.CompoundPattern)
+    if isinstance(indices, prototype):
+        pattern = indices
+    else:
+        pattern = rhythmmakertools.Pattern(
+            indices=indices,
+            invert=invert,
+            )
+    pattern = new(pattern, invert=invert)
+    return rhythmmakertools.SustainMask(pattern=pattern)
