b7d92e043a3033191fab9a786bc6c7cd78109e80
Trevor Bača <trevorbaca@gmail.com>
2015-12-31 13:50:04 -0600

    Updated Container.__setitem__() tests.

--- abjad/tools/scoretools/test/test_scoretools_Container___setitem___integer.py
+++ abjad/tools/scoretools/test/test_scoretools_Container___setitem___integer.py
@@ -4,14 +4,12 @@ from abjad import *
 
 
 def test_scoretools_Container___setitem___integer_01():
-    r'''Spanned leaves exchange correctly.
+    r'''Replaces in-score leaf with out-of-score leaf.
     '''
 
-    voice = Voice("c'8 d'8 e'8 f'8")
-    beam = Beam()
-    attach(beam, voice[:2])
-    glissando = spannertools.Glissando()
-    attach(glissando, voice.select_leaves())
+    voice = Voice("c'8 [ d'8 ] e'8 f'8")
+    leaves = iterate(voice).by_class(scoretools.Leaf)
+    attach(Glissando(), list(leaves))
 
     assert systemtools.TestManager.compare(
         voice,
@@ -25,7 +23,7 @@ def test_scoretools_Container___setitem___integer_01():
         '''
         )
 
-    voice[1] = Note(12, (1, 8))
+    voice[1] = Note("c''8")
 
     assert systemtools.TestManager.compare(
         voice,
@@ -43,14 +41,13 @@ def test_scoretools_Container___setitem___integer_01():
 
 
 def test_scoretools_Container___setitem___integer_02():
-    r'''Spanned leaf hands position over to container correctly.
+    r'''Replaces in-score leaf with out-of-score container.
     '''
 
-    voice = Voice("c'8 d'8 e'8 f'8")
-    beam = Beam()
-    attach(beam, voice[:2])
-    glissando = spannertools.Glissando(allow_repeated_pitches=True)
-    attach(glissando, voice.select_leaves())
+    voice = Voice("c'8 [ d'8 ] e'8 f'8")
+    leaves = iterate(voice).by_class(scoretools.Leaf)
+    glissando = Glissando(allow_repeated_pitches=True)
+    attach(glissando, list(leaves))
 
     assert systemtools.TestManager.compare(
         voice,
@@ -86,15 +83,12 @@ def test_scoretools_Container___setitem___integer_02():
 
 
 def test_scoretools_Container___setitem___integer_03():
-    r'''Directly spanned contains hand over correctly to a single leaf.
-    Note here that only the sequentials are initially spanned.
+    r'''Replaces in-score container with out-of-score leaf.
     '''
 
-    voice = Voice("{ c'8 d'8 } { e'8 f'8 }")
-    beam = Beam()
-    attach(beam, voice[:])
-    glissando = spannertools.Glissando()
-    attach(glissando, voice[:])
+    voice = Voice("{ c'8 [ d'8 } { e'8 f'8 ] }")
+    leaves = iterate(voice).by_class(scoretools.Leaf)
+    attach(Glissando(), list(leaves))
 
     assert systemtools.TestManager.compare(
         voice,
@@ -112,7 +106,7 @@ def test_scoretools_Container___setitem___integer_03():
         '''
         )
 
-    voice[1] = Note(12, (1, 8))
+    voice[1] = Note("c''8")
 
     assert systemtools.TestManager.compare(
         voice,
@@ -131,15 +125,12 @@ def test_scoretools_Container___setitem___integer_03():
 
 
 def test_scoretools_Container___setitem___integer_04():
-    r'''Indirectly spanned containers hand over correctly to a single leaf.
-    Notice here that only LEAVES are initially spanned.
+    r'''Replaces in-score container with out-of-score tuplet.
     '''
 
     voice = Voice("{ c'8 d'8 } { e'8 f'8 }")
-    beam = Beam()
-    attach(beam, voice.select_leaves())
-    glissando = spannertools.Glissando()
-    attach(glissando, voice.select_leaves())
+    attach(Beam(), voice[:])
+    attach(Glissando(), voice[:])
 
     assert systemtools.TestManager.compare(
         voice,
@@ -157,51 +148,7 @@ def test_scoretools_Container___setitem___integer_04():
         '''
         )
 
-    voice[1] = Note(12, (1, 8))
-
-    assert systemtools.TestManager.compare(
-        voice,
-        r'''
-        \new Voice {
-            {
-                c'8 [ \glissando
-                d'8 \glissando
-            }
-            c''8 ]
-        }
-        '''
-        )
-
-    assert inspect_(voice).is_well_formed()
-
-
-def test_scoretools_Container___setitem___integer_05():
-    r'''Directly spanned containers hand over to other containers correctly.
-    '''
-
-    voice = Voice("{ c'8 d'8 } { e'8 f'8 }")
-    beam = Beam()
-    attach(beam, voice[:])
-    glissando = spannertools.Glissando()
-    attach(glissando, voice[:])
-
-    assert systemtools.TestManager.compare(
-        voice,
-        r'''
-        \new Voice {
-            {
-                c'8 [ \glissando
-                d'8 \glissando
-            }
-            {
-                e'8 \glissando
-                f'8 ]
-            }
-        }
-        '''
-        )
-
-    voice[1] = scoretools.FixedDurationTuplet(Duration(2, 8), "c'8 d'8 e'8")
+    voice[1] = Tuplet(Multiplier(2, 3), "c'8 d'8 e'8")
 
     assert systemtools.TestManager.compare(
         voice,
@@ -223,15 +170,13 @@ def test_scoretools_Container___setitem___integer_05():
     assert inspect_(voice).is_well_formed()
 
 
-def test_scoretools_Container___setitem___integer_06():
-    r'''Indirectly spanned containers hand over correctly to a single leaf.
+def test_scoretools_Container___setitem___integer_05():
+    r'''Replaces in-score container with out-of-score leaf.
     '''
 
-    voice = Voice("{ c'8 d'8 } { e'8 f'8 }")
-    beam = Beam()
-    attach(beam, voice.select_leaves())
-    glissando = spannertools.Glissando()
-    attach(glissando, voice.select_leaves())
+    voice = Voice("{ c'8 [ d'8 } { e'8 f'8 ] }")
+    leaves = iterate(voice).by_class(scoretools.Leaf)
+    attach(Glissando(), list(leaves))
 
     assert systemtools.TestManager.compare(
         voice,
@@ -249,7 +194,7 @@ def test_scoretools_Container___setitem___integer_06():
         '''
         )
 
-    voice[1] = Note(12, (1, 8))
+    voice[1] = Note("c''8")
 
     assert systemtools.TestManager.compare(
         voice,
@@ -267,15 +212,14 @@ def test_scoretools_Container___setitem___integer_06():
     assert inspect_(voice).is_well_formed()
 
 
-def test_scoretools_Container___setitem___integer_07():
-    r'''Indirectly HALF-spanned containers hand over correctly to a
-    single leaf. WOW!
+def test_scoretools_Container___setitem___integer_06():
+    r'''Replaces in-score container with out-of-score leaf.
     '''
 
     voice = Voice(2 * Container("c'8 c'8 c'8 c'8"))
     voice = Voice("{ c'8 d'8 e'8 f'8 } { g'8 a'8 b'8 c''8 }")
-    beam = Beam()
-    attach(beam, voice.select_leaves()[0:6])
+    leaves = iterate(voice).by_class(scoretools.Leaf)
+    attach(Beam(), list(leaves)[0:6])
 
     assert systemtools.TestManager.compare(
         voice,
@@ -297,7 +241,7 @@ def test_scoretools_Container___setitem___integer_07():
         '''
         )
 
-    voice[1] = Rest((1, 2))
+    voice[1] = Rest('r2')
 
     assert systemtools.TestManager.compare(
         voice,
@@ -317,94 +261,20 @@ def test_scoretools_Container___setitem___integer_07():
     assert inspect_(voice).is_well_formed()
 
 
-def test_scoretools_Container___setitem___integer_08():
-    r'''Take spanned leaf from donor container
-    and insert into recipient container.
-    Both donor and recipient check after set item.
-    '''
-
-    notes = [Note("c'8"), Note("d'8"), Note("e'8"), Note("f'8"), Note("g'8"), Note("a'8")]
-
-    voice = Voice(notes[:3])
-    beam = Beam()
-    attach(beam, voice[:])
-
-    r'''
-    \new Voice {
-        c'8 [
-        d'8
-        e'8 ]
-    }
-    '''
-
-    u = Voice(notes[3:])
-    beam = Beam()
-    attach(beam, u[:])
-
-    r'''
-    \new Voice {
-        f'8 [
-        g'8
-        a'8 ]
-    }
-    '''
-
-    voice[1] = u[1]
-
-    "Modified voice:"
-
-    r'''
-    \new Voice {
-        c'8 [
-        g'8
-        e'8 ]
-    }
-    '''
-
-    assert inspect_(voice).is_well_formed()
-
-    assert systemtools.TestManager.compare(
-        voice,
-        r'''
-        \new Voice {
-            c'8 [
-            g'8
-            e'8 ]
-        }
-        '''
-        )
-
-    "Modified u:"
-
-    assert systemtools.TestManager.compare(
-        u,
-        r'''
-        \new Voice {
-            f'8 [
-            a'8 ]
-        }
-        '''
-        )
-
-    assert inspect_(u).is_well_formed()
-
-
-def test_scoretools_Container___setitem___integer_09():
-    r'''Take down-spanned container with completely covered spanner
-    from donor container and insert into recipient container.
-    Both donor and recipient check after set item.
+def test_scoretools_Container___setitem___integer_07():
+    r'''Replaces note in one score with note from another score.
     '''
 
     notes = [
-        Note("c'8"), Note("d'8"), Note("e'8"),
-        Note("f'8"), Note("g'8"), Note("a'8"), Note("b'8"),
+        Note("c'8"), Note("d'8"), Note("e'8"), 
+        Note("f'8"), Note("g'8"), Note("a'8"),
         ]
-    voice = Voice(notes[:3])
-    beam = Beam()
-    attach(beam, voice[:])
+
+    voice_1 = Voice(notes[:3])
+    attach(Beam(), voice_1[:])
 
     assert systemtools.TestManager.compare(
-        voice,
+        voice_1,
         r'''
         \new Voice {
             c'8 [
@@ -414,15 +284,79 @@ def test_scoretools_Container___setitem___integer_09():
         '''
         )
 
-    u = Voice(notes[3:])
-    Container(u[1:3])
-    glissando = spannertools.Glissando()
-    attach(glissando, u.select_leaves())
-    slur = Slur()
-    attach(slur, u[1].select_leaves())
+    voice_2 = Voice(notes[3:])
+    attach(Beam(), voice_2[:])
 
     assert systemtools.TestManager.compare(
-        u,
+        voice_2,
+        r'''
+        \new Voice {
+            f'8 [
+            g'8
+            a'8 ]
+        }
+        '''
+        )
+
+    voice_1[1] = voice_2[1]
+
+    assert systemtools.TestManager.compare(
+        voice_1,
+        r'''
+        \new Voice {
+            c'8 [
+            g'8
+            e'8 ]
+        }
+        '''
+        )
+
+    assert inspect_(voice_1).is_well_formed()
+
+    assert systemtools.TestManager.compare(
+        voice_2,
+        r'''
+        \new Voice {
+            f'8 [
+            a'8 ]
+        }
+        '''
+        )
+
+    assert inspect_(voice_2).is_well_formed()
+
+
+def test_scoretools_Container___setitem___integer_08():
+    r'''Replaces note in one score with container from another score.
+    '''
+
+    notes = [
+        Note("c'8"), Note("d'8"), Note("e'8"),
+        Note("f'8"), Note("g'8"), Note("a'8"), Note("b'8"),
+        ]
+    voice_1 = Voice(notes[:3])
+    attach(Beam(), voice_1[:])
+
+    assert systemtools.TestManager.compare(
+        voice_1,
+        r'''
+        \new Voice {
+            c'8 [
+            d'8
+            e'8 ]
+        }
+        '''
+        )
+
+    voice_2 = Voice(notes[3:])
+    Container(voice_2[1:3])
+    leaves = iterate(voice_2).by_class(scoretools.Leaf)
+    attach(Glissando(), list(leaves))
+    leaves = iterate(voice_2[1]).by_class(scoretools.Leaf)
+    attach(Slur(), list(leaves))
+
+    assert systemtools.TestManager.compare(
+        voice_2,
         r'''
         \new Voice {
             f'8 \glissando
@@ -435,12 +369,10 @@ def test_scoretools_Container___setitem___integer_09():
         '''
         )
 
-    voice[1] = u[1]
-
-    "Voice voice is now ..."
+    voice_1[1] = voice_2[1]
 
     assert systemtools.TestManager.compare(
-        voice,
+        voice_1,
         r'''
         \new Voice {
             c'8 [
@@ -453,13 +385,10 @@ def test_scoretools_Container___setitem___integer_09():
         '''
         )
 
-    assert inspect_(voice).is_well_formed()
+    assert inspect_(voice_1).is_well_formed()
 
-    "Voice u is now ..."
-
-    assert inspect_(u).is_well_formed()
     assert systemtools.TestManager.compare(
-        u,
+        voice_2,
         r'''
         \new Voice {
             f'8 \glissando
@@ -467,3 +396,5 @@ def test_scoretools_Container___setitem___integer_09():
         }
         '''
         )
+
+    assert inspect_(voice_2).is_well_formed()
