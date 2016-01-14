e75b7699caccbe9434f0964a13577c2654f8b447
Trevor Bača <trevorbaca@gmail.com>
2016-01-14 15:33:21 -0600

    Extended Score.add_final_markup() extra_offset=None keyword.
    OLD. Keyword did not work correctly when last leaf in score was a multimeasure
    rest.
    
    NEW. Keyword now works correctly whether last leaf in score is note, rest,
    chord or multimeasure rest.

--- abjad/tools/scoretools/Score.py
+++ abjad/tools/scoretools/Score.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import copy
 from abjad.tools.topleveltools import attach
 from abjad.tools.topleveltools import iterate
 from abjad.tools.topleveltools import override
@@ -125,63 +126,99 @@ class Score(Context):
         return double_bar
 
     def add_final_markup(self, markup, extra_offset=None):
-        r'''Adds `markup` to end of score:
+        r'''Adds `markup` to end of score.
 
-        ::
+        ..  container:: example
 
-            >>> staff = Staff("c'4 d'4 e'4 f'4")
-            >>> score = Score([staff])
-            >>> place = Markup('Bremen - Boston - LA.', direction=Down)
-            >>> date = Markup('Jul 2010 - May 2011.')
-            >>> markup = Markup.right_column([place, date], direction=Down)
-            >>> markup = markup.italic()
-            >>> markup = score.add_final_markup(markup, extra_offset=(0.5, -2))
+            **Example 1.** Adds markup to last leaf:
 
-        ::
+            ::
 
-            >>> print(format(markup))
-            _ \markup {
-                \italic
-                    \right-column
-                        {
-                            "Bremen - Boston - LA."
-                            "Jul 2010 - May 2011."
-                        }
-                }
+                >>> staff = Staff("c'4 d'4 e'4 f'4")
+                >>> score = Score([staff])
+                >>> place = Markup('Bremen - Boston - LA.', direction=Down)
+                >>> date = Markup('July 2010 - May 2011.')
+                >>> markup = Markup.right_column([place, date], direction=Down)
+                >>> markup = markup.italic()
+                >>> markup = score.add_final_markup(
+                ...     markup,
+                ...     extra_offset=(0.5, -2),
+                ...     )
+                >>> show(staff) # doctest: +SKIP
 
-        ..  doctest::
+            ..  doctest::
 
-            >>> print(format(score))
-            \new Score <<
-                \new Staff {
-                    c'4
-                    d'4
-                    e'4
-                    \once \override TextScript #'extra-offset = #'(0.5 . -2)
-                    f'4 _ \markup {
-                        \italic
-                            \right-column
-                                {
-                                    "Bremen - Boston - LA."
-                                    "Jul 2010 - May 2011."
+                >>> print(format(score))
+                \new Score <<
+                    \new Staff {
+                        c'4
+                        d'4
+                        e'4
+                        \once \override TextScript #'extra-offset = #'(0.5 . -2)
+                        f'4 _ \markup {
+                            \italic
+                                \right-column
+                                    {
+                                        "Bremen - Boston - LA."
+                                        "July 2010 - May 2011."
+                                    }
+                            }
+                    }
+                >>
+
+        ..  container:: example
+
+            **Example 2.** Adds markup to last multimeasure rest:
+
+            ::
+
+                >>> staff = Staff("c'4 d'4 e'4 f'4")
+                >>> staff.append(MultimeasureRest(Duration(4, 4)))
+                >>> score = Score([staff])
+                >>> place = Markup('Bremen - Boston - LA.', direction=Down)
+                >>> date = Markup('July 2010 - May 2011.')
+                >>> markup = Markup.right_column([place, date], direction=Down)
+                >>> markup = markup.italic()
+                >>> markup = score.add_final_markup(
+                ...     markup,
+                ...     extra_offset=(14.5, -2),
+                ...     )
+                >>> show(staff) # doctest: +SKIP
+
+            ..  doctest::
+
+                >>> print(format(score))
+                \new Score <<
+                    \new Staff {
+                        c'4
+                        d'4
+                        e'4
+                        f'4
+                        \once \override MultiMeasureRestText #'extra-offset = #'(14.5 . -2)
+                        R1
+                            _ \markup {
+                                \italic
+                                    \right-column
+                                        {
+                                            "Bremen - Boston - LA."
+                                            "July 2010 - May 2011."
+                                        }
                                 }
-                        }
-                }
-            >>
+                    }
+                >>
 
-        ::
-
-            >>> show(staff) # doctest: +SKIP
-
-        Returns `markup`.
+        Returns none.
         '''
         from abjad.tools import markuptools
         from abjad.tools import scoretools
         selection = select(self)
         last_leaf = selection._get_component(scoretools.Leaf, -1)
-        # TODO: copy markup direction from markup input
-        markup = markup = markuptools.Markup(markup, Down)
+        markup = copy.copy(markup)
         attach(markup, last_leaf)
         if extra_offset is not None:
-            override(last_leaf).text_script.extra_offset = extra_offset
+            if isinstance(last_leaf, scoretools.MultimeasureRest):
+                grob_proxy = override(last_leaf).multi_measure_rest_text
+            else:
+                grob_proxy = override(last_leaf).text_script
+            grob_proxy.extra_offset = extra_offset
         return markup
