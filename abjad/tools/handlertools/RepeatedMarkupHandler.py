341f856705adaa0fbb784b5f9f3c72cacdcbc791
Trevor Bača <trevor.baca@gmail.com>
2016-02-14 15:30:40 -0600

    Updated RepeatedMarkupHandler in handlertools.

--- abjad/tools/handlertools/RepeatedMarkupHandler.py
+++ abjad/tools/handlertools/RepeatedMarkupHandler.py
@@ -1,11 +1,40 @@
 # -*- coding: utf-8 -*-
+from abjad.tools import datastructuretools
 from abjad.tools import markuptools
 from abjad.tools import scoretools
 from abjad.tools.handlertools.ArticulationHandler import ArticulationHandler
+from abjad.tools.topleveltools import attach
+from abjad.tools.topleveltools import iterate
 
 
 class RepeatedMarkupHandler(ArticulationHandler):
     r'''Repeated markup handler.
+
+    ..  container:: example
+
+        **Example 1.** Attaches markup to every leaf:
+
+        ::
+
+            >>> handler = handlertools.RepeatedMarkupHandler(
+            ...     markups=[Markup('sec.', direction=Up)],
+            ...     )
+            >>> staff = Staff("c'4 d' e' f'")
+            >>> logical_ties = iterate(staff).by_logical_tie(pitched=True)
+            >>> logical_ties = list(logical_ties)
+            >>> handler(logical_ties)
+            >>> show(staff)  # doctest: +SKIP
+
+        ..  doctest::
+
+            >>> f(staff)
+            \new Staff {
+                c'4 ^ \markup { sec. }
+                d'4 ^ \markup { sec. }
+                e'4 ^ \markup { sec. }
+                f'4 ^ \markup { sec. }
+            }
+
     '''
 
     ### CLASS ATTRIBUTES ###
@@ -24,18 +53,16 @@ class RepeatedMarkupHandler(ArticulationHandler):
 
     ### SPECIAL METHODS ###
 
-    def __call__(self, expr):
-        r'''Calls handler on `expr`.
+    def __call__(self, logical_ties):
+        r'''Calls handler on `logical_ties`.
 
         Returns none.
         '''
-        classes = (scoretools.Note, scoretools.Chord)
         markups = datastructuretools.CyclicTuple(self.markups)
-        for i, leaf in  enumerate(
-            scoretools.iterate_components_forward_in_expr(expr, classes)):
-            markup = markup[i]
+        for i, logical_tie in enumerate(logical_ties):
+            markup = markups[i]
             markup = markuptools.Markup(markup)
-            markup(leaf)
+            attach(markup, logical_tie.head)
 
     ### PUBLIC PROPERTIES ###
 
