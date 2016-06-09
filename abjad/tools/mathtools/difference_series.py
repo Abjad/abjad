edb0380316fe2fa6984b302b589e000e8f36a753
Trevor Bača <trevor.baca@gmail.com>
2016-06-09 12:25:22 -0500

    Extended leaf multiplier behavior. NEW.
    OLD. Previously you could only attach Multiplier objects to leaves:
    
        >>> attach(Multiplier(3, 2), Note("c'4"))
    
    NEW. Now you may also attach NonreducedFraction object to leaves:
    
        >>> attach(NonreducedFraction(6, 4), Note("c'4"))
    
    The mathematical behavior of NonreducedFraction multipliers is the same as
    Multiplier multipliers.
    
    The difference lies in LilyPond output. Use NonreducedFraction objects as leaf
    duration multipliers when you want LilyPond output to contain fractions that
    are not reduced (eg, for easy visual comparison of many multipliers with the
    same denominator).

--- abjad/tools/mathtools/difference_series.py
+++ abjad/tools/mathtools/difference_series.py
@@ -2,19 +2,25 @@
 
 
 def difference_series(sequence):
-    r'''Difference series of `sequence`.
+    r'''Gets difference series of `sequence`.
+
+    **Example 1.** Monotonically increasing:
 
     ::
 
         >>> mathtools.difference_series([1, 1, 2, 3, 5, 5, 6])
         [0, 1, 1, 2, 0, 1]
 
+    **Example 2.** Alternating direction:
+
+    ::
+
+        >>> mathtools.difference_series([9, 6, 8, 5, 7, 4, 6])
+        [-3, 2, -3, 2, -3, 2]
+
     Returns list.
     '''
-
     result = []
-
     for i, n in enumerate(sequence[1:]):
         result.append(n - sequence[i])
-
     return result
