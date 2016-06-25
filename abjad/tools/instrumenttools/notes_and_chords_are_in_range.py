069e2573286e12eeec9403902f9b9831697ca299
Trevor Bača <trevor.baca@gmail.com>
2016-06-25 11:23:30 -0500

    Improved instrumenttools API examples.

--- abjad/tools/instrumenttools/notes_and_chords_are_in_range.py
+++ abjad/tools/instrumenttools/notes_and_chords_are_in_range.py
@@ -6,41 +6,49 @@ from abjad.tools.topleveltools import iterate
 
 
 def notes_and_chords_are_in_range(expr):
-    '''Is true when notes and chords in `expr` are
-    within traditional instrument ranges.
+    '''Is true when notes and chords in `expr` are within traditional
+    instrument ranges.
 
-    ::
+    ..  todo:: Move to WellformednessManager.
 
-        >>> staff = Staff("c'8 r8 <d' fs'>8 r8")
-        >>> violin = instrumenttools.Violin()
-        >>> attach(violin, staff)
+    ..  container:: example
 
-    ::
+        **Example 1.** In range:
 
-        >>> instrumenttools.notes_and_chords_are_in_range(staff)
-        True
+        ::
 
-    Otherwise false:
+            >>> staff = Staff("c'8 r8 <d' fs'>8 r8")
+            >>> violin = instrumenttools.Violin()
+            >>> attach(violin, staff)
+            >>> show(staff) # doctest: +SKIP
 
-    ::
+        ::
 
-        >>> staff = Staff("c'8 r8 <d fs>8 r8")
-        >>> violin = instrumenttools.Violin()
-        >>> attach(violin, staff)
+            >>> instrumenttools.notes_and_chords_are_in_range(staff)
+            True
 
-    ::
+    ..  container:: example
 
-        >>> instrumenttools.notes_and_chords_are_in_range(staff)
-        False
+        **Example 2.** Out of range:
+
+        ::
+
+            >>> staff = Staff("c'8 r8 <d fs>8 r8")
+            >>> violin = instrumenttools.Violin()
+            >>> attach(violin, staff)
+            >>> show(staff) # doctest: +SKIP
+
+        ::
+
+            >>> instrumenttools.notes_and_chords_are_in_range(staff)
+            False
 
     Returns true or false.
     '''
     from abjad.tools import instrumenttools
-
-    for note_or_chord in iterate(expr).by_class(
-        (scoretools.Note, scoretools.Chord)):
-        instrument = note_or_chord._get_effective(
-            instrumenttools.Instrument)
+    prototype = (scoretools.Note, scoretools.Chord)
+    for note_or_chord in iterate(expr).by_class(prototype):
+        instrument = note_or_chord._get_effective(instrumenttools.Instrument)
         if note_or_chord not in instrument.pitch_range:
             return False
     else:
