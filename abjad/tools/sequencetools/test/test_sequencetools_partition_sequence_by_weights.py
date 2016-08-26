e255890ec3bc235d438f20d0265164390cec9513
Trevor Bača <trevor.baca@gmail.com>
2016-08-26 15:53:00 -0500

    Wrote sequencetools.partition_sequence_by_weights() tests.

--- abjad/tools/sequencetools/test/test_sequencetools_partition_sequence_by_weights.py
+++ abjad/tools/sequencetools/test/test_sequencetools_partition_sequence_by_weights.py
@@ -4,151 +4,227 @@ from abjad import *
 
 def test_sequencetools_partition_sequence_by_weights_01():
 
-    sequence = [3, 3, 3, 3, 4, 4, 4, 4, 5, 5]
-    groups = sequencetools.partition_sequence_by_weights(
-        sequence,
+    list_ = [3, 3, 3, 3, 4, 4, 4, 4, 5, 5]
+    parts = sequencetools.partition_sequence_by_weights(
+        list_,
         [3, 9],
         cyclic=False,
         overhang=True,
         )
-    assert groups == [[3], [3, 3, 3], [4, 4, 4, 4, 5, 5]]
+    assert parts == [[3], [3, 3, 3], [4, 4, 4, 4, 5, 5]]
+
+    list_ = [-1 * _ for _ in list_]
+    parts = sequencetools.partition_sequence_by_weights(
+        list_,
+        [3, 9],
+        cyclic=False,
+        overhang=True,
+        )
+    assert parts == [[-3], [-3, -3, -3], [-4, -4, -4, -4, -5, -5]]
 
 
 def test_sequencetools_partition_sequence_by_weights_02():
 
-    sequence = [3, 3, 3, 3, 4, 4, 4, 4, 5, 5]
-    groups = sequencetools.partition_sequence_by_weights(
-        sequence,
+    list_ = [3, 3, 3, 3, 4, 4, 4, 4, 5, 5]
+    parts = sequencetools.partition_sequence_by_weights(
+        list_,
         [3, 9],
         cyclic=False,
         overhang=False,
         )
-    assert groups == [[3], [3, 3, 3]]
+    assert parts == [[3], [3, 3, 3]]
+
+    list_ = [-1 * _ for _ in list_]
+    parts = sequencetools.partition_sequence_by_weights(
+        list_,
+        [3, 9],
+        cyclic=False,
+        overhang=False,
+        )
+    assert parts == [[-3], [-3, -3, -3]]
 
 
 def test_sequencetools_partition_sequence_by_weights_03():
 
-    sequence = [3, 3, 3, 3, 4, 4, 4, 4, 5]
-    groups = sequencetools.partition_sequence_by_weights(
-        sequence,
+    list_ = [3, 3, 3, 3, 4, 4, 4, 4, 5]
+    parts = sequencetools.partition_sequence_by_weights(
+        list_,
         [12],
         cyclic=True,
         overhang=True,
         )
-    assert groups == [[3, 3, 3, 3], [4, 4, 4], [4, 5]]
+    assert parts == [[3, 3, 3, 3], [4, 4, 4], [4, 5]]
+
+    list_ = [-1 * _ for _ in list_]
+    parts = sequencetools.partition_sequence_by_weights(
+        list_,
+        [12],
+        cyclic=True,
+        overhang=True,
+        )
+    assert parts == [[-3, -3, -3, -3], [-4, -4, -4], [-4, -5]]
 
 
 def test_sequencetools_partition_sequence_by_weights_04():
 
-    sequence = [3, 3, 3, 3, 4, 4, 4, 4, 5]
-    groups = sequencetools.partition_sequence_by_weights(
-        sequence,
+    list_ = [3, 3, 3, 3, 4, 4, 4, 4, 5]
+    parts = sequencetools.partition_sequence_by_weights(
+        list_,
         [12],
         cyclic=True,
         overhang=False,
         )
-    assert groups == [[3, 3, 3, 3], [4, 4, 4]]
+    assert parts == [[3, 3, 3, 3], [4, 4, 4]]
+
+    list_ = [-1 * _ for _ in list_]
+    parts = sequencetools.partition_sequence_by_weights(
+        list_,
+        [12],
+        cyclic=True,
+        overhang=False,
+        )
+    assert parts == [[-3, -3, -3, -3], [-4, -4, -4]]
 
 
 def test_sequencetools_partition_sequence_by_weights_05():
 
-    sequence = [3, 3, 3, 3, 4, 4, 4, 4, 5, 5]
-    groups = sequencetools.partition_sequence_by_weights(
-        sequence,
+    list_ = [3, 3, 3, 3, 4, 4, 4, 4, 5, 5]
+    parts = sequencetools.partition_sequence_by_weights(
+        list_,
         [10, 4],
         cyclic=False,
         overhang=True,
         allow_part_weights=Less,
         )
-    assert groups == [[3, 3, 3], [3], [4, 4, 4, 4, 5, 5]]
+    assert parts == [[3, 3, 3], [3], [4, 4, 4, 4, 5, 5]]
 
 
 def test_sequencetools_partition_sequence_by_weights_06():
 
-    sequence = [3, 3, 3, 3, 4, 4, 4, 4, 5, 5]
-    groups = sequencetools.partition_sequence_by_weights(
-        sequence,
+    list_ = [3, 3, 3, 3, 4, 4, 4, 4, 5, 5]
+    parts = sequencetools.partition_sequence_by_weights(
+        list_,
         [10, 4],
         cyclic=False,
         overhang=False,
         allow_part_weights=Less,
         )
-    assert groups == [[3, 3, 3], [3]]
+    assert parts == [[3, 3, 3], [3]]
 
 
 def test_sequencetools_partition_sequence_by_weights_07():
 
-    sequence = [3, 3, 3, 3, 4, 4, 4, 4, 5, 5]
-    groups = sequencetools.partition_sequence_by_weights(
-        sequence,
+    list_ = [3, 3, 3, 3, 4, 4, 4, 4, 5, 5]
+    parts = sequencetools.partition_sequence_by_weights(
+        list_,
         [10, 5],
         cyclic=True,
         overhang=True,
         allow_part_weights=Less,
         )
-    assert groups == [[3, 3, 3], [3], [4, 4], [4], [4, 5], [5]]
+    assert parts == [[3, 3, 3], [3], [4, 4], [4], [4, 5], [5]]
 
 
 def test_sequencetools_partition_sequence_by_weights_08():
 
-    sequence = [3, 3, 3, 3, 4, 4, 4, 4, 5]
-    groups = sequencetools.partition_sequence_by_weights(
-        sequence,
+    list_ = [3, 3, 3, 3, 4, 4, 4, 4, 5]
+    parts = sequencetools.partition_sequence_by_weights(
+        list_,
         [10, 5],
         cyclic=True,
         overhang=False,
         allow_part_weights=Less,
         )
-    assert groups == [[3, 3, 3], [3], [4, 4], [4]]
+    assert parts == [[3, 3, 3], [3], [4, 4], [4]]
 
 
 def test_sequencetools_partition_sequence_by_weights_09():
 
-    sequence = [3, 3, 3, 3, 4, 4, 4, 4, 5, 5]
-    groups = sequencetools.partition_sequence_by_weights(
-        sequence,
+    list_ = [3, 3, 3, 3, 4, 4, 4, 4, 5, 5]
+    parts = sequencetools.partition_sequence_by_weights(
+        list_,
         [10, 4],
         cyclic=False,
         overhang=True,
         allow_part_weights=More,
         )
-    assert groups == [[3, 3, 3, 3], [4], [4, 4, 4, 5, 5]]
+    assert parts == [[3, 3, 3, 3], [4], [4, 4, 4, 5, 5]]
+
+    list_ = [-1 * _ for _ in list_]
+    parts = sequencetools.partition_sequence_by_weights(
+        list_,
+        [10, 4],
+        cyclic=False,
+        overhang=True,
+        allow_part_weights=More,
+        )
+    assert parts == [[-3, -3, -3, -3], [-4], [-4, -4, -4, -5, -5]]
 
 
 def test_sequencetools_partition_sequence_by_weights_10():
 
-    sequence = [3, 3, 3, 3, 4, 4, 4, 4, 5, 5]
-    groups = sequencetools.partition_sequence_by_weights(
-        sequence,
+    list_ = [3, 3, 3, 3, 4, 4, 4, 4, 5, 5]
+    parts = sequencetools.partition_sequence_by_weights(
+        list_,
         [10, 4],
         cyclic=False,
         overhang=False,
         allow_part_weights=More,
         )
-    assert groups == [[3, 3, 3, 3], [4]]
+    assert parts == [[3, 3, 3, 3], [4]]
+
+    list_ = [-1 * _ for _ in list_]
+    parts = sequencetools.partition_sequence_by_weights(
+        list_,
+        [10, 4],
+        cyclic=False,
+        overhang=False,
+        allow_part_weights=More,
+        )
+    assert parts == [[-3, -3, -3, -3], [-4]]
 
 
 def test_sequencetools_partition_sequence_by_weights_11():
 
-    sequence = [3, 3, 3, 3, 4, 4, 4, 4, 5, 5]
-    groups = sequencetools.partition_sequence_by_weights(
-        sequence,
+    list_ = [3, 3, 3, 3, 4, 4, 4, 4, 5, 5]
+    parts = sequencetools.partition_sequence_by_weights(
+        list_,
         [10, 4],
         cyclic=True,
         overhang=True,
         allow_part_weights=More,
         )
-    assert groups == [[3, 3, 3, 3], [4], [4, 4, 4], [5], [5]]
+    assert parts == [[3, 3, 3, 3], [4], [4, 4, 4], [5], [5]]
+
+    list_ = [-1 * _ for _ in list_]
+    parts = sequencetools.partition_sequence_by_weights(
+        list_,
+        [10, 4],
+        cyclic=True,
+        overhang=True,
+        allow_part_weights=More,
+        )
+    assert parts == [[-3, -3, -3, -3], [-4], [-4, -4, -4], [-5], [-5]]
 
 
 def test_sequencetools_partition_sequence_by_weights_12():
 
-    sequence = [3, 3, 3, 3, 4, 4, 4, 4, 5, 5]
-    groups = sequencetools.partition_sequence_by_weights(
-        sequence,
+    list_ = [3, 3, 3, 3, 4, 4, 4, 4, 5, 5]
+    parts = sequencetools.partition_sequence_by_weights(
+        list_,
         [10, 4],
         cyclic=True,
         overhang=False,
         allow_part_weights=More,
         )
-    assert groups == [[3, 3, 3, 3], [4], [4, 4, 4], [5]]
+    assert parts == [[3, 3, 3, 3], [4], [4, 4, 4], [5]]
+
+    list_ = [-1 * _ for _ in list_]
+    parts = sequencetools.partition_sequence_by_weights(
+        list_,
+        [10, 4],
+        cyclic=True,
+        overhang=False,
+        allow_part_weights=More,
+        )
+    assert parts == [[-3, -3, -3, -3], [-4], [-4, -4, -4], [-5]]
