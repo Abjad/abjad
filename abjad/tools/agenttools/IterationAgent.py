a4e372f5a12787bad023418831331b93fe886bf2
Trevor Bača <trevor.baca@gmail.com>
2016-10-22 15:10:38 -0500

    Added IterationAgent.by_pitch_pair().
    This deprecates pitchtools.iterate_named_pitch_pairs_in_expr().
    
    Iteration methods should implement on IterationAgent.
    
    Also, package-level functions are discouraged; methods are preferred.

--- abjad/tools/agenttools/IterationAgent.py
+++ abjad/tools/agenttools/IterationAgent.py
@@ -1674,6 +1674,123 @@ class IterationAgent(abctools.AbjadObject):
                     if x._get_parentage().logical_voice == signature:
                         yield x
 
+    def by_pitch_pair(self):
+        r'''Iterates by pitch pair.
+
+        ..  container:: example
+
+            **Example 1.** With notes:
+
+            ::
+
+                >>> score = Score([])
+                >>> notes = [Note("c'8"), Note("d'8"), Note("e'8"), Note("f'8"), Note("g'4")]
+                >>> score.append(Staff(notes))
+                >>> notes = [Note(x, (1, 4)) for x in [-12, -15, -17]]
+                >>> score.append(Staff(notes))
+                >>> clef = Clef('bass')
+                >>> attach(clef, score[1])
+                >>> show(score) # doctest: +SKIP
+
+            ..  doctest::
+
+                >>> f(score)
+                \new Score <<
+                    \new Staff {
+                        c'8
+                        d'8
+                        e'8
+                        f'8
+                        g'4
+                    }
+                    \new Staff {
+                        \clef "bass"
+                        c4
+                        a,4
+                        g,4
+                    }
+                >>
+
+            ::
+
+                >>> for pair in iterate(score).by_pitch_pair():
+                ...     pair
+                ...
+                (NamedPitch("c'"), NamedPitch('c'))
+                (NamedPitch("c'"), NamedPitch("d'"))
+                (NamedPitch('c'), NamedPitch("d'"))
+                (NamedPitch("d'"), NamedPitch("e'"))
+                (NamedPitch("d'"), NamedPitch('a,'))
+                (NamedPitch('c'), NamedPitch("e'"))
+                (NamedPitch('c'), NamedPitch('a,'))
+                (NamedPitch("e'"), NamedPitch('a,'))
+                (NamedPitch("e'"), NamedPitch("f'"))
+                (NamedPitch('a,'), NamedPitch("f'"))
+                (NamedPitch("f'"), NamedPitch("g'"))
+                (NamedPitch("f'"), NamedPitch('g,'))
+                (NamedPitch('a,'), NamedPitch("g'"))
+                (NamedPitch('a,'), NamedPitch('g,'))
+                (NamedPitch("g'"), NamedPitch('g,'))
+
+        ..  container:: example
+
+            **Example 2.** With chords:
+
+            ::
+
+                >>> chord_1 = Chord([0, 2, 4], (1, 4))
+                >>> chord_2 = Chord([17, 19], (1, 4))
+                >>> staff = Staff([chord_1, chord_2])
+
+            ..  doctest::
+
+                >>> f(staff)
+                \new Staff {
+                    <c' d' e'>4
+                    <f'' g''>4
+                }
+
+            ::
+
+                >>> for pair in iterate(staff).by_pitch_pair():
+                ...     pair
+                ...
+                (NamedPitch("c'"), NamedPitch("d'"))
+                (NamedPitch("c'"), NamedPitch("e'"))
+                (NamedPitch("d'"), NamedPitch("e'"))
+                (NamedPitch("c'"), NamedPitch("f''"))
+                (NamedPitch("c'"), NamedPitch("g''"))
+                (NamedPitch("d'"), NamedPitch("f''"))
+                (NamedPitch("d'"), NamedPitch("g''"))
+                (NamedPitch("e'"), NamedPitch("f''"))
+                (NamedPitch("e'"), NamedPitch("g''"))
+                (NamedPitch("f''"), NamedPitch("g''"))
+
+        Returns generator.
+        '''
+        from abjad.tools import pitchtools
+        for leaf_pair in self.by_leaf_pair():
+            leaf_pair_list = list(leaf_pair)
+            # iterate chord pitches if first leaf is chord
+            for pair in pitchtools.list_unordered_named_pitch_pairs_in_expr(
+                leaf_pair_list[0]):
+                yield pair
+            if isinstance(leaf_pair, set):
+                for pair in pitchtools.list_unordered_named_pitch_pairs_in_expr(
+                    leaf_pair):
+                    yield pair
+            elif isinstance(leaf_pair, tuple):
+                for pair in pitchtools.list_ordered_named_pitch_pairs_from_expr_1_to_expr_2(
+                    *leaf_pair):
+                    yield pair
+            else:
+                message = 'leaf pair must be set or tuple.'
+                raise TypeError(message)
+            # iterate chord pitches if last leaf is chord
+            for pair in pitchtools.list_unordered_named_pitch_pairs_in_expr(
+                leaf_pair_list[1]):
+                yield pair
+
     def by_run(self, prototype=None):
         r'''Iterates by run.
 
