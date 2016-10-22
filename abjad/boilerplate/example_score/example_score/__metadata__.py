b4240a0a96f2dd240d504135237bc78e0278c7e8
Trevor Bača <trevor.baca@gmail.com>
2016-10-22 13:03:14 -0500

    CONFIG: Updated vimrc and boilerplate score __metadata__.py file.
        OLD: __metadata__.py started with "from abjad import *".
        NEW: __metadata__.py starts with "import abjad".

--- abjad/boilerplate/example_score/example_score/__metadata__.py
+++ abjad/boilerplate/example_score/example_score/__metadata__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-from abjad import *
+import abjad
 
 
-metadata = datastructuretools.TypedOrderedDict(
+metadata = abjad.datastructuretools.TypedOrderedDict(
     [
         ('title', {score_title!r}),
         ('year', {year}),
