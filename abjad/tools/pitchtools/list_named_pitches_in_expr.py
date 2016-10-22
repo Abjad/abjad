056cc953aa28c56c83450912c50a1d4ada68dabe
Trevor Bača <trevor.baca@gmail.com>
2016-10-22 15:53:55 -0500

    Cleaned up pitchtools.list_named_pitches_in_expr().

--- abjad/tools/pitchtools/list_named_pitches_in_expr.py
+++ abjad/tools/pitchtools/list_named_pitches_in_expr.py
@@ -5,21 +5,25 @@ from abjad.tools.topleveltools import iterate
 def list_named_pitches_in_expr(expr):
     '''Lists named pitches in `expr`.
 
-    ::
+    ..  container:: example
 
-        >>> staff = Staff("c'4 d'4 e'4 f'4")
-        >>> beam = spannertools.Beam()
-        >>> attach(beam, staff[:])
+        **Example 1.** Lists named pitches in staff:
 
-    ::
+        ::
 
-        >>> for x in pitchtools.list_named_pitches_in_expr(beam):
-        ...     x
-        ...
-        NamedPitch("c'")
-        NamedPitch("d'")
-        NamedPitch("e'")
-        NamedPitch("f'")
+            >>> staff = Staff("c'4 d'4 e'4 f'4")
+            >>> beam = spannertools.Beam()
+            >>> attach(beam, staff[:])
+
+        ::
+
+            >>> for x in pitchtools.list_named_pitches_in_expr(beam):
+            ...     x
+            ...
+            NamedPitch("c'")
+            NamedPitch("d'")
+            NamedPitch("e'")
+            NamedPitch("f'")
 
     Returns tuple.
     '''
@@ -27,39 +31,39 @@ def list_named_pitches_in_expr(expr):
     from abjad.tools import scoretools
     from abjad.tools import spannertools
 
-    # TODO: remove try-except
-    try:
+    if isinstance(expr, pitchtools.Pitch):
         result = pitchtools.NamedPitch.from_pitch_carrier(expr)
         return pitchtools.PitchSegment(
             items=(result,),
             item_class=pitchtools.NamedPitch,
             )
-    except (TypeError, ValueError):
-        result = []
-        if hasattr(expr, 'written_pitches'):
-            result.extend(expr.written_pitches)
-        # for pitch arrays
-        elif hasattr(expr, 'pitches'):
-            result.extend(expr.pitches)
-        elif isinstance(expr, spannertools.Spanner):
-            for leaf in expr._get_leaves():
-                if (hasattr(leaf, 'written_pitch') and
-                    not isinstance(leaf, scoretools.Rest)):
-                    result.append(leaf.written_pitch)
-                elif hasattr(leaf, 'written_pitches'):
-                    result.extend(leaf.written_pitches)
-        elif isinstance(expr, pitchtools.PitchSet):
-            result.extend(sorted(list(expr)))
-        elif isinstance(expr, (list, tuple, set)):
-            for x in expr:
-                result.extend(list_named_pitches_in_expr(x))
-        else:
-            for leaf in iterate(expr).by_class(scoretools.Leaf):
-                if hasattr(leaf, 'written_pitch') and not isinstance(leaf, scoretools.Rest):
-                    result.append(leaf.written_pitch)
-                elif hasattr(leaf, 'written_pitches'):
-                    result.extend(leaf.written_pitches)
-        return pitchtools.PitchSegment(
-            items=result,
-            item_class=pitchtools.NamedPitch,
-            )
+        
+    result = []
+    if hasattr(expr, 'written_pitches'):
+        result.extend(expr.written_pitches)
+    # for pitch arrays
+    elif hasattr(expr, 'pitches'):
+        result.extend(expr.pitches)
+    elif isinstance(expr, spannertools.Spanner):
+        for leaf in expr._get_leaves():
+            if (hasattr(leaf, 'written_pitch') and
+                not isinstance(leaf, scoretools.Rest)):
+                result.append(leaf.written_pitch)
+            elif hasattr(leaf, 'written_pitches'):
+                result.extend(leaf.written_pitches)
+    elif isinstance(expr, pitchtools.PitchSet):
+        result.extend(sorted(list(expr)))
+    elif isinstance(expr, (list, tuple, set)):
+        for x in expr:
+            result.extend(list_named_pitches_in_expr(x))
+    else:
+        for leaf in iterate(expr).by_class(scoretools.Leaf):
+            if (hasattr(leaf, 'written_pitch') and
+                not isinstance(leaf, scoretools.Rest)):
+                result.append(leaf.written_pitch)
+            elif hasattr(leaf, 'written_pitches'):
+                result.extend(leaf.written_pitches)
+    return pitchtools.PitchSegment(
+        items=result,
+        item_class=pitchtools.NamedPitch,
+        )
