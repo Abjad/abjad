8cc6cc16e6dfcb6e4ef0ae906d51fc0dc9f97d84
Trevor Bača <trevor.baca@gmail.com>
2016-10-23 13:34:53 -0500

    Deprecated pitchtools.transpose_pitch_number_by_octave_transposition_mapping().
    Use pitchtools.Registration() instead.

--- abjad/tools/pitchtools/test/test_pitchtools_transpose_pitch_number_by_octave_transposition_mapping.py
+++ abjad/tools/pitchtools/test/test_pitchtools_transpose_pitch_number_by_octave_transposition_mapping.py
@@ -2,43 +2,48 @@
 from abjad import *
 
 
+function = pitchtools.transpose_pitch_number_by_octave_transposition_mapping
+
+
 def test_pitchtools_transpose_pitch_number_by_octave_transposition_mapping_01():
-    r'''Send pitch number to octave.
-    '''
 
     mapping = [((-39, -13), 0), ((-12, 23), 12), ((24, 48), 24)]
     pitch_numbers = [-30, -18, -6, 6, 18, 30, 42]
 
-    t = []
+    new_pitch_numbers = []
     for pitch_number in pitch_numbers:
-        t.append(pitchtools.transpose_pitch_number_by_octave_transposition_mapping(pitch_number, mapping))
+        new_pitch_numbers.append(function(pitch_number, mapping))
 
-    assert t == [6, 6, 18, 18, 18, 30, 30]
+    assert new_pitch_numbers == [6, 6, 18, 18, 18, 30, 30]
+
+    # NEW WAY:
+    components = [('(-39, -13)', 0), ('(-12, 23)', 12), ('(24, 48)', 24)]
+    registration = pitchtools.Registration(components)
+    pitch_numbers = [-30, -18, -6, 6, 18, 30, 42]
+    new_pitch_numbers = registration(pitch_numbers)
+
+    assert new_pitch_numbers == [6, 6, 18, 18, 18, 30, 30]
 
 
 def test_pitchtools_transpose_pitch_number_by_octave_transposition_mapping_02():
-    r'''Send pitch number to octave.
-    '''
 
     mapping = [((-39, -1), 0), ((0, 48), 6)]
     pitch_numbers = [-30, -18, -6, 6, 18, 30, 42]
 
-    t = []
+    new_pitch_numbers = []
     for pitch_number in pitch_numbers:
-        t.append(pitchtools.transpose_pitch_number_by_octave_transposition_mapping(pitch_number, mapping))
+        new_pitch_numbers.append(function(pitch_number, mapping))
 
-    assert t == [6, 6, 6, 6, 6, 6, 6]
+    assert new_pitch_numbers == [6, 6, 6, 6, 6, 6, 6]
 
 
 def test_pitchtools_transpose_pitch_number_by_octave_transposition_mapping_03():
-    r'''Send pitch number to octave.
-    '''
 
     mapping = [((-39, -1), 0), ((0, 48), 6)]
     pitch_numbers = [-34, -22, -10, 2, 14, 26, 38]
 
-    t = []
+    new_pitch_numbers = []
     for pitch_number in pitch_numbers:
-        t.append(pitchtools.transpose_pitch_number_by_octave_transposition_mapping(pitch_number, mapping))
+        new_pitch_numbers.append(function(pitch_number, mapping))
 
-    assert t == [2, 2, 2, 14, 14, 14, 14]
+    assert new_pitch_numbers == [2, 2, 2, 14, 14, 14, 14]
