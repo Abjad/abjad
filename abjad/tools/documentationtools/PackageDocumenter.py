d815ba105809383481b96160fd8c35fac2f0a01c
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2017-04-30 23:08:13 -0700

    Refactoring.

--- abjad/tools/documentationtools/PackageDocumenter.py
+++ abjad/tools/documentationtools/PackageDocumenter.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 import enum
+import importlib
 import inspect
 import types
+try:
+    import pathlib
+except ImportError:
+    import pathlib2 as pathlib
 from abjad.tools.documentationtools.Documenter import Documenter
 
 
@@ -14,15 +19,62 @@ class PackageDocumenter(Documenter):
 
     __slots__ = (
         '_members',
+        '_package_path',
         )
 
     ### INITIALIZER ###
 
     def __init__(self, manager, client):
         Documenter.__init__(self, manager, client)
+        package_path = pathlib.Path(self.client.__file__)
+        assert package_path.name == '__init__.py'
+        self._package_path = package_path.parent
         self._members = self._collect_members()
 
     ### PRIVATE METHODS ###
+
+    def _build_submodule_section(self, title, members):
+        from abjad.tools import documentationtools
+        result = []
+        rule = documentationtools.ReSTHorizontalRule()
+        result.append(rule)
+        heading = documentationtools.ReSTHeading(level=3, text=title)
+        result.append(heading)
+        result.extend(self._build_autosummary(members))
+        return result
+
+    def _build_standard_section(self, title, members):
+        from abjad.tools import documentationtools
+        result = []
+        rule = documentationtools.ReSTHorizontalRule()
+        result.append(rule)
+        heading = documentationtools.ReSTHeading(level=3, text=title)
+        result.append(heading)
+        result.extend(self._build_autosummary(members))
+        for member in members:
+            if member.__module__ != self.client.__name__:
+                continue
+            if isinstance(member, type):
+                documenter = documentationtools.ClassDocumenter(
+                    self.manager, member)
+            elif isinstance(member, types.FunctionType):
+                documenter = documentationtools.FunctionDocumenter(
+                    self.manager, member)
+            else:
+                continue
+            result.extend(documenter.build_rst())
+        return result
+
+    def _build_autosummary(self, members):
+        from abjad.tools import documentationtools
+        autosummary = documentationtools.ReSTAutosummaryDirective(
+            options={'nosignatures': True},
+            )
+        for member in members:
+            text = '~{}.{}'.format(member.__module__, member.__name__)
+            item = documentationtools.ReSTAutosummaryItem(text=text)
+            autosummary.append(item)
+        return [autosummary]
 
     def _collect_members(self):
         members = {}
@@ -33,6 +85,10 @@ class PackageDocumenter(Documenter):
             member = self.client.__dict__[name]
             if isinstance(member, type):
                 if member in ignored_classes:
+                    continue
+                module = importlib.import_module(member.__module__)
+                module_path = pathlib.Path(module.__file__)
+                if self.package_path not in module_path.parents:
                     continue
                 section = getattr(member, '__documentation_section__', None)
                 if section is None:
@@ -46,9 +102,16 @@ class PackageDocumenter(Documenter):
                         section = 'Abstract Classes'
                 members.setdefault(section, []).append(member)
             elif isinstance(member, types.FunctionType):
+                module = importlib.import_module(member.__module__)
+                module_path = pathlib.Path(module.__file__)
+                if self.package_path not in module_path.parents:
+                    continue
                 section = 'Functions'
                 members.setdefault(section, []).append(member)
             elif isinstance(member, types.ModuleType):
+                module_path = pathlib.Path(member.__file__)
+                if self.package_path not in module_path.parents:
+                    continue
                 section = 'Submodules'
                 members.setdefault(section, []).append(member)
             else:
@@ -70,4 +133,39 @@ class PackageDocumenter(Documenter):
             directive='automodule',
             )
         document.append(automodule_directive)
+        members = self.members.copy()
+        if 'Submodules' in members:
+            section = self._build_submodule_section(
+                'Submodules', members.pop('Submodules'))
+            document.extend(section)
+        for key in ('Abstract Classes', 'Main Classes'):
+            if key in members:
+                section = self._build_standard_section(key, members.pop(key))
+                document.extend(section)
+        closing_sections = ('Functions', 'Enumerations', 'Errors')
+        for key in sorted(members):
+            if key in closing_sections:
+                continue
+            section = self._build_standard_section(key, members.pop(key))
+            document.extend(section)
+        for key in closing_sections:
+            if key in members:
+                section = self._build_standard_section(key, members.pop(key))
+                document.extend(section)
         return document
+
+    ### PUBLIC PROPERTIES ###
+
+    @property
+    def members(self):
+        """
+        Gets sorted members of documenter's client.
+        """
+        return self._members
+
+    @property
+    def package_path(self):
+        """
+        Gets package path of documenter's client.
+        """
+        return self._package_path
--- abjad/tools/documentationtools/test/test_documentationtools_PackageDocumenter.py
+++ abjad/tools/documentationtools/test/test_documentationtools_PackageDocumenter.py
@@ -15,4 +15,117 @@ class TestCase(systemtools.TestCase):
         rst = documenter.build_rst()
         assert self.normalize("""
             ..  automodule:: abjad.tools.scoretools
+
+            --------
+
+            Abstract Classes
+            ----------------
+
+            ..  autosummary::
+                :nosignatures:
+
+                ~abjad.tools.scoretools.Component.Component
+                ~abjad.tools.scoretools.Leaf.Leaf
+
+            --------
+
+            Containers
+            ----------
+
+            ..  autosummary::
+                :nosignatures:
+
+                ~abjad.tools.scoretools.Cluster.Cluster
+                ~abjad.tools.scoretools.Container.Container
+                ~abjad.tools.scoretools.FixedDurationContainer.FixedDurationContainer
+                ~abjad.tools.scoretools.FixedDurationTuplet.FixedDurationTuplet
+                ~abjad.tools.scoretools.GraceContainer.GraceContainer
+                ~abjad.tools.scoretools.Measure.Measure
+                ~abjad.tools.scoretools.Tuplet.Tuplet
+
+            --------
+
+            Contexts
+            --------
+
+            ..  autosummary::
+                :nosignatures:
+
+                ~abjad.tools.scoretools.Context.Context
+                ~abjad.tools.scoretools.Score.Score
+                ~abjad.tools.scoretools.Staff.Staff
+                ~abjad.tools.scoretools.StaffGroup.StaffGroup
+                ~abjad.tools.scoretools.Voice.Voice
+
+            --------
+
+            Leaves
+            ------
+
+            ..  autosummary::
+                :nosignatures:
+
+                ~abjad.tools.scoretools.Chord.Chord
+                ~abjad.tools.scoretools.MultimeasureRest.MultimeasureRest
+                ~abjad.tools.scoretools.Note.Note
+                ~abjad.tools.scoretools.Rest.Rest
+                ~abjad.tools.scoretools.Skip.Skip
+
+            --------
+
+            Note heads
+            ----------
+
+            ..  autosummary::
+                :nosignatures:
+
+                ~abjad.tools.scoretools.DrumNoteHead.DrumNoteHead
+                ~abjad.tools.scoretools.NoteHead.NoteHead
+                ~abjad.tools.scoretools.NoteHeadList.NoteHeadList
+
+            --------
+
+            Functions
+            ---------
+
+            ..  autosummary::
+                :nosignatures:
+
+                ~abjad.tools.scoretools.append_spacer_skip_to_underfull_measure.append_spacer_skip_to_underfull_measure
+                ~abjad.tools.scoretools.append_spacer_skips_to_underfull_measures.append_spacer_skips_to_underfull_measures
+                ~abjad.tools.scoretools.apply_full_measure_tuplets_to_contents_of_measures.apply_full_measure_tuplets_to_contents_of_measures
+                ~abjad.tools.scoretools.extend_measures_and_apply_full_measure_tuplets.extend_measures_and_apply_full_measure_tuplets
+                ~abjad.tools.scoretools.fill_measures_with_full_measure_spacer_skips.fill_measures_with_full_measure_spacer_skips
+                ~abjad.tools.scoretools.fill_measures_with_minimal_number_of_notes.fill_measures_with_minimal_number_of_notes
+                ~abjad.tools.scoretools.fill_measures_with_repeated_notes.fill_measures_with_repeated_notes
+                ~abjad.tools.scoretools.fill_measures_with_time_signature_denominator_notes.fill_measures_with_time_signature_denominator_notes
+                ~abjad.tools.scoretools.get_measure.get_measure
+                ~abjad.tools.scoretools.get_measure_that_starts_with_container.get_measure_that_starts_with_container
+                ~abjad.tools.scoretools.get_measure_that_stops_with_container.get_measure_that_stops_with_container
+                ~abjad.tools.scoretools.get_next_measure_from_component.get_next_measure_from_component
+                ~abjad.tools.scoretools.get_previous_measure_from_component.get_previous_measure_from_component
+                ~abjad.tools.scoretools.make_empty_piano_score.make_empty_piano_score
+                ~abjad.tools.scoretools.make_leaves.make_leaves
+                ~abjad.tools.scoretools.make_leaves_from_talea.make_leaves_from_talea
+                ~abjad.tools.scoretools.make_multimeasure_rests.make_multimeasure_rests
+                ~abjad.tools.scoretools.make_multiplied_quarter_notes.make_multiplied_quarter_notes
+                ~abjad.tools.scoretools.make_notes.make_notes
+                ~abjad.tools.scoretools.make_notes_with_multiplied_durations.make_notes_with_multiplied_durations
+                ~abjad.tools.scoretools.make_percussion_note.make_percussion_note
+                ~abjad.tools.scoretools.make_piano_score_from_leaves.make_piano_score_from_leaves
+                ~abjad.tools.scoretools.make_piano_sketch_score_from_leaves.make_piano_sketch_score_from_leaves
+                ~abjad.tools.scoretools.make_repeated_notes.make_repeated_notes
+                ~abjad.tools.scoretools.make_repeated_notes_from_time_signature.make_repeated_notes_from_time_signature
+                ~abjad.tools.scoretools.make_repeated_notes_from_time_signatures.make_repeated_notes_from_time_signatures
+                ~abjad.tools.scoretools.make_repeated_notes_with_shorter_notes_at_end.make_repeated_notes_with_shorter_notes_at_end
+                ~abjad.tools.scoretools.make_repeated_rests_from_time_signatures.make_repeated_rests_from_time_signatures
+                ~abjad.tools.scoretools.make_repeated_skips_from_time_signatures.make_repeated_skips_from_time_signatures
+                ~abjad.tools.scoretools.make_rests.make_rests
+                ~abjad.tools.scoretools.make_skips.make_skips
+                ~abjad.tools.scoretools.make_spacer_skip_measures.make_spacer_skip_measures
+                ~abjad.tools.scoretools.make_tied_leaf.make_tied_leaf
+                ~abjad.tools.scoretools.move_full_measure_tuplet_prolation_to_measure_time_signature.move_full_measure_tuplet_prolation_to_measure_time_signature
+                ~abjad.tools.scoretools.move_measure_prolation_to_full_measure_tuplet.move_measure_prolation_to_full_measure_tuplet
+                ~abjad.tools.scoretools.scale_measure_denominator_and_adjust_measure_contents.scale_measure_denominator_and_adjust_measure_contents
+                ~abjad.tools.scoretools.set_measure_denominator_and_adjust_numerator.set_measure_denominator_and_adjust_numerator
         """) + '\n' == rst.rest_format
