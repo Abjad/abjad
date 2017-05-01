0a82e0e98c22e9ce3b7993bffa37d8656948b330
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2017-04-30 19:47:49 -0700

    Refactoring.

--- abjad/tools/documentationtools/ClassDocumenter.py
+++ abjad/tools/documentationtools/ClassDocumenter.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
+import importlib
 import inspect
+import traceback
 from abjad.tools import abctools
 
 
@@ -12,27 +14,120 @@ class ClassDocumenter(abctools.AbjadObject):
 
     __slots__ = (
         '_attributes',
-        '_class',
+        '_client',
         '_manager',
         )
 
     ### INITIALIZER ###
 
-    def __init__(self, manager, class_):
+    def __init__(self, manager, client):
         self._manager = manager
-        self._class = class_
+        self._client = client
         self._attributes = self._collect_class_attributes()
 
     ### PRIVATE METHODS ###
 
+    def _build_bases_section_rst(self):
+        from abjad.tools import documentationtools
+        result = []
+        heading = documentationtools.ReSTHeading(level=3, text='Bases')
+        result.append(heading)
+        mro = inspect.getmro(self.client)[1:]
+        for self.client in mro:
+            parts = self.client.__module__.split('.') + [self.client.__name__]
+            while 1 < len(parts) and parts[-1] == parts[-2]:
+                parts.pop()
+            packagesystem_path = '.'.join(parts)
+            text = '- :py:class:`{}`'.format(packagesystem_path)
+            paragraph = documentationtools.ReSTParagraph(
+                text=text,
+                wrap=False,
+                )
+            result.append(paragraph)
+        return result
+
+    def _build_lineage_graph(self):
+        def get_node_name(original_name):
+            parts = original_name.split('.')
+            name = [parts[0]]
+            for part in parts[1:]:
+                if part != name[-1]:
+                    name.append(part)
+            if name[0] in ('abjad', 'experimental', 'ide'):
+                return str('.'.join(name[2:]))
+            return str('.'.join(name))
+        from abjad.tools import documentationtools
+        module_name, _, class_name = self.client.__module__.rpartition('.')
+        node_name = '{}.{}'.format(self.client.__module__, self.client.__name__)
+        importlib.import_module(module_name)
+        lineage_graph_addresses = self.manager._get_lineage_graph_addresses()
+        lineage = documentationtools.InheritanceGraph(
+            addresses=lineage_graph_addresses,
+            lineage_addresses=((module_name, class_name),)
+            )
+        graph = lineage.__graph__()
+        maximum_node_count = 30
+        if maximum_node_count < len(graph.leaves):
+            lineage = documentationtools.InheritanceGraph(
+                addresses=lineage_graph_addresses,
+                lineage_addresses=((module_name, class_name),),
+                lineage_prune_distance=2,
+                )
+            graph = lineage.__graph__()
+        if maximum_node_count < len(graph.leaves):
+            lineage = documentationtools.InheritanceGraph(
+                addresses=lineage_graph_addresses,
+                lineage_addresses=((module_name, class_name),),
+                lineage_prune_distance=1,
+                )
+            graph = lineage.__graph__()
+        if maximum_node_count < len(graph.leaves):
+            lineage = documentationtools.InheritanceGraph(
+                addresses=((module_name, class_name),),
+                )
+            graph = lineage.__graph__()
+            graph_node = graph[node_name]
+            graph_node.attributes['color'] = 'black'
+            graph_node.attributes['fontcolor'] = 'white'
+            graph_node.attributes['style'] = ('filled', 'rounded')
+        graph_node = graph[node_name]
+        graph_node.attributes['label'] = \
+            '<<B>{}</B>>'.format(graph_node.attributes['label'])
+        return graph
+
+    def _build_lineage_section_rst(self):
+        from abjad.tools import documentationtools
+        result = []
+        try:
+            lineage_heading = documentationtools.ReSTHeading(
+                level=3,
+                text='Lineage',
+                )
+            result.append(lineage_heading)
+            lineage_graph = self._build_lineage_graph()
+            lineage_graph.attributes['background'] = 'transparent'
+            lineage_graph.attributes['rankdir'] = 'LR'
+            graphviz_directive = \
+                documentationtools.ReSTGraphvizDirective(
+                    graph=lineage_graph,
+                    )
+            graphviz_container = documentationtools.ReSTDirective(
+                directive='container',
+                argument='graphviz',
+                )
+            graphviz_container.append(graphviz_directive)
+        except:
+            traceback.print_exc()
+        return result
+
     def _collect_class_attributes(self):
         attributes = {}
-        for attr in inspect.classify_class_attrs(self.class_):
+        for attr in inspect.classify_class_attrs(self.client):
             if attr.defining_class is object:
                 continue
             elif attr.name in self.manager.ignored_special_methods:
                 continue
-            if attr.defining_class is not self.class_:
+            if attr.defining_class is not self.client:
                 attributes.setdefault('inherited_attributes', []).append(attr)
             if attr.kind == 'method':
                 if attr.name.startswith('__'):
@@ -59,7 +154,7 @@ class ClassDocumenter(abctools.AbjadObject):
             elif (
                 attr.kind == 'data' and
                 not attr.name.startswith('_') and
-                attr.name not in getattr(self.class_, '__slots__', ())
+                attr.name not in getattr(self.client, '__slots__', ())
                 ):
                 attributes.setdefault('data', []).append(attr)
         for key, value in attributes.items():
@@ -74,8 +169,8 @@ class ClassDocumenter(abctools.AbjadObject):
         """
         from abjad.tools import documentationtools
         manager = documentationtools.DocumentationManager()
-        module_name, _, class_name = self.class_.__module__.rpartition('.')
-        tools_package_python_path = '.'.join(self.class_.__module__.split('.')[:-1])
+        module_name, _, class_name = self.client.__module__.rpartition('.')
+        tools_package_python_path = '.'.join(self.client.__module__.split('.')[:-1])
         document = documentationtools.ReSTDocument()
         module_directive = documentationtools.ReSTDirective(
             directive='currentmodule',
@@ -85,19 +180,19 @@ class ClassDocumenter(abctools.AbjadObject):
         heading = documentationtools.ReSTHeading(level=2, text=class_name)
         document.append(heading)
         autoclass_directive = documentationtools.ReSTAutodocDirective(
-            argument=self.class_.__name__,
+            argument=self.client.__name__,
             directive='autoclass',
             )
         document.append(autoclass_directive)
-        document.extend(manager._build_class_lineage_section_rst(self.class_))
-        document.extend(manager._build_class_bases_section_rst(self.class_))
-        document.extend(manager._build_class_enumeration_section_rst(self.class_))
-        document.extend(manager._build_class_attributes_autosummary_rst(self.class_, self.attributes))
-        document.extend(manager._build_class_readonly_properties_section_rst(self.class_, self.attributes))
-        document.extend(manager._build_class_readwrite_properties_section_rst(self.class_, self.attributes))
-        document.extend(manager._build_class_methods_section_rst(self.class_, self.attributes))
-        document.extend(manager._build_class_classmethod_and_staticmethod_section_rst(self.class_, self.attributes))
-        document.extend(manager._build_class_special_methods_section_rst(self.class_, self.attributes))
+        document.extend(self._build_lineage_section_rst())
+        document.extend(self._build_bases_section_rst())
+        document.extend(manager._build_class_enumeration_section_rst(self.client))
+        document.extend(manager._build_class_attributes_autosummary_rst(self.client, self.attributes))
+        document.extend(manager._build_class_readonly_properties_section_rst(self.client, self.attributes))
+        document.extend(manager._build_class_readwrite_properties_section_rst(self.client, self.attributes))
+        document.extend(manager._build_class_methods_section_rst(self.client, self.attributes))
+        document.extend(manager._build_class_classmethod_and_staticmethod_section_rst(self.client, self.attributes))
+        document.extend(manager._build_class_special_methods_section_rst(self.client, self.attributes))
         return document
 
     ### PUBLIC PROPERTIES ###
@@ -110,11 +205,11 @@ class ClassDocumenter(abctools.AbjadObject):
         return self._attributes
 
     @property
-    def class_(self):
+    def client(self):
         """
-        Gets class_ of documenter.
+        Gets client of documenter.
         """
-        return self._class
+        return self._client
 
     @property
     def manager(self):
--- abjad/tools/documentationtools/DocumentationManager.py
+++ abjad/tools/documentationtools/DocumentationManager.py
@@ -5,7 +5,6 @@ import inspect
 import importlib
 import os
 import shutil
-import traceback
 import types
 from abjad.tools import abctools
 from abjad.tools import systemtools
@@ -152,39 +151,12 @@ class DocumentationManager(abctools.AbjadObject):
         heading = documentationtools.ReSTHeading(level=3, text=text)
         result.append(heading)
         for item in items:
-            name = item.name
-            value = item.value
-            line = '- ``{}``: {}'.format(name, value)
+            line = '- ``{}``: {}'.format(item.name, item.value)
             paragraph = documentationtools.ReSTParagraph(
                 text=line,
                 wrap=False,
                 )
             result.append(paragraph)
-        return result
-
-    def _build_class_lineage_section_rst(self, cls):
-        from abjad.tools import documentationtools
-        result = []
-        try:
-            lineage_heading = documentationtools.ReSTHeading(
-                level=3,
-                text='Lineage',
-                )
-            result.append(lineage_heading)
-            lineage_graph = self._build_lineage_graph(cls)
-            lineage_graph.attributes['background'] = 'transparent'
-            lineage_graph.attributes['rankdir'] = 'LR'
-            graphviz_directive = \
-                documentationtools.ReSTGraphvizDirective(
-                    graph=lineage_graph,
-                    )
-            graphviz_container = documentationtools.ReSTDirective(
-                directive='container',
-                argument='graphviz',
-                )
-            graphviz_container.append(graphviz_directive)
-        except:
-            traceback.print_exc()
         return result
 
     def _ensure_directory(self, path):
@@ -317,61 +289,6 @@ class DocumentationManager(abctools.AbjadObject):
             ])
         return ignored_classes
 
-    def _build_lineage_graph(self, cls):
-        def get_node_name(original_name):
-            parts = original_name.split('.')
-            name = [parts[0]]
-            for part in parts[1:]:
-                if part != name[-1]:
-                    name.append(part)
-            if name[0] in ('abjad', 'experimental', 'ide'):
-                return str('.'.join(name[2:]))
-            return str('.'.join(name))
-        from abjad.tools import documentationtools
-        module_name, _, class_name = cls.__module__.rpartition('.')
-        node_name = '{}.{}'.format(cls.__module__, cls.__name__)
-        importlib.import_module(module_name)
-        lineage_graph_addresses = self._get_lineage_graph_addresses()
-        lineage = documentationtools.InheritanceGraph(
-            addresses=lineage_graph_addresses,
-            lineage_addresses=((module_name, class_name),)
-            )
-        graph = lineage.__graph__()
-        maximum_node_count = 30
-        if maximum_node_count < len(graph.leaves):
-            lineage = documentationtools.InheritanceGraph(
-                addresses=lineage_graph_addresses,
-                lineage_addresses=((module_name, class_name),),
-                lineage_prune_distance=2,
-                )
-            graph = lineage.__graph__()
-        if maximum_node_count < len(graph.leaves):
-            lineage = documentationtools.InheritanceGraph(
-                addresses=lineage_graph_addresses,
-                lineage_addresses=((module_name, class_name),),
-                lineage_prune_distance=1,
-                )
-            graph = lineage.__graph__()
-        if maximum_node_count < len(graph.leaves):
-            lineage = documentationtools.InheritanceGraph(
-                addresses=((module_name, class_name),),
-                )
-            graph = lineage.__graph__()
-            graph_node = graph[node_name]
-            graph_node.attributes['color'] = 'black'
-            graph_node.attributes['fontcolor'] = 'white'
-            graph_node.attributes['style'] = ('filled', 'rounded')
-        graph_node = graph[node_name]
-        graph_node.attributes['label'] = \
-            '<<B>{}</B>>'.format(graph_node.attributes['label'])
-        return graph
-
-    def _get_lineage_graph_addresses(self):
-        lineage_graph_addresses = set(self.lineage_graph_addresses)
-        lineage_graph_addresses.add(self.root_package_name)
-        lineage_graph_addresses = sorted(lineage_graph_addresses)
-        return lineage_graph_addresses
-
     def _get_root_module(self):
         root_module = importlib.import_module(self.root_package_name)
         return root_module
@@ -413,6 +330,12 @@ class DocumentationManager(abctools.AbjadObject):
         functions.sort(key=lambda x: x.__name__)
         functions = tuple(functions)
         return classes, functions
+
+    def _get_lineage_graph_addresses(self):
+        lineage_graph_addresses = set(self.lineage_graph_addresses)
+        lineage_graph_addresses.add(self.root_package_name)
+        lineage_graph_addresses = sorted(lineage_graph_addresses)
+        return lineage_graph_addresses
 
     def _get_tools_package_graph(self, tools_package):
         from abjad.tools import documentationtools
