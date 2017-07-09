2443ac04551dc2e2b7ca2ae32ef7211aebbaef94
Trevor Bača <trevor.baca@gmail.com>
2017-07-09 10:40:36 -0500

    (closes #678) Refactored Ferneyhough demo as a class.

--- abjad/demos/ferneyhough/main.py
+++ abjad/demos/ferneyhough/main.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 #! /usr/bin/env python
+import abjad
+import sys
 
 
 if __name__ == '__main__':
-    import aabjad
-    import sys
 
     arguments = sys.argv[1:]
     if len(arguments) != 3:
-        print('USAGE: [tuplet_duration, row_count, column_count]:')
-        print('\ti.e.: /main.py 1/4 11 6')
+        print('USAGE: tuplet_duration row_count column_count')
+        print(' e.g.: python main.py 1/4 11 6')
         sys.exit(0)
 
     tuplet_duration = abjad.Duration(arguments[0])
@@ -20,9 +20,10 @@ if __name__ == '__main__':
     assert 0 < row_count
     assert 0 < column_count
 
-    lilypond_file = abjad.demos.ferneyhough.make_lilypond_file(
-        tuplet_duration,
-        row_count,
-        column_count,
+    demo = abjad.demos.ferneyhough.FerneyhoughDemo()
+    lilypond_file = demo(
+        tuplet_duration=tuplet_duration,
+        row_count=row_count,
+        column_count=column_count,
         )
     abjad.show(lilypond_file)
--- abjad/demos/ferneyhough/test/test_demos_ferneyhough.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from abjad import *
-from abjad.demos.ferneyhough.make_lilypond_file import make_lilypond_file
-
-
-def test_demos_ferneyhough_01():
-
-    lilypond_file = make_lilypond_file(Duration(1, 4), 10, 6)
