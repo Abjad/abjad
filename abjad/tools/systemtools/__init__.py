492a9cadc9769670278e90a3b98fde28fcba963e
Sahin Kureta <skureta@gmail.com>
2017-12-15 18:22:49 +0300

    added ImportManager to __init__.py

--- abjad/tools/systemtools/__init__.py
+++ abjad/tools/systemtools/__init__.py
@@ -5,6 +5,7 @@ from .FilesystemState import FilesystemState
 from .ForbidUpdate import ForbidUpdate
 from .FormatSpecification import FormatSpecification
 from .IOManager import IOManager
+from .ImportManager import ImportManager
 from .IndicatorWrapper import IndicatorWrapper
 from .LilyPondFormatBundle import LilyPondFormatBundle
 from .LilyPondFormatManager import LilyPondFormatManager
