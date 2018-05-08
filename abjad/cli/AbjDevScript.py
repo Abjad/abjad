01d310b894b768b5876fa29f5c7eecef877b3785
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2018-05-08 12:35:23 -0400

    Teach AbjDevScript to look for extensions.

--- abjad/cli/AbjDevScript.py
+++ abjad/cli/AbjDevScript.py
@@ -32,20 +32,37 @@ class AbjDevScript(CLIAggregator):
         """
         Lists CLI classes for aggregation.
         """
-        import abjad.book
+        def scan_module(module):
+            classes = []
+            for name in dir(module):
+                obj = getattr(module, name)
+                if not isinstance(obj, type):
+                    continue
+                elif not issubclass(obj, CLI):
+                    continue
+                elif issubclass(obj, type(self)):
+                    continue
+                elif inspect.isabstract(obj):
+                    continue
+                classes.append(obj)
+            return classes
         import abjad.cli
-        classes = []
-        for name in sorted(dir(abjad.cli)):
-            obj = getattr(abjad.cli, name)
-            if not isinstance(obj, type):
-                continue
-            elif not issubclass(obj, CLI):
-                continue
-            elif issubclass(obj, type(self)):
-                continue
-            elif inspect.isabstract(obj):
-                continue
-            classes.append(obj)
-        classes.append(abjad.book.AbjadBookScript)
+        classes = scan_module(abjad.cli)
+        # TODO: Remove this once abjad.book is externalized as abjadext.book:
+        try:
+            import abjad.book
+            classes.append(abjad.book.AbjadBookScript)
+        except ImportError:
+            pass
+        try:
+            import abjadext.book
+            classes.extend(scan_module(abjadext.book))
+        except ImportError:
+            pass
+        try:
+            import abjadext.cli
+            classes.extend(scan_module(abjadext.cli))
+        except ImportError:
+            pass
         classes.sort(key=lambda x: x.__name__)
         return classes
