2cd7cae826e0cd3a5a49028c5412b0975ef5486c
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2018-05-12 18:24:20 -0400

    Move enums in enumerations.py

--- abjad/__init__.py
+++ abjad/__init__.py
@@ -4,118 +4,20 @@ if 'topleveltools' in os.path.abspath('.'):
     raise Exception(message)
 del os
 
-
 from fractions import Fraction  # noqa
 try:
     from quicktions import Fraction  # type: ignore
 except ImportError:
     pass
 
-
-import uqbar.enums  # noqa
-
-
-class Comparison(uqbar.enums.StrictEnumeration):
-    """
-    Enumeration of amount comparisons.
-    """
-    Less = -1
-    Exact = 0
-    More = 1
-
-    def __format__(self, format_spec):
-        return repr(self)
-
-    def __repr__(self):
-        return self.name
-
-    def __str__(self):
-        return self.name
-
-
-Less = Comparison.Less
-Exact = Comparison.Exact
-More = Comparison.More
-
-
-class HorizontalAlignment(uqbar.enums.StrictEnumeration):
-    """
-    Enumeration of horizontal alignments.
-    """
-    Left = -1
-    Both = 0
-    Right = 1
-
-    def __repr__(self):
-        return self.name
-
-    def __str__(self):
-        return self.name.lower()
-
-
-Left = HorizontalAlignment.Left
-Both = HorizontalAlignment.Both
-Right = HorizontalAlignment.Right
-
-
-class VerticalAlignment(uqbar.enums.StrictEnumeration):
-    """
-    Enumeration of vertical alignments.
-    """
-    Down = -1
-    Center = 0
-    Up = 1
-    Default = 0  # aliases Center
-    Neutral = 0  # aliases Center
-
-    def __format__(self, format_spec):
-        """
-        Formats vertical alignment as ``storage`` or ``lilypond``.
-        """
-        if format_spec == 'lilypond':
-            return {
-                self.Down: '_',
-                self.Up: '^',
-                self.Center: '-',
-            }[self]
-        return repr(self)
-
-    def __repr__(self):
-        return self.name
-
-    def __str__(self):
-        return self.name.lower()
-
-    @classmethod
-    def from_expr(cls, expr):
-        lilypond_symbols = {
-            '^': cls.Up,
-            '-': cls.Center,
-            '_': cls.Down,
-        }
-        result = lilypond_symbols.get(expr)
-        if result is not None:
-            return result
-        return super().from_expr(expr)
-
-
-Down = VerticalAlignment.Down
-Center = VerticalAlignment.Center
-Up = VerticalAlignment.Up
-
-
-del uqbar.enums
-del uqbar
-
+from abjad.enumerations import *
+from abjad.exceptions import *
 
 # ensure that the ~/.abjad directory and friends are setup
 # and instantiate Abjad's configuration singleton
 from abjad.tools.systemtools.AbjadConfiguration import AbjadConfiguration
 abjad_configuration = AbjadConfiguration()
 del AbjadConfiguration
-
-
-from abjad.exceptions import *
 
 # import all tools packages
 from abjad.tools import *
@@ -160,7 +62,7 @@ silence = SilenceMask.silence
 sustain = SustainMask.sustain
 
 # import version information
-from abjad._version import __version_info__, __version__
+from ._version import __version_info__, __version__
 try:
     del _version
 except NameError:
---
+++ abjad/enumerations.py
@@ -0,0 +1,102 @@
+import uqbar.enums
+
+
+class Comparison(uqbar.enums.StrictEnumeration):
+    """
+    Enumeration of amount comparisons.
+    """
+    Less = -1
+    Exact = 0
+    More = 1
+
+    def __format__(self, format_spec):
+        return repr(self)
+
+    def __repr__(self):
+        return self.name
+
+    def __str__(self):
+        return self.name
+
+
+class HorizontalAlignment(uqbar.enums.StrictEnumeration):
+    """
+    Enumeration of horizontal alignments.
+    """
+    Left = -1
+    Both = 0
+    Right = 1
+
+    def __repr__(self):
+        return self.name
+
+    def __str__(self):
+        return self.name.lower()
+
+
+class VerticalAlignment(uqbar.enums.StrictEnumeration):
+    """
+    Enumeration of vertical alignments.
+    """
+    Down = -1
+    Center = 0
+    Up = 1
+    Default = 0  # aliases Center
+    Neutral = 0  # aliases Center
+
+    def __format__(self, format_spec):
+        """
+        Formats vertical alignment as ``storage`` or ``lilypond``.
+        """
+        if format_spec == 'lilypond':
+            return {
+                self.Down: '_',
+                self.Up: '^',
+                self.Center: '-',
+            }[self]
+        return repr(self)
+
+    def __repr__(self):
+        return self.name
+
+    def __str__(self):
+        return self.name.lower()
+
+    @classmethod
+    def from_expr(cls, expr):
+        lilypond_symbols = {
+            '^': cls.Up,
+            '-': cls.Center,
+            '_': cls.Down,
+        }
+        result = lilypond_symbols.get(expr)
+        if result is not None:
+            return result
+        return super().from_expr(expr)
+
+
+Both = HorizontalAlignment.Both
+Center = VerticalAlignment.Center
+Down = VerticalAlignment.Down
+Exact = Comparison.Exact
+Left = HorizontalAlignment.Left
+Less = Comparison.Less
+More = Comparison.More
+Right = HorizontalAlignment.Right
+Up = VerticalAlignment.Up
+
+
+__all__ = [
+    'Both',
+    'Center',
+    'Comparison',
+    'Down',
+    'Exact',
+    'HorizontalAlignment',
+    'Left',
+    'Less',
+    'More',
+    'Right',
+    'Up',
+    'VerticalAlignment',
+    ]
