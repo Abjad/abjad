8f87d2772f413bf19d131e0b5911e1328f3b4e03
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2018-06-16 14:29:26 -0400

    Collapse cli/ into cli.py

--- abjad/cli/__init__.py
+++
@@ -1,7 +0,0 @@
-from .AbjDevScript import AbjDevScript
-from .CleanScript import CleanScript
-from .ReplaceScript import ReplaceScript
-from .StatsScript import StatsScript
-from .run_ajv import run_ajv
-
-_documentation_section = 'internals'
--- abjad/cli/test/test_cli_AbjDevScript___init__.py
+++
@@ -1,5 +0,0 @@
-import abjad
-
-
-def test_cli_AbjDevScript___init___01():
-    abjad.cli.AbjDevScript()
