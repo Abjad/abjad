bd9244b4ed38600bf73eb15bf72510899498afcf
Tsz Kiu Pang <38950857+nivlekp@users.noreply.github.com>
2021-02-25 02:38:27 +1100

    taught iterpitches about trill spans (#1319)

---
+++ tests/test_iterpitches_transpose_from_sounding_pitch.py
@@ -0,0 +1,30 @@
+import abjad
+
+
+def test_transpose_from_sounding_pitch_01():
+    staff = abjad.Staff("c'4 c'4 c'4 c'4")
+    trill_pitch = abjad.NamedPitch("d'")
+    start_trill_span = abjad.StartTrillSpan(pitch=trill_pitch)
+    abjad.attach(start_trill_span, staff[1])
+    stop_trill_span = abjad.StopTrillSpan()
+    abjad.attach(stop_trill_span, staff[2])
+    instrument = abjad.AltoSaxophone()
+    abjad.attach(instrument, staff[0])
+
+    abjad.iterpitches.transpose_from_sounding_pitch(staff)
+
+    string = abjad.lilypond(staff)
+    assert string == abjad.String.normalize(
+        r"""
+        \new Staff
+        {
+            a'4
+            \pitchedTrill
+            a'4
+            \startTrillSpan b'
+            a'4
+            \stopTrillSpan
+            a'4
+        }
+        """
+    )
