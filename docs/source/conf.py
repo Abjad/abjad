e56df5bb53394a076282f335bd1dd912c392b154
Trevor Bača <trevor.baca@gmail.com>
2022-09-10 11:24:56 -0400

    Bumped to Abjad 3.11. (#1473)
    Abjad 3.11 is a maintenance release that largely preserves the functionality of Abjad 3.10 unchanged.
    
    * Cleaned up abjad.rhythmtrees.parse_rtm_syntax().
    
    * Cleaned up abjad.mutate.eject_contents().

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,7 +39,7 @@ html_theme_options = {
     # navigation_depth=1 makes sidebar completely flat;
     # leave flat navigation in place forever:
     "navigation_depth": 1,
-    "style_nav_header_background": "#4488cc",
+    "style_nav_header_background": "#55aaff",
 }
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 project = "Abjad"
