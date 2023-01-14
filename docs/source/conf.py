96c14ad796e59f293b8f1c716f444340c3105ad7
Trevor Bača <trevor.baca@gmail.com>
2023-01-14 13:58:54 -0500

    Added Gilberto's scores to score gallery. (#1508)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -47,6 +47,7 @@ release = abjad.__version__
 rst_epilog = """
 ..  _Bača: https://github.com/trevorbaca
 ..  _Fredrik Wallberg: http://quesebifurcan.github.io/music
+..  _Gilberto Agostinho: https://soundcloud.com/gilberto-agostinho
 ..  _Jeffrey Treviño: https://soundcloud.com/jefftrevino
 ..  _Josiah Wolf Oberholtzer: http://josiahwolfoberholtzer.com
 ..  _Gregory Rowland Evans: http://www.gregoryrowlandevans.com
