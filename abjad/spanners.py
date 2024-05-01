745e4bc0454f1b47c63967ca1ec4a4d3ba5812e5
Trevor Bača <trevor.baca@gmail.com>
2024-05-01 15:27:51 -0400

    Cleaned up indexed tweaks.

--- abjad/spanners.py
+++ abjad/spanners.py
@@ -34,12 +34,7 @@ def _apply_tweaks(argument, tweaks, i=None, total=None):
             if index < 0 and index != -(total - i):
                 continue
         elif isinstance(item, tuple):
-            assert len(item) == 2
-            item, index = item
-            if 0 <= index and index != i:
-                continue
-            if index < 0 and index != -(total - i):
-                continue
+            raise Exception(f"use abjad.Tweak.i instead of tuple: {item}")
         assert isinstance(item, _tweaks.Tweak), repr(item)
         tweak_objects.append(item)
     bundle = _tweaks.bundle(argument, *tweak_objects)
@@ -988,8 +983,8 @@ def glissando(
         >>> voice = abjad.Voice("d'4 d' d' d'", name="Voice")
         >>> abjad.glissando(
         ...     voice[:],
-        ...     (abjad.Tweak(r"- \tweak color #red"), 0),
-        ...     (abjad.Tweak(r"- \tweak color #red"), -1),
+        ...     abjad.Tweak(r"- \tweak color #red", i=0),
+        ...     abjad.Tweak(r"- \tweak color #red", i=-1),
         ...     allow_repeats=True,
         ...     zero_padding=True,
         ... )
