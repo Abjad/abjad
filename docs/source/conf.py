fbd6f37089f95dab3fbc89153c3f063115fd8d0a
Trevor Bača <trevor.baca@gmail.com>
2025-04-04 16:30:56 -0400

    Bump Abjad from 3.22 to 3.23

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -45,7 +45,7 @@ html_theme_options = {
     "navigation_depth": 1,
     "sticky_navigation": False,
     "style_external_links": True,
-    "style_nav_header_background": "#000000",
+    "style_nav_header_background": "#332211",
 }
 
 intersphinx_mapping = {
